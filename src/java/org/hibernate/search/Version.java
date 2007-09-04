4f12a95d45b3f2d5363aba285ac496eaff18f85d
Emmanuel Bernard <emmanuel@hibernate.org>
2007-09-04 04:03:11 +0000

    Release
    git-svn-id: https://svn.jboss.org/repos/hibernate/search/trunk@13995 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- src/java/org/hibernate/search/Version.java
+++ src/java/org/hibernate/search/Version.java
@@ -10,7 +10,7 @@ import org.apache.commons.logging.LogFactory;
  * @author Emmanuel Bernard
  */
 public class Version {
-	public static final String VERSION = "3.0.0.Beta4"; // + new Date();
+	public static final String VERSION = "3.0.0.CR1"; // + new Date();
 	private static Log log = LogFactory.getLog( Version.class );
 
 	static {
