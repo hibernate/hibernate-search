b12f73df8bbcffe82ec56afef48a13d0ebbf8783
Emmanuel Bernard <emmanuel@hibernate.org>
2008-03-20 19:01:16 +0000

    bump version
    git-svn-id: https://svn.jboss.org/repos/hibernate/search/trunk@14473 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- src/java/org/hibernate/search/Version.java
+++ src/java/org/hibernate/search/Version.java
@@ -10,7 +10,7 @@ import org.apache.commons.logging.LogFactory;
  * @author Emmanuel Bernard
  */
 public class Version {
-	public static final String VERSION = "3.0.1.GA"; // + new Date();
+	public static final String VERSION = "3.1.x." + new Date();
 	private static Log log = LogFactory.getLog( Version.class );
 
 	static {
