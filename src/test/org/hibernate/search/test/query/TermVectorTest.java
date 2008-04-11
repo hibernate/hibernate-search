5b7e65e211c5cb2a2052f4359e89ed7fcc486a43
Emmanuel Bernard <emmanuel@hibernate.org>
2008-04-11 16:56:02 +0000

    HSEARCH-141 doc (John Griffin)
    git-svn-id: https://svn.jboss.org/repos/hibernate/search/trunk@14505 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- src/test/org/hibernate/search/test/query/TermVectorTest.java
+++ src/test/org/hibernate/search/test/query/TermVectorTest.java
@@ -17,100 +17,92 @@ import org.hibernate.search.test.SearchTestCase;
  * @author John Griffin
  */
 public class TermVectorTest extends SearchTestCase {
-	private static Log log = LogFactory.getLog( TermVectorTest.class );
+   private static Log log = LogFactory.getLog(TermVectorTest.class);
 
-	public void testPositionOffsets() throws Exception {
-		FullTextSession s = Search.createFullTextSession( openSession() );
-		createIndex( s );
+   public void testPositionOffsets() throws Exception {
+      FullTextSession s = Search.createFullTextSession(openSession());
+      createIndex(s);
 
-		s.clear();
-		Transaction tx = s.beginTransaction();
+      s.clear();
+      Transaction tx = s.beginTransaction();
 
-		// Here's how to get a reader from a FullTextSession
-		SearchFactory searchFactory = s.getSearchFactory();
-		DirectoryProvider provider = searchFactory.getDirectoryProviders( ElectricalProperties.class )[0];
-		ReaderProvider readerProvider = searchFactory.getReaderProvider();
-		IndexReader reader = readerProvider.openReader( provider );
+      // Here's how to get a reader from a FullTextSession
+      SearchFactory searchFactory = s.getSearchFactory();
+      DirectoryProvider provider = searchFactory.getDirectoryProviders(ElectricalProperties.class)[0];
+      ReaderProvider readerProvider = searchFactory.getReaderProvider();
+      IndexReader reader = readerProvider.openReader(provider);
 
-		/**
-		 * Since there are so many combinations of results here, rather
-		 * than try to do assertions, this test prints out all the results
-		 * found from the three ElectricalProperties entities. This will
-		 * do a better  job of demonstrating exactly what the result are. - J.G.
-		 */
-		///TODO: try and find some ways to assert it. Nobody reads the results. I've added 		
-		for (int x = 0; x < 3; x++) {
-			TermPositionVector vector = (TermPositionVector) reader.getTermFreqVector( x, "content" );
-			assertNotNull( vector );
-			String[] terms = vector.getTerms();
-			int[] freqs = vector.getTermFrequencies();
+      /**
+       * Since there are so many combinations of results here, we are only going
+       * to assert a few. - J.G.
+       */
+      int x = 0;
+      TermPositionVector vector = (TermPositionVector) reader.getTermFreqVector(x, "content");
+      assertNotNull(vector);
+      String[] terms = vector.getTerms();
+      int[] freqs = vector.getTermFrequencies();
 
-			for (int y = 0; y < vector.size(); y++) {
-				log.info( "doc# =>" + x );
-				log.info( " term => " + terms[y] );
-				log.info( " freq => " + freqs[y] );
+      assertEquals("electrical", terms[x]);
+      assertEquals(2, freqs[x]);
 
-				int[] positions = vector.getTermPositions( y );
-				TermVectorOffsetInfo[] offsets = vector.getOffsets( y );
-				for (int z = 0; z < positions.length; z++) {
-					log.info( " position => " + positions[z] );
-					log.info( " starting offset => " + offsets[z].getStartOffset() );
-					log.info( " ending offset => " + offsets[z].getEndOffset() );
-				}
-				log.info( "---------------" );
-			}
-		}
+      TermVectorOffsetInfo[] offsets = vector.getOffsets(x);
+      assertEquals(0, offsets[x].getStartOffset());
+      assertEquals(10, offsets[x].getEndOffset());
 
-		//cleanup
-		for (Object element : s.createQuery( "from " + ElectricalProperties.class.getName() ).list())
-			s.delete( element );
-		tx.commit();
-		s.close();
-	}
+      int[] termPositions = vector.getTermPositions(0);
+      assertEquals(0, termPositions[0]);
+      assertEquals(3, termPositions[1]);
 
-	public void testNoTermVector() throws Exception {
-		FullTextSession s = Search.createFullTextSession( openSession() );
-		Transaction tx = s.beginTransaction();
+      //cleanup
+      for (Object element : s.createQuery("from " + Employee.class.getName()).list()) s.delete(element);
+      tx.commit();
+      s.close();
+   }
 
-		Employee e1 = new Employee( 1000, "Griffin", "ITech" );
-		s.save( e1 );
-		tx.commit();
-		s.clear();
 
-		tx = s.beginTransaction();
+   public void testNoTermVector() throws Exception {
+      FullTextSession s = Search.createFullTextSession(openSession());
+      Transaction tx = s.beginTransaction();
 
-		// Here's how to get a reader from a FullTextSession
-		SearchFactory searchFactory = s.getSearchFactory();
-		DirectoryProvider provider = searchFactory.getDirectoryProviders( Employee.class )[0];
-		ReaderProvider readerProvider = searchFactory.getReaderProvider();
-		IndexReader reader = readerProvider.openReader( provider );
+      Employee e1 = new Employee(1000, "Griffin", "ITech");
+      s.save(e1);
+      tx.commit();
+      s.clear();
 
-		TermPositionVector vector = (TermPositionVector) reader.getTermFreqVector( 0, "dept" );
-		assertNull( "should not find a term position vector", vector );
+      tx = s.beginTransaction();
 
-		//cleanup
-		for (Object element : s.createQuery( "from " + ElectricalProperties.class.getName() ).list())
-			s.delete( element );
-		tx.commit();
-		s.close();
-	}
+      // Here's how to get a reader from a FullTextSession
+      SearchFactory searchFactory = s.getSearchFactory();
+      DirectoryProvider provider = searchFactory.getDirectoryProviders(Employee.class)[0];
+      ReaderProvider readerProvider = searchFactory.getReaderProvider();
+      IndexReader reader = readerProvider.openReader(provider);
 
-	private void createIndex(FullTextSession s) {
-		Transaction tx = s.beginTransaction();
-		ElectricalProperties e1 = new ElectricalProperties( 1000, "Electrical Engineers measure Electrical Properties" );
-		s.save( e1 );
-		ElectricalProperties e2 = new ElectricalProperties( 1001, "Electrical Properties are interesting" );
-		s.save( e2 );
-		ElectricalProperties e3 = new ElectricalProperties( 1002, "Electrical Properties are measurable properties" );
-		s.save( e3 );
+      TermPositionVector vector = (TermPositionVector) reader.getTermFreqVector(0, "dept");
+      assertNull("should not find a term position vector", vector);
 
-		tx.commit();
-	}
+      //cleanup
+      for (Object element : s.createQuery("from " + ElectricalProperties.class.getName()).list())
+         s.delete(element);
+      tx.commit();
+      s.close();
+   }
 
-	protected Class[] getMappings() {
-		return new Class[] {
-				ElectricalProperties.class,
-				Employee.class
-		};
-	}
+   private void createIndex(FullTextSession s) {
+      Transaction tx = s.beginTransaction();
+      ElectricalProperties e1 = new ElectricalProperties(1000, "Electrical Engineers measure Electrical Properties");
+      s.save(e1);
+      ElectricalProperties e2 = new ElectricalProperties(1001, "Electrical Properties are interesting");
+      s.save(e2);
+      ElectricalProperties e3 = new ElectricalProperties(1002, "Electrical Properties are measurable properties");
+      s.save(e3);
+
+      tx.commit();
+   }
+
+   protected Class[] getMappings() {
+      return new Class[]{
+         ElectricalProperties.class,
+         Employee.class
+      };
+   }
 }
