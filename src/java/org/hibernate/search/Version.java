e988b4e8cebd9a0bd50f88470e47f273597c2527
Sanne Grinovero <sanne.grinovero@gmail.com>
2008-05-29 11:27:38 +0000

    "Version" class releases Logger to gc after use
    git-svn-id: https://svn.jboss.org/repos/hibernate/search/trunk@14704 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- src/java/org/hibernate/search/Version.java
+++ src/java/org/hibernate/search/Version.java
@@ -12,9 +12,9 @@ import org.slf4j.LoggerFactory;
  */
 public class Version {
 	public static final String VERSION = "3.1.x." + new Date();
-	private static Logger log = LoggerFactory.getLogger( Version.class );
 
 	static {
+		Logger log = LoggerFactory.getLogger( Version.class );
 		log.info( "Hibernate Search {}", VERSION );
 	}
 
