d90f4dda7bcbe2766d646557b9e15ac23934a66b
Navin Surtani <nsurtani@redhat.com>
2008-07-15 13:05:19 +0000

    TransactionContext extends Serializable
    git-svn-id: https://svn.jboss.org/repos/hibernate/search/trunk@14930 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- src/java/org/hibernate/search/transaction/TransactionContext.java
+++ src/java/org/hibernate/search/transaction/TransactionContext.java
@@ -1,11 +1,12 @@
 package org.hibernate.search.transaction;
 
 import javax.transaction.Synchronization;
+import java.io.Serializable;
 
 /**
 @author Navin Surtani  - navin@surtani.org
  */
-public interface TransactionContext
+public interface TransactionContext extends Serializable
 {
    /**
     *@return A boolean whether a transaction is in progress or not.
