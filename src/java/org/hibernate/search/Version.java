936c60cf394c6ca73c729b1cbefd12eff38cf0c6
Emmanuel Bernard <emmanuel@hibernate.org>
2008-07-28 23:10:06 +0000

    avoid confusion on the work in progress version number
    git-svn-id: https://svn.jboss.org/repos/hibernate/search/trunk@14974 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- src/java/org/hibernate/search/Version.java
+++ src/java/org/hibernate/search/Version.java
@@ -11,7 +11,7 @@ import org.slf4j.LoggerFactory;
  * @author Emmanuel Bernard
  */
 public class Version {
-	public static final String VERSION = "3.1.0.Beta1";
+	public static final String VERSION = "3.1.0.SNAPSHOT" + new Date();
 
 	static {
 		Logger log = LoggerFactory.getLogger( Version.class );
