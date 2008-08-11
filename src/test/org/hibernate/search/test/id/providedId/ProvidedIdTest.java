b468887ac3b4109a342abfc0946488fb5cbaf5b0
Emmanuel Bernard <emmanuel@hibernate.org>
2008-08-11 14:53:01 +0000

    Style improvements
    git-svn-id: https://svn.jboss.org/repos/hibernate/search/trunk@15032 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- src/test/org/hibernate/search/test/id/providedId/ProvidedIdTest.java
+++ src/test/org/hibernate/search/test/id/providedId/ProvidedIdTest.java
@@ -1,68 +1,65 @@
 package org.hibernate.search.test.id.providedId;
 
+import java.util.List;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.queryParser.ParseException;
+import org.apache.lucene.queryParser.QueryParser;
+import org.apache.lucene.search.Query;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-import org.hibernate.search.test.SearchTestCase;
-import org.hibernate.search.Search;
-import org.hibernate.search.FullTextSession;
 import org.hibernate.search.FullTextQuery;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.queryParser.QueryParser;
-import org.apache.lucene.queryParser.ParseException;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.List;
+import org.hibernate.search.FullTextSession;
+import org.hibernate.search.Search;
+import org.hibernate.search.test.SearchTestCase;
 
 /**
  * @author Navin Surtani (<a href="mailto:nsurtani@redhat.com">nsurtani@redhat.com</a>)
  */
 public class ProvidedIdTest extends SearchTestCase {
 
-   JBossCachePerson person1, person2;
+	protected Class[] getMappings() {
+		return new Class[] {
+				JBossCachePerson.class
+		};
+	}
 
-   protected Class[] getMappings() {
-      return new Class[]{
-              JBossCachePerson.class
-      };
-   }
+	public void testProvidedId() throws ParseException {
 
-   public void testProvidedId() throws ParseException {
+		JBossCachePerson person1 = new JBossCachePerson();
+		person1.setName( "Big Goat" );
+		person1.setBlurb( "Eats grass" );
 
-      person1 = new JBossCachePerson();
-      person1.setName("Big Goat");
-      person1.setBlurb("Eats grass");
-
-      person2 = new JBossCachePerson();
-      person2.setName("Mini Goat");
-      person2.setBlurb("Eats cheese");
+		JBossCachePerson person2 = new JBossCachePerson();
+		person2.setName( "Mini Goat" );
+		person2.setBlurb( "Eats cheese" );
 
 
-      Session session = openSession();
-      FullTextSession fullTextSession = Search.getFullTextSession(session);
-      Transaction transaction = session.beginTransaction();
-      session.persist(person1);
-      session.persist(person2);
+		Session session = openSession();
+		FullTextSession fullTextSession = Search.getFullTextSession( session );
+		Transaction transaction = session.beginTransaction();
+		session.persist( person1 );
+		session.persist( person2 );
 
-      transaction.commit();
-      session.clear();
+		transaction.commit();
+		session.clear();
 
-      transaction = fullTextSession.beginTransaction();
-      
-      QueryParser parser = new QueryParser("name", new StandardAnalyzer());
-      Query luceneQuery = parser.parse("Goat");
+		transaction = fullTextSession.beginTransaction();
 
-      FullTextQuery fullTextQuery = fullTextSession.createFullTextQuery(luceneQuery, JBossCachePerson.class);
+		QueryParser parser = new QueryParser( "name", new StandardAnalyzer() );
+		Query luceneQuery = parser.parse( "Goat" );
+
+		FullTextQuery fullTextQuery = fullTextSession.createFullTextQuery( luceneQuery, JBossCachePerson.class );
 
 
-      List results = fullTextQuery.list();
+		List results = fullTextQuery.list();
 
-      transaction.commit();
-      session.close();
+		transaction.commit();
+		session.close();
 
-      assert results.size() == 2;
-   }
+		assertEquals( 2, results.size() );
+	}
 
 
 }
