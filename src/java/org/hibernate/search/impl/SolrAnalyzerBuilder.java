4b4867f51bf48e5823a7796a1c9a38b590c4d704
Hardy Ferentschik <hibernate@ferentschik.de>
2008-10-13 09:51:41 +0000

    HSEARCH-254 * Upgraded to Lucene 2.4.0 * Upgraded to Solr 1.3.0 * Updated analyzer builder * moved the properties file stoplist.properties into test-resources
    git-svn-id: https://svn.jboss.org/repos/hibernate/search/trunk@15335 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- src/java/org/hibernate/search/impl/SolrAnalyzerBuilder.java
+++ src/java/org/hibernate/search/impl/SolrAnalyzerBuilder.java
@@ -8,21 +8,33 @@ import org.apache.lucene.analysis.Analyzer;
 import org.apache.solr.analysis.TokenizerChain;
 import org.apache.solr.analysis.TokenFilterFactory;
 import org.apache.solr.analysis.TokenizerFactory;
+import org.apache.solr.util.plugin.ResourceLoaderAware;
+import org.apache.solr.common.ResourceLoader;
 
 import org.hibernate.search.annotations.AnalyzerDef;
 import org.hibernate.search.annotations.TokenizerDef;
 import org.hibernate.search.annotations.TokenFilterDef;
 import org.hibernate.search.annotations.Parameter;
 import org.hibernate.search.SearchException;
+import org.hibernate.search.util.HibernateSearchResourceLoader;
 
 /**
- * This class has a direct dependency on Solr classes
+ * Instances of this class are used to build Lucene analyzers which are defined using the solr <code>TokenFilterFactory</code>.
+ * To make the dependency to the solr framework optional only this class has direct dependecies to solr. Solr dependencies
+ * are not supposed to be used anywhere else (except the actual configuration of the analzers in the domain model).
  *
  * @author Emmanuel Bernard
+ * @author Hardy Ferentschik
  */
 class SolrAnalyzerBuilder {
 	private SolrAnalyzerBuilder() {}
 
+	/**
+	 * Builds a Lucene <code>Analyzer</code> from the specified <code>AnalyzerDef</code> annotation.
+	 *
+	 * @param analyzerDef The <code>AnalyzerDef</code> annotation as found in the annotated domain class.
+	 * @return a Lucene <code>Analyzer</code>
+	 */
 	public static Analyzer buildAnalyzer(AnalyzerDef analyzerDef) {
 		TokenizerDef token = analyzerDef.tokenizer();
 		TokenizerFactory tokenFactory = ( TokenizerFactory ) instantiate( token.factory() );
@@ -30,10 +42,14 @@ class SolrAnalyzerBuilder {
 
 		final int length = analyzerDef.filters().length;
 		TokenFilterFactory[] filters = new TokenFilterFactory[length];
+		ResourceLoader resourceLoader = new HibernateSearchResourceLoader();
 		for ( int index = 0 ; index < length ; index++ ) {
 			TokenFilterDef filterDef = analyzerDef.filters()[index];
 			filters[index] = (TokenFilterFactory) instantiate( filterDef.factory() );
 			filters[index].init( getMapOfParameters( filterDef.params() ) );
+			if ( filters[index] instanceof ResourceLoaderAware ) {
+				((ResourceLoaderAware)filters[index]).inform( resourceLoader );
+			}
 		}
 		return new TokenizerChain(tokenFactory, filters);
 	}
---
+++ src/java/org/hibernate/search/util/HibernateSearchResourceLoader.java
@@ -0,0 +1,76 @@
+package org.hibernate.search.util;
+
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.Reader;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import org.apache.solr.common.ResourceLoader;
+import org.apache.solr.util.plugin.ResourceLoaderAware;
+
+import org.hibernate.annotations.common.util.ReflectHelper;
+import org.hibernate.search.SearchException;
+
+/**
+ * @author Emmanuel Bernard
+ */
+public class HibernateSearchResourceLoader implements ResourceLoader {
+	public InputStream openResource(String resource) throws IOException {
+		return Thread.currentThread().getContextClassLoader().getResourceAsStream( resource );
+	}
+
+	public List<String> getLines(String resource) throws IOException {
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader( new InputStreamReader( openResource( resource ) ) );
+			List<String> results = new ArrayList<String>();
+			String line = reader.readLine();
+			while ( line != null ) {
+				//comment or empty line
+				if ( line.length() != 0 && !line.startsWith( "#" ) ) {
+					results.add( line );
+				}
+				line = reader.readLine();
+			}
+			return Collections.unmodifiableList( results );
+		}
+		finally {
+			try {
+				if (reader != null) reader.close();
+			}
+			catch ( Exception e ) {
+				//we don't really care if we can't close
+			}
+		}
+	}
+
+	public Object newInstance(String cname, String... subpackages) {
+		if (subpackages != null && subpackages.length > 0)
+			throw new UnsupportedOperationException( "newInstance(classname, packages) not implemented" );
+
+		final Class<?> clazz;
+		try {
+			clazz = ReflectHelper.classForName( cname );
+		}
+		catch ( ClassNotFoundException e ) {
+			throw new SearchException("Unable to find class " + cname, e);
+		}
+		try {
+			final Object instance = clazz.newInstance();
+			if (instance instanceof ResourceLoaderAware) {
+				( ( ResourceLoaderAware) instance ).inform( this );
+			}
+			return instance;
+		}
+		catch ( InstantiationException e ) {
+			throw new SearchException("Unable to instanciate class with no-arg constructor: " + cname, e);
+		}
+		catch ( IllegalAccessException e ) {
+			throw new SearchException("Unable to instanciate class with no-arg constructor: " + cname, e);
+		}
+	}
+}
