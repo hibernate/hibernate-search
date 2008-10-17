3715767ec9e27ff27db2b240a6aab8b59c2f4edd
Hardy Ferentschik <hibernate@ferentschik.de>
2008-10-17 09:47:43 +0000

    HSEARCH-162 Added a test case. There seems to be NPE thrown, but rather a Hibernate Exception.
    git-svn-id: https://svn.jboss.org/repos/hibernate/search/trunk@15355 1b8cb986-b30d-0410-93ca-fae66ebed9b2

---
+++ src/test/org/hibernate/search/test/query/Person.java
@@ -0,0 +1,38 @@
+//$Id$
+package org.hibernate.search.test.query;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.search.annotations.DocumentId;
+import org.hibernate.search.annotations.Field;
+import org.hibernate.search.annotations.Index;
+import org.hibernate.search.annotations.Indexed;
+import org.hibernate.search.annotations.Store;
+
+/**
+ * @author Emmanuel Bernard
+ */
+@Entity
+public class Person {
+	@Id @GeneratedValue
+	private Integer id;
+	private String name;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ src/test/org/hibernate/search/test/query/QueryUnindexedEntityTest.java
@@ -0,0 +1,53 @@
+//$Id$
+package org.hibernate.search.test.query;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.queryParser.QueryParser;
+import org.apache.lucene.search.Query;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Transaction;
+import org.hibernate.search.FullTextQuery;
+import org.hibernate.search.FullTextSession;
+import org.hibernate.search.Search;
+import org.hibernate.search.test.SearchTestCase;
+
+/**
+ * HSEARCH-162 - trying to index an entity which is not marked with @Indexed
+ *
+ * @author Hardy Ferentschik
+ */
+public class QueryUnindexedEntityTest extends SearchTestCase {
+
+	public void testResultTransformToDelimString() throws Exception {
+
+		FullTextSession s = Search.getFullTextSession( openSession() );
+
+		Transaction tx = s.beginTransaction();
+		Person person = new Person();
+		person.setName( "Jon Doe" );
+		s.save( person );
+		tx.commit();
+
+		tx = s.beginTransaction();
+		QueryParser parser = new QueryParser( "name", new StandardAnalyzer() );
+		Query query = parser.parse( "name:foo" );
+		FullTextQuery hibQuery = s.createFullTextQuery( query, Person.class );
+		try {
+			hibQuery.list();
+			fail();
+		}
+		catch ( HibernateException e ) {
+			assertTrue( "Wrong message", e.getMessage().startsWith( "Not a mapped entity" ) );
+		}
+
+		tx.rollback();
+		s.close();
+	}
+
+	protected Class[] getMappings() {
+		return new Class[] {
+				Person.class,
+		};
+	}
+}
