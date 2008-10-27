8b9782cdf381298d75a7d0f31915a5670741f456
Hardy Ferentschik <hibernate@ferentschik.de>
2008-10-27 10:38:07 +0000

    javadoc updates; replaced references to CacheBitResults with FilterCacheModeType; added link attribute to javadoc ant target in order to link to Lucene online doc
    git-svn-id: https://svn.jboss.org/repos/hibernate/search/trunk@15395 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- src/java/org/hibernate/search/annotations/FilterCacheModeType.java
+++ src/java/org/hibernate/search/annotations/FilterCacheModeType.java
@@ -1,29 +1,35 @@
 package org.hibernate.search.annotations;
 
 /**
- * Cache mode strategy for Full Text filters
+ * Cache mode strategy for <code>FullTextFilterDef</code>s.
  *
+ * @see FullTextFilterDef
  * @author Emmanuel Bernard
  */
 public enum FilterCacheModeType {
 	/**
-	 * No filter instance and no result is cached by Hibernate Search
-	 * For every filter call, a new filter instance is created
+	 * No filter instance and no result is cached by Hibernate Search.
+	 * For every filter call, a new filter instance is created.
 	 */
 	NONE,
 
 	/**
 	 * The filter instance is cached by Hibernate Search and reused across
-	 * concurrent filter.bits() calls
-	 * Results are not cache by Hibernate Search
+	 * concurrent <code>Filter.bits()</code> calls.
+	 * Results are not cached by Hibernate Search.
+	 *
+	 * @see org.apache.lucene.search.Filter#bits(org.apache.lucene.index.IndexReader)
+
 	 */
 	INSTANCE_ONLY,
 
 	/**
-	 * Both the filter instance and the BitSet results are cached.
+	 * Both the filter instance and the <code>BitSet</code> results are cached.
 	 * The filter instance is cached by Hibernate Search and reused across
-	 * concurrent filter.bits() calls
-	 * BitSet Results are cached per IndexReader 
+	 * concurrent <code>Filter.bits()</code> calls.
+	 * <code>BitSet</code> results are cached per <code>IndexReader</code>.
+	 *
+	 * @see org.apache.lucene.search.Filter#bits(org.apache.lucene.index.IndexReader) 
 	 */
 	INSTANCE_AND_BITSETRESULTS
 
