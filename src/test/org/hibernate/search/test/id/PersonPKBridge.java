c46365864d88770c1529cd5f81e9af36a2750c8a
Hardy Ferentschik <hibernate@ferentschik.de>
2008-11-13 15:40:37 +0000

    code formatting
    git-svn-id: https://svn.jboss.org/repos/hibernate/search/trunk@15562 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- src/test/org/hibernate/search/test/id/PersonPKBridge.java
+++ src/test/org/hibernate/search/test/id/PersonPKBridge.java
@@ -3,6 +3,7 @@ package org.hibernate.search.test.id;
 
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
+
 import org.hibernate.search.bridge.LuceneOptions;
 import org.hibernate.search.bridge.TwoWayFieldBridge;
 
@@ -21,26 +22,44 @@ public class PersonPKBridge implements TwoWayFieldBridge {
 	}
 
 	public String objectToString(Object object) {
-		PersonPK id = (PersonPK) object;
+		PersonPK id = ( PersonPK ) object;
 		StringBuilder sb = new StringBuilder();
 		sb.append( id.getFirstName() ).append( " " ).append( id.getLastName() );
 		return sb.toString();
 	}
 
 	public void set(String name, Object value, Document document, LuceneOptions luceneOptions) {
-		PersonPK id = (PersonPK) value;
+		PersonPK id = ( PersonPK ) value;
 
 		//store each property in a unique field
-		Field field = new Field( name + ".firstName", id.getFirstName(), luceneOptions.getStore(), luceneOptions.getIndex(), luceneOptions.getTermVector() );
+		Field field = new Field(
+				name + ".firstName",
+				id.getFirstName(),
+				luceneOptions.getStore(),
+				luceneOptions.getIndex(),
+				luceneOptions.getTermVector()
+		);
 		field.setBoost( luceneOptions.getBoost() );
 		document.add( field );
-		
-		field = new Field( name + ".lastName", id.getLastName(), luceneOptions.getStore(), luceneOptions.getIndex(), luceneOptions.getTermVector() );
+
+		field = new Field(
+				name + ".lastName",
+				id.getLastName(),
+				luceneOptions.getStore(),
+				luceneOptions.getIndex(),
+				luceneOptions.getTermVector()
+		);
 		field.setBoost( luceneOptions.getBoost() );
 		document.add( field );
 
 		//store the unique string representation in the named field
-		field = new Field( name, objectToString( id ), luceneOptions.getStore(), luceneOptions.getIndex(), luceneOptions.getTermVector() );
+		field = new Field(
+				name,
+				objectToString( id ),
+				luceneOptions.getStore(),
+				luceneOptions.getIndex(),
+				luceneOptions.getTermVector()
+		);
 		field.setBoost( luceneOptions.getBoost() );
 		document.add( field );
 	}
