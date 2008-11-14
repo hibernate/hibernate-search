687104d77da97ea525fc1249d9245c9c248b5f7d
Hardy Ferentschik <hibernate@ferentschik.de>
2008-11-14 11:10:15 +0000

    changed version string for CR1 release
    git-svn-id: https://svn.jboss.org/repos/hibernate/search/trunk@15564 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- src/java/org/hibernate/search/Version.java
+++ src/java/org/hibernate/search/Version.java
@@ -12,7 +12,7 @@ import org.hibernate.search.util.LoggerFactory;
  * @author Emmanuel Bernard
  */
 public class Version {
-	public static final String VERSION = "3.1.0.Beta2";
+	public static final String VERSION = "3.1.0.CR1";
 
 	private static final Logger log = LoggerFactory.make();
 
