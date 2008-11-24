b0026580f617dd68a825b6b304abb8eb0288f8f4
Hardy Ferentschik <hibernate@ferentschik.de>
2008-11-24 15:46:29 +0000

    HSEARCH-285 removed deprecation on DocumentBuilder
    git-svn-id: https://svn.jboss.org/repos/hibernate/search/trunk@15607 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- src/java/org/hibernate/search/engine/DocumentBuilder.java
+++ src/java/org/hibernate/search/engine/DocumentBuilder.java
@@ -6,7 +6,6 @@ import org.hibernate.search.ProjectionConstants;
 /**
  * Interface created to keep backwards compatibility.
  *
- * @deprecated As of release 3.1.0, replaced by {@link org.hibernate.search.ProjectionConstants}
  * @author Hardy Ferentschik
  */
 public interface DocumentBuilder {
@@ -14,8 +13,6 @@ public interface DocumentBuilder {
 	/**
 	 * Lucene document field name containing the fully qualified classname of the indexed class.
 	 *
-	 * @deprecated As of release 3.1.0, replaced by {@link org.hibernate.search.ProjectionConstants#OBJECT_CLASS}
 	 */
-	@Deprecated
 	String CLASS_FIELDNAME = ProjectionConstants.OBJECT_CLASS;
 }
