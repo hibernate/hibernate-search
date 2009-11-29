626f88ef5177c95fd4d2fa09dfd23bd02279c1fd
Hardy Ferentschik <hibernate@ferentschik.de>
2009-11-29 16:13:55 +0000

    Fixed configuration of release plugin. Updated all version references to refer to 3.2.0.Beta1
    git-svn-id: https://svn.jboss.org/repos/hibernate/search/trunk@18091 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- src/main/java/org/hibernate/search/Version.java
+++ src/main/java/org/hibernate/search/Version.java
@@ -31,7 +31,7 @@ import org.hibernate.search.util.LoggerFactory;
  */
 public class Version {
 	
-	public static final String VERSION = "3.2.0-SNAPSHOT";
+	public static final String VERSION = "3.2.0.Beta1";
 
 	static {
 		LoggerFactory.make().info( "Hibernate Search {}", VERSION );
