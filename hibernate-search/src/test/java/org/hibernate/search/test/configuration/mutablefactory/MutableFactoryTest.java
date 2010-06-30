9b9f8e2f0fe1b63e24d91d499b740545bb297a54
Emmanuel Bernard <emmanuel@hibernate.org>
2010-06-30 13:46:13 +0000

    HSEARCH-397 Add performance and concurrency tests for mutating search factory
    Test on FSDirectory around 20ms per class addition (mainly the index creation)
    Test on RAMDirectory not noticeable
    Tested on 100 classes, 10 threads adding 10 classes one by one
    
    git-svn-id: https://svn.jboss.org/repos/hibernate/search/trunk@19873 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- hibernate-search/src/test/java/org/hibernate/search/test/configuration/mutablefactory/MutableFactoryTest.java
+++ hibernate-search/src/test/java/org/hibernate/search/test/configuration/mutablefactory/MutableFactoryTest.java
@@ -1,27 +1,41 @@
 package org.hibernate.search.test.configuration.mutablefactory;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadPoolExecutor;
+
 import junit.framework.TestCase;
 import org.apache.lucene.queryParser.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.TopDocs;
+import org.slf4j.Logger;
 
-import org.hibernate.search.spi.SearchFactoryIntegrator;
+import org.hibernate.annotations.common.util.ReflectHelper;
 import org.hibernate.search.backend.Work;
 import org.hibernate.search.backend.WorkType;
+import org.hibernate.search.batchindexing.Executors;
 import org.hibernate.search.engine.SearchFactoryImplementor;
 import org.hibernate.search.impl.MutableSearchFactory;
 import org.hibernate.search.impl.SearchFactoryBuilder;
+import org.hibernate.search.spi.SearchFactoryIntegrator;
 import org.hibernate.search.store.DirectoryProvider;
+import org.hibernate.search.store.FSDirectoryProvider;
 import org.hibernate.search.store.RAMDirectoryProvider;
 import org.hibernate.search.test.SearchTestCase;
+import org.hibernate.search.test.configuration.mutablefactory.generated.Generated;
 import org.hibernate.search.test.util.ManualConfiguration;
 import org.hibernate.search.test.util.ManualTransactionContext;
+import org.hibernate.search.util.FileHelper;
+import org.hibernate.search.util.LoggerFactory;
 
 /**
  * @author Emmanuel Bernard
  */
 public class MutableFactoryTest extends TestCase {
+
+	public static final Logger log = LoggerFactory.make();
 
 	public void testCreateEmptyFactory() throws Exception {
 		final ManualConfiguration configuration = new ManualConfiguration();
@@ -83,7 +97,6 @@ public class MutableFactoryTest extends TestCase {
 	public void testAddingClassSimpleAPI() throws Exception {
 		ManualConfiguration configuration = new ManualConfiguration()
 				.addProperty( "hibernate.search.default.directory_provider", RAMDirectoryProvider.class.getName() );
-
 		SearchFactoryIntegrator sf = new SearchFactoryBuilder().configuration( configuration ).buildSearchFactory();
 
 		sf.addClasses( A.class );
@@ -135,8 +148,153 @@ public class MutableFactoryTest extends TestCase {
 		sf.close();
 	}
 
-	private void doIndexWork(Object entity, Integer id, SearchFactoryIntegrator sfi, ManualTransactionContext tc) {
+	private static void doIndexWork(Object entity, Integer id, SearchFactoryIntegrator sfi, ManualTransactionContext tc) {
 		Work<?> work = new Work<Object>( entity, id, WorkType.INDEX );
 		sfi.getWorker().performWork( work, tc );
+	}
+
+	public void testMultiThreadedAddClasses() throws Exception {
+
+		File indexDir = initIndexDirectory();
+		try {
+			doTestMultiThreadedClasses(indexDir);
+		}
+		finally {
+			cleanIndexDir( indexDir );
+		}
+	}
+
+	private void doTestMultiThreadedClasses(File indexDir) throws Exception {
+		QueryParser parser = new QueryParser( SearchTestCase.getTargetLuceneVersion(), "name", SearchTestCase.standardAnalyzer );
+		ManualConfiguration configuration = new ManualConfiguration()
+				.addProperty( "hibernate.search.default.directory_provider", FSDirectoryProvider.class.getName() )
+				.addProperty( "hibernate.search.default.indexBase", indexDir.getAbsolutePath() );
+		SearchFactoryIntegrator sf = new SearchFactoryBuilder().configuration( configuration ).buildSearchFactory();
+		List<DoAddClasses> runnables = new ArrayList<DoAddClasses>(10);
+		final int nbrOfThread = 10;
+		final int nbrOfClassesPerThread = 10;
+		for (int i = 0 ; i < nbrOfThread; i++) {
+			runnables.add( new DoAddClasses( sf, i, nbrOfClassesPerThread ) );
+		}
+		final ThreadPoolExecutor poolExecutor = Executors.newFixedThreadPool( nbrOfThread, "SFI classes addition" );
+		poolExecutor.prestartAllCoreThreads();
+		for (Runnable runnable : runnables) {
+			poolExecutor.execute( runnable );
+		}
+
+		//poolExecutor.awaitTermination( 1, TimeUnit.MINUTES );
+		boolean inProgress;
+		do {
+			Thread.sleep( 100 );
+			inProgress = false;
+			for ( DoAddClasses runnable : runnables) {
+				inProgress = inProgress || runnable.isFailure() == null;
+			}
+		} while (inProgress);
+
+		for ( DoAddClasses runnable : runnables) {
+			assertNotNull( "Threads not run # " + runnable.getWorkNumber(), runnable.isFailure() );
+			assertFalse( "thread failed #" + runnable.getWorkNumber() + " Failure: " + runnable.getFailureInfo(), runnable.isFailure() );
+		}
+
+		poolExecutor.shutdown();
+
+		for (int i = 0 ; i < nbrOfThread*nbrOfClassesPerThread ; i++) {
+			Query luceneQuery = parser.parse( "Emmanuel" + i);
+			final Class<?> classByNumber = getClassAByNumber( i );
+			DirectoryProvider<?> provider = sf.getDirectoryProviders( classByNumber )[0];
+			IndexSearcher searcher = new IndexSearcher( provider.getDirectory(), true );
+			TopDocs hits = searcher.search( luceneQuery, 1000 );
+			assertEquals( 1, hits.totalHits );
+		}
+	}
+
+	private void cleanIndexDir(File indexDir) {
+		FileHelper.delete( indexDir );
+	}
+
+	private File initIndexDirectory() {
+		String buildDir = System.getProperty( "build.dir" );
+		if ( buildDir == null ) {
+			buildDir = ".";
+		}
+		File current = new File( buildDir );
+		File indexDir = new File( current, "indextemp" );
+		boolean created = indexDir.mkdir();
+		if (!created) {
+			FileHelper.delete( indexDir );
+			indexDir.mkdir();
+		}
+		return indexDir;
+	}
+
+	private static Class<?> getClassAByNumber(int i) throws ClassNotFoundException {
+		final Class<?> aClass = ReflectHelper.classForName(
+				Generated.A0.class.getName().replace(
+						"A0", "A" + i
+				)
+		);
+		return aClass;
+	}
+
+	private static class DoAddClasses implements Runnable {
+		private final SearchFactoryIntegrator factory;
+		private final int factorOfClassesPerThread;
+		private final QueryParser parser;
+		private final int nbrOfClassesPerThread;
+		private volatile Boolean failure;
+		private volatile String failureInfo;
+
+		public String getFailureInfo() {
+			return failureInfo;
+		}
+
+		public Boolean isFailure() {
+			return failure;
+		}
+
+		public int getWorkNumber() {
+			return factorOfClassesPerThread;
+		}
+
+		public DoAddClasses(SearchFactoryIntegrator factory, int factorOfClassesPerThread, int nbrOfClassesPerThread) {
+			this.factory = factory;
+			this.factorOfClassesPerThread = factorOfClassesPerThread;
+			this.parser = new QueryParser( SearchTestCase.getTargetLuceneVersion(), "name", SearchTestCase.standardAnalyzer );
+			this.nbrOfClassesPerThread = nbrOfClassesPerThread;
+		}
+
+		public void run() {
+
+			try {
+				for (int index = 0 ; index < 10 ; index++) {
+					final int i = factorOfClassesPerThread*nbrOfClassesPerThread + index;
+					final Class<?> aClass = MutableFactoryTest.getClassAByNumber( i );
+					factory.addClasses( aClass );
+					Object entity = aClass.getConstructor( Integer.class, String.class ).newInstance(i, "Emmanuel" + i);
+					ManualTransactionContext context = new ManualTransactionContext();
+					MutableFactoryTest.doIndexWork(entity, i, factory, context );
+					context.end();
+
+					Query luceneQuery = parser.parse( "Emmanuel" + i);
+					DirectoryProvider<?> provider = factory.getDirectoryProviders( aClass )[0];
+					IndexSearcher searcher = new IndexSearcher( provider.getDirectory(), true );
+					TopDocs hits = searcher.search( luceneQuery, 1000 );
+					if ( hits.totalHits != 1 ) {
+						failure = true;
+						failureInfo = "failure: Emmanuel" + i + " for " + aClass.getName();
+						return;
+					}
+					//System.out.println("success: Emmanuel" + i + " for " + aClass.getName() );
+				}
+				//System.out.println("success: Emmanuel" + factorOfClassesPerThread );
+				failure = false;
+			}
+			catch ( Exception e ) {
+				this.failure = true;
+				e.printStackTrace(  );
+				failureInfo = "failure: Emmanuel" + factorOfClassesPerThread + " exception: " + e.toString();
+			}
+		}
 	}
 }
---
+++ hibernate-search/src/test/java/org/hibernate/search/test/configuration/mutablefactory/generated/Generator.java
@@ -0,0 +1,52 @@
+package org.hibernate.search.test.configuration.mutablefactory.generated;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * @author Emmanuel Bernard
+ */
+public class Generator {
+
+	public static void main(String[] args) {
+		StringBuilder generated = new StringBuilder( );
+		generated.append( "package org.hibernate.search.test.configuration.mutablefactory.generated;\n\n" )
+			.append("import org.hibernate.search.annotations.DocumentId;\n")
+			.append( "import org.hibernate.search.annotations.Field;\n" )
+			.append( "import org.hibernate.search.annotations.Indexed;\n\n" )
+			.append( "/** Class generated container 100 inner classes */" )	
+			.append("public class Generated {\n");
+
+		StringBuilder inner = new StringBuilder( );
+		inner.append("\t").append( "@Indexed").append( "\n" )
+				.append( "\t" ).append( "public static class Ax {" ).append( "\n" )
+				.append("\t").append("\t").append( "public Ax(Integer id, String name) { this.id = id; this.name = name; }" ).append( "\n\n" )
+				.append("\t").append("\t").append( "@DocumentId" ).append( "\n" )
+				.append("\t").append("\t").append( "public Integer getId() {return id;}" ).append( "\n" )
+				.append("\t").append("\t").append( "public void setId(Integer id) { this.id = id; }" ).append( "\n" )
+				.append("\t").append("\t").append( "private Integer id;" ).append( "\n\n" )
+				.append("\t").append("\t").append( "@Field" ).append( "\n" )
+				.append("\t").append("\t").append( "public String getName() {return name;}" ).append( "\n" )
+				.append("\t").append("\t").append( "public void setName(String name) { this.name = name; }" ).append( "\n" )
+				.append("\t").append("\t").append( "private String name;" ).append( "\n" )
+				.append( "\t}\n\n" );
+		String innerString = inner.toString();
+		for (int i = 0 ; i < 100 ; i++) {
+			generated.append( innerString.replace("Ax", "A"+i ) );
+		}
+
+		generated.append( "}" );
+		File f = new File("./Generated.java");
+		try {
+			FileWriter fw = new FileWriter( f );
+			fw.write( generated.toString() );
+			fw.close();
+		}
+		catch ( IOException e ) {
+			System.out.println("Error while generating classes" );
+			e.printStackTrace(  );
+		}
+		System.out.println("Generated in :" + f.getAbsolutePath() );
+	}
+}
