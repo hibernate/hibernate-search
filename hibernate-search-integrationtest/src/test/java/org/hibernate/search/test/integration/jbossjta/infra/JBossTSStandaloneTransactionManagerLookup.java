a02831d7d62223e99a6ae381a889387240462d47
Emmanuel Bernard <emmanuel@hibernate.org>
2010-07-30 10:13:00 +0000

    HSEARCH-562 Add JTA test with JBoss TS: improve TM lookup
    Use non internal methods for the transaction manager lookup method
    
    git-svn-id: https://svn.jboss.org/repos/hibernate/search/trunk@20091 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- hibernate-search-integrationtest/src/test/java/org/hibernate/search/test/integration/jbossjta/infra/JBossTSStandaloneTransactionManagerLookup.java
+++ hibernate-search-integrationtest/src/test/java/org/hibernate/search/test/integration/jbossjta/infra/JBossTSStandaloneTransactionManagerLookup.java
@@ -1,5 +1,6 @@
 package org.hibernate.search.test.integration.jbossjta.infra;
 
+import java.lang.reflect.Method;
 import java.util.Properties;
 import javax.transaction.Transaction;
 import javax.transaction.TransactionManager;
@@ -9,19 +10,24 @@ import org.hibernate.transaction.TransactionManagerLookup;
 
 /**
  * Return a standalone JTA transaction manager for JBoss Transactions
- * 
+ *
  * @author Emmanuel Bernard
  */
 public class JBossTSStandaloneTransactionManagerLookup implements TransactionManagerLookup {
 
 	public TransactionManager getTransactionManager(Properties props) throws HibernateException {
 		try {
-			@SuppressWarnings( "unchecked" )
-			Class<? extends TransactionManager> clazz = (Class<? extends TransactionManager>)
-					Class.forName("com.arjuna.ats.internal.jta.transaction.arjunacore.TransactionManagerImple");
-			return clazz.newInstance();
+			//Call jtaPropertyManager.getJTAEnvironmentBean().getTransactionManager();
+			
+			//improper camel case name for the class
+			Class<?> propertyManager = Class.forName( "com.arjuna.ats.jta.common.jtaPropertyManager" );
+			final Method getJTAEnvironmentBean = propertyManager.getMethod( "getJTAEnvironmentBean" );
+			//static method
+			final Object jtaEnvironmentBean = getJTAEnvironmentBean.invoke( null );
+			final Method getTransactionManager = jtaEnvironmentBean.getClass().getMethod( "getTransactionManager" );
+			return ( TransactionManager ) getTransactionManager.invoke( jtaEnvironmentBean );
 		}
-		catch (Exception e) {
+		catch ( Exception e ) {
 			throw new HibernateException( "Could not obtain JBoss Transactions transaction manager instance", e );
 		}
 	}
