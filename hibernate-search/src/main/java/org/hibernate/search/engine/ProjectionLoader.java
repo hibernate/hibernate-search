cc97e704918c9d85fd6bcd162ae3d619e6e9e730
Hardy Ferentschik <hibernate@ferentschik.de>
2010-08-20 11:01:35 +0000

    HSEARCH-278 Made sure that ProjectionLoader does not skew object loading times
    git-svn-id: https://svn.jboss.org/repos/hibernate/search/trunk@20206 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- hibernate-search/src/main/java/org/hibernate/search/engine/ProjectionLoader.java
+++ hibernate-search/src/main/java/org/hibernate/search/engine/ProjectionLoader.java
@@ -1,26 +1,25 @@
-/* $Id$
- * 
+/*
  * Hibernate, Relational Persistence for Idiomatic Java
- * 
- * Copyright (c) 2009, Red Hat, Inc. and/or its affiliates or third-party contributors as
- * indicated by the @author tags or express copyright attribution
- * statements applied by the authors.  All third-party contributions are
- * distributed under license by Red Hat, Inc.
- * 
- * This copyrighted material is made available to anyone wishing to use, modify,
- * copy, or redistribute it subject to the terms and conditions of the GNU
- * Lesser General Public License, as published by the Free Software Foundation.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
- * for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public License
- * along with this distribution; if not, write to:
- * Free Software Foundation, Inc.
- * 51 Franklin Street, Fifth Floor
- * Boston, MA  02110-1301  USA
+ *
+ *  Copyright (c) 2010, Red Hat, Inc. and/or its affiliates or third-party contributors as
+ *  indicated by the @author tags or express copyright attribution
+ *  statements applied by the authors.  All third-party contributions are
+ *  distributed under license by Red Hat, Inc.
+ *
+ *  This copyrighted material is made available to anyone wishing to use, modify,
+ *  copy, or redistribute it subject to the terms and conditions of the GNU
+ *  Lesser General Public License, as published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ *  for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this distribution; if not, write to:
+ *  Free Software Foundation, Inc.
+ *  51 Franklin Street, Fifth Floor
+ *  Boston, MA  02110-1301  USA
  */
 package org.hibernate.search.engine;
 
@@ -32,7 +31,7 @@ import org.hibernate.Session;
 import org.hibernate.transform.ResultTransformer;
 
 /**
- * Implementation of the <code>Loader</code> interface used for loading entities which are projected via
+ * Implementation of the {@code Loader} interface used for loading entities which are projected via
  * {@link org.hibernate.search.ProjectionConstants#THIS}.
  *
  * @author Emmanuel Bernard
@@ -41,7 +40,7 @@ import org.hibernate.transform.ResultTransformer;
 public class ProjectionLoader implements Loader {
 	private SearchFactoryImplementor searchFactoryImplementor;
 	private Session session;
-	private Loader objectLoader;
+	private MultiClassesQueryLoader objectLoader;
 	private Boolean projectThis;
 	private ResultTransformer transformer;
 	private String[] aliases;
@@ -101,7 +100,8 @@ public class ProjectionLoader implements Loader {
 			for ( EntityInfo entityInfo : entityInfos ) {
 				for ( int index : entityInfo.indexesOfThis ) {
 					// set one by one to avoid loosing null objects (skipped in the objectLoader.load( EntityInfo[] ))
-					entityInfo.projection[index] = objectLoader.load( entityInfo );
+					// use objectLoader.executeLoad to prevent measuring load time again (see AbstractLoader)
+					entityInfo.projection[index] = objectLoader.executeLoad( entityInfo );
 				}
 			}
 		}
