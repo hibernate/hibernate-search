2ff675a5579cf90199d84e535abdc3b1f5ccc58f
Hardy Ferentschik <hibernate@ferentschik.de>
2010-08-25 10:32:57 +0000

    HSEARCH-589 Removed @Lob in Document
    git-svn-id: https://svn.jboss.org/repos/hibernate/search/trunk@20259 1b8cb986-b30d-0410-93ca-fae66ebed9b2

--- hibernate-search/src/test/java/org/hibernate/search/test/TransactionTest.java
+++ hibernate-search/src/test/java/org/hibernate/search/test/TransactionTest.java
@@ -1,26 +1,25 @@
-/* $Id$
- * 
+/*
  * Hibernate, Relational Persistence for Idiomatic Java
- * 
- * Copyright (c) 2009, Red Hat, Inc. and/or its affiliates or third-party contributors as
- * indicated by the @author tags or express copyright attribution
- * statements applied by the authors.  All third-party contributions are
- * distributed under license by Red Hat, Inc.
- * 
- * This copyrighted material is made available to anyone wishing to use, modify,
- * copy, or redistribute it subject to the terms and conditions of the GNU
- * Lesser General Public License, as published by the Free Software Foundation.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
- * for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public License
- * along with this distribution; if not, write to:
- * Free Software Foundation, Inc.
- * 51 Franklin Street, Fifth Floor
- * Boston, MA  02110-1301  USA
+ *
+ *  Copyright (c) 2010, Red Hat, Inc. and/or its affiliates or third-party contributors as
+ *  indicated by the @author tags or express copyright attribution
+ *  statements applied by the authors.  All third-party contributions are
+ *  distributed under license by Red Hat, Inc.
+ *
+ *  This copyrighted material is made available to anyone wishing to use, modify,
+ *  copy, or redistribute it subject to the terms and conditions of the GNU
+ *  Lesser General Public License, as published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ *  for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this distribution; if not, write to:
+ *  Free Software Foundation, Inc.
+ *  51 Franklin Street, Fifth Floor
+ *  Boston, MA  02110-1301  USA
  */
 package org.hibernate.search.test;
 
@@ -64,7 +63,7 @@ public class TransactionTest extends SearchTestCase {
 		s.close();
 
 		assertEquals( "rollback() should not index", 3, getDocumentNumber() );
-		
+
 		s = getSessions().openSession();
 		s.connection().setAutoCommit( true );  // www.hibernate.org/403.html
 		s.persist(
@@ -72,7 +71,7 @@ public class TransactionTest extends SearchTestCase {
 						"Java Persistence with Hibernate", "Object/relational mapping with Hibernate", "blah blah blah"
 				)
 		);
-		s.flush();	
+		s.flush();
 		s.close();
 
 		assertEquals( "no transaction should index", 4, getDocumentNumber() );
@@ -92,5 +91,4 @@ public class TransactionTest extends SearchTestCase {
 	protected Class<?>[] getAnnotatedClasses() {
 		return new Class[] { Document.class };
 	}
-	
 }
