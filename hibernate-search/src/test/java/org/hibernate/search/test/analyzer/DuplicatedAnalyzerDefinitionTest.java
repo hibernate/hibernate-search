39036499597fdf044d6d9adc86aecc7599149335
Hardy Ferentschik <hibernate@ferentschik.de>
2010-10-27 19:55:41 +0200

    HSEARCH-569 Test cases
    Testing programmatic as well as annotation based configuration

---
+++ hibernate-search/src/test/java/org/hibernate/search/test/analyzer/DuplicatedAnalyzerDefinitionTest.java
@@ -0,0 +1,105 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2010, Red Hat, Inc. and/or its affiliates or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat, Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+
+package org.hibernate.search.test.analyzer;
+
+import java.lang.annotation.ElementType;
+
+import org.apache.solr.analysis.GermanStemFilterFactory;
+import org.apache.solr.analysis.LowerCaseFilterFactory;
+import org.apache.solr.analysis.SnowballPorterFilterFactory;
+import org.apache.solr.analysis.StandardTokenizerFactory;
+import org.slf4j.Logger;
+
+import org.hibernate.HibernateException;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.search.Environment;
+import org.hibernate.search.cfg.SearchMapping;
+import org.hibernate.search.test.SearchTestCase;
+import org.hibernate.search.util.LoggerFactory;
+
+/**
+ * Tests for HSEARCH-569.
+ *
+ * @author Hardy Ferentschik
+ */
+public class DuplicatedAnalyzerDefinitionTest extends SearchTestCase {
+
+	public static final Logger log = LoggerFactory.make();
+
+	protected Class<?>[] getAnnotatedClasses() {
+		return new Class[] { };
+	}
+
+	public void testDuplicatedAnalyzerDefinitionThrowsException() throws Exception {
+		Configuration config = new AnnotationConfiguration();
+		config.addAnnotatedClass( Entity1.class );
+		config.addAnnotatedClass( Entity2.class );
+		try {
+			config.buildSessionFactory();
+			fail( "Session creation should have failed due to duplicate analyzer definition" );
+		}
+		catch ( HibernateException e ) { // the SearchException will be wrapped in a HibernateException
+			assertTrue(
+					e.getCause().getMessage().equals(
+							"Multiple analyzer definitions with the same name: my-analyzer"
+					)
+			);
+		}
+	}
+
+	public void testDuplicatedProgrammaticAnalyzerDefinitionThrowsException() throws Exception {
+		Configuration config = new AnnotationConfiguration();
+		config.getProperties().put( Environment.MODEL_MAPPING, createSearchMapping() );
+		try {
+			config.buildSessionFactory();
+			fail( "Session creation should have failed due to duplicate analyzer definition" );
+		}
+		catch ( HibernateException e ) { // the SearchException will be wrapped in a HibernateException
+			assertTrue(
+					e.getCause().getMessage().equals( "Multiple analyzer definitions with the same name: english" )
+			);
+		}
+	}
+
+	private SearchMapping createSearchMapping() {
+		SearchMapping mapping = new SearchMapping();
+
+		mapping.analyzerDef( "english", StandardTokenizerFactory.class )
+				.filter( LowerCaseFilterFactory.class )
+				.filter( SnowballPorterFilterFactory.class )
+				.analyzerDef(
+						"english", StandardTokenizerFactory.class
+				) // ups duplicate name here - this should throw an exception
+				.filter( LowerCaseFilterFactory.class )
+				.filter( GermanStemFilterFactory.class )
+				.entity( BlogEntry.class )
+				.indexed()
+				.property( "title", ElementType.METHOD );
+		return mapping;
+	}
+}
+
+
---
+++ hibernate-search/src/test/java/org/hibernate/search/test/analyzer/Entity1.java
@@ -0,0 +1,60 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2010, Red Hat, Inc. and/or its affiliates or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat, Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.hibernate.search.test.analyzer;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.apache.solr.analysis.StandardFilterFactory;
+import org.apache.solr.analysis.StandardTokenizerFactory;
+
+import org.hibernate.search.annotations.AnalyzerDef;
+import org.hibernate.search.annotations.Indexed;
+import org.hibernate.search.annotations.TokenFilterDef;
+import org.hibernate.search.annotations.TokenizerDef;
+
+/**
+ * @author Hardy Ferentschik
+ */
+@Entity
+@Indexed
+@AnalyzerDef(name = "my-analyzer",
+		tokenizer = @TokenizerDef(factory = StandardTokenizerFactory.class),
+		filters = {
+				@TokenFilterDef(factory = StandardFilterFactory.class)
+		})
+public class Entity1 {
+	@Id
+	private long id;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+}
+
+
