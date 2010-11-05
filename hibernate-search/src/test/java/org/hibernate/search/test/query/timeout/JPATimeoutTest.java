93fa23b55d90581991ef7dacb8e1260a47ec46de
Emmanuel Bernard <emmanuel@hibernate.org>
2010-11-05 16:50:48 +0100

    HSEARCH-615 Add unit tests

--- hibernate-search/src/test/java/org/hibernate/search/test/query/timeout/JPATimeoutTest.java
+++ hibernate-search/src/test/java/org/hibernate/search/test/query/timeout/JPATimeoutTest.java
@@ -1,5 +1,6 @@
 package org.hibernate.search.test.query.timeout;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import javax.persistence.QueryTimeoutException;
@@ -45,6 +46,68 @@ public class JPATimeoutTest extends JPATestCase {
 		catch ( Exception e ) {
 			fail("Expected a QueryTimeoutException");
 		}
+		em.getTransaction().commit();
+
+		em.clear();
+
+		em.getTransaction().begin();
+		assertEquals( 1000, em.createQuery( "delete from " + Clock.class.getName() ).executeUpdate() );
+		em.getTransaction().commit();
+
+		em.close();
+
+	}
+
+	public void testLimitFetchingTime() {
+		FullTextEntityManager em = Search.getFullTextEntityManager( factory.createEntityManager() );
+		em.getTransaction().begin();
+		for ( int i = 0 ; i < 1000 ; i++ ) {
+			Clock clock  = new Clock("Model cat A" + i, (i % 2 == 0) ? "Seiko" : "Swatch", new Long (2000 + i) ) ;
+			em.persist( clock );
+		}
+		em.getTransaction().commit();
+
+		em.clear();
+
+		em.getTransaction().begin();
+		final QueryBuilder builder = em.getSearchFactory().buildQueryBuilder().forEntity( Clock.class ).get();
+		Query query = builder.keyword().onField( "brand" ).matching( "Seiko" ).createQuery();
+		FullTextQuery hibernateQuery = em.createFullTextQuery( query, Clock.class );
+		List results = hibernateQuery.getResultList();
+		assertEquals( 500, results.size() );
+
+		em.clear();
+
+		query = builder.keyword().onField( "brand" ).matching( "Swatch" ).createQuery();
+		hibernateQuery = em.createFullTextQuery( query, Clock.class );
+		hibernateQuery.limitFetchingTime( 1, TimeUnit.NANOSECONDS );
+		List result = hibernateQuery.getResultList();
+		System.out.println("Result size early: " + result.size() );
+		assertEquals("Test early failure, before the number of results are even fetched", 0, result.size() );
+		if ( result.size() == 0) {
+			//sometimes, this
+			assertTrue( hibernateQuery.hasPartialResults() );
+		}
+
+		em.clear();
+
+		//We cannot test intermediate limit, Lucene / hibernate: too unpredictable
+
+//		hibernateQuery = fts.createFullTextQuery( query, Clock.class );
+//		hibernateQuery.limitFetchingTime( 1000, TimeUnit.NANOSECONDS );
+//		results = hibernateQuery.list();
+//		System.out.println("Result size partial: " + results.size() );
+//		assertTrue("Regular failure when some elements are fetched", 0 < results.size() && results.size() < 500 );
+//		assertTrue( hibernateQuery.hasPartialResults() );
+//
+//		fts.clear();
+
+		hibernateQuery = em.createFullTextQuery( query, Clock.class );
+		hibernateQuery.limitFetchingTime( 30, TimeUnit.SECONDS );
+		results = hibernateQuery.getResultList();
+		assertEquals("Test below limit termination", 500, results.size() );
+		assertFalse( hibernateQuery.hasPartialResults() );
+
 		em.getTransaction().commit();
 
 		em.clear();
--- hibernate-search/src/test/java/org/hibernate/search/test/query/timeout/TimeoutTest.java
+++ hibernate-search/src/test/java/org/hibernate/search/test/query/timeout/TimeoutTest.java
@@ -96,6 +96,70 @@ public class TimeoutTest extends SearchTestCase {
 
 	}
 
+	public void testLimitFetchingTime() {
+		FullTextSession fts = Search.getFullTextSession( openSession(  ) );
+		Transaction tx = fts.beginTransaction();
+		for ( int i = 0 ; i < 1000 ; i++ ) {
+			Clock clock  = new Clock("Model cat A" + i, (i % 2 == 0) ? "Seiko" : "Swatch", new Long (3000 + i) ) ;
+			fts.persist( clock );
+		}
+		tx.commit();
+
+		fts.clear();
+
+		tx = fts.beginTransaction();
+
+		final QueryBuilder builder = fts.getSearchFactory().buildQueryBuilder().forEntity( Clock.class ).get();
+		Query query = builder.keyword().onField( "brand" ).matching( "Seiko" ).createQuery();
+		FullTextQuery hibernateQuery = fts.createFullTextQuery( query, Clock.class );
+		List results = hibernateQuery.list();
+		assertEquals( 500, results.size() );
+
+		fts.clear();
+
+
+		query = builder.keyword().onField( "brand" ).matching( "Swatch" ).createQuery();
+		hibernateQuery = fts.createFullTextQuery( query, Clock.class );
+		hibernateQuery.limitFetchingTime( 1, TimeUnit.NANOSECONDS );
+		List result = hibernateQuery.list();
+		System.out.println("Result size early: " + result.size() );
+		assertEquals("Test early failure, before the number of results are even fetched", 0, result.size() );
+		if ( result.size() == 0) {
+			//sometimes, this
+			assertTrue( hibernateQuery.hasPartialResults() );
+		}
+
+		fts.clear();
+
+		//We cannot test intermediate limit, Lucene / hibernate: too unpredictable
+
+//		hibernateQuery = fts.createFullTextQuery( query, Clock.class );
+//		hibernateQuery.limitFetchingTime( 1000, TimeUnit.NANOSECONDS );
+//		results = hibernateQuery.list();
+//		System.out.println("Result size partial: " + results.size() );
+//		assertTrue("Regular failure when some elements are fetched", 0 < results.size() && results.size() < 500 );
+//		assertTrue( hibernateQuery.hasPartialResults() );
+//
+//		fts.clear();
+
+		hibernateQuery = fts.createFullTextQuery( query, Clock.class );
+		hibernateQuery.limitFetchingTime( 30, TimeUnit.SECONDS );
+		results = hibernateQuery.list();
+		assertEquals("Test below limit termination", 500, results.size() );
+		assertFalse( hibernateQuery.hasPartialResults() );
+
+		tx.commit();
+
+		fts.clear();
+
+		tx = fts.beginTransaction();
+		assertEquals( 1000, fts.createQuery( "delete from " + Clock.class.getName() ).executeUpdate() );
+		tx.commit();
+
+		fts.close();
+
+	}
+
 	@Override
 	protected Class<?>[] getAnnotatedClasses() {
 		return new Class<?>[] { Clock.class };
