deee8c5c6ac4799dd12554110f355da284eba68f
Sanne Grinovero <sanne@hibernate.org>
2010-12-06 15:56:14 +0000

    HSEARCH-513 add test to ensure we write using the @DocumentId if different than @Id in backend

---
+++ hibernate-search/src/test/java/org/hibernate/search/test/embedded/depth/PersonWithBrokenSocialSecurityNumber.java
@@ -0,0 +1,78 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2010, Red Hat, Inc. and/or its affiliates or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat, Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.hibernate.search.test.embedded.depth;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.hibernate.search.annotations.DocumentId;
+import org.hibernate.search.annotations.Field;
+import org.hibernate.search.annotations.Indexed;
+
+/**
+ * @author Sanne Grinovero
+ */
+@Entity
+@Indexed
+public class PersonWithBrokenSocialSecurityNumber {
+	
+	private Long id;
+	private String name;
+	
+	public PersonWithBrokenSocialSecurityNumber() {
+	}
+	
+	public PersonWithBrokenSocialSecurityNumber(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	@Id
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	@Field
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@DocumentId
+	public String getSocialSecurityNumber() {
+		// returning a constant to have changes overwrite each other in the test
+		return "100";
+	}
+	
+	public void setSocialSecurityNumber(String securityNumber) {
+	}
+	
+}
