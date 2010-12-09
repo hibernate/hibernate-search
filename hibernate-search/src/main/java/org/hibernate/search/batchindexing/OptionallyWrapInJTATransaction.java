3afe76f4082b0e69b5f2fc7703e35df22b5116bd
Emmanuel Bernard <emmanuel@hibernate.org>
2010-12-09 14:09:58 +0100

    HSEARCH-640 Provide a Runnable starting a JTA transaction if needed

---
+++ hibernate-search/src/main/java/org/hibernate/search/batchindexing/OptionallyWrapInJTATransaction.java
@@ -0,0 +1,94 @@
+package org.hibernate.search.batchindexing;
+
+import javax.transaction.Status;
+import javax.transaction.SystemException;
+import javax.transaction.TransactionManager;
+
+import org.slf4j.Logger;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.engine.SessionFactoryImplementor;
+import org.hibernate.search.util.LoggerFactory;
+import org.hibernate.transaction.TransactionFactory;
+
+/**
+ * Wrap the subsequent Runnable in a JTA Transaction if necessary:
+ *  - if the existing Hibernate Core transaction strategy requires a TransactionManager
+ *  - if no JTA transaction is already started
+ *
+ * Unfortunately at this time we need to have access to SessionFactoryImplementor
+ *
+ * @author Emmanuel Bernard
+ */
+public class OptionallyWrapInJTATransaction implements Runnable {
+
+	private static final Logger log = LoggerFactory.make();
+
+	private SessionFactoryImplementor factory;
+	private Runnable delegate;
+
+	public OptionallyWrapInJTATransaction(SessionFactory factory, Runnable delegate) {
+		/*
+		 * Unfortunately we need to access SessionFactoryImplementor to detect:
+		 *  - whether or not we need to start the JTA transaction
+		 *  - start it
+		 */
+		//TODO get SessionFactoryImplementor it from the SearchFactory as we might get a hold of the SFI at startup time
+		// if that's the case, SearchFactory should expose something like T unwrap(Class<T> clazz);
+		this.factory = (SessionFactoryImplementor) factory;
+		this.delegate = delegate;
+	}
+
+	public void run() {
+		final boolean wrapInTransaction = wrapInTransaction();
+		if (wrapInTransaction) {
+			final TransactionManager transactionManager = factory.getTransactionManager();
+			try {
+				transactionManager.begin();
+				delegate.run();
+				transactionManager.commit();
+			}
+			catch (Throwable e) {
+				//TODO exception handling seems messy-ish
+				log.error( "Error while executing runnable wrapped in a JTA transaction", e );
+				try {
+					transactionManager.rollback();
+				}
+				catch ( SystemException e1 ) {
+					// we already have an exception, don't propagate this one
+					log.error( "Error while rollbacking transaction after " + e.getMessage(), e1 );
+				}
+			}
+		}
+		else {
+			delegate.run();
+		}
+	}
+
+	boolean wrapInTransaction() {
+		final TransactionFactory transactionFactory = factory.getSettings().getTransactionFactory();
+		if ( !transactionFactory.isTransactionManagerRequired() ) {
+			//Today we only require a TransactionManager on JTA based transaction factories
+			log.trace( "TransactionFactory does nto require a TransactionManager: don't wrap in a JTA transaction" );
+			return false;
+		}
+		final TransactionManager transactionManager = factory.getTransactionManager();
+		if (transactionManager == null) {
+			//no TM, nothing to do OR configuration mistake
+			log.trace( "No TransactionManager found, do not start a surrounding JTA transaction" );
+			return false;
+		}
+		try {
+			if ( transactionManager.getStatus() == Status.STATUS_NO_TRANSACTION ) {
+				log.trace( "No Transaction in progress, needs to start a JTA transaction" );
+				return true;
+			}
+		}
+		catch ( SystemException e ) {
+			log.warn( "Cannot guess the Transaction Status: not starting a JTA transaction", e );
+			return false;
+		}
+		log.trace( "Transaction in progress, no needs to start a JTA transaction" );
+		return false;
+	}
+}
