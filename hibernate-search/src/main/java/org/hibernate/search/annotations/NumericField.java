41659b74dfa3176b55f392abe91275dd767c0307
Hardy Ferentschik <hibernate@ferentschik.de>
2010-12-14 10:54:04 +0100

    HSEARCH-629 Adding note to java- and online-docs that the numeric field feature is still considred experimental within Lucene

--- hibernate-search/src/main/java/org/hibernate/search/annotations/NumericField.java
+++ hibernate-search/src/main/java/org/hibernate/search/annotations/NumericField.java
@@ -1,31 +1,34 @@
 package org.hibernate.search.annotations;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Extension annotation for  <code>@Field</code>
+ * Extension annotation for {@code @Field} supporting Lucene's numeric field feature.
  *
- * @author: Gustavo Fernandes
+ * @author Gustavo Fernandes
+ * @experimental Lucene marks the numeric field API still as experimental and warns for incompatible changes in coming
+ * releases. Using Hibernate Search will hopefully shield you from any underlying API changes, but that is not guaranteed.
  */
-@Retention( RetentionPolicy.RUNTIME )
-@Target( { ElementType.METHOD, ElementType.FIELD } )
+@Retention(RetentionPolicy.RUNTIME)
+@Target( { ElementType.METHOD, ElementType.FIELD })
+@Documented
 public @interface NumericField {
 	/**
-	 * Precision step for numeric field. The less, more terms will be present in the index
+	 * @return Precision step for numeric field. The less, more terms will be present in the index.
 	 */
 	int precisionStep() default PRECISION_STEP_DEFAULT;
 
 	/**
-	 * Field name it refers to. If omitted, refers to the @Field annotation in case there's only one
+	 * @return Field name this annotation refers to. If omitted, refers to the @Field annotation in case there's only one
 	 */
 	String forField() default "";
 
 	/**
-	 * Default precision step.
-	 * (Mimic Lucene's default precision step value)
+	 * Default precision step, mimicking  Lucene's default precision step value.
 	 */
 	static final int PRECISION_STEP_DEFAULT = 4;
 }
