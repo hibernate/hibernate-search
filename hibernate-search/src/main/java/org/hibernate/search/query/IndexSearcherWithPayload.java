26855766244111591c4ef593839d295472c1df02
Sanne Grinovero <sanne@hibernate.org>
2011-02-10 13:51:22 +0000

    HSEARCH-684 Make IndexSearcherWithPayload public
    Conflicts (resolved:)
    
    	pom.xml

--- hibernate-search/src/main/java/org/hibernate/search/query/IndexSearcherWithPayload.java
+++ hibernate-search/src/main/java/org/hibernate/search/query/IndexSearcherWithPayload.java
@@ -5,13 +5,14 @@ import org.apache.lucene.search.IndexSearcher;
 /**
 * @author Emmanuel Bernard
 */
-//package
-class IndexSearcherWithPayload {
+//meant to be package-private, was opened up for Infinispan temporarily. Don't use outside of Hibernate Search codebase!
+@Deprecated//(warning to other frameworks only: this class is not part of public API)
+public class IndexSearcherWithPayload {
 	private final IndexSearcher searcher;
-	private  boolean fieldSortDoTrackScores;
-	private  boolean fieldSortDoMaxScore;
+	private boolean fieldSortDoTrackScores;
+	private boolean fieldSortDoMaxScore;
 
-	IndexSearcherWithPayload(IndexSearcher searcher, boolean fieldSortDoTrackScores, boolean fieldSortDoMaxScore) {
+	public IndexSearcherWithPayload(IndexSearcher searcher, boolean fieldSortDoTrackScores, boolean fieldSortDoMaxScore) {
 		this.searcher = searcher;
 		this.fieldSortDoTrackScores = fieldSortDoTrackScores;
 		this.fieldSortDoMaxScore = fieldSortDoMaxScore;
