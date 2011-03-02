32223dd4229d06b1598836ba87c9223044f2fde7
Emmanuel Bernard <emmanuel@hibernate.org>
2011-03-02 18:36:25 +0100

    HSEARCH-687 move searcher closability to IndexSearcherWithPayload

--- hibernate-search/src/main/java/org/hibernate/search/query/engine/internal/IndexSearcherWithPayload.java
+++ hibernate-search/src/main/java/org/hibernate/search/query/engine/internal/IndexSearcherWithPayload.java
@@ -20,12 +20,25 @@
  */
 package org.hibernate.search.query.engine.internal;
 
+import java.util.Set;
+
+import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.slf4j.Logger;
+
+import org.hibernate.search.SearchException;
+import org.hibernate.search.engine.SearchFactoryImplementor;
+import org.hibernate.search.reader.ReaderProvider;
+import org.hibernate.search.util.LoggerFactory;
+
+import static org.hibernate.search.reader.ReaderProviderHelper.getIndexReaders;
 
 /**
 * @author Emmanuel Bernard
 */
 public class IndexSearcherWithPayload {
+	private static final Logger log = LoggerFactory.make();
 	private final IndexSearcher searcher;
 	private boolean fieldSortDoTrackScores;
 	private boolean fieldSortDoMaxScore;
@@ -47,5 +60,23 @@ public class IndexSearcherWithPayload {
 
 	public boolean isFieldSortDoMaxScore() {
 		return fieldSortDoMaxScore;
+	}
+
+	/**
+	 * @param query toString() is invoked to display the query in the warning message
+	 * @param searchFactoryImplementor
+	 */
+	public void closeSearcher(Object query, SearchFactoryImplementor searchFactoryImplementor) {
+		Set<IndexReader> indexReaders = getIndexReaders( getSearcher() );
+		ReaderProvider readerProvider = searchFactoryImplementor.getReaderProvider();
+		for ( IndexReader indexReader : indexReaders ) {
+			try {
+				readerProvider.closeReader( indexReader );
+			}
+			catch (SearchException e) {
+				//catch is inside the for loop to make sure we try to close all of them
+				log.warn( "Unable to properly close searcher during lucene query: " + query.toString(), e );
+			}
+		}
 	}
 }
