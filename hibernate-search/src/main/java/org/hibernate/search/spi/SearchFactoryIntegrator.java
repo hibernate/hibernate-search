5182831c7a8c02aa4eaa04e65e75b49b87c5287f
Emmanuel Bernard <emmanuel@hibernate.org>
2011-03-03 15:00:10 +0100

    typo

--- hibernate-search/src/main/java/org/hibernate/search/spi/SearchFactoryIntegrator.java
+++ hibernate-search/src/main/java/org/hibernate/search/spi/SearchFactoryIntegrator.java
@@ -31,7 +31,7 @@ import org.hibernate.search.backend.Worker;
  *
  * This contract gives access to lower level APIs of Hibernate Search for
  * frameworks integrating with it. The piece of code creating the SearchFactory should
- * use this contract. It should however pass the higherlevel {@link }SearchFactory} contract to
+ * use this contract. It should however pass the higher level {@link SearchFactory} contract to
  * its clients.
  *
  * It also allows modification of some of the search factory internals:
