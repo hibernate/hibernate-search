7c91177af6dbe38bd56151824c9e7b60adfa1ff1
Hardy Ferentschik <hibernate@ferentschik.de>
2011-03-16 11:45:20 +0100

    HSEARCH-706 Switching from package.html to package-info.java

---
+++ hibernate-search/src/main/java/org/hibernate/search/query/facet/package-info.java
@@ -0,0 +1,31 @@
+/*
+* Hibernate, Relational Persistence for Idiomatic Java
+*
+* Copyright (c) 2010, Red Hat, Inc. and/or its affiliates or third-party contributors as
+* indicated by the @author tags or express copyright attribution
+* statements applied by the authors.  All third-party contributions are
+* distributed under license by Red Hat, Inc.
+*
+* This copyrighted material is made available to anyone wishing to use, modify,
+* copy, or redistribute it subject to the terms and conditions of the GNU
+* Lesser General Public License, as published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+* or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+* for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this distribution; if not, write to:
+* Free Software Foundation, Inc.
+* 51 Franklin Street, Fifth Floor
+* Boston, MA  02110-1301  USA
+*/
+
+/**
+ * This package contains the classes for the public faceting API. For a definition of faceting see
+ * <a href="http://en.wikipedia.org/wiki/Faceted_search">Faceted Search</a>.
+ */
+package org.hibernate.search.query.facet;
+
+
