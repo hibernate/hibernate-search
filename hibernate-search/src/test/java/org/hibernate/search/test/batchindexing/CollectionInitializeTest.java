ede3fd432a2d6768404027bb5a45b32bd11af1d5
Emmanuel Bernard <emmanuel@hibernate.org>
2011-03-17 00:15:24 +0100

    HSEARCH-689 clean tests and remove .sql file dependency

--- hibernate-search/src/test/java/org/hibernate/search/test/batchindexing/CollectionInitializeTest.java
+++ hibernate-search/src/test/java/org/hibernate/search/test/batchindexing/CollectionInitializeTest.java
@@ -1,4 +1,6 @@
-/* 
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
  * JBoss, Home of Professional Open Source
  * Copyright 2011 Red Hat Inc. and/or its affiliates and other contributors
  * as indicated by the @authors tag. All rights reserved.
@@ -24,6 +26,8 @@ import java.util.List;
 import junit.framework.Assert;
 
 import org.apache.lucene.search.MatchAllDocsQuery;
+
+import org.hibernate.Transaction;
 import org.hibernate.search.FullTextSession;
 import org.hibernate.search.Search;
 import org.hibernate.search.test.SearchTestCase;
@@ -35,6 +39,7 @@ public class CollectionInitializeTest extends SearchTestCase {
 
 	public void testMassIndexing() throws InterruptedException {
 		FullTextSession fullTextSession = Search.getFullTextSession( openSession() );
+		initializeDate(fullTextSession);
 		try {
 			List list = fullTextSession.createCriteria( LegacyCarPlant.class ).list();
 			Assert.assertEquals( 1, list.size() );
@@ -43,8 +48,40 @@ public class CollectionInitializeTest extends SearchTestCase {
 			Assert.assertEquals( 1, resultSize );
 		}
 		finally {
+			clearData(fullTextSession);
 			fullTextSession.close();
 		}
+	}
+
+	private void clearData(FullTextSession fullTextSession) {
+		final Transaction transaction = fullTextSession.beginTransaction();
+		final LegacyCarPlantPK id = new LegacyCarPlantPK();
+		id.setCarId( "1" );
+		id.setPlantId( "2" );
+		fullTextSession.delete( fullTextSession.get( LegacyCarPlant.class, id ) );
+		for (int i = 1 ; i < 4 ; i++) {
+			fullTextSession.delete( fullTextSession.get(LegacyCar.class, "" + i ) );
+		}
+		transaction.commit();
+	}
+
+	private void initializeDate(FullTextSession fullTextSession) {
+		final Transaction transaction = fullTextSession.beginTransaction();
+		LegacyCar[] cars = new LegacyCar[3];
+		for (int i = 1 ; i < 4 ; i++) {
+			cars[i-1] = new LegacyCar();
+			cars[i-1].setId( "" + i );
+			cars[i-1].setModel( "model" + i );
+			fullTextSession.persist( cars[i-1] );
+		}
+		LegacyCarPlant plant = new LegacyCarPlant();
+		plant.setCar( cars[0] );
+		plant.setName( "plant12" );
+		plant.setId( new LegacyCarPlantPK() );
+		plant.getId().setCarId( cars[0].getId() );
+		plant.getId().setPlantId( "2" );
+		fullTextSession.persist( plant );
+		transaction.commit();
 	}
 
 	@Override
--- hibernate-search/src/test/resources/org/hibernate/search/test/batchindexing/CollectionInitializeTest.sql
+++
@@ -1,4 +0,0 @@
-insert into z_leg_car(car_id, model) values('1', 'model1');
-insert into z_leg_car(car_id, model) values('2', 'model2');
-insert into z_leg_car(car_id, model) values('3', 'model3');
-insert into z_leg_car_plant(car_id,plant_id,plant_name) values('1','2','plant12');
