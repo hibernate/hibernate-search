73609d62460f078ab9f90cf81a8aad6057c779ee
Sanne Grinovero <sanne@hibernate.org>
2011-04-21 18:16:30 +0200

    HSEARCH-741 - forgot adding NullReturningEmptyFilter class used in test

---
+++ hibernate-search/src/test/java/org/hibernate/search/test/filter/NullReturningEmptyFilter.java
@@ -0,0 +1,56 @@
+/* 
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+
+package org.hibernate.search.test.filter;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.search.DocIdSet;
+import org.apache.lucene.search.DocIdSetIterator;
+import org.apache.lucene.search.Filter;
+
+/**
+ * Apparently it's legal for Lucene filters to return null
+ * on {@link DocIdSet#iterator()} : make sure we can deal with it as well.
+ * 
+ * @author Sanne Grinovero <sanne@hibernate.org> (C) 2011 Red Hat Inc.
+ */
+public class NullReturningEmptyFilter extends Filter implements Serializable {
+
+	public static final DocIdSet EMPTY_DOCIDSET = new DocIdSet() {
+
+		@Override
+		public DocIdSetIterator iterator() {
+			return null;
+		}
+
+		@Override
+		public boolean isCacheable() {
+			return true;
+		}
+	};
+
+	@Override
+	public DocIdSet getDocIdSet(IndexReader reader) throws IOException {
+		return EMPTY_DOCIDSET;
+	}
+
+}
