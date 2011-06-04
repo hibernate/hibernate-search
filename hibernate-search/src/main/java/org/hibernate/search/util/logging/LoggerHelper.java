27e5f0f3ccb1b10144745e36431a7e8a22b0e594
Davide D'Alto <daltodavide@gmail.com>
2011-06-04 22:55:55 +0100

    HSEARCH-675 Move LoggerHelper to util.logging

--- hibernate-search/src/main/java/org/hibernate/search/util/logging/LoggerHelper.java
+++ hibernate-search/src/main/java/org/hibernate/search/util/logging/LoggerHelper.java
@@ -21,13 +21,12 @@
  * 51 Franklin Street, Fifth Floor
  * Boston, MA  02110-1301  USA
  */
-package org.hibernate.search.util;
+package org.hibernate.search.util.logging;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 
-import org.hibernate.search.util.logging.LoggerFactory;
 import org.slf4j.Logger;
 
 /**
