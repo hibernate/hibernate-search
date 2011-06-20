293ed9610175f916d991cce3edd5ac84c586c04e
Samppa Saarela <samppa.saarela@mysema.com>
2011-06-20 15:20:28 +0300

    HSEARCH-780 Fix for @IndexedEmbedded PersistentSet dirty analysis in a super class
    Wrong collectionRole is used for collection exclusion analysis in
    FullTextIndexEventListener.processCollectionEvent > AbstractDocumentBuilder.isCollectionRoleExcluded check.
    
    A PersistentSet of an abstract superclass has it's collection role bound to the superclass, but AbstractDocumentBuilder's
    indexedEmbeddedCollectionRoles contains roles that are bound to a specific subclass. Thus when the contents of a collection
    in superclass are changed, Hibernate Search fails to re-index the change as the collection gets excluded.

---
+++ hibernate-search/src/test/java/org/hibernate/search/test/embedded/AbstractProduct.java
@@ -0,0 +1,83 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2010, Red Hat, Inc. and/or its affiliates or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat, Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.hibernate.search.test.embedded;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import org.hibernate.search.annotations.DocumentId;
+import org.hibernate.search.annotations.Field;
+import org.hibernate.search.annotations.Index;
+import org.hibernate.search.annotations.IndexedEmbedded;
+
+/**
+ * Abstract base class for products, having e.g. abstract getCode()
+ * returning depending on product type isbn, issn or ean.
+ *
+ * @author Samppa Saarela
+ */
+@Entity
+public abstract class AbstractProduct {
+
+	@Id @GeneratedValue @DocumentId
+	private Integer id;
+
+	@Field(index= Index.TOKENIZED)
+	private String name;
+
+	@ManyToMany(mappedBy="product", cascade=CascadeType.ALL) //just to make the test easier, cascade doesn't really make any business sense
+	@IndexedEmbedded
+	private Set<ProductFeature> features = new HashSet<ProductFeature>();
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<ProductFeature> getFeatures() {
+		return features;
+	}
+
+	public void setFeatures(Set<ProductFeature> authors) {
+		this.features = authors;
+	}
+
+}
