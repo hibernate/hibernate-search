231197bdcea1ede29b81c58e831cae8e34ea4a07
Emmanuel Bernard <emmanuel@hibernate.org>
2011-08-26 15:42:29 +0200

    HSEARCH-872 Move test to cleaner package name

--- hibernate-search/src/test/java/org/hibernate/search/test/serialization/AvroUtils.java
+++ hibernate-search/src/test/java/org/hibernate/search/test/serialization/AvroUtils.java
@@ -18,7 +18,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA  02110-1301, USA.
  */
-package org.hibernate.search.test.remote;
+package org.hibernate.search.test.serialization;
 
 import java.io.BufferedReader;
 import java.io.IOException;
