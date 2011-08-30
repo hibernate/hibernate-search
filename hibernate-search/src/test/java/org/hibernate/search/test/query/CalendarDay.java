17658292a06cf181da22b00f5718a7fdd9f5a788
Sanne Grinovero <sanne@hibernate.org>
2011-08-30 21:55:03 +0200

    HSEARCH-890 Adding a failing test

---
+++ hibernate-search/src/test/java/org/hibernate/search/test/query/CalendarDay.java
@@ -0,0 +1,81 @@
+/* 
+ * Hibernate, Relational Persistence for Idiomatic Java
+ * 
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+package org.hibernate.search.test.query;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
+import org.hibernate.search.annotations.DocumentId;
+import org.hibernate.search.annotations.Field;
+import org.hibernate.search.annotations.FieldBridge;
+import org.hibernate.search.annotations.Index;
+import org.hibernate.search.annotations.Indexed;
+import org.hibernate.search.annotations.Store;
+import org.hibernate.search.test.bridge.DateSplitBridge;
+import org.hibernate.search.test.bridge.PaddedIntegerBridge;
+
+/**
+ * @author Sanne Grinovero <sanne@hibernate.org> (C) 2011 Red Hat Inc.
+ */
+@Entity
+@Indexed(index = "Day" )
+public class CalendarDay {
+
+	private Integer id;
+	private Date day;
+
+	@Id
+	@DocumentId
+	@FieldBridge(impl = PaddedIntegerBridge.class) // test needs to sort on the id
+	@GeneratedValue
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	@Field(index = Index.UN_TOKENIZED, store = Store.YES)
+	@FieldBridge(impl = DateSplitBridge.class)
+	public Date getDay() {
+		return day;
+	}
+
+	public void setDay(Date day) {
+		this.day = day;
+	}
+
+	@Transient
+	public CalendarDay setDayFromItalianString(String day) throws ParseException {
+		DateFormat formatter = DateFormat.getDateInstance( DateFormat.SHORT, Locale.ITALY );
+		this.day = formatter.parse( day );
+		return this;
+	}
+
+}
