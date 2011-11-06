721a1755f405bfa2f09996a644a884c89d14f78b
Davide D'Alto <daltodavide@gmail.com>
2011-11-06 16:57:39 +0000

    HSEARCH-638 Add DepthValidator class

---
+++ hibernate-search-engine/src/main/java/org/hibernate/search/engine/spi/DepthValidator.java
@@ -0,0 +1,65 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2011, Red Hat, Inc. and/or its affiliates or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat, Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.hibernate.search.engine.spi;
+
+/**
+ * Used to check the constraints of depth when using {@link org.hibernate.search.annotations.IndexedEmbedded}
+ * or {@link org.hibernate.search.annotations.ContainedIn} annotations.
+ *
+ * @author Davide D'Alto
+ */
+public class DepthValidator {
+
+	private int maxDepth;
+	private int depth;
+
+	public DepthValidator(int maxDepth) {
+		this.maxDepth = maxDepth;
+	}
+
+	public void increaseDepth() {
+		depth++;
+	}
+
+	public boolean isMaxDepthReached() {
+		return depth > maxDepth;
+	}
+
+	public boolean isMaxDepthInfinite() {
+		return maxDepth == Integer.MAX_VALUE;
+	}
+
+	public int getMaxDepth() {
+		return maxDepth;
+	}
+
+	public void setMaxDepth(int maxDepth) {
+		this.maxDepth = maxDepth;
+	}
+
+	@Override
+	public String toString() {
+		return "[maxDepth=" + maxDepth + ", level=" + depth + "]";
+	}
+}
