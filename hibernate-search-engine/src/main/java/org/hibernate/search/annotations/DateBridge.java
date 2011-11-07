93e5c242452fd34dac55d252ee299c35502370f2
Hardy Ferentschik <hibernate@ferentschik.de>
2011-11-07 19:08:07 +0100

    HSEARCH-680 Import cleanup

--- hibernate-search-engine/src/main/java/org/hibernate/search/annotations/DateBridge.java
+++ hibernate-search-engine/src/main/java/org/hibernate/search/annotations/DateBridge.java
@@ -29,16 +29,14 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import org.hibernate.search.annotations.Resolution;
-
 /**
  * Defines the temporal resolution of a given field
  * Date are stored as String in GMT
  *
  * @author Emmanuel Bernard
  */
-@Retention( RetentionPolicy.RUNTIME )
-@Target( {ElementType.FIELD, ElementType.METHOD} )
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.FIELD, ElementType.METHOD })
 @Documented
 //TODO allow pattern like yyyyMMdd?
 //TODO allow base timezone?
