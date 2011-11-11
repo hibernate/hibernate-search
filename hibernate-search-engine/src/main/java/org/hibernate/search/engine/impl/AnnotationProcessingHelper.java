7a5f26e6da9285e1fcbfad6cff1f61bc057b48e4
Hardy Ferentschik <hibernate@ferentschik.de>
2011-11-11 15:32:57 +0100

    HSEARCH-960 Taking first steps towards resolving HSEARCH-918. Doing this prior to introducing a warning for fields indexed with the same field name, but different analyzer options

---
+++ hibernate-search-engine/src/main/java/org/hibernate/search/engine/impl/AnnotationProcessingHelper.java
@@ -0,0 +1,169 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2010, Red Hat, Inc. and/or its affiliates or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat, Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+
+package org.hibernate.search.engine.impl;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.document.Field;
+
+import org.hibernate.annotations.common.AssertionFailure;
+import org.hibernate.annotations.common.reflection.XProperty;
+import org.hibernate.annotations.common.util.StringHelper;
+import org.hibernate.search.SearchException;
+import org.hibernate.search.annotations.Analyze;
+import org.hibernate.search.annotations.Boost;
+import org.hibernate.search.annotations.DynamicBoost;
+import org.hibernate.search.annotations.Index;
+import org.hibernate.search.annotations.Norms;
+import org.hibernate.search.annotations.NumericField;
+import org.hibernate.search.annotations.TermVector;
+import org.hibernate.search.engine.BoostStrategy;
+import org.hibernate.search.impl.ConfigContext;
+import org.hibernate.search.util.impl.ClassLoaderHelper;
+
+/**
+ * A helper classes dealing with the processing of annotation. It is there to share some annotation processing
+ * between the document builder and other metadata classes, eg {@code FieldMetadata}. In the long run this class
+ * this class might become obsolete
+ *
+ * @author Hardy Ferentschik
+ */
+public final class AnnotationProcessingHelper {
+
+	/**
+	 * Using the passed field (or class bridge) settings determines the Lucene {@link org.apache.lucene.document.Field.Index}
+	 *
+	 * @param index is the field indexed or not
+	 * @param analyze should the field be analyzed
+	 * @param norms are norms to be added to index
+	 *
+	 * @return Returns the Lucene {@link org.apache.lucene.document.Field.Index} value for a given field
+	 */
+	public static Field.Index getIndex(Index index, Analyze analyze, Norms norms) {
+		if ( Index.YES.equals( index ) ) {
+			if ( Analyze.YES.equals( analyze ) ) {
+				if ( Norms.YES.equals( norms ) ) {
+					return Field.Index.ANALYZED;
+				}
+				else {
+					return Field.Index.ANALYZED_NO_NORMS;
+				}
+			}
+			else {
+				if ( Norms.YES.equals( norms ) ) {
+					return Field.Index.NOT_ANALYZED;
+				}
+				else {
+					return Field.Index.NOT_ANALYZED_NO_NORMS;
+				}
+			}
+		}
+		else {
+			return Field.Index.NO;
+		}
+	}
+
+	public static Float getBoost(XProperty member, org.hibernate.search.annotations.Field fieldAnn) {
+		float computedBoost = 1.0f;
+		Boost boostAnn = member.getAnnotation( Boost.class );
+		if ( boostAnn != null ) {
+			computedBoost = boostAnn.value();
+		}
+		if ( fieldAnn != null ) {
+			computedBoost *= fieldAnn.boost().value();
+		}
+		return computedBoost;
+	}
+
+	public static BoostStrategy getDynamicBoost(XProperty member) {
+		DynamicBoost boostAnnotation = member.getAnnotation( DynamicBoost.class );
+		if ( boostAnnotation == null ) {
+			return new DefaultBoostStrategy();
+		}
+
+		Class<? extends BoostStrategy> boostStrategyClass = boostAnnotation.impl();
+		BoostStrategy strategy;
+		try {
+			strategy = boostStrategyClass.newInstance();
+		}
+		catch ( Exception e ) {
+			throw new SearchException(
+					"Unable to instantiate boost strategy implementation: " + boostStrategyClass.getName()
+			);
+		}
+		return strategy;
+	}
+
+	public static Field.TermVector getTermVector(TermVector vector) {
+		switch ( vector ) {
+			case NO:
+				return Field.TermVector.NO;
+			case YES:
+				return Field.TermVector.YES;
+			case WITH_OFFSETS:
+				return Field.TermVector.WITH_OFFSETS;
+			case WITH_POSITIONS:
+				return Field.TermVector.WITH_POSITIONS;
+			case WITH_POSITION_OFFSETS:
+				return Field.TermVector.WITH_POSITIONS_OFFSETS;
+			default:
+				throw new AssertionFailure( "Unexpected TermVector: " + vector );
+		}
+	}
+
+	public static Analyzer getAnalyzer(org.hibernate.search.annotations.Analyzer analyzerAnn, ConfigContext context) {
+		Class<?> analyzerClass = analyzerAnn == null ? void.class : analyzerAnn.impl();
+		if ( analyzerClass == void.class ) {
+			String definition = analyzerAnn == null ? "" : analyzerAnn.definition();
+			if ( StringHelper.isEmpty( definition ) ) {
+				return null;
+			}
+			else {
+				return context.buildLazyAnalyzer( definition );
+			}
+		}
+		else {
+			try {
+				return ClassLoaderHelper.analyzerInstanceFromClass( analyzerClass, context.getLuceneMatchVersion() );
+			}
+			catch ( ClassCastException e ) {
+				throw new SearchException(
+						"Lucene analyzer does not extend " + Analyzer.class.getName() + ": " + analyzerClass.getName(),
+						e
+				);
+			}
+			catch ( Exception e ) {
+				throw new SearchException(
+						"Failed to instantiate lucene analyzer with type " + analyzerClass.getName(), e
+				);
+			}
+		}
+	}
+
+	public static Integer getPrecisionStep(NumericField numericFieldAnn) {
+		return numericFieldAnn == null ? NumericField.PRECISION_STEP_DEFAULT : numericFieldAnn.precisionStep();
+	}
+}
+
+
---
+++ hibernate-search-engine/src/main/java/org/hibernate/search/engine/impl/FieldMetadata.java
@@ -0,0 +1,135 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2010, Red Hat, Inc. and/or its affiliates or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat, Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.hibernate.search.engine.impl;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.document.Field;
+
+import org.hibernate.annotations.common.AssertionFailure;
+import org.hibernate.annotations.common.reflection.ReflectionManager;
+import org.hibernate.annotations.common.reflection.XAnnotatedElement;
+import org.hibernate.annotations.common.reflection.XProperty;
+import org.hibernate.annotations.common.util.StringHelper;
+import org.hibernate.search.SearchException;
+import org.hibernate.search.annotations.Boost;
+import org.hibernate.search.annotations.DynamicBoost;
+import org.hibernate.search.annotations.NumericField;
+import org.hibernate.search.annotations.Store;
+import org.hibernate.search.annotations.TermVector;
+import org.hibernate.search.bridge.FieldBridge;
+import org.hibernate.search.bridge.TwoWayFieldBridge;
+import org.hibernate.search.bridge.builtin.impl.NullEncodingTwoWayFieldBridge;
+import org.hibernate.search.bridge.impl.BridgeFactory;
+import org.hibernate.search.engine.BoostStrategy;
+import org.hibernate.search.engine.spi.AbstractDocumentBuilder;
+import org.hibernate.search.impl.ConfigContext;
+import org.hibernate.search.util.impl.ClassLoaderHelper;
+import org.hibernate.search.util.impl.ReflectionHelper;
+
+/**
+ * Encapsulating the metadata for a single {@code @Field} annotation.
+ *
+ * @author Hardy Ferentschik
+ */
+public class FieldMetadata {
+	private final XProperty fieldGetter;
+	private final String fieldName;
+	private final Store store;
+	private final Field.Index index;
+	private final Float boost;
+	private final BoostStrategy dynamicBoostStrategy;
+	private final Field.TermVector termVector;
+	private final Integer precisionStep;
+	private final String nullToken;
+	private final FieldBridge fieldBridge;
+	private final Analyzer analyzer;
+
+	public FieldMetadata(String prefix,
+						 XProperty member,
+						 org.hibernate.search.annotations.Field fieldAnn,
+						 NumericField numericFieldAnn,
+						 ConfigContext context,
+						 ReflectionManager reflectionManager) {
+		ReflectionHelper.setAccessible( member );
+		fieldGetter = member;
+		fieldName = prefix + ReflectionHelper.getAttributeName( member, fieldAnn.name() );
+		store = fieldAnn.store();
+		index = AnnotationProcessingHelper.getIndex( fieldAnn.index(), fieldAnn.analyze(), fieldAnn.norms() );
+		boost = AnnotationProcessingHelper.getBoost( member, fieldAnn );
+		dynamicBoostStrategy = AnnotationProcessingHelper.getDynamicBoost( member );
+		termVector = AnnotationProcessingHelper.getTermVector( fieldAnn.termVector() );
+		precisionStep = AnnotationProcessingHelper.getPrecisionStep( numericFieldAnn );
+
+		// null token
+		String indexNullAs = fieldAnn.indexNullAs();
+		if ( indexNullAs.equals( org.hibernate.search.annotations.Field.DO_NOT_INDEX_NULL ) ) {
+			indexNullAs = null;
+		}
+		else if ( indexNullAs.equals( org.hibernate.search.annotations.Field.DEFAULT_NULL_TOKEN ) ) {
+			indexNullAs = context.getDefaultNullToken();
+		}
+		nullToken = indexNullAs;
+
+		FieldBridge bridge = BridgeFactory.guessType( fieldAnn, numericFieldAnn, member, reflectionManager );
+		if ( indexNullAs != null && bridge instanceof TwoWayFieldBridge ) {
+			bridge = new NullEncodingTwoWayFieldBridge( (TwoWayFieldBridge) bridge, indexNullAs );
+		}
+		fieldBridge = bridge;
+
+		// Field > property > entity analyzer
+		Analyzer tmpAnalyzer = AnnotationProcessingHelper.getAnalyzer( fieldAnn.analyzer(), context );
+		if ( tmpAnalyzer == null ) {
+			tmpAnalyzer = AnnotationProcessingHelper.getAnalyzer( member.getAnnotation( org.hibernate.search.annotations.Analyzer.class ), context );
+		}
+		analyzer = tmpAnalyzer;
+	}
+
+	public String getFieldName() {
+		return fieldName;
+	}
+
+	public Field.Index getIndex() {
+		return index;
+	}
+
+	public Analyzer getAnalyzer() {
+		return analyzer;
+	}
+
+	public void appendToPropertiesMetadata(AbstractDocumentBuilder.PropertiesMetadata propertiesMetadata) {
+		propertiesMetadata.fieldGetters.add( fieldGetter );
+		propertiesMetadata.fieldNames.add( fieldName );
+		propertiesMetadata.fieldNameToPositionMap.put( fieldGetter.getName(), propertiesMetadata.fieldNames.size() );
+		propertiesMetadata.fieldStore.add( store );
+		propertiesMetadata.fieldIndex.add( index );
+		propertiesMetadata.fieldBoosts.add( boost );
+		propertiesMetadata.dynamicFieldBoosts.add( dynamicBoostStrategy );
+		propertiesMetadata.fieldTermVectors.add( termVector );
+		propertiesMetadata.precisionSteps.add( precisionStep );
+		propertiesMetadata.fieldNullTokens.add( nullToken );
+		propertiesMetadata.fieldBridges.add( fieldBridge );
+	}
+}
+
+
