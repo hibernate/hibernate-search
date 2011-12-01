9efbb47e023fb044638fee2ef7b5a41cae6b3052
Hardy Ferentschik <hibernate@ferentschik.de>
2011-12-01 17:09:05 +0100

    HSEARCH-996 Fixing some typos and adding javadocs

--- hibernate-search-orm/src/main/java/org/hibernate/search/jpa/impl/OptimisticLockingCompatibilityHelper.java
+++ hibernate-search-orm/src/main/java/org/hibernate/search/jpa/impl/OptimisticLockingCompatibilityHelper.java
@@ -26,30 +26,35 @@ import org.hibernate.HibernateException;
 import org.hibernate.Session;
 
 /**
- * Helper class for FullTextQueryImpl to extract information out of an
- * Hibernate OptimisticLockException and create the JPA counterpart.
+ * Helper class for {@code FullTextQueryImpl} to extract information out of an
+ * Hibernate {@code OptimisticLockException} and create the JPA counterpart.
  *
  * @author Sanne Grinovero <sanne@hibernate.org> (C) 2011 Red Hat Inc.
  */
 public class OptimisticLockingCompatibilityHelper {
 
-	//This is the new classname, and is extended by the deprecated one so it is compatible with both types
+	//This is the new class name, and is extended by the deprecated one so it is compatible with both types
 	private static final String parentOptimisticLockExceptionClassName = "org.hibernate.dialect.lock.OptimisticEntityLockException";
-	private static final String deprecatedOtimisticLockExceptionClassName =  "org.hibernate.OptimisticLockException";
-	private static final Class optimisticLockExceptionClass = figureCompatibleOptimisticLockExceptionClass();
+	private static final String deprecatedOptimisticLockExceptionClassName = "org.hibernate.OptimisticLockException";
+	private static final Class optimisticLockExceptionClass = determineCompatibleOptimisticLockExceptionClass();
 
 	/**
-	 * Looks for the new Hibernate class name, or fallbacks to the older one.
+	 * Looks for the new Hibernate class name, or falls back to the older one.
+	 *
 	 * @return the type of optimistic lock exceptions which Hibernate is going to throw
 	 */
-	private static Class figureCompatibleOptimisticLockExceptionClass() {
+	private static Class determineCompatibleOptimisticLockExceptionClass() {
 		try {
 			return Class.forName( parentOptimisticLockExceptionClassName, true, Session.class.getClassLoader() );
 		}
 		catch ( ClassNotFoundException e ) {
 			// the failing class was introduced in Hibernate Core 4.0.0.CR7 only; fall back to old name when it's not found:
 			try {
-				return Class.forName( deprecatedOtimisticLockExceptionClassName, true, Session.class.getClassLoader() );
+				return Class.forName(
+						deprecatedOptimisticLockExceptionClassName,
+						true,
+						Session.class.getClassLoader()
+				);
 			}
 			catch ( ClassNotFoundException e1 ) {
 				// this is fatal, will need to check for null at runtime
@@ -60,15 +65,18 @@ public class OptimisticLockingCompatibilityHelper {
 
 	/**
 	 * We might need different ways to extract the error message according to what is available at runtime
-	 * @param e
-	 * @return
+	 *
+	 * @param e the Hibernate exception
+	 *
+	 * @return Returns the entity passed as part of the exception context. {@code null} is returned if no entity is
+	 *         provided within the exception
 	 */
 	private static Object extractEntityOufOfException(HibernateException e) {
 		if ( parentOptimisticLockExceptionClassName.equals( optimisticLockExceptionClass.getName() ) ) {
 			org.hibernate.dialect.lock.OptimisticEntityLockException oele = (org.hibernate.dialect.lock.OptimisticEntityLockException) e;
 			return oele.getEntity();
 		}
-		else if ( deprecatedOtimisticLockExceptionClassName.equals( optimisticLockExceptionClass.getName() ) ) {
+		else if ( deprecatedOptimisticLockExceptionClassName.equals( optimisticLockExceptionClass.getName() ) ) {
 			org.hibernate.OptimisticLockException oele = (org.hibernate.OptimisticLockException) e;
 			return oele.getEntity();
 		}
@@ -77,6 +85,7 @@ public class OptimisticLockingCompatibilityHelper {
 
 	/**
 	 * @param e an Hibernate runtime exception
+	 *
 	 * @return true if it's definitely an optimistic locking exception, false if we can't tell
 	 */
 	public static boolean isOptimisticLockException(HibernateException e) {
@@ -84,11 +93,15 @@ public class OptimisticLockingCompatibilityHelper {
 	}
 
 	/**
-	 * Convert to a JPA exception; entity information is added only if possible
+	 * Convert a provided Hibernate exception to a JPA exception. If possible entity information is added.
+	 *
+	 * @param e the Hibernate exception to convert
+	 *
+	 * @return A JPA optimistic lock exception
 	 */
 	public static OptimisticLockException convertToLockException(HibernateException e) {
 		Object entity = extractEntityOufOfException( e );
-		if ( entity != null) {
+		if ( entity != null ) {
 			return new OptimisticLockException( e.getMessage(), e, entity );
 		}
 		else {
