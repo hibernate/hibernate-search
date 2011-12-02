6f3a2b7a9f63f1f645e7ec78040abc16938f982f
Hardy Ferentschik <hibernate@ferentschik.de>
2011-12-02 17:23:23 +0100

    HSEARCH-999 Removing auto generated id and switching to assigned id. Also enabling batch inserting. These changes will make debugging against a remote db faster.
    Also refactoring the tests.

--- hibernate-search-orm/src/test/java/org/hibernate/search/test/query/timeout/Clock.java
+++ hibernate-search-orm/src/test/java/org/hibernate/search/test/query/timeout/Clock.java
@@ -1,7 +1,29 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2010, Red Hat, Inc. and/or its affiliates or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat, Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
 package org.hibernate.search.test.query.timeout;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 import org.hibernate.search.annotations.Field;
@@ -13,32 +35,56 @@ import org.hibernate.search.annotations.Indexed;
 @Entity
 @Indexed
 public class Clock {
-	public Clock() {}
-	
-	public Clock(String model, String brand, Long durability) {
+	private Long id;
+	private String model;
+	private String brand;
+	private Long durability;
+
+	public Clock() {
+	}
+
+	public Clock(Long id, String model, String brand, Long durability) {
+		this.id = id;
 		this.model = model;
 		this.brand = brand;
 		this.durability = durability;
 	}
 
 	@Id
-	@GeneratedValue
-	public Long getId() { return id; }
-	public void setId(Long id) {  this.id = id; }
-	private Long id;
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 
 	@Field
-	public String getModel() { return model; }
-	public void setModel(String model) {  this.model = model; }
-	private String model;
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
 
 	@Field
-	public String getBrand() { return brand; }
-	public void setBrand(String brand) {  this.brand = brand; }
-	private String brand;
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
 
 	@Field
-	public Long getDurability() { return durability; }
-	public void setDurability(Long durability) {  this.durability = durability; }
-	private Long durability;
+	public Long getDurability() {
+		return durability;
+	}
+
+	public void setDurability(Long durability) {
+		this.durability = durability;
+	}
 }
