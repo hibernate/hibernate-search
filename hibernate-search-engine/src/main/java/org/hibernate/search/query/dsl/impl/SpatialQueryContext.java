3859c63c353d2d69bc1cc5ffa7d13c5dcfb2ed3b
Emmanuel Bernard <emmanuel@hibernate.org>
2011-12-20 19:04:21 +0100

    HSEARCH-923 Add first cut to spatial query DSL

---
+++ hibernate-search-engine/src/main/java/org/hibernate/search/query/dsl/impl/SpatialQueryContext.java
@@ -0,0 +1,81 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+package org.hibernate.search.query.dsl.impl;
+
+import org.hibernate.search.query.dsl.Unit;
+import org.hibernate.search.spatial.Coordinates;
+
+/**
+ * @author Emmanuel Bernard <emmanuel@hibernate.org>
+ */
+public class SpatialQueryContext {
+	private String coordinatesField;
+	private String latitudeField;
+	private String longitudeField;
+	private double radiusDistance;
+	private Unit distanceUnit;
+	private Coordinates coordinates;
+
+	public String getCoordinatesField() {
+		return coordinatesField;
+	}
+
+	public void setCoordinatesField(String coordinatesField) {
+		this.coordinatesField = coordinatesField;
+	}
+
+	public String getLatitudeField() {
+		return latitudeField;
+	}
+
+	public void setLatitudeField(String latitudeField) {
+		this.latitudeField = latitudeField;
+	}
+
+	public String getLongitudeField() {
+		return longitudeField;
+	}
+
+	public void setLongitudeField(String longitudeField) {
+		this.longitudeField = longitudeField;
+	}
+
+	public double getRadiusDistance() {
+		return radiusDistance;
+	}
+
+	public Unit getDistanceUnit() {
+		return distanceUnit;
+	}
+
+	public Coordinates getCoordinates() {
+		return coordinates;
+	}
+
+	public void setCoordinates(Coordinates coordinates) {
+		this.coordinates = coordinates;
+	}
+
+	public void setRadius(double distance, Unit unit) {
+		this.radiusDistance = distance;
+		this.distanceUnit = unit;
+	}
+}
