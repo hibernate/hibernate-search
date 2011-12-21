8858ca27474bfed441837f8c682c6d143b477a2e
Nicolas Helleringer <nicolas.helleringer@novacodex.net>
2011-12-21 09:18:31 +0100

    HSEARCH-923 HSEARCH-1010 Reworded Hibernate Search Spatial Index (HSSI) fields for more readability

--- hibernate-search-engine/src/main/java/org/hibernate/search/spatial/impl/GridHelper.java
+++ hibernate-search-engine/src/main/java/org/hibernate/search/spatial/impl/GridHelper.java
@@ -226,16 +226,16 @@ public abstract class GridHelper {
 		return projectedCoordinates;
 	}
 
-	private static final String FIELDNAME_TEMPLATE = "HSSI_%s_%s";
+	private static final String FIELDNAME_TEMPLATE = "%s_HSSI_%s";
 
-	private static final String LATITUDE_TEMPLATE = "HSSI_Latitude_%s";
+	private static final String LATITUDE_TEMPLATE = "%s_HSSI_Latitude";
 
-	private static final String LONGITUDE_TEMPLATE = "HSSI_Longitude_%s";
+	private static final String LONGITUDE_TEMPLATE = "%s_HSSI_Longitude";
 
 	private static final String GRID_CELL_ID_TEMPLATE = "%s|%s";
 
 	public static String formatFieldName(int gridLevel, String fieldName) {
-		return String.format( FIELDNAME_TEMPLATE, gridLevel, fieldName );
+		return String.format( FIELDNAME_TEMPLATE, fieldName, gridLevel);
 	}
 
 	public static String formatLatitude(String fieldName) {
