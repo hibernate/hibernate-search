5c74dbb0206d49f72e76d4ef282e8f8243c9f5a3
Nicolas Helleringer <nicolas.helleringer@novacodex.net>
2011-12-23 15:12:07 +0100

    HSEARCH-923 Camel case variables names + refactor coslty dual call

--- hibernate-search-engine/src/main/java/org/hibernate/search/spatial/SpatialFieldBridge.java
+++ hibernate-search-engine/src/main/java/org/hibernate/search/spatial/SpatialFieldBridge.java
@@ -40,17 +40,17 @@ public class SpatialFieldBridge implements FieldBridge, ParameterizedBridge {
 	public static final int MIN_GRID_LEVEL = 0;
 	public static final int MAX_GRID_LEVEL = 16;
 
-	private int min_grid_level = MIN_GRID_LEVEL;
-	private int max_grid_level = MAX_GRID_LEVEL;
+	private int highGridLevel = MIN_GRID_LEVEL;
+	private int lowGridLevel = MAX_GRID_LEVEL;
 
-	private boolean grid_index = true;
-	private boolean numeric_fields_index = true;
+	private boolean gridIndex = true;
+	private boolean numericFieldsIndex = true;
 
 	public SpatialFieldBridge() {}
 
-	public SpatialFieldBridge( int min_grid_level, int max_grid_level ) {
-		this.min_grid_level= min_grid_level;
-		this.max_grid_level= max_grid_level;
+	public SpatialFieldBridge( int highGridLevel, int lowGridLevel ) {
+		this.highGridLevel = highGridLevel;
+		this.lowGridLevel = lowGridLevel;
 	}
 
 	/**
@@ -67,17 +67,17 @@ public class SpatialFieldBridge implements FieldBridge, ParameterizedBridge {
 
 			Coordinates coordinates = (Coordinates) value;
 
-			if( grid_index ) {
+			if( gridIndex ) {
 				Point point = Point.fromDegrees( coordinates.getLatitude(), coordinates.getLongitude() );
 
-				Map<Integer, String> cellIds = GridHelper.getGridCellsIds( point, min_grid_level, max_grid_level );
+				Map<Integer, String> cellIds = GridHelper.getGridCellsIds( point, highGridLevel, lowGridLevel );
 
-				for ( int i = min_grid_level; i <= max_grid_level; i++ ) {
+				for ( int i = highGridLevel; i <= lowGridLevel; i++ ) {
 					luceneOptions.addFieldToDocument( GridHelper.formatFieldName( i, name ), cellIds.get( i ), document );
 				}
 			}
 
-			if( numeric_fields_index ) {
+			if( numericFieldsIndex ) {
 				luceneOptions.addNumericFieldToDocument(
 						GridHelper.formatLatitude( name ),
 						coordinates.getLatitude(),
@@ -96,25 +96,25 @@ public class SpatialFieldBridge implements FieldBridge, ParameterizedBridge {
 	/**
 	 * Override method for default min and max grid level
 	 *
-	 * @param parameters Map containing the min_grid_level and max_grid_level values
+	 * @param parameters Map containing the highGridLevel and lowGridLevel values
 	 */
 	@Override
 	public void setParameterValues(Map parameters) {
-		Object min_grid_level = parameters.get( "min_grid_level" );
-		if ( min_grid_level instanceof Integer ) {
-			this.min_grid_level = ( Integer ) min_grid_level;
+		Object highGridLevel = parameters.get( "highGridLevel" );
+		if ( highGridLevel instanceof Integer ) {
+			this.highGridLevel = ( Integer ) highGridLevel;
 		}
-		Object max_grid_level = parameters.get( "min_grid_level" );
-		if ( max_grid_level instanceof Integer ) {
-			this.max_grid_level = ( Integer ) max_grid_level;
+		Object lowGridLevel = parameters.get( "highGridLevel" );
+		if ( lowGridLevel instanceof Integer ) {
+			this.lowGridLevel = ( Integer ) lowGridLevel;
 		}
-		Object grid_index = parameters.get( "grid_index" );
-		if ( grid_index instanceof Boolean ) {
-			this.grid_index = ( Boolean ) grid_index;
+		Object gridIndex = parameters.get( "gridIndex" );
+		if ( gridIndex instanceof Boolean ) {
+			this.gridIndex = ( Boolean ) gridIndex;
 		}
-		Object numeric_fields_index = parameters.get( "numeric_fields_index" );
-		if ( numeric_fields_index instanceof Boolean ) {
-			this.numeric_fields_index = ( Boolean ) numeric_fields_index;
+		Object numericFieldsIndex = parameters.get( "numericFieldsIndex" );
+		if ( numericFieldsIndex instanceof Boolean ) {
+			this.numericFieldsIndex = ( Boolean ) numericFieldsIndex;
 		}
 	}
 }
