e68ea6f69c8b635f58671e55be07466bf000685c
Laurent Almeras <lalmeras@gmail.com>
2012-01-07 23:00:54 +0100

    HSEARCH-1030 failing test: when collectionbridge is used on an entity included with @IndexEmbedded, reindex fails with LazyInitializationException

---
+++ hibernate-search-orm/src/test/java/org/hibernate/search/test/embedded/fieldoncollection/LazyIndirectCollectionBridgeReindexTest.java
@@ -0,0 +1,98 @@
+/* 
+ * Hibernate, Relational Persistence for Idiomatic Java
+ * 
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+package org.hibernate.search.test.embedded.fieldoncollection;
+
+import java.util.List;
+
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TermQuery;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.search.FullTextQuery;
+import org.hibernate.search.FullTextSession;
+import org.hibernate.search.MassIndexer;
+import org.hibernate.search.Search;
+import org.hibernate.search.test.SearchTestCase;
+import org.hibernate.search.test.embedded.depth.Person;
+
+public class LazyIndirectCollectionBridgeReindexTest extends SearchTestCase {
+
+	public void testLazyIndirectCollectionBridgeReindex() throws InterruptedException {
+		prepareEntities();
+		verifyMatchExistsWithName( "name", "name" );
+
+		Session session = openSession();
+		FullTextSession fullTextSession = Search.getFullTextSession( session );
+		MassIndexer massIndexer = fullTextSession.createIndexer( Root.class );
+		massIndexer.startAndWait();
+		verifyMatchExistsWithName( "name", "name" );
+	}
+
+	private void verifyMatchExistsWithName(String fieldName, String fieldValue) {
+		FullTextSession fullTextSession = Search.getFullTextSession( openSession() );
+		try {
+			Transaction transaction = fullTextSession.beginTransaction();
+			Query q = new TermQuery( new Term( "name", "name" ) );
+			FullTextQuery fullTextQuery = fullTextSession.createFullTextQuery( q );
+			int resultSize = fullTextQuery.getResultSize();
+			assertEquals( 1, resultSize );
+
+			@SuppressWarnings("unchecked")
+			List<Person> list = fullTextQuery.list();
+			assertEquals( 1, list.size() );
+			transaction.commit();
+		}
+		finally {
+			fullTextSession.close();
+		}
+	}
+
+	private void prepareEntities() {
+		Session session = openSession();
+		try {
+			Transaction transaction = session.beginTransaction();
+
+			CollectionItem bridgedEntity = new CollectionItem();
+			session.save( bridgedEntity );
+
+			Leaf leaf = new Leaf();
+			leaf.getCollectionItems().add( bridgedEntity );
+			session.save( leaf );
+
+			Root root = new Root();
+			root.setName( "name" );
+			root.setLeaf( leaf );
+			session.save( root );
+
+			transaction.commit();
+		}
+		finally {
+			session.close();
+		}
+	}
+
+	@Override
+	protected Class<?>[] getAnnotatedClasses() {
+		return new Class<?>[] { CollectionItem.class, Leaf.class, Root.class };
+	}
+
+}
---
+++ hibernate-search-orm/src/test/java/org/hibernate/search/test/embedded/fieldoncollection/Leaf.java
@@ -0,0 +1,63 @@
+/* 
+ * Hibernate, Relational Persistence for Idiomatic Java
+ * 
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+package org.hibernate.search.test.embedded.fieldoncollection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import org.hibernate.search.annotations.DocumentId;
+import org.hibernate.search.annotations.Field;
+import org.hibernate.search.annotations.FieldBridge;
+
+@Entity
+public class Leaf {
+
+	@DocumentId
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	@OneToMany
+	@Field(bridge = @FieldBridge(impl = CollectionItemFieldBridge.class))
+	private List<CollectionItem> collectionItems = new ArrayList<CollectionItem>();
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public List<CollectionItem> getCollectionItems() {
+		return collectionItems;
+	}
+
+	public void setBridgedEntities(List<CollectionItem> collectionItems) {
+		this.collectionItems = collectionItems;
+	}
+
+}
