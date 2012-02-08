ee921fadc69a08c47665fb642a518be8cd8fe931
Nicolas Helleringer <nicolas.helleringer@novacodex.net>
2012-02-08 11:07:49 +0100

    HSEARCH-923 Implement a simple spatial bridge (double range query on lat and long fields)

--- hibernate-search-orm/src/test/java/org/hibernate/search/test/spatial/SimpleHotel.java
+++ hibernate-search-orm/src/test/java/org/hibernate/search/test/spatial/SimpleHotel.java
@@ -1,29 +1,24 @@
 package org.hibernate.search.test.spatial;
 
-import org.hibernate.search.annotations.Analyze;
 import org.hibernate.search.annotations.Field;
-import org.hibernate.search.annotations.FieldBridge;
 import org.hibernate.search.annotations.Index;
 import org.hibernate.search.annotations.Indexed;
-import org.hibernate.search.annotations.NumericField;
 import org.hibernate.search.annotations.Spatial;
 import org.hibernate.search.annotations.Store;
 import org.hibernate.search.spatial.Coordinates;
-import org.hibernate.search.spatial.SpatialFieldBridge;
 
-import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 /**
- * Hibernate Search spatial : Point Of Interest test entity
+ * Hibernate Search spatial : Hotel test entity with Simple Spatial indexation
  *
  * @author Nicolas Helleringer <nicolas.helleringer@novacodex.net>
  */
 @Entity
 @Indexed
-@Spatial(name = "location", gridMode = true)
-public class Hotel implements Coordinates {
+@Spatial
+public class SimpleHotel implements Coordinates {
 	@Id
 	Integer id;
 
@@ -33,10 +28,12 @@ public class Hotel implements Coordinates {
 	@Field(store = Store.YES, index = Index.YES)
 	String type;
 
+	@Field
 	double latitude;
+	@Field
 	double longitude;
 
-	public Hotel( Integer id, String name, double latitude, double longitude, String type ) {
+	public SimpleHotel( Integer id, String name, double latitude, double longitude, String type ) {
 		this.id = id;
 		this.name = name;
 		this.latitude = latitude;
@@ -44,7 +41,7 @@ public class Hotel implements Coordinates {
 		this.type= type;
 	}
 
-	public Hotel() {
+	public SimpleHotel() {
 	}
 
 	public Integer getId() {
