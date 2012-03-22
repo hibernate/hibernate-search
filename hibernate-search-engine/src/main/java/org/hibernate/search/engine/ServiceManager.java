828295df1f9f85e255c9c586fa55107287a3dd7f
Sanne Grinovero <sanne@hibernate.org>
2012-03-22 00:11:59 +0000

    HSEARCH-1072 style and missing copyright

--- hibernate-search-engine/src/main/java/org/hibernate/search/engine/ServiceManager.java
+++ hibernate-search-engine/src/main/java/org/hibernate/search/engine/ServiceManager.java
@@ -1,3 +1,26 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2012, Red Hat, Inc. and/or its affiliates or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat, Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
 package org.hibernate.search.engine;
 
 import java.io.BufferedReader;
@@ -101,14 +124,14 @@ public class ServiceManager {
 		}
 		
 		final ServiceProviderWrapper wrapper = managedProviders.get( serviceProviderClass );
-		if (wrapper == null) {
+		if ( wrapper == null ) {
 			throw new SearchException( "Unable to find service related to " + serviceProviderClass);
 		}
 		wrapper.decreaseCounter();
 	}
 
 	public void stopServices() {
-		for (ServiceProviderWrapper wrapper :  managedProviders.values() ) {
+		for ( ServiceProviderWrapper wrapper :  managedProviders.values() ) {
 			if ( wrapper.getCounter() != 0 ) {
 				log.serviceProviderNotReleased( wrapper.getServiceProvider().getClass() );
 			}
@@ -136,7 +159,7 @@ public class ServiceManager {
 
 		synchronized void increaseCounter() {
 			final int oldValue = counter.getAndIncrement();
-			if (oldValue == 0) {
+			if ( oldValue == 0 ) {
 				serviceProvider.start( properties );
 			}
 		}
