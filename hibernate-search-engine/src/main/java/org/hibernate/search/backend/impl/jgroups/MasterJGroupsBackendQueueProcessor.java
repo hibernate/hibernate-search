ad38afdc4287b83bdfc30707e721ad1aac8b82a1
Sanne Grinovero <sanne@hibernate.org>
2012-03-27 18:07:48 +0100

    HSEARCH-1070 simplify MasterJGroupsBackendQueueProcessor

--- hibernate-search-engine/src/main/java/org/hibernate/search/backend/impl/jgroups/MasterJGroupsBackendQueueProcessor.java
+++ hibernate-search-engine/src/main/java/org/hibernate/search/backend/impl/jgroups/MasterJGroupsBackendQueueProcessor.java
@@ -49,16 +49,11 @@ public class MasterJGroupsBackendQueueProcessor extends JGroupsBackendQueueProce
 
 	private static final Log log = LoggerFactory.make();
 
-	private LuceneBackendQueueProcessor luceneBackendQueueProcessor;
+	private final LuceneBackendQueueProcessor luceneBackendQueueProcessor = new LuceneBackendQueueProcessor();
 
 	@Override
 	public void initialize(Properties props, WorkerBuildContext context, DirectoryBasedIndexManager indexManager) {
 		super.initialize( props, context, indexManager );
-		initLuceneBackendQueueProcessor( props, context );
-	}
-
-	private void initLuceneBackendQueueProcessor(Properties props, WorkerBuildContext context) {
-		luceneBackendQueueProcessor = new LuceneBackendQueueProcessor();
 		luceneBackendQueueProcessor.initialize( props, context, indexManager );
 	}
 
@@ -70,9 +65,6 @@ public class MasterJGroupsBackendQueueProcessor extends JGroupsBackendQueueProce
 
 	@Override
 	public void applyWork(List<LuceneWork> workList, IndexingMonitor monitor) {
-		if ( workList == null ) {
-			throw new IllegalArgumentException( "workList should not be null" );
-		}
 		luceneBackendQueueProcessor.applyWork( workList, monitor );
 	}
 
