3d6f53068163ed65e4561b37ef7e903c32b55df2
Nicolas Helleringer <nicolas.helleringer@novacodex.net>
2012-04-30 08:13:26 +0200

    HSEARCH-923 Fix large radius query on Grid mode

--- hibernate-search-orm/src/test/java/org/hibernate/search/test/spatial/SpatialIndexingTest.java
+++ hibernate-search-orm/src/test/java/org/hibernate/search/test/spatial/SpatialIndexingTest.java
@@ -22,10 +22,20 @@ public class SpatialIndexingTest extends SearchTestCase {
 
 	public void testIndexing() throws Exception {
 		POI poi = new POI( 1, "Test", 24.0d, 32.0d, "" );
+		POI poi2= new POI(  2, "Test2", 0.0d, -179.0d, "" );
+		POI poi3= new POI(  3, "Test3", 0.0d, 179.0d, "" );
+		POI poi4= new POI(  4, "Test4", 89.0d, 1.0d, "" );
+		POI poi5= new POI(  5, "Test5", -90.0d, 17.0d, "" );
+		POI poi6= new POI(  6, "Test6", 47.0d, 154.0d, "" );
 		FullTextSession fullTextSession = Search.getFullTextSession( openSession() );
 
 		Transaction tx = fullTextSession.beginTransaction();
 		fullTextSession.save( poi );
+		fullTextSession.save( poi2 );
+		fullTextSession.save( poi3 );
+		fullTextSession.save( poi4 );
+		fullTextSession.save( poi5 );
+		fullTextSession.save( poi6 );
 		tx.commit();
 
 		tx = fullTextSession.beginTransaction();
@@ -52,6 +62,26 @@ public class SpatialIndexingTest extends SearchTestCase {
 		org.hibernate.Query hibQuery2 = fullTextSession.createFullTextQuery( luceneQuery2, POI.class );
 		List results2 = hibQuery2.list();
 		Assert.assertEquals( 1, results2.size() );
+
+		org.apache.lucene.search.Query luceneQuery3 = SpatialQueryBuilder.buildSpatialQuery(
+				0.0d,
+				180.0d,
+				112,
+				"location"
+		);
+		org.hibernate.Query hibQuery3 = fullTextSession.createFullTextQuery( luceneQuery3, POI.class );
+		List results3 = hibQuery3.list();
+		Assert.assertEquals( 2, results3.size() );
+
+		org.apache.lucene.search.Query luceneQuery4 = SpatialQueryBuilder.buildSpatialQuery(
+				0.0d,
+				0.0d,
+				100000,
+				"location"
+		);
+		org.hibernate.Query hibQuery4 = fullTextSession.createFullTextQuery( luceneQuery4, POI.class );
+		List results4 = hibQuery4.list();
+		Assert.assertEquals( 6, results4.size() );
 
 		List<?> pois = fullTextSession.createQuery( "from " + POI.class.getName() ).list();
 		for (Object entity : pois) {
