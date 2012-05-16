73233eb2a33487bc488bc36eb240a7d49e0f40e0
Emmanuel Bernard <emmanuel@hibernate.org>
2012-05-16 14:50:39 +0200

    HSEARCH-923 Fix style and add license

--- hibernate-search-engine/src/main/java/org/hibernate/search/spatial/impl/SpatialQueryBuilderFromPoint.java
+++ hibernate-search-engine/src/main/java/org/hibernate/search/spatial/impl/SpatialQueryBuilderFromPoint.java
@@ -190,15 +190,15 @@ public abstract class SpatialQueryBuilderFromPoint {
 		);
 
 		Query longQuery= null;
-		if( boundingBox.getLowerLeft().getLongitude() <= boundingBox.getUpperRight().getLongitude()) {
-			longQuery= NumericRangeQuery.newDoubleRange( "longitude_hibernate_search_spatial", boundingBox.getLowerLeft().getLongitude(),
+		if ( boundingBox.getLowerLeft().getLongitude() <= boundingBox.getUpperRight().getLongitude() ) {
+			longQuery = NumericRangeQuery.newDoubleRange( "longitude_hibernate_search_spatial", boundingBox.getLowerLeft().getLongitude(),
 					boundingBox.getUpperRight().getLongitude(), true, true);
 		}
 		else {
 			longQuery= new BooleanQuery();
-			((BooleanQuery)longQuery).add( NumericRangeQuery.newDoubleRange( "longitude_hibernate_search_spatial", boundingBox.getLowerLeft().getLongitude(),
+			( (BooleanQuery) longQuery).add( NumericRangeQuery.newDoubleRange( "longitude_hibernate_search_spatial", boundingBox.getLowerLeft().getLongitude(),
 					180.0, true, true), BooleanClause.Occur.SHOULD);
-			((BooleanQuery)longQuery).add( NumericRangeQuery.newDoubleRange( "longitude_hibernate_search_spatial", -180.0,
+			( (BooleanQuery) longQuery).add( NumericRangeQuery.newDoubleRange( "longitude_hibernate_search_spatial", -180.0,
 					boundingBox.getUpperRight().getLongitude(), true, true), BooleanClause.Occur.SHOULD);
 		}
 
