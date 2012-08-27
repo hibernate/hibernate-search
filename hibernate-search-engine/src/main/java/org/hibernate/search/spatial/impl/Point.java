a8d436019fe16c6ab430d8b56ee6f32512c6808e
Nicolas Helleringer <nicolas.helleringer@novacodex.net>
2012-08-27 18:27:40 +0200

    HSEARCH-1174 Removed uneeded Point hashCode and equals non default/standard implementations

--- hibernate-search-engine/src/main/java/org/hibernate/search/spatial/impl/Point.java
+++ hibernate-search-engine/src/main/java/org/hibernate/search/spatial/impl/Point.java
@@ -227,36 +227,4 @@ public final class Point implements Coordinates {
 		sb.append( '}' );
 		return sb.toString();
 	}
-
-	@Override
-	public boolean equals(Object o) {
-		if ( this == o ) {
-			return true;
-		}
-		if ( o == null || getClass() != o.getClass() ) {
-			return false;
-		}
-
-		Point point = ( Point ) o;
-
-		if ( Double.compare( point.latitude, latitude ) != 0 ) {
-			return false;
-		}
-		if ( Double.compare( point.longitude, longitude ) != 0 ) {
-			return false;
-		}
-
-		return true;
-	}
-
-	@Override
-	public int hashCode() {
-		int result;
-		long temp;
-		temp = latitude != +0.0d ? Double.doubleToLongBits( latitude ) : 0L;
-		result = ( int ) ( temp ^ ( temp >>> 32 ) );
-		temp = longitude != +0.0d ? Double.doubleToLongBits( longitude ) : 0L;
-		result = 31 * result + ( int ) ( temp ^ ( temp >>> 32 ) );
-		return result;
-	}
 }
