eb2b3f955e477f84d27b25178dfefcf941fdc5fb
Nicolas Helleringer <nicolas.helleringer@novacodex.net>
2012-08-27 10:03:35 +0200

    HSEARCH-1174 Fix auto generated Java Doc

--- hibernate-search-engine/src/main/java/org/hibernate/search/spatial/impl/DistanceSortField.java
+++ hibernate-search-engine/src/main/java/org/hibernate/search/spatial/impl/DistanceSortField.java
@@ -3,11 +3,12 @@ package org.hibernate.search.spatial.impl;
 import org.apache.lucene.search.SortField;
 
 /**
- * Created with IntelliJ IDEA.
- * User: nicolashelleringer
- * Date: 26/07/12
- * Time: 14:36
- * To change this template use File | Settings | File Templates.
+ * Lucene SortField for sorting documents which have been indexed with Hibernate Search spatial
+ *
+ * @author Nicolas Helleringer <nicolas.helleringer@novacodex.net>
+ * @see org.hibernate.search.spatial.SpatialFieldBridgeByGrid
+ * @see org.hibernate.search.spatial.SpatialFieldBridgeByRange
+ * @see org.hibernate.search.spatial.Coordinates
  */
 public class DistanceSortField extends SortField {
 
