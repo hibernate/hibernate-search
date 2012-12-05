0f21fad7eda26291a55cf7121ec96b05a88b99aa
Yoann Rodière <yoann.rodiere@openwide.fr>
2012-12-05 10:56:44 +0100

    HSEARCH-1241 Fix @ContainedIn behavior with polymorphism and lazy proxies

---
+++ hibernate-search-orm/src/test/java/org/hibernate/search/test/embedded/polymorphism/Level3.java
@@ -0,0 +1,74 @@
+/* 
+ * Hibernate, Relational Persistence for Idiomatic Java
+ * 
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+package org.hibernate.search.test.embedded.polymorphism;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import org.hibernate.search.annotations.ContainedIn;
+import org.hibernate.search.annotations.DocumentId;
+import org.hibernate.search.annotations.Field;
+import org.hibernate.search.annotations.Indexed;
+
+@Entity
+@Indexed
+class Level3 {
+
+	@Id
+	@DocumentId
+	@GeneratedValue
+	private Integer id;
+
+	@Field
+	private String name;
+
+	@OneToOne(fetch = FetchType.LAZY)
+	@ContainedIn
+	private Level2 level2Parent;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Level2 getLevel2Parent() {
+		return level2Parent;
+	}
+
+	public void setLevel2Parent(Level2 level2Parent) {
+		this.level2Parent = level2Parent;
+	}
+
+}
