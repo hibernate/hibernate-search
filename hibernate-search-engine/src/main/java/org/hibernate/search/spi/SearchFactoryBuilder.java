7e01a81aacd495364fa5230f155a3748e962448f
Emmanuel Bernard <emmanuel@hibernate.org>
2013-04-09 15:36:46 +0200

    HSEARCH-472 Add some comment to the code about dynamic shards being eagerly loaded at SF creation

--- hibernate-search-engine/src/main/java/org/hibernate/search/spi/SearchFactoryBuilder.java
+++ hibernate-search-engine/src/main/java/org/hibernate/search/spi/SearchFactoryBuilder.java
@@ -288,6 +288,8 @@ public class SearchFactoryBuilder {
 					documentBuildersIndexedEntities.put( clazz, entityMapping );
 				}
 			}
+			//note that calling EIB.getIndexManagers() will eagerly load all known shard IndexManagers
+			//which is a good thing and should be retained
 			IndexManager[] indexManagers = entityMapping.getIndexManagers();
 			for ( IndexManager indexManager : indexManagers ) {
 				Similarity indexSimilarity = indexManager.getSimilarity();
