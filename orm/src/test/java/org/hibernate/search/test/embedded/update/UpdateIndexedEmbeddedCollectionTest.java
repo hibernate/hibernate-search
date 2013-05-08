591bc92bb985236def718c28f73ab0cc4e1ac7a7
Davide Di Somma <davide.disomma@gmail.com>
2013-05-08 17:46:13 +0200

    HSEARCH-735 fixed checkstyle errors

--- orm/src/test/java/org/hibernate/search/test/embedded/update/UpdateIndexedEmbeddedCollectionTest.java
+++ orm/src/test/java/org/hibernate/search/test/embedded/update/UpdateIndexedEmbeddedCollectionTest.java
@@ -1,8 +1,8 @@
-/* 
+/*
  * Hibernate, Relational Persistence for Idiomatic Java
- * 
+ *
  * JBoss, Home of Professional Open Source
- * Copyright 2012 Red Hat Inc. and/or its affiliates and other contributors
+ * Copyright 2013 Red Hat Inc. and/or its affiliates and other contributors
  * as indicated by the @authors tag. All rights reserved.
  * See the copyright.txt in the distribution for a
  * full listing of individual contributors.
@@ -42,21 +42,20 @@ import org.hibernate.search.annotations.Field;
 import org.hibernate.search.annotations.Indexed;
 import org.hibernate.search.annotations.IndexedEmbedded;
 import org.hibernate.search.test.SearchTestCase;
+import org.hibernate.search.test.util.TestForIssue;
 
 /**
+ *
  * Unit test about updating an entity with collection marked with @IndexedEmbedded annotation
- * 
+ *
  * @author Davide Di Somma <davide.disomma@gmail.com>
  */
 public class UpdateIndexedEmbeddedCollectionTest extends SearchTestCase {
 
-	/**
-	 * This test case refers to HSEARCH-735 issue which prevents the NPE reported in HSEARCH-734 issue. 
-	 * @throws Exception
-	 */
+	@TestForIssue(jiraKey = "HSEARCH-734")
 	public void testUpdateIndexedEmbeddedCollectionWithNull() throws Exception {
 
-		//load the truck with number plate "LVN 746 XD" guided by driver Mark Smith 
+		// load the truck with number plate "LVN 746 XD" guided by driver Mark Smith
 		Driver driverSmith = new Driver( "Mark", "Smith" );
 		Truck truckLVN746XD = new Truck( "LVN 746 XD" );
 		driverSmith.setTruck( truckLVN746XD );
@@ -68,8 +67,7 @@ public class UpdateIndexedEmbeddedCollectionTest extends SearchTestCase {
 		itemsTruckLVN746XD.add( item2 );
 		itemsTruckLVN746XD.add( item3 );
 		truckLVN746XD.setItems( itemsTruckLVN746XD );
-		
-		//load the truck with number plate "MLN 666 DJ" guided by driver John Doe 
+		// load the truck with number plate "MLN 666 DJ" guided by driver John Doe
 		Driver driverDoe = new Driver( "John", "Doe" );
 		Truck truckMLN666DJ = new Truck( "MLN 666 DJ" );
 		driverDoe.setTruck( truckMLN666DJ );
@@ -79,70 +77,57 @@ public class UpdateIndexedEmbeddedCollectionTest extends SearchTestCase {
 		itemsTruckMLN666DJ.add( item4 );
 		itemsTruckMLN666DJ.add( item5 );
 		truckMLN666DJ.setItems( itemsTruckMLN666DJ );
-		
 
 		// first operation -> save
 		FullTextSession session = Search.getFullTextSession( openSession() );
 		Transaction tx = session.beginTransaction();
 		session.save( driverSmith );
 		session.save( driverDoe );
-		
 		tx.commit();
 		session.close();
 
 		final Long truckLVN746XDId = truckLVN746XD.getId();
 		final Long truckMLN666DJId = truckMLN666DJ.getId();
-
 		// assert that everything got saved correctly
 		session = Search.getFullTextSession( openSession() );
 		tx = session.beginTransaction();
-
 		// everything gets indexed correctly
 		assertEquals( truckMLN666DJId, findTruckIdFromIndex( session, "armchair" ) );
 		assertEquals( truckLVN746XDId, findTruckIdFromIndex( session, "table" ) );
-
 		tx.commit();
 		session.close();
 
-		// now unload the truck "LVN 746 XD" because his driver, Mark Smith, arrived at his destination.  
+		// now unload the truck "LVN 746 XD" because his driver, Mark Smith, arrived at his destination.
 		truckLVN746XD.setItems( null );
-
 		// let's update
 		session = Search.getFullTextSession( openSession() );
 		tx = session.beginTransaction();
-
 		session.update( driverSmith );
-
-		// if the Hibernate-Search release is 3.4.0.CR2, it throws NullPointerException described in HSEARCH-734 during commit phase 
+		// if the Hibernate-Search release is 3.4.0.CR2, it throws NullPointerException described in HSEARCH-734 during
+		// commit phase
 		tx.commit();
 		session.close();
 
 		session = Search.getFullTextSession( openSession() );
 		tx = session.beginTransaction();
-
-		//let's assert that indexes got updated correctly
+		// let's assert that indexes got updated correctly
 		assertNull( findTruckIdFromIndex( session, "table" ) );
-		
 		tx.commit();
 		session.close();
 
-		//Now let's clean up everything
+		// Now let's clean up everything
 		session = Search.getFullTextSession( openSession() );
 		tx = session.beginTransaction();
-
-		// everything gets indexed correctly
 		session.delete( driverSmith );
 		session.delete( driverDoe );
-
 		tx.commit();
 		session.close();
 
 	}
 
-	
-	
 	private Long findTruckIdFromIndex(FullTextSession session, String itemDescription) {
-		FullTextQuery q = session.createFullTextQuery( new TermQuery( new Term( "truck.items.description", itemDescription ) ), Driver.class );
+		FullTextQuery q = session.createFullTextQuery( new TermQuery( new Term( "truck.items.description",
+				itemDescription ) ), Driver.class );
 		q.setProjection( "truck.id" );
 		@SuppressWarnings("unchecked")
 		List<Object[]> results = q.list();
@@ -152,7 +137,6 @@ public class UpdateIndexedEmbeddedCollectionTest extends SearchTestCase {
 		return (Long) results.get( 0 )[0];
 	}
 
-
 	@Override
 	protected Class<?>[] getAnnotatedClasses() {
 		return new Class[] { Driver.class, Truck.class, Item.class };
@@ -160,173 +144,80 @@ public class UpdateIndexedEmbeddedCollectionTest extends SearchTestCase {
 
 	@Entity
 	@Indexed
-	public class Driver {
-
-		private Long id;
-		private String firstName;
-		private String lastName;
-		private Truck truck;
-
-		public Driver() {
-		}
-		
+	public static class Driver {
 		public Driver(String firstName, String lastName) {
 			this.firstName = firstName;
 			this.lastName = lastName;
 		}
-		
 
-		@Id
-		@GeneratedValue
-		@DocumentId
-		public Long getId() {
-			return id;
-		}
-
-		public void setId(Long id) {
-			this.id = id;
-		}
+		@Id @GeneratedValue @DocumentId
+		public Long getId() { return id; }
+		public void setId(Long id) { this.id = id; }
+		private Long id;
 
 		@Field
-		public String getFirstName() {
-			return firstName;
-		}
+		public String getFirstName() { return firstName; }
+		public void setFirstName(String firstName) { this.firstName = firstName; }
+		private String firstName;
 
-		public void setFirstName(String firstName) {
-			this.firstName = firstName;
-		}
-		
 		@Field
-		public String getLastName() {
-			return lastName;
-		}
+		public String getLastName() { return lastName; }
+		public void setLastName(String lastName) { this.lastName = lastName; }
+		private String lastName;
 
-		public void setLastName(String lastName) {
-			this.lastName = lastName;
-		}
-
-		@IndexedEmbedded
-		@OneToOne(cascade=CascadeType.ALL)
-		public Truck getTruck() {
-			return truck;
-		}
-
-		public void setTruck(Truck truck) {
-			this.truck = truck;
-		}
-
-
+		@IndexedEmbedded @OneToOne(cascade = CascadeType.ALL)
+		public Truck getTruck() { return truck; }
+		public void setTruck(Truck truck) { this.truck = truck; }
+		private Truck truck;
 	}
 
-	
 	@Entity
 	@Indexed
-	public class Truck {
-
-		private Long id;
-		private String numberPlate;
-		private Set<Item> items;
-		
-		public Truck() {
-		}
-
-
+	public static class Truck {
 		public Truck(String numberPlate) {
 			super();
 			this.numberPlate = numberPlate;
 		}
 
-		@Id
-		@GeneratedValue
-		@DocumentId
-		public Long getId() {
-			return id;
-		}
-
-		public void setId(Long id) {
-			this.id = id;
-		}
+		@Id @GeneratedValue @DocumentId
+		public Long getId() { return id; }
+		public void setId(Long id) { this.id = id; }
+		private Long id;
 
 		@Field
-		public String getNumberPlate() {
-			return numberPlate;
-		}
+		public String getNumberPlate() { return numberPlate; }
+		public void setNumberPlate(String numberPlate) { this.numberPlate = numberPlate; }
+		private String numberPlate;
 
-		public void setNumberPlate(String numberPlate) {
-			this.numberPlate = numberPlate;
-		}
-
-		
-		@IndexedEmbedded
-		@OneToMany(cascade=CascadeType.ALL)
-		public Set<Item> getItems() {
-			return items;
-		}
-
-		public void setItems(Set<Item> items) {
-			this.items = items;
-		}
-		
+		@IndexedEmbedded @OneToMany(cascade = CascadeType.ALL)
+		public Set<Item> getItems() { return items; }
+		public void setItems(Set<Item> items) { this.items = items; }
+		private Set<Item> items;
 	}
 
 	@Entity
 	@Indexed
-	public class Item {
-
-		private Long id;
-		private String description;
-		private Integer quantity;
-		
-		
-		public Item() {
-		}
-
-
+	public static class Item {
 		public Item(String description, Integer quantity) {
 			super();
 			this.description = description;
 			this.quantity = quantity;
 		}
 
-
-
-		@Id
-		@GeneratedValue
-		@DocumentId
-		public Long getId() {
-			return id;
-		}
-
-
-		public void setId(Long id) {
-			this.id = id;
-		}
-
+		@Id @GeneratedValue @DocumentId
+		public Long getId() { return id; }
+		public void setId(Long id) { this.id = id; }
+		private Long id;
 
 		@Field
-		public String getDescription() {
-			return description;
-		}
-
-
-		public void setDescription(String description) {
-			this.description = description;
-		}
-
+		public String getDescription() { return description; }
+		public void setDescription(String description) { this.description = description; }
+		private String description;
 
 		@Field
-		public Integer getQuantity() {
-			return quantity;
-		}
-
-
-		public void setQuantity(Integer quantity) {
-			this.quantity = quantity;
-		}
-		
-		
+		public Integer getQuantity() { return quantity; }
+		public void setQuantity(Integer quantity) { this.quantity = quantity; }
+		private Integer quantity;
 	}
 
 }
-
-
