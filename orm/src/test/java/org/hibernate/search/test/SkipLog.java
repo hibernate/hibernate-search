818ba6dfe2d7c2fe6b175b0132a71e12c6db27f5
Hardy Ferentschik <hibernate@ferentschik.de>
2013-10-04 11:54:02 +0200

    HSEARCH-1271 Moving SkipLog into org.hibernate.search.test

--- orm/src/test/java/org/hibernate/search/test/SkipLog.java
+++ orm/src/test/java/org/hibernate/search/test/SkipLog.java
@@ -18,7 +18,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA  02110-1301, USA.
  */
-package org.hibernate.search.test.fwk;
+package org.hibernate.search.test;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
