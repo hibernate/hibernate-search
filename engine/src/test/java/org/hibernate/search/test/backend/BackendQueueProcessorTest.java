85c549bc73c9fd9568fde7126571885ad006bc1e
Hardy Ferentschik <hibernate@ferentschik.de>
2013-12-03 00:59:24 +0000

    HSEARCH-1365 Rename package for backends tests

--- engine/src/test/java/org/hibernate/search/test/backend/BackendQueueProcessorTest.java
+++ engine/src/test/java/org/hibernate/search/test/backend/BackendQueueProcessorTest.java
@@ -18,7 +18,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA  02110-1301, USA.
  */
-package org.hibernate.search.test.backends;
+package org.hibernate.search.test.backend;
 
 import org.junit.Test;
 
--- engine/src/test/java/org/hibernate/search/test/backend/lucene/SharedReleasesLocksTest.java
+++ engine/src/test/java/org/hibernate/search/test/backend/lucene/SharedReleasesLocksTest.java
@@ -18,7 +18,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA  02110-1301, USA.
  */
-package org.hibernate.search.test.backends.lucene;
+package org.hibernate.search.test.backend.lucene;
 
 import java.io.IOException;
 
