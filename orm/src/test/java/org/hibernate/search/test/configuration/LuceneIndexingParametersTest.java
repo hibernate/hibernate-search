f34a61e90b7dbec38a01f0a7d612e21423ba0e58
Sanne Grinovero <sanne@hibernate.org>
2014-01-02 16:05:56 +0000

    HSEARCH-1476 Configuration property 'merge_calibrate_by_deletes' not parsed correctly

--- orm/src/test/java/org/hibernate/search/test/configuration/LuceneIndexingParametersTest.java
+++ orm/src/test/java/org/hibernate/search/test/configuration/LuceneIndexingParametersTest.java
@@ -37,6 +37,7 @@ import static org.hibernate.search.backend.configuration.impl.IndexWriterSetting
 import static org.hibernate.search.backend.configuration.impl.IndexWriterSetting.MERGE_FACTOR;
 import static org.hibernate.search.backend.configuration.impl.IndexWriterSetting.RAM_BUFFER_SIZE;
 import static org.hibernate.search.backend.configuration.impl.IndexWriterSetting.USE_COMPOUND_FILE;
+import static org.hibernate.search.backend.configuration.impl.IndexWriterSetting.MERGE_CALIBRATE_BY_DELETES;
 import static org.hibernate.search.backend.configuration.impl.IndexWriterSetting.MERGE_MAX_OPTIMIZE_SIZE;
 
 /**
@@ -50,12 +51,14 @@ public class LuceneIndexingParametersTest extends ConfigurationReadTestCase {
 
 		cfg.setProperty( "hibernate.search.default.indexwriter.ram_buffer_size", "1" );
 		cfg.setProperty( "hibernate.search.default.indexwriter.use_compound_file", "false" );
+		cfg.setProperty( "hibernate.search.default.indexwriter.merge_calibrate_by_deletes", "false" );
 //set by super : cfg.setProperty( "hibernate.search.default.indexwriter.max_buffered_docs", "1000" );
 		cfg.setProperty( "hibernate.search.default.indexwriter.max_merge_docs", "9" );
 //set by super : cfg.setProperty( "hibernate.search.default.indexwriter.merge_factor", "100" );
 
 		cfg.setProperty( "hibernate.search.Book.indexwriter.max_merge_docs", "12" );
 		cfg.setProperty( "hibernate.search.Book.indexwriter.use_compound_file", "false" );
+		cfg.setProperty( "hibernate.search.Book.indexwriter.merge_calibrate_by_deletes", "false" );
 		cfg.setProperty( "hibernate.search.Book.indexwriter.merge_factor", "13" );
 		cfg.setProperty( "hibernate.search.Book.indexwriter.max_buffered_docs", "14" );
 		cfg.setProperty( "hibernate.search.Book.indexwriter.ram_buffer_size", "4" );
@@ -70,6 +73,7 @@ public class LuceneIndexingParametersTest extends ConfigurationReadTestCase {
 
 	public void testDefaultIndexProviderParameters() {
 		assertValueIsSet( Author.class, USE_COMPOUND_FILE, 0 );
+		assertValueIsSet( Author.class, MERGE_CALIBRATE_BY_DELETES, 0 );
 		assertValueIsSet( Author.class, RAM_BUFFER_SIZE, 1 );
 		assertValueIsSet( Author.class, MAX_MERGE_DOCS, 9 );
 		assertValueIsSet( Author.class, MAX_BUFFERED_DOCS, 1000 );
@@ -81,6 +85,7 @@ public class LuceneIndexingParametersTest extends ConfigurationReadTestCase {
 		assertValueIsSet( Book.class, MAX_BUFFERED_DOCS, 14 );
 		assertValueIsSet( Book.class, MERGE_FACTOR, 13 );
 		assertValueIsSet( Book.class, USE_COMPOUND_FILE, 0 );
+		assertValueIsSet( Book.class, MERGE_CALIBRATE_BY_DELETES, 0 );
 		assertValueIsSet( Book.class, RAM_BUFFER_SIZE, 4 );
 		assertValueIsSet( Book.class, MERGE_MAX_OPTIMIZE_SIZE, 256 );
 	}
