34908d58bb1dbb9a943fc889ad5908ab5e1059ff
Hardy Ferentschik <hibernate@ferentschik.de>
2014-01-28 20:42:33 +0100

    HSEARCH-1447 Adding new helper methods to determine whether a given XClass contains Search annotations at all. Adding test case as well

--- engine/src/main/java/org/hibernate/search/util/impl/ReflectionHelper.java
+++ engine/src/main/java/org/hibernate/search/util/impl/ReflectionHelper.java
@@ -23,6 +23,7 @@
  */
 package org.hibernate.search.util.impl;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.AccessibleObject;
 import java.lang.reflect.Member;
 import java.lang.reflect.Modifier;
@@ -31,6 +32,7 @@ import java.util.List;
 
 import org.hibernate.annotations.common.reflection.XClass;
 import org.hibernate.annotations.common.reflection.XMember;
+import org.hibernate.annotations.common.reflection.XProperty;
 import org.hibernate.search.util.StringHelper;
 
 /**
@@ -47,6 +49,7 @@ public abstract class ReflectionHelper {
 	 *
 	 * @param member <code>XMember</code> from which to extract the name.
 	 * @param name Override value which will be returned in case it is not empty.
+	 *
 	 * @return attribute name out of member unless overridden by <code>name</code>.
 	 */
 	public static String getAttributeName(XMember member, String name) {
@@ -102,10 +105,11 @@ public abstract class ReflectionHelper {
 	 * Creates the class hierarchy for a given {@code XClass}.
 	 *
 	 * @param clazz the class for which to create the hierarchy
+	 *
 	 * @return the list of classes in the hierarchy starting at {@code java.lang.Object}
 	 */
 	public static List<XClass> createXClassHierarchy(XClass clazz) {
-		List<XClass> hierarchy = new LinkedList<XClass>( );
+		List<XClass> hierarchy = new LinkedList<XClass>();
 		XClass next;
 		for ( XClass previousClass = clazz; previousClass != null; previousClass = next ) {
 			next = previousClass.getSuperclass();
@@ -114,5 +118,53 @@ public abstract class ReflectionHelper {
 			}
 		}
 		return hierarchy;
+	}
+
+	/**
+	 * Checks whether the specified class contains any Search specific annotations.
+	 *
+	 * @param mappedClass the {@code XClass} to check for Search annotations
+	 *
+	 * @return Returns {@code true} if the class contains at least one Search annotation, {@code false} otherwise
+	 */
+	public static boolean containsSearchAnnotations(XClass mappedClass) {
+		// check the type annotations
+		if ( containsSearchAnnotation( mappedClass.getAnnotations() ) ) {
+			return true;
+		}
+
+		for ( XProperty method : mappedClass.getDeclaredProperties( XClass.ACCESS_PROPERTY ) ) {
+			if ( containsSearchAnnotation( method.getAnnotations() ) ) {
+				return true;
+			}
+		}
+
+		for ( XProperty field : mappedClass.getDeclaredProperties( XClass.ACCESS_FIELD ) ) {
+			if ( containsSearchAnnotation( field.getAnnotations() ) ) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * Checks if the annotation is a Search annotation by comparing the package of the annotation.
+	 *
+	 * @param annotation the annotation to check
+	 *
+	 * @return Returns {@code true} if the annotation is a Search annotation, {@code false} otherwise
+	 */
+	public static boolean isSearchAnnotation(Annotation annotation) {
+		return "org.hibernate.search.annotations".equals( annotation.annotationType().getPackage().getName() );
+	}
+
+	private static boolean containsSearchAnnotation(Annotation[] annotations) {
+		for ( Annotation annotation : annotations ) {
+			if ( isSearchAnnotation( annotation ) ) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
