2ea1c04f5e361316483a5f3aaa8f9e4066966bc0
Emmanuel Bernard <emmanuel@hibernate.org>
2014-02-07 17:46:47 +0100

    HSEARCH-1492 Add interfaces for MoreLikeThis DSL

---
+++ engine/src/main/java/org/hibernate/search/query/dsl/MoreLikeThisContext.java
@@ -0,0 +1,71 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2014, Red Hat, Inc. and/or its affiliates or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat, Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+
+package org.hibernate.search.query.dsl;
+
+/**
+ * @author Emmanuel Bernard <emmanuel@hibernate.org>
+ */
+public interface MoreLikeThisContext extends QueryCustomization<MoreLikeThisContext> {
+
+	//TODO add returnEntityComparedWith?
+	//TODO today we return the matching entity we could exclude it with a Boolean MUST NOT
+
+	/**
+	 * Match the content using "all" of the indexed fields of the entity.
+	 *
+	 * More precisely, only fields storing term vectors or physically stored will be compared.
+	 * Fields storing the id and the class type are ignored.
+	 *
+	 * We highly recommend to store the term vectors if you plan on using More Like This queries.
+	 */
+	MoreLikeThisTerminalMatchingContext comparingAllFields();
+
+	/**
+	 * Match the content using the selected fields of the entity.
+	 *
+	 * An exception is thrown if the fields are neither storing the term vectors nor physically stored.
+	 *
+	 * We highly recommend to store the term vectors if you plan on using More Like This queries.
+	 */
+	MoreLikeThisOpenedMatchingContext comparingFields(String... fieldNames);
+
+	/**
+	 * Match the content using the selected field of the entity.
+	 *
+	 * An exception is thrown if the field are neither storing the term vectors nor physically stored.
+	 *
+	 * We highly recommend to store the term vectors if you plan on using More Like This queries.
+	 */
+	MoreLikeThisOpenedMatchingContext comparingField(String fieldName);
+
+	//TODO should we keep it
+	// if yes it should contain the list of specific options
+	// a class hierarchy of MoreLikeThisContext <- {MorelikeThisBeginningContext|MoreLikeThisFavorFieldSimilarityContext|MoreLikeThisFavorOverallSimilarityContext}
+	// *beginning would have favorPerFieldSimilarity and favorOverallSimilarity
+	// favorFieldSimilarity would have DisMax specific settings and can carry on to the field selection
+	// favorOverallimilarity would have Boolean specific settings and can carry on to the field selection
+	MoreLikeThisContext favorPerFieldSimilarity();
+
+}
