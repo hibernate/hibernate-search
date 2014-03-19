1fffa9812355dbdd327f6ba55136f175bd8fe5c8
Sanne Grinovero <sanne@hibernate.org>
2014-03-19 11:34:40 +0000

    HSEARCH-1562 Race condition in error reporting from IdentifierProducer
    Conflicts:
    	engine/src/main/java/org/hibernate/search/util/logging/impl/Log.java

--- engine/src/main/java/org/hibernate/search/util/logging/impl/Log.java
+++ engine/src/main/java/org/hibernate/search/util/logging/impl/Log.java
@@ -760,4 +760,8 @@ public interface Log extends BasicLogger {
 
 	@Message(id = 194, value = "Unable to load configured class '%s' as 'sharding_strategy'")
 	SearchException getUnableToLoadShardingStrategyClassException(String className);
+
+	@Message(id = 211, value = "An exception occurred while the MassIndexer was fetching the primary identifiers list")
+	String massIndexerExceptionWhileFetchingIds();
+
 }
