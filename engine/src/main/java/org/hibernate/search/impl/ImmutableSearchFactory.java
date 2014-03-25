42a207de712b6c4c2098f5698a31af229ba68bd3
Hardy Ferentschik <hibernate@ferentschik.de>
2014-03-25 12:59:43 +0100

    HSEARCH-1560 Moving org.hibernate.search.Version to org.hibernate.search.engine.Version

--- engine/src/main/java/org/hibernate/search/impl/ImmutableSearchFactory.java
+++ engine/src/main/java/org/hibernate/search/impl/ImmutableSearchFactory.java
@@ -34,7 +34,7 @@ import org.apache.lucene.analysis.Analyzer;
 import org.hibernate.search.exception.AssertionFailure;
 import org.hibernate.search.Environment;
 import org.hibernate.search.SearchException;
-import org.hibernate.search.Version;
+import org.hibernate.search.engine.Version;
 import org.hibernate.search.backend.impl.batch.BatchBackend;
 import org.hibernate.search.backend.impl.batch.DefaultBatchBackend;
 import org.hibernate.search.backend.spi.Worker;
