15ba9f58f3b9e58c4bb6c9dced6bb70398fc6177
Sanne Grinovero <sanne@hibernate.org>
2014-07-26 23:04:18 +0100

    HSEARCH-1637 Extract concurrent test pattern for reuse

---
+++ engine/src/test/java/org/hibernate/search/testsupport/concurrency/ConcurrentRunner.java
@@ -0,0 +1,115 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.testsupport.concurrency;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.junit.Assert;
+
+
+/**
+ * Helper to create tests which need to execute multiple tasks at "approximately same time",
+ * like stress tests.
+ * Note that we assume the number of threads and tasks match.
+ *
+ * If any exception happens, a JUnit failure is caused.
+ *
+ * @author Sanne Grinovero <sanne@hibernate.org> (C) 2014 Red Hat Inc.
+ */
+public class ConcurrentRunner {
+
+	public static final int DEFAULT_THREADS = 300;
+
+	private final AtomicBoolean somethingFailed = new AtomicBoolean( false );
+	private final int threads;
+	private final ExecutorService executor;
+	private final CountDownLatch startLatch = new CountDownLatch( 1 );
+	private final CountDownLatch endLatch;
+	private final TaskFactory factory;
+
+	/**
+	 * Provide a source for {@link Runnable} instances to run concurrently.
+	 * This is meant to simplify collection and creation of such tasks.
+	 * @param factory the source of Runnable instances to run
+	 */
+	public ConcurrentRunner(TaskFactory factory) {
+		this( DEFAULT_THREADS, factory );
+	}
+
+	/**
+	 * /**
+	 * Provide a source for {@link Runnable} instances to run concurrently.
+	 * This is meant to simplify collection and creation of such tasks.
+	 * @param threads the number of threads (and tasks) to create and run.
+	 * @param factory the source of Runnable instances to run.
+	 */
+	public ConcurrentRunner(int threads, TaskFactory factory) {
+		this.threads = threads;
+		this.factory = factory;
+		executor = Executors.newFixedThreadPool( threads );
+		endLatch = new CountDownLatch( threads );
+	}
+
+	/**
+	 * Invokes the {@link TaskFactory} and runs all the built tasks in
+	 * an Executor.
+	 * @throws Exception if interrupted, any exception is thrown by the tasks,
+	 * or any exception is thrown during the creation of tasks.
+	 */
+	public void execute() throws Exception {
+		for ( int i = 0; i < threads; i++ ) {
+			Runnable userRunnable = factory.createRunnable( i );
+			executor.execute( new WrapRunnable( startLatch, endLatch, userRunnable ) );
+		}
+		executor.shutdown();
+		startLatch.countDown();
+		try {
+			endLatch.await();
+		}
+		catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			e.printStackTrace();
+			Assert.fail( "Interrupted while awaiting for end of execution" );
+		}
+		Assert.assertFalse( somethingFailed.get() );
+	}
+
+	private class WrapRunnable implements Runnable {
+
+		private final CountDownLatch startLatch;
+		private final CountDownLatch endLatch;
+		private final Runnable userRunnable;
+
+		public WrapRunnable(CountDownLatch startLatch, CountDownLatch endLatch, Runnable userRunnable) {
+			this.startLatch = startLatch;
+			this.endLatch = endLatch;
+			this.userRunnable = userRunnable;
+		}
+
+		@Override
+		public void run() {
+			try {
+				startLatch.await(); // Maximize chances of working concurrently on the Serializer
+				userRunnable.run();
+			}
+			catch (InterruptedException | RuntimeException e) {
+				e.printStackTrace();
+				somethingFailed.set( true );
+			}
+			endLatch.countDown();
+		}
+
+	}
+
+	public interface TaskFactory {
+		Runnable createRunnable(int i) throws Exception;
+	}
+
+}
--- serialization/avro/src/test/java/org/hibernate/search/test/serialization/ConcurrentServiceTest.java
+++ serialization/avro/src/test/java/org/hibernate/search/test/serialization/ConcurrentServiceTest.java
@@ -9,18 +9,15 @@ package org.hibernate.search.test.serialization;
 import static org.junit.Assert.assertTrue;
 
 import java.util.List;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.hibernate.search.backend.LuceneWork;
 import org.hibernate.search.indexes.serialization.avro.impl.AvroSerializationProvider;
 import org.hibernate.search.indexes.serialization.spi.LuceneWorkSerializer;
 import org.hibernate.search.indexes.serialization.spi.SerializationProvider;
 import org.hibernate.search.testsupport.TestForIssue;
+import org.hibernate.search.testsupport.concurrency.ConcurrentRunner;
+import org.hibernate.search.testsupport.concurrency.ConcurrentRunner.TaskFactory;
 import org.hibernate.search.testsupport.junit.SearchFactoryHolder;
-import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -31,8 +28,6 @@ import org.junit.Test;
  */
 @TestForIssue(jiraKey = "HSEARCH-1637")
 public class ConcurrentServiceTest {
-
-	private static final int THREADS = 300;
 
 	@Rule
 	public SearchFactoryHolder searchFactoryHolder = new SearchFactoryHolder( RemoteEntity.class );
@@ -45,99 +40,67 @@ public class ConcurrentServiceTest {
 
 	@Test
 	public void concurrentSerialization() throws Exception {
-		final AtomicBoolean somethingFailed = new AtomicBoolean( false );
 		final LuceneWorkSerializer serializer = extractSerializer();
-		final ExecutorService executor = Executors.newFixedThreadPool( THREADS );
-		final CountDownLatch startLatch = new CountDownLatch( 1 );
-		final CountDownLatch endLatch = new CountDownLatch( THREADS );
-		for ( int i = 0; i < THREADS; i++ ) {
-			executor.execute( new SerializingThread( serializer, startLatch, endLatch, somethingFailed, SerializationTest.buildWorks() ) );
-		}
-		executor.shutdown();
-		startLatch.countDown();
-		endLatch.await();
-		Assert.assertFalse( somethingFailed.get() );
+		final List<LuceneWork> works = SerializationTest.buildWorks();
+		final ConcurrentRunner runner = new ConcurrentRunner( new TaskFactory() {
+			@Override
+			public Runnable createRunnable(int i) throws Exception {
+				return new SerializingThread( serializer, works );
+			}
+		});
+		runner.execute();
 	}
 
 	@Test
 	public void concurrentDeserialization() throws Exception {
-		final AtomicBoolean somethingFailed = new AtomicBoolean( false );
 		final LuceneWorkSerializer serializer = extractSerializer();
-		final ExecutorService executor = Executors.newFixedThreadPool( THREADS );
-		final CountDownLatch startLatch = new CountDownLatch( 1 );
-		final CountDownLatch endLatch = new CountDownLatch( THREADS );
 		final byte[] serializedModel = serializer.toSerializedModel( SerializationTest.buildWorks() );
-		for ( int i = 0; i < THREADS; i++ ) {
-			executor.execute( new DeserializingThread( serializer, startLatch, endLatch, somethingFailed, serializedModel ) );
-		}
-		executor.shutdown();
-		startLatch.countDown();
-		endLatch.await();
-		Assert.assertFalse( somethingFailed.get() );
+		final ConcurrentRunner runner = new ConcurrentRunner( new TaskFactory() {
+			@Override
+			public Runnable createRunnable(int i) throws Exception {
+				return new DeserializingThread( serializer, serializedModel );
+			}
+		});
+		runner.execute();
 	}
 
 	private LuceneWorkSerializer extractSerializer() {
-		return searchFactoryHolder.getSearchFactory().getIndexManagerHolder().getIndexManager( RemoteEntity.class.getName() ).getSerializer();
+		return searchFactoryHolder.getSearchFactory()
+				.getIndexManagerHolder()
+				.getIndexManager( RemoteEntity.class.getName() )
+				.getSerializer();
 	}
 
 	private static class SerializingThread implements Runnable {
 
-		private final CountDownLatch startLatch;
-		private final CountDownLatch endLatch;
-		private final AtomicBoolean somethingFailed;
 		private final LuceneWorkSerializer serializer;
 		private final List<LuceneWork> workList;
 
-		public SerializingThread(LuceneWorkSerializer serializer, CountDownLatch startLatch, CountDownLatch endLatch, AtomicBoolean somethingFailed, List<LuceneWork> workList) {
+		public SerializingThread(LuceneWorkSerializer serializer, List<LuceneWork> workList) {
 			this.serializer = serializer;
-			this.startLatch = startLatch;
-			this.endLatch = endLatch;
-			this.somethingFailed = somethingFailed;
 			this.workList = workList;
 		}
 
 		@Override
 		public void run() {
-			try {
-				startLatch.await(); //Maximize chances of working concurrently on the Serializer
-				serializer.toSerializedModel( workList );
-			}
-			catch (InterruptedException|RuntimeException e) {
-				e.printStackTrace();
-				somethingFailed.set( true );
-			}
-			endLatch.countDown();
+			serializer.toSerializedModel( workList );
 		}
 
 	}
 
 	private static class DeserializingThread implements Runnable {
 
-		private final CountDownLatch startLatch;
-		private final CountDownLatch endLatch;
-		private final AtomicBoolean somethingFailed;
 		private final LuceneWorkSerializer serializer;
 		private final byte[] serializedModel;
 
-		public DeserializingThread(LuceneWorkSerializer serializer, CountDownLatch startLatch, CountDownLatch endLatch, AtomicBoolean somethingFailed, byte[] serializedModel) {
+		public DeserializingThread(LuceneWorkSerializer serializer, byte[] serializedModel) {
 			this.serializer = serializer;
-			this.startLatch = startLatch;
-			this.endLatch = endLatch;
-			this.somethingFailed = somethingFailed;
 			this.serializedModel = serializedModel;
 		}
 
 		@Override
 		public void run() {
-			try {
-				startLatch.await(); //Maximize chances of working concurrently on the Serializer
-				serializer.toLuceneWorks( serializedModel );
-			}
-			catch (InterruptedException|RuntimeException e) {
-				e.printStackTrace();
-				somethingFailed.set( true );
-			}
-			endLatch.countDown();
+			serializer.toLuceneWorks( serializedModel );
 		}
 
 	}
