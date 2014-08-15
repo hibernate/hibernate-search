6b5e00d31407a238c1c2ad486f06722e9cd62af5
Sanne Grinovero <sanne@hibernate.org>
2014-08-15 20:28:36 +0100

    HSEARCH-1652 Refactor SerializationTest to allow reuse of most of its helpers

--- serialization/avro/src/test/java/org/hibernate/search/test/serialization/SerializationTest.java
+++ serialization/avro/src/test/java/org/hibernate/search/test/serialization/SerializationTest.java
@@ -6,41 +6,23 @@
  */
 package org.hibernate.search.test.serialization;
 
-import java.io.IOException;
-import java.io.Reader;
-import java.io.Serializable;
+import static org.fest.assertions.Assertions.assertThat;
+import static org.junit.Assert.assertTrue;
+
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.lucene.analysis.TokenStream;
-import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
-import org.apache.lucene.analysis.tokenattributes.CharTermAttributeImpl;
-import org.apache.lucene.analysis.tokenattributes.FlagsAttribute;
-import org.apache.lucene.analysis.tokenattributes.FlagsAttributeImpl;
-import org.apache.lucene.analysis.tokenattributes.KeywordAttribute;
-import org.apache.lucene.analysis.tokenattributes.KeywordAttributeImpl;
-import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
-import org.apache.lucene.analysis.tokenattributes.OffsetAttributeImpl;
-import org.apache.lucene.analysis.tokenattributes.PayloadAttribute;
-import org.apache.lucene.analysis.tokenattributes.PayloadAttributeImpl;
-import org.apache.lucene.analysis.tokenattributes.PositionIncrementAttribute;
-import org.apache.lucene.analysis.tokenattributes.PositionIncrementAttributeImpl;
-import org.apache.lucene.analysis.tokenattributes.TypeAttribute;
-import org.apache.lucene.analysis.tokenattributes.TypeAttributeImpl;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.DoubleField;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.Field.Store;
-import org.apache.lucene.document.FieldType;
 import org.apache.lucene.document.FloatField;
 import org.apache.lucene.document.IntField;
 import org.apache.lucene.document.LongField;
-import org.apache.lucene.index.IndexableField;
 import org.apache.lucene.util.AttributeImpl;
-import org.apache.lucene.util.BytesRef;
 import org.hibernate.search.backend.AddLuceneWork;
 import org.hibernate.search.backend.DeleteLuceneWork;
 import org.hibernate.search.backend.LuceneWork;
@@ -53,19 +35,13 @@ import org.hibernate.search.indexes.serialization.avro.impl.AvroSerializationPro
 import org.hibernate.search.indexes.serialization.impl.CopyTokenStream;
 import org.hibernate.search.indexes.serialization.impl.LuceneWorkSerializerImpl;
 import org.hibernate.search.indexes.serialization.spi.LuceneWorkSerializer;
-import org.hibernate.search.indexes.serialization.spi.SerializableTokenStream;
 import org.hibernate.search.indexes.serialization.spi.SerializationProvider;
+import org.hibernate.search.test.serialization.AvroTestHelpers.SerializableStringReader;
 import org.hibernate.search.testsupport.junit.SearchFactoryHolder;
 import org.hibernate.search.testsupport.setup.SearchConfigurationForTest;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-
-import static org.fest.assertions.Assertions.assertThat;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 /**
  * @author Emmanuel Bernard <emmanuel@hibernate.org>
@@ -103,7 +79,7 @@ public class SerializationTest {
 
 		assertThat( copyOfWorks ).hasSize( works.size() );
 		for ( int index = 0; index < works.size(); index++ ) {
-			assertLuceneWork( works.get( index ), copyOfWorks.get( index ) );
+			AvroTestHelpers.assertLuceneWork( works.get( index ), copyOfWorks.get( index ) );
 		}
 	}
 
@@ -175,7 +151,7 @@ public class SerializationTest {
 		field = new Field( "ReaderField", reader, Field.TermVector.WITH_OFFSETS );
 		doc.add( field );
 
-		List<List<AttributeImpl>> tokens = buildTokenSteamWithAttributes();
+		List<List<AttributeImpl>> tokens = AvroTestHelpers.buildTokenSteamWithAttributes();
 
 		CopyTokenStream tokenStream = new CopyTokenStream( tokens );
 		field = new Field( "tokenstream", tokenStream, Field.TermVector.WITH_POSITIONS_OFFSETS );
@@ -189,242 +165,4 @@ public class SerializationTest {
 		return works;
 	}
 
-	private static List<List<AttributeImpl>> buildTokenSteamWithAttributes() {
-		List<List<AttributeImpl>> tokens = new ArrayList<List<AttributeImpl>>();
-		tokens.add( new ArrayList<AttributeImpl>() );
-
-		CharTermAttributeImpl charAttr = new CharTermAttributeImpl();
-		charAttr.append( "Wazzza" );
-		tokens.get( 0 ).add( charAttr );
-
-		PayloadAttributeImpl payloadAttribute = new PayloadAttributeImpl();
-		payloadAttribute.setPayload( new BytesRef( new byte[] { 0, 1, 2, 3 } ) );
-		tokens.get( 0 ).add( payloadAttribute );
-
-		KeywordAttributeImpl keywordAttr = new KeywordAttributeImpl();
-		keywordAttr.setKeyword( true );
-		tokens.get( 0 ).add( keywordAttr );
-
-		PositionIncrementAttributeImpl posIncrAttr = new PositionIncrementAttributeImpl();
-		posIncrAttr.setPositionIncrement( 3 );
-		tokens.get( 0 ).add( posIncrAttr );
-
-		FlagsAttributeImpl flagsAttr = new FlagsAttributeImpl();
-		flagsAttr.setFlags( 435 );
-		tokens.get( 0 ).add( flagsAttr );
-
-		TypeAttributeImpl typeAttr = new TypeAttributeImpl();
-		typeAttr.setType( "acronym" );
-		tokens.get( 0 ).add( typeAttr );
-
-		OffsetAttributeImpl offsetAttr = new OffsetAttributeImpl();
-		offsetAttr.setOffset( 4, 7 );
-		tokens.get( 0 ).add( offsetAttr );
-		return tokens;
-	}
-
-	private void assertLuceneWork(LuceneWork work, LuceneWork copy) {
-		assertThat( copy ).isInstanceOf( work.getClass() );
-		if ( work instanceof OptimizeLuceneWork ) {
-			assertNotNull( copy );
-			assertTrue( copy instanceof OptimizeLuceneWork );
-		}
-		else if ( work instanceof PurgeAllLuceneWork ) {
-			assertPurgeAll( (PurgeAllLuceneWork) work, (PurgeAllLuceneWork) copy );
-		}
-		else if ( work instanceof DeleteLuceneWork ) {
-			assertDelete( (DeleteLuceneWork) work, (DeleteLuceneWork) copy );
-		}
-		else if ( work instanceof AddLuceneWork ) {
-			assertAdd( (AddLuceneWork) work, (AddLuceneWork) copy );
-		}
-		else if ( work instanceof UpdateLuceneWork ) {
-			assertUpdate( (UpdateLuceneWork) work, (UpdateLuceneWork) copy );
-		}
-		else {
-			fail( "unexpected type" );
-		}
-	}
-
-	private void assertAdd(AddLuceneWork work, AddLuceneWork copy) {
-		assertThat( work.getEntityClass() ).as( "Add.getEntityClass is not copied" ).isEqualTo( copy.getEntityClass() );
-		assertThat( work.getId() ).as( "Add.getId is not copied" ).isEqualTo( copy.getId() );
-		assertThat( work.getIdInString() ).as( "Add.getIdInString is not the same" ).isEqualTo( copy.getIdInString() );
-		assertThat( work.getFieldToAnalyzerMap() ).as( "Add.getFieldToAnalyzerMap is not the same" )
-				.isEqualTo( copy.getFieldToAnalyzerMap() );
-		assertDocument( work.getDocument(), copy.getDocument() );
-	}
-
-	private void assertUpdate(UpdateLuceneWork work, UpdateLuceneWork copy) {
-		assertThat( work.getEntityClass() ).as( "Add.getEntityClass is not copied" ).isEqualTo( copy.getEntityClass() );
-		assertThat( work.getId() ).as( "Add.getId is not copied" ).isEqualTo( copy.getId() );
-		assertThat( work.getIdInString() ).as( "Add.getIdInString is not the same" ).isEqualTo( copy.getIdInString() );
-		assertThat( work.getFieldToAnalyzerMap() ).as( "Add.getFieldToAnalyzerMap is not the same" )
-				.isEqualTo( copy.getFieldToAnalyzerMap() );
-		assertDocument( work.getDocument(), copy.getDocument() );
-	}
-
-	private void assertDocument(Document original, Document copy) {
-		assertThat( original.getFields().size() ).isEqualTo( copy.getFields().size() );
-		for ( int index = 0; index < original.getFields().size(); index++ ) {
-			IndexableField field = original.getFields().get( index );
-			IndexableField fieldCopy = copy.getFields().get( index );
-			assertFieldEquality( (Field) field, (Field) fieldCopy );
-		}
-	}
-
-	private void assertFieldEquality(Field original, Field copy) {
-		assertThat( copy.name() ).isEqualTo( original.name() );
-		assertThat( copy.binaryValue() ).isEqualTo( original.binaryValue() );
-		assertThat( copy.boost() ).isEqualTo( original.boost() );
-		assertFieldType( copy.fieldType(), original.fieldType() );
-		assertThat( compareReaders( copy.readerValue(), original.readerValue() ) ).isTrue();
-		assertThat( compareTokenStreams( original.tokenStreamValue(), copy.tokenStreamValue() ) ).isTrue();
-		assertThat( copy.stringValue() ).isEqualTo( original.stringValue() );
-	}
-
-	private void assertFieldType(FieldType copy, FieldType original) {
-		assertThat( original.omitNorms() ).isEqualTo( copy.omitNorms() );
-		assertThat( original.storeTermVectorOffsets() ).isEqualTo( copy.storeTermVectorOffsets() );
-		assertThat( original.storeTermVectorPayloads() ).isEqualTo( copy.storeTermVectorPayloads() );
-		assertThat( original.storeTermVectorOffsets() ).isEqualTo( copy.storeTermVectorOffsets() );
-		assertThat( original.docValueType() ).isEqualTo( copy.docValueType() );
-		assertThat( original.indexed() ).isEqualTo( copy.indexed() );
-		assertThat( original.indexOptions() ).isEqualTo( copy.indexOptions() );
-		assertThat( original.numericPrecisionStep() ).isEqualTo( copy.numericPrecisionStep() );
-		assertThat( original.numericType() ).isEqualTo( copy.numericType() );
-		assertThat( original.stored() ).isEqualTo( copy.stored() );
-		assertThat( original.storeTermVectors() ).isEqualTo( copy.storeTermVectors() );
-		assertThat( original.tokenized() ).isEqualTo( copy.tokenized() );
-		assertThat( original.toString() ).isEqualTo( copy.toString() );
-	}
-
-	private boolean compareTokenStreams(TokenStream original, TokenStream copy) {
-		if ( original == null ) {
-			return copy == null;
-		}
-		try {
-			original.reset();
-		}
-		catch (IOException e) {
-			throw new RuntimeException( e );
-		}
-		SerializableTokenStream serOriginal = CopyTokenStream.buildSerializabletokenStream( original );
-		SerializableTokenStream serCopy = CopyTokenStream.buildSerializabletokenStream( copy );
-		if ( serOriginal.getStream().size() != serCopy.getStream().size() ) {
-			return false;
-		}
-		for ( int i = 0; i < serOriginal.getStream().size(); i++ ) {
-			List<AttributeImpl> origToken = serOriginal.getStream().get( i );
-			List<AttributeImpl> copyToken = serCopy.getStream().get( i );
-			if ( origToken.size() != copyToken.size() ) {
-				return false;
-			}
-			for ( int j = 0; j < origToken.size(); j++ ) {
-				AttributeImpl origAttr = origToken.get( j );
-				AttributeImpl copyAttr = copyToken.get( j );
-				if ( origAttr.getClass() != copyAttr.getClass() ) {
-					return false;
-				}
-				testAttributeTypes( origAttr, copyAttr );
-			}
-		}
-		return true;
-	}
-
-	private void testAttributeTypes(AttributeImpl origAttr, AttributeImpl copyAttr) {
-		if ( origAttr instanceof CharTermAttribute ) {
-			assertThat( origAttr.toString() ).isEqualTo( copyAttr.toString() );
-		}
-		else if ( origAttr instanceof PayloadAttribute ) {
-			assertThat( ( (PayloadAttribute) origAttr ).getPayload() ).isEqualTo(
-					( (PayloadAttribute) copyAttr ).getPayload()
-			);
-		}
-		else if ( origAttr instanceof KeywordAttribute ) {
-			assertThat( ( (KeywordAttribute) origAttr ).isKeyword() ).isEqualTo(
-					( (KeywordAttribute) copyAttr ).isKeyword()
-			);
-		}
-		else if ( origAttr instanceof PositionIncrementAttribute ) {
-			assertThat( ( (PositionIncrementAttribute) origAttr ).getPositionIncrement() ).isEqualTo(
-					( (PositionIncrementAttribute) copyAttr ).getPositionIncrement()
-			);
-		}
-		else if ( origAttr instanceof FlagsAttribute ) {
-			assertThat( ( (FlagsAttribute) origAttr ).getFlags() ).isEqualTo(
-					( (FlagsAttribute) copyAttr ).getFlags()
-			);
-		}
-		else if ( origAttr instanceof TypeAttribute ) {
-			assertThat( ( (TypeAttribute) origAttr ).type() ).isEqualTo(
-					( (TypeAttribute) copyAttr ).type()
-			);
-		}
-		else if ( origAttr instanceof OffsetAttribute ) {
-			OffsetAttribute orig = (OffsetAttribute) origAttr;
-			OffsetAttribute cop = (OffsetAttribute) copyAttr;
-			assertThat( orig.startOffset() ).isEqualTo( cop.startOffset() );
-			assertThat( orig.endOffset() ).isEqualTo( cop.endOffset() );
-		}
-		else {
-			Assert.fail( "Unexpected Attribute implementation received" );
-		}
-	}
-
-	private boolean compareReaders(Reader copy, Reader original) {
-		if ( original == null ) {
-			return copy == null;
-		}
-		try {
-			for ( int o = original.read(); o != -1; o = original.read() ) {
-				int c = copy.read();
-				if ( o != c ) {
-					return false;
-				}
-			}
-			return copy.read() == -1;
-		}
-		catch (IOException e) {
-			throw new RuntimeException( e );
-		}
-	}
-
-	private void assertDelete(DeleteLuceneWork work, DeleteLuceneWork copy) {
-		assertThat( work.getEntityClass() ).as( "Delete.getEntityClass is not copied" )
-				.isEqualTo( copy.getEntityClass() );
-		assertThat( work.getId() ).as( "Delete.getId is not copied" ).isEqualTo( copy.getId() );
-		assertThat( (Object) work.getDocument() ).as( "Delete.getDocument is not the same" )
-				.isEqualTo( copy.getDocument() );
-		assertThat( work.getIdInString() ).as( "Delete.getIdInString is not the same" )
-				.isEqualTo( copy.getIdInString() );
-		assertThat( work.getFieldToAnalyzerMap() ).as( "Delete.getFieldToAnalyzerMap is not the same" )
-				.isEqualTo( copy.getFieldToAnalyzerMap() );
-	}
-
-	private void assertPurgeAll(PurgeAllLuceneWork work, PurgeAllLuceneWork copy) {
-		assertThat( work.getEntityClass() ).as( "PurgeAllLuceneWork.getEntityClass is not copied" )
-				.isEqualTo( copy.getEntityClass() );
-	}
-
-	private static class SerializableStringReader extends Reader implements Serializable {
-		private boolean read = false;
-
-		@Override
-		public int read(char[] cbuf, int off, int len) throws IOException {
-			if ( read ) {
-				return -1;
-			}
-			else {
-				read = true;
-				cbuf[off] = 2;
-				return 1;
-			}
-		}
-
-		@Override
-		public void close() throws IOException {
-		}
-
-	}
 }
