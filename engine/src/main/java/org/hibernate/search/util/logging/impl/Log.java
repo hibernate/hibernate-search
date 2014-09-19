478cb02ec3c225c8352f6f909f8aacf45c393f3b
Sanne Grinovero <sanne@hibernate.org>
2014-09-19 18:09:40 +0100

    HSEARCH-1672 backport utility method ConfigurationParseHelper.getIntValue(Properties, String)

--- engine/src/main/java/org/hibernate/search/util/logging/impl/Log.java
+++ engine/src/main/java/org/hibernate/search/util/logging/impl/Log.java
@@ -761,6 +761,9 @@ public interface Log extends BasicLogger {
 	@Message(id = 194, value = "Unable to load configured class '%s' as 'sharding_strategy'")
 	SearchException getUnableToLoadShardingStrategyClassException(String className);
 
+	@Message(id = 195, value = "Configuration property '%s' should not be empty: illegal format.")
+	SearchException configurationPropertyCantBeEmpty(String key);
+
 	@Message(id = 211, value = "An exception occurred while the MassIndexer was fetching the primary identifiers list")
 	String massIndexerExceptionWhileFetchingIds();
 
