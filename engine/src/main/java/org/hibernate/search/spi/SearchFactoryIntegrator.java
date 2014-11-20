a5c71698f86d9c80106d6ad19d6e9808a1d1d8af
Sanne Grinovero <sanne@hibernate.org>
2014-11-20 15:38:44 +0000

    HSEARCH-1727 Re-introduce a temporary deprecated version of SearchFactoryIntegrator to be able to upgrade dependencies which require it

---
+++ engine/src/main/java/org/hibernate/search/spi/SearchFactoryIntegrator.java
@@ -0,0 +1,17 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.spi;
+
+
+/**
+ * This interface is a temporary placeholder and will be removed at the next micro release!
+ * @deprecated Do not use, and immediately migrate all code to use org.hibernate.search.spi.SearchIntegrator.
+ */
+@Deprecated
+public interface SearchFactoryIntegrator extends SearchIntegrator {
+
+}
