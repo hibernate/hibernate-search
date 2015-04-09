42539c19af4666b86399f84d37c8774f770da791
Gunnar Morling <gunnar.morling@googlemail.com>
2015-04-09 12:18:28 +0200

    HSEARCH-1793 Making CriteriaObjectInitializer work correctly for cross-hierarchy query results by creating one Criteria for each id space

---
+++ orm/src/test/java/org/hibernate/search/test/query/dsl/mixedhierarchy/CommunityCollege.java
@@ -0,0 +1,26 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.test.query.dsl.mixedhierarchy;
+
+import javax.persistence.Entity;
+
+import org.hibernate.search.annotations.Indexed;
+
+/**
+ * @author Gunnar Morling
+ */
+@Indexed
+@Entity
+public class CommunityCollege extends College {
+
+	CommunityCollege() {
+	}
+
+	public CommunityCollege(long identifier, String name) {
+		super( identifier, name );
+	}
+}
---
+++ orm/src/test/java/org/hibernate/search/test/query/dsl/mixedhierarchy/University.java
@@ -0,0 +1,39 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.test.query.dsl.mixedhierarchy;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.hibernate.search.annotations.Indexed;
+
+/**
+ * @author Gunnar Morling
+ */
+@Indexed
+@Entity
+public class University extends EducationalInstitution {
+
+	@Id
+	Integer id;
+
+	University() {
+	}
+
+	public University(int id, String name) {
+		super( name );
+		this.id = id;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+}
