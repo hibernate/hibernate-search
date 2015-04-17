b0b43a8b1f7ea2da66c53a41647903a4fefa0ae8
Hardy Ferentschik <hardy@hibernate.org>
2015-04-17 12:55:28 +0200

    HSEARCH-812 Adding the ability to select facets either via 'OR' or 'AND' using FacetCombine

---
+++ orm/src/test/java/org/hibernate/search/test/query/facet/Ingredient.java
@@ -0,0 +1,45 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.test.query.facet;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.search.annotations.Analyze;
+import org.hibernate.search.annotations.Facet;
+import org.hibernate.search.annotations.Field;
+
+@Entity
+public class Ingredient {
+	@Id
+	@GeneratedValue
+	private int id;
+
+	@Field(analyze = Analyze.NO)
+	@Facet
+	String name;
+
+	public Ingredient() {
+	}
+
+	public Ingredient(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String toString() {
+		return "Ingredient{" +
+				"id=" + id +
+				", name='" + name + '\'' +
+				'}';
+	}
+}
