f167a5401d74f812a58aa5968b1eaf16fc3420c5
Gunnar Morling <gunnar.morling@googlemail.com>
2015-07-07 18:38:33 +0200

    HSEARCH-1849 Using UninvertingReader for sorting on non doc value fields

--- engine/src/main/java/org/hibernate/search/reader/impl/ManagedMultiReader.java
+++ engine/src/main/java/org/hibernate/search/reader/impl/ManagedMultiReader.java
@@ -8,9 +8,16 @@ package org.hibernate.search.reader.impl;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.MultiReader;
+import org.apache.lucene.search.Sort;
+import org.apache.lucene.search.SortField;
+import org.apache.lucene.uninverting.UninvertingReader;
+import org.apache.lucene.uninverting.UninvertingReader.Type;
 import org.hibernate.search.indexes.spi.ReaderProvider;
 import org.hibernate.search.util.logging.impl.Log;
 import org.hibernate.search.util.logging.impl.LoggerFactory;
@@ -25,15 +32,94 @@ public class ManagedMultiReader extends MultiReader {
 
 	private static final Log log = LoggerFactory.make();
 
-	final IndexReader[] subReaders;
+	/**
+	 * The index readers to be closed in {@link #doClose()}. Will be the readers originally given upon instantiation in
+	 * case it was required to wrap those with {@link UninvertingReader}s for the purposes of sorting prior to passing
+	 * them to super.
+	 */
+	final IndexReader[] readersForClosing;
 	final ReaderProvider[] managers;
 
-	public ManagedMultiReader(IndexReader[] subReaders, ReaderProvider[] managers) throws IOException {
+	private ManagedMultiReader(IndexReader[] subReaders, IndexReader[] readersForClosing, ReaderProvider[] managers) throws IOException {
 		// If this flag isn't set to true, the MultiReader will increase the usage counter!
 		super( subReaders, true );
-		this.subReaders = subReaders;
+
+		this.readersForClosing = readersForClosing;
 		this.managers = managers;
-		assert subReaders.length == managers.length;
+		assert readersForClosing.length == managers.length;
+	}
+
+	public static ManagedMultiReader createInstance(IndexReader[] subReaders, ReaderProvider[] managers, Sort sort) throws IOException {
+		IndexReader[] effectiveReaders = getEffectiveReaders( subReaders, sort );
+		return new ManagedMultiReader( effectiveReaders, subReaders, managers );
+	}
+
+	/**
+	 * Gets the readers to be effectively used. Will be the given readers, if there is no sort involved. Otherwise each
+	 * directory reader will be wrapped in a {@link UninvertingReader} configured in a way to satisfy the requested
+	 * sorts.
+	 */
+	private static IndexReader[] getEffectiveReaders(IndexReader[] subReaders, Sort sort) {
+		if ( sort == null || sort.getSort().length == 0 ) {
+			return subReaders;
+		}
+
+		IndexReader[] uninvertingReaders = new IndexReader[subReaders.length];
+		Map<String, Type> mappings = getMappings( sort );
+
+		int i = 0;
+		for ( IndexReader reader : subReaders ) {
+			if ( reader instanceof DirectoryReader ) {
+				DirectoryReader directoryReader = (DirectoryReader) reader;
+
+				try {
+					uninvertingReaders[i] = UninvertingReader.wrap( directoryReader, mappings );
+				}
+				catch (IOException e) {
+					throw log.couldNotCreateUninvertingReader( directoryReader, e );
+				}
+			}
+			else {
+				log.readerTypeUnsupportedForInverting( reader.getClass() );
+				uninvertingReaders[i] = reader;
+			}
+
+			i++;
+		}
+
+		return uninvertingReaders;
+	}
+
+	/**
+	 * Returns the uninverting reader mappings required for the given non-null sort.
+	 */
+	private static Map<String, UninvertingReader.Type> getMappings(Sort sort) {
+		Map<String,UninvertingReader.Type> mappings = new HashMap<>();
+
+		for ( SortField sortField : sort.getSort() ) {
+			if ( sortField.getField() != null ) {
+				switch ( sortField.getType() ) {
+					case INT: mappings.put( sortField.getField(), Type.INTEGER );
+					break;
+					case LONG: mappings.put( sortField.getField(), Type.LONG );
+					break;
+					case FLOAT: mappings.put( sortField.getField(), Type.FLOAT );
+					break;
+					case DOUBLE: mappings.put( sortField.getField(), Type.DOUBLE );
+					break;
+					case STRING:
+					case STRING_VAL: mappings.put( sortField.getField(), Type.SORTED );
+					break;
+					case BYTES: mappings.put( sortField.getField(), Type.BINARY );
+					break;
+					case CUSTOM: // Nothing to do; expecting doc value fields created by the user
+					break;
+					default: log.sortFieldTypeUnsupported( sortField.getField(), sortField.getType() );
+				}
+			}
+		}
+
+		return mappings;
 	}
 
 	@Override
@@ -47,9 +133,9 @@ public class ManagedMultiReader extends MultiReader {
 		if ( debugEnabled ) {
 			log.debugf( "Closing MultiReader: %s", this );
 		}
-		for ( int i = 0; i < subReaders.length; i++ ) {
+		for ( int i = 0; i < readersForClosing.length; i++ ) {
 			ReaderProvider container = managers[i];
-			container.closeIndexReader( subReaders[i] ); // might be virtual
+			container.closeIndexReader( readersForClosing[i] ); // might be virtual
 		}
 		if ( debugEnabled ) {
 			log.trace( "MultiReader closed." );
@@ -58,7 +144,6 @@ public class ManagedMultiReader extends MultiReader {
 
 	@Override
 	public String toString() {
-		return ManagedMultiReader.class.getSimpleName() + " [subReaders=" + Arrays.toString( subReaders ) + ", managers=" + Arrays.toString( managers ) + "]";
+		return ManagedMultiReader.class.getSimpleName() + " [readersForClosing=" + Arrays.toString( readersForClosing ) + ", managers=" + Arrays.toString( managers ) + "]";
 	}
-
 }
