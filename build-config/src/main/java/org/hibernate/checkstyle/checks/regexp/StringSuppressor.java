a29015d7132bdfc1efe9623d9ef38818d7d8bc9c
Sanne Grinovero <sanne@hibernate.org>
2015-09-08 16:24:37 +0100

    Avoid linking to internal javadoc from CheckStyle

--- build-config/src/main/java/org/hibernate/checkstyle/checks/regexp/StringSuppressor.java
+++ build-config/src/main/java/org/hibernate/checkstyle/checks/regexp/StringSuppressor.java
@@ -9,7 +9,7 @@ package org.hibernate.checkstyle.checks.regexp;
 import com.puppycrawl.tools.checkstyle.api.FileContents;
 
 /**
- * {@link com.puppycrawl.tools.checkstyle.checks.regexp.MatchSuppressor} used to suppress a violation if it happens inside a {@link String}.
+ * A MatchSuppressor used to suppress a violation if it happens inside a {@link String}.
  *
  * @author Davide D'Alto
  */
