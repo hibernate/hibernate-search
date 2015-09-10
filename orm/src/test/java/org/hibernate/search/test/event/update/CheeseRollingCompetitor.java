957d702fef3b6384c62c8d75e455f86a1e68afb1
Davide D'Alto <davide@hibernate.org>
2015-09-10 18:54:51 +0100

    HSEARCH-1999 Test

---
+++ orm/src/test/java/org/hibernate/search/test/event/update/CheeseRollingCompetitor.java
@@ -0,0 +1,28 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.test.event.update;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.hibernate.search.annotations.Analyze;
+import org.hibernate.search.annotations.Field;
+import org.hibernate.search.annotations.Index;
+import org.hibernate.search.annotations.Indexed;
+import org.hibernate.search.annotations.Store;
+
+@Indexed
+@Entity
+public class CheeseRollingCompetitor {
+
+	@Id
+	public Integer id;
+
+	@Field(name = "Nickname", index = Index.YES, analyze = Analyze.YES, store = Store.YES)
+	public String nickname;
+
+}
