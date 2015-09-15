31458900721bbafaf580c9c0c6f27754db799e34
Gunnar Morling <gunnar.morling@googlemail.com>
2015-09-15 09:01:52 +0200

    HSEARCH-2003 Considering field name when configuring numeric fields through the DSL

--- engine/src/main/java/org/hibernate/search/cfg/NumericFieldMapping.java
+++ engine/src/main/java/org/hibernate/search/cfg/NumericFieldMapping.java
@@ -14,10 +14,11 @@ import java.util.Map;
  */
 public class NumericFieldMapping extends FieldMapping {
 
-	private final Map<String, Object> numericField = new HashMap<String, Object>();
+	private final Map<String, Object> numericField = new HashMap<String, Object>( 2 );
 
-	public NumericFieldMapping(PropertyDescriptor property, EntityDescriptor entity, SearchMapping mapping) {
+	public NumericFieldMapping(String fieldName, PropertyDescriptor property, EntityDescriptor entity, SearchMapping mapping) {
 		super( property, entity, mapping );
+		numericField.put( "forField", fieldName );
 		property.addNumericField( numericField );
 	}
 
@@ -25,5 +26,4 @@ public class NumericFieldMapping extends FieldMapping {
 		numericField.put( "precisionStep", precisionStep );
 		return this;
 	}
-
 }
