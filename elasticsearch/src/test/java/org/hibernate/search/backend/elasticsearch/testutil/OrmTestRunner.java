c97b8ffd0f599d8ad755148d0a5b1faabadf7711
Sanne Grinovero <sanne@hibernate.org>
2016-02-26 15:36:16 +0000

    HSEARCH-2144 Remind how to run an Elasticsearch node for IDE debugging

--- elasticsearch/src/test/java/org/hibernate/search/backend/elasticsearch/testutil/OrmTestRunner.java
+++ elasticsearch/src/test/java/org/hibernate/search/backend/elasticsearch/testutil/OrmTestRunner.java
@@ -13,6 +13,10 @@ import org.junit.runner.RunWith;
  * Helper for running specific tests from the "orm" module in the context of the ES backend module. Adapt the classname
  * filter as needed to run a single test or an entire test package.
  *
+ * Running such tests from the IDE will require having an Elasticsearch
+ * node running. You can easily start one by running 'mvn elasticsearch:run'
+ * from the elasticsearch module in this project.
+ *
  * @author Gunnar Morling
  */
 @RunWith(ClasspathSuite.class)
