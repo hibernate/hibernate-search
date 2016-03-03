b109116ceeced4421382bd09bfefdefb40184871
Gunnar Morling <gunnar.morling@googlemail.com>
2016-03-03 19:44:23 +0100

    HSEARCH-2152 Bulk requests per list of Lucene works

--- elasticsearch/src/main/java/org/hibernate/search/backend/elasticsearch/impl/ElasticsearchBackendQueueProcessor.java
+++ elasticsearch/src/main/java/org/hibernate/search/backend/elasticsearch/impl/ElasticsearchBackendQueueProcessor.java
@@ -6,15 +6,26 @@
  */
 package org.hibernate.search.backend.elasticsearch.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.locks.Lock;
 
 import org.hibernate.search.backend.IndexingMonitor;
 import org.hibernate.search.backend.LuceneWork;
+import org.hibernate.search.backend.elasticsearch.client.impl.JestClient;
 import org.hibernate.search.backend.spi.BackendQueueProcessor;
+import org.hibernate.search.engine.integration.impl.ExtendedSearchIntegrator;
+import org.hibernate.search.engine.service.spi.ServiceReference;
 import org.hibernate.search.indexes.spi.IndexManager;
 import org.hibernate.search.spi.WorkerBuildContext;
+
+import io.searchbox.action.Action;
+import io.searchbox.action.BulkableAction;
+import io.searchbox.core.Bulk;
+import io.searchbox.core.Bulk.Builder;
+import io.searchbox.indices.Refresh;
+import io.searchbox.params.Parameters;
 
 /**
  * {@link BackendQueueProcessor} applying index changes to an Elasticsearch server.
@@ -24,10 +35,12 @@ import org.hibernate.search.spi.WorkerBuildContext;
 public class ElasticsearchBackendQueueProcessor implements BackendQueueProcessor {
 
 	private ElasticsearchIndexManager indexManager;
+	private ExtendedSearchIntegrator searchIntegrator;
 
 	@Override
 	public void initialize(Properties props, WorkerBuildContext context, IndexManager indexManager) {
 		this.indexManager = (ElasticsearchIndexManager) indexManager;
+		this.searchIntegrator = context.getUninitializedSearchIntegrator();
 	}
 
 	@Override
@@ -36,26 +49,83 @@ public class ElasticsearchBackendQueueProcessor implements BackendQueueProcessor
 
 	@Override
 	public void applyWork(List<LuceneWork> workList, IndexingMonitor monitor) {
+		List<BulkableAction<?>> actions = new ArrayList<>( workList.size() );
+
+		// group actions into bulks if their type permits it; otherwise execute them right away and start a new bulk
 		for ( LuceneWork luceneWork : workList ) {
-			luceneWork.acceptIndexWorkVisitor(
+			Action<?> action = luceneWork.acceptIndexWorkVisitor(
 					new ElasticsearchIndexWorkVisitor(
 							indexManager.getActualIndexName(),
 							indexManager.searchIntegrator
 					),
-					null
+					false
 			);
+
+			// either add to bulk
+			if ( action instanceof BulkableAction ) {
+				actions.add( (BulkableAction<?>) action );
+			}
+			// or execute the bulk built so far and execute the non-bulkable action
+			else {
+				executeBulkAndClear( actions, false );
+
+				try ( ServiceReference<JestClient> client = searchIntegrator.getServiceManager().requestReference( JestClient.class ) ) {
+					client.get().executeRequest( action );
+				}
+			}
 		}
+
+		boolean lastActionWasBulk = executeBulkAndClear( actions, true );
+
+		if ( !lastActionWasBulk ) {
+			Refresh refresh = new Refresh.Builder().addIndex( indexManager.getActualIndexName() ).build();
+			try ( ServiceReference<JestClient> client = searchIntegrator.getServiceManager().requestReference( JestClient.class ) ) {
+				client.get().executeRequest( refresh );
+			}
+		}
+	}
+
+	/**
+	 * Creates a bulk action from the given list, executes it and clears the list.
+	 */
+	private boolean executeBulkAndClear(List<BulkableAction<?>> actions, boolean refresh) {
+		if ( actions.isEmpty() ) {
+			return false;
+		}
+
+		Builder bulkBuilder = new Bulk.Builder()
+				.setParameter( Parameters.REFRESH, refresh );
+
+		for ( BulkableAction<?> action : actions ) {
+			bulkBuilder.addAction( action );
+		}
+
+		try ( ServiceReference<JestClient> client = searchIntegrator.getServiceManager().requestReference( JestClient.class ) ) {
+			client.get().executeRequest( bulkBuilder.build() );
+		}
+
+		actions.clear();
+
+		return true;
 	}
 
 	@Override
 	public void applyStreamWork(LuceneWork singleOperation, IndexingMonitor monitor) {
-		singleOperation.acceptIndexWorkVisitor(
+		Action<?> action = singleOperation.acceptIndexWorkVisitor(
 				new ElasticsearchIndexWorkVisitor(
 						indexManager.getActualIndexName(),
 						indexManager.searchIntegrator
 				),
-				null
+				true
 		);
+
+		if ( action == null ) {
+			return;
+		}
+
+		try ( ServiceReference<JestClient> client = searchIntegrator.getServiceManager().requestReference( JestClient.class ) ) {
+			client.get().executeRequest( action );
+		}
 	}
 
 	@Override
