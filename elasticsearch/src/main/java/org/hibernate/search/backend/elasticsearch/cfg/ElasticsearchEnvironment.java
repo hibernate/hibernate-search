31b4378fa3fedb148c020e4e5efedfb1c4c1d082
Gunnar Morling <gunnar.morling@googlemail.com>
2016-03-09 09:28:58 +0100

    HSEARCH-2146 Centralizing default values for ES backend properties

--- elasticsearch/src/main/java/org/hibernate/search/backend/elasticsearch/cfg/ElasticsearchEnvironment.java
+++ elasticsearch/src/main/java/org/hibernate/search/backend/elasticsearch/cfg/ElasticsearchEnvironment.java
@@ -13,15 +13,39 @@ package org.hibernate.search.backend.elasticsearch.cfg;
  */
 public final class ElasticsearchEnvironment {
 
+	/**
+	 * Default values for the different settings if no values are given.
+	 */
 	public static final class Defaults {
+		public static final String SERVER_URI = "http://localhost:9200";
+		public static final IndexManagementStrategy INDEX_MANAGEMENT_STRATEGY = IndexManagementStrategy.NONE;
 		public static final int INDEX_MANAGEMENT_WAIT_TIMEOUT = 10_000;
 	}
 
+	/**
+	 * Property for specifying the host name of the Elasticsearch server to connect to. Only a single server (i.e. no
+	 * clusters) is supported at this point.
+	 * <p>
+	 * An URI such as http://myeshost.com:9200 is expected.
+	 * <p>
+	 * Defaults to {@link Defaults#SERVER_URI}.
+	 */
 	public static final String SERVER_URI = "hibernate.search.elasticsearch.host";
+
+	/**
+	 * Property for specifying the strategy for maintaining the Elasticsearch index.
+	 * <p>
+	 * The name of one of the {@link IndexManagementStrategy} constants is expected, e.g. MERGE.
+	 * <p>
+	 *
+	 */
 	public static final String INDEX_MANAGEMENT_STRATEGY = "hibernate.search.elasticsearch.index_management_strategy";
 
 	/**
 	 * Property for specifying the timeout for index management operations (index creation etc.) in milli-seconds.
+	 * <p>
+	 * A numeric value such as 1000 is expected.
+	 * <p>
 	 * Defaults to {@link Defaults#INDEX_MANAGEMENT_WAIT_TIMEOUT} ms.
 	 */
 	public static final String INDEX_MANAGEMENT_WAIT_TIMEOUT = "hibernate.search.elasticsearch.index_management_wait_timeout";
