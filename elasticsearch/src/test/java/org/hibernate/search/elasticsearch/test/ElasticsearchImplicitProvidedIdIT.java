5b6599784222bef2972d9091568a66bc7ca652af
gustavonalle <gustavonalle@gmail.com>
2016-10-26 14:42:38 +0100

    HSEARCH-2432 Test for entity Ids containing underscore lost when querying

--- elasticsearch/src/test/java/org/hibernate/search/elasticsearch/test/ElasticsearchImplicitProvidedIdIT.java
+++ elasticsearch/src/test/java/org/hibernate/search/elasticsearch/test/ElasticsearchImplicitProvidedIdIT.java
@@ -7,6 +7,7 @@
 package org.hibernate.search.elasticsearch.test;
 
 import java.util.ArrayList;
+import java.io.Serializable;
 import java.util.List;
 
 import org.apache.lucene.search.Query;
@@ -20,86 +21,104 @@ import org.hibernate.search.backend.spi.Work;
 import org.hibernate.search.backend.spi.WorkType;
 import org.hibernate.search.cfg.Environment;
 import org.hibernate.search.elasticsearch.cfg.ElasticsearchEnvironment;
+import org.hibernate.search.elasticsearch.cfg.IndexSchemaManagementStrategy;
+import org.hibernate.search.engine.integration.impl.ExtendedSearchIntegrator;
 import org.hibernate.search.query.dsl.QueryBuilder;
 import org.hibernate.search.query.engine.spi.EntityInfo;
 import org.hibernate.search.spi.SearchIntegrator;
-import org.hibernate.search.spi.SearchIntegratorBuilder;
 import org.hibernate.search.testsupport.TestForIssue;
-import org.hibernate.search.testsupport.setup.SearchConfigurationForTest;
+import org.hibernate.search.testsupport.junit.SearchFactoryHolder;
 import org.hibernate.search.testsupport.setup.TransactionContextForTest;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
 
 public class ElasticsearchImplicitProvidedIdIT {
 
+	@Rule
+	public SearchFactoryHolder sfHolder = new SearchFactoryHolder( User.class, UserWithAddress.class )
+			.withProperty( "hibernate.search.default." + Environment.INDEX_MANAGER_IMPL_NAME, "elasticsearch" )
+			.withProperty( "hibernate.search.default." + ElasticsearchEnvironment.REFRESH_AFTER_WRITE, "true" )
+			.withProperty( "hibernate.search.default." + ElasticsearchEnvironment.INDEX_SCHEMA_MANAGEMENT_STRATEGY,
+					IndexSchemaManagementStrategy.RECREATE_DELETE.name() )
+			.withIdProvidedImplicit( true );
+
 	@Test
 	@TestForIssue(jiraKey = "HSEARCH-2431")
 	public void testWithProjection() throws Exception {
-		SearchConfigurationForTest cfg = new SearchConfigurationForTest()
-				.addProperty( "hibernate.search.default." + Environment.INDEX_MANAGER_IMPL_NAME, "elasticsearch" )
-				.addProperty( "hibernate.search.default." + ElasticsearchEnvironment.REFRESH_AFTER_WRITE, "true" )
-				.addClass( User.class )
-				.setIdProvidedImplicit( true );
+		ExtendedSearchIntegrator searchIntegrator = sfHolder.getSearchFactory();
+		User user = new User();
+		user.setId( 1 );
+		user.setSurname( "Smith" );
 
-		SearchIntegratorBuilder searchIntegratorBuilder = new SearchIntegratorBuilder().configuration( cfg );
+		indexUser( user, "S:1", searchIntegrator );
 
-		try (SearchIntegrator searchIntegrator = searchIntegratorBuilder.buildSearchIntegrator();) {
-			User user = new User();
-			user.setId( 1 );
-			user.setSurname( "Smith" );
+		QueryBuilder queryBuilder = searchIntegrator.buildQueryBuilder().forEntity( User.class ).get();
+		Query query = queryBuilder.keyword().onField( "surname" ).matching( "smith" ).createQuery();
 
-			Work work = new Work( user, "S:1", WorkType.ADD, false );
-			TransactionContextForTest tc = new TransactionContextForTest();
-			searchIntegrator.getWorker().performWork( work, tc );
-			tc.end();
+		List<EntityInfo> entityInfoList = searchIntegrator.createHSQuery( query, User.class )
+				.projection( "surname" )
+				.queryEntityInfos();
 
-			QueryBuilder queryBuilder = searchIntegrator.buildQueryBuilder().forEntity( User.class ).get();
-			Query query = queryBuilder.keyword().onField( "surname" ).matching( "smith" ).createQuery();
+		Assert.assertEquals( 1, entityInfoList.size() );
+	}
 
-			List<EntityInfo> entityInfoList = searchIntegrator.createHSQuery( query, User.class )
-					.projection( "surname" )
-					.queryEntityInfos();
+	@Test
+	@TestForIssue(jiraKey = "HSEARCH-2432")
+	public void testWithCustomId() throws Exception {
+		ExtendedSearchIntegrator searchIntegrator = sfHolder.getSearchFactory();
+		User user = new User();
+		user.setId( 325 );
+		user.setSurname( "Lee" );
 
-			Assert.assertEquals( 1, entityInfoList.size() );
-		}
+		Serializable customId = "S:custom_id_325";
+
+		indexUser( user, customId, searchIntegrator );
+
+		QueryBuilder queryBuilder = searchIntegrator.buildQueryBuilder().forEntity( User.class ).get();
+		Query query = queryBuilder.keyword().onField( "surname" ).matching( "Lee" ).createQuery();
+
+		List<EntityInfo> entityInfoList = searchIntegrator.createHSQuery( query, User.class ).queryEntityInfos();
+
+		Assert.assertEquals( 1, entityInfoList.size() );
+		Assert.assertEquals( customId, entityInfoList.iterator().next().getId() );
 	}
 
 	@Test
 	@TestForIssue(jiraKey = "HSEARCH-2433")
 	public void testWithEmbedded() throws Exception {
-		SearchConfigurationForTest cfg = new SearchConfigurationForTest()
-				.addProperty( "hibernate.search.default." + Environment.INDEX_MANAGER_IMPL_NAME, "elasticsearch" )
-				.addProperty( "hibernate.search.default." + ElasticsearchEnvironment.REFRESH_AFTER_WRITE, "true" )
-				.addClass( UserWithAddress.class )
-				.setIdProvidedImplicit( true );
+		ExtendedSearchIntegrator searchIntegrator = sfHolder.getSearchFactory();
 
-		SearchIntegratorBuilder searchIntegratorBuilder = new SearchIntegratorBuilder().configuration( cfg );
+		UserWithAddress user = new UserWithAddress();
+		user.setId( 1 );
+		user.setSurname( "Smith" );
 
-		try (SearchIntegrator searchIntegrator = searchIntegratorBuilder.buildSearchIntegrator();) {
-			UserWithAddress user = new UserWithAddress();
-			user.setId( 1 );
-			user.setSurname( "Smith" );
+		Address address = new Address();
+		address.setStreet( "21st steet" );
+		address.setNumber( 42 );
+		address.setPostCode( "8964" );
+		user.addAddress( address );
 
-			Address address = new Address();
-			address.setStreet( "21st steet" );
-			address.setNumber( 42 );
-			address.setPostCode( "8964" );
-			user.addAddress( address );
+		Work work = new Work( user, "S:1", WorkType.ADD, false );
+		TransactionContextForTest tc = new TransactionContextForTest();
+		searchIntegrator.getWorker().performWork( work, tc );
+		tc.end();
 
-			Work work = new Work( user, "S:1", WorkType.ADD, false );
-			TransactionContextForTest tc = new TransactionContextForTest();
-			searchIntegrator.getWorker().performWork( work, tc );
-			tc.end();
+		QueryBuilder queryBuilder = searchIntegrator.buildQueryBuilder().forEntity( UserWithAddress.class ).get();
+		Query query = queryBuilder.keyword().onField( "surname" ).matching( "smith" ).createQuery();
 
-			QueryBuilder queryBuilder = searchIntegrator.buildQueryBuilder().forEntity( UserWithAddress.class ).get();
-			Query query = queryBuilder.keyword().onField( "surname" ).matching( "smith" ).createQuery();
+		List<EntityInfo> entityInfoList = searchIntegrator.createHSQuery( query, UserWithAddress.class )
+				.projection( "surname" )
+				.queryEntityInfos();
 
-			List<EntityInfo> entityInfoList = searchIntegrator.createHSQuery( query, UserWithAddress.class )
-					.projection( "surname" )
-					.queryEntityInfos();
+		Assert.assertEquals( 1, entityInfoList.size() );
+	}
 
-			Assert.assertEquals( 1, entityInfoList.size() );
-		}
+	private void indexUser(User user, Serializable id, SearchIntegrator searchIntegrator) {
+		Work work = new Work( user, id, WorkType.ADD, false );
+		TransactionContextForTest tc = new TransactionContextForTest();
+		searchIntegrator.getWorker().performWork( work, tc );
+		tc.end();
 	}
 
 	@Indexed
