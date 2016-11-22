hibernate/hibernate-search

commit 3b2927a8cdbfb07945d25313c1b9dedc95482d6c

HSEARCH-2450 Move bridge defined fields metadata to their parent field metadata
Because it's much more convenient when inspecting bridge defined fields.
We almost never have to list the bridge defined fields for a property,
we always start from a document field.

engine/src/main/java/org/hibernate/search/engine/metadata/impl/AnnotationMetadataProvider.java
@@ -368,23 +368,27 @@ public class AnnotationMetadataProvider implements MetadataProvider {
 				.boost( AnnotationProcessingHelper.getBoost( member, null ) )
 				.fieldBridge( idBridge );
 
+		parseContext.setIdFieldPath( path );
+
 		NumericEncodingType numericEncodingType = determineNumericFieldEncoding( idBridge );
 		if ( numericEncodingType != NumericEncodingType.UNKNOWN ) {
 			idMetadataBuilder.numeric();
 			idMetadataBuilder.numericEncodingType( numericEncodingType );
 		}
-		DocumentFieldMetadata fieldMetadata = idMetadataBuilder.build();
-		propertyMetadataBuilder.addDocumentField( fieldMetadata );
 		checkForSortableField( member, typeMetadataBuilder, propertyMetadataBuilder, "", true, null, parseContext );
 		checkForSortableFields( member, typeMetadataBuilder, propertyMetadataBuilder, "", true, null, parseContext );
 
 		if ( idBridge instanceof MetadataProvidingFieldBridge ) {
-			FieldMetadataBuilderImpl bridgeDefinedMetadata = getBridgeContributedFieldMetadata( fieldMetadata, (MetadataProvidingFieldBridge) idBridge );
+			FieldMetadataBuilderImpl bridgeDefinedMetadata = getBridgeContributedFieldMetadata(
+					idMetadataBuilder, (MetadataProvidingFieldBridge) idBridge );
 
 			for ( BridgeDefinedField bridgeDefinedField : bridgeDefinedMetadata.getFields() ) {
-				propertyMetadataBuilder.addBridgeDefinedField( bridgeDefinedField );
+				idMetadataBuilder.addBridgeDefinedField( bridgeDefinedField );
 			}
 		}
+
+		DocumentFieldMetadata fieldMetadata = idMetadataBuilder.build();
+		propertyMetadataBuilder.addDocumentField( fieldMetadata );
 
 		PropertyMetadata idPropertyMetadata = propertyMetadataBuilder
 				.build();
@@ -730,29 +734,29 @@ public class AnnotationMetadataProvider implements MetadataProvider {
 		);
 		Field.TermVector termVector = AnnotationProcessingHelper.getTermVector( classBridgeAnnotation.termVector() );
 
-		AnalyzerReference analyzerReference = null;
-		if ( !parseContext.skipAnalyzers() ) {
-			analyzerReference = AnnotationProcessingHelper.getAnalyzerReference(
-					classBridgeAnnotation.analyzer(),
-					configContext,
-					parseContext.isAnalyzerRemote() );
-			typeMetadataBuilder.addToScopedAnalyzerReference( fieldName, analyzerReference, index );
-		}
-
-		DocumentFieldMetadata fieldMetadata =
+		DocumentFieldMetadata.Builder fieldMetadataBuilder =
 				new DocumentFieldMetadata.Builder(
 						typeMetadataBuilder.getResultReference(),
 						BackReference.<PropertyMetadata>empty(), // Class bridge, there's no related property
 						fieldName, store, index, termVector
 				)
 				.boost( classBridgeAnnotation.boost().value() )
-				.analyzerReference( analyzerReference )
-				.fieldBridge( fieldBridge )
-				.build();
+				.fieldBridge( fieldBridge );
 
+
+		contributeClassBridgeDefinedFields( typeMetadataBuilder, fieldMetadataBuilder, fieldBridge );
+
+		if ( !parseContext.skipAnalyzers() ) {
+			AnalyzerReference analyzerReference = AnnotationProcessingHelper.getAnalyzerReference(
+					classBridgeAnnotation.analyzer(),
+					configContext,
+					parseContext.isAnalyzerRemote() );
+			typeMetadataBuilder.addToScopedAnalyzerReference( fieldName, analyzerReference, index );
+			fieldMetadataBuilder.analyzerReference( analyzerReference );
+		}
+
+		DocumentFieldMetadata fieldMetadata = fieldMetadataBuilder.build();
 		typeMetadataBuilder.addClassBridgeField( fieldMetadata );
-
-		contributeClassBridgeDefinedFields( typeMetadataBuilder, fieldMetadata, fieldBridge );
 	}
 
 	private void bindSpatialAnnotation(Spatial spatialAnnotation,
@@ -780,7 +784,7 @@ public class AnnotationMetadataProvider implements MetadataProvider {
 				configContext.getServiceManager()
 		);
 
-		DocumentFieldMetadata fieldMetadata =
+		DocumentFieldMetadata.Builder fieldMetadataBuilder =
 				new DocumentFieldMetadata.Builder(
 						typeMetadataBuilder.getResultReference(),
 						propertyMetadataBuilder.getResultReference(),
@@ -788,18 +792,19 @@ public class AnnotationMetadataProvider implements MetadataProvider {
 				)
 				.boost( AnnotationProcessingHelper.getBoost( member, spatialAnnotation ) )
 				.fieldBridge( fieldBridge )
-				.spatial()
-				.build();
-
-		propertyMetadataBuilder.addDocumentField( fieldMetadata );
+				.spatial();
 
 		if ( fieldBridge instanceof MetadataProvidingFieldBridge ) {
 			MetadataProvidingFieldBridge metadataProvidingFieldBridge = (MetadataProvidingFieldBridge) fieldBridge;
-			FieldMetadataBuilderImpl bridgeContributedMetadata = getBridgeContributedFieldMetadata( fieldMetadata, metadataProvidingFieldBridge );
+			FieldMetadataBuilderImpl bridgeContributedMetadata = getBridgeContributedFieldMetadata(
+					fieldMetadataBuilder, metadataProvidingFieldBridge );
 			for ( BridgeDefinedField field : bridgeContributedMetadata.getFields() ) {
-				propertyMetadataBuilder.addBridgeDefinedField( field );
+				fieldMetadataBuilder.addBridgeDefinedField( field );
 			}
 		}
+
+		DocumentFieldMetadata fieldMetadata = fieldMetadataBuilder.build();
+		propertyMetadataBuilder.addDocumentField( fieldMetadata );
 
 		if ( member.isCollection() ) {
 			parseContext.collectUnqualifiedCollectionRole( member.getName() );
@@ -828,7 +833,7 @@ public class AnnotationMetadataProvider implements MetadataProvider {
 		Field.TermVector termVector = AnnotationProcessingHelper.getTermVector( TermVector.NO );
 		FieldBridge spatialBridge = determineSpatialFieldBridge( spatialAnnotation, parseContext );
 
-		DocumentFieldMetadata fieldMetadata =
+		DocumentFieldMetadata.Builder fieldMetadataBuilder =
 				new DocumentFieldMetadata.Builder(
 						typeMetadataBuilder.getResultReference(),
 						BackReference.<PropertyMetadata>empty(), // Class-level spatial annotation, there's no related property
@@ -836,12 +841,12 @@ public class AnnotationMetadataProvider implements MetadataProvider {
 				)
 				.boost( spatialAnnotation.boost().value() )
 				.fieldBridge( spatialBridge )
-				.spatial()
-				.build();
+				.spatial();
 
+		contributeClassBridgeDefinedFields( typeMetadataBuilder, fieldMetadataBuilder, spatialBridge );
+
+		DocumentFieldMetadata fieldMetadata = fieldMetadataBuilder.build();
 		typeMetadataBuilder.addClassBridgeField( fieldMetadata );
-
-		contributeClassBridgeDefinedFields( typeMetadataBuilder, fieldMetadata, spatialBridge );
 
 		AnalyzerReference analyzerReference = typeMetadataBuilder.getAnalyzerReference();
 		if ( analyzerReference == null ) {
@@ -849,14 +854,18 @@ public class AnnotationMetadataProvider implements MetadataProvider {
 		}
 	}
 
-	private void contributeClassBridgeDefinedFields(TypeMetadata.Builder typeMetadataBuilder, DocumentFieldMetadata fieldMetadata, FieldBridge fieldBridge) {
+	private void contributeClassBridgeDefinedFields(TypeMetadata.Builder typeMetadataBuilder,
+			DocumentFieldMetadata.Builder fieldMetadataBuilder, FieldBridge fieldBridge) {
 		if ( fieldBridge instanceof MetadataProvidingFieldBridge ) {
 			MetadataProvidingFieldBridge metadataProvidingFieldBridge = (MetadataProvidingFieldBridge) fieldBridge;
 
-			FieldMetadataBuilderImpl classBridgeContributedFieldMetadata = getBridgeContributedFieldMetadata( fieldMetadata, metadataProvidingFieldBridge );
+			FieldMetadataBuilderImpl classBridgeContributedFieldMetadata =
+					getBridgeContributedFieldMetadata( fieldMetadataBuilder, metadataProvidingFieldBridge );
 
 			typeMetadataBuilder.addClassBridgeSortableFields( classBridgeContributedFieldMetadata.getSortableFields() );
-			typeMetadataBuilder.addClassBridgeDefinedFields( classBridgeContributedFieldMetadata.getFields() );
+			for ( BridgeDefinedField bridgeDefinedField : classBridgeContributedFieldMetadata.getFields() ) {
+				fieldMetadataBuilder.addBridgeDefinedField( bridgeDefinedField );
+			}
 		}
 	}
 
@@ -946,25 +955,15 @@ public class AnnotationMetadataProvider implements MetadataProvider {
 			ParseContext parseContext) {
 
 		String sortedFieldName = prefix + ReflectionHelper.getAttributeName( member, sortableFieldAnnotation.forField() );
-		String idFieldName = null;
+		String idFieldPath = parseContext.getIdFieldPath();
 
 		// Make sure a sort on the id field is only added to the idPropertyMetadata
-		if ( isIdProperty ) {
-			idFieldName = propertyMetadataBuilder.getFieldMetadata().iterator().next().getFieldName();
-			if ( !sortedFieldName.equals( idFieldName ) ) {
-				return;
-			}
-		}
-		else {
-			if ( typeMetadataBuilder.getIdPropertyMetadata() != null ) {
-				idFieldName = typeMetadataBuilder.getIdPropertyMetadata().getFieldMetadataSet().iterator().next().getFieldName();
-			}
-			if ( sortedFieldName.equals( idFieldName ) ) {
-				return;
-			}
+		if ( isIdProperty && !sortedFieldName.equals( idFieldPath )
+				|| !isIdProperty && sortedFieldName.equalsIgnoreCase( idFieldPath ) ) {
+			return;
 		}
 
-		if ( !sortedFieldName.equals( idFieldName ) && !containsField( propertyMetadataBuilder, sortedFieldName ) ) {
+		if ( !sortedFieldName.equals( idFieldPath ) && !containsField( propertyMetadataBuilder, sortedFieldName ) ) {
 			if ( parseContext.getLevel() != 0 ) {
 				// Sortable defined on a property not indexed when the entity is embedded. We can skip it.
 				return;
@@ -1334,12 +1333,10 @@ public class AnnotationMetadataProvider implements MetadataProvider {
 			fieldMetadataBuilder.addFacetMetadata( facetMetadataBuilder.build() );
 		}
 
-		DocumentFieldMetadata fieldMetadata = fieldMetadataBuilder.build();
-		propertyMetadataBuilder.addDocumentField( fieldMetadata );
-
 		if ( fieldBridge instanceof MetadataProvidingFieldBridge ) {
 			MetadataProvidingFieldBridge metadataProvidingFieldBridge = (MetadataProvidingFieldBridge) fieldBridge;
-			FieldMetadataBuilderImpl bridgeContributedMetadata = getBridgeContributedFieldMetadata( fieldMetadata, metadataProvidingFieldBridge );
+			FieldMetadataBuilderImpl bridgeContributedMetadata = getBridgeContributedFieldMetadata(
+					fieldMetadataBuilder, metadataProvidingFieldBridge );
 
 			for ( String sortableField : bridgeContributedMetadata.getSortableFields() ) {
 				SortableFieldMetadata sortableFieldMetadata = new SortableFieldMetadata.Builder()
@@ -1349,9 +1346,12 @@ public class AnnotationMetadataProvider implements MetadataProvider {
 			}
 
 			for ( BridgeDefinedField field : bridgeContributedMetadata.getFields() ) {
-				propertyMetadataBuilder.addBridgeDefinedField( field );
+				fieldMetadataBuilder.addBridgeDefinedField( field );
 			}
 		}
+
+		DocumentFieldMetadata fieldMetadata = fieldMetadataBuilder.build();
+		propertyMetadataBuilder.addDocumentField( fieldMetadata );
 
 		// keep track of collection role names for ORM integration optimization based on collection update events
 		parseContext.collectUnqualifiedCollectionRole( member.getName() );
@@ -2001,9 +2001,10 @@ public class AnnotationMetadataProvider implements MetadataProvider {
 		return true;
 	}
 
-	private FieldMetadataBuilderImpl getBridgeContributedFieldMetadata(DocumentFieldMetadata fieldMetadata, MetadataProvidingFieldBridge metadataProvidingFieldBridge) {
-		FieldMetadataBuilderImpl builder = new FieldMetadataBuilderImpl( fieldMetadata );
-		metadataProvidingFieldBridge.configureFieldMetadata( fieldMetadata.getFieldName(), builder );
+	private FieldMetadataBuilderImpl getBridgeContributedFieldMetadata(DocumentFieldMetadata.Builder fieldMetadataBuilder,
+			MetadataProvidingFieldBridge metadataProvidingFieldBridge) {
+		FieldMetadataBuilderImpl builder = new FieldMetadataBuilderImpl( fieldMetadataBuilder.getResultReference() );
+		metadataProvidingFieldBridge.configureFieldMetadata( fieldMetadataBuilder.getFieldName(), builder );
 		return builder;
 	}
 

elasticsearch/src/main/java/org/hibernate/search/elasticsearch/impl/ElasticsearchHSQueryImpl.java
@@ -49,7 +49,6 @@ import org.hibernate.search.engine.impl.FilterDef;
 import org.hibernate.search.engine.integration.impl.ExtendedSearchIntegrator;
 import org.hibernate.search.engine.metadata.impl.BridgeDefinedField;
 import org.hibernate.search.engine.metadata.impl.DocumentFieldMetadata;
-import org.hibernate.search.engine.metadata.impl.PropertyMetadata;
 import org.hibernate.search.engine.service.spi.ServiceReference;
 import org.hibernate.search.engine.spi.DocumentBuilderIndexedEntity;
 import org.hibernate.search.engine.spi.EntityIndexBinding;
@@ -696,7 +695,9 @@ public class ElasticsearchHSQueryImpl extends AbstractHSQuery {
 		// Add to the document the additional fields created when indexing the id
 		private void addIdBridgeDefinedFields(JsonObject hit, EntityIndexBinding binding, Document tmp, ConversionContext conversionContext) {
 			Set<BridgeDefinedField> allBridgeDefinedFields = new HashSet<>();
-			allBridgeDefinedFields.addAll( binding.getDocumentBuilder().getMetadata().getIdPropertyMetadata().getBridgeDefinedFields().values() );
+			String idDocumentFieldName = binding.getDocumentBuilder().getIdKeywordName();
+			allBridgeDefinedFields.addAll( binding.getDocumentBuilder().getMetadata().getIdPropertyMetadata().getFieldMetadata( idDocumentFieldName )
+					.getBridgeDefinedFields().values() );
 			for ( BridgeDefinedField bridgeDefinedField : allBridgeDefinedFields ) {
 				Object fieldValue = getFieldValue( binding, hit, bridgeDefinedField.getName(), conversionContext );
 				tmp.add( new StringField( bridgeDefinedField.getName(), String.valueOf( fieldValue ), Store.NO ) );
@@ -708,7 +709,7 @@ public class ElasticsearchHSQueryImpl extends AbstractHSQuery {
 		 * field bridge. In case this bridge is not a 2-way bridge, the unconverted value will be returned.
 		 */
 		private Object getFieldValue(EntityIndexBinding binding, JsonObject hit, String projectedField, ConversionContext conversionContext) {
-			DocumentFieldMetadata field = FieldHelper.getFieldMetadata( binding, projectedField );
+			DocumentFieldMetadata field = binding.getDocumentBuilder().getTypeMetadata().getDocumentFieldMetadataFor( projectedField );
 
 			if ( field == null ) {
 				// We check if it is a field created by a field bridge
@@ -747,18 +748,7 @@ public class ElasticsearchHSQueryImpl extends AbstractHSQuery {
 		}
 
 		private boolean isBridgeDefinedField(EntityIndexBinding binding, String projectedField) {
-			BridgeDefinedField bridgeDefinedField = binding.getDocumentBuilder().getMetadata().getIdPropertyMetadata().getBridgeDefinedFields().get( projectedField );
-			if ( bridgeDefinedField != null ) {
-				return true;
-			}
-			Set<PropertyMetadata> allPropertyMetadata = binding.getDocumentBuilder().getMetadata().getAllPropertyMetadata();
-			for ( PropertyMetadata propertyMetadata : allPropertyMetadata ) {
-				bridgeDefinedField = propertyMetadata.getBridgeDefinedFields().get( projectedField );
-				if ( bridgeDefinedField != null && bridgeDefinedField.getName().equals( projectedField ) ) {
-					return true;
-				}
-			}
-			return false;
+			return binding.getDocumentBuilder().getTypeMetadata().getBridgeDefinedFieldMetadataFor( projectedField ) != null;
 		}
 
 		private Object convertFieldValue(EntityIndexBinding binding, DocumentFieldMetadata field, JsonElement value, ConversionContext conversionContext) {

elasticsearch/src/main/java/org/hibernate/search/elasticsearch/impl/ElasticsearchIndexManager.java
@@ -698,17 +698,9 @@ public class ElasticsearchIndexManager implements IndexManager, RemoteAnalyzerPr
 	 */
 	private Set<BridgeDefinedField> getNonEmbeddedBridgeDefinedFields(TypeMetadata type) {
 		Set<BridgeDefinedField> bridgeDefinedFields = new HashSet<>();
-
-		bridgeDefinedFields.addAll( type.getClassBridgeDefinedFields() );
-
-		if ( type.getIdPropertyMetadata() != null ) {
-			bridgeDefinedFields.addAll( type.getIdPropertyMetadata().getBridgeDefinedFields().values() );
+		for ( DocumentFieldMetadata documentFieldMetadata : type.getNonEmbeddedDocumentFieldMetadata() ) {
+			bridgeDefinedFields.addAll( documentFieldMetadata.getBridgeDefinedFields().values() );
 		}
-
-		for ( PropertyMetadata property : type.getAllPropertyMetadata() ) {
-			bridgeDefinedFields.addAll( property.getBridgeDefinedFields().values() );
-		}
-
 		return bridgeDefinedFields;
 	}
 

elasticsearch/src/main/java/org/hibernate/search/elasticsearch/impl/FieldHelper.java
@@ -9,7 +9,6 @@ package org.hibernate.search.elasticsearch.impl;
 import java.util.Calendar;
 import java.util.Collection;
 import java.util.Date;
-import java.util.Set;
 import java.util.regex.Pattern;
 
 import org.hibernate.search.bridge.FieldBridge;
@@ -21,7 +20,6 @@ import org.hibernate.search.engine.metadata.impl.DocumentFieldMetadata;
 import org.hibernate.search.engine.metadata.impl.PropertyMetadata;
 import org.hibernate.search.engine.metadata.impl.SortableFieldMetadata;
 import org.hibernate.search.engine.metadata.impl.TypeMetadata;
-import org.hibernate.search.engine.spi.EntityIndexBinding;
 import org.hibernate.search.metadata.NumericFieldSettingsDescriptor.NumericEncodingType;
 
 /**
@@ -92,12 +90,9 @@ class FieldHelper {
 		NumericEncodingType numericEncodingType = field.getNumericEncodingType();
 
 		if ( numericEncodingType == NumericEncodingType.UNKNOWN ) {
-			PropertyMetadata hostingProperty = field.getSourceProperty();
-			if ( hostingProperty != null ) {
-				BridgeDefinedField bridgeDefinedField = hostingProperty.getBridgeDefinedFields().get( field.getName() );
-				if ( bridgeDefinedField != null ) {
-					numericEncodingType = getNumericEncodingType( bridgeDefinedField.getType() );
-				}
+			BridgeDefinedField bridgeDefinedField = field.getBridgeDefinedFields().get( field.getName() );
+			if ( bridgeDefinedField != null ) {
+				numericEncodingType = getNumericEncodingType( bridgeDefinedField.getType() );
 			}
 		}
 
@@ -233,23 +228,6 @@ class FieldHelper {
 	static String[] getFieldNameParts(String fieldName) {
 		boolean isEmbeddedField = isEmbeddedField( fieldName );
 		return isEmbeddedField ? DOT.split( fieldName ) : new String[]{ fieldName };
-	}
-
-	static DocumentFieldMetadata getFieldMetadata(EntityIndexBinding indexBinding, String fieldName) {
-		// This also addresses the ID case
-		DocumentFieldMetadata result = indexBinding.getDocumentBuilder().getMetadata().getDocumentFieldMetadataFor( fieldName );
-		if ( result != null ) {
-			return result;
-		}
-
-		Set<DocumentFieldMetadata> classBridgeMetadata = indexBinding.getDocumentBuilder().getMetadata().getClassBridgeMetadata();
-		for ( DocumentFieldMetadata documentFieldMetadata : classBridgeMetadata ) {
-			if ( documentFieldMetadata.getFieldName().equals( fieldName ) ) {
-				return documentFieldMetadata;
-			}
-		}
-
-		return null;
 	}
 
 	public static boolean isSortableField(TypeMetadata sourceType, PropertyMetadata sourceProperty, String fieldName) {

engine/src/main/java/org/hibernate/search/engine/metadata/impl/BridgeDefinedField.java
@@ -16,12 +16,12 @@ import org.hibernate.search.bridge.spi.FieldType;
  */
 public class BridgeDefinedField {
 
-	private final DocumentFieldMetadata sourceField;
+	private final BackReference<DocumentFieldMetadata> sourceField;
 
 	private final String name;
 	private final FieldType type;
 
-	public BridgeDefinedField(DocumentFieldMetadata sourceField, String name, FieldType type) {
+	public BridgeDefinedField(BackReference<DocumentFieldMetadata> sourceField, String name, FieldType type) {
 		this.sourceField = sourceField;
 		this.name = name;
 		this.type = type;
@@ -31,11 +31,11 @@ public class BridgeDefinedField {
 	 * @return The {@link DocumentFieldMetadata} whose field bridge declared this field.
 	 */
 	public DocumentFieldMetadata getSourceField() {
-		return sourceField;
+		return sourceField.get();
 	}
 
 	public Field.Index getIndex() {
-		return sourceField.getIndex();
+		return getSourceField().getIndex();
 	}
 
 	public String getName() {

engine/src/main/java/org/hibernate/search/engine/metadata/impl/DocumentFieldMetadata.java
@@ -7,7 +7,9 @@
 package org.hibernate.search.engine.metadata.impl;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import org.apache.lucene.document.Field;
@@ -46,6 +48,14 @@ public class DocumentFieldMetadata {
 	private final NumericEncodingType numericEncodingType;
 	private final Set<FacetMetadata> facetMetadata;
 
+	/**
+	 * Fields explicitly declared by {@link org.hibernate.search.bridge.MetadataProvidingFieldBridge}s.
+	 * <p>
+	 * Note: This is only to be used for validation / schema creation in ES, don't use it to drive invocation of field
+	 * bridges at indexing time!
+	 */
+	private final Map<String, BridgeDefinedField> bridgeDefinedFields;
+
 	private DocumentFieldMetadata(Builder builder) {
 		this.sourceType = builder.sourceType;
 		this.sourceProperty = builder.sourceProperty;
@@ -65,6 +75,7 @@ public class DocumentFieldMetadata {
 		this.precisionStep = builder.precisionStep;
 		this.numericEncodingType = builder.numericEncodingType;
 		this.facetMetadata = Collections.unmodifiableSet( builder.facetMetadata );
+		this.bridgeDefinedFields = Collections.unmodifiableMap( builder.bridgeDefinedFields );
 	}
 
 	/**
@@ -152,6 +163,10 @@ public class DocumentFieldMetadata {
 		return facetMetadata;
 	}
 
+	public Map<String, BridgeDefinedField> getBridgeDefinedFields() {
+		return bridgeDefinedFields;
+	}
+
 	@Override
 	public String toString() {
 		return "DocumentFieldMetadata{" +
@@ -176,6 +191,8 @@ public class DocumentFieldMetadata {
 	}
 
 	public static class Builder {
+		protected final BackReference<DocumentFieldMetadata> resultReference = new BackReference<>();
+
 		// required parameters
 		private final BackReference<TypeMetadata> sourceType;
 		private final BackReference<PropertyMetadata> sourceProperty;
@@ -196,6 +213,7 @@ public class DocumentFieldMetadata {
 		private NumericEncodingType numericEncodingType;
 		private Set<FacetMetadata> facetMetadata;
 		private NullMarkerCodec nullMarkerCodec = NotEncodingCodec.SINGLETON;
+		private final Map<String, BridgeDefinedField> bridgeDefinedFields;
 
 		public Builder(BackReference<TypeMetadata> sourceType,
 				BackReference<PropertyMetadata> sourceProperty,
@@ -210,6 +228,11 @@ public class DocumentFieldMetadata {
 			this.index = index;
 			this.termVector = termVector;
 			this.facetMetadata = new HashSet<>( 1 ); // the most common case is a single facet
+			this.bridgeDefinedFields = new HashMap<>();
+		}
+
+		public String getFieldName() {
+			return fieldName;
 		}
 
 		public Builder fieldBridge(FieldBridge fieldBridge) {
@@ -267,8 +290,19 @@ public class DocumentFieldMetadata {
 			return this;
 		}
 
+		public Builder addBridgeDefinedField(BridgeDefinedField bridgeDefinedField) {
+			this.bridgeDefinedFields.put( bridgeDefinedField.getName(), bridgeDefinedField );
+			return this;
+		}
+
+		public BackReference<DocumentFieldMetadata> getResultReference() {
+			return resultReference;
+		}
+
 		public DocumentFieldMetadata build() {
-			return new DocumentFieldMetadata( this );
+			DocumentFieldMetadata result = new DocumentFieldMetadata( this );
+			resultReference.initialize( result );
+			return result;
 		}
 
 		@Override

engine/src/main/java/org/hibernate/search/engine/metadata/impl/FieldMetadataBuilderImpl.java
@@ -22,9 +22,9 @@ class FieldMetadataBuilderImpl implements FieldMetadataBuilder {
 
 	private final Set<String> sortableFields = new HashSet<>();
 	private final Set<BridgeDefinedField> fields = new HashSet<>();
-	private final DocumentFieldMetadata fieldMetadata;
+	private final BackReference<DocumentFieldMetadata> fieldMetadata;
 
-	public FieldMetadataBuilderImpl(DocumentFieldMetadata fieldMetadata) {
+	public FieldMetadataBuilderImpl(BackReference<DocumentFieldMetadata> fieldMetadata) {
 		this.fieldMetadata = fieldMetadata;
 	}
 

engine/src/main/java/org/hibernate/search/engine/metadata/impl/ParseContext.java
@@ -29,6 +29,7 @@ public class ParseContext {
 	private int level = 0;
 	private int maxLevel = Integer.MAX_VALUE;
 	private boolean explicitDocumentId = false;
+	private String idFieldPath;
 	private boolean includeEmbeddedObjectId = false;
 
 	public IndexManagerType getIndexManagerType() {
@@ -117,6 +118,14 @@ public class ParseContext {
 
 	public void decrementLevel() {
 		this.level--;
+	}
+
+	public String getIdFieldPath() {
+		return idFieldPath;
+	}
+
+	public void setIdFieldPath(String idFieldPath) {
+		this.idFieldPath = idFieldPath;
 	}
 
 	public boolean isExplicitDocumentId() {

commit c09bf696ba8728d1c6edb54ad26b47b41133dc3f

HSEARCH-2457 Fixed a case sensitivity issue introduced in 3b2927a8cdbfb07945d25313c1b9dedc95482d6c

engine/src/main/java/org/hibernate/search/engine/metadata/impl/AnnotationMetadataProvider.java
@@ -963,7 +963,7 @@ public class AnnotationMetadataProvider implements MetadataProvider {
 
 		// Make sure a sort on the id field is only added to the idPropertyMetadata
 		if ( isIdProperty && !sortedFieldAbsoluteName.equals( idFieldPath )
-				|| !isIdProperty && sortedFieldAbsoluteName.equalsIgnoreCase( idFieldPath ) ) {
+				|| !isIdProperty && sortedFieldAbsoluteName.equals( idFieldPath ) ) {
 			return;
 		}
 