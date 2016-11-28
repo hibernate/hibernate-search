1f1ca7f333121b63090f8836cdaa62c551c9b7b8
Yoann Rodière <yoann@hibernate.org>
2016-11-28 11:53:27 +0100

    HSEARCH-2419 Split container bridges tests to test null-embedded naming conflicts separately
    This is necessary because such conflicts trigger a totally different
    behavior with Elasticsearch (following HSEARCH-2448), preventing the
    search factory to build.

--- orm/src/test/java/org/hibernate/search/test/bridge/MapBridgeNullEmbeddedTestEntity.java
+++ orm/src/test/java/org/hibernate/search/test/bridge/MapBridgeNullEmbeddedTestEntity.java
@@ -6,7 +6,6 @@
  */
 package org.hibernate.search.test.bridge;
 
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import javax.persistence.CollectionTable;
@@ -20,10 +19,9 @@ import javax.persistence.MapKeyColumn;
 import javax.persistence.Table;
 
 import org.hibernate.search.annotations.Analyze;
-import org.hibernate.search.annotations.DateBridge;
 import org.hibernate.search.annotations.Field;
 import org.hibernate.search.annotations.Indexed;
-import org.hibernate.search.annotations.Resolution;
+import org.hibernate.search.annotations.IndexedEmbedded;
 import org.hibernate.search.annotations.Store;
 
 /**
@@ -32,19 +30,17 @@ import org.hibernate.search.annotations.Store;
 @Entity
 @Indexed
 @Table(name = "IBT_Entity")
-public class MapBridgeTestEntity {
+public class MapBridgeNullEmbeddedTestEntity {
 
 	static final String NULL_TOKEN = "NULL_MARKER";
 	static final String NULL_NUMERIC_TOKEN = "-7777";
+	static final String NULL_EMBEDDED = "EMBEDDED_NULL";
+	static final String NULL_EMBEDDED_NUMERIC = "-4444";
 
 	private Long id;
 	private String name;
 	private Map<Integer, Language> nullIndexed = new HashMap<Integer, Language>();
-	private Map<Integer, String> nullNotIndexed = new HashMap<Integer, String>();
 	private Map<Integer, Integer> numericNullIndexed = new HashMap<Integer, Integer>();
-	private Map<Integer, Long> numericNullNotIndexed = new HashMap<Integer, Long>();
-
-	private Map<Integer, Date> dates = new HashMap<Integer, Date>();
 
 	public enum Language {
 		ITALIAN, ENGLISH, PIRATE, KLINGON
@@ -73,6 +69,11 @@ public class MapBridgeTestEntity {
 
 	@Field(indexNullAs = NULL_TOKEN, analyze = Analyze.NO)
 	@ElementCollection
+	/*
+	 * This will only have an effect for null maps, since the type for the map values
+	 * does not contain any @Field annotation (which means there is nothing to embed).
+	 */
+	@IndexedEmbedded(indexNullAs = NULL_EMBEDDED)
 	@CollectionTable(name = "NullIndexed", joinColumns = @JoinColumn(name = "iterable_id"))
 	@Column(name = "nullIndexed")
 	@MapKeyColumn(nullable = false)
@@ -90,6 +91,11 @@ public class MapBridgeTestEntity {
 
 	@Field(store = Store.YES, indexNullAs = NULL_NUMERIC_TOKEN, analyze = Analyze.NO)
 	@ElementCollection
+	/*
+	 * This will only have an effect for null maps, since the type for the map values
+	 * does not contain any @Field annotation (which means there is nothing to embed).
+	 */
+	@IndexedEmbedded(prefix = "embeddedNum.", indexNullAs = NULL_EMBEDDED_NUMERIC)
 	@CollectionTable(name = "NumericNullIndexed", joinColumns = @JoinColumn(name = "iterable_id"))
 	@Column(name = "numericNullIndexed")
 	@MapKeyColumn(nullable = false)
@@ -105,60 +111,8 @@ public class MapBridgeTestEntity {
 		this.numericNullIndexed.put( key, number );
 	}
 
-	@Field(store = Store.YES)
-	@ElementCollection
-	@CollectionTable(name = "NullNotIndexed", joinColumns = @JoinColumn(name = "iterable_id"))
-	@Column(name = "nullNotIndexed")
-	@MapKeyColumn(nullable = false)
-	public Map<Integer, String> getNullNotIndexed() {
-		return nullNotIndexed;
-	}
-
-	public void setNullNotIndexed(Map<Integer, String> nullNotIndexed) {
-		this.nullNotIndexed = nullNotIndexed;
-	}
-
-	public void addNullNotIndexed(Integer key, String value) {
-		this.nullNotIndexed.put( key, value );
-	}
-
-	@Field(store = Store.YES)
-	@ElementCollection
-	@CollectionTable(name = "NumericNullNotIndexed", joinColumns = @JoinColumn(name = "iterable_id"))
-	@Column(name = "numericNullNotIndexed")
-	@MapKeyColumn(nullable = false)
-	public Map<Integer, Long> getNumericNullNotIndexed() {
-		return numericNullNotIndexed;
-	}
-
-	public void setNumericNullNotIndexed(Map<Integer, Long> numericSkipNullCollection) {
-		this.numericNullNotIndexed = numericSkipNullCollection;
-	}
-
-	public void addNumericNullNotIndexed(Integer key, Long value) {
-		this.numericNullNotIndexed.put( key, value );
-	}
-
-	@Field(analyze = Analyze.NO, store = Store.YES)
-	@ElementCollection
-	@DateBridge(resolution = Resolution.SECOND)
-	@CollectionTable(name = "Dates", joinColumns = @JoinColumn(name = "iterable_id"))
-	@Column(name = "dates")
-	@MapKeyColumn(nullable = false)
-	public Map<Integer, Date> getDates() {
-		return dates;
-	}
-
-	public void setDates(Map<Integer, Date> dates) {
-		this.dates = dates;
-	}
-
-	public void addDate(Integer key, Date value) {
-		this.dates.put( key, value );
-	}
-
 	@Override
 	public String toString() {
-		return MapBridgeTestEntity.class.getSimpleName() + "[id=" + id + ", name=" + name + "]";
+		return MapBridgeNullEmbeddedTestEntity.class.getSimpleName() + "[id=" + id + ", name=" + name + "]";
 	}
 }
