f99a595379e516b42c780dcf00aab2c11391472b
Yoann Rodière <yoann@hibernate.org>
2017-01-04 11:11:11 +0100

    HSEARCH-2510 Copy CollectionHelper.iteratorFromArray from Hibernate Validator

--- engine/src/main/java/org/hibernate/search/util/impl/CollectionHelper.java
+++ engine/src/main/java/org/hibernate/search/util/impl/CollectionHelper.java
@@ -13,6 +13,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.SortedMap;
@@ -84,4 +85,242 @@ public final class CollectionHelper {
 		}
 	}
 
+	/**
+	 * Builds an {@link Iterator} for a given array. It is (un)necessarily ugly because we have to deal with array of primitives.
+	 *
+	 * @param object a given array
+	 * @return an {@code Iterator} iterating over the array
+	 */
+	public static Iterator<?> iteratorFromArray(Object object) {
+		Iterator<?> iterator;
+		if ( Object.class.isAssignableFrom( object.getClass().getComponentType() ) ) {
+			iterator = new ObjectArrayIterator( (Object[]) object );
+		}
+		else if ( object.getClass() == boolean[].class ) {
+			iterator = new BooleanArrayIterator( (boolean[]) object );
+		}
+		else if ( object.getClass() == int[].class ) {
+			iterator = new IntArrayIterator( (int[]) object );
+		}
+		else if ( object.getClass() == long[].class ) {
+			iterator = new LongArrayIterator( (long[]) object );
+		}
+		else if ( object.getClass() == double[].class ) {
+			iterator = new DoubleArrayIterator( (double[]) object );
+		}
+		else if ( object.getClass() == float[].class ) {
+			iterator = new FloatArrayIterator( (float[]) object );
+		}
+		else if ( object.getClass() == byte[].class ) {
+			iterator = new ByteArrayIterator( (byte[]) object );
+		}
+		else if ( object.getClass() == short[].class ) {
+			iterator = new ShortArrayIterator( (short[]) object );
+		}
+		else if ( object.getClass() == char[].class ) {
+			iterator = new CharArrayIterator( (char[]) object );
+		}
+		else {
+			throw new IllegalArgumentException( "Provided object " + object + " is not a supported array type" );
+		}
+		return iterator;
+	}
+
+	private static class ObjectArrayIterator implements Iterator<Object> {
+
+		private Object[] values;
+		private int current = 0;
+
+		private ObjectArrayIterator(Object[] values) {
+			this.values = values;
+		}
+
+		@Override
+		public boolean hasNext() {
+			return current < values.length;
+		}
+
+		@Override
+		public Object next() {
+			Object result = values[current];
+			current++;
+			return result;
+		}
+	}
+
+	private static class BooleanArrayIterator implements Iterator<Boolean> {
+
+		private boolean[] values;
+		private int current = 0;
+
+		private BooleanArrayIterator(boolean[] values) {
+			this.values = values;
+		}
+
+		@Override
+		public boolean hasNext() {
+			return current < values.length;
+		}
+
+		@Override
+		public Boolean next() {
+			boolean result = values[current];
+			current++;
+			return result;
+		}
+	}
+
+	private static class IntArrayIterator implements Iterator<Integer> {
+
+		private int[] values;
+		private int current = 0;
+
+		private IntArrayIterator(int[] values) {
+			this.values = values;
+		}
+
+		@Override
+		public boolean hasNext() {
+			return current < values.length;
+		}
+
+		@Override
+		public Integer next() {
+			int result = values[current];
+			current++;
+			return result;
+		}
+	}
+
+	private static class LongArrayIterator implements Iterator<Long> {
+
+		private long[] values;
+		private int current = 0;
+
+		private LongArrayIterator(long[] values) {
+			this.values = values;
+		}
+
+		@Override
+		public boolean hasNext() {
+			return current < values.length;
+		}
+
+		@Override
+		public Long next() {
+			long result = values[current];
+			current++;
+			return result;
+		}
+	}
+
+	private static class DoubleArrayIterator implements Iterator<Double> {
+
+		private double[] values;
+		private int current = 0;
+
+		private DoubleArrayIterator(double[] values) {
+			this.values = values;
+		}
+
+		@Override
+		public boolean hasNext() {
+			return current < values.length;
+		}
+
+		@Override
+		public Double next() {
+			double result = values[current];
+			current++;
+			return result;
+		}
+	}
+
+	private static class FloatArrayIterator implements Iterator<Float> {
+
+		private float[] values;
+		private int current = 0;
+
+		private FloatArrayIterator(float[] values) {
+			this.values = values;
+		}
+
+		@Override
+		public boolean hasNext() {
+			return current < values.length;
+		}
+
+		@Override
+		public Float next() {
+			float result = values[current];
+			current++;
+			return result;
+		}
+	}
+
+	private static class ByteArrayIterator implements Iterator<Byte> {
+
+		private byte[] values;
+		private int current = 0;
+
+		private ByteArrayIterator(byte[] values) {
+			this.values = values;
+		}
+
+		@Override
+		public boolean hasNext() {
+			return current < values.length;
+		}
+
+		@Override
+		public Byte next() {
+			byte result = values[current];
+			current++;
+			return result;
+		}
+	}
+
+	private static class ShortArrayIterator implements Iterator<Short> {
+
+		private short[] values;
+		private int current = 0;
+
+		private ShortArrayIterator(short[] values) {
+			this.values = values;
+		}
+
+		@Override
+		public boolean hasNext() {
+			return current < values.length;
+		}
+
+		@Override
+		public Short next() {
+			short result = values[current];
+			current++;
+			return result;
+		}
+	}
+
+	private static class CharArrayIterator implements Iterator<Character> {
+
+		private char[] values;
+		private int current = 0;
+
+		private CharArrayIterator(char[] values) {
+			this.values = values;
+		}
+
+		@Override
+		public boolean hasNext() {
+			return current < values.length;
+		}
+
+		@Override
+		public Character next() {
+			char result = values[current];
+			current++;
+			return result;
+		}
+	}
 }
