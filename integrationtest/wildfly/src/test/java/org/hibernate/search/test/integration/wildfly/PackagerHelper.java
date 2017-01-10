2a196173e41d630766f097d29beee0aa839402ef
Sanne Grinovero <sanne@hibernate.org>
2017-01-10 23:19:56 +0000

    HSEARCH-2550 Improve injection of current version to make debugging from IDE less troublesome

--- integrationtest/wildfly/src/test/java/org/hibernate/search/test/integration/wildfly/PackagerHelper.java
+++ integrationtest/wildfly/src/test/java/org/hibernate/search/test/integration/wildfly/PackagerHelper.java
@@ -7,6 +7,7 @@
 package org.hibernate.search.test.integration.wildfly;
 
 import org.hibernate.search.engine.Version;
+import org.hibernate.search.test.integration.VersionTestHelper;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 import org.jboss.shrinkwrap.resolver.api.maven.strategy.RejectDependenciesStrategy;
@@ -55,9 +56,8 @@ public class PackagerHelper {
 	 * @return the set of dependencies a user would need to bundle in a web app
 	 */
 	public static JavaArchive[] hibernateSearchLibraries() {
-		String currentVersion = Version.getVersionString();
 		return Maven.resolver()
-			.resolve( "org.hibernate:hibernate-search-orm:" + currentVersion )
+			.resolve( "org.hibernate:hibernate-search-orm:" + getCurrentVersion() )
 			// we need some dependencies at the right version: Lucene, search-engine, etc..
 			.using( new RejectDependenciesStrategy( false, exclusions ) )
 			.as( JavaArchive.class );
@@ -68,9 +68,22 @@ public class PackagerHelper {
 	public static JavaArchive[] hibernateSearchTestingLibraries() {
 		String currentVersion = Version.getVersionString();
 		return Maven.resolver()
-			.resolve( "org.hibernate:hibernate-search-testing:" + currentVersion )
+			.resolve( "org.hibernate:hibernate-search-testing:" + getCurrentVersion() )
 			.using( new RejectDependenciesStrategy( false, exclusions ) )
 			.as( JavaArchive.class );
+	}
+
+	/**
+	 * @return the current version of Hibernate Search
+	 */
+	private static String getCurrentVersion() {
+		//This will return 'null' when running the tests from Eclipse
+		//while having all modules open, so be prepared to fall back to build properties:
+		String currentVersion = Version.getVersionString();
+		if ( currentVersion != null ) {
+			return currentVersion;
+		}
+		return VersionTestHelper.getDependencyVersionHibernateSearch();
 	}
 
 }
