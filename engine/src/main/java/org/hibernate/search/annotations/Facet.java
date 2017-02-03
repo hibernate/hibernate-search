6fd6ee46e7bbb00942a4c1165dd1ade74dcb59f7
Sanne Grinovero <sanne@hibernate.org>
2017-02-03 15:18:10 +0000

    HSEARCH-2574 Convert @Facet to be a Repeatable Annotation

--- engine/src/main/java/org/hibernate/search/annotations/Facet.java
+++ engine/src/main/java/org/hibernate/search/annotations/Facet.java
@@ -8,6 +8,7 @@ package org.hibernate.search.annotations;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -21,6 +22,7 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.METHOD, ElementType.FIELD })
 @Documented
+@Repeatable(Facets.class)
 public @interface Facet {
 
 	/**
