70aa3d82f75c7b84e23ff58e0da843389f9eda33
Sanne Grinovero <sanne@hibernate.org>
2017-02-03 15:31:57 +0000

    HSEARCH-2574 Promote FullTextFilterDef to be a Repeatable Annotation

--- engine/src/main/java/org/hibernate/search/annotations/FullTextFilterDef.java
+++ engine/src/main/java/org/hibernate/search/annotations/FullTextFilterDef.java
@@ -8,6 +8,7 @@ package org.hibernate.search.annotations;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -22,6 +23,7 @@ import java.lang.annotation.Target;
 @Retention( RetentionPolicy.RUNTIME )
 @Target( { ElementType.PACKAGE, ElementType.TYPE } )
 @Documented
+@Repeatable(FullTextFilterDefs.class)
 public @interface FullTextFilterDef {
 
 	/**
