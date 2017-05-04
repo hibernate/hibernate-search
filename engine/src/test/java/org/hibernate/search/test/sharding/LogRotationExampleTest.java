942c98812d6d4a4b2076f6cf4a0fa7aadc868bb1
Yoann Rodière <yoann@hibernate.org>
2017-05-04 16:42:23 +0200

    HSEARCH-2477 Test shard filtering with Elasticsearch queries

--- engine/src/test/java/org/hibernate/search/test/sharding/LogRotationExampleTest.java
+++ engine/src/test/java/org/hibernate/search/test/sharding/LogRotationExampleTest.java
@@ -32,12 +32,10 @@ import org.hibernate.search.query.engine.spi.HSQuery;
 import org.hibernate.search.spi.BuildContext;
 import org.hibernate.search.store.ShardIdentifierProvider;
 import org.hibernate.search.testsupport.setup.TransactionContextForTest;
-import org.hibernate.search.testsupport.junit.ElasticsearchSupportInProgress;
 import org.hibernate.search.testsupport.junit.SearchFactoryHolder;
 import org.hibernate.search.testsupport.TestForIssue;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.experimental.categories.Category;
 
 /**
  * The example scenario: a system which indexes log messages of some periodic event
@@ -72,7 +70,6 @@ public class LogRotationExampleTest {
 		.withProperty( "hibernate.search.logs.sharding_strategy", LogMessageShardingStrategy.class.getName() );
 
 	@Test
-	@Category(ElasticsearchSupportInProgress.class) // HSEARCH-2477 Shard filtering doesn't work with Elasticsearch queries
 	public void filtersTest() {
 		ExtendedSearchIntegrator searchFactory = sfHolder.getSearchFactory();
 		Assert.assertNotNull( searchFactory.getIndexManagerHolder() );
