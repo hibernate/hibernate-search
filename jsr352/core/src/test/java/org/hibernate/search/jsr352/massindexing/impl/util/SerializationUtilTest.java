e281ce8d458ed9c66f2173f563fcb859dbb3e56e
Mincong HUANG <mincong.h@gmail.com>
2017-05-07 11:24:11 +0200

    HSEARCH-2632 Test validation and de/serialization

---
+++ jsr352/core/src/test/java/org/hibernate/search/jsr352/massindexing/impl/util/SerializationUtilTest.java
@@ -0,0 +1,130 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.jsr352.massindexing.impl.util;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.search.exception.SearchException;
+import org.hibernate.search.jsr352.massindexing.MassIndexingJobParameters;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+
+/**
+ * @author Mincong Huang
+ */
+public class SerializationUtilTest {
+
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+
+	@Test
+	public void serializeAndDeserialize() throws Exception {
+		Object i = SerializationUtil.deserialize( SerializationUtil.serialize( 1 ) );
+		assertThat( (Integer) i ).isEqualTo( 1 );
+	}
+
+	@Test
+	public void deserializeInt_fromInt() throws Exception {
+		int i = SerializationUtil.parseIntegerParameter( "My parameter", "1" );
+		assertThat( i ).isEqualTo( 1 );
+	}
+
+	@Test
+	public void deserializeInt_fromDouble() throws Exception {
+		thrown.expect( SearchException.class );
+		thrown.expectMessage( "HSEARCH500029: Unable to parse value '1.0' for job parameter 'My parameter'." );
+
+		SerializationUtil.parseIntegerParameter( "My parameter", "1.0" );
+	}
+
+	@Test
+	public void deserializeInt_fromOther() throws Exception {
+		thrown.expect( SearchException.class );
+		thrown.expectMessage( "HSEARCH500029: Unable to parse value 'foo' for job parameter 'My parameter'." );
+
+		SerializationUtil.parseIntegerParameter( "My parameter", "foo" );
+	}
+
+	@Test
+	public void deserializeBoolean_fromLowerCase() throws Exception {
+		boolean t = SerializationUtil.parseBooleanParameter( "My parameter 1", "true" );
+		boolean f = SerializationUtil.parseBooleanParameter( "My parameter 2", "false" );
+		assertThat( t ).isTrue();
+		assertThat( f ).isFalse();
+	}
+
+	@Test
+	public void deserializeBoolean_fromUpperCase() throws Exception {
+		boolean t = SerializationUtil.parseBooleanParameter( "My parameter 1", "TRUE" );
+		boolean f = SerializationUtil.parseBooleanParameter( "My parameter 2", "FALSE" );
+		assertThat( t ).isTrue();
+		assertThat( f ).isFalse();
+	}
+
+	@Test
+	public void deserializeBoolean_fromIrregularCase() throws Exception {
+		boolean t = SerializationUtil.parseBooleanParameter( "My parameter 1", "TruE" );
+		boolean f = SerializationUtil.parseBooleanParameter( "My parameter 2", "FalSe" );
+		assertThat( t ).as( "Case should be ignored." ).isTrue();
+		assertThat( f ).as( "Case should be ignored." ).isFalse();
+	}
+
+	@Test
+	public void deserializeBoolean_fromOthers() throws Exception {
+		boolean f0 = SerializationUtil.parseBooleanParameter( "My parameter 1", "0" );
+		boolean f1 = SerializationUtil.parseBooleanParameter( "My parameter 2", "1" );
+		boolean ft = SerializationUtil.parseBooleanParameter( "My parameter 3", "t" );
+		boolean ff = SerializationUtil.parseBooleanParameter( "My parameter 4", "f" );
+		assertThat( f0 ).isFalse();
+		assertThat( f1 ).isFalse();
+		assertThat( ft ).isFalse();
+		assertThat( ff ).isFalse();
+	}
+
+	@Test
+	@SuppressWarnings("unchecked")
+	public void deserializeCriteria() throws Exception {
+		Criterion criterion1 = Restrictions.in( "keyA", "value1", "value2" );
+		Criterion criterion2 = Restrictions.between( "keyB", "low", "high" );
+
+		// Given a serialized criteria
+		Set<Criterion> inputCriteria = new HashSet<>();
+		inputCriteria.add( criterion1 );
+		inputCriteria.add( criterion2 );
+		String serializedCriteria = SerializationUtil.serialize( inputCriteria );
+
+		// When it is deserialized
+		Set<Criterion> actualCriteria = SerializationUtil
+				.parseParameter( Set.class, MassIndexingJobParameters.CUSTOM_QUERY_CRITERIA, serializedCriteria );
+
+		// Then there're 2 criteria found.
+
+		/*
+		 * Hibernate ORM Issue:
+		 *
+		 * The implementations of interface 'org.hibernate.criterion.Criterion' haven't overridden the
+		 * methods #equals and #hashCode, so the equality-by-value is actually equality-by-reference.
+		 * Enable the following lines to see what happens.
+		 */
+//		assertThat( actualCriteria ).containsOnly( criterion1, criterion2 );
+
+		Set<String> actualCriteriaAsStrings = actualCriteria.stream()
+				.map( Object::toString )
+				.collect( Collectors.toSet() );
+		assertThat( actualCriteriaAsStrings ).containsOnly( criterion1.toString(), criterion2.toString() );
+	}
+
+}
--- jsr352/core/src/test/java/org/hibernate/search/jsr352/massindexing/impl/util/ValidationUtilTest.java
+++ jsr352/core/src/test/java/org/hibernate/search/jsr352/massindexing/impl/util/ValidationUtilTest.java
@@ -6,14 +6,70 @@
  */
 package org.hibernate.search.jsr352.massindexing.impl.util;
 
-import org.hibernate.search.exception.SearchException;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
+import org.hibernate.search.exception.SearchException;
+import org.hibernate.search.jsr352.massindexing.test.entity.Company;
+import org.hibernate.search.jsr352.massindexing.test.entity.Person;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 /**
  * @author Mincong Huang
  */
 public class ValidationUtilTest {
+
+	private static final String PERSISTENCE_UNIT_NAME = "h2";
+
+	private static final String EMF_SCOPE = "persistence-unit-name";
+
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+
+	private EntityManagerFactory emf;
+
+	@Before
+	public void setUp() throws Exception {
+		emf = Persistence.createEntityManagerFactory( PERSISTENCE_UNIT_NAME );
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		if ( emf != null ) {
+			emf.close();
+			emf = null;
+		}
+	}
+
+	@Test
+	public void validateEntityTypes_whenAllTypesAreAvailableInEMF() throws Exception {
+		String serializedEntityTypes = Stream
+				.of( Company.class, Person.class )
+				.map( Class::getName )
+				.collect( Collectors.joining( "," ) );
+
+		ValidationUtil.validateEntityTypes( null, EMF_SCOPE, PERSISTENCE_UNIT_NAME, serializedEntityTypes );
+	}
+
+	@Test
+	public void validateEntityTypes_whenContainingNonIndexedTypes() throws Exception {
+		String serializedEntityTypes = Stream
+				.of( Company.class, Person.class, NotIndexed.class )
+				.map( Class::getName )
+				.collect( Collectors.joining( "," ) );
+
+		thrown.expect( SearchException.class );
+		thrown.expectMessage( "HSEARCH500032: The following selected entity types aren't indexable: " + NotIndexed.class
+				.getName() + ". Please check if the annotation '@Indexed' has been added to each of them." );
+		ValidationUtil.validateEntityTypes( null, EMF_SCOPE, PERSISTENCE_UNIT_NAME, serializedEntityTypes );
+	}
 
 	@Test(expected = SearchException.class)
 	public void validatePositive_valueIsNegative() throws Exception {
@@ -57,6 +113,12 @@ public class ValidationUtilTest {
 	@Test(expected = SearchException.class)
 	public void validateCheckpointInterval_greaterThanRowsPerPartitions() throws Exception {
 		ValidationUtil.validateCheckpointInterval( 101, 100 );
+	}
+
+	private static class NotIndexed {
+		private NotIndexed() {
+			// Private constructor, do not use it.
+		}
 	}
 
 }
