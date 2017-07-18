d5aa35fc12976e9e5f9e2016929b134463ea9780
Yoann Rodière <yoann@hibernate.org>
2017-07-18 16:39:36 +0200

    HSEARCH-2764 Add a parallel orchestrator for streamed work
    This could lead to better performance with large Elasticsearch
    connection pools when works affect multiple indexes.

---
+++ elasticsearch/src/main/java/org/hibernate/search/elasticsearch/processor/impl/BarrierElasticsearchWorkOrchestrator.java
@@ -0,0 +1,26 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.elasticsearch.processor.impl;
+
+/**
+ * An {@link ElasticsearchWorkOrchestrator} providing a synchronization barrier
+ * through its {@link #awaitCompletion()} method.
+ *
+ * @author Yoann Rodiere
+ */
+public interface BarrierElasticsearchWorkOrchestrator extends ElasticsearchWorkOrchestrator {
+
+	/**
+	 * Block until there is no more work to execute.
+	 * <p>
+	 * N.B. if more works are submitted in the meantime, this might delay the wait.
+	 *
+	 * @throws InterruptedException if thread interrupted while waiting
+	 */
+	void awaitCompletion() throws InterruptedException;
+
+}
