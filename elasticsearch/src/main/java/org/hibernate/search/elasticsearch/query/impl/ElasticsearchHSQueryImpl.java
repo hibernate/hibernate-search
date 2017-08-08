81d5344bb3fea03a905e991d9b11f695d859bc96
Yoann Rodière <yoann@hibernate.org>
2017-08-08 11:10:28 +0200

    HSEARCH-2841 Move Elasticsearch query hit conversion code out of ElasticsearchHSQueryImpl

--- elasticsearch/src/main/java/org/hibernate/search/elasticsearch/query/impl/ElasticsearchHSQueryImpl.java
+++ elasticsearch/src/main/java/org/hibernate/search/elasticsearch/query/impl/ElasticsearchHSQueryImpl.java
@@ -7,7 +7,6 @@
 package org.hibernate.search.elasticsearch.query.impl;
 
 import java.io.IOException;
-import java.io.Serializable;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -24,10 +23,6 @@ import org.apache.lucene.search.Explanation;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.SortField;
 import org.apache.lucene.search.TopDocs;
-import org.hibernate.search.bridge.FieldBridge;
-import org.hibernate.search.bridge.TwoWayFieldBridge;
-import org.hibernate.search.bridge.spi.ConversionContext;
-import org.hibernate.search.bridge.util.impl.ContextualExceptionBridgeHelper;
 import org.hibernate.search.elasticsearch.ElasticsearchProjectionConstants;
 import org.hibernate.search.elasticsearch.client.impl.URLEncodedString;
 import org.hibernate.search.elasticsearch.filter.ElasticsearchFilter;
@@ -37,8 +32,6 @@ import org.hibernate.search.elasticsearch.impl.ElasticsearchService;
 import org.hibernate.search.elasticsearch.impl.JsonBuilder;
 import org.hibernate.search.elasticsearch.impl.ToElasticsearch;
 import org.hibernate.search.elasticsearch.logging.impl.Log;
-import org.hibernate.search.elasticsearch.util.impl.FieldHelper;
-import org.hibernate.search.elasticsearch.util.impl.FieldHelper.ExtendedFieldType;
 import org.hibernate.search.elasticsearch.util.impl.Window;
 import org.hibernate.search.elasticsearch.work.impl.ElasticsearchWork;
 import org.hibernate.search.elasticsearch.work.impl.ExplainResult;
@@ -46,10 +39,7 @@ import org.hibernate.search.elasticsearch.work.impl.SearchResult;
 import org.hibernate.search.elasticsearch.work.impl.builder.SearchWorkBuilder;
 import org.hibernate.search.engine.impl.FilterDef;
 import org.hibernate.search.engine.integration.impl.ExtendedSearchIntegrator;
-import org.hibernate.search.engine.metadata.impl.BridgeDefinedField;
-import org.hibernate.search.engine.metadata.impl.DocumentFieldMetadata;
 import org.hibernate.search.engine.metadata.impl.FacetMetadata;
-import org.hibernate.search.engine.metadata.impl.TypeMetadata;
 import org.hibernate.search.engine.spi.DocumentBuilderIndexedEntity;
 import org.hibernate.search.engine.spi.EntityIndexBinding;
 import org.hibernate.search.exception.AssertionFailure;
@@ -59,7 +49,6 @@ import org.hibernate.search.query.dsl.impl.DiscreteFacetRequest;
 import org.hibernate.search.query.dsl.impl.FacetRange;
 import org.hibernate.search.query.dsl.impl.RangeFacetRequest;
 import org.hibernate.search.query.engine.impl.AbstractHSQuery;
-import org.hibernate.search.query.engine.impl.EntityInfoImpl;
 import org.hibernate.search.query.engine.impl.FacetComparators;
 import org.hibernate.search.query.engine.impl.FacetManagerImpl;
 import org.hibernate.search.query.engine.impl.TimeoutManagerImpl;
@@ -71,7 +60,6 @@ import org.hibernate.search.query.facet.FacetSortOrder;
 import org.hibernate.search.query.facet.FacetingRequest;
 import org.hibernate.search.spatial.DistanceSortField;
 import org.hibernate.search.spi.CustomTypeMetadata;
-import org.hibernate.search.spi.IndexedTypeIdentifier;
 import org.hibernate.search.spi.IndexedTypeMap;
 import org.hibernate.search.spi.IndexedTypeSet;
 import org.hibernate.search.util.impl.CollectionHelper;
@@ -82,7 +70,6 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.google.gson.JsonPrimitive;
 
 /**
  * Query implementation based on Elasticsearch.
@@ -94,8 +81,6 @@ public class ElasticsearchHSQueryImpl extends AbstractHSQuery {
 	private static final JsonParser JSON_PARSER = new JsonParser();
 
 	private static final Log LOG = LoggerFactory.make( Log.class );
-
-	private static final String SPATIAL_DISTANCE_FIELD = "_distance";
 
 	/**
 	 * ES default limit for (firstResult + maxResult)
@@ -128,8 +113,6 @@ public class ElasticsearchHSQueryImpl extends AbstractHSQuery {
 	private Integer resultSize;
 	private IndexSearcher searcher;
 	private SearchResult searchResult;
-
-	private int sortByDistanceIndex = -1;
 
 	private transient FacetManagerImpl facetManager;
 
@@ -355,19 +338,16 @@ public class ElasticsearchHSQueryImpl extends AbstractHSQuery {
 	 */
 	private class IndexSearcher {
 		private final ElasticsearchService elasticsearchService;
-		private final Map<String, EntityIndexBinding> targetedEntityBindingsByName;
 		private final Set<URLEncodedString> indexNames;
 
 		private final ElasticsearchQueryOptions queryOptions;
-		private final Map<EntityIndexBinding, FieldProjection> idProjectionByEntityBinding = new HashMap<>();
-		private final Map<EntityIndexBinding, FieldProjection[]> fieldProjectionsByEntityBinding = new HashMap<>();
+		private final QueryHitConverter queryHitConverter;
 		private final Map<FacetingRequest, FacetMetadata> facetingRequestsAndMetadata;
 		private final JsonObject payload;
 
 		private IndexSearcher(ElasticsearchService elasticsearchService,
 				Map<String, EntityIndexBinding> targetedEntityBindingsByName, Set<URLEncodedString> indexNames) {
 			this.elasticsearchService = elasticsearchService;
-			this.targetedEntityBindingsByName = targetedEntityBindingsByName;
 			this.indexNames = indexNames;
 
 			JsonArray typeFilters = new JsonArray();
@@ -383,7 +363,17 @@ public class ElasticsearchHSQueryImpl extends AbstractHSQuery {
 			JsonBuilder.Object payloadBuilder = JsonBuilder.object();
 			payloadBuilder.add( "query", filteredQuery );
 
-			addProjections( payloadBuilder );
+			/*
+			 * Initialize the sortByDistanceIndex to detect if the results are sorted
+			 * by distance and the position
+			 */
+			Integer sortByDistanceIndex = getSortByDistanceIndex();
+
+			this.queryHitConverter = QueryHitConverter.builder( elasticsearchService.getQueryFactory(), targetedEntityBindingsByName )
+					.setProjectedFields( projectedFields )
+					.setSortByDistance( sortByDistanceIndex, spatialSearchCenter, spatialFieldName )
+					.build();
+			queryHitConverter.contributeToPayload( payloadBuilder );
 
 			Collection<FacetingRequest> facetingRequests = getFacetManager().getFacetRequests().values();
 			if ( !facetingRequests.isEmpty() ) {
@@ -400,11 +390,6 @@ public class ElasticsearchHSQueryImpl extends AbstractHSQuery {
 			else {
 				facetingRequestsAndMetadata = Collections.emptyMap();
 			}
-
-			// Initialize the sortByDistanceIndex to detect if the results are sorted by distance and the position
-			// of the sort
-			sortByDistanceIndex = getSortByDistanceIndex();
-			addScriptFields( payloadBuilder );
 
 			// TODO: HSEARCH-2254 embedded fields (see https://github.com/searchbox-io/Jest/issues/304)
 			if ( sort != null ) {
@@ -485,146 +470,6 @@ public class ElasticsearchHSQueryImpl extends AbstractHSQuery {
 			return tenantFilter;
 		}
 
-		private void addProjections(JsonBuilder.Object payloadBuilder) {
-			boolean includeAllSource = false;
-			JsonBuilder.Array builder = JsonBuilder.array();
-
-			/*
-			 * IDs are always projected: always initialize their projections regardless of the
-			 * "projectedFields" attribute.
-			 */
-			for ( EntityIndexBinding binding : targetedEntityBindingsByName.values() ) {
-				DocumentBuilderIndexedEntity documentBuilder = binding.getDocumentBuilder();
-				String idFieldName = documentBuilder.getIdFieldName();
-				TypeMetadata typeMetadata = documentBuilder.getTypeMetadata();
-				FieldProjection projection = createProjection( builder, typeMetadata, idFieldName );
-				idProjectionByEntityBinding.put( binding, projection );
-			}
-
-			if ( projectedFields != null ) {
-				for ( int i = 0 ; i < projectedFields.length ; ++i ) {
-					String projectedField = projectedFields[i];
-					if ( projectedField == null ) {
-						continue;
-					}
-					switch ( projectedField ) {
-						case ElasticsearchProjectionConstants.SOURCE:
-							includeAllSource = true;
-							break;
-						case ElasticsearchProjectionConstants.SCORE:
-							// Make sure to compute scores even if we don't sort by relevance
-							payloadBuilder.addProperty( "track_scores", true );
-							break;
-						case ElasticsearchProjectionConstants.ID:
-						case ElasticsearchProjectionConstants.THIS:
-						case ElasticsearchProjectionConstants.OBJECT_CLASS:
-						case ElasticsearchProjectionConstants.SPATIAL_DISTANCE:
-						case ElasticsearchProjectionConstants.TOOK:
-						case ElasticsearchProjectionConstants.TIMED_OUT:
-							// Ignore: no impact on source filtering
-							break;
-						default:
-							for ( EntityIndexBinding binding : targetedEntityBindingsByName.values() ) {
-								TypeMetadata typeMetadata = binding.getDocumentBuilder().getTypeMetadata();
-								FieldProjection projection = createProjection( builder, typeMetadata, projectedField );
-								FieldProjection[] projectionsForType = fieldProjectionsByEntityBinding.get( binding );
-								if ( projectionsForType == null ) {
-									projectionsForType = new FieldProjection[projectedFields.length];
-									fieldProjectionsByEntityBinding.put( binding, projectionsForType );
-								}
-								projectionsForType[i] = projection;
-							}
-							break;
-					}
-				}
-			}
-
-			JsonElement filter;
-			if ( includeAllSource ) {
-				filter = new JsonPrimitive( "*" );
-			}
-			else {
-				JsonArray array = builder.build();
-				if ( array.size() > 0 ) {
-					filter = array;
-				}
-				else {
-					// Projecting only on score or other document-independent values
-					filter = new JsonPrimitive( false );
-				}
-			}
-
-			payloadBuilder.add( "_source", filter );
-		}
-
-		private FieldProjection createProjection(JsonBuilder.Array sourceFilterCollector, TypeMetadata rootTypeMetadata, String projectedField) {
-			DocumentFieldMetadata fieldMetadata = rootTypeMetadata.getDocumentFieldMetadataFor( projectedField );
-			if ( fieldMetadata != null ) {
-				return createProjection( sourceFilterCollector, rootTypeMetadata, fieldMetadata );
-			}
-			else {
-				// We check if it is a field created by a field bridge
-				BridgeDefinedField bridgeDefinedField = rootTypeMetadata.getBridgeDefinedFieldMetadataFor( projectedField );
-				if ( bridgeDefinedField != null ) {
-					return createProjection( sourceFilterCollector, rootTypeMetadata, bridgeDefinedField );
-				}
-				else {
-					/*
-					 * No metadata: fall back to dynamically converting the resulting
-					 * JSON to the most appropriate Java type.
-					 */
-					sourceFilterCollector.add( new JsonPrimitive( projectedField ) );
-					return new JsonDrivenProjection( projectedField );
-				}
-			}
-		}
-
-		private FieldProjection createProjection(JsonBuilder.Array sourceFilterCollector, TypeMetadata rootTypeMetadata,
-				DocumentFieldMetadata fieldMetadata) {
-			String absoluteName = fieldMetadata.getAbsoluteName();
-			FieldBridge fieldBridge = fieldMetadata.getFieldBridge();
-			ExtendedFieldType type = FieldHelper.getType( fieldMetadata );
-
-			if ( ExtendedFieldType.BOOLEAN.equals( type ) ) {
-				sourceFilterCollector.add( new JsonPrimitive( absoluteName ) );
-
-				return new PrimitiveProjection( rootTypeMetadata, absoluteName, type );
-			}
-			else if ( fieldBridge instanceof TwoWayFieldBridge ) {
-				sourceFilterCollector.add( new JsonPrimitive( absoluteName ) );
-
-				PrimitiveProjection defaultFieldProjection = new PrimitiveProjection( rootTypeMetadata, absoluteName, type );
-
-				Collection<BridgeDefinedField> bridgeDefinedFields = fieldMetadata.getBridgeDefinedFields().values();
-				List<PrimitiveProjection> bridgeDefinedFieldsProjections = CollectionHelper.newArrayList( bridgeDefinedFields.size() );
-				for ( BridgeDefinedField bridgeDefinedField : bridgeDefinedFields ) {
-					PrimitiveProjection primitiveProjection = createProjection( sourceFilterCollector, rootTypeMetadata, bridgeDefinedField );
-					bridgeDefinedFieldsProjections.add( primitiveProjection );
-				}
-				return new TwoWayFieldBridgeProjection(
-						absoluteName, (TwoWayFieldBridge) fieldBridge, defaultFieldProjection, bridgeDefinedFieldsProjections
-						);
-			}
-			else {
-				/*
-				 * Don't fail immediately: this entity type may not be present in the results, in which case
-				 * we don't need to be able to project on this field for this exact entity type.
-				 * Just make sure we *will* ultimately fail if we encounter this entity type.
-				 */
-				return new FailingOneWayFieldBridgeProjection( absoluteName, fieldBridge.getClass() );
-			}
-		}
-
-		private PrimitiveProjection createProjection(JsonBuilder.Array sourceFilterCollector, TypeMetadata rootTypeMetadata,
-				BridgeDefinedField bridgeDefinedField) {
-			String absoluteName = bridgeDefinedField.getAbsoluteName();
-			ExtendedFieldType type = FieldHelper.getType( bridgeDefinedField );
-
-			sourceFilterCollector.add( new JsonPrimitive( absoluteName ) );
-
-			return new PrimitiveProjection( rootTypeMetadata, absoluteName, type );
-		}
-
 		private void addFacetingRequest(JsonBuilder.Object facets, FacetingRequest facetingRequest, FacetMetadata facetMetadata) {
 			String sourceFieldAbsoluteName = facetMetadata.getSourceField().getAbsoluteName();
 			String facetSubfieldName = facetMetadata.getPath().getRelativeName();
@@ -635,9 +480,9 @@ public class ElasticsearchHSQueryImpl extends AbstractHSQuery {
 		/**
 		 * Returns the index of the DistanceSortField in the Sort array.
 		 *
-		 * @return the index, -1 if no DistanceSortField has been found
+		 * @return the index, null if no DistanceSortField has been found
 		 */
-		private int getSortByDistanceIndex() {
+		private Integer getSortByDistanceIndex() {
 			int i = 0;
 			if ( sort != null ) {
 				for ( SortField sortField : sort.getSort() ) {
@@ -647,30 +492,7 @@ public class ElasticsearchHSQueryImpl extends AbstractHSQuery {
 					i++;
 				}
 			}
-			return -1;
-		}
-
-		/**
-		 * Indicates if the results are sorted by distance (note that it might be a secondary order).
-		 */
-		private boolean isSortedByDistance() {
-			return sortByDistanceIndex >= 0;
-		}
-
-		private void addScriptFields(JsonBuilder.Object payloadBuilder) {
-			if ( isPartOfProjectedFields( ElasticsearchProjectionConstants.SPATIAL_DISTANCE ) && !isSortedByDistance() ) {
-				// when the results are sorted by distance, Elasticsearch returns the distance in a "sort" field in
-				// the results. If we don't sort by distance, we need to request for the distance using a script_field.
-				payloadBuilder.add( "script_fields",
-						JsonBuilder.object().add( SPATIAL_DISTANCE_FIELD, JsonBuilder.object()
-							.add(
-									"script",
-									elasticsearchService.getQueryFactory()
-											.createSpatialDistanceScript( spatialSearchCenter, spatialFieldName )
-							)
-						)
-				);
-			}
+			return null;
 		}
 
 		SearchResult search() {
@@ -737,101 +559,7 @@ public class ElasticsearchHSQueryImpl extends AbstractHSQuery {
 		}
 
 		EntityInfo convertQueryHit(SearchResult searchResult, JsonObject hit) {
-			String type = hit.get( "_type" ).getAsString();
-			EntityIndexBinding binding = targetedEntityBindingsByName.get( type );
-
-			if ( binding == null ) {
-				LOG.warnf( "Found unknown type in Elasticsearch index: " + type );
-				return null;
-			}
-
-			DocumentBuilderIndexedEntity documentBuilder = binding.getDocumentBuilder();
-			IndexedTypeIdentifier typeId = documentBuilder.getTypeIdentifier();
-
-			ConversionContext conversionContext = new ContextualExceptionBridgeHelper();
-			conversionContext.setConvertedTypeId( typeId );
-			FieldProjection idProjection = idProjectionByEntityBinding.get( binding );
-			Object id = idProjection.convertHit( hit, conversionContext );
-			Object[] projections = null;
-
-			if ( projectedFields != null ) {
-				projections = new Object[projectedFields.length];
-
-				for ( int i = 0; i < projections.length; i++ ) {
-					String field = projectedFields[i];
-					if ( field == null ) {
-						continue;
-					}
-					switch ( field ) {
-						case ElasticsearchProjectionConstants.SOURCE:
-							projections[i] = hit.getAsJsonObject().get( "_source" ).toString();
-							break;
-						case ElasticsearchProjectionConstants.ID:
-							projections[i] = id;
-							break;
-						case ElasticsearchProjectionConstants.OBJECT_CLASS:
-							projections[i] = typeId.getPojoType();
-							break;
-						case ElasticsearchProjectionConstants.SCORE:
-							projections[i] = hit.getAsJsonObject().get( "_score" ).getAsFloat();
-							break;
-						case ElasticsearchProjectionConstants.SPATIAL_DISTANCE:
-							JsonElement distance = null;
-							// if we sort by distance, we need to find the index of the DistanceSortField and use it
-							// to extract the values from the sort array
-							// if we don't sort by distance, we use the field generated by the script_field added earlier
-							if ( isSortedByDistance() ) {
-								distance = hit.getAsJsonObject().get( "sort" ).getAsJsonArray().get( sortByDistanceIndex );
-							}
-							else {
-								JsonElement fields = hit.getAsJsonObject().get( "fields" );
-								if ( fields != null ) { // "fields" seems to be missing if there are only null results in script fields
-									distance = hit.getAsJsonObject().get( "fields" ).getAsJsonObject().get( SPATIAL_DISTANCE_FIELD );
-								}
-							}
-							if ( distance != null && distance.isJsonArray() ) {
-								JsonArray array = distance.getAsJsonArray();
-								distance = array.size() >= 1 ? array.get( 0 ) : null;
-							}
-							if ( distance == null || distance.isJsonNull() ) {
-								projections[i] = null;
-							}
-							else {
-								Double distanceAsDouble = distance.getAsDouble();
-
-								if ( distanceAsDouble == Double.MAX_VALUE || distanceAsDouble.isInfinite() ) {
-									/*
-									 * When we extract the distance from the sort, its default value is:
-									 *  - Double.MAX_VALUE on older ES versions (5.0 and lower)
-									 *  - Double.POSITIVE_INFINITY on newer ES versions (from somewhere around 5.2 onwards)
-									 */
-									projections[i] = null;
-								}
-								else {
-									projections[i] = distance.getAsDouble();
-								}
-							}
-							break;
-						case ElasticsearchProjectionConstants.TOOK:
-							projections[i] = searchResult.getTook();
-							break;
-						case ElasticsearchProjectionConstants.TIMED_OUT:
-							projections[i] = searchResult.getTimedOut();
-							break;
-						case ElasticsearchProjectionConstants.THIS:
-							// Use EntityInfo.ENTITY_PLACEHOLDER as placeholder.
-							// It will be replaced when we populate
-							// the EntityInfo with the real entity.
-							projections[i] = EntityInfo.ENTITY_PLACEHOLDER;
-							break;
-						default:
-							FieldProjection projection = fieldProjectionsByEntityBinding.get( binding )[i];
-							projections[i] = projection.convertHit( hit, conversionContext );
-					}
-				}
-			}
-
-			return new EntityInfoImpl( typeId, documentBuilder.getIdPropertyName(), (Serializable) id, projections );
+			return this.queryHitConverter.convert( searchResult, hit );
 		}
 
 	}
@@ -970,18 +698,6 @@ public class ElasticsearchHSQueryImpl extends AbstractHSQuery {
 	private boolean isNested(DiscreteFacetRequest facetRequest) {
 		//TODO HSEARCH-2097 Drive through meta-data
 //		return FieldHelper.isEmbeddedField( facetRequest.getFieldName() );
-		return false;
-	}
-
-	private boolean isPartOfProjectedFields(String projectionName) {
-		if ( projectedFields == null ) {
-			return false;
-		}
-		for ( String projectedField : projectedFields ) {
-			if ( projectionName.equals( projectedField ) ) {
-				return true;
-			}
-		}
 		return false;
 	}
 
