c5cb8a83a897537902a0607d83fff2f758be53ea
Sanne Grinovero <sanne@hibernate.org>
2017-09-13 12:35:57 +0100

    HSEARCH-2867 Add missing entries to the module-info of the module integration tests

--- integrationtest/jdk9-modules/src/main/java/module-info.java
+++ integrationtest/jdk9-modules/src/main/java/module-info.java
@@ -2,4 +2,8 @@ module hibernate.search.integrationtest.jdk9.modules.client {
 	exports org.hibernate.search.test.integration.jdk9_modules.client.service;
 	requires hibernate.search.engine;
 	requires hibernate.search.orm;
+	requires hibernate.jpa;
+	requires hibernate.core;
+	requires lucene.analyzers.common;
+	requires lucene.core;
 }
