f496413effc427deb1f904de9c0057e5d733adf9
Sanne Grinovero <sanne@hibernate.org>
2018-04-26 16:08:13 +0100

    HSEARCH-3143 Update to Hibernate ORM 5.3.0.CR2

--- orm/src/test/java/org/hibernate/search/test/configuration/integration/DuplicationStrategyTest.java
+++ orm/src/test/java/org/hibernate/search/test/configuration/integration/DuplicationStrategyTest.java
@@ -6,13 +6,28 @@
  */
 package org.hibernate.search.test.configuration.integration;
 
-import org.hibernate.event.service.internal.EventListenerRegistryImpl;
+import java.util.Collections;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.internal.BootstrapContextImpl;
+import org.hibernate.boot.internal.MetadataBuilderImpl;
+import org.hibernate.boot.internal.SessionFactoryOptionsBuilder;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.boot.spi.MetadataBuildingOptions;
+import org.hibernate.engine.spi.SessionFactoryImplementor;
+import org.hibernate.event.service.internal.EventListenerServiceInitiator;
 import org.hibernate.event.service.spi.EventListenerGroup;
 import org.hibernate.event.service.spi.EventListenerRegistry;
 import org.hibernate.event.spi.EventType;
 import org.hibernate.event.spi.PostInsertEventListener;
 import org.hibernate.search.event.impl.FullTextIndexEventListener;
 import org.hibernate.search.hcore.impl.HibernateSearchIntegrator;
+import org.hibernate.service.internal.SessionFactoryServiceRegistryImpl;
+import org.hibernate.service.spi.ServiceRegistryImplementor;
+
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
@@ -24,12 +39,28 @@ public class DuplicationStrategyTest {
 
 	@Test
 	public void testMultipleRegistrationOfEventListenerKeepsOriginalListener() {
-		EventListenerRegistry eventListenerRegistry = new EventListenerRegistryImpl();
+
+		final StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().build();
+		final Metadata metadata = new MetadataSources( serviceRegistry ).buildMetadata();
+		final SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
+		final SessionFactoryImplementor sessionFactoryImplementor = (SessionFactoryImplementor) sessionFactory;
+		final MetadataBuildingOptions buildingOptions = new MetadataBuilderImpl.MetadataBuildingOptionsImpl( serviceRegistry );
+		final BootstrapContextImpl ctx = new BootstrapContextImpl( serviceRegistry, buildingOptions );
+		final SessionFactoryOptionsBuilder sfOptions = new SessionFactoryOptionsBuilder( serviceRegistry, ctx );
+		SessionFactoryServiceRegistryImpl sessionFactoryServiceRegistry = new SessionFactoryServiceRegistryImpl(
+				(ServiceRegistryImplementor)serviceRegistry, Collections.emptyList(), Collections.emptyList(),
+				sessionFactoryImplementor, ctx, sfOptions
+		);
+
+		EventListenerRegistry eventListenerRegistry = EventListenerServiceInitiator.INSTANCE.initiateService( sessionFactoryServiceRegistry );
 
 		EventListenerGroup<PostInsertEventListener> eventEventListenerGroup = eventListenerRegistry.getEventListenerGroup(
 				EventType.POST_INSERT
 		);
-		assertTrue( "We should start of with no listeners", eventEventListenerGroup.count() == 0 );
+		//Remove all default listeners:
+		eventListenerRegistry.setListeners( EventType.POST_INSERT, new PostInsertEventListener[]{} );
+
+		assertTrue( "We should start off with no listeners", eventEventListenerGroup.count() == 0 );
 
 		FullTextIndexEventListener firstFullTextIndexEventListener = new FullTextIndexEventListener();
 
@@ -42,7 +73,7 @@ public class DuplicationStrategyTest {
 		eventListenerRegistry.appendListeners( EventType.POST_INSERT, new FullTextIndexEventListener() );
 
 		assertTrue(
-				"We should only be one listener, but we have " + eventEventListenerGroup.count(),
+				"We should only have one listener, but we have " + eventEventListenerGroup.count(),
 				eventEventListenerGroup.count() == 1
 		);
 		assertTrue(
