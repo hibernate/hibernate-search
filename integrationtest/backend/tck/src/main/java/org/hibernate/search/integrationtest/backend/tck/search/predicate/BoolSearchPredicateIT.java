0b7cdea4ee40499b5470780237e240e8137cf201
Guillaume Smet <guillaume.smet@gmail.com>
2018-04-30 12:26:01 +0200

    HSEARCH-3060 Add tests for bool() predicate

---
+++ integrationtest/backend/tck/src/main/java/org/hibernate/search/integrationtest/backend/tck/search/predicate/BoolSearchPredicateIT.java
@@ -0,0 +1,373 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.integrationtest.backend.tck.search.predicate;
+
+import static org.hibernate.search.util.impl.integrationtest.common.assertion.DocumentReferencesSearchResultAssert.assertThat;
+import static org.hibernate.search.util.impl.integrationtest.common.stub.mapper.StubMapperUtils.referenceProvider;
+
+import org.hibernate.search.engine.backend.document.DocumentElement;
+import org.hibernate.search.engine.backend.document.IndexFieldAccessor;
+import org.hibernate.search.engine.backend.document.model.dsl.IndexSchemaElement;
+import org.hibernate.search.engine.backend.index.spi.ChangesetIndexWorker;
+import org.hibernate.search.engine.backend.index.spi.IndexManager;
+import org.hibernate.search.engine.backend.index.spi.IndexSearchTarget;
+import org.hibernate.search.engine.common.spi.SessionContext;
+import org.hibernate.search.integrationtest.backend.tck.util.rule.SearchSetupHelper;
+import org.hibernate.search.engine.search.DocumentReference;
+import org.hibernate.search.engine.search.SearchPredicate;
+import org.hibernate.search.engine.search.SearchQuery;
+import org.hibernate.search.util.impl.integrationtest.common.assertion.DocumentReferencesSearchResultAssert;
+import org.hibernate.search.util.impl.integrationtest.common.stub.StubSessionContext;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class BoolSearchPredicateIT {
+
+	private static final String DOCUMENT_1 = "1";
+	private static final String DOCUMENT_2 = "2";
+	private static final String DOCUMENT_3 = "3";
+
+	// Document 1
+
+	private static final String STRING_1 = "Irving";
+	private static final Integer INTEGER_1 = 3;
+
+	// Document 2
+
+	private static final String STRING_2 = "Auster";
+	private static final Integer INTEGER_2 = 13;
+
+	// Document 3
+
+	private static final String STRING_3 = "Coe";
+	private static final Integer INTEGER_3 = 25;
+
+	@Rule
+	public SearchSetupHelper setupHelper = new SearchSetupHelper();
+
+	private IndexAccessors indexAccessors;
+	private IndexManager<?> indexManager;
+	private String indexName;
+	private SessionContext sessionContext = new StubSessionContext();
+
+	@Before
+	public void setup() {
+		setupHelper.withDefaultConfiguration()
+				.withIndex(
+						"MappedType", "IndexName",
+						ctx -> this.indexAccessors = new IndexAccessors( ctx.getSchemaElement() ),
+						(indexManager, indexName) -> {
+							this.indexManager = indexManager;
+							this.indexName = indexName;
+						}
+				)
+				.setup();
+
+		initData();
+	}
+
+	@Test
+	public void must() {
+		IndexSearchTarget searchTarget = indexManager.createSearchTarget().build();
+
+		SearchQuery<DocumentReference> query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().bool()
+						.must().match().onField( "string" ).matching( STRING_1 )
+				.end()
+				.build();
+
+		DocumentReferencesSearchResultAssert.assertThat( query )
+				.hasReferencesHitsAnyOrder( indexName, DOCUMENT_1 );
+
+		query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().bool()
+						.must().match().onField( "string" ).matching( STRING_1 )
+						.must().match().onField( "integer" ).matching( INTEGER_2 )
+				.end()
+				.build();
+
+		DocumentReferencesSearchResultAssert.assertThat( query ).hasNoHits();
+
+		query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().bool()
+						.must().match().onField( "string" ).matching( STRING_1 )
+						.must().match().onField( "integer" ).matching( INTEGER_1 )
+				.end()
+				.build();
+
+		DocumentReferencesSearchResultAssert.assertThat( query )
+				.hasReferencesHitsAnyOrder( indexName, DOCUMENT_1 );
+	}
+
+	@Test
+	public void must_consumer() {
+		IndexSearchTarget searchTarget = indexManager.createSearchTarget().build();
+
+		SearchQuery<DocumentReference> query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().bool()
+						.must( c -> c.match().onField( "string" ).matching( STRING_1 ) )
+				.end()
+				.build();
+
+		DocumentReferencesSearchResultAssert.assertThat( query )
+				.hasReferencesHitsAnyOrder( indexName, DOCUMENT_1 );
+	}
+
+	@Test
+	public void must_predicate() {
+		IndexSearchTarget searchTarget = indexManager.createSearchTarget().build();
+
+		SearchPredicate predicate = searchTarget.predicate().match().onField( "string" ).matching( STRING_1 );
+
+		SearchQuery<DocumentReference> query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().bool()
+						.must( predicate )
+				.end()
+				.build();
+
+		DocumentReferencesSearchResultAssert.assertThat( query )
+				.hasReferencesHitsAnyOrder( indexName, DOCUMENT_1 );
+	}
+
+	@Test
+	public void should() {
+		IndexSearchTarget searchTarget = indexManager.createSearchTarget().build();
+
+		SearchQuery<DocumentReference> query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().bool()
+						.should().match().onField( "string" ).matching( STRING_1 )
+				.end()
+				.build();
+
+		DocumentReferencesSearchResultAssert.assertThat( query )
+				.hasReferencesHitsAnyOrder( indexName, DOCUMENT_1 );
+
+		query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().bool()
+						.should().match().onField( "string" ).matching( STRING_1 )
+						.should().match().onField( "string" ).matching( STRING_2 )
+				.end()
+				.build();
+
+		DocumentReferencesSearchResultAssert.assertThat( query )
+				.hasReferencesHitsAnyOrder( indexName, DOCUMENT_1, DOCUMENT_2 );
+	}
+
+	@Test
+	public void should_consumer() {
+		IndexSearchTarget searchTarget = indexManager.createSearchTarget().build();
+
+		SearchQuery<DocumentReference> query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().bool()
+						.should( c -> c.match().onField( "string" ).matching( STRING_1 ) )
+						.should( c -> c.match().onField( "string" ).matching( STRING_2 ) )
+				.end()
+				.build();
+
+		DocumentReferencesSearchResultAssert.assertThat( query )
+				.hasReferencesHitsAnyOrder( indexName, DOCUMENT_1, DOCUMENT_2 );
+	}
+
+	@Test
+	public void should_predicate() {
+		IndexSearchTarget searchTarget = indexManager.createSearchTarget().build();
+
+		SearchPredicate predicate1 = searchTarget.predicate().match().onField( "string" ).matching( STRING_1 );
+		SearchPredicate predicate2 = searchTarget.predicate().match().onField( "string" ).matching( STRING_3 );
+
+		SearchQuery<DocumentReference> query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().bool()
+						.should( predicate1 )
+						.should( predicate2 )
+				.end()
+				.build();
+
+		DocumentReferencesSearchResultAssert.assertThat( query )
+				.hasReferencesHitsAnyOrder( indexName, DOCUMENT_1, DOCUMENT_3 );
+	}
+
+	@Test
+	public void mustNot() {
+		IndexSearchTarget searchTarget = indexManager.createSearchTarget().build();
+
+		SearchQuery<DocumentReference> query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().bool()
+						.mustNot().match().onField( "string" ).matching( STRING_1 )
+				.end()
+				.build();
+
+		DocumentReferencesSearchResultAssert.assertThat( query )
+				.hasReferencesHitsAnyOrder( indexName, DOCUMENT_2, DOCUMENT_3 );
+
+		query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().bool()
+						.mustNot().match().onField( "string" ).matching( STRING_1 )
+						.mustNot().match().onField( "string" ).matching( STRING_3 )
+				.end()
+				.build();
+
+		DocumentReferencesSearchResultAssert.assertThat( query )
+				.hasReferencesHitsAnyOrder( indexName, DOCUMENT_2 );
+	}
+
+	@Test
+	public void mustNot_consumer() {
+		IndexSearchTarget searchTarget = indexManager.createSearchTarget().build();
+
+		SearchQuery<DocumentReference> query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().bool()
+						.mustNot( c -> c.match().onField( "string" ).matching( STRING_1 ) )
+				.end()
+				.build();
+
+		DocumentReferencesSearchResultAssert.assertThat( query )
+				.hasReferencesHitsAnyOrder( indexName, DOCUMENT_2, DOCUMENT_3 );
+	}
+
+	@Test
+	public void mustNot_predicate() {
+		IndexSearchTarget searchTarget = indexManager.createSearchTarget().build();
+
+		SearchPredicate predicate = searchTarget.predicate().match().onField( "string" ).matching( STRING_2 );
+
+		SearchQuery<DocumentReference> query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().bool()
+						.mustNot( predicate )
+				.end()
+				.build();
+
+		DocumentReferencesSearchResultAssert.assertThat( query )
+				.hasReferencesHitsAnyOrder( indexName, DOCUMENT_1, DOCUMENT_3 );
+	}
+
+	@Test
+	public void should_mustNot() {
+		IndexSearchTarget searchTarget = indexManager.createSearchTarget().build();
+
+		SearchQuery<DocumentReference> query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().bool()
+						.should().match().onField( "string" ).matching( STRING_1 )
+						.should().match().onField( "string" ).matching( STRING_3 )
+						.mustNot().match().onField( "string" ).matching( STRING_1 )
+				.end()
+				.build();
+
+		DocumentReferencesSearchResultAssert.assertThat( query )
+				.hasReferencesHitsAnyOrder( indexName, DOCUMENT_3 );
+	}
+
+	@Test
+	public void must_mustNot() {
+		IndexSearchTarget searchTarget = indexManager.createSearchTarget().build();
+
+		SearchQuery<DocumentReference> query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().bool()
+						.must().match().onField( "string" ).matching( STRING_1 )
+						.mustNot().match().onField( "string" ).matching( STRING_1 )
+				.end()
+				.build();
+
+		DocumentReferencesSearchResultAssert.assertThat( query ).hasNoHits();
+
+		query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().bool()
+						.must().match().onField( "string" ).matching( STRING_1 )
+						.mustNot().match().onField( "string" ).matching( STRING_2 )
+				.end()
+				.build();
+
+		DocumentReferencesSearchResultAssert.assertThat( query )
+				.hasReferencesHitsAnyOrder( indexName, DOCUMENT_1 );
+	}
+
+	@Test
+	public void nested() {
+		IndexSearchTarget searchTarget = indexManager.createSearchTarget().build();
+
+		SearchQuery<DocumentReference> query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().bool()
+						.must(
+								c -> c.bool()
+										.should().match().onField( "string" ).matching( STRING_1 )
+										.should().match().onField( "string" ).matching( STRING_3 )
+						)
+				.end()
+				.build();
+
+		DocumentReferencesSearchResultAssert.assertThat( query )
+				.hasReferencesHitsAnyOrder( indexName, DOCUMENT_1, DOCUMENT_3 );
+
+		query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().bool()
+						.must(
+								c -> c.bool()
+										.should().match().onField( "string" ).matching( STRING_1 )
+										.should().match().onField( "string" ).matching( STRING_3 )
+						)
+						.mustNot().match().onField( "string" ).matching( STRING_3 )
+				.end()
+				.build();
+
+		DocumentReferencesSearchResultAssert.assertThat( query )
+				.hasReferencesHitsAnyOrder( indexName, DOCUMENT_1 );
+	}
+
+	private void initData() {
+		ChangesetIndexWorker<? extends DocumentElement> worker = indexManager.createWorker( sessionContext );
+		worker.add( referenceProvider( DOCUMENT_1 ), document -> {
+			indexAccessors.string.write( document, STRING_1 );
+			indexAccessors.integer.write( document, INTEGER_1 );
+		} );
+		worker.add( referenceProvider( DOCUMENT_2 ), document -> {
+			indexAccessors.string.write( document, STRING_2 );
+			indexAccessors.integer.write( document, INTEGER_2 );
+		} );
+		worker.add( referenceProvider( DOCUMENT_3 ), document -> {
+			indexAccessors.string.write( document, STRING_3 );
+			indexAccessors.integer.write( document, INTEGER_3 );
+		} );
+
+		worker.execute().join();
+
+		// Check that all documents are searchable
+		IndexSearchTarget searchTarget = indexManager.createSearchTarget().build();
+		SearchQuery<DocumentReference> query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().all().end()
+				.build();
+		assertThat( query ).hasReferencesHitsAnyOrder( indexName, DOCUMENT_1, DOCUMENT_2, DOCUMENT_3 );
+	}
+
+	private static class IndexAccessors {
+		final IndexFieldAccessor<String> string;
+		final IndexFieldAccessor<Integer> integer;
+
+		IndexAccessors(IndexSchemaElement root) {
+			string = root.field( "string" ).asString().createAccessor();
+			integer = root.field( "integer" ).asInteger().createAccessor();
+		}
+	}
+}
