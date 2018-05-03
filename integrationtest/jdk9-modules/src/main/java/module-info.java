6d11e372628ac704facdcdf3002972d0893eaaab
Sanne Grinovero <sanne@hibernate.org>
2018-05-03 10:51:26 +0100

    HSEARCH-3000 Pick Jigsaw Automatic Module names for all published modules

--- integrationtest/jdk9-modules/src/main/java/module-info.java
+++ integrationtest/jdk9-modules/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module hibernate.search.integrationtest.jdk9.modules.client {
 	exports org.hibernate.search.test.integration.jdk9_modules.client.service;
-	requires hibernate.search.engine;
-	requires hibernate.search.orm;
+	requires org.hibernate.search.engine;
+	requires org.hibernate.search.orm;
 	requires java.persistence;
 	requires org.hibernate.orm.core;
 	requires lucene.analyzers.common;
