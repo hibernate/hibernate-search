bc996bf4554c798bc0e56598067b4573a970b58b
Yoann Rodière <yoann@hibernate.org>
2018-05-25 09:57:00 +0200

    Remove some obsolete comments

--- mapper/orm/src/main/java/org/hibernate/search/mapper/orm/mapping/HibernateOrmMappingInitiator.java
+++ mapper/orm/src/main/java/org/hibernate/search/mapper/orm/mapping/HibernateOrmMappingInitiator.java
@@ -26,13 +26,11 @@ import org.hibernate.search.mapper.pojo.mapping.building.spi.PojoTypeMetadataCon
 import org.hibernate.search.mapper.pojo.mapping.spi.PojoMappingInitiatorImpl;
 
 /*
- * TODO create a Hibernate ORM specific mapper, with the following additions:
+ * TODO make the following additions to the Hibernate ORM specific mapping:
  *  1. During mapping creation, use the Hibernate ORM identifier as a fallback when no document ID was found
- *  2. Save additional information regarding containedIn, and make it available in the mapping
- *  3. Use a specific introspector that will comply with Hibernate ORM's access mode
- *  4. When the @DocumentId is the @Id, use the provided ID in priority and only if it's missing, unproxy the entity and get the ID;
+ *  2. When the @DocumentId is the @Id, use the provided ID in priority and only if it's missing, unproxy the entity and get the ID;
  *     when the @DocumentId is NOT the @Id, always ignore the provided ID. See org.hibernate.search.engine.common.impl.WorkPlan.PerClassWork.extractProperId(Work)
- *  5. And more?
+ *  3. And more?
  */
 public class HibernateOrmMappingInitiator extends PojoMappingInitiatorImpl<HibernateOrmMapping> {
 
