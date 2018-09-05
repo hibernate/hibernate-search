hibernate/hibernate-search-6-poc

HSEARCH-3128 Add user APIs to access the backends

backend-elasticsearch/src/main/java/org/hibernate/search/v6poc/backend/elasticsearch/impl/ElasticsearchBackendFactory.java
@@ -21,7 +21,7 @@ import org.hibernate.search.v6poc.backend.elasticsearch.gson.impl.ES5NormsTypeJs
 import org.hibernate.search.v6poc.backend.elasticsearch.gson.impl.GsonProvider;
 import org.hibernate.search.v6poc.backend.elasticsearch.work.impl.ElasticsearchWorkFactory;
 import org.hibernate.search.v6poc.backend.elasticsearch.work.impl.StubElasticsearchWorkFactory;
-import org.hibernate.search.v6poc.backend.spi.Backend;
+import org.hibernate.search.v6poc.backend.spi.BackendImplementor;
 import org.hibernate.search.v6poc.backend.spi.BackendFactory;
 import org.hibernate.search.v6poc.cfg.ConfigurationPropertySource;
 import org.hibernate.search.v6poc.cfg.spi.ConfigurationProperty;
@@ -49,7 +49,7 @@ public class ElasticsearchBackendFactory implements BackendFactory {
 					.build();
 
 	@Override
-	public Backend<?> create(String name, BuildContext context, ConfigurationPropertySource propertySource) {
+	public BackendImplementor<?> create(String name, BuildContext context, ConfigurationPropertySource propertySource) {
 		ElasticsearchClientFactory clientFactory = new DefaultElasticsearchClientFactory();
 
 		boolean logPrettyPrinting = LOG_JSON_PRETTY_PRINTING.get( propertySource );
@@ -65,7 +65,7 @@ public class ElasticsearchBackendFactory implements BackendFactory {
 
 		ElasticsearchWorkFactory workFactory = new StubElasticsearchWorkFactory( dialectSpecificGsonProvider );
 
-		return new ElasticsearchBackend( client, name, workFactory, getMultiTenancyStrategy( name, propertySource ) );
+		return new ElasticsearchBackendImpl( client, name, workFactory, getMultiTenancyStrategy( name, propertySource ) );
 	}
 
 	private GsonBuilder createES5GsonBuilderBase() {

backend-lucene/src/main/java/org/hibernate/search/v6poc/backend/lucene/impl/LuceneBackendImplementor.java
@@ -9,12 +9,12 @@ package org.hibernate.search.v6poc.backend.lucene.impl;
 import org.hibernate.search.v6poc.backend.lucene.document.impl.LuceneRootDocumentBuilder;
 import org.hibernate.search.v6poc.backend.lucene.orchestration.impl.LuceneQueryWorkOrchestrator;
 import org.hibernate.search.v6poc.backend.lucene.work.impl.LuceneWorkFactory;
-import org.hibernate.search.v6poc.backend.spi.Backend;
+import org.hibernate.search.v6poc.backend.spi.BackendImplementor;
 
 /**
  * @author Guillaume Smet
  */
-public interface LuceneBackend extends Backend<LuceneRootDocumentBuilder> {
+public interface LuceneBackendImplementor extends BackendImplementor<LuceneRootDocumentBuilder> {
 
 	LuceneWorkFactory getWorkFactory();
 

backend-lucene/src/main/java/org/hibernate/search/v6poc/backend/lucene/impl/LuceneLocalDirectoryBackend.java
@@ -10,6 +10,7 @@ import java.lang.invoke.MethodHandles;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import org.hibernate.search.v6poc.backend.Backend;
 import org.hibernate.search.v6poc.backend.index.spi.IndexManagerBuilder;
 import org.hibernate.search.v6poc.backend.lucene.document.impl.LuceneRootDocumentBuilder;
 import org.hibernate.search.v6poc.backend.lucene.index.impl.LuceneLocalDirectoryIndexManagerBuilder;
@@ -25,7 +26,7 @@ import org.hibernate.search.v6poc.util.impl.common.LoggerFactory;
 /**
  * @author Guillaume Smet
  */
-public class LuceneLocalDirectoryBackend implements LuceneBackend {
+public class LuceneLocalDirectoryBackend implements LuceneBackendImplementor, Backend {
 
 	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
 
@@ -50,6 +51,11 @@ public class LuceneLocalDirectoryBackend implements LuceneBackend {
 		this.multiTenancyStrategy = multiTenancyStrategy;
 
 		initializeRootDirectory( name, rootDirectory );
+	}
+
+	@Override
+	public Backend toAPI() {
+		return this;
 	}
 
 	@Override

engine/src/main/java/org/hibernate/search/v6poc/engine/impl/IndexManagerBuildingStateHolder.java
@@ -14,7 +14,7 @@ import org.hibernate.search.v6poc.backend.document.DocumentElement;
 import org.hibernate.search.v6poc.backend.document.model.spi.IndexSchemaCollector;
 import org.hibernate.search.v6poc.backend.index.spi.IndexManager;
 import org.hibernate.search.v6poc.backend.index.spi.IndexManagerBuilder;
-import org.hibernate.search.v6poc.backend.spi.Backend;
+import org.hibernate.search.v6poc.backend.spi.BackendImplementor;
 import org.hibernate.search.v6poc.backend.spi.BackendFactory;
 import org.hibernate.search.v6poc.cfg.ConfigurationPropertySource;
 import org.hibernate.search.v6poc.cfg.spi.ConfigurationProperty;
@@ -41,7 +41,7 @@ class IndexManagerBuildingStateHolder {
 	private final ConfigurationPropertySource propertySource;
 	private final ConfigurationPropertySource defaultIndexPropertySource;
 
-	private final Map<String, Backend<?>> backendsByName = new HashMap<>();
+	private final Map<String, BackendImplementor<?>> backendsByName = new HashMap<>();
 	private final Map<String, IndexManagerBuildingState<?>> indexManagerBuildingStateByName = new HashMap<>();
 
 	IndexManagerBuildingStateHolder(BuildContext buildContext,
@@ -56,7 +56,7 @@ class IndexManagerBuildingStateHolder {
 				.withFallback( defaultIndexPropertySource );
 		// TODO more checks on the backend name (is non-null, non-empty)
 		String backendName = INDEX_BACKEND_NAME.get( indexPropertySource ).get();
-		Backend<?> backend = backendsByName.computeIfAbsent( backendName, this::createBackend );
+		BackendImplementor<?> backend = backendsByName.computeIfAbsent( backendName, this::createBackend );
 		String normalizedIndexName = backend.normalizeIndexName( rawIndexName );
 
 		IndexManagerBuildingState<?> state = indexManagerBuildingStateByName.get( normalizedIndexName );
@@ -67,19 +67,20 @@ class IndexManagerBuildingStateHolder {
 		return state;
 	}
 
-	public Map<String, Backend<?>> getBackendsByName() {
+	public Map<String, BackendImplementor<?>> getBackendsByName() {
 		return backendsByName;
 	}
 
 	private <D extends DocumentElement> IndexManagerBuildingState<D> createIndexManagerBuildingState(
-			Backend<D> backend, String normalizedIndexName, boolean multiTenancyEnabled, ConfigurationPropertySource indexPropertySource) {
+			BackendImplementor<D> backend, String normalizedIndexName, boolean multiTenancyEnabled,
+			ConfigurationPropertySource indexPropertySource) {
 		IndexManagerBuilder<D> builder = backend.createIndexManagerBuilder( normalizedIndexName, multiTenancyEnabled, buildContext, indexPropertySource );
 		IndexSchemaCollector schemaCollector = builder.getSchemaCollector();
 		IndexModelBindingContext bindingContext = new IndexModelBindingContextImpl( schemaCollector );
 		return new IndexMappingBuildingStateImpl<>( normalizedIndexName, builder, bindingContext );
 	}
 
-	private Backend<?> createBackend(String backendName) {
+	private BackendImplementor<?> createBackend(String backendName) {
 		ConfigurationPropertySource backendPropertySource = propertySource.withMask( "backend." + backendName );
 		// TODO more checks on the backend type (non-null, non-empty)
 		String backendType = BACKEND_TYPE.get( backendPropertySource ).get();

hibernate/hibernate-search

backend/elasticsearch/src/main/java/org/hibernate/search/backend/elasticsearch/impl/ElasticsearchBackendFactory.java
@@ -21,7 +21,7 @@ import org.hibernate.search.backend.elasticsearch.gson.impl.ES5NormsTypeJsonAdap
 import org.hibernate.search.backend.elasticsearch.gson.impl.GsonProvider;
 import org.hibernate.search.backend.elasticsearch.work.impl.ElasticsearchWorkFactory;
 import org.hibernate.search.backend.elasticsearch.work.impl.StubElasticsearchWorkFactory;
-import org.hibernate.search.engine.backend.spi.Backend;
+import org.hibernate.search.engine.backend.spi.BackendImplementor;
 import org.hibernate.search.engine.backend.spi.BackendFactory;
 import org.hibernate.search.engine.cfg.ConfigurationPropertySource;
 import org.hibernate.search.engine.cfg.spi.ConfigurationProperty;
@@ -49,7 +49,7 @@ public class ElasticsearchBackendFactory implements BackendFactory {
 					.build();
 
 	@Override
-	public Backend<?> create(String name, BuildContext context, ConfigurationPropertySource propertySource) {
+	public BackendImplementor<?> create(String name, BuildContext context, ConfigurationPropertySource propertySource) {
 		ElasticsearchClientFactory clientFactory = new DefaultElasticsearchClientFactory();
 
 		boolean logPrettyPrinting = LOG_JSON_PRETTY_PRINTING.get( propertySource );
@@ -65,7 +65,7 @@ public class ElasticsearchBackendFactory implements BackendFactory {
 
 		ElasticsearchWorkFactory workFactory = new StubElasticsearchWorkFactory( dialectSpecificGsonProvider );
 
-		return new ElasticsearchBackend( client, name, workFactory, getMultiTenancyStrategy( name, propertySource ) );
+		return new ElasticsearchBackendImpl( client, name, workFactory, getMultiTenancyStrategy( name, propertySource ) );
 	}
 
 	private GsonBuilder createES5GsonBuilderBase() {

backend/lucene/src/main/java/org/hibernate/search/backend/lucene/impl/LuceneBackendImplementor.java
@@ -9,12 +9,12 @@ package org.hibernate.search.backend.lucene.impl;
 import org.hibernate.search.backend.lucene.document.impl.LuceneRootDocumentBuilder;
 import org.hibernate.search.backend.lucene.orchestration.impl.LuceneQueryWorkOrchestrator;
 import org.hibernate.search.backend.lucene.work.impl.LuceneWorkFactory;
-import org.hibernate.search.engine.backend.spi.Backend;
+import org.hibernate.search.engine.backend.spi.BackendImplementor;
 
 /**
  * @author Guillaume Smet
  */
-public interface LuceneBackend extends Backend<LuceneRootDocumentBuilder> {
+public interface LuceneBackendImplementor extends BackendImplementor<LuceneRootDocumentBuilder> {
 
 	LuceneWorkFactory getWorkFactory();
 

backend/lucene/src/main/java/org/hibernate/search/backend/lucene/impl/LuceneLocalDirectoryBackend.java
@@ -10,6 +10,7 @@ import java.lang.invoke.MethodHandles;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import org.hibernate.search.engine.backend.Backend;
 import org.hibernate.search.engine.backend.index.spi.IndexManagerBuilder;
 import org.hibernate.search.backend.lucene.document.impl.LuceneRootDocumentBuilder;
 import org.hibernate.search.backend.lucene.index.impl.LuceneLocalDirectoryIndexManagerBuilder;
@@ -25,7 +26,7 @@ import org.hibernate.search.util.impl.common.LoggerFactory;
 /**
  * @author Guillaume Smet
  */
-public class LuceneLocalDirectoryBackend implements LuceneBackend {
+public class LuceneLocalDirectoryBackend implements LuceneBackendImplementor, Backend {
 
 	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
 
@@ -50,6 +51,11 @@ public class LuceneLocalDirectoryBackend implements LuceneBackend {
 		this.multiTenancyStrategy = multiTenancyStrategy;
 
 		initializeRootDirectory( name, rootDirectory );
+	}
+
+	@Override
+	public Backend toAPI() {
+		return this;
 	}
 
 	@Override

engine/src/main/java/org/hibernate/search/engine/common/impl/IndexManagerBuildingStateHolder.java
@@ -14,7 +14,7 @@ import org.hibernate.search.engine.backend.document.DocumentElement;
 import org.hibernate.search.engine.backend.document.model.spi.IndexSchemaCollector;
 import org.hibernate.search.engine.backend.index.spi.IndexManager;
 import org.hibernate.search.engine.backend.index.spi.IndexManagerBuilder;
-import org.hibernate.search.engine.backend.spi.Backend;
+import org.hibernate.search.engine.backend.spi.BackendImplementor;
 import org.hibernate.search.engine.backend.spi.BackendFactory;
 import org.hibernate.search.engine.cfg.ConfigurationPropertySource;
 import org.hibernate.search.engine.cfg.spi.ConfigurationProperty;
@@ -41,7 +41,7 @@ class IndexManagerBuildingStateHolder {
 	private final ConfigurationPropertySource propertySource;
 	private final ConfigurationPropertySource defaultIndexPropertySource;
 
-	private final Map<String, Backend<?>> backendsByName = new HashMap<>();
+	private final Map<String, BackendImplementor<?>> backendsByName = new HashMap<>();
 	private final Map<String, IndexManagerBuildingState<?>> indexManagerBuildingStateByName = new HashMap<>();
 
 	IndexManagerBuildingStateHolder(BuildContext buildContext,
@@ -56,7 +56,7 @@ class IndexManagerBuildingStateHolder {
 				.withFallback( defaultIndexPropertySource );
 		// TODO more checks on the backend name (is non-null, non-empty)
 		String backendName = INDEX_BACKEND_NAME.get( indexPropertySource ).get();
-		Backend<?> backend = backendsByName.computeIfAbsent( backendName, this::createBackend );
+		BackendImplementor<?> backend = backendsByName.computeIfAbsent( backendName, this::createBackend );
 		String normalizedIndexName = backend.normalizeIndexName( rawIndexName );
 
 		IndexManagerBuildingState<?> state = indexManagerBuildingStateByName.get( normalizedIndexName );
@@ -67,19 +67,20 @@ class IndexManagerBuildingStateHolder {
 		return state;
 	}
 
-	public Map<String, Backend<?>> getBackendsByName() {
+	public Map<String, BackendImplementor<?>> getBackendsByName() {
 		return backendsByName;
 	}
 
 	private <D extends DocumentElement> IndexManagerBuildingState<D> createIndexManagerBuildingState(
-			Backend<D> backend, String normalizedIndexName, boolean multiTenancyEnabled, ConfigurationPropertySource indexPropertySource) {
+			BackendImplementor<D> backend, String normalizedIndexName, boolean multiTenancyEnabled,
+			ConfigurationPropertySource indexPropertySource) {
 		IndexManagerBuilder<D> builder = backend.createIndexManagerBuilder( normalizedIndexName, multiTenancyEnabled, buildContext, indexPropertySource );
 		IndexSchemaCollector schemaCollector = builder.getSchemaCollector();
 		IndexModelBindingContext bindingContext = new IndexModelBindingContextImpl( schemaCollector );
 		return new IndexMappingBuildingStateImpl<>( normalizedIndexName, builder, bindingContext );
 	}
 
-	private Backend<?> createBackend(String backendName) {
+	private BackendImplementor<?> createBackend(String backendName) {
 		ConfigurationPropertySource backendPropertySource = propertySource.withMask( "backend." + backendName );
 		// TODO more checks on the backend type (non-null, non-empty)
 		String backendType = BACKEND_TYPE.get( backendPropertySource ).get();