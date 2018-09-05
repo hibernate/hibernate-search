hibernate/hibernate-search-6-poc

HSEARCH-3191 Use .end() terminal calls for all predicates

engine/src/main/java/org/hibernate/search/v6poc/search/dsl/predicate/MatchPredicateFieldSetContext.java
@@ -6,6 +6,7 @@
  */
 package org.hibernate.search.v6poc.search.dsl.predicate;
 
+import org.hibernate.search.v6poc.search.dsl.ExplicitEndContext;
 
 /**
  * The context used when defining a match predicate, after at least one field was mentioned.
@@ -14,6 +15,6 @@ package org.hibernate.search.v6poc.search.dsl.predicate;
  */
 public interface MatchPredicateFieldSetContext<N> extends MultiFieldPredicateFieldSetContext<MatchPredicateFieldSetContext<N>> {
 
-	N matching(Object value);
+	ExplicitEndContext<N> matching(Object value);
 
 }

engine/src/main/java/org/hibernate/search/v6poc/search/dsl/predicate/RangePredicateFieldSetContext.java
@@ -6,6 +6,7 @@
  */
 package org.hibernate.search.v6poc.search.dsl.predicate;
 
+import org.hibernate.search.v6poc.search.dsl.ExplicitEndContext;
 
 /**
  * The context used when defining a range predicate, after at least one field was mentioned.
@@ -21,15 +22,15 @@ public interface RangePredicateFieldSetContext<N> extends MultiFieldPredicateFie
 		return from( value, RangeBoundInclusion.INCLUDED );
 	}
 
-	N above(Object value, RangeBoundInclusion inclusion);
+	ExplicitEndContext<N> above(Object value, RangeBoundInclusion inclusion);
 
-	default N above(Object value) {
+	default ExplicitEndContext<N> above(Object value) {
 		return above( value, RangeBoundInclusion.INCLUDED );
 	}
 
-	N below(Object value, RangeBoundInclusion inclusion);
+	ExplicitEndContext<N> below(Object value, RangeBoundInclusion inclusion);
 
-	default N below(Object value) {
+	default ExplicitEndContext<N> below(Object value) {
 		return below( value, RangeBoundInclusion.INCLUDED );
 	}
 

engine/src/main/java/org/hibernate/search/v6poc/search/dsl/predicate/RangePredicateFromContext.java
@@ -6,6 +6,7 @@
  */
 package org.hibernate.search.v6poc.search.dsl.predicate;
 
+import org.hibernate.search.v6poc.search.dsl.ExplicitEndContext;
 
 /**
  * The context used when defining a range predicate,
@@ -15,10 +16,10 @@ package org.hibernate.search.v6poc.search.dsl.predicate;
  */
 public interface RangePredicateFromContext<N> {
 
-	default N to(Object value) {
+	default ExplicitEndContext<N> to(Object value) {
 		return to( value, RangeBoundInclusion.INCLUDED );
 	}
 
-	N to(Object value, RangeBoundInclusion inclusion);
+	ExplicitEndContext<N> to(Object value, RangeBoundInclusion inclusion);
 
 }

engine/src/main/java/org/hibernate/search/v6poc/search/dsl/predicate/impl/MatchPredicateFieldSetContextImpl.java
@@ -16,6 +16,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.hibernate.search.v6poc.logging.impl.Log;
+import org.hibernate.search.v6poc.search.dsl.ExplicitEndContext;
 import org.hibernate.search.v6poc.search.dsl.predicate.MatchPredicateFieldSetContext;
 import org.hibernate.search.v6poc.search.predicate.spi.MatchPredicateBuilder;
 import org.hibernate.search.v6poc.search.predicate.spi.SearchPredicateFactory;
@@ -54,7 +55,7 @@ class MatchPredicateFieldSetContextImpl<N, B>
 	}
 
 	@Override
-	public N matching(Object value) {
+	public ExplicitEndContext<N> matching(Object value) {
 		return commonState.matching( value );
 	}
 
@@ -65,17 +66,23 @@ class MatchPredicateFieldSetContextImpl<N, B>
 		}
 	}
 
-	static class CommonState<N, B> extends MultiFieldPredicateCommonState<N, B, MatchPredicateFieldSetContextImpl<N, B>> {
+	static class CommonState<N, B> extends MultiFieldPredicateCommonState<N, B, MatchPredicateFieldSetContextImpl<N, B>>
+			implements ExplicitEndContext<N> {
 
 		CommonState(SearchPredicateFactory<?, B> factory, Supplier<N> nextContextProvider) {
 			super( factory, nextContextProvider );
 		}
 
-		public N matching(Object value) {
+		public ExplicitEndContext<N> matching(Object value) {
 			if ( value == null ) {
 				throw log.matchPredicateCannotMatchNullValue( collectAbsoluteFieldPaths() );
 			}
 			getQueryBuilders().forEach( b -> b.value( value ) );
+			return this;
+		}
+
+		@Override
+		public N end() {
 			return getNextContextProvider().get();
 		}
 

engine/src/main/java/org/hibernate/search/v6poc/search/dsl/predicate/impl/RangePredicateFieldSetContextImpl.java
@@ -16,6 +16,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.hibernate.search.v6poc.logging.impl.Log;
+import org.hibernate.search.v6poc.search.dsl.ExplicitEndContext;
 import org.hibernate.search.v6poc.search.dsl.predicate.RangeBoundInclusion;
 import org.hibernate.search.v6poc.search.dsl.predicate.RangePredicateFieldSetContext;
 import org.hibernate.search.v6poc.search.dsl.predicate.RangePredicateFromContext;
@@ -61,12 +62,12 @@ class RangePredicateFieldSetContextImpl<N, B>
 	}
 
 	@Override
-	public N above(Object value, RangeBoundInclusion inclusion) {
+	public ExplicitEndContext<N> above(Object value, RangeBoundInclusion inclusion) {
 		return commonState.above( value, inclusion );
 	}
 
 	@Override
-	public N below(Object value, RangeBoundInclusion inclusion) {
+	public ExplicitEndContext<N> below(Object value, RangeBoundInclusion inclusion) {
 		return commonState.below( value, inclusion );
 	}
 
@@ -77,7 +78,8 @@ class RangePredicateFieldSetContextImpl<N, B>
 		}
 	}
 
-	static class CommonState<N, B> extends MultiFieldPredicateCommonState<N, B, RangePredicateFieldSetContextImpl<N, B>> {
+	static class CommonState<N, B> extends MultiFieldPredicateCommonState<N, B, RangePredicateFieldSetContextImpl<N, B>>
+			implements RangePredicateFromContext<N>, ExplicitEndContext<N> {
 
 		private boolean hasNonNullBound = false;
 
@@ -104,15 +106,15 @@ class RangePredicateFieldSetContextImpl<N, B>
 				case INCLUDED:
 					break;
 			}
-			return new RangePredicateFromContext<N>() {
-				@Override
-				public N to(Object value, RangeBoundInclusion inclusion) {
-					return below( value, inclusion );
-				}
-			};
+			return this;
 		}
 
-		N above(Object value, RangeBoundInclusion inclusion) {
+		@Override
+		public ExplicitEndContext<N> to(Object value, RangeBoundInclusion inclusion) {
+			return below( value, inclusion );
+		}
+
+		ExplicitEndContext<N> above(Object value, RangeBoundInclusion inclusion) {
 			if ( value != null ) {
 				hasNonNullBound = true;
 				getQueryBuilders().forEach( q -> q.lowerLimit( value ) );
@@ -125,10 +127,10 @@ class RangePredicateFieldSetContextImpl<N, B>
 					break;
 			}
 			checkHasNonNullBound();
-			return getNextContextProvider().get();
+			return this;
 		}
 
-		N below(Object value, RangeBoundInclusion inclusion) {
+		ExplicitEndContext<N> below(Object value, RangeBoundInclusion inclusion) {
 			if ( value != null ) {
 				hasNonNullBound = true;
 				getQueryBuilders().forEach( q -> q.upperLimit( value ) );
@@ -141,6 +143,11 @@ class RangePredicateFieldSetContextImpl<N, B>
 					break;
 			}
 			checkHasNonNullBound();
+			return this;
+		}
+
+		@Override
+		public N end() {
 			return getNextContextProvider().get();
 		}
 

hibernate/hibernate-search

engine/src/main/java/org/hibernate/search/engine/search/dsl/predicate/MatchPredicateFieldSetContext.java
@@ -6,6 +6,7 @@
  */
 package org.hibernate.search.engine.search.dsl.predicate;
 
+import org.hibernate.search.engine.search.dsl.ExplicitEndContext;
 
 /**
  * The context used when defining a match predicate, after at least one field was mentioned.
@@ -14,6 +15,6 @@ package org.hibernate.search.engine.search.dsl.predicate;
  */
 public interface MatchPredicateFieldSetContext<N> extends MultiFieldPredicateFieldSetContext<MatchPredicateFieldSetContext<N>> {
 
-	N matching(Object value);
+	ExplicitEndContext<N> matching(Object value);
 
 }

engine/src/main/java/org/hibernate/search/engine/search/dsl/predicate/RangePredicateFieldSetContext.java
@@ -6,6 +6,7 @@
  */
 package org.hibernate.search.engine.search.dsl.predicate;
 
+import org.hibernate.search.engine.search.dsl.ExplicitEndContext;
 
 /**
  * The context used when defining a range predicate, after at least one field was mentioned.
@@ -21,15 +22,15 @@ public interface RangePredicateFieldSetContext<N> extends MultiFieldPredicateFie
 		return from( value, RangeBoundInclusion.INCLUDED );
 	}
 
-	N above(Object value, RangeBoundInclusion inclusion);
+	ExplicitEndContext<N> above(Object value, RangeBoundInclusion inclusion);
 
-	default N above(Object value) {
+	default ExplicitEndContext<N> above(Object value) {
 		return above( value, RangeBoundInclusion.INCLUDED );
 	}
 
-	N below(Object value, RangeBoundInclusion inclusion);
+	ExplicitEndContext<N> below(Object value, RangeBoundInclusion inclusion);
 
-	default N below(Object value) {
+	default ExplicitEndContext<N> below(Object value) {
 		return below( value, RangeBoundInclusion.INCLUDED );
 	}
 

engine/src/main/java/org/hibernate/search/engine/search/dsl/predicate/RangePredicateFromContext.java
@@ -6,6 +6,7 @@
  */
 package org.hibernate.search.engine.search.dsl.predicate;
 
+import org.hibernate.search.engine.search.dsl.ExplicitEndContext;
 
 /**
  * The context used when defining a range predicate,
@@ -15,10 +16,10 @@ package org.hibernate.search.engine.search.dsl.predicate;
  */
 public interface RangePredicateFromContext<N> {
 
-	default N to(Object value) {
+	default ExplicitEndContext<N> to(Object value) {
 		return to( value, RangeBoundInclusion.INCLUDED );
 	}
 
-	N to(Object value, RangeBoundInclusion inclusion);
+	ExplicitEndContext<N> to(Object value, RangeBoundInclusion inclusion);
 
 }

engine/src/main/java/org/hibernate/search/engine/search/dsl/predicate/impl/MatchPredicateFieldSetContextImpl.java
@@ -16,6 +16,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.hibernate.search.engine.logging.impl.Log;
+import org.hibernate.search.engine.search.dsl.ExplicitEndContext;
 import org.hibernate.search.engine.search.dsl.predicate.MatchPredicateFieldSetContext;
 import org.hibernate.search.engine.search.predicate.spi.MatchPredicateBuilder;
 import org.hibernate.search.engine.search.predicate.spi.SearchPredicateFactory;
@@ -54,7 +55,7 @@ class MatchPredicateFieldSetContextImpl<N, B>
 	}
 
 	@Override
-	public N matching(Object value) {
+	public ExplicitEndContext<N> matching(Object value) {
 		return commonState.matching( value );
 	}
 
@@ -65,17 +66,23 @@ class MatchPredicateFieldSetContextImpl<N, B>
 		}
 	}
 
-	static class CommonState<N, B> extends MultiFieldPredicateCommonState<N, B, MatchPredicateFieldSetContextImpl<N, B>> {
+	static class CommonState<N, B> extends MultiFieldPredicateCommonState<N, B, MatchPredicateFieldSetContextImpl<N, B>>
+			implements ExplicitEndContext<N> {
 
 		CommonState(SearchPredicateFactory<?, B> factory, Supplier<N> nextContextProvider) {
 			super( factory, nextContextProvider );
 		}
 
-		public N matching(Object value) {
+		public ExplicitEndContext<N> matching(Object value) {
 			if ( value == null ) {
 				throw log.matchPredicateCannotMatchNullValue( collectAbsoluteFieldPaths() );
 			}
 			getQueryBuilders().forEach( b -> b.value( value ) );
+			return this;
+		}
+
+		@Override
+		public N end() {
 			return getNextContextProvider().get();
 		}
 

engine/src/main/java/org/hibernate/search/engine/search/dsl/predicate/impl/RangePredicateFieldSetContextImpl.java
@@ -16,6 +16,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.hibernate.search.engine.logging.impl.Log;
+import org.hibernate.search.engine.search.dsl.ExplicitEndContext;
 import org.hibernate.search.engine.search.dsl.predicate.RangeBoundInclusion;
 import org.hibernate.search.engine.search.dsl.predicate.RangePredicateFieldSetContext;
 import org.hibernate.search.engine.search.dsl.predicate.RangePredicateFromContext;
@@ -61,12 +62,12 @@ class RangePredicateFieldSetContextImpl<N, B>
 	}
 
 	@Override
-	public N above(Object value, RangeBoundInclusion inclusion) {
+	public ExplicitEndContext<N> above(Object value, RangeBoundInclusion inclusion) {
 		return commonState.above( value, inclusion );
 	}
 
 	@Override
-	public N below(Object value, RangeBoundInclusion inclusion) {
+	public ExplicitEndContext<N> below(Object value, RangeBoundInclusion inclusion) {
 		return commonState.below( value, inclusion );
 	}
 
@@ -77,7 +78,8 @@ class RangePredicateFieldSetContextImpl<N, B>
 		}
 	}
 
-	static class CommonState<N, B> extends MultiFieldPredicateCommonState<N, B, RangePredicateFieldSetContextImpl<N, B>> {
+	static class CommonState<N, B> extends MultiFieldPredicateCommonState<N, B, RangePredicateFieldSetContextImpl<N, B>>
+			implements RangePredicateFromContext<N>, ExplicitEndContext<N> {
 
 		private boolean hasNonNullBound = false;
 
@@ -104,15 +106,15 @@ class RangePredicateFieldSetContextImpl<N, B>
 				case INCLUDED:
 					break;
 			}
-			return new RangePredicateFromContext<N>() {
-				@Override
-				public N to(Object value, RangeBoundInclusion inclusion) {
-					return below( value, inclusion );
-				}
-			};
+			return this;
 		}
 
-		N above(Object value, RangeBoundInclusion inclusion) {
+		@Override
+		public ExplicitEndContext<N> to(Object value, RangeBoundInclusion inclusion) {
+			return below( value, inclusion );
+		}
+
+		ExplicitEndContext<N> above(Object value, RangeBoundInclusion inclusion) {
 			if ( value != null ) {
 				hasNonNullBound = true;
 				getQueryBuilders().forEach( q -> q.lowerLimit( value ) );
@@ -125,10 +127,10 @@ class RangePredicateFieldSetContextImpl<N, B>
 					break;
 			}
 			checkHasNonNullBound();
-			return getNextContextProvider().get();
+			return this;
 		}
 
-		N below(Object value, RangeBoundInclusion inclusion) {
+		ExplicitEndContext<N> below(Object value, RangeBoundInclusion inclusion) {
 			if ( value != null ) {
 				hasNonNullBound = true;
 				getQueryBuilders().forEach( q -> q.upperLimit( value ) );
@@ -141,6 +143,11 @@ class RangePredicateFieldSetContextImpl<N, B>
 					break;
 			}
 			checkHasNonNullBound();
+			return this;
+		}
+
+		@Override
+		public N end() {
 			return getNextContextProvider().get();
 		}
 