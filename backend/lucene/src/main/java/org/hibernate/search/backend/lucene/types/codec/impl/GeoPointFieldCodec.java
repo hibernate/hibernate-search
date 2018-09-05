hibernate/hibernate-search-6-poc

HSEARCH-3105 Separate the formatter in codec and formatter

backend-lucene/src/main/java/org/hibernate/search/v6poc/backend/lucene/types/codec/impl/GeoPointFieldCodec.java
@@ -1,0 +1,108 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.v6poc.backend.lucene.types.codec.impl;
+
+import static org.hibernate.search.v6poc.backend.lucene.document.model.impl.LuceneFields.internalFieldName;
+
+import java.util.Collections;
+import java.util.Objects;
+import java.util.Set;
+
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.LatLonPoint;
+import org.apache.lucene.document.StoredField;
+import org.apache.lucene.index.IndexableField;
+import org.hibernate.search.v6poc.backend.document.model.Store;
+import org.hibernate.search.v6poc.backend.lucene.document.impl.LuceneDocumentBuilder;
+import org.hibernate.search.v6poc.backend.lucene.document.model.impl.LuceneFieldCodec;
+import org.hibernate.search.v6poc.backend.lucene.document.model.impl.LuceneIndexSchemaObjectNode;
+import org.hibernate.search.v6poc.backend.spatial.GeoPoint;
+import org.hibernate.search.v6poc.backend.spatial.ImmutableGeoPoint;
+import org.hibernate.search.v6poc.util.impl.common.CollectionHelper;
+
+public final class GeoPointFieldCodec implements LuceneFieldCodec<GeoPoint> {
+
+	private static final String LATITUDE = "latitude";
+	private static final String LONGITUDE = "longitude";
+
+	private final Store store;
+
+	private final String latitudeFieldName;
+	private final String longitudeFieldName;
+
+	private final Set<String> storedFields;
+
+	public GeoPointFieldCodec(String fieldName, Store store) {
+		this.store = store;
+
+		if ( Store.YES.equals( store ) ) {
+			latitudeFieldName = internalFieldName( fieldName, LATITUDE );
+			longitudeFieldName = internalFieldName( fieldName, LONGITUDE );
+			storedFields = CollectionHelper.asSet( latitudeFieldName, longitudeFieldName );
+		}
+		else {
+			latitudeFieldName = null;
+			longitudeFieldName = null;
+			storedFields = Collections.emptySet();
+		}
+	}
+
+	@Override
+	public void encode(LuceneDocumentBuilder documentBuilder, LuceneIndexSchemaObjectNode parentNode, String fieldName, GeoPoint value) {
+		if ( value == null ) {
+			return;
+		}
+
+		if ( Store.YES.equals( store ) ) {
+			documentBuilder.addField( parentNode, new StoredField( latitudeFieldName, value.getLatitude() ) );
+			documentBuilder.addField( parentNode, new StoredField( longitudeFieldName, value.getLongitude() ) );
+		}
+
+		documentBuilder.addField( parentNode, new LatLonPoint( fieldName, value.getLatitude(), value.getLongitude() ) );
+	}
+
+	@Override
+	public GeoPoint decode(Document document, String fieldName) {
+		IndexableField latitudeField = document.getField( latitudeFieldName );
+		IndexableField longitudeField = document.getField( longitudeFieldName );
+
+		if ( latitudeField == null || longitudeField == null ) {
+			return null;
+		}
+
+		return new ImmutableGeoPoint( (double) latitudeField.numericValue(), (double) longitudeField.numericValue() );
+	}
+
+	@Override
+	public Set<String> getOverriddenStoredFields() {
+		return storedFields;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if ( this == obj ) {
+			return true;
+		}
+		if ( obj == null ) {
+			return false;
+		}
+		if ( GeoPointFieldCodec.class != obj.getClass() ) {
+			return false;
+		}
+
+		GeoPointFieldCodec other = (GeoPointFieldCodec) obj;
+
+		return Objects.equals( store, other.store ) &&
+				Objects.equals( latitudeFieldName, other.latitudeFieldName ) &&
+				Objects.equals( longitudeFieldName, other.longitudeFieldName );
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash( store, latitudeFieldName, longitudeFieldName );
+	}
+}

backend-lucene/src/main/java/org/hibernate/search/v6poc/backend/lucene/types/codec/impl/IntegerFieldCodec.java
@@ -1,0 +1,82 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.v6poc.backend.lucene.types.codec.impl;
+
+import java.util.Objects;
+
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.IntPoint;
+import org.apache.lucene.document.NumericDocValuesField;
+import org.apache.lucene.document.StoredField;
+import org.apache.lucene.index.IndexableField;
+import org.hibernate.search.v6poc.backend.document.model.Sortable;
+import org.hibernate.search.v6poc.backend.document.model.Store;
+import org.hibernate.search.v6poc.backend.lucene.document.impl.LuceneDocumentBuilder;
+import org.hibernate.search.v6poc.backend.lucene.document.model.impl.LuceneFieldCodec;
+import org.hibernate.search.v6poc.backend.lucene.document.model.impl.LuceneIndexSchemaObjectNode;
+
+public final class IntegerFieldCodec implements LuceneFieldCodec<Integer> {
+
+	private final Store store;
+
+	private final Sortable sortable;
+
+	public IntegerFieldCodec(Store store, Sortable sortable) {
+		this.store = store;
+		this.sortable = sortable;
+	}
+
+	@Override
+	public void encode(LuceneDocumentBuilder documentBuilder, LuceneIndexSchemaObjectNode parentNode, String fieldName, Integer value) {
+		if ( value == null ) {
+			return;
+		}
+
+		if ( Store.YES.equals( store ) ) {
+			documentBuilder.addField( parentNode, new StoredField( fieldName, value ) );
+		}
+
+		if ( Sortable.YES.equals( sortable ) ) {
+			documentBuilder.addField( parentNode, new NumericDocValuesField( fieldName, value.longValue() ) );
+		}
+
+		documentBuilder.addField( parentNode, new IntPoint( fieldName, value ) );
+	}
+
+	@Override
+	public Integer decode(Document document, String fieldName) {
+		IndexableField field = document.getField( fieldName );
+
+		if ( field == null ) {
+			return null;
+		}
+
+		return (Integer) field.numericValue();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if ( this == obj ) {
+			return true;
+		}
+		if ( obj == null ) {
+			return false;
+		}
+		if ( IntegerFieldCodec.class != obj.getClass() ) {
+			return false;
+		}
+
+		IntegerFieldCodec other = (IntegerFieldCodec) obj;
+
+		return Objects.equals( store, other.store ) && Objects.equals( sortable, other.sortable );
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash( store, sortable );
+	}
+}

backend-lucene/src/main/java/org/hibernate/search/v6poc/backend/lucene/types/codec/impl/StringFieldCodec.java
@@ -1,0 +1,81 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.v6poc.backend.lucene.types.codec.impl;
+
+import java.util.Objects;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.FieldType;
+import org.apache.lucene.document.SortedDocValuesField;
+import org.apache.lucene.util.BytesRef;
+import org.hibernate.search.v6poc.backend.document.model.Sortable;
+import org.hibernate.search.v6poc.backend.lucene.document.impl.LuceneDocumentBuilder;
+import org.hibernate.search.v6poc.backend.lucene.document.model.impl.LuceneFieldCodec;
+import org.hibernate.search.v6poc.backend.lucene.document.model.impl.LuceneIndexSchemaObjectNode;
+import org.hibernate.search.v6poc.backend.lucene.util.impl.AnalyzerUtils;
+
+public final class StringFieldCodec implements LuceneFieldCodec<String> {
+
+	private final Sortable sortable;
+
+	private final FieldType fieldType;
+
+	private final Analyzer normalizer;
+
+	public StringFieldCodec(Sortable sortable, FieldType fieldType, Analyzer normalizer) {
+		this.sortable = sortable;
+		this.fieldType = fieldType;
+		this.normalizer = normalizer;
+	}
+
+	@Override
+	public void encode(LuceneDocumentBuilder documentBuilder, LuceneIndexSchemaObjectNode parentNode, String fieldName, String value) {
+		if ( value == null ) {
+			return;
+		}
+
+		documentBuilder.addField( parentNode, new Field( fieldName, value, fieldType ) );
+
+		if ( Sortable.YES.equals( sortable ) ) {
+			documentBuilder.addField( parentNode, new SortedDocValuesField(
+					fieldName,
+					new BytesRef( normalizer != null ? AnalyzerUtils.analyzeSortableValue( normalizer, fieldName, value ) : value )
+			) );
+		}
+	}
+
+	@Override
+	public String decode(Document document, String fieldName) {
+		return document.get( fieldName );
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if ( this == obj ) {
+			return true;
+		}
+		if ( obj == null ) {
+			return false;
+		}
+		if ( StringFieldCodec.class != obj.getClass() ) {
+			return false;
+		}
+
+		StringFieldCodec other = (StringFieldCodec) obj;
+
+		return Objects.equals( sortable, other.sortable ) &&
+				Objects.equals( fieldType, other.fieldType ) &&
+				Objects.equals( normalizer, other.normalizer );
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash( sortable, fieldType, normalizer );
+	}
+}

hibernate/hibernate-search

backend/lucene/src/main/java/org/hibernate/search/backend/lucene/types/codec/impl/GeoPointFieldCodec.java
@@ -1,0 +1,108 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.backend.lucene.types.codec.impl;
+
+import static org.hibernate.search.backend.lucene.document.model.impl.LuceneFields.internalFieldName;
+
+import java.util.Collections;
+import java.util.Objects;
+import java.util.Set;
+
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.LatLonPoint;
+import org.apache.lucene.document.StoredField;
+import org.apache.lucene.index.IndexableField;
+import org.hibernate.search.engine.backend.document.model.Store;
+import org.hibernate.search.backend.lucene.document.impl.LuceneDocumentBuilder;
+import org.hibernate.search.backend.lucene.document.model.impl.LuceneFieldCodec;
+import org.hibernate.search.backend.lucene.document.model.impl.LuceneIndexSchemaObjectNode;
+import org.hibernate.search.engine.backend.spatial.GeoPoint;
+import org.hibernate.search.engine.backend.spatial.ImmutableGeoPoint;
+import org.hibernate.search.util.impl.common.CollectionHelper;
+
+public final class GeoPointFieldCodec implements LuceneFieldCodec<GeoPoint> {
+
+	private static final String LATITUDE = "latitude";
+	private static final String LONGITUDE = "longitude";
+
+	private final Store store;
+
+	private final String latitudeFieldName;
+	private final String longitudeFieldName;
+
+	private final Set<String> storedFields;
+
+	public GeoPointFieldCodec(String fieldName, Store store) {
+		this.store = store;
+
+		if ( Store.YES.equals( store ) ) {
+			latitudeFieldName = internalFieldName( fieldName, LATITUDE );
+			longitudeFieldName = internalFieldName( fieldName, LONGITUDE );
+			storedFields = CollectionHelper.asSet( latitudeFieldName, longitudeFieldName );
+		}
+		else {
+			latitudeFieldName = null;
+			longitudeFieldName = null;
+			storedFields = Collections.emptySet();
+		}
+	}
+
+	@Override
+	public void encode(LuceneDocumentBuilder documentBuilder, LuceneIndexSchemaObjectNode parentNode, String fieldName, GeoPoint value) {
+		if ( value == null ) {
+			return;
+		}
+
+		if ( Store.YES.equals( store ) ) {
+			documentBuilder.addField( parentNode, new StoredField( latitudeFieldName, value.getLatitude() ) );
+			documentBuilder.addField( parentNode, new StoredField( longitudeFieldName, value.getLongitude() ) );
+		}
+
+		documentBuilder.addField( parentNode, new LatLonPoint( fieldName, value.getLatitude(), value.getLongitude() ) );
+	}
+
+	@Override
+	public GeoPoint decode(Document document, String fieldName) {
+		IndexableField latitudeField = document.getField( latitudeFieldName );
+		IndexableField longitudeField = document.getField( longitudeFieldName );
+
+		if ( latitudeField == null || longitudeField == null ) {
+			return null;
+		}
+
+		return new ImmutableGeoPoint( (double) latitudeField.numericValue(), (double) longitudeField.numericValue() );
+	}
+
+	@Override
+	public Set<String> getOverriddenStoredFields() {
+		return storedFields;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if ( this == obj ) {
+			return true;
+		}
+		if ( obj == null ) {
+			return false;
+		}
+		if ( GeoPointFieldCodec.class != obj.getClass() ) {
+			return false;
+		}
+
+		GeoPointFieldCodec other = (GeoPointFieldCodec) obj;
+
+		return Objects.equals( store, other.store ) &&
+				Objects.equals( latitudeFieldName, other.latitudeFieldName ) &&
+				Objects.equals( longitudeFieldName, other.longitudeFieldName );
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash( store, latitudeFieldName, longitudeFieldName );
+	}
+}

backend/lucene/src/main/java/org/hibernate/search/backend/lucene/types/codec/impl/IntegerFieldCodec.java
@@ -1,0 +1,82 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.backend.lucene.types.codec.impl;
+
+import java.util.Objects;
+
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.IntPoint;
+import org.apache.lucene.document.NumericDocValuesField;
+import org.apache.lucene.document.StoredField;
+import org.apache.lucene.index.IndexableField;
+import org.hibernate.search.engine.backend.document.model.Sortable;
+import org.hibernate.search.engine.backend.document.model.Store;
+import org.hibernate.search.backend.lucene.document.impl.LuceneDocumentBuilder;
+import org.hibernate.search.backend.lucene.document.model.impl.LuceneFieldCodec;
+import org.hibernate.search.backend.lucene.document.model.impl.LuceneIndexSchemaObjectNode;
+
+public final class IntegerFieldCodec implements LuceneFieldCodec<Integer> {
+
+	private final Store store;
+
+	private final Sortable sortable;
+
+	public IntegerFieldCodec(Store store, Sortable sortable) {
+		this.store = store;
+		this.sortable = sortable;
+	}
+
+	@Override
+	public void encode(LuceneDocumentBuilder documentBuilder, LuceneIndexSchemaObjectNode parentNode, String fieldName, Integer value) {
+		if ( value == null ) {
+			return;
+		}
+
+		if ( Store.YES.equals( store ) ) {
+			documentBuilder.addField( parentNode, new StoredField( fieldName, value ) );
+		}
+
+		if ( Sortable.YES.equals( sortable ) ) {
+			documentBuilder.addField( parentNode, new NumericDocValuesField( fieldName, value.longValue() ) );
+		}
+
+		documentBuilder.addField( parentNode, new IntPoint( fieldName, value ) );
+	}
+
+	@Override
+	public Integer decode(Document document, String fieldName) {
+		IndexableField field = document.getField( fieldName );
+
+		if ( field == null ) {
+			return null;
+		}
+
+		return (Integer) field.numericValue();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if ( this == obj ) {
+			return true;
+		}
+		if ( obj == null ) {
+			return false;
+		}
+		if ( IntegerFieldCodec.class != obj.getClass() ) {
+			return false;
+		}
+
+		IntegerFieldCodec other = (IntegerFieldCodec) obj;
+
+		return Objects.equals( store, other.store ) && Objects.equals( sortable, other.sortable );
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash( store, sortable );
+	}
+}

backend/lucene/src/main/java/org/hibernate/search/backend/lucene/types/codec/impl/StringFieldCodec.java
@@ -1,0 +1,81 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.backend.lucene.types.codec.impl;
+
+import java.util.Objects;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.FieldType;
+import org.apache.lucene.document.SortedDocValuesField;
+import org.apache.lucene.util.BytesRef;
+import org.hibernate.search.engine.backend.document.model.Sortable;
+import org.hibernate.search.backend.lucene.document.impl.LuceneDocumentBuilder;
+import org.hibernate.search.backend.lucene.document.model.impl.LuceneFieldCodec;
+import org.hibernate.search.backend.lucene.document.model.impl.LuceneIndexSchemaObjectNode;
+import org.hibernate.search.backend.lucene.util.impl.AnalyzerUtils;
+
+public final class StringFieldCodec implements LuceneFieldCodec<String> {
+
+	private final Sortable sortable;
+
+	private final FieldType fieldType;
+
+	private final Analyzer normalizer;
+
+	public StringFieldCodec(Sortable sortable, FieldType fieldType, Analyzer normalizer) {
+		this.sortable = sortable;
+		this.fieldType = fieldType;
+		this.normalizer = normalizer;
+	}
+
+	@Override
+	public void encode(LuceneDocumentBuilder documentBuilder, LuceneIndexSchemaObjectNode parentNode, String fieldName, String value) {
+		if ( value == null ) {
+			return;
+		}
+
+		documentBuilder.addField( parentNode, new Field( fieldName, value, fieldType ) );
+
+		if ( Sortable.YES.equals( sortable ) ) {
+			documentBuilder.addField( parentNode, new SortedDocValuesField(
+					fieldName,
+					new BytesRef( normalizer != null ? AnalyzerUtils.analyzeSortableValue( normalizer, fieldName, value ) : value )
+			) );
+		}
+	}
+
+	@Override
+	public String decode(Document document, String fieldName) {
+		return document.get( fieldName );
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if ( this == obj ) {
+			return true;
+		}
+		if ( obj == null ) {
+			return false;
+		}
+		if ( StringFieldCodec.class != obj.getClass() ) {
+			return false;
+		}
+
+		StringFieldCodec other = (StringFieldCodec) obj;
+
+		return Objects.equals( sortable, other.sortable ) &&
+				Objects.equals( fieldType, other.fieldType ) &&
+				Objects.equals( normalizer, other.normalizer );
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash( sortable, fieldType, normalizer );
+	}
+}