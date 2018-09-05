hibernate/hibernate-search-6-poc

HSEARCH-3175 Split the BeanResolver interface into two: the implementor interface, and the user interface

engine/src/main/java/org/hibernate/search/v6poc/engine/impl/BeanProviderImpl.java
@@ -1,0 +1,63 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.v6poc.engine.impl;
+
+import org.hibernate.search.v6poc.engine.spi.BeanProvider;
+import org.hibernate.search.v6poc.engine.spi.BeanReference;
+import org.hibernate.search.v6poc.engine.spi.BeanResolver;
+import org.hibernate.search.v6poc.util.SearchException;
+import org.hibernate.search.v6poc.util.impl.common.StringHelper;
+
+class BeanProviderImpl implements BeanProvider {
+
+	private final BeanResolver beanResolver;
+
+	BeanProviderImpl(BeanResolver beanResolver) {
+		this.beanResolver = beanResolver;
+	}
+
+	@Override
+	public <T> T getBean(Class<?> typeReference, Class<T> expectedClass) {
+		if ( typeReference == null ) {
+			throw new SearchException( "Got an empty bean reference (type is null)" );
+		}
+		return beanResolver.resolve( typeReference, expectedClass );
+	}
+
+	@Override
+	public <T> T getBean(String nameReference, Class<T> expectedClass) {
+		if ( StringHelper.isEmpty( nameReference ) ) {
+			throw new SearchException( "Got an empty bean reference (name is null or empty)" );
+		}
+		return beanResolver.resolve( nameReference, expectedClass );
+	}
+
+	@Override
+	public <T> T getBean(BeanReference reference, Class<T> expectedClass) {
+		if ( reference == null ) {
+			throw new SearchException( "Got an empty bean reference (reference is null)" );
+		}
+
+		String nameReference = reference.getName();
+		Class<?> typeReference = reference.getType();
+		boolean nameProvided = StringHelper.isNotEmpty( nameReference );
+		boolean typeProvided = typeReference != null;
+
+		if ( nameProvided && typeProvided ) {
+			return beanResolver.resolve( nameReference, typeReference, expectedClass );
+		}
+		else if ( nameProvided ) {
+			return beanResolver.resolve( nameReference, expectedClass );
+		}
+		else if ( typeProvided ) {
+			return beanResolver.resolve( typeReference, expectedClass );
+		}
+		else {
+			throw new SearchException( "Got an empty bean reference (no name, no type)" );
+		}
+	}
+}

engine/src/main/java/org/hibernate/search/v6poc/engine/impl/IndexManagerBuildingStateHolder.java
@@ -19,7 +19,7 @@ import org.hibernate.search.v6poc.backend.spi.BackendImplementor;
 import org.hibernate.search.v6poc.backend.spi.BackendFactory;
 import org.hibernate.search.v6poc.cfg.ConfigurationPropertySource;
 import org.hibernate.search.v6poc.cfg.spi.ConfigurationProperty;
-import org.hibernate.search.v6poc.engine.spi.BeanResolver;
+import org.hibernate.search.v6poc.engine.spi.BeanProvider;
 import org.hibernate.search.v6poc.engine.spi.BuildContext;
 import org.hibernate.search.v6poc.entity.mapping.building.impl.RootIndexModelBindingContext;
 import org.hibernate.search.v6poc.entity.mapping.building.spi.IndexManagerBuildingState;
@@ -101,8 +101,8 @@ class IndexManagerBuildingStateHolder {
 		// TODO more checks on the backend type (non-null, non-empty)
 		String backendType = BACKEND_TYPE.get( backendPropertySource ).get();
 
-		BeanResolver beanResolver = buildContext.getServiceManager().getBeanResolver();
-		BackendFactory backendFactory = beanResolver.resolve( backendType, BackendFactory.class );
+		BeanProvider beanProvider = buildContext.getServiceManager().getBeanProvider();
+		BackendFactory backendFactory = beanProvider.getBean( backendType, BackendFactory.class );
 		return backendFactory.create( backendName, buildContext, backendPropertySource );
 	}
 

engine/src/main/java/org/hibernate/search/v6poc/engine/impl/ServiceManagerImpl.java
@@ -6,7 +6,7 @@
  */
 package org.hibernate.search.v6poc.engine.impl;
 
-import org.hibernate.search.v6poc.engine.spi.BeanResolver;
+import org.hibernate.search.v6poc.engine.spi.BeanProvider;
 import org.hibernate.search.v6poc.engine.spi.ServiceManager;
 
 
@@ -15,15 +15,15 @@ import org.hibernate.search.v6poc.engine.spi.ServiceManager;
  */
 public class ServiceManagerImpl implements ServiceManager {
 
-	private final BeanResolver beanResolver;
+	private final BeanProvider beanProvider;
 
-	public ServiceManagerImpl(BeanResolver beanResolver) {
-		this.beanResolver = beanResolver;
+	public ServiceManagerImpl(BeanProvider beanProvider) {
+		this.beanProvider = beanProvider;
 	}
 
 	@Override
-	public BeanResolver getBeanResolver() {
-		return beanResolver;
+	public BeanProvider getBeanProvider() {
+		return beanProvider;
 	}
 
 }

engine/src/main/java/org/hibernate/search/v6poc/engine/spi/BeanProvider.java
@@ -1,0 +1,58 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.v6poc.engine.spi;
+
+
+import org.hibernate.search.v6poc.util.SearchException;
+
+/**
+ * The main entry point for components looking to retrieve user-provided beans.
+ * <p>
+ * Depending on the integration, beans may be resolved using reflection (expecting a no-argument constructor),
+ * or using a more advanced dependency injection context (CDI, Spring DI).
+ * <p>
+ * Regardless of the implementations, this interface is used to retrieve the beans,
+ * referenced either by their name, by their type, or both.
+ * <p>
+ * This interface may be used by any Hibernate Search module,
+ * but should only be implemented by the Hibernate Search engine itself;
+ * if you are looking for implementing your own bean resolver,
+ * you should implement {@link BeanResolver} instead.
+ */
+public interface BeanProvider {
+
+	/**
+	 * Retrieve a bean referenced by its type.
+	 * @param typeReference The type used as a reference to the bean to retrieve.
+	 * @param expectedClass The expected class. Must be non-null. The returned bean will implement this class.
+	 * @param <T> The expected return type.
+	 * @return The resolved bean.
+	 * @throws SearchException if the reference is invalid (null) or the bean cannot be resolved.
+	 */
+	<T> T getBean(Class<?> typeReference, Class<T> expectedClass);
+
+	/**
+	 * Retrieve a bean referenced by its type.
+	 * @param nameReference The name used as a reference to the bean to retrieve. Must be non-null and non-empty.
+	 * @param expectedClass The expected class. Must be non-null. The returned bean will implement this class.
+	 * @param <T> The expected return type.
+	 * @return The resolved bean.
+	 * @throws SearchException if the reference is invalid (null or empty) or the bean cannot be resolved.
+	 */
+	<T> T getBean(String nameReference, Class<T> expectedClass);
+
+	/**
+	 * Retrieve a bean referenced by its type, name, or both, depending on the content of the {@link BeanReference}.
+	 * @param reference The reference to the bean to retrieve. Must be non-null.
+	 * @param expectedClass The expected class. Must be non-null. The returned bean will implement this class.
+	 * @param <T> The expected return type.
+	 * @return The resolved bean.
+	 * @throws SearchException if the reference is invalid (null or empty) or the bean cannot be resolved.
+	 */
+	<T> T getBean(BeanReference reference, Class<T> expectedClass);
+
+}

mapper-pojo/src/main/java/org/hibernate/search/v6poc/entity/pojo/bridge/impl/BeanResolverBridgeBuilder.java
@@ -7,7 +7,7 @@
 package org.hibernate.search.v6poc.entity.pojo.bridge.impl;
 
 import org.hibernate.search.v6poc.engine.spi.BeanReference;
-import org.hibernate.search.v6poc.engine.spi.BeanResolver;
+import org.hibernate.search.v6poc.engine.spi.BeanProvider;
 import org.hibernate.search.v6poc.engine.spi.BuildContext;
 import org.hibernate.search.v6poc.entity.pojo.bridge.mapping.BridgeBuilder;
 
@@ -24,7 +24,7 @@ public class BeanResolverBridgeBuilder<T> implements BridgeBuilder<T> {
 
 	@Override
 	public T build(BuildContext buildContext) {
-		BeanResolver beanResolver = buildContext.getServiceManager().getBeanResolver();
-		return beanResolver.resolve( beanReference, expectedType );
+		BeanProvider beanProvider = buildContext.getServiceManager().getBeanProvider();
+		return beanProvider.getBean( beanReference, expectedType );
 	}
 }

hibernate/hibernate-search

engine/src/main/java/org/hibernate/search/engine/common/impl/BeanProviderImpl.java
@@ -1,0 +1,63 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.engine.common.impl;
+
+import org.hibernate.search.engine.common.spi.BeanProvider;
+import org.hibernate.search.engine.common.spi.BeanReference;
+import org.hibernate.search.engine.common.spi.BeanResolver;
+import org.hibernate.search.util.SearchException;
+import org.hibernate.search.util.impl.common.StringHelper;
+
+class BeanProviderImpl implements BeanProvider {
+
+	private final BeanResolver beanResolver;
+
+	BeanProviderImpl(BeanResolver beanResolver) {
+		this.beanResolver = beanResolver;
+	}
+
+	@Override
+	public <T> T getBean(Class<?> typeReference, Class<T> expectedClass) {
+		if ( typeReference == null ) {
+			throw new SearchException( "Got an empty bean reference (type is null)" );
+		}
+		return beanResolver.resolve( typeReference, expectedClass );
+	}
+
+	@Override
+	public <T> T getBean(String nameReference, Class<T> expectedClass) {
+		if ( StringHelper.isEmpty( nameReference ) ) {
+			throw new SearchException( "Got an empty bean reference (name is null or empty)" );
+		}
+		return beanResolver.resolve( nameReference, expectedClass );
+	}
+
+	@Override
+	public <T> T getBean(BeanReference reference, Class<T> expectedClass) {
+		if ( reference == null ) {
+			throw new SearchException( "Got an empty bean reference (reference is null)" );
+		}
+
+		String nameReference = reference.getName();
+		Class<?> typeReference = reference.getType();
+		boolean nameProvided = StringHelper.isNotEmpty( nameReference );
+		boolean typeProvided = typeReference != null;
+
+		if ( nameProvided && typeProvided ) {
+			return beanResolver.resolve( nameReference, typeReference, expectedClass );
+		}
+		else if ( nameProvided ) {
+			return beanResolver.resolve( nameReference, expectedClass );
+		}
+		else if ( typeProvided ) {
+			return beanResolver.resolve( typeReference, expectedClass );
+		}
+		else {
+			throw new SearchException( "Got an empty bean reference (no name, no type)" );
+		}
+	}
+}

engine/src/main/java/org/hibernate/search/engine/common/impl/IndexManagerBuildingStateHolder.java
@@ -19,7 +19,7 @@ import org.hibernate.search.engine.backend.spi.BackendImplementor;
 import org.hibernate.search.engine.backend.spi.BackendFactory;
 import org.hibernate.search.engine.cfg.ConfigurationPropertySource;
 import org.hibernate.search.engine.cfg.spi.ConfigurationProperty;
-import org.hibernate.search.engine.common.spi.BeanResolver;
+import org.hibernate.search.engine.common.spi.BeanProvider;
 import org.hibernate.search.engine.common.spi.BuildContext;
 import org.hibernate.search.engine.mapper.mapping.building.impl.RootIndexModelBindingContext;
 import org.hibernate.search.engine.mapper.mapping.building.spi.IndexManagerBuildingState;
@@ -101,8 +101,8 @@ class IndexManagerBuildingStateHolder {
 		// TODO more checks on the backend type (non-null, non-empty)
 		String backendType = BACKEND_TYPE.get( backendPropertySource ).get();
 
-		BeanResolver beanResolver = buildContext.getServiceManager().getBeanResolver();
-		BackendFactory backendFactory = beanResolver.resolve( backendType, BackendFactory.class );
+		BeanProvider beanProvider = buildContext.getServiceManager().getBeanProvider();
+		BackendFactory backendFactory = beanProvider.getBean( backendType, BackendFactory.class );
 		return backendFactory.create( backendName, buildContext, backendPropertySource );
 	}
 

engine/src/main/java/org/hibernate/search/engine/common/impl/ServiceManagerImpl.java
@@ -6,7 +6,7 @@
  */
 package org.hibernate.search.engine.common.impl;
 
-import org.hibernate.search.engine.common.spi.BeanResolver;
+import org.hibernate.search.engine.common.spi.BeanProvider;
 import org.hibernate.search.engine.common.spi.ServiceManager;
 
 
@@ -15,15 +15,15 @@ import org.hibernate.search.engine.common.spi.ServiceManager;
  */
 public class ServiceManagerImpl implements ServiceManager {
 
-	private final BeanResolver beanResolver;
+	private final BeanProvider beanProvider;
 
-	public ServiceManagerImpl(BeanResolver beanResolver) {
-		this.beanResolver = beanResolver;
+	public ServiceManagerImpl(BeanProvider beanProvider) {
+		this.beanProvider = beanProvider;
 	}
 
 	@Override
-	public BeanResolver getBeanResolver() {
-		return beanResolver;
+	public BeanProvider getBeanProvider() {
+		return beanProvider;
 	}
 
 }

engine/src/main/java/org/hibernate/search/engine/common/spi/BeanProvider.java
@@ -1,0 +1,58 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.engine.common.spi;
+
+
+import org.hibernate.search.util.SearchException;
+
+/**
+ * The main entry point for components looking to retrieve user-provided beans.
+ * <p>
+ * Depending on the integration, beans may be resolved using reflection (expecting a no-argument constructor),
+ * or using a more advanced dependency injection context (CDI, Spring DI).
+ * <p>
+ * Regardless of the implementations, this interface is used to retrieve the beans,
+ * referenced either by their name, by their type, or both.
+ * <p>
+ * This interface may be used by any Hibernate Search module,
+ * but should only be implemented by the Hibernate Search engine itself;
+ * if you are looking for implementing your own bean resolver,
+ * you should implement {@link BeanResolver} instead.
+ */
+public interface BeanProvider {
+
+	/**
+	 * Retrieve a bean referenced by its type.
+	 * @param typeReference The type used as a reference to the bean to retrieve.
+	 * @param expectedClass The expected class. Must be non-null. The returned bean will implement this class.
+	 * @param <T> The expected return type.
+	 * @return The resolved bean.
+	 * @throws SearchException if the reference is invalid (null) or the bean cannot be resolved.
+	 */
+	<T> T getBean(Class<?> typeReference, Class<T> expectedClass);
+
+	/**
+	 * Retrieve a bean referenced by its type.
+	 * @param nameReference The name used as a reference to the bean to retrieve. Must be non-null and non-empty.
+	 * @param expectedClass The expected class. Must be non-null. The returned bean will implement this class.
+	 * @param <T> The expected return type.
+	 * @return The resolved bean.
+	 * @throws SearchException if the reference is invalid (null or empty) or the bean cannot be resolved.
+	 */
+	<T> T getBean(String nameReference, Class<T> expectedClass);
+
+	/**
+	 * Retrieve a bean referenced by its type, name, or both, depending on the content of the {@link BeanReference}.
+	 * @param reference The reference to the bean to retrieve. Must be non-null.
+	 * @param expectedClass The expected class. Must be non-null. The returned bean will implement this class.
+	 * @param <T> The expected return type.
+	 * @return The resolved bean.
+	 * @throws SearchException if the reference is invalid (null or empty) or the bean cannot be resolved.
+	 */
+	<T> T getBean(BeanReference reference, Class<T> expectedClass);
+
+}

mapper/pojo/src/main/java/org/hibernate/search/mapper/pojo/bridge/impl/BeanResolverBridgeBuilder.java
@@ -7,7 +7,7 @@
 package org.hibernate.search.mapper.pojo.bridge.impl;
 
 import org.hibernate.search.engine.common.spi.BeanReference;
-import org.hibernate.search.engine.common.spi.BeanResolver;
+import org.hibernate.search.engine.common.spi.BeanProvider;
 import org.hibernate.search.engine.common.spi.BuildContext;
 import org.hibernate.search.mapper.pojo.bridge.mapping.BridgeBuilder;
 
@@ -24,7 +24,7 @@ public class BeanResolverBridgeBuilder<T> implements BridgeBuilder<T> {
 
 	@Override
 	public T build(BuildContext buildContext) {
-		BeanResolver beanResolver = buildContext.getServiceManager().getBeanResolver();
-		return beanResolver.resolve( beanReference, expectedType );
+		BeanProvider beanProvider = buildContext.getServiceManager().getBeanProvider();
+		return beanProvider.getBean( beanReference, expectedType );
 	}
 }