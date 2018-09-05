hibernate/hibernate-search-6-poc

HSEARCH-2546 Test and fix exceptions thrown when setting analyzers/normalizers in the index model definition DSL during bootstrapping

backend-elasticsearch/src/main/java/org/hibernate/search/v6poc/backend/elasticsearch/types/dsl/impl/AbstractElasticsearchIndexSchemaFieldTypedContext.java
@@ -6,6 +6,7 @@
  */
 package org.hibernate.search.v6poc.backend.elasticsearch.types.dsl.impl;
 
+import org.hibernate.search.v6poc.backend.document.model.dsl.spi.IndexSchemaContext;
 import org.hibernate.search.v6poc.backend.document.spi.DeferredInitializationIndexFieldAccessor;
 import org.hibernate.search.v6poc.backend.document.IndexFieldAccessor;
 import org.hibernate.search.v6poc.backend.elasticsearch.document.model.dsl.ElasticsearchIndexSchemaFieldTypedContext;
@@ -21,7 +22,13 @@ public abstract class AbstractElasticsearchIndexSchemaFieldTypedContext<T>
 		implements ElasticsearchIndexSchemaFieldTypedContext<T>,
 		ElasticsearchIndexSchemaNodeContributor<PropertyMapping> {
 
-	private DeferredInitializationIndexFieldAccessor<T> accessor = new DeferredInitializationIndexFieldAccessor<>();
+	protected final IndexSchemaContext schemaContext;
+
+	private final DeferredInitializationIndexFieldAccessor<T> accessor = new DeferredInitializationIndexFieldAccessor<>();
+
+	AbstractElasticsearchIndexSchemaFieldTypedContext(IndexSchemaContext schemaContext) {
+		this.schemaContext = schemaContext;
+	}
 
 	@Override
 	public IndexFieldAccessor<T> createAccessor() {

backend-elasticsearch/src/main/java/org/hibernate/search/v6poc/backend/elasticsearch/types/dsl/impl/AbstractScalarFieldTypedContext.java
@@ -8,6 +8,7 @@ package org.hibernate.search.v6poc.backend.elasticsearch.types.dsl.impl;
 
 import java.lang.invoke.MethodHandles;
 
+import org.hibernate.search.v6poc.backend.document.model.dsl.spi.IndexSchemaContext;
 import org.hibernate.search.v6poc.backend.document.spi.DeferredInitializationIndexFieldAccessor;
 import org.hibernate.search.v6poc.backend.document.model.dsl.Sortable;
 import org.hibernate.search.v6poc.backend.document.model.dsl.Store;
@@ -33,7 +34,9 @@ abstract class AbstractScalarFieldTypedContext<T> extends AbstractElasticsearchI
 	private Store store = Store.DEFAULT;
 	private Sortable sortable = Sortable.DEFAULT;
 
-	public AbstractScalarFieldTypedContext(String relativeFieldName, DataType dataType) {
+	AbstractScalarFieldTypedContext(IndexSchemaContext schemaContext,
+			String relativeFieldName, DataType dataType) {
+		super( schemaContext );
 		this.relativeFieldName = relativeFieldName;
 		this.dataType = dataType;
 	}
@@ -72,10 +75,10 @@ abstract class AbstractScalarFieldTypedContext<T> extends AbstractElasticsearchI
 		mapping.setType( dataType );
 
 		if ( analyzerName != null ) {
-			throw log.cannotUseAnalyzerOnFieldType( parentNode.getAbsolutePath( relativeFieldName ), dataType );
+			throw log.cannotUseAnalyzerOnFieldType( relativeFieldName, dataType, schemaContext.getFailureContext() );
 		}
 		if ( normalizerName != null ) {
-			throw log.cannotUseNormalizerOnFieldType( parentNode.getAbsolutePath( relativeFieldName ), dataType );
+			throw log.cannotUseNormalizerOnFieldType( relativeFieldName, dataType, schemaContext.getFailureContext() );
 		}
 
 		switch ( store ) {

backend-lucene/src/main/java/org/hibernate/search/v6poc/backend/lucene/document/model/dsl/impl/LuceneIndexSchemaFieldContextImpl.java
@@ -7,9 +7,12 @@
 package org.hibernate.search.v6poc.backend.lucene.document.model.dsl.impl;
 
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 
 import org.hibernate.search.v6poc.backend.document.model.dsl.IndexSchemaFieldTerminalContext;
 import org.hibernate.search.v6poc.backend.document.model.dsl.IndexSchemaFieldTypedContext;
+import org.hibernate.search.v6poc.backend.document.model.dsl.spi.IndexSchemaContext;
 import org.hibernate.search.v6poc.backend.lucene.document.model.LuceneFieldContributor;
 import org.hibernate.search.v6poc.backend.lucene.document.model.LuceneFieldValueExtractor;
 import org.hibernate.search.v6poc.backend.lucene.document.model.dsl.LuceneIndexSchemaFieldContext;
@@ -21,6 +24,9 @@ import org.hibernate.search.v6poc.backend.lucene.types.dsl.impl.IntegerIndexSche
 import org.hibernate.search.v6poc.backend.lucene.types.dsl.impl.LocalDateIndexSchemaFieldContext;
 import org.hibernate.search.v6poc.backend.lucene.types.dsl.impl.LuceneFieldIndexSchemaFieldContext;
 import org.hibernate.search.v6poc.backend.lucene.types.dsl.impl.StringIndexSchemaFieldContext;
+import org.hibernate.search.v6poc.backend.lucene.util.impl.LuceneFields;
+import org.hibernate.search.v6poc.logging.spi.FailureContextElement;
+import org.hibernate.search.v6poc.logging.spi.FailureContexts;
 import org.hibernate.search.v6poc.spatial.GeoPoint;
 import org.hibernate.search.v6poc.util.SearchException;
 import org.hibernate.search.v6poc.util.impl.common.Contracts;
@@ -29,14 +35,20 @@ import org.hibernate.search.v6poc.util.impl.common.Contracts;
 /**
  * @author Guillaume Smet
  */
-public class LuceneIndexSchemaFieldContextImpl implements LuceneIndexSchemaFieldContext, LuceneIndexSchemaNodeContributor {
+class LuceneIndexSchemaFieldContextImpl
+		implements LuceneIndexSchemaFieldContext, LuceneIndexSchemaNodeContributor, IndexSchemaContext {
 
+	private final AbstractLuceneIndexSchemaObjectNodeBuilder parent;
 	private final String relativeFieldName;
+	private final String absoluteFieldPath;
 
 	private LuceneIndexSchemaNodeContributor delegate;
 
-	public LuceneIndexSchemaFieldContextImpl(String relativeFieldName) {
+	LuceneIndexSchemaFieldContextImpl(AbstractLuceneIndexSchemaObjectNodeBuilder parent,
+			String relativeFieldName) {
+		this.parent = parent;
 		this.relativeFieldName = relativeFieldName;
+		this.absoluteFieldPath = LuceneFields.compose( parent.getAbsolutePath(), relativeFieldName );
 	}
 
 	@Override
@@ -62,22 +74,22 @@ public class LuceneIndexSchemaFieldContextImpl implements LuceneIndexSchemaField
 
 	@Override
 	public IndexSchemaFieldTypedContext<String> asString() {
-		return setDelegate( new StringIndexSchemaFieldContext( relativeFieldName ) );
+		return setDelegate( new StringIndexSchemaFieldContext( this, relativeFieldName ) );
 	}
 
 	@Override
 	public IndexSchemaFieldTypedContext<Integer> asInteger() {
-		return setDelegate( new IntegerIndexSchemaFieldContext( relativeFieldName ) );
+		return setDelegate( new IntegerIndexSchemaFieldContext( this, relativeFieldName ) );
 	}
 
 	@Override
 	public IndexSchemaFieldTypedContext<LocalDate> asLocalDate() {
-		return setDelegate( new LocalDateIndexSchemaFieldContext( relativeFieldName ) );
+		return setDelegate( new LocalDateIndexSchemaFieldContext( this, relativeFieldName ) );
 	}
 
 	@Override
 	public IndexSchemaFieldTypedContext<GeoPoint> asGeoPoint() {
-		return setDelegate( new GeoPointIndexSchemaFieldContext( relativeFieldName ) );
+		return setDelegate( new GeoPointIndexSchemaFieldContext( this, relativeFieldName ) );
 	}
 
 	@Override
@@ -90,7 +102,17 @@ public class LuceneIndexSchemaFieldContextImpl implements LuceneIndexSchemaField
 	@Override
 	public <V> IndexSchemaFieldTerminalContext<V> asLuceneField(LuceneFieldContributor<V> fieldContributor,
 			LuceneFieldValueExtractor<V> fieldValueExtractor) {
-		return setDelegate( new LuceneFieldIndexSchemaFieldContext<>( relativeFieldName, fieldContributor, fieldValueExtractor ) );
+		return setDelegate( new LuceneFieldIndexSchemaFieldContext<>(
+				this, relativeFieldName, fieldContributor, fieldValueExtractor
+		) );
+	}
+
+	@Override
+	public List<FailureContextElement> getFailureContext() {
+		return Arrays.asList(
+				parent.getRootNodeBuilder().getIndexFailureContextElement(),
+				FailureContexts.fromIndexFieldAbsolutePath( absoluteFieldPath )
+		);
 	}
 
 	private <T extends LuceneIndexSchemaNodeContributor> T setDelegate(T context) {

backend-lucene/src/main/java/org/hibernate/search/v6poc/backend/lucene/types/dsl/impl/AbstractLuceneIndexSchemaFieldTypedContext.java
@@ -10,6 +10,7 @@ import java.lang.invoke.MethodHandles;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.hibernate.search.v6poc.backend.document.IndexFieldAccessor;
+import org.hibernate.search.v6poc.backend.document.model.dsl.spi.IndexSchemaContext;
 import org.hibernate.search.v6poc.backend.document.spi.DeferredInitializationIndexFieldAccessor;
 import org.hibernate.search.v6poc.backend.document.model.dsl.IndexSchemaFieldTypedContext;
 import org.hibernate.search.v6poc.backend.document.model.dsl.Sortable;
@@ -29,13 +30,15 @@ public abstract class AbstractLuceneIndexSchemaFieldTypedContext<T>
 
 	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
 
-	private DeferredInitializationIndexFieldAccessor<T> accessor = new DeferredInitializationIndexFieldAccessor<>();
-
+	private final IndexSchemaContext schemaContext;
 	private final String relativeFieldName;
+
+	private final DeferredInitializationIndexFieldAccessor<T> accessor = new DeferredInitializationIndexFieldAccessor<>();
 
 	private Store store;
 
-	protected AbstractLuceneIndexSchemaFieldTypedContext(String relativeFieldName) {
+	protected AbstractLuceneIndexSchemaFieldTypedContext(IndexSchemaContext schemaContext, String relativeFieldName) {
+		this.schemaContext = schemaContext;
 		this.relativeFieldName = relativeFieldName;
 	}
 
@@ -65,12 +68,12 @@ public abstract class AbstractLuceneIndexSchemaFieldTypedContext<T>
 
 	@Override
 	public IndexSchemaFieldTypedContext<T> analyzer(String analyzerName) {
-		throw log.cannotUseAnalyzerOnFieldType( relativeFieldName );
+		throw log.cannotUseAnalyzerOnFieldType( relativeFieldName, schemaContext.getFailureContext() );
 	}
 
 	@Override
 	public IndexSchemaFieldTypedContext<T> normalizer(String normalizerName) {
-		throw log.cannotUseNormalizerOnFieldType( relativeFieldName );
+		throw log.cannotUseNormalizerOnFieldType( relativeFieldName, schemaContext.getFailureContext() );
 	}
 
 	protected String getRelativeFieldName() {

backend-lucene/src/main/java/org/hibernate/search/v6poc/backend/lucene/types/dsl/impl/LuceneFieldIndexSchemaFieldContext.java
@@ -7,6 +7,8 @@
 package org.hibernate.search.v6poc.backend.lucene.types.dsl.impl;
 
 import org.apache.lucene.index.IndexableField;
+
+import org.hibernate.search.v6poc.backend.document.model.dsl.spi.IndexSchemaContext;
 import org.hibernate.search.v6poc.backend.document.spi.DeferredInitializationIndexFieldAccessor;
 import org.hibernate.search.v6poc.backend.lucene.document.impl.LuceneIndexFieldAccessor;
 import org.hibernate.search.v6poc.backend.lucene.document.model.LuceneFieldContributor;
@@ -26,9 +28,9 @@ public class LuceneFieldIndexSchemaFieldContext<V, F extends IndexableField> ext
 
 	private LuceneFieldValueExtractor<V> fieldValueExtractor;
 
-	public LuceneFieldIndexSchemaFieldContext(String relativeFieldName, LuceneFieldContributor<V> fieldContributor,
-			LuceneFieldValueExtractor<V> fieldValueExtractor) {
-		super( relativeFieldName );
+	public LuceneFieldIndexSchemaFieldContext(IndexSchemaContext schemaContext, String relativeFieldName,
+			LuceneFieldContributor<V> fieldContributor, LuceneFieldValueExtractor<V> fieldValueExtractor) {
+		super( schemaContext, relativeFieldName );
 
 		this.fieldContributor = fieldContributor;
 		this.fieldValueExtractor = fieldValueExtractor;

hibernate/hibernate-search

backend/elasticsearch/src/main/java/org/hibernate/search/backend/elasticsearch/types/dsl/impl/AbstractElasticsearchIndexSchemaFieldTypedContext.java
@@ -6,6 +6,7 @@
  */
 package org.hibernate.search.backend.elasticsearch.types.dsl.impl;
 
+import org.hibernate.search.engine.backend.document.model.dsl.spi.IndexSchemaContext;
 import org.hibernate.search.engine.backend.document.spi.DeferredInitializationIndexFieldAccessor;
 import org.hibernate.search.engine.backend.document.IndexFieldAccessor;
 import org.hibernate.search.backend.elasticsearch.document.model.dsl.ElasticsearchIndexSchemaFieldTypedContext;
@@ -21,7 +22,13 @@ public abstract class AbstractElasticsearchIndexSchemaFieldTypedContext<T>
 		implements ElasticsearchIndexSchemaFieldTypedContext<T>,
 		ElasticsearchIndexSchemaNodeContributor<PropertyMapping> {
 
-	private DeferredInitializationIndexFieldAccessor<T> accessor = new DeferredInitializationIndexFieldAccessor<>();
+	protected final IndexSchemaContext schemaContext;
+
+	private final DeferredInitializationIndexFieldAccessor<T> accessor = new DeferredInitializationIndexFieldAccessor<>();
+
+	AbstractElasticsearchIndexSchemaFieldTypedContext(IndexSchemaContext schemaContext) {
+		this.schemaContext = schemaContext;
+	}
 
 	@Override
 	public IndexFieldAccessor<T> createAccessor() {

backend/elasticsearch/src/main/java/org/hibernate/search/backend/elasticsearch/types/dsl/impl/AbstractScalarFieldTypedContext.java
@@ -8,6 +8,7 @@ package org.hibernate.search.backend.elasticsearch.types.dsl.impl;
 
 import java.lang.invoke.MethodHandles;
 
+import org.hibernate.search.engine.backend.document.model.dsl.spi.IndexSchemaContext;
 import org.hibernate.search.engine.backend.document.spi.DeferredInitializationIndexFieldAccessor;
 import org.hibernate.search.engine.backend.document.model.dsl.Sortable;
 import org.hibernate.search.engine.backend.document.model.dsl.Store;
@@ -33,7 +34,9 @@ abstract class AbstractScalarFieldTypedContext<T> extends AbstractElasticsearchI
 	private Store store = Store.DEFAULT;
 	private Sortable sortable = Sortable.DEFAULT;
 
-	public AbstractScalarFieldTypedContext(String relativeFieldName, DataType dataType) {
+	AbstractScalarFieldTypedContext(IndexSchemaContext schemaContext,
+			String relativeFieldName, DataType dataType) {
+		super( schemaContext );
 		this.relativeFieldName = relativeFieldName;
 		this.dataType = dataType;
 	}
@@ -72,10 +75,10 @@ abstract class AbstractScalarFieldTypedContext<T> extends AbstractElasticsearchI
 		mapping.setType( dataType );
 
 		if ( analyzerName != null ) {
-			throw log.cannotUseAnalyzerOnFieldType( parentNode.getAbsolutePath( relativeFieldName ), dataType );
+			throw log.cannotUseAnalyzerOnFieldType( relativeFieldName, dataType, schemaContext.getFailureContext() );
 		}
 		if ( normalizerName != null ) {
-			throw log.cannotUseNormalizerOnFieldType( parentNode.getAbsolutePath( relativeFieldName ), dataType );
+			throw log.cannotUseNormalizerOnFieldType( relativeFieldName, dataType, schemaContext.getFailureContext() );
 		}
 
 		switch ( store ) {

backend/lucene/src/main/java/org/hibernate/search/backend/lucene/document/model/dsl/impl/LuceneIndexSchemaFieldContextImpl.java
@@ -7,9 +7,12 @@
 package org.hibernate.search.backend.lucene.document.model.dsl.impl;
 
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 
 import org.hibernate.search.engine.backend.document.model.dsl.IndexSchemaFieldTerminalContext;
 import org.hibernate.search.engine.backend.document.model.dsl.IndexSchemaFieldTypedContext;
+import org.hibernate.search.engine.backend.document.model.dsl.spi.IndexSchemaContext;
 import org.hibernate.search.backend.lucene.document.model.LuceneFieldContributor;
 import org.hibernate.search.backend.lucene.document.model.LuceneFieldValueExtractor;
 import org.hibernate.search.backend.lucene.document.model.dsl.LuceneIndexSchemaFieldContext;
@@ -21,6 +24,9 @@ import org.hibernate.search.backend.lucene.types.dsl.impl.IntegerIndexSchemaFiel
 import org.hibernate.search.backend.lucene.types.dsl.impl.LocalDateIndexSchemaFieldContext;
 import org.hibernate.search.backend.lucene.types.dsl.impl.LuceneFieldIndexSchemaFieldContext;
 import org.hibernate.search.backend.lucene.types.dsl.impl.StringIndexSchemaFieldContext;
+import org.hibernate.search.backend.lucene.util.impl.LuceneFields;
+import org.hibernate.search.engine.logging.spi.FailureContextElement;
+import org.hibernate.search.engine.logging.spi.FailureContexts;
 import org.hibernate.search.engine.spatial.GeoPoint;
 import org.hibernate.search.util.SearchException;
 import org.hibernate.search.util.impl.common.Contracts;
@@ -29,14 +35,20 @@ import org.hibernate.search.util.impl.common.Contracts;
 /**
  * @author Guillaume Smet
  */
-public class LuceneIndexSchemaFieldContextImpl implements LuceneIndexSchemaFieldContext, LuceneIndexSchemaNodeContributor {
+class LuceneIndexSchemaFieldContextImpl
+		implements LuceneIndexSchemaFieldContext, LuceneIndexSchemaNodeContributor, IndexSchemaContext {
 
+	private final AbstractLuceneIndexSchemaObjectNodeBuilder parent;
 	private final String relativeFieldName;
+	private final String absoluteFieldPath;
 
 	private LuceneIndexSchemaNodeContributor delegate;
 
-	public LuceneIndexSchemaFieldContextImpl(String relativeFieldName) {
+	LuceneIndexSchemaFieldContextImpl(AbstractLuceneIndexSchemaObjectNodeBuilder parent,
+			String relativeFieldName) {
+		this.parent = parent;
 		this.relativeFieldName = relativeFieldName;
+		this.absoluteFieldPath = LuceneFields.compose( parent.getAbsolutePath(), relativeFieldName );
 	}
 
 	@Override
@@ -62,22 +74,22 @@ public class LuceneIndexSchemaFieldContextImpl implements LuceneIndexSchemaField
 
 	@Override
 	public IndexSchemaFieldTypedContext<String> asString() {
-		return setDelegate( new StringIndexSchemaFieldContext( relativeFieldName ) );
+		return setDelegate( new StringIndexSchemaFieldContext( this, relativeFieldName ) );
 	}
 
 	@Override
 	public IndexSchemaFieldTypedContext<Integer> asInteger() {
-		return setDelegate( new IntegerIndexSchemaFieldContext( relativeFieldName ) );
+		return setDelegate( new IntegerIndexSchemaFieldContext( this, relativeFieldName ) );
 	}
 
 	@Override
 	public IndexSchemaFieldTypedContext<LocalDate> asLocalDate() {
-		return setDelegate( new LocalDateIndexSchemaFieldContext( relativeFieldName ) );
+		return setDelegate( new LocalDateIndexSchemaFieldContext( this, relativeFieldName ) );
 	}
 
 	@Override
 	public IndexSchemaFieldTypedContext<GeoPoint> asGeoPoint() {
-		return setDelegate( new GeoPointIndexSchemaFieldContext( relativeFieldName ) );
+		return setDelegate( new GeoPointIndexSchemaFieldContext( this, relativeFieldName ) );
 	}
 
 	@Override
@@ -90,7 +102,17 @@ public class LuceneIndexSchemaFieldContextImpl implements LuceneIndexSchemaField
 	@Override
 	public <V> IndexSchemaFieldTerminalContext<V> asLuceneField(LuceneFieldContributor<V> fieldContributor,
 			LuceneFieldValueExtractor<V> fieldValueExtractor) {
-		return setDelegate( new LuceneFieldIndexSchemaFieldContext<>( relativeFieldName, fieldContributor, fieldValueExtractor ) );
+		return setDelegate( new LuceneFieldIndexSchemaFieldContext<>(
+				this, relativeFieldName, fieldContributor, fieldValueExtractor
+		) );
+	}
+
+	@Override
+	public List<FailureContextElement> getFailureContext() {
+		return Arrays.asList(
+				parent.getRootNodeBuilder().getIndexFailureContextElement(),
+				FailureContexts.fromIndexFieldAbsolutePath( absoluteFieldPath )
+		);
 	}
 
 	private <T extends LuceneIndexSchemaNodeContributor> T setDelegate(T context) {

backend/lucene/src/main/java/org/hibernate/search/backend/lucene/types/dsl/impl/AbstractLuceneIndexSchemaFieldTypedContext.java
@@ -10,6 +10,7 @@ import java.lang.invoke.MethodHandles;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.hibernate.search.engine.backend.document.IndexFieldAccessor;
+import org.hibernate.search.engine.backend.document.model.dsl.spi.IndexSchemaContext;
 import org.hibernate.search.engine.backend.document.spi.DeferredInitializationIndexFieldAccessor;
 import org.hibernate.search.engine.backend.document.model.dsl.IndexSchemaFieldTypedContext;
 import org.hibernate.search.engine.backend.document.model.dsl.Sortable;
@@ -29,13 +30,15 @@ public abstract class AbstractLuceneIndexSchemaFieldTypedContext<T>
 
 	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
 
-	private DeferredInitializationIndexFieldAccessor<T> accessor = new DeferredInitializationIndexFieldAccessor<>();
-
+	private final IndexSchemaContext schemaContext;
 	private final String relativeFieldName;
+
+	private final DeferredInitializationIndexFieldAccessor<T> accessor = new DeferredInitializationIndexFieldAccessor<>();
 
 	private Store store;
 
-	protected AbstractLuceneIndexSchemaFieldTypedContext(String relativeFieldName) {
+	protected AbstractLuceneIndexSchemaFieldTypedContext(IndexSchemaContext schemaContext, String relativeFieldName) {
+		this.schemaContext = schemaContext;
 		this.relativeFieldName = relativeFieldName;
 	}
 
@@ -65,12 +68,12 @@ public abstract class AbstractLuceneIndexSchemaFieldTypedContext<T>
 
 	@Override
 	public IndexSchemaFieldTypedContext<T> analyzer(String analyzerName) {
-		throw log.cannotUseAnalyzerOnFieldType( relativeFieldName );
+		throw log.cannotUseAnalyzerOnFieldType( relativeFieldName, schemaContext.getFailureContext() );
 	}
 
 	@Override
 	public IndexSchemaFieldTypedContext<T> normalizer(String normalizerName) {
-		throw log.cannotUseNormalizerOnFieldType( relativeFieldName );
+		throw log.cannotUseNormalizerOnFieldType( relativeFieldName, schemaContext.getFailureContext() );
 	}
 
 	protected String getRelativeFieldName() {

backend/lucene/src/main/java/org/hibernate/search/backend/lucene/types/dsl/impl/LuceneFieldIndexSchemaFieldContext.java
@@ -7,6 +7,8 @@
 package org.hibernate.search.backend.lucene.types.dsl.impl;
 
 import org.apache.lucene.index.IndexableField;
+
+import org.hibernate.search.engine.backend.document.model.dsl.spi.IndexSchemaContext;
 import org.hibernate.search.engine.backend.document.spi.DeferredInitializationIndexFieldAccessor;
 import org.hibernate.search.backend.lucene.document.impl.LuceneIndexFieldAccessor;
 import org.hibernate.search.backend.lucene.document.model.LuceneFieldContributor;
@@ -26,9 +28,9 @@ public class LuceneFieldIndexSchemaFieldContext<V, F extends IndexableField> ext
 
 	private LuceneFieldValueExtractor<V> fieldValueExtractor;
 
-	public LuceneFieldIndexSchemaFieldContext(String relativeFieldName, LuceneFieldContributor<V> fieldContributor,
-			LuceneFieldValueExtractor<V> fieldValueExtractor) {
-		super( relativeFieldName );
+	public LuceneFieldIndexSchemaFieldContext(IndexSchemaContext schemaContext, String relativeFieldName,
+			LuceneFieldContributor<V> fieldContributor, LuceneFieldValueExtractor<V> fieldValueExtractor) {
+		super( schemaContext, relativeFieldName );
 
 		this.fieldContributor = fieldContributor;
 		this.fieldValueExtractor = fieldValueExtractor;