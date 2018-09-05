hibernate/hibernate-search-6-poc

HSEARCH-2546 Allow to throw exceptions with context during backend bootstrapping

engine/src/main/java/org/hibernate/search/v6poc/logging/impl/RootFailureCollector.java
@@ -17,6 +17,7 @@ import java.util.StringJoiner;
 import org.hibernate.search.v6poc.logging.spi.ContextualFailureCollector;
 import org.hibernate.search.v6poc.logging.spi.FailureCollector;
 import org.hibernate.search.v6poc.logging.spi.FailureContextElement;
+import org.hibernate.search.v6poc.logging.spi.SearchExceptionWithContext;
 import org.hibernate.search.v6poc.util.impl.common.LoggerFactory;
 import org.hibernate.search.v6poc.util.impl.common.ToStringStyle;
 import org.hibernate.search.v6poc.util.impl.common.ToStringTreeBuilder;
@@ -136,7 +137,7 @@ public class RootFailureCollector implements FailureCollector {
 		private final FailureCollectorImpl parent;
 		private final FailureContextElement context;
 
-		private List<Throwable> failures;
+		private List<String> failureMessages;
 
 		private ContextualFailureCollectorImpl(FailureCollectorImpl parent, FailureContextElement context) {
 			super( parent );
@@ -146,7 +147,7 @@ public class RootFailureCollector implements FailureCollector {
 
 		@Override
 		public boolean hasFailure() {
-			if ( failures != null && !failures.isEmpty() ) {
+			if ( failureMessages != null && !failureMessages.isEmpty() ) {
 				return true;
 			}
 			for ( ContextualFailureCollectorImpl child : getChildren().values() ) {
@@ -159,16 +160,18 @@ public class RootFailureCollector implements FailureCollector {
 
 		@Override
 		public void add(Throwable t) {
-			if ( failures == null ) {
-				failures = new ArrayList<>();
+			if ( t instanceof SearchExceptionWithContext ) {
+				SearchExceptionWithContext e = (SearchExceptionWithContext) t;
+				ContextualFailureCollectorImpl failureCollector = this;
+				for ( FailureContextElement contextElement : e.getContextElements() ) {
+					failureCollector = failureCollector.withContext( contextElement );
+				}
+				// Do not include the context in the failure message, since we will render it as part of the failure report
+				failureCollector.doAdd( e, e.getMessageWithoutContext() );
 			}
-			failures.add( t );
-
-			StringJoiner contextJoiner = new StringJoiner( MESSAGES.contextSeparator() );
-			appendContextTo( contextJoiner );
-			log.newBootstrapCollectedFailure( contextJoiner.toString(), t );
-
-			root.onAddFailure();
+			else {
+				doAdd( t, t.getMessage() );
+			}
 		}
 
 		@Override
@@ -180,15 +183,28 @@ public class RootFailureCollector implements FailureCollector {
 		@Override
 		void appendFailuresTo(ToStringTreeBuilder builder) {
 			builder.startObject( context.render() );
-			if ( failures != null ) {
+			if ( failureMessages != null ) {
 				builder.startList( MESSAGES.failures() );
-				for ( Throwable failure : failures ) {
-					builder.value( failure.getMessage() );
+				for ( String failureMessage : failureMessages ) {
+					builder.value( failureMessage );
 				}
 				builder.endList();
 			}
 			appendChildrenFailuresTo( builder );
 			builder.endObject();
+		}
+
+		private void doAdd(Throwable failure, String failureMessage) {
+			if ( failureMessages == null ) {
+				failureMessages = new ArrayList<>();
+			}
+			failureMessages.add( failureMessage );
+
+			StringJoiner contextJoiner = new StringJoiner( MESSAGES.contextSeparator() );
+			appendContextTo( contextJoiner );
+			log.newBootstrapCollectedFailure( contextJoiner.toString(), failure );
+
+			root.onAddFailure();
 		}
 	}
 

engine/src/main/java/org/hibernate/search/v6poc/logging/spi/SearchExceptionWithContext.java
@@ -1,0 +1,38 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.v6poc.logging.spi;
+
+import java.util.List;
+
+import org.hibernate.search.v6poc.util.SearchException;
+
+public class SearchExceptionWithContext extends SearchException {
+	private final String messageWithoutContext;
+	private final List<FailureContextElement> contextElements;
+
+	public SearchExceptionWithContext(String message, Throwable cause, List<FailureContextElement> contextElements) {
+		super( message + FailureContexts.renderForException( contextElements ), cause );
+		this.messageWithoutContext = message;
+		this.contextElements = contextElements;
+	}
+
+	public SearchExceptionWithContext(String message, List<FailureContextElement> contextElements) {
+		this( message, null, contextElements );
+	}
+
+	public SearchExceptionWithContext(Throwable cause, List<FailureContextElement> contextElements) {
+		this( null, cause, contextElements );
+	}
+
+	public String getMessageWithoutContext() {
+		return messageWithoutContext;
+	}
+
+	public List<FailureContextElement> getContextElements() {
+		return contextElements;
+	}
+}

engine/src/main/java/org/hibernate/search/v6poc/entity/mapping/spi/MappingBuildContext.java
@@ -8,6 +8,7 @@ package org.hibernate.search.v6poc.entity.mapping.spi;
 
 import org.hibernate.search.v6poc.engine.spi.ServiceManager;
 import org.hibernate.search.v6poc.logging.spi.ContextualFailureCollector;
+import org.hibernate.search.v6poc.logging.spi.SearchExceptionWithContext;
 
 /**
  * A build context for mappings.
@@ -21,6 +22,12 @@ public interface MappingBuildContext {
 	 * that something went wrong and bootstrap should be aborted at some point,
 	 * while still continuing the bootstrap process for some time to collect other errors
 	 * that could be relevant to users.
+	 * <p>
+	 * Note that this is primarily intended for mapper implementations.
+	 * Backend implementations should use the collector with caution:
+	 * in many cases it's a better idea to throw a {@link SearchExceptionWithContext}
+	 * so that the mapper can catch it and add it to the failure collector,
+	 * optionally prepending some context of its own.
 	 *
 	 * @return A failure collector.
 	 */

integrationtest/mapper-pojo/src/test/java/org/hibernate/search/v6poc/integrationtest/mapper/pojo/JavaBeanMappingFailureReportIT.java
@@ -7,11 +7,14 @@
 package org.hibernate.search.v6poc.integrationtest.mapper.pojo;
 
 import java.lang.invoke.MethodHandles;
+import java.util.Arrays;
 
 import org.hibernate.search.v6poc.entity.pojo.mapping.definition.annotation.DocumentId;
 import org.hibernate.search.v6poc.entity.pojo.mapping.definition.annotation.Field;
 import org.hibernate.search.v6poc.entity.pojo.mapping.definition.annotation.Indexed;
 import org.hibernate.search.v6poc.integrationtest.mapper.pojo.test.util.rule.JavaBeanMappingSetupHelper;
+import org.hibernate.search.v6poc.logging.spi.FailureContexts;
+import org.hibernate.search.v6poc.logging.spi.SearchExceptionWithContext;
 import org.hibernate.search.v6poc.util.SearchException;
 import org.hibernate.search.v6poc.util.impl.integrationtest.common.rule.BackendMock;
 import org.hibernate.search.v6poc.util.impl.test.ExceptionMatcherBuilder;
@@ -251,6 +254,82 @@ public class JavaBeanMappingFailureReportIT {
 								+ "            path '.myProperty2': \n"
 								+ "                failures: \n"
 								+ "                  - " + field2FailureMessage
+				);
+	}
+
+	@Test
+	public void failuresFromBackend() {
+		final String indexName = "indexName";
+		@Indexed(index = indexName)
+		class IndexedEntity {
+			Integer id;
+			Integer myProperty;
+			@DocumentId
+			public Integer getId() {
+				return id;
+			}
+			@Field(name = "failingField1")
+			@Field(name = "failingField2")
+			public Integer getMyProperty() {
+				return myProperty;
+			}
+		}
+		String field1FailureMessage = "This is the failure message for field 1";
+		String field2FailureMessage = "This is the failure message for field 2";
+
+		backendMock.expectFailingField(
+				indexName, "failingField1",
+				() -> new SearchExceptionWithContext(
+						field1FailureMessage,
+						Arrays.asList(
+								FailureContexts.fromIndexName( indexName ),
+								FailureContexts.fromIndexFieldAbsolutePath( "failingField1" )
+						)
+				)
+		);
+		backendMock.expectFailingField(
+				indexName, "failingField2", () -> new SearchExceptionWithContext(
+						field2FailureMessage,
+						Arrays.asList(
+								FailureContexts.fromIndexName( indexName ),
+								FailureContexts.fromIndexFieldAbsolutePath( "failingField2" )
+						)
+				)
+		);
+
+		logged.expectEvent(
+				Level.ERROR,
+				ExceptionMatcherBuilder.isException( SearchExceptionWithContext.class )
+						.withMessage( field1FailureMessage ).build(),
+				FAILURE_LOG_INTRODUCTION
+						+ "JavaBean mapping, type '" + IndexedEntity.class.getName() + "', path '.myProperty',"
+						+ " index '" + indexName + "', field 'failingField1'\n"
+		);
+		logged.expectEvent(
+				Level.ERROR,
+				ExceptionMatcherBuilder.isException( SearchExceptionWithContext.class )
+						.withMessage( field2FailureMessage ).build(),
+				FAILURE_LOG_INTRODUCTION
+						+ "JavaBean mapping, type '" + IndexedEntity.class.getName() + "', path '.myProperty',"
+						+ " index '" + indexName + "', field 'failingField2'\n"
+		);
+
+		SubTest.expectException(
+				() -> setupHelper.withBackendMock( backendMock ).setup( IndexedEntity.class )
+		)
+				.assertThrown()
+				.isInstanceOf( SearchException.class )
+				.hasMessage(
+						FAILURE_REPORT_INTRODUCTION
+								+ "        type '" + IndexedEntity.class.getName() + "': \n"
+								+ "            path '.myProperty': \n"
+								+ "                index '" + indexName + "': \n"
+								+ "                    field 'failingField1': \n"
+								+ "                        failures: \n"
+								+ "                          - " + field1FailureMessage + "\n"
+								+ "                    field 'failingField2': \n"
+								+ "                        failures: \n"
+								+ "                          - " + field2FailureMessage
 				);
 	}
 

hibernate/hibernate-search

engine/src/main/java/org/hibernate/search/engine/logging/impl/RootFailureCollector.java
@@ -17,6 +17,7 @@ import java.util.StringJoiner;
 import org.hibernate.search.engine.logging.spi.ContextualFailureCollector;
 import org.hibernate.search.engine.logging.spi.FailureCollector;
 import org.hibernate.search.engine.logging.spi.FailureContextElement;
+import org.hibernate.search.engine.logging.spi.SearchExceptionWithContext;
 import org.hibernate.search.util.impl.common.LoggerFactory;
 import org.hibernate.search.util.impl.common.ToStringStyle;
 import org.hibernate.search.util.impl.common.ToStringTreeBuilder;
@@ -136,7 +137,7 @@ public class RootFailureCollector implements FailureCollector {
 		private final FailureCollectorImpl parent;
 		private final FailureContextElement context;
 
-		private List<Throwable> failures;
+		private List<String> failureMessages;
 
 		private ContextualFailureCollectorImpl(FailureCollectorImpl parent, FailureContextElement context) {
 			super( parent );
@@ -146,7 +147,7 @@ public class RootFailureCollector implements FailureCollector {
 
 		@Override
 		public boolean hasFailure() {
-			if ( failures != null && !failures.isEmpty() ) {
+			if ( failureMessages != null && !failureMessages.isEmpty() ) {
 				return true;
 			}
 			for ( ContextualFailureCollectorImpl child : getChildren().values() ) {
@@ -159,16 +160,18 @@ public class RootFailureCollector implements FailureCollector {
 
 		@Override
 		public void add(Throwable t) {
-			if ( failures == null ) {
-				failures = new ArrayList<>();
+			if ( t instanceof SearchExceptionWithContext ) {
+				SearchExceptionWithContext e = (SearchExceptionWithContext) t;
+				ContextualFailureCollectorImpl failureCollector = this;
+				for ( FailureContextElement contextElement : e.getContextElements() ) {
+					failureCollector = failureCollector.withContext( contextElement );
+				}
+				// Do not include the context in the failure message, since we will render it as part of the failure report
+				failureCollector.doAdd( e, e.getMessageWithoutContext() );
 			}
-			failures.add( t );
-
-			StringJoiner contextJoiner = new StringJoiner( MESSAGES.contextSeparator() );
-			appendContextTo( contextJoiner );
-			log.newBootstrapCollectedFailure( contextJoiner.toString(), t );
-
-			root.onAddFailure();
+			else {
+				doAdd( t, t.getMessage() );
+			}
 		}
 
 		@Override
@@ -180,15 +183,28 @@ public class RootFailureCollector implements FailureCollector {
 		@Override
 		void appendFailuresTo(ToStringTreeBuilder builder) {
 			builder.startObject( context.render() );
-			if ( failures != null ) {
+			if ( failureMessages != null ) {
 				builder.startList( MESSAGES.failures() );
-				for ( Throwable failure : failures ) {
-					builder.value( failure.getMessage() );
+				for ( String failureMessage : failureMessages ) {
+					builder.value( failureMessage );
 				}
 				builder.endList();
 			}
 			appendChildrenFailuresTo( builder );
 			builder.endObject();
+		}
+
+		private void doAdd(Throwable failure, String failureMessage) {
+			if ( failureMessages == null ) {
+				failureMessages = new ArrayList<>();
+			}
+			failureMessages.add( failureMessage );
+
+			StringJoiner contextJoiner = new StringJoiner( MESSAGES.contextSeparator() );
+			appendContextTo( contextJoiner );
+			log.newBootstrapCollectedFailure( contextJoiner.toString(), failure );
+
+			root.onAddFailure();
 		}
 	}
 

engine/src/main/java/org/hibernate/search/engine/logging/spi/SearchExceptionWithContext.java
@@ -1,0 +1,38 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.engine.logging.spi;
+
+import java.util.List;
+
+import org.hibernate.search.util.SearchException;
+
+public class SearchExceptionWithContext extends SearchException {
+	private final String messageWithoutContext;
+	private final List<FailureContextElement> contextElements;
+
+	public SearchExceptionWithContext(String message, Throwable cause, List<FailureContextElement> contextElements) {
+		super( message + FailureContexts.renderForException( contextElements ), cause );
+		this.messageWithoutContext = message;
+		this.contextElements = contextElements;
+	}
+
+	public SearchExceptionWithContext(String message, List<FailureContextElement> contextElements) {
+		this( message, null, contextElements );
+	}
+
+	public SearchExceptionWithContext(Throwable cause, List<FailureContextElement> contextElements) {
+		this( null, cause, contextElements );
+	}
+
+	public String getMessageWithoutContext() {
+		return messageWithoutContext;
+	}
+
+	public List<FailureContextElement> getContextElements() {
+		return contextElements;
+	}
+}

engine/src/main/java/org/hibernate/search/engine/mapper/mapping/spi/MappingBuildContext.java
@@ -8,6 +8,7 @@ package org.hibernate.search.engine.mapper.mapping.spi;
 
 import org.hibernate.search.engine.common.spi.ServiceManager;
 import org.hibernate.search.engine.logging.spi.ContextualFailureCollector;
+import org.hibernate.search.engine.logging.spi.SearchExceptionWithContext;
 
 /**
  * A build context for mappings.
@@ -21,6 +22,12 @@ public interface MappingBuildContext {
 	 * that something went wrong and bootstrap should be aborted at some point,
 	 * while still continuing the bootstrap process for some time to collect other errors
 	 * that could be relevant to users.
+	 * <p>
+	 * Note that this is primarily intended for mapper implementations.
+	 * Backend implementations should use the collector with caution:
+	 * in many cases it's a better idea to throw a {@link SearchExceptionWithContext}
+	 * so that the mapper can catch it and add it to the failure collector,
+	 * optionally prepending some context of its own.
 	 *
 	 * @return A failure collector.
 	 */

integrationtest/mapper/pojo/src/test/java/org/hibernate/search/integrationtest/mapper/pojo/JavaBeanMappingFailureReportIT.java
@@ -7,11 +7,14 @@
 package org.hibernate.search.integrationtest.mapper.pojo;
 
 import java.lang.invoke.MethodHandles;
+import java.util.Arrays;
 
 import org.hibernate.search.mapper.pojo.mapping.definition.annotation.DocumentId;
 import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Field;
 import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
 import org.hibernate.search.integrationtest.mapper.pojo.test.util.rule.JavaBeanMappingSetupHelper;
+import org.hibernate.search.engine.logging.spi.FailureContexts;
+import org.hibernate.search.engine.logging.spi.SearchExceptionWithContext;
 import org.hibernate.search.util.SearchException;
 import org.hibernate.search.util.impl.integrationtest.common.rule.BackendMock;
 import org.hibernate.search.util.impl.test.ExceptionMatcherBuilder;
@@ -251,6 +254,82 @@ public class JavaBeanMappingFailureReportIT {
 								+ "            path '.myProperty2': \n"
 								+ "                failures: \n"
 								+ "                  - " + field2FailureMessage
+				);
+	}
+
+	@Test
+	public void failuresFromBackend() {
+		final String indexName = "indexName";
+		@Indexed(index = indexName)
+		class IndexedEntity {
+			Integer id;
+			Integer myProperty;
+			@DocumentId
+			public Integer getId() {
+				return id;
+			}
+			@Field(name = "failingField1")
+			@Field(name = "failingField2")
+			public Integer getMyProperty() {
+				return myProperty;
+			}
+		}
+		String field1FailureMessage = "This is the failure message for field 1";
+		String field2FailureMessage = "This is the failure message for field 2";
+
+		backendMock.expectFailingField(
+				indexName, "failingField1",
+				() -> new SearchExceptionWithContext(
+						field1FailureMessage,
+						Arrays.asList(
+								FailureContexts.fromIndexName( indexName ),
+								FailureContexts.fromIndexFieldAbsolutePath( "failingField1" )
+						)
+				)
+		);
+		backendMock.expectFailingField(
+				indexName, "failingField2", () -> new SearchExceptionWithContext(
+						field2FailureMessage,
+						Arrays.asList(
+								FailureContexts.fromIndexName( indexName ),
+								FailureContexts.fromIndexFieldAbsolutePath( "failingField2" )
+						)
+				)
+		);
+
+		logged.expectEvent(
+				Level.ERROR,
+				ExceptionMatcherBuilder.isException( SearchExceptionWithContext.class )
+						.withMessage( field1FailureMessage ).build(),
+				FAILURE_LOG_INTRODUCTION
+						+ "JavaBean mapping, type '" + IndexedEntity.class.getName() + "', path '.myProperty',"
+						+ " index '" + indexName + "', field 'failingField1'\n"
+		);
+		logged.expectEvent(
+				Level.ERROR,
+				ExceptionMatcherBuilder.isException( SearchExceptionWithContext.class )
+						.withMessage( field2FailureMessage ).build(),
+				FAILURE_LOG_INTRODUCTION
+						+ "JavaBean mapping, type '" + IndexedEntity.class.getName() + "', path '.myProperty',"
+						+ " index '" + indexName + "', field 'failingField2'\n"
+		);
+
+		SubTest.expectException(
+				() -> setupHelper.withBackendMock( backendMock ).setup( IndexedEntity.class )
+		)
+				.assertThrown()
+				.isInstanceOf( SearchException.class )
+				.hasMessage(
+						FAILURE_REPORT_INTRODUCTION
+								+ "        type '" + IndexedEntity.class.getName() + "': \n"
+								+ "            path '.myProperty': \n"
+								+ "                index '" + indexName + "': \n"
+								+ "                    field 'failingField1': \n"
+								+ "                        failures: \n"
+								+ "                          - " + field1FailureMessage + "\n"
+								+ "                    field 'failingField2': \n"
+								+ "                        failures: \n"
+								+ "                          - " + field2FailureMessage
 				);
 	}
 