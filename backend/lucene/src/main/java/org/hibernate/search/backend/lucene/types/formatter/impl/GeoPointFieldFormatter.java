hibernate/hibernate-search-6-poc

HSEARCH-3094 Throw exceptions for match/range queries on GeoPoint
Note that, in the case of Lucene, we could support exact match queries
by using a PointRangeQuery (see LatLonPoint for how to use it) but, for
the time being, we chose to be consistent with Elasticsearch.

backend-lucene/src/main/java/org/hibernate/search/v6poc/backend/lucene/types/formatter/impl/GeoPointFieldFormatter.java
@@ -6,7 +6,9 @@
  */
 package org.hibernate.search.v6poc.backend.lucene.types.formatter.impl;
 
-public final class GeoPointFieldFormatter implements LuceneFieldFormatter<Void> {
+import org.hibernate.search.v6poc.backend.spatial.GeoPoint;
+
+public final class GeoPointFieldFormatter implements LuceneFieldFormatter<GeoPoint> {
 
 	public static final GeoPointFieldFormatter INSTANCE = new GeoPointFieldFormatter();
 
@@ -14,8 +16,7 @@ public final class GeoPointFieldFormatter implements LuceneFieldFormatter<Void> 
 	}
 
 	@Override
-	public Void format(Object value) {
-		// TODO see what we should do here.
-		throw new UnsupportedOperationException( "format() not supported for GeoPoint" );
+	public GeoPoint format(Object value) {
+		return (GeoPoint) value;
 	}
 }

hibernate/hibernate-search

backend/lucene/src/main/java/org/hibernate/search/backend/lucene/types/formatter/impl/GeoPointFieldFormatter.java
@@ -6,7 +6,9 @@
  */
 package org.hibernate.search.backend.lucene.types.formatter.impl;
 
-public final class GeoPointFieldFormatter implements LuceneFieldFormatter<Void> {
+import org.hibernate.search.engine.backend.spatial.GeoPoint;
+
+public final class GeoPointFieldFormatter implements LuceneFieldFormatter<GeoPoint> {
 
 	public static final GeoPointFieldFormatter INSTANCE = new GeoPointFieldFormatter();
 
@@ -14,8 +16,7 @@ public final class GeoPointFieldFormatter implements LuceneFieldFormatter<Void> 
 	}
 
 	@Override
-	public Void format(Object value) {
-		// TODO see what we should do here.
-		throw new UnsupportedOperationException( "format() not supported for GeoPoint" );
+	public GeoPoint format(Object value) {
+		return (GeoPoint) value;
 	}
 }