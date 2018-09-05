yrodiere/hibernate-search-6-poc

HSEARCH-3221 Add a generic parameter to Elasticsearch codecs and schema field nodes
This will be useful in the next commits, when we move the responsibility
of casting input values from the DSL to another class.

Also, it's more consistent with the Lucene backend.

backend-elasticsearch/src/main/java/org/hibernate/search/v6poc/backend/elasticsearch/types/predicate/impl/GeoPointSpatialWithinBoundingBoxPredicateBuilder.java
@@ -13,6 +13,7 @@ import org.hibernate.search.v6poc.backend.elasticsearch.search.predicate.impl.El
 import org.hibernate.search.v6poc.backend.elasticsearch.types.codec.impl.ElasticsearchFieldCodec;
 import org.hibernate.search.v6poc.search.predicate.spi.SpatialWithinBoundingBoxPredicateBuilder;
 import org.hibernate.search.v6poc.spatial.GeoBoundingBox;
+import org.hibernate.search.v6poc.spatial.GeoPoint;
 
 import com.google.gson.JsonObject;
 
@@ -27,9 +28,9 @@ class GeoPointSpatialWithinBoundingBoxPredicateBuilder extends AbstractSearchPre
 
 	private final String absoluteFieldPath;
 
-	private final ElasticsearchFieldCodec codec;
+	private final ElasticsearchFieldCodec<GeoPoint> codec;
 
-	GeoPointSpatialWithinBoundingBoxPredicateBuilder(String absoluteFieldPath, ElasticsearchFieldCodec codec) {
+	GeoPointSpatialWithinBoundingBoxPredicateBuilder(String absoluteFieldPath, ElasticsearchFieldCodec<GeoPoint> codec) {
 		this.absoluteFieldPath = absoluteFieldPath;
 		this.codec = codec;
 	}

hibernate/hibernate-search

backend/elasticsearch/src/main/java/org/hibernate/search/backend/elasticsearch/types/predicate/impl/GeoPointSpatialWithinBoundingBoxPredicateBuilder.java
@@ -13,6 +13,7 @@ import org.hibernate.search.backend.elasticsearch.search.predicate.impl.Elastics
 import org.hibernate.search.backend.elasticsearch.types.codec.impl.ElasticsearchFieldCodec;
 import org.hibernate.search.engine.search.predicate.spi.SpatialWithinBoundingBoxPredicateBuilder;
 import org.hibernate.search.engine.spatial.GeoBoundingBox;
+import org.hibernate.search.engine.spatial.GeoPoint;
 
 import com.google.gson.JsonObject;
 
@@ -27,9 +28,9 @@ class GeoPointSpatialWithinBoundingBoxPredicateBuilder extends AbstractSearchPre
 
 	private final String absoluteFieldPath;
 
-	private final ElasticsearchFieldCodec codec;
+	private final ElasticsearchFieldCodec<GeoPoint> codec;
 
-	GeoPointSpatialWithinBoundingBoxPredicateBuilder(String absoluteFieldPath, ElasticsearchFieldCodec codec) {
+	GeoPointSpatialWithinBoundingBoxPredicateBuilder(String absoluteFieldPath, ElasticsearchFieldCodec<GeoPoint> codec) {
 		this.absoluteFieldPath = absoluteFieldPath;
 		this.codec = codec;
 	}