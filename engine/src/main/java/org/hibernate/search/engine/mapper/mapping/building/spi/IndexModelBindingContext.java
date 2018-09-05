yrodiere/hibernate-search-6-poc

HSEARCH-3221 Use dedicated interfaces for the binding context passed to bridges
Because:

1. We will very probably need to expose different ways of defining
the functions to apply when bridges are enabled in the search DSL.
2. Even if we don't, there's simply too much uncertainty here, and
having a dedicated interface to expose parameters to the bind function
will allow us more flexibility in the future (we'll be able to add
parameters without breaking existing user code).
3. Exposing the "SearchModel" class from the engine to bridges directly
is a bad idea, as it means users won't be able to use POJO concepts
when manipulating the SearchModel. So we need an interface defined in
the POJO model.

engine/src/main/java/org/hibernate/search/v6poc/entity/mapping/building/spi/IndexModelBindingContext.java
@@ -13,8 +13,8 @@ import java.util.Set;
 import org.hibernate.search.v6poc.backend.document.IndexObjectFieldAccessor;
 import org.hibernate.search.v6poc.backend.document.model.dsl.IndexSchemaElement;
 import org.hibernate.search.v6poc.backend.document.model.dsl.ObjectFieldStorage;
-import org.hibernate.search.v6poc.entity.model.SearchModel;
 import org.hibernate.search.v6poc.entity.model.spi.MappableTypeModel;
+import org.hibernate.search.v6poc.entity.model.spi.SearchModel;
 
 public interface IndexModelBindingContext {
 

hibernate/hibernate-search

engine/src/main/java/org/hibernate/search/engine/mapper/mapping/building/spi/IndexModelBindingContext.java
@@ -13,8 +13,8 @@ import java.util.Set;
 import org.hibernate.search.engine.backend.document.IndexObjectFieldAccessor;
 import org.hibernate.search.engine.backend.document.model.dsl.IndexSchemaElement;
 import org.hibernate.search.engine.backend.document.model.dsl.ObjectFieldStorage;
-import org.hibernate.search.engine.mapper.model.SearchModel;
 import org.hibernate.search.engine.mapper.model.spi.MappableTypeModel;
+import org.hibernate.search.engine.mapper.model.spi.SearchModel;
 
 public interface IndexModelBindingContext {
 