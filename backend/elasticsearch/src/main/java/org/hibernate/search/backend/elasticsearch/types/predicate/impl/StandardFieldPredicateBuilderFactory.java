hibernate/hibernate-search-6-poc

HSEARCH-3087 Add support for within circle spatial predicates

backend-elasticsearch/src/main/java/org/hibernate/search/v6poc/backend/elasticsearch/types/predicate/impl/StandardFieldPredicateBuilderFactory.java
@@ -6,15 +6,22 @@
  */
 package org.hibernate.search.v6poc.backend.elasticsearch.types.predicate.impl;
 
+import java.lang.invoke.MethodHandles;
+
+import org.hibernate.search.v6poc.backend.elasticsearch.logging.impl.Log;
 import org.hibernate.search.v6poc.backend.elasticsearch.search.predicate.impl.ElasticsearchSearchPredicateCollector;
 import org.hibernate.search.v6poc.backend.elasticsearch.search.predicate.impl.MatchPredicateBuilderImpl;
 import org.hibernate.search.v6poc.backend.elasticsearch.search.predicate.impl.RangePredicateBuilderImpl;
 import org.hibernate.search.v6poc.backend.elasticsearch.types.codec.impl.ElasticsearchFieldCodec;
 import org.hibernate.search.v6poc.search.predicate.spi.MatchPredicateBuilder;
 import org.hibernate.search.v6poc.search.predicate.spi.RangePredicateBuilder;
+import org.hibernate.search.v6poc.search.predicate.spi.SpatialWithinCirclePredicateBuilder;
+import org.hibernate.search.v6poc.util.impl.common.LoggerFactory;
 
 
 public class StandardFieldPredicateBuilderFactory implements ElasticsearchFieldPredicateBuilderFactory {
+
+	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
 
 	private final ElasticsearchFieldCodec codec;
 
@@ -30,5 +37,10 @@ public class StandardFieldPredicateBuilderFactory implements ElasticsearchFieldP
 	@Override
 	public RangePredicateBuilder<ElasticsearchSearchPredicateCollector> createRangePredicateBuilder(String absoluteFieldPath) {
 		return new RangePredicateBuilderImpl( absoluteFieldPath, codec );
+	}
+
+	@Override
+	public SpatialWithinCirclePredicateBuilder<ElasticsearchSearchPredicateCollector> createSpatialWithinCirclePredicateBuilder(String absoluteFieldPath) {
+		throw log.spatialPredicatesNotSupportedByFieldType( absoluteFieldPath );
 	}
 }

backend-lucene/src/main/java/org/hibernate/search/v6poc/backend/lucene/search/predicate/impl/AbstractSpatialWithinCirclePredicateBuilder.java
@@ -1,0 +1,31 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.v6poc.backend.lucene.search.predicate.impl;
+
+import org.hibernate.search.v6poc.backend.spatial.GeoPoint;
+import org.hibernate.search.v6poc.search.predicate.spi.SpatialWithinCirclePredicateBuilder;
+
+
+public abstract class AbstractSpatialWithinCirclePredicateBuilder<T> extends AbstractSearchPredicateBuilder
+		implements SpatialWithinCirclePredicateBuilder<LuceneSearchPredicateCollector> {
+
+	protected final String absoluteFieldPath;
+
+	protected GeoPoint center;
+
+	protected double radiusInMeters;
+
+	protected AbstractSpatialWithinCirclePredicateBuilder(String absoluteFieldPath) {
+		this.absoluteFieldPath = absoluteFieldPath;
+	}
+
+	@Override
+	public void circle(GeoPoint center, double radiusInMeters) {
+		this.center = center;
+		this.radiusInMeters = radiusInMeters;
+	}
+}

backend-lucene/src/main/java/org/hibernate/search/v6poc/backend/lucene/types/predicate/impl/LuceneFieldPredicateBuilderFactory.java
@@ -9,6 +9,7 @@ package org.hibernate.search.v6poc.backend.lucene.types.predicate.impl;
 import org.hibernate.search.v6poc.backend.lucene.search.predicate.impl.LuceneSearchPredicateCollector;
 import org.hibernate.search.v6poc.search.predicate.spi.MatchPredicateBuilder;
 import org.hibernate.search.v6poc.search.predicate.spi.RangePredicateBuilder;
+import org.hibernate.search.v6poc.search.predicate.spi.SpatialWithinCirclePredicateBuilder;
 
 /**
  * @author Guillaume Smet
@@ -18,6 +19,8 @@ public interface LuceneFieldPredicateBuilderFactory {
 	MatchPredicateBuilder<LuceneSearchPredicateCollector> createMatchPredicateBuilder(String absoluteFieldPath);
 
 	RangePredicateBuilder<LuceneSearchPredicateCollector> createRangePredicateBuilder(String absoluteFieldPath);
+
+	SpatialWithinCirclePredicateBuilder<LuceneSearchPredicateCollector> createSpatialWithinCirclePredicateBuilder(String absoluteFieldPath);
 
 	// equals()/hashCode() needs to be implemented if the predicate factory is not a singleton
 

engine/src/main/java/org/hibernate/search/v6poc/search/dsl/predicate/impl/SpatialPredicateContextImpl.java
@@ -1,0 +1,40 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.v6poc.search.dsl.predicate.impl;
+
+import java.util.function.Supplier;
+
+import org.hibernate.search.v6poc.search.dsl.predicate.SpatialPredicateContext;
+import org.hibernate.search.v6poc.search.dsl.predicate.SpatialWithinPredicateContext;
+import org.hibernate.search.v6poc.search.predicate.spi.SearchPredicateContributor;
+import org.hibernate.search.v6poc.search.predicate.spi.SearchPredicateFactory;
+
+class SpatialPredicateContextImpl<N, C> implements SpatialPredicateContext<N>, SearchPredicateContributor<C> {
+
+	private final SearchPredicateFactory<C> factory;
+
+	private final Supplier<N> nextContextProvider;
+
+	private SearchPredicateContributor<C> child;
+
+	SpatialPredicateContextImpl(SearchPredicateFactory<C> factory, Supplier<N> nextContextProvider) {
+		this.factory = factory;
+		this.nextContextProvider = nextContextProvider;
+	}
+
+	@Override
+	public SpatialWithinPredicateContext<N> within() {
+		SpatialWithinPredicateContextImpl<N, C> child = new SpatialWithinPredicateContextImpl<>( factory, nextContextProvider );
+		this.child = child;
+		return child;
+	}
+
+	@Override
+	public void contribute(C collector) {
+		child.contribute( collector );
+	}
+}

engine/src/main/java/org/hibernate/search/v6poc/search/dsl/predicate/impl/SpatialWithinCirclePredicateContextImpl.java
@@ -1,0 +1,25 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.v6poc.search.dsl.predicate.impl;
+
+import java.util.function.Supplier;
+
+import org.hibernate.search.v6poc.search.dsl.predicate.SpatialWithinCirclePredicateContext;
+
+public class SpatialWithinCirclePredicateContextImpl<N> implements SpatialWithinCirclePredicateContext<N> {
+
+	private final Supplier<N> nextContextProvider;
+
+	SpatialWithinCirclePredicateContextImpl(Supplier<N> nextContextProvider) {
+		this.nextContextProvider = nextContextProvider;
+	}
+
+	@Override
+	public N end() {
+		return nextContextProvider.get();
+	}
+}

integrationtest/backend-tck/src/main/java/org/hibernate/search/v6poc/integrationtest/backend/tck/search/predicate/spatial/AbstractSpatialWithinSearchPredicateIT.java
@@ -1,0 +1,120 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.v6poc.integrationtest.backend.tck.search.predicate.spatial;
+
+import static org.hibernate.search.v6poc.util.impl.integrationtest.common.assertion.DocumentReferencesSearchResultAssert.assertThat;
+import static org.hibernate.search.v6poc.util.impl.integrationtest.common.stub.mapper.StubMapperUtils.referenceProvider;
+
+import org.hibernate.search.v6poc.backend.document.DocumentElement;
+import org.hibernate.search.v6poc.backend.document.IndexFieldAccessor;
+import org.hibernate.search.v6poc.backend.document.model.dsl.IndexSchemaElement;
+import org.hibernate.search.v6poc.backend.index.spi.ChangesetIndexWorker;
+import org.hibernate.search.v6poc.backend.index.spi.IndexManager;
+import org.hibernate.search.v6poc.backend.index.spi.IndexSearchTarget;
+import org.hibernate.search.v6poc.backend.spatial.GeoPoint;
+import org.hibernate.search.v6poc.backend.spatial.ImmutableGeoPoint;
+import org.hibernate.search.v6poc.engine.spi.SessionContext;
+import org.hibernate.search.v6poc.integrationtest.backend.tck.util.rule.SearchSetupHelper;
+import org.hibernate.search.v6poc.search.DocumentReference;
+import org.hibernate.search.v6poc.search.SearchQuery;
+import org.hibernate.search.v6poc.util.impl.integrationtest.common.stub.StubSessionContext;
+import org.junit.Before;
+import org.junit.Rule;
+
+public abstract class AbstractSpatialWithinSearchPredicateIT {
+
+	protected static final String OURSON_QUI_BOIT_ID = "ourson qui boit";
+	protected static final GeoPoint OURSON_QUI_BOIT_GEO_POINT = new ImmutableGeoPoint( 45.7705687,4.835233 );
+	protected static final String OURSON_QUI_BOIT_STRING = "L'ourson qui boit";
+
+	protected static final String IMOUTO_ID = "imouto";
+	protected static final GeoPoint IMOUTO_GEO_POINT = new ImmutableGeoPoint( 45.7541719, 4.8386221 );
+	protected static final String IMOUTO_STRING = "Imouto";
+
+	protected static final String CHEZ_MARGOTTE_ID = "chez margotte";
+	protected static final GeoPoint CHEZ_MARGOTTE_GEO_POINT = new ImmutableGeoPoint( 45.7530374, 4.8510299 );
+	protected static final String CHEZ_MARGOTTE_STRING = "Chez Margotte";
+
+	protected static final String EMPTY_ID = "empty";
+
+	@Rule
+	public SearchSetupHelper setupHelper = new SearchSetupHelper();
+
+	private IndexAccessors indexAccessors;
+	protected IndexManager<?> indexManager;
+	protected String indexName;
+	protected SessionContext sessionContext = new StubSessionContext();
+
+	@Before
+	public void setup() {
+		setupHelper.withDefaultConfiguration()
+				.withIndex(
+						"MappedType", "IndexName",
+						ctx -> this.indexAccessors = new IndexAccessors( ctx.getSchemaElement() ),
+						(indexManager, indexName) -> {
+							this.indexManager = indexManager;
+							this.indexName = indexName;
+						}
+				)
+				.setup();
+
+		initData();
+	}
+
+	private void initData() {
+		ChangesetIndexWorker<? extends DocumentElement> worker = indexManager.createWorker( sessionContext );
+		worker.add( referenceProvider( OURSON_QUI_BOIT_ID ), document -> {
+			indexAccessors.string.write( document, OURSON_QUI_BOIT_STRING );
+			indexAccessors.geoPoint.write( document, OURSON_QUI_BOIT_GEO_POINT );
+			indexAccessors.geoPoint_1.write( document, new ImmutableGeoPoint( OURSON_QUI_BOIT_GEO_POINT.getLatitude() - 1,
+					OURSON_QUI_BOIT_GEO_POINT.getLongitude() - 1 ) );
+			indexAccessors.geoPoint_2.write( document, new ImmutableGeoPoint( OURSON_QUI_BOIT_GEO_POINT.getLatitude() - 2,
+					OURSON_QUI_BOIT_GEO_POINT.getLongitude() - 2 ) );
+		} );
+		worker.add( referenceProvider( IMOUTO_ID ), document -> {
+			indexAccessors.string.write( document, IMOUTO_STRING );
+			indexAccessors.geoPoint.write( document, IMOUTO_GEO_POINT );
+			indexAccessors.geoPoint_1.write( document, new ImmutableGeoPoint( IMOUTO_GEO_POINT.getLatitude() - 1,
+					IMOUTO_GEO_POINT.getLongitude() - 1 ) );
+			indexAccessors.geoPoint_2.write( document, new ImmutableGeoPoint( IMOUTO_GEO_POINT.getLatitude() - 2,
+					IMOUTO_GEO_POINT.getLongitude() - 2 ) );
+		} );
+		worker.add( referenceProvider( CHEZ_MARGOTTE_ID ), document -> {
+			indexAccessors.string.write( document, CHEZ_MARGOTTE_STRING );
+			indexAccessors.geoPoint.write( document, CHEZ_MARGOTTE_GEO_POINT );
+			indexAccessors.geoPoint_1.write( document, new ImmutableGeoPoint( CHEZ_MARGOTTE_GEO_POINT.getLatitude() - 1,
+					CHEZ_MARGOTTE_GEO_POINT.getLongitude() - 1 ) );
+			indexAccessors.geoPoint_2.write( document, new ImmutableGeoPoint( CHEZ_MARGOTTE_GEO_POINT.getLatitude() - 2,
+					CHEZ_MARGOTTE_GEO_POINT.getLongitude() - 2 ) );
+		} );
+		worker.add( referenceProvider( EMPTY_ID ), document -> { } );
+
+		worker.execute().join();
+
+		// Check that all documents are searchable
+		IndexSearchTarget searchTarget = indexManager.createSearchTarget().build();
+		SearchQuery<DocumentReference> query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().matchAll().end()
+				.build();
+		assertThat( query ).hasReferencesHitsAnyOrder( indexName, OURSON_QUI_BOIT_ID, IMOUTO_ID, CHEZ_MARGOTTE_ID, EMPTY_ID );
+	}
+
+	private static class IndexAccessors {
+		final IndexFieldAccessor<GeoPoint> geoPoint;
+		final IndexFieldAccessor<GeoPoint> geoPoint_1;
+		final IndexFieldAccessor<GeoPoint> geoPoint_2;
+		final IndexFieldAccessor<String> string;
+
+		IndexAccessors(IndexSchemaElement root) {
+			geoPoint = root.field( "geoPoint" ).asGeoPoint().createAccessor();
+			geoPoint_1 = root.field( "geoPoint_1" ).asGeoPoint().createAccessor();
+			geoPoint_2 = root.field( "geoPoint_2" ).asGeoPoint().createAccessor();
+			string = root.field( "string" ).asString().createAccessor();
+		}
+	}
+}

hibernate/hibernate-search

backend/elasticsearch/src/main/java/org/hibernate/search/backend/elasticsearch/types/predicate/impl/StandardFieldPredicateBuilderFactory.java
@@ -6,15 +6,22 @@
  */
 package org.hibernate.search.backend.elasticsearch.types.predicate.impl;
 
+import java.lang.invoke.MethodHandles;
+
+import org.hibernate.search.backend.elasticsearch.logging.impl.Log;
 import org.hibernate.search.backend.elasticsearch.search.predicate.impl.ElasticsearchSearchPredicateCollector;
 import org.hibernate.search.backend.elasticsearch.search.predicate.impl.MatchPredicateBuilderImpl;
 import org.hibernate.search.backend.elasticsearch.search.predicate.impl.RangePredicateBuilderImpl;
 import org.hibernate.search.backend.elasticsearch.types.codec.impl.ElasticsearchFieldCodec;
 import org.hibernate.search.engine.search.predicate.spi.MatchPredicateBuilder;
 import org.hibernate.search.engine.search.predicate.spi.RangePredicateBuilder;
+import org.hibernate.search.engine.search.predicate.spi.SpatialWithinCirclePredicateBuilder;
+import org.hibernate.search.util.impl.common.LoggerFactory;
 
 
 public class StandardFieldPredicateBuilderFactory implements ElasticsearchFieldPredicateBuilderFactory {
+
+	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
 
 	private final ElasticsearchFieldCodec codec;
 
@@ -30,5 +37,10 @@ public class StandardFieldPredicateBuilderFactory implements ElasticsearchFieldP
 	@Override
 	public RangePredicateBuilder<ElasticsearchSearchPredicateCollector> createRangePredicateBuilder(String absoluteFieldPath) {
 		return new RangePredicateBuilderImpl( absoluteFieldPath, codec );
+	}
+
+	@Override
+	public SpatialWithinCirclePredicateBuilder<ElasticsearchSearchPredicateCollector> createSpatialWithinCirclePredicateBuilder(String absoluteFieldPath) {
+		throw log.spatialPredicatesNotSupportedByFieldType( absoluteFieldPath );
 	}
 }

backend/lucene/src/main/java/org/hibernate/search/backend/lucene/search/predicate/impl/AbstractSpatialWithinCirclePredicateBuilder.java
@@ -1,0 +1,31 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.backend.lucene.search.predicate.impl;
+
+import org.hibernate.search.engine.backend.spatial.GeoPoint;
+import org.hibernate.search.engine.search.predicate.spi.SpatialWithinCirclePredicateBuilder;
+
+
+public abstract class AbstractSpatialWithinCirclePredicateBuilder<T> extends AbstractSearchPredicateBuilder
+		implements SpatialWithinCirclePredicateBuilder<LuceneSearchPredicateCollector> {
+
+	protected final String absoluteFieldPath;
+
+	protected GeoPoint center;
+
+	protected double radiusInMeters;
+
+	protected AbstractSpatialWithinCirclePredicateBuilder(String absoluteFieldPath) {
+		this.absoluteFieldPath = absoluteFieldPath;
+	}
+
+	@Override
+	public void circle(GeoPoint center, double radiusInMeters) {
+		this.center = center;
+		this.radiusInMeters = radiusInMeters;
+	}
+}

backend/lucene/src/main/java/org/hibernate/search/backend/lucene/types/predicate/impl/LuceneFieldPredicateBuilderFactory.java
@@ -9,6 +9,7 @@ package org.hibernate.search.backend.lucene.types.predicate.impl;
 import org.hibernate.search.backend.lucene.search.predicate.impl.LuceneSearchPredicateCollector;
 import org.hibernate.search.engine.search.predicate.spi.MatchPredicateBuilder;
 import org.hibernate.search.engine.search.predicate.spi.RangePredicateBuilder;
+import org.hibernate.search.engine.search.predicate.spi.SpatialWithinCirclePredicateBuilder;
 
 /**
  * @author Guillaume Smet
@@ -18,6 +19,8 @@ public interface LuceneFieldPredicateBuilderFactory {
 	MatchPredicateBuilder<LuceneSearchPredicateCollector> createMatchPredicateBuilder(String absoluteFieldPath);
 
 	RangePredicateBuilder<LuceneSearchPredicateCollector> createRangePredicateBuilder(String absoluteFieldPath);
+
+	SpatialWithinCirclePredicateBuilder<LuceneSearchPredicateCollector> createSpatialWithinCirclePredicateBuilder(String absoluteFieldPath);
 
 	// equals()/hashCode() needs to be implemented if the predicate factory is not a singleton
 

engine/src/main/java/org/hibernate/search/engine/search/dsl/predicate/impl/SpatialPredicateContextImpl.java
@@ -1,0 +1,40 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.engine.search.dsl.predicate.impl;
+
+import java.util.function.Supplier;
+
+import org.hibernate.search.engine.search.dsl.predicate.SpatialPredicateContext;
+import org.hibernate.search.engine.search.dsl.predicate.SpatialWithinPredicateContext;
+import org.hibernate.search.engine.search.predicate.spi.SearchPredicateContributor;
+import org.hibernate.search.engine.search.predicate.spi.SearchPredicateFactory;
+
+class SpatialPredicateContextImpl<N, C> implements SpatialPredicateContext<N>, SearchPredicateContributor<C> {
+
+	private final SearchPredicateFactory<C> factory;
+
+	private final Supplier<N> nextContextProvider;
+
+	private SearchPredicateContributor<C> child;
+
+	SpatialPredicateContextImpl(SearchPredicateFactory<C> factory, Supplier<N> nextContextProvider) {
+		this.factory = factory;
+		this.nextContextProvider = nextContextProvider;
+	}
+
+	@Override
+	public SpatialWithinPredicateContext<N> within() {
+		SpatialWithinPredicateContextImpl<N, C> child = new SpatialWithinPredicateContextImpl<>( factory, nextContextProvider );
+		this.child = child;
+		return child;
+	}
+
+	@Override
+	public void contribute(C collector) {
+		child.contribute( collector );
+	}
+}

engine/src/main/java/org/hibernate/search/engine/search/dsl/predicate/impl/SpatialWithinCirclePredicateContextImpl.java
@@ -1,0 +1,25 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.engine.search.dsl.predicate.impl;
+
+import java.util.function.Supplier;
+
+import org.hibernate.search.engine.search.dsl.predicate.SpatialWithinCirclePredicateContext;
+
+public class SpatialWithinCirclePredicateContextImpl<N> implements SpatialWithinCirclePredicateContext<N> {
+
+	private final Supplier<N> nextContextProvider;
+
+	SpatialWithinCirclePredicateContextImpl(Supplier<N> nextContextProvider) {
+		this.nextContextProvider = nextContextProvider;
+	}
+
+	@Override
+	public N end() {
+		return nextContextProvider.get();
+	}
+}

integrationtest/backend/tck/src/main/java/org/hibernate/search/integrationtest/backend/tck/search/predicate/spatial/AbstractSpatialWithinSearchPredicateIT.java
@@ -1,0 +1,120 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.integrationtest.backend.tck.search.predicate.spatial;
+
+import static org.hibernate.search.util.impl.integrationtest.common.assertion.DocumentReferencesSearchResultAssert.assertThat;
+import static org.hibernate.search.util.impl.integrationtest.common.stub.mapper.StubMapperUtils.referenceProvider;
+
+import org.hibernate.search.engine.backend.document.DocumentElement;
+import org.hibernate.search.engine.backend.document.IndexFieldAccessor;
+import org.hibernate.search.engine.backend.document.model.dsl.IndexSchemaElement;
+import org.hibernate.search.engine.backend.index.spi.ChangesetIndexWorker;
+import org.hibernate.search.engine.backend.index.spi.IndexManager;
+import org.hibernate.search.engine.backend.index.spi.IndexSearchTarget;
+import org.hibernate.search.engine.backend.spatial.GeoPoint;
+import org.hibernate.search.engine.backend.spatial.ImmutableGeoPoint;
+import org.hibernate.search.engine.common.spi.SessionContext;
+import org.hibernate.search.integrationtest.backend.tck.util.rule.SearchSetupHelper;
+import org.hibernate.search.engine.search.DocumentReference;
+import org.hibernate.search.engine.search.SearchQuery;
+import org.hibernate.search.util.impl.integrationtest.common.stub.StubSessionContext;
+import org.junit.Before;
+import org.junit.Rule;
+
+public abstract class AbstractSpatialWithinSearchPredicateIT {
+
+	protected static final String OURSON_QUI_BOIT_ID = "ourson qui boit";
+	protected static final GeoPoint OURSON_QUI_BOIT_GEO_POINT = new ImmutableGeoPoint( 45.7705687,4.835233 );
+	protected static final String OURSON_QUI_BOIT_STRING = "L'ourson qui boit";
+
+	protected static final String IMOUTO_ID = "imouto";
+	protected static final GeoPoint IMOUTO_GEO_POINT = new ImmutableGeoPoint( 45.7541719, 4.8386221 );
+	protected static final String IMOUTO_STRING = "Imouto";
+
+	protected static final String CHEZ_MARGOTTE_ID = "chez margotte";
+	protected static final GeoPoint CHEZ_MARGOTTE_GEO_POINT = new ImmutableGeoPoint( 45.7530374, 4.8510299 );
+	protected static final String CHEZ_MARGOTTE_STRING = "Chez Margotte";
+
+	protected static final String EMPTY_ID = "empty";
+
+	@Rule
+	public SearchSetupHelper setupHelper = new SearchSetupHelper();
+
+	private IndexAccessors indexAccessors;
+	protected IndexManager<?> indexManager;
+	protected String indexName;
+	protected SessionContext sessionContext = new StubSessionContext();
+
+	@Before
+	public void setup() {
+		setupHelper.withDefaultConfiguration()
+				.withIndex(
+						"MappedType", "IndexName",
+						ctx -> this.indexAccessors = new IndexAccessors( ctx.getSchemaElement() ),
+						(indexManager, indexName) -> {
+							this.indexManager = indexManager;
+							this.indexName = indexName;
+						}
+				)
+				.setup();
+
+		initData();
+	}
+
+	private void initData() {
+		ChangesetIndexWorker<? extends DocumentElement> worker = indexManager.createWorker( sessionContext );
+		worker.add( referenceProvider( OURSON_QUI_BOIT_ID ), document -> {
+			indexAccessors.string.write( document, OURSON_QUI_BOIT_STRING );
+			indexAccessors.geoPoint.write( document, OURSON_QUI_BOIT_GEO_POINT );
+			indexAccessors.geoPoint_1.write( document, new ImmutableGeoPoint( OURSON_QUI_BOIT_GEO_POINT.getLatitude() - 1,
+					OURSON_QUI_BOIT_GEO_POINT.getLongitude() - 1 ) );
+			indexAccessors.geoPoint_2.write( document, new ImmutableGeoPoint( OURSON_QUI_BOIT_GEO_POINT.getLatitude() - 2,
+					OURSON_QUI_BOIT_GEO_POINT.getLongitude() - 2 ) );
+		} );
+		worker.add( referenceProvider( IMOUTO_ID ), document -> {
+			indexAccessors.string.write( document, IMOUTO_STRING );
+			indexAccessors.geoPoint.write( document, IMOUTO_GEO_POINT );
+			indexAccessors.geoPoint_1.write( document, new ImmutableGeoPoint( IMOUTO_GEO_POINT.getLatitude() - 1,
+					IMOUTO_GEO_POINT.getLongitude() - 1 ) );
+			indexAccessors.geoPoint_2.write( document, new ImmutableGeoPoint( IMOUTO_GEO_POINT.getLatitude() - 2,
+					IMOUTO_GEO_POINT.getLongitude() - 2 ) );
+		} );
+		worker.add( referenceProvider( CHEZ_MARGOTTE_ID ), document -> {
+			indexAccessors.string.write( document, CHEZ_MARGOTTE_STRING );
+			indexAccessors.geoPoint.write( document, CHEZ_MARGOTTE_GEO_POINT );
+			indexAccessors.geoPoint_1.write( document, new ImmutableGeoPoint( CHEZ_MARGOTTE_GEO_POINT.getLatitude() - 1,
+					CHEZ_MARGOTTE_GEO_POINT.getLongitude() - 1 ) );
+			indexAccessors.geoPoint_2.write( document, new ImmutableGeoPoint( CHEZ_MARGOTTE_GEO_POINT.getLatitude() - 2,
+					CHEZ_MARGOTTE_GEO_POINT.getLongitude() - 2 ) );
+		} );
+		worker.add( referenceProvider( EMPTY_ID ), document -> { } );
+
+		worker.execute().join();
+
+		// Check that all documents are searchable
+		IndexSearchTarget searchTarget = indexManager.createSearchTarget().build();
+		SearchQuery<DocumentReference> query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().matchAll().end()
+				.build();
+		assertThat( query ).hasReferencesHitsAnyOrder( indexName, OURSON_QUI_BOIT_ID, IMOUTO_ID, CHEZ_MARGOTTE_ID, EMPTY_ID );
+	}
+
+	private static class IndexAccessors {
+		final IndexFieldAccessor<GeoPoint> geoPoint;
+		final IndexFieldAccessor<GeoPoint> geoPoint_1;
+		final IndexFieldAccessor<GeoPoint> geoPoint_2;
+		final IndexFieldAccessor<String> string;
+
+		IndexAccessors(IndexSchemaElement root) {
+			geoPoint = root.field( "geoPoint" ).asGeoPoint().createAccessor();
+			geoPoint_1 = root.field( "geoPoint_1" ).asGeoPoint().createAccessor();
+			geoPoint_2 = root.field( "geoPoint_2" ).asGeoPoint().createAccessor();
+			string = root.field( "string" ).asString().createAccessor();
+		}
+	}
+}