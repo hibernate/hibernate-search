hibernate/hibernate-search-6-poc

HSEARCH-2546 Collect failures into a failure report and continue on failure during bootstrap

engine/src/main/java/org/hibernate/search/v6poc/logging/spi/FailureContextElement.java
@@ -1,0 +1,22 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.v6poc.logging.spi;
+
+/**
+ * An atomic element of context when a failure occurs:
+ * a mapped type, or an index, or a field path, ...
+ */
+public interface FailureContextElement {
+
+	/**
+	 * @return A human-readable representation of this context.
+	 * The representation should use brief, natural language to refer to objects rather than class names,
+	 * e.g. "index 'myIndexName'" rather than "ElasticsearchIndexManager{name = 'myIndexName'}".
+	 */
+	String render();
+
+}

mapper-pojo/src/main/java/org/hibernate/search/v6poc/entity/pojo/mapping/building/impl/PojoMapper.java
@@ -41,6 +41,7 @@ import org.hibernate.search.v6poc.entity.pojo.model.additionalmetadata.building.
 import org.hibernate.search.v6poc.entity.pojo.model.path.spi.PojoPathFilterFactory;
 import org.hibernate.search.v6poc.entity.pojo.model.spi.PojoBootstrapIntrospector;
 import org.hibernate.search.v6poc.entity.pojo.model.spi.PojoRawTypeModel;
+import org.hibernate.search.v6poc.logging.spi.ContextualFailureCollector;
 import org.hibernate.search.v6poc.util.AssertionFailure;
 import org.hibernate.search.v6poc.util.impl.common.Closer;
 import org.hibernate.search.v6poc.util.impl.common.LoggerFactory;
@@ -50,6 +51,7 @@ public class PojoMapper<M> implements Mapper<M> {
 
 	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
 
+	private final ContextualFailureCollector failureCollector;
 	private final ConfigurationPropertySource propertySource;
 	private final TypeMetadataContributorProvider<PojoTypeMetadataContributor> contributorProvider;
 	private final PojoBootstrapIntrospector introspector;
@@ -70,13 +72,16 @@ public class PojoMapper<M> implements Mapper<M> {
 			PojoBootstrapIntrospector introspector,
 			boolean implicitProvidedId,
 			BiFunction<ConfigurationPropertySource, PojoMappingDelegate, MappingImplementor<M>> wrapperFactory) {
+		this.failureCollector = buildContext.getFailureCollector();
 		this.propertySource = propertySource;
 		this.contributorProvider = contributorProvider;
 		this.introspector = introspector;
 		this.implicitProvidedId = implicitProvidedId;
 		this.wrapperFactory = wrapperFactory;
 
-		typeAdditionalMetadataProvider = new PojoTypeAdditionalMetadataProvider( contributorProvider );
+		typeAdditionalMetadataProvider = new PojoTypeAdditionalMetadataProvider(
+				failureCollector, contributorProvider
+		);
 		extractorBinder = new ContainerValueExtractorBinder( buildContext );
 
 		BridgeResolver bridgeResolver = new BridgeResolver();
@@ -85,7 +90,7 @@ public class PojoMapper<M> implements Mapper<M> {
 		);
 
 		mappingHelper = new PojoMappingHelper(
-				contributorProvider, indexModelBinder
+				failureCollector, contributorProvider, indexModelBinder
 		);
 	}
 

mapper-pojo/src/main/java/org/hibernate/search/v6poc/entity/pojo/mapping/building/impl/PojoMappingHelper.java
@@ -8,14 +8,18 @@ package org.hibernate.search.v6poc.entity.pojo.mapping.building.impl;
 
 import org.hibernate.search.v6poc.entity.mapping.building.spi.TypeMetadataContributorProvider;
 import org.hibernate.search.v6poc.entity.pojo.mapping.building.spi.PojoTypeMetadataContributor;
+import org.hibernate.search.v6poc.logging.spi.FailureCollector;
 
 public class PojoMappingHelper {
 
+	private final FailureCollector failureCollector;
 	private final TypeMetadataContributorProvider<PojoTypeMetadataContributor> contributorProvider;
 	private final PojoIndexModelBinder indexModelBinder;
 
-	PojoMappingHelper(TypeMetadataContributorProvider<PojoTypeMetadataContributor> contributorProvider,
+	PojoMappingHelper(FailureCollector failureCollector,
+			TypeMetadataContributorProvider<PojoTypeMetadataContributor> contributorProvider,
 			PojoIndexModelBinder indexModelBinder) {
+		this.failureCollector = failureCollector;
 		this.contributorProvider = contributorProvider;
 		this.indexModelBinder = indexModelBinder;
 	}
@@ -28,4 +32,7 @@ public class PojoMappingHelper {
 		return indexModelBinder;
 	}
 
+	public FailureCollector getFailureCollector() {
+		return failureCollector;
+	}
 }

mapper-pojo/src/main/java/org/hibernate/search/v6poc/entity/pojo/model/additionalmetadata/building/impl/PojoValueAdditionalMetadataBuilder.java
@@ -11,15 +11,37 @@ import java.util.Optional;
 import java.util.Set;
 
 import org.hibernate.search.v6poc.entity.pojo.dirtiness.ReindexOnUpdate;
+import org.hibernate.search.v6poc.entity.pojo.extractor.ContainerValueExtractorPath;
+import org.hibernate.search.v6poc.entity.pojo.logging.spi.PojoFailureContexts;
 import org.hibernate.search.v6poc.entity.pojo.model.additionalmetadata.building.spi.PojoAdditionalMetadataCollectorValueNode;
 import org.hibernate.search.v6poc.entity.pojo.model.additionalmetadata.impl.PojoValueAdditionalMetadata;
+import org.hibernate.search.v6poc.entity.pojo.model.path.PojoModelPath;
 import org.hibernate.search.v6poc.entity.pojo.model.path.PojoModelPathValueNode;
+import org.hibernate.search.v6poc.logging.spi.ContextualFailureCollector;
 
 class PojoValueAdditionalMetadataBuilder implements PojoAdditionalMetadataCollectorValueNode {
+	private final PojoTypeAdditionalMetadataBuilder rootBuilder;
+	private final String propertyName;
+	private final ContainerValueExtractorPath extractorPath;
+
 	private PojoModelPathValueNode inverseSidePath;
 	private boolean associationEmbedded = false;
 	private Optional<ReindexOnUpdate> reindexOnUpdate = Optional.empty();
 	private Set<PojoModelPathValueNode> derivedFrom = Collections.emptySet();
+
+	PojoValueAdditionalMetadataBuilder(PojoTypeAdditionalMetadataBuilder rootBuilder, String propertyName,
+			ContainerValueExtractorPath extractorPath) {
+		this.rootBuilder = rootBuilder;
+		this.propertyName = propertyName;
+		this.extractorPath = extractorPath;
+	}
+
+	@Override
+	public ContextualFailureCollector getFailureCollector() {
+		return rootBuilder.getFailureCollector().withContext(
+				PojoFailureContexts.fromPath( PojoModelPath.fromRoot( propertyName ).value( extractorPath ) )
+		);
+	}
 
 	@Override
 	public void associationInverseSide(PojoModelPathValueNode inverseSidePath) {

hibernate/hibernate-search

engine/src/main/java/org/hibernate/search/engine/logging/spi/FailureContextElement.java
@@ -1,0 +1,22 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.engine.logging.spi;
+
+/**
+ * An atomic element of context when a failure occurs:
+ * a mapped type, or an index, or a field path, ...
+ */
+public interface FailureContextElement {
+
+	/**
+	 * @return A human-readable representation of this context.
+	 * The representation should use brief, natural language to refer to objects rather than class names,
+	 * e.g. "index 'myIndexName'" rather than "ElasticsearchIndexManager{name = 'myIndexName'}".
+	 */
+	String render();
+
+}

mapper/pojo/src/main/java/org/hibernate/search/mapper/pojo/mapping/building/impl/PojoMapper.java
@@ -41,6 +41,7 @@ import org.hibernate.search.mapper.pojo.model.additionalmetadata.building.impl.P
 import org.hibernate.search.mapper.pojo.model.path.spi.PojoPathFilterFactory;
 import org.hibernate.search.mapper.pojo.model.spi.PojoBootstrapIntrospector;
 import org.hibernate.search.mapper.pojo.model.spi.PojoRawTypeModel;
+import org.hibernate.search.engine.logging.spi.ContextualFailureCollector;
 import org.hibernate.search.util.AssertionFailure;
 import org.hibernate.search.util.impl.common.Closer;
 import org.hibernate.search.util.impl.common.LoggerFactory;
@@ -50,6 +51,7 @@ public class PojoMapper<M> implements Mapper<M> {
 
 	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
 
+	private final ContextualFailureCollector failureCollector;
 	private final ConfigurationPropertySource propertySource;
 	private final TypeMetadataContributorProvider<PojoTypeMetadataContributor> contributorProvider;
 	private final PojoBootstrapIntrospector introspector;
@@ -70,13 +72,16 @@ public class PojoMapper<M> implements Mapper<M> {
 			PojoBootstrapIntrospector introspector,
 			boolean implicitProvidedId,
 			BiFunction<ConfigurationPropertySource, PojoMappingDelegate, MappingImplementor<M>> wrapperFactory) {
+		this.failureCollector = buildContext.getFailureCollector();
 		this.propertySource = propertySource;
 		this.contributorProvider = contributorProvider;
 		this.introspector = introspector;
 		this.implicitProvidedId = implicitProvidedId;
 		this.wrapperFactory = wrapperFactory;
 
-		typeAdditionalMetadataProvider = new PojoTypeAdditionalMetadataProvider( contributorProvider );
+		typeAdditionalMetadataProvider = new PojoTypeAdditionalMetadataProvider(
+				failureCollector, contributorProvider
+		);
 		extractorBinder = new ContainerValueExtractorBinder( buildContext );
 
 		BridgeResolver bridgeResolver = new BridgeResolver();
@@ -85,7 +90,7 @@ public class PojoMapper<M> implements Mapper<M> {
 		);
 
 		mappingHelper = new PojoMappingHelper(
-				contributorProvider, indexModelBinder
+				failureCollector, contributorProvider, indexModelBinder
 		);
 	}
 

mapper/pojo/src/main/java/org/hibernate/search/mapper/pojo/mapping/building/impl/PojoMappingHelper.java
@@ -8,14 +8,18 @@ package org.hibernate.search.mapper.pojo.mapping.building.impl;
 
 import org.hibernate.search.engine.mapper.mapping.building.spi.TypeMetadataContributorProvider;
 import org.hibernate.search.mapper.pojo.mapping.building.spi.PojoTypeMetadataContributor;
+import org.hibernate.search.engine.logging.spi.FailureCollector;
 
 public class PojoMappingHelper {
 
+	private final FailureCollector failureCollector;
 	private final TypeMetadataContributorProvider<PojoTypeMetadataContributor> contributorProvider;
 	private final PojoIndexModelBinder indexModelBinder;
 
-	PojoMappingHelper(TypeMetadataContributorProvider<PojoTypeMetadataContributor> contributorProvider,
+	PojoMappingHelper(FailureCollector failureCollector,
+			TypeMetadataContributorProvider<PojoTypeMetadataContributor> contributorProvider,
 			PojoIndexModelBinder indexModelBinder) {
+		this.failureCollector = failureCollector;
 		this.contributorProvider = contributorProvider;
 		this.indexModelBinder = indexModelBinder;
 	}
@@ -28,4 +32,7 @@ public class PojoMappingHelper {
 		return indexModelBinder;
 	}
 
+	public FailureCollector getFailureCollector() {
+		return failureCollector;
+	}
 }

mapper/pojo/src/main/java/org/hibernate/search/mapper/pojo/model/additionalmetadata/building/impl/PojoValueAdditionalMetadataBuilder.java
@@ -11,15 +11,37 @@ import java.util.Optional;
 import java.util.Set;
 
 import org.hibernate.search.mapper.pojo.dirtiness.ReindexOnUpdate;
+import org.hibernate.search.mapper.pojo.extractor.ContainerValueExtractorPath;
+import org.hibernate.search.mapper.pojo.logging.spi.PojoFailureContexts;
 import org.hibernate.search.mapper.pojo.model.additionalmetadata.building.spi.PojoAdditionalMetadataCollectorValueNode;
 import org.hibernate.search.mapper.pojo.model.additionalmetadata.impl.PojoValueAdditionalMetadata;
+import org.hibernate.search.mapper.pojo.model.path.PojoModelPath;
 import org.hibernate.search.mapper.pojo.model.path.PojoModelPathValueNode;
+import org.hibernate.search.engine.logging.spi.ContextualFailureCollector;
 
 class PojoValueAdditionalMetadataBuilder implements PojoAdditionalMetadataCollectorValueNode {
+	private final PojoTypeAdditionalMetadataBuilder rootBuilder;
+	private final String propertyName;
+	private final ContainerValueExtractorPath extractorPath;
+
 	private PojoModelPathValueNode inverseSidePath;
 	private boolean associationEmbedded = false;
 	private Optional<ReindexOnUpdate> reindexOnUpdate = Optional.empty();
 	private Set<PojoModelPathValueNode> derivedFrom = Collections.emptySet();
+
+	PojoValueAdditionalMetadataBuilder(PojoTypeAdditionalMetadataBuilder rootBuilder, String propertyName,
+			ContainerValueExtractorPath extractorPath) {
+		this.rootBuilder = rootBuilder;
+		this.propertyName = propertyName;
+		this.extractorPath = extractorPath;
+	}
+
+	@Override
+	public ContextualFailureCollector getFailureCollector() {
+		return rootBuilder.getFailureCollector().withContext(
+				PojoFailureContexts.fromPath( PojoModelPath.fromRoot( propertyName ).value( extractorPath ) )
+		);
+	}
 
 	@Override
 	public void associationInverseSide(PojoModelPathValueNode inverseSidePath) {