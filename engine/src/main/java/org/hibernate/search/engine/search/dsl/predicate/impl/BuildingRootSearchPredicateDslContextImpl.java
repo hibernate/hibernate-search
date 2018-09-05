hibernate/hibernate-search-6-poc

HSEARCH-3207 Add missing javadoc to "building root" DSL contexts

engine/src/main/java/org/hibernate/search/v6poc/search/dsl/predicate/impl/BuildingRootSearchPredicateDslContextImpl.java
@@ -7,14 +7,23 @@
 package org.hibernate.search.v6poc.search.dsl.predicate.impl;
 
 import java.lang.invoke.MethodHandles;
+import java.util.function.Consumer;
 
 import org.hibernate.search.v6poc.logging.impl.Log;
+import org.hibernate.search.v6poc.backend.index.spi.IndexSearchTarget;
 import org.hibernate.search.v6poc.search.SearchPredicate;
 import org.hibernate.search.v6poc.search.dsl.predicate.spi.SearchPredicateDslContext;
+import org.hibernate.search.v6poc.search.dsl.query.SearchQueryResultContext;
 import org.hibernate.search.v6poc.search.predicate.spi.SearchPredicateContributor;
 import org.hibernate.search.v6poc.search.predicate.spi.SearchPredicateFactory;
 import org.hibernate.search.v6poc.util.impl.common.LoggerFactory;
 
+/**
+ * A DSL context used when building a {@link SearchPredicate} object,
+ * either when calling {@link IndexSearchTarget#predicate()} from a search target
+ * or when calling {@link SearchQueryResultContext#predicate(Consumer)} to build the predicate using a lambda
+ * (in which case the lambda may retrieve the resulting {@link SearchPredicate} object and cache it).
+ */
 public final class BuildingRootSearchPredicateDslContextImpl<C>
 		implements SearchPredicateDslContext<SearchPredicate, C>, SearchPredicateContributor<C> {
 

engine/src/main/java/org/hibernate/search/v6poc/search/dsl/sort/impl/BuildingRootSearchSortDslContextImpl.java
@@ -8,12 +8,21 @@ package org.hibernate.search.v6poc.search.dsl.sort.impl;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
+import org.hibernate.search.v6poc.backend.index.spi.IndexSearchTarget;
 import org.hibernate.search.v6poc.search.SearchSort;
+import org.hibernate.search.v6poc.search.dsl.query.SearchQueryContext;
 import org.hibernate.search.v6poc.search.dsl.sort.spi.SearchSortDslContext;
 import org.hibernate.search.v6poc.search.sort.spi.SearchSortContributor;
 import org.hibernate.search.v6poc.search.sort.spi.SearchSortFactory;
 
+/**
+ * A DSL context used when building a {@link SearchSort} object,
+ * either when calling {@link IndexSearchTarget#sort()} from a search target
+ * or when calling {@link SearchQueryContext#sort(Consumer)} to build the sort using a lambda
+ * (in which case the lambda may retrieve the resulting {@link SearchSort} object and cache it).
+ */
 public final class BuildingRootSearchSortDslContextImpl<C>
 		implements SearchSortDslContext<SearchSort, C>, SearchSortContributor<C> {
 

hibernate/hibernate-search

engine/src/main/java/org/hibernate/search/engine/search/dsl/predicate/impl/BuildingRootSearchPredicateDslContextImpl.java
@@ -7,14 +7,23 @@
 package org.hibernate.search.engine.search.dsl.predicate.impl;
 
 import java.lang.invoke.MethodHandles;
+import java.util.function.Consumer;
 
 import org.hibernate.search.engine.logging.impl.Log;
+import org.hibernate.search.engine.backend.index.spi.IndexSearchTarget;
 import org.hibernate.search.engine.search.SearchPredicate;
 import org.hibernate.search.engine.search.dsl.predicate.spi.SearchPredicateDslContext;
+import org.hibernate.search.engine.search.dsl.query.SearchQueryResultContext;
 import org.hibernate.search.engine.search.predicate.spi.SearchPredicateContributor;
 import org.hibernate.search.engine.search.predicate.spi.SearchPredicateFactory;
 import org.hibernate.search.util.impl.common.LoggerFactory;
 
+/**
+ * A DSL context used when building a {@link SearchPredicate} object,
+ * either when calling {@link IndexSearchTarget#predicate()} from a search target
+ * or when calling {@link SearchQueryResultContext#predicate(Consumer)} to build the predicate using a lambda
+ * (in which case the lambda may retrieve the resulting {@link SearchPredicate} object and cache it).
+ */
 public final class BuildingRootSearchPredicateDslContextImpl<C>
 		implements SearchPredicateDslContext<SearchPredicate, C>, SearchPredicateContributor<C> {
 

engine/src/main/java/org/hibernate/search/engine/search/dsl/sort/impl/BuildingRootSearchSortDslContextImpl.java
@@ -8,12 +8,21 @@ package org.hibernate.search.engine.search.dsl.sort.impl;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
+import org.hibernate.search.engine.backend.index.spi.IndexSearchTarget;
 import org.hibernate.search.engine.search.SearchSort;
+import org.hibernate.search.engine.search.dsl.query.SearchQueryContext;
 import org.hibernate.search.engine.search.dsl.sort.spi.SearchSortDslContext;
 import org.hibernate.search.engine.search.sort.spi.SearchSortContributor;
 import org.hibernate.search.engine.search.sort.spi.SearchSortFactory;
 
+/**
+ * A DSL context used when building a {@link SearchSort} object,
+ * either when calling {@link IndexSearchTarget#sort()} from a search target
+ * or when calling {@link SearchQueryContext#sort(Consumer)} to build the sort using a lambda
+ * (in which case the lambda may retrieve the resulting {@link SearchSort} object and cache it).
+ */
 public final class BuildingRootSearchSortDslContextImpl<C>
 		implements SearchSortDslContext<SearchSort, C>, SearchSortContributor<C> {
 