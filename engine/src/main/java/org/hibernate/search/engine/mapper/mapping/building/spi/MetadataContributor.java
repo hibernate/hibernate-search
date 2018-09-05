hibernate/hibernate-search-6-poc

HSEARCH-3177 Move ORM user-provided mapping contributor handling to HibernateOrmMappingContributor
This way, we can avoid the awkward use of BeanResolver from the ORM
module, and use a BeanProvider like everywhere else.

engine/src/main/java/org/hibernate/search/v6poc/entity/mapping/building/spi/MetadataContributor.java
@@ -6,6 +6,7 @@
  */
 package org.hibernate.search.v6poc.entity.mapping.building.spi;
 
+import org.hibernate.search.v6poc.cfg.ConfigurationPropertySource;
 import org.hibernate.search.v6poc.engine.spi.BuildContext;
 
 /**
@@ -13,6 +14,7 @@ import org.hibernate.search.v6poc.engine.spi.BuildContext;
  */
 public interface MetadataContributor {
 
-	void contribute(BuildContext buildContext, MetadataCollector collector);
+	void contribute(BuildContext buildContext, ConfigurationPropertySource propertySource,
+			MetadataCollector collector);
 
 }

orm/src/main/java/org/hibernate/search/v6poc/entity/orm/mapping/HibernateOrmMappingContributor.java
@@ -6,10 +6,18 @@
  */
 package org.hibernate.search.v6poc.entity.orm.mapping;
 
+import java.util.Optional;
+
 import org.hibernate.MultiTenancyStrategy;
 import org.hibernate.boot.Metadata;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
+import org.hibernate.search.v6poc.cfg.ConfigurationPropertySource;
+import org.hibernate.search.v6poc.cfg.spi.ConfigurationProperty;
 import org.hibernate.search.v6poc.engine.SearchMappingRepositoryBuilder;
+import org.hibernate.search.v6poc.engine.spi.BeanProvider;
+import org.hibernate.search.v6poc.engine.spi.BuildContext;
+import org.hibernate.search.v6poc.entity.mapping.building.spi.MetadataCollector;
+import org.hibernate.search.v6poc.entity.orm.cfg.SearchOrmSettings;
 import org.hibernate.search.v6poc.entity.orm.mapping.impl.HibernateOrmMappingFactory;
 import org.hibernate.search.v6poc.entity.orm.mapping.impl.HibernateOrmMappingKey;
 import org.hibernate.search.v6poc.entity.orm.mapping.impl.HibernateOrmMetatadaContributor;
@@ -53,9 +61,26 @@ public class HibernateOrmMappingContributor extends PojoMappingContributorImpl<H
 				annotatedTypeDiscoveryEnabled,
 				!MultiTenancyStrategy.NONE.equals( sessionFactoryImplementor.getSessionFactoryOptions().getMultiTenancyStrategy() )
 		);
-		mappingRepositoryBuilder.addMetadataContributor(
+		addMetadataContributor(
 				new HibernateOrmMetatadaContributor( getMapperFactory(), introspector, metadata )
 		);
 	}
 
+	@Override
+	public void contribute(BuildContext buildContext, ConfigurationPropertySource propertySource,
+			MetadataCollector collector) {
+		// Apply the user-provided metadata contributor if necessary
+		final BeanProvider beanProvider = buildContext.getServiceManager().getBeanProvider();
+		ConfigurationProperty<Optional<HibernateOrmSearchMappingContributor>> userMappingContributorProperty =
+				ConfigurationProperty.forKey( SearchOrmSettings.Radicals.MAPPING_CONTRIBUTOR )
+						.as(
+								HibernateOrmSearchMappingContributor.class,
+								reference -> beanProvider.getBean( reference, HibernateOrmSearchMappingContributor.class )
+						)
+						.build();
+		userMappingContributorProperty.get( propertySource )
+				.ifPresent( userContributor -> userContributor.contribute( this ) );
+
+		super.contribute( buildContext, propertySource, collector );
+	}
 }

orm/src/main/java/org/hibernate/search/v6poc/entity/orm/mapping/impl/HibernateOrmMetatadaContributor.java
@@ -20,6 +20,7 @@ import org.hibernate.mapping.PersistentClass;
 import org.hibernate.mapping.Property;
 import org.hibernate.mapping.ToOne;
 import org.hibernate.mapping.Value;
+import org.hibernate.search.v6poc.cfg.ConfigurationPropertySource;
 import org.hibernate.search.v6poc.engine.spi.BuildContext;
 import org.hibernate.search.v6poc.entity.mapping.building.spi.MapperFactory;
 import org.hibernate.search.v6poc.entity.mapping.building.spi.MetadataCollector;
@@ -49,7 +50,8 @@ public final class HibernateOrmMetatadaContributor implements MetadataContributo
 	}
 
 	@Override
-	public void contribute(BuildContext buildContext, MetadataCollector collector) {
+	public void contribute(BuildContext buildContext, ConfigurationPropertySource propertySource,
+			MetadataCollector collector) {
 		PropertyDelegatesCollector delegatesCollector = new PropertyDelegatesCollector();
 		// Ensure all entities are declared as such and have their inverse associations declared
 		for ( PersistentClass persistentClass : metadata.getEntityBindings() ) {

mapper-pojo/src/main/java/org/hibernate/search/v6poc/entity/pojo/mapping/definition/annotation/impl/AnnotationMappingDefinitionImpl.java
@@ -11,6 +11,7 @@ import java.util.LinkedHashSet;
 import java.util.Optional;
 import java.util.Set;
 
+import org.hibernate.search.v6poc.cfg.ConfigurationPropertySource;
 import org.hibernate.search.v6poc.engine.spi.BeanProvider;
 import org.hibernate.search.v6poc.engine.spi.BuildContext;
 import org.hibernate.search.v6poc.entity.mapping.building.spi.MapperFactory;
@@ -53,7 +54,8 @@ public class AnnotationMappingDefinitionImpl implements AnnotationMappingDefinit
 	}
 
 	@Override
-	public void contribute(BuildContext buildContext, MetadataCollector collector) {
+	public void contribute(BuildContext buildContext, ConfigurationPropertySource propertySource,
+			MetadataCollector collector) {
 		BeanProvider beanProvider = buildContext.getServiceManager().getBeanProvider();
 
 		/*

mapper-pojo/src/main/java/org/hibernate/search/v6poc/entity/pojo/mapping/definition/programmatic/impl/ProgrammaticMappingDefinitionImpl.java
@@ -9,6 +9,7 @@ package org.hibernate.search.v6poc.entity.pojo.mapping.definition.programmatic.i
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import org.hibernate.search.v6poc.cfg.ConfigurationPropertySource;
 import org.hibernate.search.v6poc.engine.spi.BuildContext;
 import org.hibernate.search.v6poc.entity.mapping.building.spi.MapperFactory;
 import org.hibernate.search.v6poc.entity.mapping.building.spi.MetadataCollector;
@@ -34,9 +35,10 @@ public class ProgrammaticMappingDefinitionImpl implements ProgrammaticMappingDef
 	}
 
 	@Override
-	public void contribute(BuildContext buildContext, MetadataCollector collector) {
+	public void contribute(BuildContext buildContext, ConfigurationPropertySource propertySource,
+			MetadataCollector collector) {
 		for ( TypeMappingContextImpl contextImpl : entities.values() ) {
-			contextImpl.contribute( buildContext, collector );
+			contextImpl.contribute( buildContext, propertySource, collector );
 		}
 	}
 

mapper-pojo/src/main/java/org/hibernate/search/v6poc/entity/pojo/mapping/definition/programmatic/impl/TypeMappingContextImpl.java
@@ -9,6 +9,7 @@ package org.hibernate.search.v6poc.entity.pojo.mapping.definition.programmatic.i
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.search.v6poc.cfg.ConfigurationPropertySource;
 import org.hibernate.search.v6poc.engine.spi.BeanReference;
 import org.hibernate.search.v6poc.engine.spi.BuildContext;
 import org.hibernate.search.v6poc.engine.spi.ImmutableBeanReference;
@@ -46,7 +47,8 @@ public class TypeMappingContextImpl implements TypeMappingContext, MetadataContr
 	}
 
 	@Override
-	public void contribute(BuildContext buildContext, MetadataCollector collector) {
+	public void contribute(BuildContext buildContext, ConfigurationPropertySource propertySource,
+			MetadataCollector collector) {
 		if ( indexName != null ) {
 			collector.mapToIndex( mapperFactory, typeModel, indexName );
 		}

mapper-pojo/src/main/java/org/hibernate/search/v6poc/entity/pojo/mapping/spi/PojoMappingContributorImpl.java
@@ -6,8 +6,15 @@
  */
 package org.hibernate.search.v6poc.entity.pojo.mapping.spi;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.search.v6poc.cfg.ConfigurationPropertySource;
 import org.hibernate.search.v6poc.engine.SearchMappingRepositoryBuilder;
+import org.hibernate.search.v6poc.engine.spi.BuildContext;
 import org.hibernate.search.v6poc.entity.mapping.building.spi.MapperFactory;
+import org.hibernate.search.v6poc.entity.mapping.building.spi.MetadataCollector;
+import org.hibernate.search.v6poc.entity.mapping.building.spi.MetadataContributor;
 import org.hibernate.search.v6poc.entity.mapping.spi.MappingKey;
 import org.hibernate.search.v6poc.entity.pojo.mapping.PojoMapping;
 import org.hibernate.search.v6poc.entity.pojo.mapping.PojoMappingContributor;
@@ -20,13 +27,15 @@ import org.hibernate.search.v6poc.entity.pojo.mapping.definition.programmatic.im
 import org.hibernate.search.v6poc.entity.pojo.model.spi.PojoBootstrapIntrospector;
 
 public abstract class PojoMappingContributorImpl<M extends PojoMapping>
-		implements PojoMappingContributor<M> {
+		implements PojoMappingContributor<M>, MetadataContributor {
 
 	private final SearchMappingRepositoryBuilder mappingRepositoryBuilder;
 	private final PojoMapperFactory<M> mapperFactory;
 	private final PojoBootstrapIntrospector introspector;
 
 	private final AnnotationMappingDefinitionImpl annotationMappingDefinition;
+
+	private final List<MetadataContributor> delegates = new ArrayList<>();
 
 	protected PojoMappingContributorImpl(SearchMappingRepositoryBuilder mappingRepositoryBuilder,
 			MappingKey<M> mappingKey, PojoMappingFactory<M> mappingFactory,
@@ -40,6 +49,8 @@ public abstract class PojoMappingContributorImpl<M extends PojoMapping>
 		);
 		this.introspector = introspector;
 
+		mappingRepositoryBuilder.addMetadataContributor( this );
+
 		/*
 		 * Make sure to create and add the annotation mapping even if the user does not call the
 		 * annotationMapping() method to register annotated types explicitly,
@@ -50,14 +61,14 @@ public abstract class PojoMappingContributorImpl<M extends PojoMapping>
 		annotationMappingDefinition = new AnnotationMappingDefinitionImpl(
 				mapperFactory, introspector, annotatedTypeDiscoveryEnabled
 		);
-		mappingRepositoryBuilder.addMetadataContributor( annotationMappingDefinition );
+		addMetadataContributor( annotationMappingDefinition );
 	}
 
 	@Override
 	public ProgrammaticMappingDefinition programmaticMapping() {
 		ProgrammaticMappingDefinitionImpl definition =
 				new ProgrammaticMappingDefinitionImpl( mapperFactory, introspector );
-		mappingRepositoryBuilder.addMetadataContributor( definition );
+		addMetadataContributor( definition );
 		return definition;
 	}
 
@@ -71,7 +82,19 @@ public abstract class PojoMappingContributorImpl<M extends PojoMapping>
 		return mappingRepositoryBuilder.getBuiltResult().getMapping( mapperFactory.getMappingKey() );
 	}
 
+	@Override
+	public void contribute(BuildContext buildContext, ConfigurationPropertySource propertySource,
+			MetadataCollector collector) {
+		for ( MetadataContributor delegate : delegates ) {
+			delegate.contribute( buildContext, propertySource, collector );
+		}
+	}
+
 	protected final MapperFactory<PojoTypeMetadataContributor, ?> getMapperFactory() {
 		return mapperFactory;
+	}
+
+	protected final void addMetadataContributor(MetadataContributor contributor) {
+		delegates.add( contributor );
 	}
 }

util/internal/integrationtest/common/src/main/java/org/hibernate/search/v6poc/util/impl/integrationtest/common/stub/mapper/StubMetadataContributor.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 
+import org.hibernate.search.v6poc.cfg.ConfigurationPropertySource;
 import org.hibernate.search.v6poc.engine.SearchMappingRepositoryBuilder;
 import org.hibernate.search.v6poc.engine.spi.BuildContext;
 import org.hibernate.search.v6poc.entity.mapping.building.spi.IndexModelBindingContext;
@@ -33,7 +34,8 @@ public class StubMetadataContributor implements MetadataContributor {
 	}
 
 	@Override
-	public void contribute(BuildContext buildContext, MetadataCollector collector) {
+	public void contribute(BuildContext buildContext, ConfigurationPropertySource propertySource,
+			MetadataCollector collector) {
 		for ( StubTypeMetadataContributor mappingContributor : mappingContributors ) {
 			mappingContributor.contribute( mapperFactory, collector );
 		}

hibernate/hibernate-search

engine/src/main/java/org/hibernate/search/engine/mapper/mapping/building/spi/MetadataContributor.java
@@ -6,6 +6,7 @@
  */
 package org.hibernate.search.engine.mapper.mapping.building.spi;
 
+import org.hibernate.search.engine.cfg.ConfigurationPropertySource;
 import org.hibernate.search.engine.common.spi.BuildContext;
 
 /**
@@ -13,6 +14,7 @@ import org.hibernate.search.engine.common.spi.BuildContext;
  */
 public interface MetadataContributor {
 
-	void contribute(BuildContext buildContext, MetadataCollector collector);
+	void contribute(BuildContext buildContext, ConfigurationPropertySource propertySource,
+			MetadataCollector collector);
 
 }

mapper/orm/src/main/java/org/hibernate/search/mapper/orm/mapping/HibernateOrmMappingContributor.java
@@ -6,10 +6,18 @@
  */
 package org.hibernate.search.mapper.orm.mapping;
 
+import java.util.Optional;
+
 import org.hibernate.MultiTenancyStrategy;
 import org.hibernate.boot.Metadata;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
+import org.hibernate.search.engine.cfg.ConfigurationPropertySource;
+import org.hibernate.search.engine.cfg.spi.ConfigurationProperty;
 import org.hibernate.search.engine.common.SearchMappingRepositoryBuilder;
+import org.hibernate.search.engine.common.spi.BeanProvider;
+import org.hibernate.search.engine.common.spi.BuildContext;
+import org.hibernate.search.engine.mapper.mapping.building.spi.MetadataCollector;
+import org.hibernate.search.mapper.orm.cfg.SearchOrmSettings;
 import org.hibernate.search.mapper.orm.mapping.impl.HibernateOrmMappingFactory;
 import org.hibernate.search.mapper.orm.mapping.impl.HibernateOrmMappingKey;
 import org.hibernate.search.mapper.orm.mapping.impl.HibernateOrmMetatadaContributor;
@@ -53,9 +61,26 @@ public class HibernateOrmMappingContributor extends PojoMappingContributorImpl<H
 				annotatedTypeDiscoveryEnabled,
 				!MultiTenancyStrategy.NONE.equals( sessionFactoryImplementor.getSessionFactoryOptions().getMultiTenancyStrategy() )
 		);
-		mappingRepositoryBuilder.addMetadataContributor(
+		addMetadataContributor(
 				new HibernateOrmMetatadaContributor( getMapperFactory(), introspector, metadata )
 		);
 	}
 
+	@Override
+	public void contribute(BuildContext buildContext, ConfigurationPropertySource propertySource,
+			MetadataCollector collector) {
+		// Apply the user-provided metadata contributor if necessary
+		final BeanProvider beanProvider = buildContext.getServiceManager().getBeanProvider();
+		ConfigurationProperty<Optional<HibernateOrmSearchMappingContributor>> userMappingContributorProperty =
+				ConfigurationProperty.forKey( SearchOrmSettings.Radicals.MAPPING_CONTRIBUTOR )
+						.as(
+								HibernateOrmSearchMappingContributor.class,
+								reference -> beanProvider.getBean( reference, HibernateOrmSearchMappingContributor.class )
+						)
+						.build();
+		userMappingContributorProperty.get( propertySource )
+				.ifPresent( userContributor -> userContributor.contribute( this ) );
+
+		super.contribute( buildContext, propertySource, collector );
+	}
 }

mapper/orm/src/main/java/org/hibernate/search/mapper/orm/mapping/impl/HibernateOrmMetatadaContributor.java
@@ -20,6 +20,7 @@ import org.hibernate.mapping.PersistentClass;
 import org.hibernate.mapping.Property;
 import org.hibernate.mapping.ToOne;
 import org.hibernate.mapping.Value;
+import org.hibernate.search.engine.cfg.ConfigurationPropertySource;
 import org.hibernate.search.engine.common.spi.BuildContext;
 import org.hibernate.search.engine.mapper.mapping.building.spi.MapperFactory;
 import org.hibernate.search.engine.mapper.mapping.building.spi.MetadataCollector;
@@ -49,7 +50,8 @@ public final class HibernateOrmMetatadaContributor implements MetadataContributo
 	}
 
 	@Override
-	public void contribute(BuildContext buildContext, MetadataCollector collector) {
+	public void contribute(BuildContext buildContext, ConfigurationPropertySource propertySource,
+			MetadataCollector collector) {
 		PropertyDelegatesCollector delegatesCollector = new PropertyDelegatesCollector();
 		// Ensure all entities are declared as such and have their inverse associations declared
 		for ( PersistentClass persistentClass : metadata.getEntityBindings() ) {

mapper/pojo/src/main/java/org/hibernate/search/mapper/pojo/mapping/definition/annotation/impl/AnnotationMappingDefinitionImpl.java
@@ -11,6 +11,7 @@ import java.util.LinkedHashSet;
 import java.util.Optional;
 import java.util.Set;
 
+import org.hibernate.search.engine.cfg.ConfigurationPropertySource;
 import org.hibernate.search.engine.common.spi.BeanProvider;
 import org.hibernate.search.engine.common.spi.BuildContext;
 import org.hibernate.search.engine.mapper.mapping.building.spi.MapperFactory;
@@ -53,7 +54,8 @@ public class AnnotationMappingDefinitionImpl implements AnnotationMappingDefinit
 	}
 
 	@Override
-	public void contribute(BuildContext buildContext, MetadataCollector collector) {
+	public void contribute(BuildContext buildContext, ConfigurationPropertySource propertySource,
+			MetadataCollector collector) {
 		BeanProvider beanProvider = buildContext.getServiceManager().getBeanProvider();
 
 		/*

mapper/pojo/src/main/java/org/hibernate/search/mapper/pojo/mapping/definition/programmatic/impl/ProgrammaticMappingDefinitionImpl.java
@@ -9,6 +9,7 @@ package org.hibernate.search.mapper.pojo.mapping.definition.programmatic.impl;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import org.hibernate.search.engine.cfg.ConfigurationPropertySource;
 import org.hibernate.search.engine.common.spi.BuildContext;
 import org.hibernate.search.engine.mapper.mapping.building.spi.MapperFactory;
 import org.hibernate.search.engine.mapper.mapping.building.spi.MetadataCollector;
@@ -34,9 +35,10 @@ public class ProgrammaticMappingDefinitionImpl implements ProgrammaticMappingDef
 	}
 
 	@Override
-	public void contribute(BuildContext buildContext, MetadataCollector collector) {
+	public void contribute(BuildContext buildContext, ConfigurationPropertySource propertySource,
+			MetadataCollector collector) {
 		for ( TypeMappingContextImpl contextImpl : entities.values() ) {
-			contextImpl.contribute( buildContext, collector );
+			contextImpl.contribute( buildContext, propertySource, collector );
 		}
 	}
 

mapper/pojo/src/main/java/org/hibernate/search/mapper/pojo/mapping/definition/programmatic/impl/TypeMappingContextImpl.java
@@ -9,6 +9,7 @@ package org.hibernate.search.mapper.pojo.mapping.definition.programmatic.impl;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.search.engine.cfg.ConfigurationPropertySource;
 import org.hibernate.search.engine.common.spi.BeanReference;
 import org.hibernate.search.engine.common.spi.BuildContext;
 import org.hibernate.search.engine.common.spi.ImmutableBeanReference;
@@ -46,7 +47,8 @@ public class TypeMappingContextImpl implements TypeMappingContext, MetadataContr
 	}
 
 	@Override
-	public void contribute(BuildContext buildContext, MetadataCollector collector) {
+	public void contribute(BuildContext buildContext, ConfigurationPropertySource propertySource,
+			MetadataCollector collector) {
 		if ( indexName != null ) {
 			collector.mapToIndex( mapperFactory, typeModel, indexName );
 		}

mapper/pojo/src/main/java/org/hibernate/search/mapper/pojo/mapping/spi/PojoMappingContributorImpl.java
@@ -6,8 +6,15 @@
  */
 package org.hibernate.search.mapper.pojo.mapping.spi;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.search.engine.cfg.ConfigurationPropertySource;
 import org.hibernate.search.engine.common.SearchMappingRepositoryBuilder;
+import org.hibernate.search.engine.common.spi.BuildContext;
 import org.hibernate.search.engine.mapper.mapping.building.spi.MapperFactory;
+import org.hibernate.search.engine.mapper.mapping.building.spi.MetadataCollector;
+import org.hibernate.search.engine.mapper.mapping.building.spi.MetadataContributor;
 import org.hibernate.search.engine.mapper.mapping.spi.MappingKey;
 import org.hibernate.search.mapper.pojo.mapping.PojoMapping;
 import org.hibernate.search.mapper.pojo.mapping.PojoMappingContributor;
@@ -20,13 +27,15 @@ import org.hibernate.search.mapper.pojo.mapping.definition.programmatic.impl.Pro
 import org.hibernate.search.mapper.pojo.model.spi.PojoBootstrapIntrospector;
 
 public abstract class PojoMappingContributorImpl<M extends PojoMapping>
-		implements PojoMappingContributor<M> {
+		implements PojoMappingContributor<M>, MetadataContributor {
 
 	private final SearchMappingRepositoryBuilder mappingRepositoryBuilder;
 	private final PojoMapperFactory<M> mapperFactory;
 	private final PojoBootstrapIntrospector introspector;
 
 	private final AnnotationMappingDefinitionImpl annotationMappingDefinition;
+
+	private final List<MetadataContributor> delegates = new ArrayList<>();
 
 	protected PojoMappingContributorImpl(SearchMappingRepositoryBuilder mappingRepositoryBuilder,
 			MappingKey<M> mappingKey, PojoMappingFactory<M> mappingFactory,
@@ -40,6 +49,8 @@ public abstract class PojoMappingContributorImpl<M extends PojoMapping>
 		);
 		this.introspector = introspector;
 
+		mappingRepositoryBuilder.addMetadataContributor( this );
+
 		/*
 		 * Make sure to create and add the annotation mapping even if the user does not call the
 		 * annotationMapping() method to register annotated types explicitly,
@@ -50,14 +61,14 @@ public abstract class PojoMappingContributorImpl<M extends PojoMapping>
 		annotationMappingDefinition = new AnnotationMappingDefinitionImpl(
 				mapperFactory, introspector, annotatedTypeDiscoveryEnabled
 		);
-		mappingRepositoryBuilder.addMetadataContributor( annotationMappingDefinition );
+		addMetadataContributor( annotationMappingDefinition );
 	}
 
 	@Override
 	public ProgrammaticMappingDefinition programmaticMapping() {
 		ProgrammaticMappingDefinitionImpl definition =
 				new ProgrammaticMappingDefinitionImpl( mapperFactory, introspector );
-		mappingRepositoryBuilder.addMetadataContributor( definition );
+		addMetadataContributor( definition );
 		return definition;
 	}
 
@@ -71,7 +82,19 @@ public abstract class PojoMappingContributorImpl<M extends PojoMapping>
 		return mappingRepositoryBuilder.getBuiltResult().getMapping( mapperFactory.getMappingKey() );
 	}
 
+	@Override
+	public void contribute(BuildContext buildContext, ConfigurationPropertySource propertySource,
+			MetadataCollector collector) {
+		for ( MetadataContributor delegate : delegates ) {
+			delegate.contribute( buildContext, propertySource, collector );
+		}
+	}
+
 	protected final MapperFactory<PojoTypeMetadataContributor, ?> getMapperFactory() {
 		return mapperFactory;
+	}
+
+	protected final void addMetadataContributor(MetadataContributor contributor) {
+		delegates.add( contributor );
 	}
 }

util/internal/integrationtest/common/src/main/java/org/hibernate/search/util/impl/integrationtest/common/stub/mapper/StubMetadataContributor.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 
+import org.hibernate.search.engine.cfg.ConfigurationPropertySource;
 import org.hibernate.search.engine.common.SearchMappingRepositoryBuilder;
 import org.hibernate.search.engine.common.spi.BuildContext;
 import org.hibernate.search.engine.mapper.mapping.building.spi.IndexModelBindingContext;
@@ -33,7 +34,8 @@ public class StubMetadataContributor implements MetadataContributor {
 	}
 
 	@Override
-	public void contribute(BuildContext buildContext, MetadataCollector collector) {
+	public void contribute(BuildContext buildContext, ConfigurationPropertySource propertySource,
+			MetadataCollector collector) {
 		for ( StubTypeMetadataContributor mappingContributor : mappingContributors ) {
 			mappingContributor.contribute( mapperFactory, collector );
 		}