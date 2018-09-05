hibernate/hibernate-search-6-poc

HSEARCH-3087 Support sorting by distance to a given point

backend-lucene/src/main/java/org/hibernate/search/v6poc/backend/lucene/types/sort/impl/AbstractStandardLuceneFieldSortContributor.java
@@ -6,10 +6,18 @@
  */
 package org.hibernate.search.v6poc.backend.lucene.types.sort.impl;
 
+import java.lang.invoke.MethodHandles;
+
 import org.apache.lucene.search.SortField;
+import org.hibernate.search.v6poc.backend.lucene.logging.impl.Log;
+import org.hibernate.search.v6poc.backend.lucene.search.sort.impl.LuceneSearchSortCollector;
+import org.hibernate.search.v6poc.backend.spatial.GeoPoint;
 import org.hibernate.search.v6poc.search.dsl.sort.SortOrder;
+import org.hibernate.search.v6poc.util.impl.common.LoggerFactory;
 
 abstract class AbstractStandardLuceneFieldSortContributor implements LuceneFieldSortContributor {
+
+	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
 
 	private Object sortMissingValueFirstPlaceholder;
 
@@ -38,5 +46,10 @@ abstract class AbstractStandardLuceneFieldSortContributor implements LuceneField
 		}
 
 		sortField.setMissingValue( effectiveMissingValue );
+	}
+
+	@Override
+	public void contributeDistanceSort(LuceneSearchSortCollector collector, String absoluteFieldPath, GeoPoint location, SortOrder order) {
+		throw log.distanceOperationsNotSupportedByFieldType( absoluteFieldPath );
 	}
 }

engine/src/main/java/org/hibernate/search/v6poc/search/dsl/sort/SearchSortContainerContext.java
@@ -8,6 +8,8 @@ package org.hibernate.search.v6poc.search.dsl.sort;
 
 import java.util.function.Consumer;
 
+import org.hibernate.search.v6poc.backend.spatial.GeoPoint;
+import org.hibernate.search.v6poc.backend.spatial.ImmutableGeoPoint;
 import org.hibernate.search.v6poc.search.SearchSort;
 import org.hibernate.search.v6poc.search.dsl.sort.spi.SearchSortContainerContextExtension;
 import org.hibernate.search.v6poc.util.SearchException;
@@ -45,7 +47,34 @@ public interface SearchSortContainerContext<N> {
 	 */
 	FieldSortContext<N> byField(String absoluteFieldPath);
 
-	// TODO other sorts (spatial, ...)
+	/**
+	 * Order elements by the distance from the location stored in the specified field to the location specified.
+	 *
+	 * <p>The default order is <strong>ascending</strong>.
+	 *
+	 * @param absoluteFieldPath The absolute path of the indexed location field to sort by.
+	 * @param location The location to which we want to compute the distance.
+	 * @throws SearchException If the field type does not constitute a valid location.
+	 */
+	DistanceSortContext<N> byDistance(String absoluteFieldPath, GeoPoint location);
+
+	/**
+	 * Order elements by the distance from the location stored in the specified field to the location specified.
+	 *
+	 * <p>
+	 * The default order is <strong>ascending</strong>.
+	 *
+	 * @param absoluteFieldPath The absolute path of the indexed location field to sort by.
+	 * @param latitude The latitude of the location to which we want to compute the distance.
+	 * @param longitude The longitude of the location to which we want to compute the distance.
+	 * @throws SearchException If the field type does not constitute a valid location.
+	 */
+	default DistanceSortContext<N> byDistance(String absoluteFieldPath, double latitude, double longitude) {
+		return byDistance( absoluteFieldPath, new ImmutableGeoPoint( latitude, longitude ) );
+	}
+
+	// TODO other sorts
+
 
 	/**
 	 * Order by the given sort.

engine/src/main/java/org/hibernate/search/v6poc/search/dsl/sort/impl/DistanceSortContextImpl.java
@@ -1,0 +1,53 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.v6poc.search.dsl.sort.impl;
+
+import java.util.function.Supplier;
+
+import org.hibernate.search.v6poc.backend.spatial.GeoPoint;
+import org.hibernate.search.v6poc.search.dsl.sort.DistanceSortContext;
+import org.hibernate.search.v6poc.search.dsl.sort.SearchSortContainerContext;
+import org.hibernate.search.v6poc.search.dsl.sort.SortOrder;
+import org.hibernate.search.v6poc.search.sort.spi.DistanceSortBuilder;
+import org.hibernate.search.v6poc.search.sort.spi.SearchSortContributor;
+import org.hibernate.search.v6poc.search.sort.spi.SearchSortFactory;
+
+class DistanceSortContextImpl<N, C> implements DistanceSortContext<N>, SearchSortContributor<C> {
+
+	private final SearchSortContainerContext<N> containerContext;
+	private final Supplier<N> nextContextProvider;
+	private final DistanceSortBuilder<C> builder;
+
+	DistanceSortContextImpl(SearchSortContainerContext<N> containerContext,
+			SearchSortFactory<C> factory, Supplier<N> nextContextProvider,
+			String absoluteFieldPath, GeoPoint location) {
+		this.containerContext = containerContext;
+		this.nextContextProvider = nextContextProvider;
+		this.builder = factory.distance( absoluteFieldPath, location );
+	}
+
+	@Override
+	public DistanceSortContext<N> order(SortOrder order) {
+		builder.order( order );
+		return this;
+	}
+
+	@Override
+	public SearchSortContainerContext<N> then() {
+		return containerContext;
+	}
+
+	@Override
+	public N end() {
+		return nextContextProvider.get();
+	}
+
+	@Override
+	public void contribute(C collector) {
+		builder.contribute( collector );
+	}
+}

engine/src/main/java/org/hibernate/search/v6poc/search/sort/spi/SearchSortFactory.java
@@ -6,6 +6,7 @@
  */
 package org.hibernate.search.v6poc.search.sort.spi;
 
+import org.hibernate.search.v6poc.backend.spatial.GeoPoint;
 import org.hibernate.search.v6poc.search.SearchSort;
 
 /**
@@ -26,6 +27,8 @@ public interface SearchSortFactory<C> {
 	ScoreSortBuilder<C> score();
 
 	FieldSortBuilder<C> field(String absoluteFieldPath);
+
+	DistanceSortBuilder<C> distance(String absoluteFieldPath, GeoPoint location);
 
 	SearchSortContributor<C> indexOrder();
 

integrationtest/backend-tck/src/main/java/org/hibernate/search/v6poc/integrationtest/backend/tck/search/SearchSortIT.java
@@ -18,17 +18,16 @@ import org.hibernate.search.v6poc.backend.document.DocumentElement;
 import org.hibernate.search.v6poc.backend.document.IndexFieldAccessor;
 import org.hibernate.search.v6poc.backend.document.IndexObjectFieldAccessor;
 import org.hibernate.search.v6poc.backend.document.model.dsl.IndexSchemaElement;
+import org.hibernate.search.v6poc.backend.document.model.dsl.IndexSchemaObjectField;
 import org.hibernate.search.v6poc.backend.document.model.dsl.ObjectFieldStorage;
 import org.hibernate.search.v6poc.backend.document.model.dsl.Sortable;
-import org.hibernate.search.v6poc.backend.document.model.dsl.IndexSchemaObjectField;
 import org.hibernate.search.v6poc.backend.index.spi.ChangesetIndexWorker;
 import org.hibernate.search.v6poc.backend.index.spi.IndexManager;
 import org.hibernate.search.v6poc.backend.index.spi.IndexSearchTarget;
+import org.hibernate.search.v6poc.backend.spatial.GeoPoint;
+import org.hibernate.search.v6poc.backend.spatial.ImmutableGeoPoint;
 import org.hibernate.search.v6poc.engine.spi.SessionContext;
 import org.hibernate.search.v6poc.integrationtest.backend.tck.util.rule.SearchSetupHelper;
-import org.hibernate.search.v6poc.util.impl.integrationtest.common.assertion.DocumentReferencesSearchResultAssert;
-import org.hibernate.search.v6poc.util.impl.integrationtest.common.assertion.SearchResultAssert;
-import org.hibernate.search.v6poc.util.impl.integrationtest.common.stub.StubSessionContext;
 import org.hibernate.search.v6poc.search.DocumentReference;
 import org.hibernate.search.v6poc.search.SearchPredicate;
 import org.hibernate.search.v6poc.search.SearchQuery;
@@ -36,7 +35,9 @@ import org.hibernate.search.v6poc.search.SearchResult;
 import org.hibernate.search.v6poc.search.SearchSort;
 import org.hibernate.search.v6poc.search.dsl.sort.SearchSortContainerContext;
 import org.hibernate.search.v6poc.util.SearchException;
-
+import org.hibernate.search.v6poc.util.impl.integrationtest.common.assertion.DocumentReferencesSearchResultAssert;
+import org.hibernate.search.v6poc.util.impl.integrationtest.common.assertion.SearchResultAssert;
+import org.hibernate.search.v6poc.util.impl.integrationtest.common.stub.StubSessionContext;
 import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Rule;
@@ -256,6 +257,22 @@ public class SearchSortIT {
 	}
 
 	@Test
+	public void byDistance() {
+		SearchQuery<DocumentReference> query = simpleQuery( b -> b.byDistance( "geoPoint", new ImmutableGeoPoint( 45.757864, 4.834496 ) ) );
+
+		DocumentReferencesSearchResultAssert.assertThat( query )
+				.hasReferencesHitsExactOrder( indexName, FIRST_ID, THIRD_ID, SECOND_ID, EMPTY_ID );
+
+		query = simpleQuery( b -> b.byDistance( "geoPoint", 45.757864, 4.834496 ) );
+
+		DocumentReferencesSearchResultAssert.assertThat( query )
+				.hasReferencesHitsExactOrder( indexName, FIRST_ID, THIRD_ID, SECOND_ID, EMPTY_ID );
+
+		// we don't test the descending order here as it's currently not supported by Lucene
+		// see the additional tests in the specific backend tests
+	}
+
+	@Test
 	public void byField_error_unsortable() {
 		Assume.assumeTrue( "Errors on attempt to sort on unsortable fields are not supported yet", false );
 		// TODO throw an error on attempts to sort on unsortable fields
@@ -326,6 +343,7 @@ public class SearchSortIT {
 			indexAccessors.string_analyzed.write( document, "George" );
 			indexAccessors.integer.write( document, 2 );
 			indexAccessors.localDate.write( document, LocalDate.of( 2018, 2, 2 ) );
+			indexAccessors.geoPoint.write( document, new ImmutableGeoPoint( 45.7705687,4.835233 ) );
 
 			indexAccessors.string_analyzed_forScore.write( document, "Hooray Hooray" );
 			indexAccessors.unsortable.write( document, "george" );
@@ -347,6 +365,7 @@ public class SearchSortIT {
 			indexAccessors.string_analyzed.write( document, "Aaron" );
 			indexAccessors.integer.write( document, 1 );
 			indexAccessors.localDate.write( document, LocalDate.of( 2018, 2, 1 ) );
+			indexAccessors.geoPoint.write( document, new ImmutableGeoPoint( 45.7541719, 4.8386221 ) );
 
 			indexAccessors.string_analyzed_forScore.write( document, "Hooray Hooray Hooray" );
 			indexAccessors.unsortable.write( document, "aaron" );
@@ -368,6 +387,7 @@ public class SearchSortIT {
 			indexAccessors.string_analyzed.write( document, "Zach" );
 			indexAccessors.integer.write( document, 3 );
 			indexAccessors.localDate.write( document, LocalDate.of( 2019, 1, 2 ) );
+			indexAccessors.geoPoint.write( document, new ImmutableGeoPoint( 45.7530374, 4.8510299 ) );
 
 			indexAccessors.string_analyzed_forScore.write( document, "Hooray" );
 			indexAccessors.unsortable.write( document, "zach" );
@@ -402,6 +422,7 @@ public class SearchSortIT {
 		final IndexFieldAccessor<String> string_analyzed;
 		final IndexFieldAccessor<Integer> integer;
 		final IndexFieldAccessor<LocalDate> localDate;
+		final IndexFieldAccessor<GeoPoint> geoPoint;
 
 		final IndexFieldAccessor<String> string_analyzed_forScore;
 		final IndexFieldAccessor<String> unsortable;
@@ -417,6 +438,7 @@ public class SearchSortIT {
 					.analyzer( "default" ).sortable( Sortable.YES ).createAccessor();
 			integer = root.field( "integer" ).asInteger().sortable( Sortable.YES ).createAccessor();
 			localDate = root.field( "localDate" ).asLocalDate().sortable( Sortable.YES ).createAccessor();
+			geoPoint = root.field( "geoPoint" ).asGeoPoint().sortable( Sortable.YES ).createAccessor();
 
 			string_analyzed_forScore = root.field( "string_analyzed_forScore" ).asString()
 					.analyzer( "default" ).createAccessor();

util/internal/integrationtest/common/src/main/java/org/hibernate/search/v6poc/util/impl/integrationtest/common/stub/backend/search/sort/StubSearchSortFactory.java
@@ -7,7 +7,9 @@
 package org.hibernate.search.v6poc.util.impl.integrationtest.common.stub.backend.search.sort;
 
 import org.hibernate.search.v6poc.util.impl.integrationtest.common.stub.backend.search.StubQueryElementCollector;
+import org.hibernate.search.v6poc.backend.spatial.GeoPoint;
 import org.hibernate.search.v6poc.search.SearchSort;
+import org.hibernate.search.v6poc.search.sort.spi.DistanceSortBuilder;
 import org.hibernate.search.v6poc.search.sort.spi.FieldSortBuilder;
 import org.hibernate.search.v6poc.search.sort.spi.ScoreSortBuilder;
 import org.hibernate.search.v6poc.search.sort.spi.SearchSortContributor;
@@ -38,5 +40,10 @@ public class StubSearchSortFactory implements SearchSortFactory<StubQueryElement
 	@Override
 	public SearchSortContributor<StubQueryElementCollector> indexOrder() {
 		return new StubSearchSort();
+	}
+
+	@Override
+	public DistanceSortBuilder<StubQueryElementCollector> distance(String absoluteFieldPath, GeoPoint location) {
+		return new StubSortBuilder();
 	}
 }

util/internal/integrationtest/common/src/main/java/org/hibernate/search/v6poc/util/impl/integrationtest/common/stub/backend/search/sort/StubSortBuilder.java
@@ -6,13 +6,14 @@
  */
 package org.hibernate.search.v6poc.util.impl.integrationtest.common.stub.backend.search.sort;
 
-import org.hibernate.search.v6poc.util.impl.integrationtest.common.stub.backend.search.StubQueryElementCollector;
 import org.hibernate.search.v6poc.search.dsl.sort.SortOrder;
+import org.hibernate.search.v6poc.search.sort.spi.DistanceSortBuilder;
 import org.hibernate.search.v6poc.search.sort.spi.FieldSortBuilder;
 import org.hibernate.search.v6poc.search.sort.spi.ScoreSortBuilder;
+import org.hibernate.search.v6poc.util.impl.integrationtest.common.stub.backend.search.StubQueryElementCollector;
 
 public class StubSortBuilder implements ScoreSortBuilder<StubQueryElementCollector>,
-		FieldSortBuilder<StubQueryElementCollector> {
+		FieldSortBuilder<StubQueryElementCollector>, DistanceSortBuilder<StubQueryElementCollector> {
 
 	@Override
 	public void missingFirst() {

hibernate/hibernate-search

backend/lucene/src/main/java/org/hibernate/search/backend/lucene/types/sort/impl/AbstractStandardLuceneFieldSortContributor.java
@@ -6,10 +6,18 @@
  */
 package org.hibernate.search.backend.lucene.types.sort.impl;
 
+import java.lang.invoke.MethodHandles;
+
 import org.apache.lucene.search.SortField;
+import org.hibernate.search.backend.lucene.logging.impl.Log;
+import org.hibernate.search.backend.lucene.search.sort.impl.LuceneSearchSortCollector;
+import org.hibernate.search.engine.backend.spatial.GeoPoint;
 import org.hibernate.search.engine.search.dsl.sort.SortOrder;
+import org.hibernate.search.util.impl.common.LoggerFactory;
 
 abstract class AbstractStandardLuceneFieldSortContributor implements LuceneFieldSortContributor {
+
+	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
 
 	private Object sortMissingValueFirstPlaceholder;
 
@@ -38,5 +46,10 @@ abstract class AbstractStandardLuceneFieldSortContributor implements LuceneField
 		}
 
 		sortField.setMissingValue( effectiveMissingValue );
+	}
+
+	@Override
+	public void contributeDistanceSort(LuceneSearchSortCollector collector, String absoluteFieldPath, GeoPoint location, SortOrder order) {
+		throw log.distanceOperationsNotSupportedByFieldType( absoluteFieldPath );
 	}
 }

engine/src/main/java/org/hibernate/search/engine/search/dsl/sort/SearchSortContainerContext.java
@@ -8,6 +8,8 @@ package org.hibernate.search.engine.search.dsl.sort;
 
 import java.util.function.Consumer;
 
+import org.hibernate.search.engine.backend.spatial.GeoPoint;
+import org.hibernate.search.engine.backend.spatial.ImmutableGeoPoint;
 import org.hibernate.search.engine.search.SearchSort;
 import org.hibernate.search.engine.search.dsl.sort.spi.SearchSortContainerContextExtension;
 import org.hibernate.search.util.SearchException;
@@ -45,7 +47,34 @@ public interface SearchSortContainerContext<N> {
 	 */
 	FieldSortContext<N> byField(String absoluteFieldPath);
 
-	// TODO other sorts (spatial, ...)
+	/**
+	 * Order elements by the distance from the location stored in the specified field to the location specified.
+	 *
+	 * <p>The default order is <strong>ascending</strong>.
+	 *
+	 * @param absoluteFieldPath The absolute path of the indexed location field to sort by.
+	 * @param location The location to which we want to compute the distance.
+	 * @throws SearchException If the field type does not constitute a valid location.
+	 */
+	DistanceSortContext<N> byDistance(String absoluteFieldPath, GeoPoint location);
+
+	/**
+	 * Order elements by the distance from the location stored in the specified field to the location specified.
+	 *
+	 * <p>
+	 * The default order is <strong>ascending</strong>.
+	 *
+	 * @param absoluteFieldPath The absolute path of the indexed location field to sort by.
+	 * @param latitude The latitude of the location to which we want to compute the distance.
+	 * @param longitude The longitude of the location to which we want to compute the distance.
+	 * @throws SearchException If the field type does not constitute a valid location.
+	 */
+	default DistanceSortContext<N> byDistance(String absoluteFieldPath, double latitude, double longitude) {
+		return byDistance( absoluteFieldPath, new ImmutableGeoPoint( latitude, longitude ) );
+	}
+
+	// TODO other sorts
+
 
 	/**
 	 * Order by the given sort.

engine/src/main/java/org/hibernate/search/engine/search/dsl/sort/impl/DistanceSortContextImpl.java
@@ -1,0 +1,53 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.engine.search.dsl.sort.impl;
+
+import java.util.function.Supplier;
+
+import org.hibernate.search.engine.backend.spatial.GeoPoint;
+import org.hibernate.search.engine.search.dsl.sort.DistanceSortContext;
+import org.hibernate.search.engine.search.dsl.sort.SearchSortContainerContext;
+import org.hibernate.search.engine.search.dsl.sort.SortOrder;
+import org.hibernate.search.engine.search.sort.spi.DistanceSortBuilder;
+import org.hibernate.search.engine.search.sort.spi.SearchSortContributor;
+import org.hibernate.search.engine.search.sort.spi.SearchSortFactory;
+
+class DistanceSortContextImpl<N, C> implements DistanceSortContext<N>, SearchSortContributor<C> {
+
+	private final SearchSortContainerContext<N> containerContext;
+	private final Supplier<N> nextContextProvider;
+	private final DistanceSortBuilder<C> builder;
+
+	DistanceSortContextImpl(SearchSortContainerContext<N> containerContext,
+			SearchSortFactory<C> factory, Supplier<N> nextContextProvider,
+			String absoluteFieldPath, GeoPoint location) {
+		this.containerContext = containerContext;
+		this.nextContextProvider = nextContextProvider;
+		this.builder = factory.distance( absoluteFieldPath, location );
+	}
+
+	@Override
+	public DistanceSortContext<N> order(SortOrder order) {
+		builder.order( order );
+		return this;
+	}
+
+	@Override
+	public SearchSortContainerContext<N> then() {
+		return containerContext;
+	}
+
+	@Override
+	public N end() {
+		return nextContextProvider.get();
+	}
+
+	@Override
+	public void contribute(C collector) {
+		builder.contribute( collector );
+	}
+}

engine/src/main/java/org/hibernate/search/engine/search/sort/spi/SearchSortFactory.java
@@ -6,6 +6,7 @@
  */
 package org.hibernate.search.engine.search.sort.spi;
 
+import org.hibernate.search.engine.backend.spatial.GeoPoint;
 import org.hibernate.search.engine.search.SearchSort;
 
 /**
@@ -26,6 +27,8 @@ public interface SearchSortFactory<C> {
 	ScoreSortBuilder<C> score();
 
 	FieldSortBuilder<C> field(String absoluteFieldPath);
+
+	DistanceSortBuilder<C> distance(String absoluteFieldPath, GeoPoint location);
 
 	SearchSortContributor<C> indexOrder();
 

integrationtest/backend/tck/src/main/java/org/hibernate/search/integrationtest/backend/tck/search/SearchSortIT.java
@@ -18,17 +18,16 @@ import org.hibernate.search.engine.backend.document.DocumentElement;
 import org.hibernate.search.engine.backend.document.IndexFieldAccessor;
 import org.hibernate.search.engine.backend.document.IndexObjectFieldAccessor;
 import org.hibernate.search.engine.backend.document.model.dsl.IndexSchemaElement;
+import org.hibernate.search.engine.backend.document.model.dsl.IndexSchemaObjectField;
 import org.hibernate.search.engine.backend.document.model.dsl.ObjectFieldStorage;
 import org.hibernate.search.engine.backend.document.model.dsl.Sortable;
-import org.hibernate.search.engine.backend.document.model.dsl.IndexSchemaObjectField;
 import org.hibernate.search.engine.backend.index.spi.ChangesetIndexWorker;
 import org.hibernate.search.engine.backend.index.spi.IndexManager;
 import org.hibernate.search.engine.backend.index.spi.IndexSearchTarget;
+import org.hibernate.search.engine.backend.spatial.GeoPoint;
+import org.hibernate.search.engine.backend.spatial.ImmutableGeoPoint;
 import org.hibernate.search.engine.common.spi.SessionContext;
 import org.hibernate.search.integrationtest.backend.tck.util.rule.SearchSetupHelper;
-import org.hibernate.search.util.impl.integrationtest.common.assertion.DocumentReferencesSearchResultAssert;
-import org.hibernate.search.util.impl.integrationtest.common.assertion.SearchResultAssert;
-import org.hibernate.search.util.impl.integrationtest.common.stub.StubSessionContext;
 import org.hibernate.search.engine.search.DocumentReference;
 import org.hibernate.search.engine.search.SearchPredicate;
 import org.hibernate.search.engine.search.SearchQuery;
@@ -36,7 +35,9 @@ import org.hibernate.search.engine.search.SearchResult;
 import org.hibernate.search.engine.search.SearchSort;
 import org.hibernate.search.engine.search.dsl.sort.SearchSortContainerContext;
 import org.hibernate.search.util.SearchException;
-
+import org.hibernate.search.util.impl.integrationtest.common.assertion.DocumentReferencesSearchResultAssert;
+import org.hibernate.search.util.impl.integrationtest.common.assertion.SearchResultAssert;
+import org.hibernate.search.util.impl.integrationtest.common.stub.StubSessionContext;
 import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Rule;
@@ -256,6 +257,22 @@ public class SearchSortIT {
 	}
 
 	@Test
+	public void byDistance() {
+		SearchQuery<DocumentReference> query = simpleQuery( b -> b.byDistance( "geoPoint", new ImmutableGeoPoint( 45.757864, 4.834496 ) ) );
+
+		DocumentReferencesSearchResultAssert.assertThat( query )
+				.hasReferencesHitsExactOrder( indexName, FIRST_ID, THIRD_ID, SECOND_ID, EMPTY_ID );
+
+		query = simpleQuery( b -> b.byDistance( "geoPoint", 45.757864, 4.834496 ) );
+
+		DocumentReferencesSearchResultAssert.assertThat( query )
+				.hasReferencesHitsExactOrder( indexName, FIRST_ID, THIRD_ID, SECOND_ID, EMPTY_ID );
+
+		// we don't test the descending order here as it's currently not supported by Lucene
+		// see the additional tests in the specific backend tests
+	}
+
+	@Test
 	public void byField_error_unsortable() {
 		Assume.assumeTrue( "Errors on attempt to sort on unsortable fields are not supported yet", false );
 		// TODO throw an error on attempts to sort on unsortable fields
@@ -326,6 +343,7 @@ public class SearchSortIT {
 			indexAccessors.string_analyzed.write( document, "George" );
 			indexAccessors.integer.write( document, 2 );
 			indexAccessors.localDate.write( document, LocalDate.of( 2018, 2, 2 ) );
+			indexAccessors.geoPoint.write( document, new ImmutableGeoPoint( 45.7705687,4.835233 ) );
 
 			indexAccessors.string_analyzed_forScore.write( document, "Hooray Hooray" );
 			indexAccessors.unsortable.write( document, "george" );
@@ -347,6 +365,7 @@ public class SearchSortIT {
 			indexAccessors.string_analyzed.write( document, "Aaron" );
 			indexAccessors.integer.write( document, 1 );
 			indexAccessors.localDate.write( document, LocalDate.of( 2018, 2, 1 ) );
+			indexAccessors.geoPoint.write( document, new ImmutableGeoPoint( 45.7541719, 4.8386221 ) );
 
 			indexAccessors.string_analyzed_forScore.write( document, "Hooray Hooray Hooray" );
 			indexAccessors.unsortable.write( document, "aaron" );
@@ -368,6 +387,7 @@ public class SearchSortIT {
 			indexAccessors.string_analyzed.write( document, "Zach" );
 			indexAccessors.integer.write( document, 3 );
 			indexAccessors.localDate.write( document, LocalDate.of( 2019, 1, 2 ) );
+			indexAccessors.geoPoint.write( document, new ImmutableGeoPoint( 45.7530374, 4.8510299 ) );
 
 			indexAccessors.string_analyzed_forScore.write( document, "Hooray" );
 			indexAccessors.unsortable.write( document, "zach" );
@@ -402,6 +422,7 @@ public class SearchSortIT {
 		final IndexFieldAccessor<String> string_analyzed;
 		final IndexFieldAccessor<Integer> integer;
 		final IndexFieldAccessor<LocalDate> localDate;
+		final IndexFieldAccessor<GeoPoint> geoPoint;
 
 		final IndexFieldAccessor<String> string_analyzed_forScore;
 		final IndexFieldAccessor<String> unsortable;
@@ -417,6 +438,7 @@ public class SearchSortIT {
 					.analyzer( "default" ).sortable( Sortable.YES ).createAccessor();
 			integer = root.field( "integer" ).asInteger().sortable( Sortable.YES ).createAccessor();
 			localDate = root.field( "localDate" ).asLocalDate().sortable( Sortable.YES ).createAccessor();
+			geoPoint = root.field( "geoPoint" ).asGeoPoint().sortable( Sortable.YES ).createAccessor();
 
 			string_analyzed_forScore = root.field( "string_analyzed_forScore" ).asString()
 					.analyzer( "default" ).createAccessor();

util/internal/integrationtest/common/src/main/java/org/hibernate/search/util/impl/integrationtest/common/stub/backend/search/sort/StubSearchSortFactory.java
@@ -7,7 +7,9 @@
 package org.hibernate.search.util.impl.integrationtest.common.stub.backend.search.sort;
 
 import org.hibernate.search.util.impl.integrationtest.common.stub.backend.search.StubQueryElementCollector;
+import org.hibernate.search.engine.backend.spatial.GeoPoint;
 import org.hibernate.search.engine.search.SearchSort;
+import org.hibernate.search.engine.search.sort.spi.DistanceSortBuilder;
 import org.hibernate.search.engine.search.sort.spi.FieldSortBuilder;
 import org.hibernate.search.engine.search.sort.spi.ScoreSortBuilder;
 import org.hibernate.search.engine.search.sort.spi.SearchSortContributor;
@@ -38,5 +40,10 @@ public class StubSearchSortFactory implements SearchSortFactory<StubQueryElement
 	@Override
 	public SearchSortContributor<StubQueryElementCollector> indexOrder() {
 		return new StubSearchSort();
+	}
+
+	@Override
+	public DistanceSortBuilder<StubQueryElementCollector> distance(String absoluteFieldPath, GeoPoint location) {
+		return new StubSortBuilder();
 	}
 }

util/internal/integrationtest/common/src/main/java/org/hibernate/search/util/impl/integrationtest/common/stub/backend/search/sort/StubSortBuilder.java
@@ -6,13 +6,14 @@
  */
 package org.hibernate.search.util.impl.integrationtest.common.stub.backend.search.sort;
 
-import org.hibernate.search.util.impl.integrationtest.common.stub.backend.search.StubQueryElementCollector;
 import org.hibernate.search.engine.search.dsl.sort.SortOrder;
+import org.hibernate.search.engine.search.sort.spi.DistanceSortBuilder;
 import org.hibernate.search.engine.search.sort.spi.FieldSortBuilder;
 import org.hibernate.search.engine.search.sort.spi.ScoreSortBuilder;
+import org.hibernate.search.util.impl.integrationtest.common.stub.backend.search.StubQueryElementCollector;
 
 public class StubSortBuilder implements ScoreSortBuilder<StubQueryElementCollector>,
-		FieldSortBuilder<StubQueryElementCollector> {
+		FieldSortBuilder<StubQueryElementCollector>, DistanceSortBuilder<StubQueryElementCollector> {
 
 	@Override
 	public void missingFirst() {