hibernate/hibernate-search-6-poc

HSEARCH-2546 Clean up and test error messages during the mapping

engine/src/main/java/org/hibernate/search/v6poc/logging/spi/MappableTypeModelFormatter.java
@@ -1,0 +1,23 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.v6poc.logging.spi;
+
+import org.hibernate.search.v6poc.entity.model.spi.MappableTypeModel;
+
+public class MappableTypeModelFormatter {
+
+	private final String formatted;
+
+	public MappableTypeModelFormatter(MappableTypeModel typeModel) {
+		this.formatted = typeModel.getName();
+	}
+
+	@Override
+	public String toString() {
+		return formatted;
+	}
+}

hibernate/hibernate-search

engine/src/main/java/org/hibernate/search/engine/logging/spi/MappableTypeModelFormatter.java
@@ -1,0 +1,23 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.engine.logging.spi;
+
+import org.hibernate.search.engine.mapper.model.spi.MappableTypeModel;
+
+public class MappableTypeModelFormatter {
+
+	private final String formatted;
+
+	public MappableTypeModelFormatter(MappableTypeModel typeModel) {
+		this.formatted = typeModel.getName();
+	}
+
+	@Override
+	public String toString() {
+		return formatted;
+	}
+}