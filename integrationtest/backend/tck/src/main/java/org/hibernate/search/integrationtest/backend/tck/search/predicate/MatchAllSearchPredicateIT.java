hibernate/hibernate-search-6-poc

HSEARCH-3060 Add a test for MatchAll predicate

integrationtest/backend-tck/src/main/java/org/hibernate/search/v6poc/integrationtest/backend/tck/search/predicate/MatchAllSearchPredicateIT.java
@@ -1,0 +1,137 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.v6poc.integrationtest.backend.tck.search.predicate;
+
+import static org.hibernate.search.v6poc.util.impl.integrationtest.common.stub.mapper.StubMapperUtils.referenceProvider;
+
+import org.hibernate.search.v6poc.backend.document.DocumentElement;
+import org.hibernate.search.v6poc.backend.document.IndexFieldAccessor;
+import org.hibernate.search.v6poc.backend.document.model.dsl.IndexSchemaElement;
+import org.hibernate.search.v6poc.backend.index.spi.ChangesetIndexWorker;
+import org.hibernate.search.v6poc.backend.index.spi.IndexManager;
+import org.hibernate.search.v6poc.backend.index.spi.IndexSearchTarget;
+import org.hibernate.search.v6poc.engine.spi.SessionContext;
+import org.hibernate.search.v6poc.integrationtest.backend.tck.util.rule.SearchSetupHelper;
+import org.hibernate.search.v6poc.search.DocumentReference;
+import org.hibernate.search.v6poc.search.SearchPredicate;
+import org.hibernate.search.v6poc.search.SearchQuery;
+import org.hibernate.search.v6poc.util.impl.integrationtest.common.assertion.DocumentReferencesSearchResultAssert;
+import org.hibernate.search.v6poc.util.impl.integrationtest.common.stub.StubSessionContext;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class MatchAllSearchPredicateIT {
+
+	private static final String DOCUMENT_1 = "1";
+	private static final String STRING_1 = "aaa";
+
+	private static final String DOCUMENT_2 = "2";
+	private static final String STRING_2 = "bbb";
+
+	private static final String DOCUMENT_3 = "3";
+	private static final String STRING_3 = "ccc";
+
+	@Rule
+	public SearchSetupHelper setupHelper = new SearchSetupHelper();
+
+	private IndexAccessors indexAccessors;
+	private IndexManager<?> indexManager;
+	private String indexName;
+	private SessionContext sessionContext = new StubSessionContext();
+
+	@Before
+	public void setup() {
+		setupHelper.withDefaultConfiguration()
+				.withIndex(
+						"MappedType", "IndexName",
+						ctx -> this.indexAccessors = new IndexAccessors( ctx.getSchemaElement() ),
+						(indexManager, indexName) -> {
+							this.indexManager = indexManager;
+							this.indexName = indexName;
+						}
+				)
+				.setup();
+
+		initData();
+	}
+
+	@Test
+	public void matchAll() {
+		IndexSearchTarget searchTarget = indexManager.createSearchTarget().build();
+
+		SearchQuery<DocumentReference> query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().all().end()
+				.build();
+
+		DocumentReferencesSearchResultAssert.assertThat( query )
+				.hasReferencesHitsAnyOrder( indexName, DOCUMENT_1, DOCUMENT_2, DOCUMENT_3 );
+	}
+
+	@Test
+	public void matchAll_except() {
+		IndexSearchTarget searchTarget = indexManager.createSearchTarget().build();
+
+		SearchQuery<DocumentReference> query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().all().except().match().onField( "string" ).matching( STRING_1 ).end()
+				.build();
+
+		DocumentReferencesSearchResultAssert.assertThat( query )
+				.hasReferencesHitsAnyOrder( indexName, DOCUMENT_2, DOCUMENT_3 );
+
+		query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().all().except( c -> c.match().onField( "string" ).matching( STRING_2 ) ).end()
+				.build();
+
+		DocumentReferencesSearchResultAssert.assertThat( query )
+				.hasReferencesHitsAnyOrder( indexName, DOCUMENT_1, DOCUMENT_3 );
+
+		SearchPredicate searchPredicate = searchTarget.predicate().match().onField( "string" ).matching( STRING_3 );
+
+		query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().all().except( searchPredicate ).end()
+				.build();
+
+		DocumentReferencesSearchResultAssert.assertThat( query )
+				.hasReferencesHitsAnyOrder( indexName, DOCUMENT_1, DOCUMENT_2 );
+	}
+
+	private void initData() {
+		ChangesetIndexWorker<? extends DocumentElement> worker = indexManager.createWorker( sessionContext );
+		worker.add( referenceProvider( DOCUMENT_1 ), document -> {
+			indexAccessors.string.write( document, STRING_1 );
+		} );
+		worker.add( referenceProvider( DOCUMENT_2 ), document -> {
+			indexAccessors.string.write( document, STRING_2 );
+		} );
+		worker.add( referenceProvider( DOCUMENT_3 ), document -> {
+			indexAccessors.string.write( document, STRING_3 );
+		} );
+
+		worker.execute().join();
+
+		// Check that all documents are searchable
+		IndexSearchTarget searchTarget = indexManager.createSearchTarget().build();
+		SearchQuery<DocumentReference> query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().all().end()
+				.build();
+		DocumentReferencesSearchResultAssert.assertThat( query ).hasReferencesHitsAnyOrder( indexName, DOCUMENT_1, DOCUMENT_2, DOCUMENT_3 );
+	}
+
+	private static class IndexAccessors {
+		final IndexFieldAccessor<String> string;
+
+		IndexAccessors(IndexSchemaElement root) {
+			string = root.field( "string" ).asString().createAccessor();
+		}
+	}
+}

hibernate/hibernate-search

integrationtest/backend/tck/src/main/java/org/hibernate/search/integrationtest/backend/tck/search/predicate/MatchAllSearchPredicateIT.java
@@ -1,0 +1,137 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.integrationtest.backend.tck.search.predicate;
+
+import static org.hibernate.search.util.impl.integrationtest.common.stub.mapper.StubMapperUtils.referenceProvider;
+
+import org.hibernate.search.engine.backend.document.DocumentElement;
+import org.hibernate.search.engine.backend.document.IndexFieldAccessor;
+import org.hibernate.search.engine.backend.document.model.dsl.IndexSchemaElement;
+import org.hibernate.search.engine.backend.index.spi.ChangesetIndexWorker;
+import org.hibernate.search.engine.backend.index.spi.IndexManager;
+import org.hibernate.search.engine.backend.index.spi.IndexSearchTarget;
+import org.hibernate.search.engine.common.spi.SessionContext;
+import org.hibernate.search.integrationtest.backend.tck.util.rule.SearchSetupHelper;
+import org.hibernate.search.engine.search.DocumentReference;
+import org.hibernate.search.engine.search.SearchPredicate;
+import org.hibernate.search.engine.search.SearchQuery;
+import org.hibernate.search.util.impl.integrationtest.common.assertion.DocumentReferencesSearchResultAssert;
+import org.hibernate.search.util.impl.integrationtest.common.stub.StubSessionContext;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class MatchAllSearchPredicateIT {
+
+	private static final String DOCUMENT_1 = "1";
+	private static final String STRING_1 = "aaa";
+
+	private static final String DOCUMENT_2 = "2";
+	private static final String STRING_2 = "bbb";
+
+	private static final String DOCUMENT_3 = "3";
+	private static final String STRING_3 = "ccc";
+
+	@Rule
+	public SearchSetupHelper setupHelper = new SearchSetupHelper();
+
+	private IndexAccessors indexAccessors;
+	private IndexManager<?> indexManager;
+	private String indexName;
+	private SessionContext sessionContext = new StubSessionContext();
+
+	@Before
+	public void setup() {
+		setupHelper.withDefaultConfiguration()
+				.withIndex(
+						"MappedType", "IndexName",
+						ctx -> this.indexAccessors = new IndexAccessors( ctx.getSchemaElement() ),
+						(indexManager, indexName) -> {
+							this.indexManager = indexManager;
+							this.indexName = indexName;
+						}
+				)
+				.setup();
+
+		initData();
+	}
+
+	@Test
+	public void matchAll() {
+		IndexSearchTarget searchTarget = indexManager.createSearchTarget().build();
+
+		SearchQuery<DocumentReference> query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().all().end()
+				.build();
+
+		DocumentReferencesSearchResultAssert.assertThat( query )
+				.hasReferencesHitsAnyOrder( indexName, DOCUMENT_1, DOCUMENT_2, DOCUMENT_3 );
+	}
+
+	@Test
+	public void matchAll_except() {
+		IndexSearchTarget searchTarget = indexManager.createSearchTarget().build();
+
+		SearchQuery<DocumentReference> query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().all().except().match().onField( "string" ).matching( STRING_1 ).end()
+				.build();
+
+		DocumentReferencesSearchResultAssert.assertThat( query )
+				.hasReferencesHitsAnyOrder( indexName, DOCUMENT_2, DOCUMENT_3 );
+
+		query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().all().except( c -> c.match().onField( "string" ).matching( STRING_2 ) ).end()
+				.build();
+
+		DocumentReferencesSearchResultAssert.assertThat( query )
+				.hasReferencesHitsAnyOrder( indexName, DOCUMENT_1, DOCUMENT_3 );
+
+		SearchPredicate searchPredicate = searchTarget.predicate().match().onField( "string" ).matching( STRING_3 );
+
+		query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().all().except( searchPredicate ).end()
+				.build();
+
+		DocumentReferencesSearchResultAssert.assertThat( query )
+				.hasReferencesHitsAnyOrder( indexName, DOCUMENT_1, DOCUMENT_2 );
+	}
+
+	private void initData() {
+		ChangesetIndexWorker<? extends DocumentElement> worker = indexManager.createWorker( sessionContext );
+		worker.add( referenceProvider( DOCUMENT_1 ), document -> {
+			indexAccessors.string.write( document, STRING_1 );
+		} );
+		worker.add( referenceProvider( DOCUMENT_2 ), document -> {
+			indexAccessors.string.write( document, STRING_2 );
+		} );
+		worker.add( referenceProvider( DOCUMENT_3 ), document -> {
+			indexAccessors.string.write( document, STRING_3 );
+		} );
+
+		worker.execute().join();
+
+		// Check that all documents are searchable
+		IndexSearchTarget searchTarget = indexManager.createSearchTarget().build();
+		SearchQuery<DocumentReference> query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().all().end()
+				.build();
+		DocumentReferencesSearchResultAssert.assertThat( query ).hasReferencesHitsAnyOrder( indexName, DOCUMENT_1, DOCUMENT_2, DOCUMENT_3 );
+	}
+
+	private static class IndexAccessors {
+		final IndexFieldAccessor<String> string;
+
+		IndexAccessors(IndexSchemaElement root) {
+			string = root.field( "string" ).asString().createAccessor();
+		}
+	}
+}