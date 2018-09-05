hibernate/hibernate-search-6-poc

HSEARCH-3087 Force a check that the polygon is properly closed

engine/src/main/java/org/hibernate/search/v6poc/logging/impl/Log.java
@@ -10,8 +10,8 @@ package org.hibernate.search.v6poc.logging.impl;
 import java.util.List;
 
 import org.hibernate.search.v6poc.entity.model.spi.MappableTypeModel;
+import org.hibernate.search.v6poc.spatial.GeoPoint;
 import org.hibernate.search.v6poc.util.SearchException;
-
 import org.jboss.logging.BasicLogger;
 import org.jboss.logging.annotations.Cause;
 import org.jboss.logging.annotations.Message;
@@ -66,4 +66,7 @@ public interface Log extends BasicLogger {
 			+ " or if you are declaring the field in a bridge and want a tree of fields,"
 			+ " declare an object field using the objectField() method." )
 	SearchException relativeFieldNameCannotContainDot(String relativeFieldName);
+
+	@Message(id = 16, value = "Invalid polygon: the first point '%1$s' should be identical to the last point '%2$s' to properly close the polygon." )
+	IllegalArgumentException invalidGeoPolygonFirstPointNotIdenticalToLastPoint(GeoPoint firstPoint, GeoPoint lastPoint);
 }

engine/src/main/java/org/hibernate/search/v6poc/spatial/ImmutableGeoPolygon.java
@@ -6,19 +6,30 @@
  */
 package org.hibernate.search.v6poc.spatial;
 
+import java.lang.invoke.MethodHandles;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.hibernate.search.v6poc.logging.impl.Log;
 import org.hibernate.search.v6poc.util.impl.common.CollectionHelper;
 import org.hibernate.search.v6poc.util.impl.common.Contracts;
+import org.hibernate.search.v6poc.util.impl.common.LoggerFactory;
 
 public class ImmutableGeoPolygon implements GeoPolygon {
+
+	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
 
 	private List<GeoPoint> points;
 
 	public ImmutableGeoPolygon(List<GeoPoint> points) {
 		Contracts.assertNotNull( points, "points" );
+
+		GeoPoint firstPoint = points.get( 0 );
+		GeoPoint lastPoint = points.get( points.size() - 1 );
+		if ( !firstPoint.equals( lastPoint ) ) {
+			throw log.invalidGeoPolygonFirstPointNotIdenticalToLastPoint( firstPoint, lastPoint );
+		}
 
 		this.points = CollectionHelper.toImmutableList( new ArrayList<>( points ) );
 	}
@@ -35,6 +46,11 @@ public class ImmutableGeoPolygon implements GeoPolygon {
 		points.add( thirdPoint );
 		points.add( fourthPoint );
 		Collections.addAll( points, additionalPoints );
+
+		GeoPoint lastPoint = points.get( points.size() - 1 );
+		if ( !firstPoint.equals( lastPoint ) ) {
+			throw log.invalidGeoPolygonFirstPointNotIdenticalToLastPoint( firstPoint, lastPoint );
+		}
 
 		this.points = CollectionHelper.toImmutableList( points );
 	}

engine/src/test/java/org/hibernate/search/v6poc/spatial/GeoPolygonTest.java
@@ -1,0 +1,56 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.v6poc.spatial;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Arrays;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class GeoPolygonTest {
+
+	@Rule
+	public final ExpectedException thrown = ExpectedException.none();
+
+	@Test
+	public void validPolygon() {
+		GeoPolygon polygon = new ImmutableGeoPolygon( new ImmutableGeoPoint( 26, 23 ), new ImmutableGeoPoint( 26, 26 ), new ImmutableGeoPoint( 24, 26 ),
+				new ImmutableGeoPoint( 24, 23 ), new ImmutableGeoPoint( 26, 23 ) );
+		assertNotNull( polygon );
+
+		polygon = new ImmutableGeoPolygon( Arrays.asList( new ImmutableGeoPoint( 26, 23 ), new ImmutableGeoPoint( 26, 26 ), new ImmutableGeoPoint( 24, 26 ),
+				new ImmutableGeoPoint( 24, 23 ), new ImmutableGeoPoint( 26, 23 ) ) );
+		assertNotNull( polygon );
+	}
+
+	@Test
+	public void invalidPolygon() {
+		try {
+			new ImmutableGeoPolygon( new ImmutableGeoPoint( 26, 23 ), new ImmutableGeoPoint( 26, 26 ), new ImmutableGeoPoint( 24, 26 ),
+					new ImmutableGeoPoint( 24, 23 ) );
+		}
+		catch (Exception e) {
+			Assertions.assertThat( e )
+					.isInstanceOf( IllegalArgumentException.class )
+					.hasMessageContaining( "HSEARCH000016" );
+		}
+
+		try {
+			new ImmutableGeoPolygon( Arrays.asList( new ImmutableGeoPoint( 26, 23 ), new ImmutableGeoPoint( 26, 26 ), new ImmutableGeoPoint( 24, 26 ),
+					new ImmutableGeoPoint( 24, 23 ) ) );
+		}
+		catch (Exception e) {
+			Assertions.assertThat( e )
+					.isInstanceOf( IllegalArgumentException.class )
+					.hasMessageContaining( "HSEARCH000016" );
+		}
+	}
+}

hibernate/hibernate-search

engine/src/main/java/org/hibernate/search/engine/logging/impl/Log.java
@@ -10,8 +10,8 @@ package org.hibernate.search.engine.logging.impl;
 import java.util.List;
 
 import org.hibernate.search.engine.mapper.model.spi.MappableTypeModel;
+import org.hibernate.search.engine.spatial.GeoPoint;
 import org.hibernate.search.util.SearchException;
-
 import org.jboss.logging.BasicLogger;
 import org.jboss.logging.annotations.Cause;
 import org.jboss.logging.annotations.Message;
@@ -66,4 +66,7 @@ public interface Log extends BasicLogger {
 			+ " or if you are declaring the field in a bridge and want a tree of fields,"
 			+ " declare an object field using the objectField() method." )
 	SearchException relativeFieldNameCannotContainDot(String relativeFieldName);
+
+	@Message(id = 16, value = "Invalid polygon: the first point '%1$s' should be identical to the last point '%2$s' to properly close the polygon." )
+	IllegalArgumentException invalidGeoPolygonFirstPointNotIdenticalToLastPoint(GeoPoint firstPoint, GeoPoint lastPoint);
 }

engine/src/main/java/org/hibernate/search/engine/spatial/ImmutableGeoPolygon.java
@@ -6,19 +6,30 @@
  */
 package org.hibernate.search.engine.spatial;
 
+import java.lang.invoke.MethodHandles;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.hibernate.search.engine.logging.impl.Log;
 import org.hibernate.search.util.impl.common.CollectionHelper;
 import org.hibernate.search.util.impl.common.Contracts;
+import org.hibernate.search.util.impl.common.LoggerFactory;
 
 public class ImmutableGeoPolygon implements GeoPolygon {
+
+	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
 
 	private List<GeoPoint> points;
 
 	public ImmutableGeoPolygon(List<GeoPoint> points) {
 		Contracts.assertNotNull( points, "points" );
+
+		GeoPoint firstPoint = points.get( 0 );
+		GeoPoint lastPoint = points.get( points.size() - 1 );
+		if ( !firstPoint.equals( lastPoint ) ) {
+			throw log.invalidGeoPolygonFirstPointNotIdenticalToLastPoint( firstPoint, lastPoint );
+		}
 
 		this.points = CollectionHelper.toImmutableList( new ArrayList<>( points ) );
 	}
@@ -35,6 +46,11 @@ public class ImmutableGeoPolygon implements GeoPolygon {
 		points.add( thirdPoint );
 		points.add( fourthPoint );
 		Collections.addAll( points, additionalPoints );
+
+		GeoPoint lastPoint = points.get( points.size() - 1 );
+		if ( !firstPoint.equals( lastPoint ) ) {
+			throw log.invalidGeoPolygonFirstPointNotIdenticalToLastPoint( firstPoint, lastPoint );
+		}
 
 		this.points = CollectionHelper.toImmutableList( points );
 	}

engine/src/test/java/org/hibernate/search/engine/spatial/GeoPolygonTest.java
@@ -1,0 +1,56 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.engine.spatial;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Arrays;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class GeoPolygonTest {
+
+	@Rule
+	public final ExpectedException thrown = ExpectedException.none();
+
+	@Test
+	public void validPolygon() {
+		GeoPolygon polygon = new ImmutableGeoPolygon( new ImmutableGeoPoint( 26, 23 ), new ImmutableGeoPoint( 26, 26 ), new ImmutableGeoPoint( 24, 26 ),
+				new ImmutableGeoPoint( 24, 23 ), new ImmutableGeoPoint( 26, 23 ) );
+		assertNotNull( polygon );
+
+		polygon = new ImmutableGeoPolygon( Arrays.asList( new ImmutableGeoPoint( 26, 23 ), new ImmutableGeoPoint( 26, 26 ), new ImmutableGeoPoint( 24, 26 ),
+				new ImmutableGeoPoint( 24, 23 ), new ImmutableGeoPoint( 26, 23 ) ) );
+		assertNotNull( polygon );
+	}
+
+	@Test
+	public void invalidPolygon() {
+		try {
+			new ImmutableGeoPolygon( new ImmutableGeoPoint( 26, 23 ), new ImmutableGeoPoint( 26, 26 ), new ImmutableGeoPoint( 24, 26 ),
+					new ImmutableGeoPoint( 24, 23 ) );
+		}
+		catch (Exception e) {
+			Assertions.assertThat( e )
+					.isInstanceOf( IllegalArgumentException.class )
+					.hasMessageContaining( "HSEARCH000016" );
+		}
+
+		try {
+			new ImmutableGeoPolygon( Arrays.asList( new ImmutableGeoPoint( 26, 23 ), new ImmutableGeoPoint( 26, 26 ), new ImmutableGeoPoint( 24, 26 ),
+					new ImmutableGeoPoint( 24, 23 ) ) );
+		}
+		catch (Exception e) {
+			Assertions.assertThat( e )
+					.isInstanceOf( IllegalArgumentException.class )
+					.hasMessageContaining( "HSEARCH000016" );
+		}
+	}
+}