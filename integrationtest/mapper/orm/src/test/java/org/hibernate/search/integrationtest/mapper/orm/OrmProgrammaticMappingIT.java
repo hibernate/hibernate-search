hibernate/hibernate-search-6-poc

HSEARCH-3083 Test that all resources are closed as expected when startup fails.

integrationtest/orm/src/test/java/org/hibernate/search/v6poc/integrationtest/orm/OrmProgrammaticMappingIT.java
@@ -7,7 +7,6 @@
 package org.hibernate.search.v6poc.integrationtest.orm;
 
 import static org.hibernate.search.v6poc.util.impl.integrationtest.common.stub.backend.StubBackendUtils.reference;
-import static org.junit.Assert.assertEquals;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -50,13 +49,12 @@ import org.hibernate.search.v6poc.integrationtest.orm.bridge.IntegerAsStringValu
 import org.hibernate.search.v6poc.integrationtest.orm.bridge.OptionalIntAsStringValueBridge;
 import org.hibernate.search.v6poc.integrationtest.orm.usertype.OptionalIntUserType;
 import org.hibernate.search.v6poc.integrationtest.orm.usertype.OptionalStringUserType;
+import org.hibernate.search.v6poc.search.ProjectionConstants;
 import org.hibernate.search.v6poc.util.impl.integrationtest.common.rule.BackendMock;
-import org.hibernate.search.v6poc.util.impl.test.rule.StaticCounters;
 import org.hibernate.search.v6poc.util.impl.integrationtest.common.rule.StubSearchWorkBehavior;
 import org.hibernate.search.v6poc.util.impl.integrationtest.common.stub.backend.index.impl.StubBackendFactory;
-import org.hibernate.search.v6poc.util.impl.integrationtest.common.stub.backend.index.impl.StubIndexManager;
 import org.hibernate.search.v6poc.util.impl.integrationtest.orm.OrmUtils;
-import org.hibernate.search.v6poc.search.ProjectionConstants;
+import org.hibernate.search.v6poc.util.impl.test.rule.StaticCounters;
 import org.hibernate.service.ServiceRegistry;
 
 import org.junit.After;
@@ -168,34 +166,6 @@ public class OrmProgrammaticMappingIT {
 		if ( sessionFactory != null ) {
 			sessionFactory.close();
 		}
-	}
-
-	@Test
-	public void lifecycle() {
-		// More bridges may have been instantiated, but only the below number should be active
-		// (the others should have been dropped because they were completely filtered out)
-		assertEquals( 4, counters.get( CustomTypeBridge.INSTANCE_COUNTER_KEY )
-				- counters.get( CustomTypeBridge.CLOSE_COUNTER_KEY ) );
-		assertEquals( 3, counters.get( CustomPropertyBridge.INSTANCE_COUNTER_KEY )
-				- counters.get( CustomPropertyBridge.CLOSE_COUNTER_KEY ) );
-		assertEquals( 1, counters.get( IntegerAsStringValueBridge.INSTANCE_COUNTER_KEY )
-				- counters.get( IntegerAsStringValueBridge.CLOSE_COUNTER_KEY ) );
-		assertEquals( 1, counters.get( OptionalIntAsStringValueBridge.INSTANCE_COUNTER_KEY )
-				- counters.get( OptionalIntAsStringValueBridge.CLOSE_COUNTER_KEY ) );
-		assertEquals( 3, counters.get( StubIndexManager.INSTANCE_COUNTER_KEY ) );
-		sessionFactory.close();
-		sessionFactory = null;
-		// All instantiated resources should have been closed
-		assertEquals( 0, counters.get( CustomTypeBridge.INSTANCE_COUNTER_KEY )
-				- counters.get( CustomTypeBridge.CLOSE_COUNTER_KEY ) );
-		assertEquals( 0, counters.get( CustomPropertyBridge.INSTANCE_COUNTER_KEY )
-				- counters.get( CustomPropertyBridge.CLOSE_COUNTER_KEY ) );
-		assertEquals( 0, counters.get( IntegerAsStringValueBridge.INSTANCE_COUNTER_KEY )
-				- counters.get( IntegerAsStringValueBridge.CLOSE_COUNTER_KEY ) );
-		assertEquals( 0, counters.get( OptionalIntAsStringValueBridge.INSTANCE_COUNTER_KEY )
-				- counters.get( OptionalIntAsStringValueBridge.CLOSE_COUNTER_KEY ) );
-		assertEquals( 0, counters.get( StubIndexManager.INSTANCE_COUNTER_KEY )
-				- counters.get( StubIndexManager.CLOSE_COUNTER_KEY ) );
 	}
 
 	@Test

integrationtest/mapper-pojo/src/test/java/org/hibernate/search/v6poc/integrationtest/mapper/pojo/JavaBeanProgrammaticMappingIT.java
@@ -8,7 +8,6 @@ package org.hibernate.search.v6poc.integrationtest.mapper.pojo;
 
 import static org.hibernate.search.v6poc.util.impl.integrationtest.common.assertion.SearchResultAssert.assertThat;
 import static org.hibernate.search.v6poc.util.impl.integrationtest.common.stub.backend.StubBackendUtils.reference;
-import static org.junit.Assert.assertEquals;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -34,13 +33,12 @@ import org.hibernate.search.v6poc.integrationtest.mapper.pojo.bridge.CustomPrope
 import org.hibernate.search.v6poc.integrationtest.mapper.pojo.bridge.CustomTypeBridge;
 import org.hibernate.search.v6poc.integrationtest.mapper.pojo.bridge.IntegerAsStringValueBridge;
 import org.hibernate.search.v6poc.integrationtest.mapper.pojo.bridge.OptionalIntAsStringValueBridge;
-import org.hibernate.search.v6poc.util.impl.integrationtest.common.rule.BackendMock;
-import org.hibernate.search.v6poc.util.impl.test.rule.StaticCounters;
-import org.hibernate.search.v6poc.util.impl.integrationtest.common.rule.StubSearchWorkBehavior;
-import org.hibernate.search.v6poc.util.impl.integrationtest.common.stub.backend.index.impl.StubBackendFactory;
-import org.hibernate.search.v6poc.util.impl.integrationtest.common.stub.backend.index.impl.StubIndexManager;
 import org.hibernate.search.v6poc.search.ProjectionConstants;
 import org.hibernate.search.v6poc.search.SearchQuery;
+import org.hibernate.search.v6poc.util.impl.integrationtest.common.rule.BackendMock;
+import org.hibernate.search.v6poc.util.impl.integrationtest.common.rule.StubSearchWorkBehavior;
+import org.hibernate.search.v6poc.util.impl.integrationtest.common.stub.backend.index.impl.StubBackendFactory;
+import org.hibernate.search.v6poc.util.impl.test.rule.StaticCounters;
 
 import org.junit.After;
 import org.junit.Before;
@@ -210,34 +208,6 @@ public class JavaBeanProgrammaticMappingIT {
 		if ( mappingRepository != null ) {
 			mappingRepository.close();
 		}
-	}
-
-	@Test
-	public void lifecycle() {
-		// More bridges may have been instantiated, but only the below number should be active
-		// (the others should have been dropped because they were completely filtered out)
-		assertEquals( 4, counters.get( CustomTypeBridge.INSTANCE_COUNTER_KEY )
-				- counters.get( CustomTypeBridge.CLOSE_COUNTER_KEY ) );
-		assertEquals( 4, counters.get( CustomPropertyBridge.INSTANCE_COUNTER_KEY )
-				- counters.get( CustomPropertyBridge.CLOSE_COUNTER_KEY ) );
-		assertEquals( 1, counters.get( IntegerAsStringValueBridge.INSTANCE_COUNTER_KEY )
-				- counters.get( IntegerAsStringValueBridge.CLOSE_COUNTER_KEY ) );
-		assertEquals( 1, counters.get( OptionalIntAsStringValueBridge.INSTANCE_COUNTER_KEY )
-				- counters.get( OptionalIntAsStringValueBridge.CLOSE_COUNTER_KEY ) );
-		assertEquals( 3, counters.get( StubIndexManager.INSTANCE_COUNTER_KEY ) );
-		mappingRepository.close();
-		mappingRepository = null;
-		// All instantiated resources should have been closed
-		assertEquals( 0, counters.get( CustomTypeBridge.INSTANCE_COUNTER_KEY )
-				- counters.get( CustomTypeBridge.CLOSE_COUNTER_KEY ) );
-		assertEquals( 0, counters.get( CustomPropertyBridge.INSTANCE_COUNTER_KEY )
-				- counters.get( CustomPropertyBridge.CLOSE_COUNTER_KEY ) );
-		assertEquals( 0, counters.get( IntegerAsStringValueBridge.INSTANCE_COUNTER_KEY )
-				- counters.get( IntegerAsStringValueBridge.CLOSE_COUNTER_KEY ) );
-		assertEquals( 0, counters.get( OptionalIntAsStringValueBridge.INSTANCE_COUNTER_KEY )
-				- counters.get( OptionalIntAsStringValueBridge.CLOSE_COUNTER_KEY ) );
-		assertEquals( 0, counters.get( StubIndexManager.INSTANCE_COUNTER_KEY )
-				- counters.get( StubIndexManager.CLOSE_COUNTER_KEY ) );
 	}
 
 	@Test

integrationtest/mapper-pojo/src/test/java/org/hibernate/search/v6poc/integrationtest/mapper/pojo/bridge/StartupStubBridge.java
@@ -1,0 +1,116 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.v6poc.integrationtest.mapper.pojo.bridge;
+
+import org.hibernate.search.v6poc.backend.document.DocumentElement;
+import org.hibernate.search.v6poc.backend.document.model.dsl.IndexSchemaElement;
+import org.hibernate.search.v6poc.entity.model.SearchModel;
+import org.hibernate.search.v6poc.entity.pojo.bridge.IdentifierBridge;
+import org.hibernate.search.v6poc.entity.pojo.bridge.PropertyBridge;
+import org.hibernate.search.v6poc.entity.pojo.bridge.RoutingKeyBridge;
+import org.hibernate.search.v6poc.entity.pojo.bridge.TypeBridge;
+import org.hibernate.search.v6poc.entity.pojo.bridge.ValueBridge;
+import org.hibernate.search.v6poc.entity.pojo.model.PojoElement;
+import org.hibernate.search.v6poc.entity.pojo.model.PojoModelElement;
+import org.hibernate.search.v6poc.entity.pojo.model.PojoModelProperty;
+import org.hibernate.search.v6poc.entity.pojo.model.PojoModelType;
+import org.hibernate.search.v6poc.util.AssertionFailure;
+import org.hibernate.search.v6poc.util.impl.test.rule.StaticCounters;
+
+/**
+ * A stub bridge for use in startup tests. Any runtime use of this bridge will fail.
+ * <p>
+ * For our own convenience, all bridge types are implemented in the same class.
+ */
+public class StartupStubBridge
+		implements TypeBridge, PropertyBridge, ValueBridge<Object, String>,
+		RoutingKeyBridge, IdentifierBridge<Object> {
+	public static class CounterKeys {
+		public final StaticCounters.Key instance = StaticCounters.createKey();
+		public final StaticCounters.Key close = StaticCounters.createKey();
+
+		private CounterKeys() {
+		}
+	}
+
+	public static CounterKeys createKeys() {
+		return new CounterKeys();
+	}
+
+	public final CounterKeys counterKeys;
+
+	private boolean closed = false;
+
+	public StartupStubBridge(CounterKeys counterKeys) {
+		StaticCounters.get().increment( counterKeys.instance );
+		this.counterKeys = counterKeys;
+	}
+
+	@Override
+	public void close() {
+		/*
+		 * This is important so that multiple calls to close on a single bridge
+		 * won't be interpreted as closing multiple objects in test assertions.
+		 */
+		if ( closed ) {
+			return;
+		}
+		StaticCounters.get().increment( counterKeys.close );
+		closed = true;
+	}
+
+	@Override
+	public void bind(IndexSchemaElement indexSchemaElement, PojoModelProperty bridgedPojoModelProperty,
+			SearchModel searchModel) {
+		// Add at least one field so that the bridge is not removed
+		indexSchemaElement.field( "fieldFromPropertyBridge" ).asString().createAccessor();
+	}
+
+	@Override
+	public void bind(IndexSchemaElement indexSchemaElement, PojoModelType bridgedPojoModelType,
+			SearchModel searchModel) {
+		// Add at least one field so that the bridge is not removed
+		indexSchemaElement.field( "fieldFromTypeBridge" ).asString().createAccessor();
+	}
+
+	@Override
+	public void bind(PojoModelElement pojoModelElement) {
+		// Nothing to do
+	}
+
+	@Override
+	public void write(DocumentElement target, PojoElement source) {
+		throw shouldNotBeUsed();
+	}
+
+	@Override
+	public String toIndexedValue(Object value) {
+		throw shouldNotBeUsed();
+	}
+
+	@Override
+	public String toRoutingKey(String tenantIdentifier, Object entityIdentifier, PojoElement source) {
+		throw shouldNotBeUsed();
+	}
+
+	@Override
+	public String toDocumentIdentifier(Object propertyValue) {
+		throw shouldNotBeUsed();
+	}
+
+	@Override
+	public Object fromDocumentIdentifier(String documentIdentifier) {
+		throw shouldNotBeUsed();
+	}
+
+	private AssertionFailure shouldNotBeUsed() {
+		return new AssertionFailure(
+				"Instances of " + getClass().getSimpleName() + " are not supposed to be used at runtime,"
+				+ " they should only be used to test the startup process."
+		);
+	}
+}

hibernate/hibernate-search

integrationtest/mapper/orm/src/test/java/org/hibernate/search/integrationtest/mapper/orm/OrmProgrammaticMappingIT.java
@@ -7,7 +7,6 @@
 package org.hibernate.search.integrationtest.mapper.orm;
 
 import static org.hibernate.search.util.impl.integrationtest.common.stub.backend.StubBackendUtils.reference;
-import static org.junit.Assert.assertEquals;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -50,13 +49,12 @@ import org.hibernate.search.integrationtest.mapper.orm.bridge.IntegerAsStringVal
 import org.hibernate.search.integrationtest.mapper.orm.bridge.OptionalIntAsStringValueBridge;
 import org.hibernate.search.integrationtest.mapper.orm.usertype.OptionalIntUserType;
 import org.hibernate.search.integrationtest.mapper.orm.usertype.OptionalStringUserType;
+import org.hibernate.search.engine.search.ProjectionConstants;
 import org.hibernate.search.util.impl.integrationtest.common.rule.BackendMock;
-import org.hibernate.search.util.impl.test.rule.StaticCounters;
 import org.hibernate.search.util.impl.integrationtest.common.rule.StubSearchWorkBehavior;
 import org.hibernate.search.util.impl.integrationtest.common.stub.backend.index.impl.StubBackendFactory;
-import org.hibernate.search.util.impl.integrationtest.common.stub.backend.index.impl.StubIndexManager;
 import org.hibernate.search.util.impl.integrationtest.orm.OrmUtils;
-import org.hibernate.search.engine.search.ProjectionConstants;
+import org.hibernate.search.util.impl.test.rule.StaticCounters;
 import org.hibernate.service.ServiceRegistry;
 
 import org.junit.After;
@@ -168,34 +166,6 @@ public class OrmProgrammaticMappingIT {
 		if ( sessionFactory != null ) {
 			sessionFactory.close();
 		}
-	}
-
-	@Test
-	public void lifecycle() {
-		// More bridges may have been instantiated, but only the below number should be active
-		// (the others should have been dropped because they were completely filtered out)
-		assertEquals( 4, counters.get( CustomTypeBridge.INSTANCE_COUNTER_KEY )
-				- counters.get( CustomTypeBridge.CLOSE_COUNTER_KEY ) );
-		assertEquals( 3, counters.get( CustomPropertyBridge.INSTANCE_COUNTER_KEY )
-				- counters.get( CustomPropertyBridge.CLOSE_COUNTER_KEY ) );
-		assertEquals( 1, counters.get( IntegerAsStringValueBridge.INSTANCE_COUNTER_KEY )
-				- counters.get( IntegerAsStringValueBridge.CLOSE_COUNTER_KEY ) );
-		assertEquals( 1, counters.get( OptionalIntAsStringValueBridge.INSTANCE_COUNTER_KEY )
-				- counters.get( OptionalIntAsStringValueBridge.CLOSE_COUNTER_KEY ) );
-		assertEquals( 3, counters.get( StubIndexManager.INSTANCE_COUNTER_KEY ) );
-		sessionFactory.close();
-		sessionFactory = null;
-		// All instantiated resources should have been closed
-		assertEquals( 0, counters.get( CustomTypeBridge.INSTANCE_COUNTER_KEY )
-				- counters.get( CustomTypeBridge.CLOSE_COUNTER_KEY ) );
-		assertEquals( 0, counters.get( CustomPropertyBridge.INSTANCE_COUNTER_KEY )
-				- counters.get( CustomPropertyBridge.CLOSE_COUNTER_KEY ) );
-		assertEquals( 0, counters.get( IntegerAsStringValueBridge.INSTANCE_COUNTER_KEY )
-				- counters.get( IntegerAsStringValueBridge.CLOSE_COUNTER_KEY ) );
-		assertEquals( 0, counters.get( OptionalIntAsStringValueBridge.INSTANCE_COUNTER_KEY )
-				- counters.get( OptionalIntAsStringValueBridge.CLOSE_COUNTER_KEY ) );
-		assertEquals( 0, counters.get( StubIndexManager.INSTANCE_COUNTER_KEY )
-				- counters.get( StubIndexManager.CLOSE_COUNTER_KEY ) );
 	}
 
 	@Test

integrationtest/mapper/pojo/src/test/java/org/hibernate/search/integrationtest/mapper/pojo/JavaBeanProgrammaticMappingIT.java
@@ -8,7 +8,6 @@ package org.hibernate.search.integrationtest.mapper.pojo;
 
 import static org.hibernate.search.util.impl.integrationtest.common.assertion.SearchResultAssert.assertThat;
 import static org.hibernate.search.util.impl.integrationtest.common.stub.backend.StubBackendUtils.reference;
-import static org.junit.Assert.assertEquals;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -34,13 +33,12 @@ import org.hibernate.search.integrationtest.mapper.pojo.bridge.CustomPropertyBri
 import org.hibernate.search.integrationtest.mapper.pojo.bridge.CustomTypeBridge;
 import org.hibernate.search.integrationtest.mapper.pojo.bridge.IntegerAsStringValueBridge;
 import org.hibernate.search.integrationtest.mapper.pojo.bridge.OptionalIntAsStringValueBridge;
-import org.hibernate.search.util.impl.integrationtest.common.rule.BackendMock;
-import org.hibernate.search.util.impl.test.rule.StaticCounters;
-import org.hibernate.search.util.impl.integrationtest.common.rule.StubSearchWorkBehavior;
-import org.hibernate.search.util.impl.integrationtest.common.stub.backend.index.impl.StubBackendFactory;
-import org.hibernate.search.util.impl.integrationtest.common.stub.backend.index.impl.StubIndexManager;
 import org.hibernate.search.engine.search.ProjectionConstants;
 import org.hibernate.search.engine.search.SearchQuery;
+import org.hibernate.search.util.impl.integrationtest.common.rule.BackendMock;
+import org.hibernate.search.util.impl.integrationtest.common.rule.StubSearchWorkBehavior;
+import org.hibernate.search.util.impl.integrationtest.common.stub.backend.index.impl.StubBackendFactory;
+import org.hibernate.search.util.impl.test.rule.StaticCounters;
 
 import org.junit.After;
 import org.junit.Before;
@@ -210,34 +208,6 @@ public class JavaBeanProgrammaticMappingIT {
 		if ( mappingRepository != null ) {
 			mappingRepository.close();
 		}
-	}
-
-	@Test
-	public void lifecycle() {
-		// More bridges may have been instantiated, but only the below number should be active
-		// (the others should have been dropped because they were completely filtered out)
-		assertEquals( 4, counters.get( CustomTypeBridge.INSTANCE_COUNTER_KEY )
-				- counters.get( CustomTypeBridge.CLOSE_COUNTER_KEY ) );
-		assertEquals( 4, counters.get( CustomPropertyBridge.INSTANCE_COUNTER_KEY )
-				- counters.get( CustomPropertyBridge.CLOSE_COUNTER_KEY ) );
-		assertEquals( 1, counters.get( IntegerAsStringValueBridge.INSTANCE_COUNTER_KEY )
-				- counters.get( IntegerAsStringValueBridge.CLOSE_COUNTER_KEY ) );
-		assertEquals( 1, counters.get( OptionalIntAsStringValueBridge.INSTANCE_COUNTER_KEY )
-				- counters.get( OptionalIntAsStringValueBridge.CLOSE_COUNTER_KEY ) );
-		assertEquals( 3, counters.get( StubIndexManager.INSTANCE_COUNTER_KEY ) );
-		mappingRepository.close();
-		mappingRepository = null;
-		// All instantiated resources should have been closed
-		assertEquals( 0, counters.get( CustomTypeBridge.INSTANCE_COUNTER_KEY )
-				- counters.get( CustomTypeBridge.CLOSE_COUNTER_KEY ) );
-		assertEquals( 0, counters.get( CustomPropertyBridge.INSTANCE_COUNTER_KEY )
-				- counters.get( CustomPropertyBridge.CLOSE_COUNTER_KEY ) );
-		assertEquals( 0, counters.get( IntegerAsStringValueBridge.INSTANCE_COUNTER_KEY )
-				- counters.get( IntegerAsStringValueBridge.CLOSE_COUNTER_KEY ) );
-		assertEquals( 0, counters.get( OptionalIntAsStringValueBridge.INSTANCE_COUNTER_KEY )
-				- counters.get( OptionalIntAsStringValueBridge.CLOSE_COUNTER_KEY ) );
-		assertEquals( 0, counters.get( StubIndexManager.INSTANCE_COUNTER_KEY )
-				- counters.get( StubIndexManager.CLOSE_COUNTER_KEY ) );
 	}
 
 	@Test

integrationtest/mapper/pojo/src/test/java/org/hibernate/search/integrationtest/mapper/pojo/bridge/StartupStubBridge.java
@@ -1,0 +1,116 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.integrationtest.mapper.pojo.bridge;
+
+import org.hibernate.search.engine.backend.document.DocumentElement;
+import org.hibernate.search.engine.backend.document.model.dsl.IndexSchemaElement;
+import org.hibernate.search.engine.mapper.model.SearchModel;
+import org.hibernate.search.mapper.pojo.bridge.IdentifierBridge;
+import org.hibernate.search.mapper.pojo.bridge.PropertyBridge;
+import org.hibernate.search.mapper.pojo.bridge.RoutingKeyBridge;
+import org.hibernate.search.mapper.pojo.bridge.TypeBridge;
+import org.hibernate.search.mapper.pojo.bridge.ValueBridge;
+import org.hibernate.search.mapper.pojo.model.PojoElement;
+import org.hibernate.search.mapper.pojo.model.PojoModelElement;
+import org.hibernate.search.mapper.pojo.model.PojoModelProperty;
+import org.hibernate.search.mapper.pojo.model.PojoModelType;
+import org.hibernate.search.util.AssertionFailure;
+import org.hibernate.search.util.impl.test.rule.StaticCounters;
+
+/**
+ * A stub bridge for use in startup tests. Any runtime use of this bridge will fail.
+ * <p>
+ * For our own convenience, all bridge types are implemented in the same class.
+ */
+public class StartupStubBridge
+		implements TypeBridge, PropertyBridge, ValueBridge<Object, String>,
+		RoutingKeyBridge, IdentifierBridge<Object> {
+	public static class CounterKeys {
+		public final StaticCounters.Key instance = StaticCounters.createKey();
+		public final StaticCounters.Key close = StaticCounters.createKey();
+
+		private CounterKeys() {
+		}
+	}
+
+	public static CounterKeys createKeys() {
+		return new CounterKeys();
+	}
+
+	public final CounterKeys counterKeys;
+
+	private boolean closed = false;
+
+	public StartupStubBridge(CounterKeys counterKeys) {
+		StaticCounters.get().increment( counterKeys.instance );
+		this.counterKeys = counterKeys;
+	}
+
+	@Override
+	public void close() {
+		/*
+		 * This is important so that multiple calls to close on a single bridge
+		 * won't be interpreted as closing multiple objects in test assertions.
+		 */
+		if ( closed ) {
+			return;
+		}
+		StaticCounters.get().increment( counterKeys.close );
+		closed = true;
+	}
+
+	@Override
+	public void bind(IndexSchemaElement indexSchemaElement, PojoModelProperty bridgedPojoModelProperty,
+			SearchModel searchModel) {
+		// Add at least one field so that the bridge is not removed
+		indexSchemaElement.field( "fieldFromPropertyBridge" ).asString().createAccessor();
+	}
+
+	@Override
+	public void bind(IndexSchemaElement indexSchemaElement, PojoModelType bridgedPojoModelType,
+			SearchModel searchModel) {
+		// Add at least one field so that the bridge is not removed
+		indexSchemaElement.field( "fieldFromTypeBridge" ).asString().createAccessor();
+	}
+
+	@Override
+	public void bind(PojoModelElement pojoModelElement) {
+		// Nothing to do
+	}
+
+	@Override
+	public void write(DocumentElement target, PojoElement source) {
+		throw shouldNotBeUsed();
+	}
+
+	@Override
+	public String toIndexedValue(Object value) {
+		throw shouldNotBeUsed();
+	}
+
+	@Override
+	public String toRoutingKey(String tenantIdentifier, Object entityIdentifier, PojoElement source) {
+		throw shouldNotBeUsed();
+	}
+
+	@Override
+	public String toDocumentIdentifier(Object propertyValue) {
+		throw shouldNotBeUsed();
+	}
+
+	@Override
+	public Object fromDocumentIdentifier(String documentIdentifier) {
+		throw shouldNotBeUsed();
+	}
+
+	private AssertionFailure shouldNotBeUsed() {
+		return new AssertionFailure(
+				"Instances of " + getClass().getSimpleName() + " are not supposed to be used at runtime,"
+				+ " they should only be used to test the startup process."
+		);
+	}
+}