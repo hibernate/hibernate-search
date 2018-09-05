hibernate/hibernate-search-6-poc

HSEARCH-3087 Enable spatial support in the showcase

integrationtest/showcase/library/src/main/java/org/hibernate/search/v6poc/integrationtest/showcase/library/dao/syntax/fluidandlambda/FluidAndLambdaSyntaxDocumentDao.java
@@ -8,8 +8,10 @@ package org.hibernate.search.v6poc.integrationtest.showcase.library.dao.syntax.f
 
 import java.util.List;
 import java.util.Optional;
+
 import javax.persistence.EntityManager;
 
+import org.hibernate.search.v6poc.backend.spatial.DistanceUnit;
 import org.hibernate.search.v6poc.backend.spatial.GeoPoint;
 import org.hibernate.search.v6poc.entity.orm.hibernate.FullTextSession;
 import org.hibernate.search.v6poc.entity.orm.jpa.FullTextQuery;
@@ -99,17 +101,15 @@ class FluidAndLambdaSyntaxDocumentDao extends DocumentDao {
 							}
 						} )
 						// Spatial query
-						// TODO spatial query
-						/*
 						.must( ctx -> {
 							if ( myLocation != null && maxDistanceInKilometers != null ) {
-								ctx.spatial()
+								ctx.nested().onObjectField( "copies" )
+										.spatial()
+										.within()
 										.onField( "copies.library.location" )
-										.within( maxDistanceInKilometers, DistanceUnit.KM )
-										.of( myLocation );
+										.circle( myLocation, maxDistanceInKilometers, DistanceUnit.KILOMETERS );
 							}
-						} );
-						*/
+						} )
 						// Nested query + must loop
 						.must( ctx -> {
 							if ( libraryServices != null && !libraryServices.isEmpty() ) {

integrationtest/showcase/library/src/main/java/org/hibernate/search/v6poc/integrationtest/showcase/library/dao/syntax/fluidandobject/FluidAndObjectSyntaxDocumentDao.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Optional;
 import javax.persistence.EntityManager;
 
+import org.hibernate.search.v6poc.backend.spatial.DistanceUnit;
 import org.hibernate.search.v6poc.backend.spatial.GeoPoint;
 import org.hibernate.search.v6poc.entity.orm.hibernate.FullTextSession;
 import org.hibernate.search.v6poc.entity.orm.jpa.FullTextQuery;
@@ -100,16 +101,13 @@ class FluidAndObjectSyntaxDocumentDao extends DocumentDao {
 		}
 
 		// Spatial query
-		// TODO spatial query
 
-		/*
 		if ( myLocation != null && maxDistanceInKilometers != null ) {
-			booleanBuilder.must().spatial()
+			booleanBuilder.must().nested().onObjectField( "copies" ).spatial()
+					.within()
 					.onField( "copies.library.location" )
-					.within( maxDistanceInKilometers, DistanceUnit.KM )
-					.of( myLocation );
+					.circle( myLocation, maxDistanceInKilometers, DistanceUnit.KILOMETERS );
 		}
-		*/
 
 		// Nested query + must loop
 		if ( libraryServices != null && !libraryServices.isEmpty() ) {

integrationtest/showcase/library/src/main/java/org/hibernate/search/v6poc/integrationtest/showcase/library/dao/syntax/lambda/LambdaSyntaxDocumentDao.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Optional;
 import javax.persistence.EntityManager;
 
+import org.hibernate.search.v6poc.backend.spatial.DistanceUnit;
 import org.hibernate.search.v6poc.backend.spatial.GeoPoint;
 import org.hibernate.search.v6poc.entity.orm.hibernate.FullTextSession;
 import org.hibernate.search.v6poc.entity.orm.jpa.FullTextQuery;
@@ -100,17 +101,15 @@ class LambdaSyntaxDocumentDao extends DocumentDao {
 						}
 					} );
 					// Spatial query
-					// TODO spatial query
-					/*
 					b.must( ctx -> {
 						if ( myLocation != null && maxDistanceInKilometers != null ) {
-							ctx.spatial()
+							ctx.nested().onObjectField( "copies" )
+									.spatial()
+									.within()
 									.onField( "copies.library.location" )
-									.within( maxDistanceInKilometers, DistanceUnit.KM )
-									.of( myLocation );
+									.circle( myLocation, maxDistanceInKilometers, DistanceUnit.KILOMETERS );
 						}
 					} );
-					*/
 					// Nested query + must loop
 					b.must( ctx -> {
 						if ( libraryServices != null && !libraryServices.isEmpty() ) {

integrationtest/showcase/library/src/main/java/org/hibernate/search/v6poc/integrationtest/showcase/library/dao/syntax/object/ObjectSyntaxDocumentDao.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Optional;
 import javax.persistence.EntityManager;
 
+import org.hibernate.search.v6poc.backend.spatial.DistanceUnit;
 import org.hibernate.search.v6poc.backend.spatial.GeoPoint;
 import org.hibernate.search.v6poc.entity.orm.hibernate.FullTextSession;
 import org.hibernate.search.v6poc.entity.orm.jpa.FullTextQuery;
@@ -112,18 +113,16 @@ class ObjectSyntaxDocumentDao extends DocumentDao {
 		}
 
 		// Spatial query
-		// TODO spatial query
 
-		/*
 		if ( myLocation != null && maxDistanceInKilometers != null ) {
 			booleanBuilder.must(
-					target.predicate().spatial()
-					.onField( "copies.library.location" )
-					.within( maxDistanceInKilometers, DistanceUnit.KM )
-					.of( myLocation )
+					target.predicate().nested().onObjectField( "copies" ).spatial()
+							.within()
+							.onField( "copies.library.location" )
+							.circle( myLocation, maxDistanceInKilometers, DistanceUnit.KILOMETERS )
+							.end()
 			);
 		}
-		*/
 
 		// Nested query + must loop
 		if ( libraryServices != null && !libraryServices.isEmpty() ) {

hibernate/hibernate-search

integrationtest/showcase/library/src/main/java/org/hibernate/search/integrationtest/showcase/library/dao/syntax/fluidandlambda/FluidAndLambdaSyntaxDocumentDao.java
@@ -8,8 +8,10 @@ package org.hibernate.search.integrationtest.showcase.library.dao.syntax.fluidan
 
 import java.util.List;
 import java.util.Optional;
+
 import javax.persistence.EntityManager;
 
+import org.hibernate.search.engine.backend.spatial.DistanceUnit;
 import org.hibernate.search.engine.backend.spatial.GeoPoint;
 import org.hibernate.search.mapper.orm.hibernate.FullTextSession;
 import org.hibernate.search.mapper.orm.jpa.FullTextQuery;
@@ -99,17 +101,15 @@ class FluidAndLambdaSyntaxDocumentDao extends DocumentDao {
 							}
 						} )
 						// Spatial query
-						// TODO spatial query
-						/*
 						.must( ctx -> {
 							if ( myLocation != null && maxDistanceInKilometers != null ) {
-								ctx.spatial()
+								ctx.nested().onObjectField( "copies" )
+										.spatial()
+										.within()
 										.onField( "copies.library.location" )
-										.within( maxDistanceInKilometers, DistanceUnit.KM )
-										.of( myLocation );
+										.circle( myLocation, maxDistanceInKilometers, DistanceUnit.KILOMETERS );
 							}
-						} );
-						*/
+						} )
 						// Nested query + must loop
 						.must( ctx -> {
 							if ( libraryServices != null && !libraryServices.isEmpty() ) {

integrationtest/showcase/library/src/main/java/org/hibernate/search/integrationtest/showcase/library/dao/syntax/fluidandobject/FluidAndObjectSyntaxDocumentDao.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Optional;
 import javax.persistence.EntityManager;
 
+import org.hibernate.search.engine.backend.spatial.DistanceUnit;
 import org.hibernate.search.engine.backend.spatial.GeoPoint;
 import org.hibernate.search.mapper.orm.hibernate.FullTextSession;
 import org.hibernate.search.mapper.orm.jpa.FullTextQuery;
@@ -100,16 +101,13 @@ class FluidAndObjectSyntaxDocumentDao extends DocumentDao {
 		}
 
 		// Spatial query
-		// TODO spatial query
 
-		/*
 		if ( myLocation != null && maxDistanceInKilometers != null ) {
-			booleanBuilder.must().spatial()
+			booleanBuilder.must().nested().onObjectField( "copies" ).spatial()
+					.within()
 					.onField( "copies.library.location" )
-					.within( maxDistanceInKilometers, DistanceUnit.KM )
-					.of( myLocation );
+					.circle( myLocation, maxDistanceInKilometers, DistanceUnit.KILOMETERS );
 		}
-		*/
 
 		// Nested query + must loop
 		if ( libraryServices != null && !libraryServices.isEmpty() ) {

integrationtest/showcase/library/src/main/java/org/hibernate/search/integrationtest/showcase/library/dao/syntax/lambda/LambdaSyntaxDocumentDao.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Optional;
 import javax.persistence.EntityManager;
 
+import org.hibernate.search.engine.backend.spatial.DistanceUnit;
 import org.hibernate.search.engine.backend.spatial.GeoPoint;
 import org.hibernate.search.mapper.orm.hibernate.FullTextSession;
 import org.hibernate.search.mapper.orm.jpa.FullTextQuery;
@@ -100,17 +101,15 @@ class LambdaSyntaxDocumentDao extends DocumentDao {
 						}
 					} );
 					// Spatial query
-					// TODO spatial query
-					/*
 					b.must( ctx -> {
 						if ( myLocation != null && maxDistanceInKilometers != null ) {
-							ctx.spatial()
+							ctx.nested().onObjectField( "copies" )
+									.spatial()
+									.within()
 									.onField( "copies.library.location" )
-									.within( maxDistanceInKilometers, DistanceUnit.KM )
-									.of( myLocation );
+									.circle( myLocation, maxDistanceInKilometers, DistanceUnit.KILOMETERS );
 						}
 					} );
-					*/
 					// Nested query + must loop
 					b.must( ctx -> {
 						if ( libraryServices != null && !libraryServices.isEmpty() ) {

integrationtest/showcase/library/src/main/java/org/hibernate/search/integrationtest/showcase/library/dao/syntax/object/ObjectSyntaxDocumentDao.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Optional;
 import javax.persistence.EntityManager;
 
+import org.hibernate.search.engine.backend.spatial.DistanceUnit;
 import org.hibernate.search.engine.backend.spatial.GeoPoint;
 import org.hibernate.search.mapper.orm.hibernate.FullTextSession;
 import org.hibernate.search.mapper.orm.jpa.FullTextQuery;
@@ -112,18 +113,16 @@ class ObjectSyntaxDocumentDao extends DocumentDao {
 		}
 
 		// Spatial query
-		// TODO spatial query
 
-		/*
 		if ( myLocation != null && maxDistanceInKilometers != null ) {
 			booleanBuilder.must(
-					target.predicate().spatial()
-					.onField( "copies.library.location" )
-					.within( maxDistanceInKilometers, DistanceUnit.KM )
-					.of( myLocation )
+					target.predicate().nested().onObjectField( "copies" ).spatial()
+							.within()
+							.onField( "copies.library.location" )
+							.circle( myLocation, maxDistanceInKilometers, DistanceUnit.KILOMETERS )
+							.end()
 			);
 		}
-		*/
 
 		// Nested query + must loop
 		if ( libraryServices != null && !libraryServices.isEmpty() ) {