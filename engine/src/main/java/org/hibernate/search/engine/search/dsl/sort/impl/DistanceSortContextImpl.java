hibernate/hibernate-search-6-poc

HSEARCH-3087 Move the spatial types to their own package

engine/src/main/java/org/hibernate/search/v6poc/search/dsl/sort/impl/DistanceSortContextImpl.java
@@ -8,13 +8,13 @@ package org.hibernate.search.v6poc.search.dsl.sort.impl;
 
 import java.util.function.Supplier;
 
-import org.hibernate.search.v6poc.backend.spatial.GeoPoint;
 import org.hibernate.search.v6poc.search.dsl.sort.DistanceSortContext;
 import org.hibernate.search.v6poc.search.dsl.sort.SearchSortContainerContext;
 import org.hibernate.search.v6poc.search.dsl.sort.SortOrder;
 import org.hibernate.search.v6poc.search.sort.spi.DistanceSortBuilder;
 import org.hibernate.search.v6poc.search.sort.spi.SearchSortContributor;
 import org.hibernate.search.v6poc.search.sort.spi.SearchSortFactory;
+import org.hibernate.search.v6poc.spatial.GeoPoint;
 
 class DistanceSortContextImpl<N, C> implements DistanceSortContext<N>, SearchSortContributor<C> {
 

engine/src/main/java/org/hibernate/search/v6poc/search/predicate/spi/SpatialWithinBoundingBoxPredicateBuilder.java
@@ -6,7 +6,7 @@
  */
 package org.hibernate.search.v6poc.search.predicate.spi;
 
-import org.hibernate.search.v6poc.backend.spatial.GeoBoundingBox;
+import org.hibernate.search.v6poc.spatial.GeoBoundingBox;
 
 public interface SpatialWithinBoundingBoxPredicateBuilder<C> extends SearchPredicateBuilder<C> {
 

engine/src/main/java/org/hibernate/search/v6poc/search/predicate/spi/SpatialWithinCirclePredicateBuilder.java
@@ -6,7 +6,7 @@
  */
 package org.hibernate.search.v6poc.search.predicate.spi;
 
-import org.hibernate.search.v6poc.backend.spatial.GeoPoint;
+import org.hibernate.search.v6poc.spatial.GeoPoint;
 
 public interface SpatialWithinCirclePredicateBuilder<C> extends SearchPredicateBuilder<C> {
 

engine/src/main/java/org/hibernate/search/v6poc/spatial/GeoPoint.java
@@ -4,7 +4,7 @@
  * License: GNU Lesser General Public License (LGPL), version 2.1 or later
  * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
  */
-package org.hibernate.search.v6poc.backend.spatial;
+package org.hibernate.search.v6poc.spatial;
 
 /**
  * @author Nicolas Helleringer

engine/src/main/java/org/hibernate/search/v6poc/spatial/ImmutableGeoBoundingBox.java
@@ -4,7 +4,7 @@
  * License: GNU Lesser General Public License (LGPL), version 2.1 or later
  * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
  */
-package org.hibernate.search.v6poc.backend.spatial;
+package org.hibernate.search.v6poc.spatial;
 
 import java.util.Objects;
 

integrationtest/backend-tck/src/main/java/org/hibernate/search/v6poc/integrationtest/backend/tck/search/predicate/RangeSearchPredicateIT.java
@@ -21,13 +21,13 @@ import org.hibernate.search.v6poc.backend.document.model.dsl.IndexSchemaElement;
 import org.hibernate.search.v6poc.backend.index.spi.ChangesetIndexWorker;
 import org.hibernate.search.v6poc.backend.index.spi.IndexManager;
 import org.hibernate.search.v6poc.backend.index.spi.IndexSearchTarget;
-import org.hibernate.search.v6poc.backend.spatial.GeoPoint;
-import org.hibernate.search.v6poc.backend.spatial.ImmutableGeoPoint;
 import org.hibernate.search.v6poc.engine.spi.SessionContext;
 import org.hibernate.search.v6poc.integrationtest.backend.tck.util.rule.SearchSetupHelper;
 import org.hibernate.search.v6poc.search.DocumentReference;
 import org.hibernate.search.v6poc.search.SearchQuery;
 import org.hibernate.search.v6poc.search.dsl.predicate.RangeBoundInclusion;
+import org.hibernate.search.v6poc.spatial.GeoPoint;
+import org.hibernate.search.v6poc.spatial.ImmutableGeoPoint;
 import org.hibernate.search.v6poc.util.SearchException;
 import org.hibernate.search.v6poc.util.impl.integrationtest.common.assertion.DocumentReferencesSearchResultAssert;
 import org.hibernate.search.v6poc.util.impl.integrationtest.common.stub.StubSessionContext;

integrationtest/showcase/library/src/main/java/org/hibernate/search/v6poc/integrationtest/showcase/library/dao/syntax/fluidandobject/FluidAndObjectSyntaxDocumentDao.java
@@ -10,8 +10,6 @@ import java.util.List;
 import java.util.Optional;
 import javax.persistence.EntityManager;
 
-import org.hibernate.search.v6poc.backend.spatial.DistanceUnit;
-import org.hibernate.search.v6poc.backend.spatial.GeoPoint;
 import org.hibernate.search.v6poc.entity.orm.hibernate.FullTextSession;
 import org.hibernate.search.v6poc.entity.orm.jpa.FullTextQuery;
 import org.hibernate.search.v6poc.entity.orm.jpa.FullTextSearchTarget;
@@ -22,6 +20,8 @@ import org.hibernate.search.v6poc.integrationtest.showcase.library.model.Documen
 import org.hibernate.search.v6poc.integrationtest.showcase.library.model.LibraryService;
 import org.hibernate.search.v6poc.search.SearchPredicate;
 import org.hibernate.search.v6poc.search.dsl.predicate.BooleanJunctionPredicateContext;
+import org.hibernate.search.v6poc.spatial.DistanceUnit;
+import org.hibernate.search.v6poc.spatial.GeoPoint;
 
 class FluidAndObjectSyntaxDocumentDao extends DocumentDao {
 	FluidAndObjectSyntaxDocumentDao(EntityManager entityManager) {

integrationtest/showcase/library/src/main/java/org/hibernate/search/v6poc/integrationtest/showcase/library/dao/syntax/object/ObjectSyntaxDocumentDao.java
@@ -10,8 +10,6 @@ import java.util.List;
 import java.util.Optional;
 import javax.persistence.EntityManager;
 
-import org.hibernate.search.v6poc.backend.spatial.DistanceUnit;
-import org.hibernate.search.v6poc.backend.spatial.GeoPoint;
 import org.hibernate.search.v6poc.entity.orm.hibernate.FullTextSession;
 import org.hibernate.search.v6poc.entity.orm.jpa.FullTextQuery;
 import org.hibernate.search.v6poc.entity.orm.jpa.FullTextSearchTarget;
@@ -22,6 +20,8 @@ import org.hibernate.search.v6poc.integrationtest.showcase.library.model.Documen
 import org.hibernate.search.v6poc.integrationtest.showcase.library.model.LibraryService;
 import org.hibernate.search.v6poc.search.SearchPredicate;
 import org.hibernate.search.v6poc.search.dsl.predicate.BooleanJunctionPredicateContext;
+import org.hibernate.search.v6poc.spatial.DistanceUnit;
+import org.hibernate.search.v6poc.spatial.GeoPoint;
 
 class ObjectSyntaxDocumentDao extends DocumentDao {
 	ObjectSyntaxDocumentDao(EntityManager entityManager) {

hibernate/hibernate-search

engine/src/main/java/org/hibernate/search/engine/search/dsl/sort/impl/DistanceSortContextImpl.java
@@ -8,13 +8,13 @@ package org.hibernate.search.engine.search.dsl.sort.impl;
 
 import java.util.function.Supplier;
 
-import org.hibernate.search.engine.backend.spatial.GeoPoint;
 import org.hibernate.search.engine.search.dsl.sort.DistanceSortContext;
 import org.hibernate.search.engine.search.dsl.sort.SearchSortContainerContext;
 import org.hibernate.search.engine.search.dsl.sort.SortOrder;
 import org.hibernate.search.engine.search.sort.spi.DistanceSortBuilder;
 import org.hibernate.search.engine.search.sort.spi.SearchSortContributor;
 import org.hibernate.search.engine.search.sort.spi.SearchSortFactory;
+import org.hibernate.search.engine.spatial.GeoPoint;
 
 class DistanceSortContextImpl<N, C> implements DistanceSortContext<N>, SearchSortContributor<C> {
 

engine/src/main/java/org/hibernate/search/engine/search/predicate/spi/SpatialWithinBoundingBoxPredicateBuilder.java
@@ -6,7 +6,7 @@
  */
 package org.hibernate.search.engine.search.predicate.spi;
 
-import org.hibernate.search.engine.backend.spatial.GeoBoundingBox;
+import org.hibernate.search.engine.spatial.GeoBoundingBox;
 
 public interface SpatialWithinBoundingBoxPredicateBuilder<C> extends SearchPredicateBuilder<C> {
 

engine/src/main/java/org/hibernate/search/engine/search/predicate/spi/SpatialWithinCirclePredicateBuilder.java
@@ -6,7 +6,7 @@
  */
 package org.hibernate.search.engine.search.predicate.spi;
 
-import org.hibernate.search.engine.backend.spatial.GeoPoint;
+import org.hibernate.search.engine.spatial.GeoPoint;
 
 public interface SpatialWithinCirclePredicateBuilder<C> extends SearchPredicateBuilder<C> {
 

engine/src/main/java/org/hibernate/search/engine/spatial/GeoPoint.java
@@ -4,7 +4,7 @@
  * License: GNU Lesser General Public License (LGPL), version 2.1 or later
  * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
  */
-package org.hibernate.search.engine.backend.spatial;
+package org.hibernate.search.engine.spatial;
 
 /**
  * @author Nicolas Helleringer

engine/src/main/java/org/hibernate/search/engine/spatial/ImmutableGeoBoundingBox.java
@@ -4,7 +4,7 @@
  * License: GNU Lesser General Public License (LGPL), version 2.1 or later
  * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
  */
-package org.hibernate.search.engine.backend.spatial;
+package org.hibernate.search.engine.spatial;
 
 import java.util.Objects;
 

integrationtest/backend/tck/src/main/java/org/hibernate/search/integrationtest/backend/tck/search/predicate/RangeSearchPredicateIT.java
@@ -21,13 +21,13 @@ import org.hibernate.search.engine.backend.document.model.dsl.IndexSchemaElement
 import org.hibernate.search.engine.backend.index.spi.ChangesetIndexWorker;
 import org.hibernate.search.engine.backend.index.spi.IndexManager;
 import org.hibernate.search.engine.backend.index.spi.IndexSearchTarget;
-import org.hibernate.search.engine.backend.spatial.GeoPoint;
-import org.hibernate.search.engine.backend.spatial.ImmutableGeoPoint;
 import org.hibernate.search.engine.common.spi.SessionContext;
 import org.hibernate.search.integrationtest.backend.tck.util.rule.SearchSetupHelper;
 import org.hibernate.search.engine.search.DocumentReference;
 import org.hibernate.search.engine.search.SearchQuery;
 import org.hibernate.search.engine.search.dsl.predicate.RangeBoundInclusion;
+import org.hibernate.search.engine.spatial.GeoPoint;
+import org.hibernate.search.engine.spatial.ImmutableGeoPoint;
 import org.hibernate.search.util.SearchException;
 import org.hibernate.search.util.impl.integrationtest.common.assertion.DocumentReferencesSearchResultAssert;
 import org.hibernate.search.util.impl.integrationtest.common.stub.StubSessionContext;

integrationtest/showcase/library/src/main/java/org/hibernate/search/integrationtest/showcase/library/dao/syntax/fluidandobject/FluidAndObjectSyntaxDocumentDao.java
@@ -10,8 +10,6 @@ import java.util.List;
 import java.util.Optional;
 import javax.persistence.EntityManager;
 
-import org.hibernate.search.engine.backend.spatial.DistanceUnit;
-import org.hibernate.search.engine.backend.spatial.GeoPoint;
 import org.hibernate.search.mapper.orm.hibernate.FullTextSession;
 import org.hibernate.search.mapper.orm.jpa.FullTextQuery;
 import org.hibernate.search.mapper.orm.jpa.FullTextSearchTarget;
@@ -22,6 +20,8 @@ import org.hibernate.search.integrationtest.showcase.library.model.Document;
 import org.hibernate.search.integrationtest.showcase.library.model.LibraryService;
 import org.hibernate.search.engine.search.SearchPredicate;
 import org.hibernate.search.engine.search.dsl.predicate.BooleanJunctionPredicateContext;
+import org.hibernate.search.engine.spatial.DistanceUnit;
+import org.hibernate.search.engine.spatial.GeoPoint;
 
 class FluidAndObjectSyntaxDocumentDao extends DocumentDao {
 	FluidAndObjectSyntaxDocumentDao(EntityManager entityManager) {

integrationtest/showcase/library/src/main/java/org/hibernate/search/integrationtest/showcase/library/dao/syntax/object/ObjectSyntaxDocumentDao.java
@@ -10,8 +10,6 @@ import java.util.List;
 import java.util.Optional;
 import javax.persistence.EntityManager;
 
-import org.hibernate.search.engine.backend.spatial.DistanceUnit;
-import org.hibernate.search.engine.backend.spatial.GeoPoint;
 import org.hibernate.search.mapper.orm.hibernate.FullTextSession;
 import org.hibernate.search.mapper.orm.jpa.FullTextQuery;
 import org.hibernate.search.mapper.orm.jpa.FullTextSearchTarget;
@@ -22,6 +20,8 @@ import org.hibernate.search.integrationtest.showcase.library.model.Document;
 import org.hibernate.search.integrationtest.showcase.library.model.LibraryService;
 import org.hibernate.search.engine.search.SearchPredicate;
 import org.hibernate.search.engine.search.dsl.predicate.BooleanJunctionPredicateContext;
+import org.hibernate.search.engine.spatial.DistanceUnit;
+import org.hibernate.search.engine.spatial.GeoPoint;
 
 class ObjectSyntaxDocumentDao extends DocumentDao {
 	ObjectSyntaxDocumentDao(EntityManager entityManager) {