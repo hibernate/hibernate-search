hibernate/hibernate-search-6-poc

HSEARCH-3261 Rename *Worker interfaces and methods to *WorkPlan
Because that's really what they are: not "actors" executing arbitrary
works, but containers holding a set of works to be forwarded to other
objects that are actually able to execute the works.

integrationtest/backend-lucene/src/test/java/org/hibernate/search/v6poc/integrationtest/backend/lucene/search/LuceneSearchMultiIndexIT.java
@@ -14,7 +14,7 @@ import org.hibernate.search.v6poc.backend.document.IndexFieldAccessor;
 import org.hibernate.search.v6poc.backend.document.model.dsl.IndexSchemaElement;
 import org.hibernate.search.v6poc.backend.document.model.dsl.Sortable;
 import org.hibernate.search.v6poc.backend.document.model.dsl.Store;
-import org.hibernate.search.v6poc.backend.index.spi.ChangesetIndexWorker;
+import org.hibernate.search.v6poc.backend.index.spi.IndexWorkPlan;
 import org.hibernate.search.v6poc.backend.index.spi.IndexManager;
 import org.hibernate.search.v6poc.backend.index.spi.IndexSearchTarget;
 import org.hibernate.search.v6poc.backend.index.spi.IndexSearchTargetBuilder;
@@ -120,18 +120,18 @@ public class LuceneSearchMultiIndexIT {
 	private void initData() {
 		// Backend 1 / Index 1
 
-		ChangesetIndexWorker<? extends DocumentElement> worker = indexManager_1_1.createWorker( sessionContext );
+		IndexWorkPlan<? extends DocumentElement> workPlan = indexManager_1_1.createWorkPlan( sessionContext );
 
-		worker.add( referenceProvider( DOCUMENT_1_1_1 ), document -> {
+		workPlan.add( referenceProvider( DOCUMENT_1_1_1 ), document -> {
 			indexAccessors_1_1.string.write( document, STRING_1 );
 			indexAccessors_1_1.additionalField.write( document, ADDITIONAL_FIELD_1_1_1 );
 		} );
-		worker.add( referenceProvider( DOCUMENT_1_1_2 ), document -> {
+		workPlan.add( referenceProvider( DOCUMENT_1_1_2 ), document -> {
 			indexAccessors_1_1.string.write( document, STRING_2 );
 			indexAccessors_1_1.additionalField.write( document, ADDITIONAL_FIELD_1_1_2 );
 		} );
 
-		worker.execute().join();
+		workPlan.execute().join();
 
 		IndexSearchTarget searchTarget = indexManager_1_1.createSearchTarget().build();
 		SearchQuery<DocumentReference> query = searchTarget.query( sessionContext )
@@ -142,13 +142,13 @@ public class LuceneSearchMultiIndexIT {
 
 		// Backend 1 / Index 2
 
-		worker = indexManager_1_2.createWorker( sessionContext );
+		workPlan = indexManager_1_2.createWorkPlan( sessionContext );
 
-		worker.add( referenceProvider( DOCUMENT_1_2_1 ), document -> {
+		workPlan.add( referenceProvider( DOCUMENT_1_2_1 ), document -> {
 			indexAccessors_1_2.string.write( document, STRING_1 );
 		} );
 
-		worker.execute().join();
+		workPlan.execute().join();
 
 		searchTarget = indexManager_1_2.createSearchTarget().build();
 		query = searchTarget.query( sessionContext )

integrationtest/backend-tck/src/main/java/org/hibernate/search/v6poc/integrationtest/backend/tck/search/SearchMultiIndexIT.java
@@ -16,7 +16,7 @@ import org.hibernate.search.v6poc.backend.document.IndexFieldAccessor;
 import org.hibernate.search.v6poc.backend.document.model.dsl.IndexSchemaElement;
 import org.hibernate.search.v6poc.backend.document.model.dsl.Sortable;
 import org.hibernate.search.v6poc.backend.document.model.dsl.Store;
-import org.hibernate.search.v6poc.backend.index.spi.ChangesetIndexWorker;
+import org.hibernate.search.v6poc.backend.index.spi.IndexWorkPlan;
 import org.hibernate.search.v6poc.backend.index.spi.IndexManager;
 import org.hibernate.search.v6poc.backend.index.spi.IndexSearchTarget;
 import org.hibernate.search.v6poc.backend.index.spi.IndexSearchTargetBuilder;
@@ -309,22 +309,22 @@ public class SearchMultiIndexIT {
 	private void initData() {
 		// Backend 1 / Index 1
 
-		ChangesetIndexWorker<? extends DocumentElement> worker = indexManager_1_1.createWorker( sessionContext );
+		IndexWorkPlan<? extends DocumentElement> workPlan = indexManager_1_1.createWorkPlan( sessionContext );
 
-		worker.add( referenceProvider( DOCUMENT_1_1_1 ), document -> {
+		workPlan.add( referenceProvider( DOCUMENT_1_1_1 ), document -> {
 			indexAccessors_1_1.string.write( document, STRING_1 );
 			indexAccessors_1_1.additionalField.write( document, ADDITIONAL_FIELD_1_1_1 );
 			indexAccessors_1_1.differentTypesField.write( document, DIFFERENT_TYPES_FIELD_1_1_1 );
 			indexAccessors_1_1.sortField.write( document, SORT_FIELD_1_1_1 );
 		} );
-		worker.add( referenceProvider( DOCUMENT_1_1_2 ), document -> {
+		workPlan.add( referenceProvider( DOCUMENT_1_1_2 ), document -> {
 			indexAccessors_1_1.string.write( document, STRING_2 );
 			indexAccessors_1_1.additionalField.write( document, ADDITIONAL_FIELD_1_1_2 );
 			indexAccessors_1_1.differentTypesField.write( document, DIFFERENT_TYPES_FIELD_1_1_2 );
 			indexAccessors_1_1.sortField.write( document, SORT_FIELD_1_1_2 );
 		} );
 
-		worker.execute().join();
+		workPlan.execute().join();
 
 		IndexSearchTarget searchTarget = indexManager_1_1.createSearchTarget().build();
 		SearchQuery<DocumentReference> query = searchTarget.query( sessionContext )
@@ -335,15 +335,15 @@ public class SearchMultiIndexIT {
 
 		// Backend 1 / Index 2
 
-		worker = indexManager_1_2.createWorker( sessionContext );
+		workPlan = indexManager_1_2.createWorkPlan( sessionContext );
 
-		worker.add( referenceProvider( DOCUMENT_1_2_1 ), document -> {
+		workPlan.add( referenceProvider( DOCUMENT_1_2_1 ), document -> {
 			indexAccessors_1_2.string.write( document, STRING_1 );
 			indexAccessors_1_2.differentTypesField.write( document, DIFFERENT_TYPES_FIELD_1_2_1 );
 			indexAccessors_1_2.sortField.write( document, SORT_FIELD_1_2_1 );
 		} );
 
-		worker.execute().join();
+		workPlan.execute().join();
 
 		searchTarget = indexManager_1_2.createSearchTarget().build();
 		query = searchTarget.query( sessionContext )
@@ -354,16 +354,16 @@ public class SearchMultiIndexIT {
 
 		// Backend 2 / Index 1
 
-		worker = indexManager_2_1.createWorker( sessionContext );
+		workPlan = indexManager_2_1.createWorkPlan( sessionContext );
 
-		worker.add( referenceProvider( DOCUMENT_2_1_1 ), document -> {
+		workPlan.add( referenceProvider( DOCUMENT_2_1_1 ), document -> {
 			indexAccessors_2_1.string.write( document, STRING_1 );
 		} );
-		worker.add( referenceProvider( DOCUMENT_2_1_2 ), document -> {
+		workPlan.add( referenceProvider( DOCUMENT_2_1_2 ), document -> {
 			indexAccessors_2_1.string.write( document, STRING_2 );
 		} );
 
-		worker.execute().join();
+		workPlan.execute().join();
 
 		searchTarget = indexManager_2_1.createSearchTarget().build();
 		query = searchTarget.query( sessionContext )

util/internal/integrationtest/common/src/main/java/org/hibernate/search/v6poc/util/impl/integrationtest/common/stub/backend/index/impl/StubIndexWorkPlan.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
-import org.hibernate.search.v6poc.backend.index.spi.ChangesetIndexWorker;
+import org.hibernate.search.v6poc.backend.index.spi.IndexWorkPlan;
 import org.hibernate.search.v6poc.backend.index.spi.DocumentContributor;
 import org.hibernate.search.v6poc.backend.index.spi.DocumentReferenceProvider;
 import org.hibernate.search.v6poc.engine.spi.SessionContext;
@@ -18,7 +18,7 @@ import org.hibernate.search.v6poc.util.impl.integrationtest.common.stub.backend.
 import org.hibernate.search.v6poc.util.impl.integrationtest.common.stub.backend.index.StubIndexWork;
 import org.hibernate.search.v6poc.util.impl.integrationtest.common.stub.backend.document.impl.StubDocumentElement;
 
-class StubChangesetIndexWorker implements ChangesetIndexWorker<StubDocumentElement> {
+class StubIndexWorkPlan implements IndexWorkPlan<StubDocumentElement> {
 	private final StubIndexManager indexManager;
 	private final SessionContext sessionContext;
 
@@ -26,7 +26,7 @@ class StubChangesetIndexWorker implements ChangesetIndexWorker<StubDocumentEleme
 
 	private int preparedIndex = 0;
 
-	StubChangesetIndexWorker(StubIndexManager indexManager, SessionContext sessionContext) {
+	StubIndexWorkPlan(StubIndexManager indexManager, SessionContext sessionContext) {
 		this.sessionContext = sessionContext;
 		this.indexManager = indexManager;
 	}

hibernate/hibernate-search

integrationtest/backend/lucene/src/test/java/org/hibernate/search/integrationtest/backend/lucene/search/LuceneSearchMultiIndexIT.java
@@ -14,7 +14,7 @@ import org.hibernate.search.engine.backend.document.IndexFieldAccessor;
 import org.hibernate.search.engine.backend.document.model.dsl.IndexSchemaElement;
 import org.hibernate.search.engine.backend.document.model.dsl.Sortable;
 import org.hibernate.search.engine.backend.document.model.dsl.Store;
-import org.hibernate.search.engine.backend.index.spi.ChangesetIndexWorker;
+import org.hibernate.search.engine.backend.index.spi.IndexWorkPlan;
 import org.hibernate.search.engine.backend.index.spi.IndexManager;
 import org.hibernate.search.engine.backend.index.spi.IndexSearchTarget;
 import org.hibernate.search.engine.backend.index.spi.IndexSearchTargetBuilder;
@@ -120,18 +120,18 @@ public class LuceneSearchMultiIndexIT {
 	private void initData() {
 		// Backend 1 / Index 1
 
-		ChangesetIndexWorker<? extends DocumentElement> worker = indexManager_1_1.createWorker( sessionContext );
+		IndexWorkPlan<? extends DocumentElement> workPlan = indexManager_1_1.createWorkPlan( sessionContext );
 
-		worker.add( referenceProvider( DOCUMENT_1_1_1 ), document -> {
+		workPlan.add( referenceProvider( DOCUMENT_1_1_1 ), document -> {
 			indexAccessors_1_1.string.write( document, STRING_1 );
 			indexAccessors_1_1.additionalField.write( document, ADDITIONAL_FIELD_1_1_1 );
 		} );
-		worker.add( referenceProvider( DOCUMENT_1_1_2 ), document -> {
+		workPlan.add( referenceProvider( DOCUMENT_1_1_2 ), document -> {
 			indexAccessors_1_1.string.write( document, STRING_2 );
 			indexAccessors_1_1.additionalField.write( document, ADDITIONAL_FIELD_1_1_2 );
 		} );
 
-		worker.execute().join();
+		workPlan.execute().join();
 
 		IndexSearchTarget searchTarget = indexManager_1_1.createSearchTarget().build();
 		SearchQuery<DocumentReference> query = searchTarget.query( sessionContext )
@@ -142,13 +142,13 @@ public class LuceneSearchMultiIndexIT {
 
 		// Backend 1 / Index 2
 
-		worker = indexManager_1_2.createWorker( sessionContext );
+		workPlan = indexManager_1_2.createWorkPlan( sessionContext );
 
-		worker.add( referenceProvider( DOCUMENT_1_2_1 ), document -> {
+		workPlan.add( referenceProvider( DOCUMENT_1_2_1 ), document -> {
 			indexAccessors_1_2.string.write( document, STRING_1 );
 		} );
 
-		worker.execute().join();
+		workPlan.execute().join();
 
 		searchTarget = indexManager_1_2.createSearchTarget().build();
 		query = searchTarget.query( sessionContext )

integrationtest/backend/tck/src/main/java/org/hibernate/search/integrationtest/backend/tck/search/SearchMultiIndexIT.java
@@ -16,7 +16,7 @@ import org.hibernate.search.engine.backend.document.IndexFieldAccessor;
 import org.hibernate.search.engine.backend.document.model.dsl.IndexSchemaElement;
 import org.hibernate.search.engine.backend.document.model.dsl.Sortable;
 import org.hibernate.search.engine.backend.document.model.dsl.Store;
-import org.hibernate.search.engine.backend.index.spi.ChangesetIndexWorker;
+import org.hibernate.search.engine.backend.index.spi.IndexWorkPlan;
 import org.hibernate.search.engine.backend.index.spi.IndexManager;
 import org.hibernate.search.engine.backend.index.spi.IndexSearchTarget;
 import org.hibernate.search.engine.backend.index.spi.IndexSearchTargetBuilder;
@@ -309,22 +309,22 @@ public class SearchMultiIndexIT {
 	private void initData() {
 		// Backend 1 / Index 1
 
-		ChangesetIndexWorker<? extends DocumentElement> worker = indexManager_1_1.createWorker( sessionContext );
+		IndexWorkPlan<? extends DocumentElement> workPlan = indexManager_1_1.createWorkPlan( sessionContext );
 
-		worker.add( referenceProvider( DOCUMENT_1_1_1 ), document -> {
+		workPlan.add( referenceProvider( DOCUMENT_1_1_1 ), document -> {
 			indexAccessors_1_1.string.write( document, STRING_1 );
 			indexAccessors_1_1.additionalField.write( document, ADDITIONAL_FIELD_1_1_1 );
 			indexAccessors_1_1.differentTypesField.write( document, DIFFERENT_TYPES_FIELD_1_1_1 );
 			indexAccessors_1_1.sortField.write( document, SORT_FIELD_1_1_1 );
 		} );
-		worker.add( referenceProvider( DOCUMENT_1_1_2 ), document -> {
+		workPlan.add( referenceProvider( DOCUMENT_1_1_2 ), document -> {
 			indexAccessors_1_1.string.write( document, STRING_2 );
 			indexAccessors_1_1.additionalField.write( document, ADDITIONAL_FIELD_1_1_2 );
 			indexAccessors_1_1.differentTypesField.write( document, DIFFERENT_TYPES_FIELD_1_1_2 );
 			indexAccessors_1_1.sortField.write( document, SORT_FIELD_1_1_2 );
 		} );
 
-		worker.execute().join();
+		workPlan.execute().join();
 
 		IndexSearchTarget searchTarget = indexManager_1_1.createSearchTarget().build();
 		SearchQuery<DocumentReference> query = searchTarget.query( sessionContext )
@@ -335,15 +335,15 @@ public class SearchMultiIndexIT {
 
 		// Backend 1 / Index 2
 
-		worker = indexManager_1_2.createWorker( sessionContext );
+		workPlan = indexManager_1_2.createWorkPlan( sessionContext );
 
-		worker.add( referenceProvider( DOCUMENT_1_2_1 ), document -> {
+		workPlan.add( referenceProvider( DOCUMENT_1_2_1 ), document -> {
 			indexAccessors_1_2.string.write( document, STRING_1 );
 			indexAccessors_1_2.differentTypesField.write( document, DIFFERENT_TYPES_FIELD_1_2_1 );
 			indexAccessors_1_2.sortField.write( document, SORT_FIELD_1_2_1 );
 		} );
 
-		worker.execute().join();
+		workPlan.execute().join();
 
 		searchTarget = indexManager_1_2.createSearchTarget().build();
 		query = searchTarget.query( sessionContext )
@@ -354,16 +354,16 @@ public class SearchMultiIndexIT {
 
 		// Backend 2 / Index 1
 
-		worker = indexManager_2_1.createWorker( sessionContext );
+		workPlan = indexManager_2_1.createWorkPlan( sessionContext );
 
-		worker.add( referenceProvider( DOCUMENT_2_1_1 ), document -> {
+		workPlan.add( referenceProvider( DOCUMENT_2_1_1 ), document -> {
 			indexAccessors_2_1.string.write( document, STRING_1 );
 		} );
-		worker.add( referenceProvider( DOCUMENT_2_1_2 ), document -> {
+		workPlan.add( referenceProvider( DOCUMENT_2_1_2 ), document -> {
 			indexAccessors_2_1.string.write( document, STRING_2 );
 		} );
 
-		worker.execute().join();
+		workPlan.execute().join();
 
 		searchTarget = indexManager_2_1.createSearchTarget().build();
 		query = searchTarget.query( sessionContext )

util/internal/integrationtest/common/src/main/java/org/hibernate/search/util/impl/integrationtest/common/stub/backend/index/impl/StubIndexWorkPlan.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
-import org.hibernate.search.engine.backend.index.spi.ChangesetIndexWorker;
+import org.hibernate.search.engine.backend.index.spi.IndexWorkPlan;
 import org.hibernate.search.engine.backend.index.spi.DocumentContributor;
 import org.hibernate.search.engine.backend.index.spi.DocumentReferenceProvider;
 import org.hibernate.search.engine.common.spi.SessionContext;
@@ -18,7 +18,7 @@ import org.hibernate.search.util.impl.integrationtest.common.stub.backend.docume
 import org.hibernate.search.util.impl.integrationtest.common.stub.backend.index.StubIndexWork;
 import org.hibernate.search.util.impl.integrationtest.common.stub.backend.document.impl.StubDocumentElement;
 
-class StubChangesetIndexWorker implements ChangesetIndexWorker<StubDocumentElement> {
+class StubIndexWorkPlan implements IndexWorkPlan<StubDocumentElement> {
 	private final StubIndexManager indexManager;
 	private final SessionContext sessionContext;
 
@@ -26,7 +26,7 @@ class StubChangesetIndexWorker implements ChangesetIndexWorker<StubDocumentEleme
 
 	private int preparedIndex = 0;
 
-	StubChangesetIndexWorker(StubIndexManager indexManager, SessionContext sessionContext) {
+	StubIndexWorkPlan(StubIndexManager indexManager, SessionContext sessionContext) {
 		this.sessionContext = sessionContext;
 		this.indexManager = indexManager;
 	}