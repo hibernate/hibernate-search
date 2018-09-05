hibernate/hibernate-search-6-poc

HSEARCH-2546 Add context to exceptions thrown by the index model definition DSL during bootstrapping
... and update tests accordingly.

backend-elasticsearch/src/main/java/org/hibernate/search/v6poc/backend/elasticsearch/document/model/dsl/impl/ElasticsearchIndexSchemaObjectFieldNodeBuilder.java
@@ -6,6 +6,9 @@
  */
 package org.hibernate.search.v6poc.backend.elasticsearch.document.model.dsl.impl;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.hibernate.search.v6poc.backend.document.IndexObjectFieldAccessor;
 import org.hibernate.search.v6poc.backend.document.spi.DeferredInitializationIndexObjectFieldAccessor;
 import org.hibernate.search.v6poc.backend.document.model.dsl.ObjectFieldStorage;
@@ -19,10 +22,13 @@ import org.hibernate.search.v6poc.backend.elasticsearch.document.model.impl.esna
 import org.hibernate.search.v6poc.backend.elasticsearch.document.model.impl.esnative.PropertyMapping;
 import org.hibernate.search.v6poc.backend.elasticsearch.gson.impl.JsonAccessor;
 import org.hibernate.search.v6poc.backend.elasticsearch.gson.impl.JsonObjectAccessor;
+import org.hibernate.search.v6poc.logging.spi.FailureContextElement;
+import org.hibernate.search.v6poc.logging.spi.FailureContexts;
 
 class ElasticsearchIndexSchemaObjectFieldNodeBuilder extends AbstractElasticsearchIndexSchemaObjectNodeBuilder
 		implements IndexSchemaObjectFieldNodeBuilder, ElasticsearchIndexSchemaNodeContributor<PropertyMapping> {
 
+	private final AbstractElasticsearchIndexSchemaObjectNodeBuilder parent;
 	private final String absoluteFieldPath;
 	private final String relativeFieldName;
 	private final ObjectFieldStorage storage;
@@ -30,11 +36,21 @@ class ElasticsearchIndexSchemaObjectFieldNodeBuilder extends AbstractElasticsear
 	private final DeferredInitializationIndexObjectFieldAccessor accessor =
 			new DeferredInitializationIndexObjectFieldAccessor();
 
-	ElasticsearchIndexSchemaObjectFieldNodeBuilder(String parentPath, String relativeFieldName,
-			ObjectFieldStorage storage) {
-		this.absoluteFieldPath = parentPath == null ? relativeFieldName : parentPath + "." + relativeFieldName;
+	ElasticsearchIndexSchemaObjectFieldNodeBuilder(AbstractElasticsearchIndexSchemaObjectNodeBuilder parent,
+			String relativeFieldName, ObjectFieldStorage storage) {
+		this.parent = parent;
+		String parentAbsolutePath = parent.getAbsolutePath();
+		this.absoluteFieldPath = parentAbsolutePath == null ? relativeFieldName : parentAbsolutePath + "." + relativeFieldName;
 		this.relativeFieldName = relativeFieldName;
 		this.storage = storage;
+	}
+
+	@Override
+	public List<FailureContextElement> getFailureContext() {
+		return Arrays.asList(
+				getRootNodeBuilder().getIndexFailureContextElement(),
+				FailureContexts.fromIndexFieldAbsolutePath( absoluteFieldPath )
+		);
 	}
 
 	@Override
@@ -74,6 +90,11 @@ class ElasticsearchIndexSchemaObjectFieldNodeBuilder extends AbstractElasticsear
 		contributeChildren( mapping, node, collector );
 
 		return mapping;
+	}
+
+	@Override
+	ElasticsearchIndexSchemaRootNodeBuilder getRootNodeBuilder() {
+		return parent.getRootNodeBuilder();
 	}
 
 	@Override

backend-elasticsearch/src/main/java/org/hibernate/search/v6poc/backend/elasticsearch/document/model/dsl/impl/ElasticsearchIndexSchemaRootNodeBuilder.java
@@ -6,6 +6,9 @@
  */
 package org.hibernate.search.v6poc.backend.elasticsearch.document.model.dsl.impl;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.hibernate.search.v6poc.backend.document.model.dsl.spi.IndexSchemaRootNodeBuilder;
 import org.hibernate.search.v6poc.backend.elasticsearch.document.model.impl.ElasticsearchIndexSchemaNodeCollector;
 import org.hibernate.search.v6poc.backend.elasticsearch.document.model.impl.ElasticsearchIndexSchemaObjectNode;
@@ -14,16 +17,29 @@ import org.hibernate.search.v6poc.backend.elasticsearch.document.model.impl.esna
 import org.hibernate.search.v6poc.backend.elasticsearch.document.model.impl.esnative.RootTypeMapping;
 import org.hibernate.search.v6poc.backend.elasticsearch.document.model.impl.esnative.RoutingType;
 import org.hibernate.search.v6poc.backend.elasticsearch.multitenancy.impl.MultiTenancyStrategy;
+import org.hibernate.search.v6poc.logging.spi.FailureContextElement;
+import org.hibernate.search.v6poc.logging.spi.FailureContexts;
 
 public class ElasticsearchIndexSchemaRootNodeBuilder extends AbstractElasticsearchIndexSchemaObjectNodeBuilder
 		implements IndexSchemaRootNodeBuilder, ElasticsearchRootIndexSchemaContributor {
 
-	private MultiTenancyStrategy multiTenancyStrategy;
+	private final String hibernateSearchIndexName;
+	private final MultiTenancyStrategy multiTenancyStrategy;
 
 	private RoutingType routing = null;
 
-	public ElasticsearchIndexSchemaRootNodeBuilder(MultiTenancyStrategy multiTenancyStrategy) {
+	public ElasticsearchIndexSchemaRootNodeBuilder(String hibernateSearchIndexName,
+			MultiTenancyStrategy multiTenancyStrategy) {
+		this.hibernateSearchIndexName = hibernateSearchIndexName;
 		this.multiTenancyStrategy = multiTenancyStrategy;
+	}
+
+	@Override
+	public List<FailureContextElement> getFailureContext() {
+		return Arrays.asList(
+				getIndexFailureContextElement(),
+				FailureContexts.indexSchemaRoot()
+		);
 	}
 
 	@Override
@@ -51,7 +67,16 @@ public class ElasticsearchIndexSchemaRootNodeBuilder extends AbstractElasticsear
 	}
 
 	@Override
+	ElasticsearchIndexSchemaRootNodeBuilder getRootNodeBuilder() {
+		return this;
+	}
+
+	@Override
 	String getAbsolutePath() {
 		return null;
+	}
+
+	FailureContextElement getIndexFailureContextElement() {
+		return FailureContexts.fromIndexName( hibernateSearchIndexName );
 	}
 }

backend-lucene/src/main/java/org/hibernate/search/v6poc/backend/lucene/document/model/dsl/impl/LuceneIndexSchemaObjectFieldNodeBuilder.java
@@ -6,6 +6,9 @@
  */
 package org.hibernate.search.v6poc.backend.lucene.document.model.dsl.impl;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.hibernate.search.v6poc.backend.document.IndexObjectFieldAccessor;
 import org.hibernate.search.v6poc.backend.document.spi.DeferredInitializationIndexObjectFieldAccessor;
 import org.hibernate.search.v6poc.backend.document.model.dsl.ObjectFieldStorage;
@@ -15,25 +18,32 @@ import org.hibernate.search.v6poc.backend.lucene.util.impl.LuceneFields;
 import org.hibernate.search.v6poc.backend.lucene.document.model.impl.LuceneIndexSchemaNodeCollector;
 import org.hibernate.search.v6poc.backend.lucene.document.model.impl.LuceneIndexSchemaNodeContributor;
 import org.hibernate.search.v6poc.backend.lucene.document.model.impl.LuceneIndexSchemaObjectNode;
+import org.hibernate.search.v6poc.logging.spi.FailureContextElement;
+import org.hibernate.search.v6poc.logging.spi.FailureContexts;
 
 class LuceneIndexSchemaObjectFieldNodeBuilder extends AbstractLuceneIndexSchemaObjectNodeBuilder
 		implements IndexSchemaObjectFieldNodeBuilder, LuceneIndexSchemaNodeContributor {
 
-	private final DeferredInitializationIndexObjectFieldAccessor accessor =
-			new DeferredInitializationIndexObjectFieldAccessor();
-
+	private final AbstractLuceneIndexSchemaObjectNodeBuilder parent;
 	private final String absoluteFieldPath;
 	private final ObjectFieldStorage storage;
 
-	LuceneIndexSchemaObjectFieldNodeBuilder(String parentPath, String relativeFieldName,
-			ObjectFieldStorage storage) {
-		this.absoluteFieldPath = LuceneFields.compose( parentPath, relativeFieldName );
+	private final DeferredInitializationIndexObjectFieldAccessor accessor =
+			new DeferredInitializationIndexObjectFieldAccessor();
+
+	LuceneIndexSchemaObjectFieldNodeBuilder(AbstractLuceneIndexSchemaObjectNodeBuilder parent,
+			String relativeFieldName, ObjectFieldStorage storage) {
+		this.parent = parent;
+		this.absoluteFieldPath = LuceneFields.compose( parent.getAbsolutePath(), relativeFieldName );
 		this.storage = storage;
 	}
 
 	@Override
-	public String getAbsolutePath() {
-		return absoluteFieldPath;
+	public List<FailureContextElement> getFailureContext() {
+		return Arrays.asList(
+				getRootNodeBuilder().getIndexFailureContextElement(),
+				FailureContexts.fromIndexFieldAbsolutePath( absoluteFieldPath )
+		);
 	}
 
 	@Override
@@ -49,5 +59,15 @@ class LuceneIndexSchemaObjectFieldNodeBuilder extends AbstractLuceneIndexSchemaO
 		accessor.initialize( new LuceneIndexObjectFieldAccessor( node, storage ) );
 
 		contributeChildren( node, collector );
+	}
+
+	@Override
+	LuceneIndexSchemaRootNodeBuilder getRootNodeBuilder() {
+		return parent.getRootNodeBuilder();
+	}
+
+	@Override
+	String getAbsolutePath() {
+		return absoluteFieldPath;
 	}
 }

engine/src/main/java/org/hibernate/search/v6poc/backend/document/model/dsl/spi/IndexSchemaContext.java
@@ -1,0 +1,21 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.v6poc.backend.document.model.dsl.spi;
+
+import java.util.List;
+
+import org.hibernate.search.v6poc.logging.spi.FailureContextElement;
+
+public interface IndexSchemaContext {
+
+	/**
+	 * @return A list of failure context elements to be passed to the constructor of any
+	 * {@link org.hibernate.search.v6poc.logging.spi.SearchExceptionWithContext} occurring in this context.
+	 */
+	List<FailureContextElement> getFailureContext();
+
+}

util/internal/integrationtest/common/src/main/java/org/hibernate/search/v6poc/util/impl/integrationtest/common/FailureReportUtils.java
@@ -7,10 +7,36 @@
 package org.hibernate.search.v6poc.util.impl.integrationtest.common;
 
 import java.lang.annotation.Annotation;
+import java.util.Arrays;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+import org.hibernate.search.v6poc.logging.spi.FailureContextElement;
+import org.hibernate.search.v6poc.logging.spi.SearchExceptionWithContext;
+
+import org.assertj.core.api.Assertions;
 
 public final class FailureReportUtils {
 
 	private FailureReportUtils() {
+	}
+
+	/**
+	 * @param contextElements The expect context elements, in order.
+	 * @return A consumer representing an assertion to be passed as a parameter to
+	 * {@link org.assertj.core.api.AbstractThrowableAssert#satisfies(Consumer)}.
+	 */
+	public static Consumer<Throwable> hasContext(FailureContextElement ... contextElements) {
+		return throwable -> {
+			Assertions.assertThat( throwable )
+					.isInstanceOf( SearchExceptionWithContext.class );
+			Assertions.assertThat( ( (SearchExceptionWithContext) throwable ).getContextElements() )
+					.containsExactly( contextElements );
+			String renderedContexts = Arrays.stream( contextElements ).map( FailureContextElement::render )
+					.collect( Collectors.joining( ", " ) );
+			Assertions.assertThat( throwable.getMessage() )
+					.endsWith( "Context: " + renderedContexts );
+		};
 	}
 
 	public static SingleContextFailureReportPatternBuilder buildSingleContextFailureReportPattern() {

util/internal/integrationtest/common/src/main/java/org/hibernate/search/v6poc/util/impl/integrationtest/common/stub/backend/document/model/impl/StubIndexSchemaObjectFieldNodeBuilder.java
@@ -6,8 +6,13 @@
  */
 package org.hibernate.search.v6poc.util.impl.integrationtest.common.stub.backend.document.model.impl;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.hibernate.search.v6poc.backend.document.IndexObjectFieldAccessor;
 import org.hibernate.search.v6poc.backend.document.model.dsl.spi.IndexSchemaObjectFieldNodeBuilder;
+import org.hibernate.search.v6poc.logging.spi.FailureContextElement;
+import org.hibernate.search.v6poc.logging.spi.FailureContexts;
 import org.hibernate.search.v6poc.util.impl.integrationtest.common.stub.backend.document.model.StubIndexSchemaNode;
 
 class StubIndexSchemaObjectFieldNodeBuilder extends StubIndexSchemaObjectNodeBuilder
@@ -22,6 +27,14 @@ class StubIndexSchemaObjectFieldNodeBuilder extends StubIndexSchemaObjectNodeBui
 		super( builder );
 		this.parent = parent;
 		this.included = included;
+	}
+
+	@Override
+	public List<FailureContextElement> getFailureContext() {
+		return Arrays.asList(
+				getRootNodeBuilder().getIndexFailureContextElement(),
+				FailureContexts.fromIndexFieldAbsolutePath( builder.getAbsolutePath() )
+		);
 	}
 
 	@Override

util/internal/integrationtest/common/src/main/java/org/hibernate/search/v6poc/util/impl/integrationtest/common/stub/backend/document/model/impl/StubIndexSchemaRootNodeBuilder.java
@@ -6,7 +6,12 @@
  */
 package org.hibernate.search.v6poc.util.impl.integrationtest.common.stub.backend.document.model.impl;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.hibernate.search.v6poc.backend.document.model.dsl.spi.IndexSchemaRootNodeBuilder;
+import org.hibernate.search.v6poc.logging.spi.FailureContextElement;
+import org.hibernate.search.v6poc.logging.spi.FailureContexts;
 import org.hibernate.search.v6poc.util.impl.integrationtest.common.stub.backend.StubBackendBehavior;
 import org.hibernate.search.v6poc.util.impl.integrationtest.common.stub.backend.document.model.StubIndexSchemaNode;
 
@@ -32,6 +37,14 @@ public class StubIndexSchemaRootNodeBuilder extends StubIndexSchemaObjectNodeBui
 		builder.explicitRouting();
 	}
 
+	@Override
+	public List<FailureContextElement> getFailureContext() {
+		return Arrays.asList(
+				getIndexFailureContextElement(),
+				FailureContexts.indexSchemaRoot()
+		);
+	}
+
 	public StubIndexSchemaNode build() {
 		return builder.build();
 	}
@@ -39,6 +52,10 @@ public class StubIndexSchemaRootNodeBuilder extends StubIndexSchemaObjectNodeBui
 	@Override
 	StubIndexSchemaRootNodeBuilder getRootNodeBuilder() {
 		return this;
+	}
+
+	FailureContextElement getIndexFailureContextElement() {
+		return FailureContexts.fromIndexName( indexName );
 	}
 
 	StubBackendBehavior getBackendBehavior() {

hibernate/hibernate-search

backend/elasticsearch/src/main/java/org/hibernate/search/backend/elasticsearch/document/model/dsl/impl/ElasticsearchIndexSchemaObjectFieldNodeBuilder.java
@@ -6,6 +6,9 @@
  */
 package org.hibernate.search.backend.elasticsearch.document.model.dsl.impl;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.hibernate.search.engine.backend.document.IndexObjectFieldAccessor;
 import org.hibernate.search.engine.backend.document.spi.DeferredInitializationIndexObjectFieldAccessor;
 import org.hibernate.search.engine.backend.document.model.dsl.ObjectFieldStorage;
@@ -19,10 +22,13 @@ import org.hibernate.search.backend.elasticsearch.document.model.impl.esnative.D
 import org.hibernate.search.backend.elasticsearch.document.model.impl.esnative.PropertyMapping;
 import org.hibernate.search.backend.elasticsearch.gson.impl.JsonAccessor;
 import org.hibernate.search.backend.elasticsearch.gson.impl.JsonObjectAccessor;
+import org.hibernate.search.engine.logging.spi.FailureContextElement;
+import org.hibernate.search.engine.logging.spi.FailureContexts;
 
 class ElasticsearchIndexSchemaObjectFieldNodeBuilder extends AbstractElasticsearchIndexSchemaObjectNodeBuilder
 		implements IndexSchemaObjectFieldNodeBuilder, ElasticsearchIndexSchemaNodeContributor<PropertyMapping> {
 
+	private final AbstractElasticsearchIndexSchemaObjectNodeBuilder parent;
 	private final String absoluteFieldPath;
 	private final String relativeFieldName;
 	private final ObjectFieldStorage storage;
@@ -30,11 +36,21 @@ class ElasticsearchIndexSchemaObjectFieldNodeBuilder extends AbstractElasticsear
 	private final DeferredInitializationIndexObjectFieldAccessor accessor =
 			new DeferredInitializationIndexObjectFieldAccessor();
 
-	ElasticsearchIndexSchemaObjectFieldNodeBuilder(String parentPath, String relativeFieldName,
-			ObjectFieldStorage storage) {
-		this.absoluteFieldPath = parentPath == null ? relativeFieldName : parentPath + "." + relativeFieldName;
+	ElasticsearchIndexSchemaObjectFieldNodeBuilder(AbstractElasticsearchIndexSchemaObjectNodeBuilder parent,
+			String relativeFieldName, ObjectFieldStorage storage) {
+		this.parent = parent;
+		String parentAbsolutePath = parent.getAbsolutePath();
+		this.absoluteFieldPath = parentAbsolutePath == null ? relativeFieldName : parentAbsolutePath + "." + relativeFieldName;
 		this.relativeFieldName = relativeFieldName;
 		this.storage = storage;
+	}
+
+	@Override
+	public List<FailureContextElement> getFailureContext() {
+		return Arrays.asList(
+				getRootNodeBuilder().getIndexFailureContextElement(),
+				FailureContexts.fromIndexFieldAbsolutePath( absoluteFieldPath )
+		);
 	}
 
 	@Override
@@ -74,6 +90,11 @@ class ElasticsearchIndexSchemaObjectFieldNodeBuilder extends AbstractElasticsear
 		contributeChildren( mapping, node, collector );
 
 		return mapping;
+	}
+
+	@Override
+	ElasticsearchIndexSchemaRootNodeBuilder getRootNodeBuilder() {
+		return parent.getRootNodeBuilder();
 	}
 
 	@Override

backend/elasticsearch/src/main/java/org/hibernate/search/backend/elasticsearch/document/model/dsl/impl/ElasticsearchIndexSchemaRootNodeBuilder.java
@@ -6,6 +6,9 @@
  */
 package org.hibernate.search.backend.elasticsearch.document.model.dsl.impl;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.hibernate.search.engine.backend.document.model.dsl.spi.IndexSchemaRootNodeBuilder;
 import org.hibernate.search.backend.elasticsearch.document.model.impl.ElasticsearchIndexSchemaNodeCollector;
 import org.hibernate.search.backend.elasticsearch.document.model.impl.ElasticsearchIndexSchemaObjectNode;
@@ -14,16 +17,29 @@ import org.hibernate.search.backend.elasticsearch.document.model.impl.esnative.D
 import org.hibernate.search.backend.elasticsearch.document.model.impl.esnative.RootTypeMapping;
 import org.hibernate.search.backend.elasticsearch.document.model.impl.esnative.RoutingType;
 import org.hibernate.search.backend.elasticsearch.multitenancy.impl.MultiTenancyStrategy;
+import org.hibernate.search.engine.logging.spi.FailureContextElement;
+import org.hibernate.search.engine.logging.spi.FailureContexts;
 
 public class ElasticsearchIndexSchemaRootNodeBuilder extends AbstractElasticsearchIndexSchemaObjectNodeBuilder
 		implements IndexSchemaRootNodeBuilder, ElasticsearchRootIndexSchemaContributor {
 
-	private MultiTenancyStrategy multiTenancyStrategy;
+	private final String hibernateSearchIndexName;
+	private final MultiTenancyStrategy multiTenancyStrategy;
 
 	private RoutingType routing = null;
 
-	public ElasticsearchIndexSchemaRootNodeBuilder(MultiTenancyStrategy multiTenancyStrategy) {
+	public ElasticsearchIndexSchemaRootNodeBuilder(String hibernateSearchIndexName,
+			MultiTenancyStrategy multiTenancyStrategy) {
+		this.hibernateSearchIndexName = hibernateSearchIndexName;
 		this.multiTenancyStrategy = multiTenancyStrategy;
+	}
+
+	@Override
+	public List<FailureContextElement> getFailureContext() {
+		return Arrays.asList(
+				getIndexFailureContextElement(),
+				FailureContexts.indexSchemaRoot()
+		);
 	}
 
 	@Override
@@ -51,7 +67,16 @@ public class ElasticsearchIndexSchemaRootNodeBuilder extends AbstractElasticsear
 	}
 
 	@Override
+	ElasticsearchIndexSchemaRootNodeBuilder getRootNodeBuilder() {
+		return this;
+	}
+
+	@Override
 	String getAbsolutePath() {
 		return null;
+	}
+
+	FailureContextElement getIndexFailureContextElement() {
+		return FailureContexts.fromIndexName( hibernateSearchIndexName );
 	}
 }

backend/lucene/src/main/java/org/hibernate/search/backend/lucene/document/model/dsl/impl/LuceneIndexSchemaObjectFieldNodeBuilder.java
@@ -6,6 +6,9 @@
  */
 package org.hibernate.search.backend.lucene.document.model.dsl.impl;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.hibernate.search.engine.backend.document.IndexObjectFieldAccessor;
 import org.hibernate.search.engine.backend.document.spi.DeferredInitializationIndexObjectFieldAccessor;
 import org.hibernate.search.engine.backend.document.model.dsl.ObjectFieldStorage;
@@ -15,25 +18,32 @@ import org.hibernate.search.backend.lucene.util.impl.LuceneFields;
 import org.hibernate.search.backend.lucene.document.model.impl.LuceneIndexSchemaNodeCollector;
 import org.hibernate.search.backend.lucene.document.model.impl.LuceneIndexSchemaNodeContributor;
 import org.hibernate.search.backend.lucene.document.model.impl.LuceneIndexSchemaObjectNode;
+import org.hibernate.search.engine.logging.spi.FailureContextElement;
+import org.hibernate.search.engine.logging.spi.FailureContexts;
 
 class LuceneIndexSchemaObjectFieldNodeBuilder extends AbstractLuceneIndexSchemaObjectNodeBuilder
 		implements IndexSchemaObjectFieldNodeBuilder, LuceneIndexSchemaNodeContributor {
 
-	private final DeferredInitializationIndexObjectFieldAccessor accessor =
-			new DeferredInitializationIndexObjectFieldAccessor();
-
+	private final AbstractLuceneIndexSchemaObjectNodeBuilder parent;
 	private final String absoluteFieldPath;
 	private final ObjectFieldStorage storage;
 
-	LuceneIndexSchemaObjectFieldNodeBuilder(String parentPath, String relativeFieldName,
-			ObjectFieldStorage storage) {
-		this.absoluteFieldPath = LuceneFields.compose( parentPath, relativeFieldName );
+	private final DeferredInitializationIndexObjectFieldAccessor accessor =
+			new DeferredInitializationIndexObjectFieldAccessor();
+
+	LuceneIndexSchemaObjectFieldNodeBuilder(AbstractLuceneIndexSchemaObjectNodeBuilder parent,
+			String relativeFieldName, ObjectFieldStorage storage) {
+		this.parent = parent;
+		this.absoluteFieldPath = LuceneFields.compose( parent.getAbsolutePath(), relativeFieldName );
 		this.storage = storage;
 	}
 
 	@Override
-	public String getAbsolutePath() {
-		return absoluteFieldPath;
+	public List<FailureContextElement> getFailureContext() {
+		return Arrays.asList(
+				getRootNodeBuilder().getIndexFailureContextElement(),
+				FailureContexts.fromIndexFieldAbsolutePath( absoluteFieldPath )
+		);
 	}
 
 	@Override
@@ -49,5 +59,15 @@ class LuceneIndexSchemaObjectFieldNodeBuilder extends AbstractLuceneIndexSchemaO
 		accessor.initialize( new LuceneIndexObjectFieldAccessor( node, storage ) );
 
 		contributeChildren( node, collector );
+	}
+
+	@Override
+	LuceneIndexSchemaRootNodeBuilder getRootNodeBuilder() {
+		return parent.getRootNodeBuilder();
+	}
+
+	@Override
+	String getAbsolutePath() {
+		return absoluteFieldPath;
 	}
 }

engine/src/main/java/org/hibernate/search/engine/backend/document/model/dsl/spi/IndexSchemaContext.java
@@ -1,0 +1,21 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.engine.backend.document.model.dsl.spi;
+
+import java.util.List;
+
+import org.hibernate.search.engine.logging.spi.FailureContextElement;
+
+public interface IndexSchemaContext {
+
+	/**
+	 * @return A list of failure context elements to be passed to the constructor of any
+	 * {@link org.hibernate.search.engine.logging.spi.SearchExceptionWithContext} occurring in this context.
+	 */
+	List<FailureContextElement> getFailureContext();
+
+}

util/internal/integrationtest/common/src/main/java/org/hibernate/search/util/impl/integrationtest/common/FailureReportUtils.java
@@ -7,10 +7,36 @@
 package org.hibernate.search.util.impl.integrationtest.common;
 
 import java.lang.annotation.Annotation;
+import java.util.Arrays;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+import org.hibernate.search.engine.logging.spi.FailureContextElement;
+import org.hibernate.search.engine.logging.spi.SearchExceptionWithContext;
+
+import org.assertj.core.api.Assertions;
 
 public final class FailureReportUtils {
 
 	private FailureReportUtils() {
+	}
+
+	/**
+	 * @param contextElements The expect context elements, in order.
+	 * @return A consumer representing an assertion to be passed as a parameter to
+	 * {@link org.assertj.core.api.AbstractThrowableAssert#satisfies(Consumer)}.
+	 */
+	public static Consumer<Throwable> hasContext(FailureContextElement ... contextElements) {
+		return throwable -> {
+			Assertions.assertThat( throwable )
+					.isInstanceOf( SearchExceptionWithContext.class );
+			Assertions.assertThat( ( (SearchExceptionWithContext) throwable ).getContextElements() )
+					.containsExactly( contextElements );
+			String renderedContexts = Arrays.stream( contextElements ).map( FailureContextElement::render )
+					.collect( Collectors.joining( ", " ) );
+			Assertions.assertThat( throwable.getMessage() )
+					.endsWith( "Context: " + renderedContexts );
+		};
 	}
 
 	public static SingleContextFailureReportPatternBuilder buildSingleContextFailureReportPattern() {

util/internal/integrationtest/common/src/main/java/org/hibernate/search/util/impl/integrationtest/common/stub/backend/document/model/impl/StubIndexSchemaObjectFieldNodeBuilder.java
@@ -6,8 +6,13 @@
  */
 package org.hibernate.search.util.impl.integrationtest.common.stub.backend.document.model.impl;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.hibernate.search.engine.backend.document.IndexObjectFieldAccessor;
 import org.hibernate.search.engine.backend.document.model.dsl.spi.IndexSchemaObjectFieldNodeBuilder;
+import org.hibernate.search.engine.logging.spi.FailureContextElement;
+import org.hibernate.search.engine.logging.spi.FailureContexts;
 import org.hibernate.search.util.impl.integrationtest.common.stub.backend.document.model.StubIndexSchemaNode;
 
 class StubIndexSchemaObjectFieldNodeBuilder extends StubIndexSchemaObjectNodeBuilder
@@ -22,6 +27,14 @@ class StubIndexSchemaObjectFieldNodeBuilder extends StubIndexSchemaObjectNodeBui
 		super( builder );
 		this.parent = parent;
 		this.included = included;
+	}
+
+	@Override
+	public List<FailureContextElement> getFailureContext() {
+		return Arrays.asList(
+				getRootNodeBuilder().getIndexFailureContextElement(),
+				FailureContexts.fromIndexFieldAbsolutePath( builder.getAbsolutePath() )
+		);
 	}
 
 	@Override

util/internal/integrationtest/common/src/main/java/org/hibernate/search/util/impl/integrationtest/common/stub/backend/document/model/impl/StubIndexSchemaRootNodeBuilder.java
@@ -6,7 +6,12 @@
  */
 package org.hibernate.search.util.impl.integrationtest.common.stub.backend.document.model.impl;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.hibernate.search.engine.backend.document.model.dsl.spi.IndexSchemaRootNodeBuilder;
+import org.hibernate.search.engine.logging.spi.FailureContextElement;
+import org.hibernate.search.engine.logging.spi.FailureContexts;
 import org.hibernate.search.util.impl.integrationtest.common.stub.backend.StubBackendBehavior;
 import org.hibernate.search.util.impl.integrationtest.common.stub.backend.document.model.StubIndexSchemaNode;
 
@@ -32,6 +37,14 @@ public class StubIndexSchemaRootNodeBuilder extends StubIndexSchemaObjectNodeBui
 		builder.explicitRouting();
 	}
 
+	@Override
+	public List<FailureContextElement> getFailureContext() {
+		return Arrays.asList(
+				getIndexFailureContextElement(),
+				FailureContexts.indexSchemaRoot()
+		);
+	}
+
 	public StubIndexSchemaNode build() {
 		return builder.build();
 	}
@@ -39,6 +52,10 @@ public class StubIndexSchemaRootNodeBuilder extends StubIndexSchemaObjectNodeBui
 	@Override
 	StubIndexSchemaRootNodeBuilder getRootNodeBuilder() {
 		return this;
+	}
+
+	FailureContextElement getIndexFailureContextElement() {
+		return FailureContexts.fromIndexName( indexName );
 	}
 
 	StubBackendBehavior getBackendBehavior() {