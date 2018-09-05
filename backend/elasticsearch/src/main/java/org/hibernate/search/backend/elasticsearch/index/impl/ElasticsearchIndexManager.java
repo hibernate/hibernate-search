hibernate/hibernate-search-6-poc

HSEARCH-3083 Move the responsibility of closing index managers to the engine
This makes sense considering that:

* The engine is already responsible for closing backends.
* The engine is already responsible for closing index manager builders
on bootstrap failure.
* We already know that SearchMappingRepository is badly named and
already want to change its name, because it provides access to backends.
So adding a map of index managers in there is not as far-fetched as one
might think.

backend-elasticsearch/src/main/java/org/hibernate/search/v6poc/backend/elasticsearch/index/impl/ElasticsearchIndexManager.java
@@ -8,14 +8,14 @@ package org.hibernate.search.v6poc.backend.elasticsearch.index.impl;
 
 import java.lang.invoke.MethodHandles;
 
-import org.hibernate.search.v6poc.backend.elasticsearch.util.impl.URLEncodedString;
 import org.hibernate.search.v6poc.backend.elasticsearch.document.impl.ElasticsearchDocumentObjectBuilder;
 import org.hibernate.search.v6poc.backend.elasticsearch.document.model.impl.ElasticsearchIndexModel;
 import org.hibernate.search.v6poc.backend.elasticsearch.logging.impl.Log;
 import org.hibernate.search.v6poc.backend.elasticsearch.orchestration.impl.ElasticsearchWorkOrchestrator;
 import org.hibernate.search.v6poc.backend.elasticsearch.search.query.impl.SearchBackendContext;
+import org.hibernate.search.v6poc.backend.elasticsearch.util.impl.URLEncodedString;
 import org.hibernate.search.v6poc.backend.index.spi.ChangesetIndexWorker;
-import org.hibernate.search.v6poc.backend.index.spi.IndexManager;
+import org.hibernate.search.v6poc.backend.index.spi.IndexManagerImplementor;
 import org.hibernate.search.v6poc.backend.index.spi.IndexSearchTargetBuilder;
 import org.hibernate.search.v6poc.backend.index.spi.StreamIndexWorker;
 import org.hibernate.search.v6poc.engine.spi.SessionContext;
@@ -25,7 +25,7 @@ import org.hibernate.search.v6poc.util.impl.common.LoggerFactory;
 /**
  * @author Yoann Rodiere
  */
-public class ElasticsearchIndexManager implements IndexManager<ElasticsearchDocumentObjectBuilder> {
+public class ElasticsearchIndexManager implements IndexManagerImplementor<ElasticsearchDocumentObjectBuilder> {
 
 	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
 

backend-lucene/src/main/java/org/hibernate/search/v6poc/backend/lucene/index/impl/LuceneIndexManager.java
@@ -6,7 +6,7 @@
  */
 package org.hibernate.search.v6poc.backend.lucene.index.impl;
 
-import org.hibernate.search.v6poc.backend.index.spi.IndexManager;
+import org.hibernate.search.v6poc.backend.index.spi.IndexManagerImplementor;
 import org.hibernate.search.v6poc.backend.lucene.document.impl.LuceneRootDocumentBuilder;
 import org.hibernate.search.v6poc.backend.lucene.document.model.impl.LuceneIndexModel;
 import org.hibernate.search.v6poc.backend.lucene.index.spi.ReaderProvider;
@@ -14,7 +14,7 @@ import org.hibernate.search.v6poc.backend.lucene.index.spi.ReaderProvider;
 /**
  * @author Guillaume Smet
  */
-public interface LuceneIndexManager extends IndexManager<LuceneRootDocumentBuilder> {
+public interface LuceneIndexManager extends IndexManagerImplementor<LuceneRootDocumentBuilder> {
 
 	String getName();
 

engine/src/main/java/org/hibernate/search/v6poc/backend/index/spi/IndexManagerImplementor.java
@@ -1,0 +1,21 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.v6poc.backend.index.spi;
+
+import org.hibernate.search.v6poc.backend.document.DocumentElement;
+
+/**
+ * The @link {@link IndexManager} as viewed from the engine.
+ * <p>
+ * This interface provides additional methods to allow the engine to manage the index manager lifecycle.
+ */
+public interface IndexManagerImplementor<D extends DocumentElement> extends IndexManager<D>, AutoCloseable {
+
+	@Override
+	void close();
+
+}

engine/src/main/java/org/hibernate/search/v6poc/engine/impl/SearchMappingRepositoryImpl.java
@@ -9,6 +9,7 @@ package org.hibernate.search.v6poc.engine.impl;
 import java.util.Map;
 
 import org.hibernate.search.v6poc.backend.Backend;
+import org.hibernate.search.v6poc.backend.index.spi.IndexManagerImplementor;
 import org.hibernate.search.v6poc.backend.spi.BackendImplementor;
 import org.hibernate.search.v6poc.engine.SearchMappingRepository;
 import org.hibernate.search.v6poc.entity.mapping.spi.MappingKey;
@@ -20,11 +21,14 @@ public class SearchMappingRepositoryImpl implements SearchMappingRepository {
 
 	private final Map<MappingKey<?>, MappingImplementor<?>> mappings;
 	private final Map<String, BackendImplementor<?>> backends;
+	private final Map<String, IndexManagerImplementor<?>> indexManagers;
 
 	SearchMappingRepositoryImpl(Map<MappingKey<?>, MappingImplementor<?>> mappings,
-			Map<String, BackendImplementor<?>> backends) {
+			Map<String, BackendImplementor<?>> backends,
+			Map<String, IndexManagerImplementor<?>> indexManagers) {
 		this.mappings = mappings;
 		this.backends = backends;
+		this.indexManagers = indexManagers;
 	}
 
 	@Override
@@ -51,6 +55,7 @@ public class SearchMappingRepositoryImpl implements SearchMappingRepository {
 	public void close() {
 		try ( Closer<RuntimeException> closer = new Closer<>() ) {
 			closer.pushAll( MappingImplementor::close, mappings.values() );
+			closer.pushAll( IndexManagerImplementor::close, indexManagers.values() );
 			closer.pushAll( BackendImplementor::close, backends.values() );
 		}
 	}

util/internal/integrationtest/common/src/main/java/org/hibernate/search/v6poc/util/impl/integrationtest/common/stub/backend/index/impl/StubIndexManager.java
@@ -10,16 +10,16 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 import org.hibernate.search.v6poc.backend.index.spi.ChangesetIndexWorker;
-import org.hibernate.search.v6poc.backend.index.spi.IndexManager;
+import org.hibernate.search.v6poc.backend.index.spi.IndexManagerImplementor;
 import org.hibernate.search.v6poc.backend.index.spi.IndexSearchTargetBuilder;
 import org.hibernate.search.v6poc.backend.index.spi.StreamIndexWorker;
 import org.hibernate.search.v6poc.engine.spi.SessionContext;
-import org.hibernate.search.v6poc.util.impl.test.rule.StaticCounters;
-import org.hibernate.search.v6poc.util.impl.integrationtest.common.stub.backend.index.StubIndexWork;
 import org.hibernate.search.v6poc.util.impl.integrationtest.common.stub.backend.document.impl.StubDocumentElement;
 import org.hibernate.search.v6poc.util.impl.integrationtest.common.stub.backend.document.model.StubIndexSchemaNode;
+import org.hibernate.search.v6poc.util.impl.integrationtest.common.stub.backend.index.StubIndexWork;
+import org.hibernate.search.v6poc.util.impl.test.rule.StaticCounters;
 
-public class StubIndexManager implements IndexManager<StubDocumentElement> {
+public class StubIndexManager implements IndexManagerImplementor<StubDocumentElement> {
 
 	public static final StaticCounters.Key INSTANCE_COUNTER_KEY = StaticCounters.createKey();
 	public static final StaticCounters.Key CLOSE_COUNTER_KEY = StaticCounters.createKey();

util/internal/integrationtest/common/src/main/java/org/hibernate/search/v6poc/util/impl/integrationtest/common/stub/backend/index/impl/StubIndexManagerBuilder.java
@@ -7,8 +7,8 @@
 package org.hibernate.search.v6poc.util.impl.integrationtest.common.stub.backend.index.impl;
 
 import org.hibernate.search.v6poc.backend.document.model.dsl.spi.IndexSchemaRootNodeBuilder;
-import org.hibernate.search.v6poc.backend.index.spi.IndexManager;
 import org.hibernate.search.v6poc.backend.index.spi.IndexManagerBuilder;
+import org.hibernate.search.v6poc.backend.index.spi.IndexManagerImplementor;
 import org.hibernate.search.v6poc.util.AssertionFailure;
 import org.hibernate.search.v6poc.util.impl.integrationtest.common.stub.backend.document.impl.StubDocumentElement;
 import org.hibernate.search.v6poc.util.impl.integrationtest.common.stub.backend.document.model.impl.StubIndexSchemaRootNodeBuilder;
@@ -51,7 +51,7 @@ public class StubIndexManagerBuilder implements IndexManagerBuilder<StubDocument
 	}
 
 	@Override
-	public IndexManager<StubDocumentElement> build() {
+	public IndexManagerImplementor<StubDocumentElement> build() {
 		if ( closed ) {
 			throw new AssertionFailure( "Unexpected call to build after a call to build() or to closeOnFailure()" );
 		}

hibernate/hibernate-search

backend/elasticsearch/src/main/java/org/hibernate/search/backend/elasticsearch/index/impl/ElasticsearchIndexManager.java
@@ -8,14 +8,14 @@ package org.hibernate.search.backend.elasticsearch.index.impl;
 
 import java.lang.invoke.MethodHandles;
 
-import org.hibernate.search.backend.elasticsearch.util.impl.URLEncodedString;
 import org.hibernate.search.backend.elasticsearch.document.impl.ElasticsearchDocumentObjectBuilder;
 import org.hibernate.search.backend.elasticsearch.document.model.impl.ElasticsearchIndexModel;
 import org.hibernate.search.backend.elasticsearch.logging.impl.Log;
 import org.hibernate.search.backend.elasticsearch.orchestration.impl.ElasticsearchWorkOrchestrator;
 import org.hibernate.search.backend.elasticsearch.search.query.impl.SearchBackendContext;
+import org.hibernate.search.backend.elasticsearch.util.impl.URLEncodedString;
 import org.hibernate.search.engine.backend.index.spi.ChangesetIndexWorker;
-import org.hibernate.search.engine.backend.index.spi.IndexManager;
+import org.hibernate.search.engine.backend.index.spi.IndexManagerImplementor;
 import org.hibernate.search.engine.backend.index.spi.IndexSearchTargetBuilder;
 import org.hibernate.search.engine.backend.index.spi.StreamIndexWorker;
 import org.hibernate.search.engine.common.spi.SessionContext;
@@ -25,7 +25,7 @@ import org.hibernate.search.util.impl.common.LoggerFactory;
 /**
  * @author Yoann Rodiere
  */
-public class ElasticsearchIndexManager implements IndexManager<ElasticsearchDocumentObjectBuilder> {
+public class ElasticsearchIndexManager implements IndexManagerImplementor<ElasticsearchDocumentObjectBuilder> {
 
 	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
 

backend/lucene/src/main/java/org/hibernate/search/backend/lucene/index/impl/LuceneIndexManager.java
@@ -6,7 +6,7 @@
  */
 package org.hibernate.search.backend.lucene.index.impl;
 
-import org.hibernate.search.engine.backend.index.spi.IndexManager;
+import org.hibernate.search.engine.backend.index.spi.IndexManagerImplementor;
 import org.hibernate.search.backend.lucene.document.impl.LuceneRootDocumentBuilder;
 import org.hibernate.search.backend.lucene.document.model.impl.LuceneIndexModel;
 import org.hibernate.search.backend.lucene.index.spi.ReaderProvider;
@@ -14,7 +14,7 @@ import org.hibernate.search.backend.lucene.index.spi.ReaderProvider;
 /**
  * @author Guillaume Smet
  */
-public interface LuceneIndexManager extends IndexManager<LuceneRootDocumentBuilder> {
+public interface LuceneIndexManager extends IndexManagerImplementor<LuceneRootDocumentBuilder> {
 
 	String getName();
 

engine/src/main/java/org/hibernate/search/engine/backend/index/spi/IndexManagerImplementor.java
@@ -1,0 +1,21 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.engine.backend.index.spi;
+
+import org.hibernate.search.engine.backend.document.DocumentElement;
+
+/**
+ * The @link {@link IndexManager} as viewed from the engine.
+ * <p>
+ * This interface provides additional methods to allow the engine to manage the index manager lifecycle.
+ */
+public interface IndexManagerImplementor<D extends DocumentElement> extends IndexManager<D>, AutoCloseable {
+
+	@Override
+	void close();
+
+}

engine/src/main/java/org/hibernate/search/engine/common/impl/SearchMappingRepositoryImpl.java
@@ -9,6 +9,7 @@ package org.hibernate.search.engine.common.impl;
 import java.util.Map;
 
 import org.hibernate.search.engine.backend.Backend;
+import org.hibernate.search.engine.backend.index.spi.IndexManagerImplementor;
 import org.hibernate.search.engine.backend.spi.BackendImplementor;
 import org.hibernate.search.engine.common.SearchMappingRepository;
 import org.hibernate.search.engine.mapper.mapping.spi.MappingKey;
@@ -20,11 +21,14 @@ public class SearchMappingRepositoryImpl implements SearchMappingRepository {
 
 	private final Map<MappingKey<?>, MappingImplementor<?>> mappings;
 	private final Map<String, BackendImplementor<?>> backends;
+	private final Map<String, IndexManagerImplementor<?>> indexManagers;
 
 	SearchMappingRepositoryImpl(Map<MappingKey<?>, MappingImplementor<?>> mappings,
-			Map<String, BackendImplementor<?>> backends) {
+			Map<String, BackendImplementor<?>> backends,
+			Map<String, IndexManagerImplementor<?>> indexManagers) {
 		this.mappings = mappings;
 		this.backends = backends;
+		this.indexManagers = indexManagers;
 	}
 
 	@Override
@@ -51,6 +55,7 @@ public class SearchMappingRepositoryImpl implements SearchMappingRepository {
 	public void close() {
 		try ( Closer<RuntimeException> closer = new Closer<>() ) {
 			closer.pushAll( MappingImplementor::close, mappings.values() );
+			closer.pushAll( IndexManagerImplementor::close, indexManagers.values() );
 			closer.pushAll( BackendImplementor::close, backends.values() );
 		}
 	}

util/internal/integrationtest/common/src/main/java/org/hibernate/search/util/impl/integrationtest/common/stub/backend/index/impl/StubIndexManager.java
@@ -10,16 +10,16 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 import org.hibernate.search.engine.backend.index.spi.ChangesetIndexWorker;
-import org.hibernate.search.engine.backend.index.spi.IndexManager;
+import org.hibernate.search.engine.backend.index.spi.IndexManagerImplementor;
 import org.hibernate.search.engine.backend.index.spi.IndexSearchTargetBuilder;
 import org.hibernate.search.engine.backend.index.spi.StreamIndexWorker;
 import org.hibernate.search.engine.common.spi.SessionContext;
-import org.hibernate.search.util.impl.test.rule.StaticCounters;
-import org.hibernate.search.util.impl.integrationtest.common.stub.backend.index.StubIndexWork;
 import org.hibernate.search.util.impl.integrationtest.common.stub.backend.document.impl.StubDocumentElement;
 import org.hibernate.search.util.impl.integrationtest.common.stub.backend.document.model.StubIndexSchemaNode;
+import org.hibernate.search.util.impl.integrationtest.common.stub.backend.index.StubIndexWork;
+import org.hibernate.search.util.impl.test.rule.StaticCounters;
 
-public class StubIndexManager implements IndexManager<StubDocumentElement> {
+public class StubIndexManager implements IndexManagerImplementor<StubDocumentElement> {
 
 	public static final StaticCounters.Key INSTANCE_COUNTER_KEY = StaticCounters.createKey();
 	public static final StaticCounters.Key CLOSE_COUNTER_KEY = StaticCounters.createKey();

util/internal/integrationtest/common/src/main/java/org/hibernate/search/util/impl/integrationtest/common/stub/backend/index/impl/StubIndexManagerBuilder.java
@@ -7,8 +7,8 @@
 package org.hibernate.search.util.impl.integrationtest.common.stub.backend.index.impl;
 
 import org.hibernate.search.engine.backend.document.model.dsl.spi.IndexSchemaRootNodeBuilder;
-import org.hibernate.search.engine.backend.index.spi.IndexManager;
 import org.hibernate.search.engine.backend.index.spi.IndexManagerBuilder;
+import org.hibernate.search.engine.backend.index.spi.IndexManagerImplementor;
 import org.hibernate.search.util.AssertionFailure;
 import org.hibernate.search.util.impl.integrationtest.common.stub.backend.document.impl.StubDocumentElement;
 import org.hibernate.search.util.impl.integrationtest.common.stub.backend.document.model.impl.StubIndexSchemaRootNodeBuilder;
@@ -51,7 +51,7 @@ public class StubIndexManagerBuilder implements IndexManagerBuilder<StubDocument
 	}
 
 	@Override
-	public IndexManager<StubDocumentElement> build() {
+	public IndexManagerImplementor<StubDocumentElement> build() {
 		if ( closed ) {
 			throw new AssertionFailure( "Unexpected call to build after a call to build() or to closeOnFailure()" );
 		}