hibernate/hibernate-search-6-poc

HSEARCH-3087 Add support for within polygon spatial predicates

backend-elasticsearch/src/main/java/org/hibernate/search/v6poc/backend/elasticsearch/types/predicate/impl/StandardFieldPredicateBuilderFactory.java
@@ -16,6 +16,7 @@ import org.hibernate.search.v6poc.backend.elasticsearch.types.codec.impl.Elastic
 import org.hibernate.search.v6poc.search.predicate.spi.MatchPredicateBuilder;
 import org.hibernate.search.v6poc.search.predicate.spi.RangePredicateBuilder;
 import org.hibernate.search.v6poc.search.predicate.spi.SpatialWithinCirclePredicateBuilder;
+import org.hibernate.search.v6poc.search.predicate.spi.SpatialWithinPolygonPredicateBuilder;
 import org.hibernate.search.v6poc.util.impl.common.LoggerFactory;
 
 
@@ -41,6 +42,11 @@ public class StandardFieldPredicateBuilderFactory implements ElasticsearchFieldP
 
 	@Override
 	public SpatialWithinCirclePredicateBuilder<ElasticsearchSearchPredicateCollector> createSpatialWithinCirclePredicateBuilder(String absoluteFieldPath) {
+		throw log.spatialPredicatesNotSupportedByFieldType( absoluteFieldPath );
+	}
+
+	@Override
+	public SpatialWithinPolygonPredicateBuilder<ElasticsearchSearchPredicateCollector> createSpatialWithinPolygonPredicateBuilder(String absoluteFieldPath) {
 		throw log.spatialPredicatesNotSupportedByFieldType( absoluteFieldPath );
 	}
 }

backend-lucene/src/main/java/org/hibernate/search/v6poc/backend/lucene/types/predicate/impl/AbstractStandardLuceneFieldPredicateBuilderFactory.java
@@ -11,6 +11,7 @@ import java.lang.invoke.MethodHandles;
 import org.hibernate.search.v6poc.backend.lucene.logging.impl.Log;
 import org.hibernate.search.v6poc.backend.lucene.search.predicate.impl.LuceneSearchPredicateCollector;
 import org.hibernate.search.v6poc.search.predicate.spi.SpatialWithinCirclePredicateBuilder;
+import org.hibernate.search.v6poc.search.predicate.spi.SpatialWithinPolygonPredicateBuilder;
 import org.hibernate.search.v6poc.util.impl.common.LoggerFactory;
 
 abstract class AbstractStandardLuceneFieldPredicateBuilderFactory implements LuceneFieldPredicateBuilderFactory {
@@ -22,4 +23,8 @@ abstract class AbstractStandardLuceneFieldPredicateBuilderFactory implements Luc
 		throw log.spatialPredicatesNotSupportedByFieldType( absoluteFieldPath );
 	}
 
+	@Override
+	public SpatialWithinPolygonPredicateBuilder<LuceneSearchPredicateCollector> createSpatialWithinPolygonPredicateBuilder(String absoluteFieldPath) {
+		throw log.spatialPredicatesNotSupportedByFieldType( absoluteFieldPath );
+	}
 }

engine/src/main/java/org/hibernate/search/v6poc/backend/spatial/GeoPolygon.java
@@ -1,0 +1,14 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.v6poc.backend.spatial;
+
+import java.util.List;
+
+public interface GeoPolygon {
+
+	List<GeoPoint> getPoints();
+}

engine/src/main/java/org/hibernate/search/v6poc/search/dsl/predicate/SpatialWithinPolygonPredicateContext.java
@@ -1,0 +1,18 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.v6poc.search.dsl.predicate;
+
+
+/**
+ * The context used when closing a within polygon spatial predicate.
+ *
+ * @param <N> The type of the next context.
+ */
+public interface SpatialWithinPolygonPredicateContext<N> {
+
+	N end();
+}

engine/src/main/java/org/hibernate/search/v6poc/search/dsl/predicate/SpatialWithinPredicateFieldSetContext.java
@@ -8,6 +8,7 @@ package org.hibernate.search.v6poc.search.dsl.predicate;
 
 import org.hibernate.search.v6poc.backend.spatial.DistanceUnit;
 import org.hibernate.search.v6poc.backend.spatial.GeoPoint;
+import org.hibernate.search.v6poc.backend.spatial.GeoPolygon;
 import org.hibernate.search.v6poc.backend.spatial.ImmutableGeoPoint;
 import org.hibernate.search.v6poc.util.impl.common.Contracts;
 
@@ -37,4 +38,6 @@ public interface SpatialWithinPredicateFieldSetContext<N> extends MultiFieldPred
 
 		return circle( new ImmutableGeoPoint( latitude, longitude ), unit.toMeters( radius ) );
 	}
+
+	SpatialWithinPolygonPredicateContext<N> polygon(GeoPolygon polygon);
 }

engine/src/main/java/org/hibernate/search/v6poc/search/dsl/predicate/impl/SpatialWithinPolygonPredicateContextImpl.java
@@ -1,0 +1,25 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.v6poc.search.dsl.predicate.impl;
+
+import java.util.function.Supplier;
+
+import org.hibernate.search.v6poc.search.dsl.predicate.SpatialWithinPolygonPredicateContext;
+
+public class SpatialWithinPolygonPredicateContextImpl<N> implements SpatialWithinPolygonPredicateContext<N> {
+
+	private final Supplier<N> nextContextProvider;
+
+	SpatialWithinPolygonPredicateContextImpl(Supplier<N> nextContextProvider) {
+		this.nextContextProvider = nextContextProvider;
+	}
+
+	@Override
+	public N end() {
+		return nextContextProvider.get();
+	}
+}

engine/src/main/java/org/hibernate/search/v6poc/search/dsl/predicate/impl/SpatialWithinPredicateFieldSetContextImpl.java
@@ -13,11 +13,14 @@ import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 import org.hibernate.search.v6poc.backend.spatial.GeoPoint;
+import org.hibernate.search.v6poc.backend.spatial.GeoPolygon;
 import org.hibernate.search.v6poc.search.dsl.predicate.SpatialWithinCirclePredicateContext;
+import org.hibernate.search.v6poc.search.dsl.predicate.SpatialWithinPolygonPredicateContext;
 import org.hibernate.search.v6poc.search.dsl.predicate.SpatialWithinPredicateFieldSetContext;
 import org.hibernate.search.v6poc.search.predicate.spi.SearchPredicateBuilder;
 import org.hibernate.search.v6poc.search.predicate.spi.SearchPredicateFactory;
 import org.hibernate.search.v6poc.search.predicate.spi.SpatialWithinCirclePredicateBuilder;
+import org.hibernate.search.v6poc.search.predicate.spi.SpatialWithinPolygonPredicateBuilder;
 import org.hibernate.search.v6poc.util.impl.common.CollectionHelper;
 import org.hibernate.search.v6poc.util.impl.common.Contracts;
 
@@ -60,6 +63,13 @@ class SpatialWithinPredicateFieldSetContextImpl<N, C>
 	}
 
 	@Override
+	public SpatialWithinPolygonPredicateContext<N> polygon(GeoPolygon polygon) {
+		Contracts.assertNotNull( polygon, "polygon" );
+
+		return commonState.polygon( polygon );
+	}
+
+	@Override
 	public void contributePredicateBuilders(Consumer<SearchPredicateBuilder<? super C>> collector) {
 		queryBuilders.forEach( collector );
 	}
@@ -68,6 +78,17 @@ class SpatialWithinPredicateFieldSetContextImpl<N, C>
 		for ( String absoluteFieldPath : absoluteFieldPaths ) {
 			SpatialWithinCirclePredicateBuilder<C> predicateBuilder = commonState.getFactory().spatialWithinCircle( absoluteFieldPath );
 			predicateBuilder.circle( center, radiusInMeters );
+			if ( boost != null ) {
+				predicateBuilder.boost( boost );
+			}
+			queryBuilders.add( predicateBuilder );
+		}
+	}
+
+	private void generateWithinPolygonQueryBuilders(GeoPolygon polygon) {
+		for ( String absoluteFieldPath : absoluteFieldPaths ) {
+			SpatialWithinPolygonPredicateBuilder<C> predicateBuilder = commonState.getFactory().spatialWithinPolygon( absoluteFieldPath );
+			predicateBuilder.polygon( polygon );
 			if ( boost != null ) {
 				predicateBuilder.boost( boost );
 			}
@@ -87,6 +108,14 @@ class SpatialWithinPredicateFieldSetContextImpl<N, C>
 			}
 
 			return new SpatialWithinCirclePredicateContextImpl<N>( getNextContextProvider() );
+		}
+
+		public SpatialWithinPolygonPredicateContext<N> polygon(GeoPolygon polygon) {
+			for ( SpatialWithinPredicateFieldSetContextImpl<N, C> fieldSetContext : getFieldSetContexts() ) {
+				fieldSetContext.generateWithinPolygonQueryBuilders( polygon );
+			}
+
+			return new SpatialWithinPolygonPredicateContextImpl<N>( getNextContextProvider() );
 		}
 	}
 }

hibernate/hibernate-search

backend/elasticsearch/src/main/java/org/hibernate/search/backend/elasticsearch/types/predicate/impl/StandardFieldPredicateBuilderFactory.java
@@ -16,6 +16,7 @@ import org.hibernate.search.backend.elasticsearch.types.codec.impl.Elasticsearch
 import org.hibernate.search.engine.search.predicate.spi.MatchPredicateBuilder;
 import org.hibernate.search.engine.search.predicate.spi.RangePredicateBuilder;
 import org.hibernate.search.engine.search.predicate.spi.SpatialWithinCirclePredicateBuilder;
+import org.hibernate.search.engine.search.predicate.spi.SpatialWithinPolygonPredicateBuilder;
 import org.hibernate.search.util.impl.common.LoggerFactory;
 
 
@@ -41,6 +42,11 @@ public class StandardFieldPredicateBuilderFactory implements ElasticsearchFieldP
 
 	@Override
 	public SpatialWithinCirclePredicateBuilder<ElasticsearchSearchPredicateCollector> createSpatialWithinCirclePredicateBuilder(String absoluteFieldPath) {
+		throw log.spatialPredicatesNotSupportedByFieldType( absoluteFieldPath );
+	}
+
+	@Override
+	public SpatialWithinPolygonPredicateBuilder<ElasticsearchSearchPredicateCollector> createSpatialWithinPolygonPredicateBuilder(String absoluteFieldPath) {
 		throw log.spatialPredicatesNotSupportedByFieldType( absoluteFieldPath );
 	}
 }

backend/lucene/src/main/java/org/hibernate/search/backend/lucene/types/predicate/impl/AbstractStandardLuceneFieldPredicateBuilderFactory.java
@@ -11,6 +11,7 @@ import java.lang.invoke.MethodHandles;
 import org.hibernate.search.backend.lucene.logging.impl.Log;
 import org.hibernate.search.backend.lucene.search.predicate.impl.LuceneSearchPredicateCollector;
 import org.hibernate.search.engine.search.predicate.spi.SpatialWithinCirclePredicateBuilder;
+import org.hibernate.search.engine.search.predicate.spi.SpatialWithinPolygonPredicateBuilder;
 import org.hibernate.search.util.impl.common.LoggerFactory;
 
 abstract class AbstractStandardLuceneFieldPredicateBuilderFactory implements LuceneFieldPredicateBuilderFactory {
@@ -22,4 +23,8 @@ abstract class AbstractStandardLuceneFieldPredicateBuilderFactory implements Luc
 		throw log.spatialPredicatesNotSupportedByFieldType( absoluteFieldPath );
 	}
 
+	@Override
+	public SpatialWithinPolygonPredicateBuilder<LuceneSearchPredicateCollector> createSpatialWithinPolygonPredicateBuilder(String absoluteFieldPath) {
+		throw log.spatialPredicatesNotSupportedByFieldType( absoluteFieldPath );
+	}
 }

engine/src/main/java/org/hibernate/search/engine/backend/spatial/GeoPolygon.java
@@ -1,0 +1,14 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.engine.backend.spatial;
+
+import java.util.List;
+
+public interface GeoPolygon {
+
+	List<GeoPoint> getPoints();
+}

engine/src/main/java/org/hibernate/search/engine/search/dsl/predicate/SpatialWithinPolygonPredicateContext.java
@@ -1,0 +1,18 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.engine.search.dsl.predicate;
+
+
+/**
+ * The context used when closing a within polygon spatial predicate.
+ *
+ * @param <N> The type of the next context.
+ */
+public interface SpatialWithinPolygonPredicateContext<N> {
+
+	N end();
+}

engine/src/main/java/org/hibernate/search/engine/search/dsl/predicate/SpatialWithinPredicateFieldSetContext.java
@@ -8,6 +8,7 @@ package org.hibernate.search.engine.search.dsl.predicate;
 
 import org.hibernate.search.engine.backend.spatial.DistanceUnit;
 import org.hibernate.search.engine.backend.spatial.GeoPoint;
+import org.hibernate.search.engine.backend.spatial.GeoPolygon;
 import org.hibernate.search.engine.backend.spatial.ImmutableGeoPoint;
 import org.hibernate.search.util.impl.common.Contracts;
 
@@ -37,4 +38,6 @@ public interface SpatialWithinPredicateFieldSetContext<N> extends MultiFieldPred
 
 		return circle( new ImmutableGeoPoint( latitude, longitude ), unit.toMeters( radius ) );
 	}
+
+	SpatialWithinPolygonPredicateContext<N> polygon(GeoPolygon polygon);
 }

engine/src/main/java/org/hibernate/search/engine/search/dsl/predicate/impl/SpatialWithinPolygonPredicateContextImpl.java
@@ -1,0 +1,25 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.engine.search.dsl.predicate.impl;
+
+import java.util.function.Supplier;
+
+import org.hibernate.search.engine.search.dsl.predicate.SpatialWithinPolygonPredicateContext;
+
+public class SpatialWithinPolygonPredicateContextImpl<N> implements SpatialWithinPolygonPredicateContext<N> {
+
+	private final Supplier<N> nextContextProvider;
+
+	SpatialWithinPolygonPredicateContextImpl(Supplier<N> nextContextProvider) {
+		this.nextContextProvider = nextContextProvider;
+	}
+
+	@Override
+	public N end() {
+		return nextContextProvider.get();
+	}
+}

engine/src/main/java/org/hibernate/search/engine/search/dsl/predicate/impl/SpatialWithinPredicateFieldSetContextImpl.java
@@ -13,11 +13,14 @@ import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 import org.hibernate.search.engine.backend.spatial.GeoPoint;
+import org.hibernate.search.engine.backend.spatial.GeoPolygon;
 import org.hibernate.search.engine.search.dsl.predicate.SpatialWithinCirclePredicateContext;
+import org.hibernate.search.engine.search.dsl.predicate.SpatialWithinPolygonPredicateContext;
 import org.hibernate.search.engine.search.dsl.predicate.SpatialWithinPredicateFieldSetContext;
 import org.hibernate.search.engine.search.predicate.spi.SearchPredicateBuilder;
 import org.hibernate.search.engine.search.predicate.spi.SearchPredicateFactory;
 import org.hibernate.search.engine.search.predicate.spi.SpatialWithinCirclePredicateBuilder;
+import org.hibernate.search.engine.search.predicate.spi.SpatialWithinPolygonPredicateBuilder;
 import org.hibernate.search.util.impl.common.CollectionHelper;
 import org.hibernate.search.util.impl.common.Contracts;
 
@@ -60,6 +63,13 @@ class SpatialWithinPredicateFieldSetContextImpl<N, C>
 	}
 
 	@Override
+	public SpatialWithinPolygonPredicateContext<N> polygon(GeoPolygon polygon) {
+		Contracts.assertNotNull( polygon, "polygon" );
+
+		return commonState.polygon( polygon );
+	}
+
+	@Override
 	public void contributePredicateBuilders(Consumer<SearchPredicateBuilder<? super C>> collector) {
 		queryBuilders.forEach( collector );
 	}
@@ -68,6 +78,17 @@ class SpatialWithinPredicateFieldSetContextImpl<N, C>
 		for ( String absoluteFieldPath : absoluteFieldPaths ) {
 			SpatialWithinCirclePredicateBuilder<C> predicateBuilder = commonState.getFactory().spatialWithinCircle( absoluteFieldPath );
 			predicateBuilder.circle( center, radiusInMeters );
+			if ( boost != null ) {
+				predicateBuilder.boost( boost );
+			}
+			queryBuilders.add( predicateBuilder );
+		}
+	}
+
+	private void generateWithinPolygonQueryBuilders(GeoPolygon polygon) {
+		for ( String absoluteFieldPath : absoluteFieldPaths ) {
+			SpatialWithinPolygonPredicateBuilder<C> predicateBuilder = commonState.getFactory().spatialWithinPolygon( absoluteFieldPath );
+			predicateBuilder.polygon( polygon );
 			if ( boost != null ) {
 				predicateBuilder.boost( boost );
 			}
@@ -87,6 +108,14 @@ class SpatialWithinPredicateFieldSetContextImpl<N, C>
 			}
 
 			return new SpatialWithinCirclePredicateContextImpl<N>( getNextContextProvider() );
+		}
+
+		public SpatialWithinPolygonPredicateContext<N> polygon(GeoPolygon polygon) {
+			for ( SpatialWithinPredicateFieldSetContextImpl<N, C> fieldSetContext : getFieldSetContexts() ) {
+				fieldSetContext.generateWithinPolygonQueryBuilders( polygon );
+			}
+
+			return new SpatialWithinPolygonPredicateContextImpl<N>( getNextContextProvider() );
 		}
 	}
 }