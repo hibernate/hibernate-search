hibernate/hibernate-search-6-poc

HSEARCH-3221 Use more explicit error messages when conversion errors occur in the DSL

backend-elasticsearch/src/main/java/org/hibernate/search/v6poc/backend/elasticsearch/search/predicate/impl/MatchPredicateBuilderImpl.java
@@ -6,10 +6,15 @@
  */
 package org.hibernate.search.v6poc.backend.elasticsearch.search.predicate.impl;
 
+import java.lang.invoke.MethodHandles;
+
 import org.hibernate.search.v6poc.backend.elasticsearch.gson.impl.JsonAccessor;
 import org.hibernate.search.v6poc.backend.elasticsearch.gson.impl.JsonObjectAccessor;
+import org.hibernate.search.v6poc.backend.elasticsearch.logging.impl.Log;
 import org.hibernate.search.v6poc.backend.elasticsearch.types.converter.impl.ElasticsearchFieldConverter;
+import org.hibernate.search.v6poc.logging.spi.EventContexts;
 import org.hibernate.search.v6poc.search.predicate.spi.MatchPredicateBuilder;
+import org.hibernate.search.v6poc.util.impl.common.LoggerFactory;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
@@ -19,6 +24,8 @@ import com.google.gson.JsonObject;
  */
 public class MatchPredicateBuilderImpl extends AbstractSearchPredicateBuilder
 		implements MatchPredicateBuilder<ElasticsearchSearchPredicateBuilder> {
+
+	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
 
 	private static final JsonAccessor<JsonElement> QUERY = JsonAccessor.root().property( "query" );
 
@@ -34,7 +41,16 @@ public class MatchPredicateBuilderImpl extends AbstractSearchPredicateBuilder
 
 	@Override
 	public void value(Object value) {
-		QUERY.set( getInnerObject(), converter.convertFromDsl( value ) );
+		JsonElement element;
+		try {
+			element = converter.convertFromDsl( value );
+		}
+		catch (RuntimeException e) {
+			throw log.cannotConvertDslParameter(
+					e.getMessage(), e, EventContexts.fromIndexFieldAbsolutePath( absoluteFieldPath )
+			);
+		}
+		QUERY.set( getInnerObject(), element );
 	}
 
 	@Override

backend-elasticsearch/src/main/java/org/hibernate/search/v6poc/backend/elasticsearch/search/predicate/impl/RangePredicateBuilderImpl.java
@@ -6,10 +6,15 @@
  */
 package org.hibernate.search.v6poc.backend.elasticsearch.search.predicate.impl;
 
+import java.lang.invoke.MethodHandles;
+
 import org.hibernate.search.v6poc.backend.elasticsearch.gson.impl.JsonAccessor;
 import org.hibernate.search.v6poc.backend.elasticsearch.gson.impl.JsonObjectAccessor;
+import org.hibernate.search.v6poc.backend.elasticsearch.logging.impl.Log;
 import org.hibernate.search.v6poc.backend.elasticsearch.types.converter.impl.ElasticsearchFieldConverter;
+import org.hibernate.search.v6poc.logging.spi.EventContexts;
 import org.hibernate.search.v6poc.search.predicate.spi.RangePredicateBuilder;
+import org.hibernate.search.v6poc.util.impl.common.LoggerFactory;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
@@ -19,6 +24,8 @@ import com.google.gson.JsonObject;
  */
 public class RangePredicateBuilderImpl extends AbstractSearchPredicateBuilder
 		implements RangePredicateBuilder<ElasticsearchSearchPredicateBuilder> {
+
+	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
 
 	private static final JsonObjectAccessor RANGE = JsonAccessor.root().property( "range" ).asObject();
 
@@ -42,7 +49,14 @@ public class RangePredicateBuilderImpl extends AbstractSearchPredicateBuilder
 
 	@Override
 	public void lowerLimit(Object value) {
-		this.lowerLimit = converter.convertFromDsl( value );
+		try {
+			this.lowerLimit = converter.convertFromDsl( value );
+		}
+		catch (RuntimeException e) {
+			throw log.cannotConvertDslParameter(
+					e.getMessage(), e, EventContexts.fromIndexFieldAbsolutePath( absoluteFieldPath )
+			);
+		}
 	}
 
 	@Override
@@ -52,7 +66,14 @@ public class RangePredicateBuilderImpl extends AbstractSearchPredicateBuilder
 
 	@Override
 	public void upperLimit(Object value) {
-		this.upperLimit = converter.convertFromDsl( value );
+		try {
+			this.upperLimit = converter.convertFromDsl( value );
+		}
+		catch (RuntimeException e) {
+			throw log.cannotConvertDslParameter(
+					e.getMessage(), e, EventContexts.fromIndexFieldAbsolutePath( absoluteFieldPath )
+			);
+		}
 	}
 
 	@Override

backend-lucene/src/main/java/org/hibernate/search/v6poc/backend/lucene/search/predicate/impl/AbstractMatchPredicateBuilder.java
@@ -6,12 +6,19 @@
  */
 package org.hibernate.search.v6poc.backend.lucene.search.predicate.impl;
 
+import java.lang.invoke.MethodHandles;
+
+import org.hibernate.search.v6poc.backend.lucene.logging.impl.Log;
 import org.hibernate.search.v6poc.backend.lucene.types.converter.impl.LuceneFieldConverter;
+import org.hibernate.search.v6poc.logging.spi.EventContexts;
 import org.hibernate.search.v6poc.search.predicate.spi.MatchPredicateBuilder;
+import org.hibernate.search.v6poc.util.impl.common.LoggerFactory;
 
 
 public abstract class AbstractMatchPredicateBuilder<F, T> extends AbstractSearchPredicateBuilder
 		implements MatchPredicateBuilder<LuceneSearchPredicateBuilder> {
+
+	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
 
 	protected final String absoluteFieldPath;
 	private final LuceneFieldConverter<?, T> converter;
@@ -25,6 +32,13 @@ public abstract class AbstractMatchPredicateBuilder<F, T> extends AbstractSearch
 
 	@Override
 	public void value(Object value) {
-		this.value = converter.convertFromDsl( value );
+		try {
+			this.value = converter.convertFromDsl( value );
+		}
+		catch (RuntimeException e) {
+			throw log.cannotConvertDslParameter(
+					e.getMessage(), e, EventContexts.fromIndexFieldAbsolutePath( absoluteFieldPath )
+			);
+		}
 	}
 }

backend-lucene/src/main/java/org/hibernate/search/v6poc/backend/lucene/search/predicate/impl/AbstractRangePredicateBuilder.java
@@ -6,12 +6,19 @@
  */
 package org.hibernate.search.v6poc.backend.lucene.search.predicate.impl;
 
+import java.lang.invoke.MethodHandles;
+
+import org.hibernate.search.v6poc.backend.lucene.logging.impl.Log;
 import org.hibernate.search.v6poc.backend.lucene.types.converter.impl.LuceneFieldConverter;
+import org.hibernate.search.v6poc.logging.spi.EventContexts;
 import org.hibernate.search.v6poc.search.predicate.spi.RangePredicateBuilder;
+import org.hibernate.search.v6poc.util.impl.common.LoggerFactory;
 
 
 public abstract class AbstractRangePredicateBuilder<F> extends AbstractSearchPredicateBuilder
 		implements RangePredicateBuilder<LuceneSearchPredicateBuilder> {
+
+	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
 
 	protected final String absoluteFieldPath;
 
@@ -32,7 +39,14 @@ public abstract class AbstractRangePredicateBuilder<F> extends AbstractSearchPre
 
 	@Override
 	public void lowerLimit(Object value) {
-		lowerLimit = converter.convertFromDsl( value );
+		try {
+			lowerLimit = converter.convertFromDsl( value );
+		}
+		catch (RuntimeException e) {
+			throw log.cannotConvertDslParameter(
+					e.getMessage(), e, EventContexts.fromIndexFieldAbsolutePath( absoluteFieldPath )
+			);
+		}
 	}
 
 	@Override
@@ -42,7 +56,14 @@ public abstract class AbstractRangePredicateBuilder<F> extends AbstractSearchPre
 
 	@Override
 	public void upperLimit(Object value) {
-		upperLimit = converter.convertFromDsl( value );
+		try {
+			upperLimit = converter.convertFromDsl( value );
+		}
+		catch (RuntimeException e) {
+			throw log.cannotConvertDslParameter(
+					e.getMessage(), e, EventContexts.fromIndexFieldAbsolutePath( absoluteFieldPath )
+			);
+		}
 	}
 
 	@Override

hibernate/hibernate-search

backend/elasticsearch/src/main/java/org/hibernate/search/backend/elasticsearch/search/predicate/impl/MatchPredicateBuilderImpl.java
@@ -6,10 +6,15 @@
  */
 package org.hibernate.search.backend.elasticsearch.search.predicate.impl;
 
+import java.lang.invoke.MethodHandles;
+
 import org.hibernate.search.backend.elasticsearch.gson.impl.JsonAccessor;
 import org.hibernate.search.backend.elasticsearch.gson.impl.JsonObjectAccessor;
+import org.hibernate.search.backend.elasticsearch.logging.impl.Log;
 import org.hibernate.search.backend.elasticsearch.types.converter.impl.ElasticsearchFieldConverter;
+import org.hibernate.search.engine.logging.spi.EventContexts;
 import org.hibernate.search.engine.search.predicate.spi.MatchPredicateBuilder;
+import org.hibernate.search.util.impl.common.LoggerFactory;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
@@ -19,6 +24,8 @@ import com.google.gson.JsonObject;
  */
 public class MatchPredicateBuilderImpl extends AbstractSearchPredicateBuilder
 		implements MatchPredicateBuilder<ElasticsearchSearchPredicateBuilder> {
+
+	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
 
 	private static final JsonAccessor<JsonElement> QUERY = JsonAccessor.root().property( "query" );
 
@@ -34,7 +41,16 @@ public class MatchPredicateBuilderImpl extends AbstractSearchPredicateBuilder
 
 	@Override
 	public void value(Object value) {
-		QUERY.set( getInnerObject(), converter.convertFromDsl( value ) );
+		JsonElement element;
+		try {
+			element = converter.convertFromDsl( value );
+		}
+		catch (RuntimeException e) {
+			throw log.cannotConvertDslParameter(
+					e.getMessage(), e, EventContexts.fromIndexFieldAbsolutePath( absoluteFieldPath )
+			);
+		}
+		QUERY.set( getInnerObject(), element );
 	}
 
 	@Override

backend/elasticsearch/src/main/java/org/hibernate/search/backend/elasticsearch/search/predicate/impl/RangePredicateBuilderImpl.java
@@ -6,10 +6,15 @@
  */
 package org.hibernate.search.backend.elasticsearch.search.predicate.impl;
 
+import java.lang.invoke.MethodHandles;
+
 import org.hibernate.search.backend.elasticsearch.gson.impl.JsonAccessor;
 import org.hibernate.search.backend.elasticsearch.gson.impl.JsonObjectAccessor;
+import org.hibernate.search.backend.elasticsearch.logging.impl.Log;
 import org.hibernate.search.backend.elasticsearch.types.converter.impl.ElasticsearchFieldConverter;
+import org.hibernate.search.engine.logging.spi.EventContexts;
 import org.hibernate.search.engine.search.predicate.spi.RangePredicateBuilder;
+import org.hibernate.search.util.impl.common.LoggerFactory;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
@@ -19,6 +24,8 @@ import com.google.gson.JsonObject;
  */
 public class RangePredicateBuilderImpl extends AbstractSearchPredicateBuilder
 		implements RangePredicateBuilder<ElasticsearchSearchPredicateBuilder> {
+
+	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
 
 	private static final JsonObjectAccessor RANGE = JsonAccessor.root().property( "range" ).asObject();
 
@@ -42,7 +49,14 @@ public class RangePredicateBuilderImpl extends AbstractSearchPredicateBuilder
 
 	@Override
 	public void lowerLimit(Object value) {
-		this.lowerLimit = converter.convertFromDsl( value );
+		try {
+			this.lowerLimit = converter.convertFromDsl( value );
+		}
+		catch (RuntimeException e) {
+			throw log.cannotConvertDslParameter(
+					e.getMessage(), e, EventContexts.fromIndexFieldAbsolutePath( absoluteFieldPath )
+			);
+		}
 	}
 
 	@Override
@@ -52,7 +66,14 @@ public class RangePredicateBuilderImpl extends AbstractSearchPredicateBuilder
 
 	@Override
 	public void upperLimit(Object value) {
-		this.upperLimit = converter.convertFromDsl( value );
+		try {
+			this.upperLimit = converter.convertFromDsl( value );
+		}
+		catch (RuntimeException e) {
+			throw log.cannotConvertDslParameter(
+					e.getMessage(), e, EventContexts.fromIndexFieldAbsolutePath( absoluteFieldPath )
+			);
+		}
 	}
 
 	@Override

backend/lucene/src/main/java/org/hibernate/search/backend/lucene/search/predicate/impl/AbstractMatchPredicateBuilder.java
@@ -6,12 +6,19 @@
  */
 package org.hibernate.search.backend.lucene.search.predicate.impl;
 
+import java.lang.invoke.MethodHandles;
+
+import org.hibernate.search.backend.lucene.logging.impl.Log;
 import org.hibernate.search.backend.lucene.types.converter.impl.LuceneFieldConverter;
+import org.hibernate.search.engine.logging.spi.EventContexts;
 import org.hibernate.search.engine.search.predicate.spi.MatchPredicateBuilder;
+import org.hibernate.search.util.impl.common.LoggerFactory;
 
 
 public abstract class AbstractMatchPredicateBuilder<F, T> extends AbstractSearchPredicateBuilder
 		implements MatchPredicateBuilder<LuceneSearchPredicateBuilder> {
+
+	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
 
 	protected final String absoluteFieldPath;
 	private final LuceneFieldConverter<?, T> converter;
@@ -25,6 +32,13 @@ public abstract class AbstractMatchPredicateBuilder<F, T> extends AbstractSearch
 
 	@Override
 	public void value(Object value) {
-		this.value = converter.convertFromDsl( value );
+		try {
+			this.value = converter.convertFromDsl( value );
+		}
+		catch (RuntimeException e) {
+			throw log.cannotConvertDslParameter(
+					e.getMessage(), e, EventContexts.fromIndexFieldAbsolutePath( absoluteFieldPath )
+			);
+		}
 	}
 }

backend/lucene/src/main/java/org/hibernate/search/backend/lucene/search/predicate/impl/AbstractRangePredicateBuilder.java
@@ -6,12 +6,19 @@
  */
 package org.hibernate.search.backend.lucene.search.predicate.impl;
 
+import java.lang.invoke.MethodHandles;
+
+import org.hibernate.search.backend.lucene.logging.impl.Log;
 import org.hibernate.search.backend.lucene.types.converter.impl.LuceneFieldConverter;
+import org.hibernate.search.engine.logging.spi.EventContexts;
 import org.hibernate.search.engine.search.predicate.spi.RangePredicateBuilder;
+import org.hibernate.search.util.impl.common.LoggerFactory;
 
 
 public abstract class AbstractRangePredicateBuilder<F> extends AbstractSearchPredicateBuilder
 		implements RangePredicateBuilder<LuceneSearchPredicateBuilder> {
+
+	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
 
 	protected final String absoluteFieldPath;
 
@@ -32,7 +39,14 @@ public abstract class AbstractRangePredicateBuilder<F> extends AbstractSearchPre
 
 	@Override
 	public void lowerLimit(Object value) {
-		lowerLimit = converter.convertFromDsl( value );
+		try {
+			lowerLimit = converter.convertFromDsl( value );
+		}
+		catch (RuntimeException e) {
+			throw log.cannotConvertDslParameter(
+					e.getMessage(), e, EventContexts.fromIndexFieldAbsolutePath( absoluteFieldPath )
+			);
+		}
 	}
 
 	@Override
@@ -42,7 +56,14 @@ public abstract class AbstractRangePredicateBuilder<F> extends AbstractSearchPre
 
 	@Override
 	public void upperLimit(Object value) {
-		upperLimit = converter.convertFromDsl( value );
+		try {
+			upperLimit = converter.convertFromDsl( value );
+		}
+		catch (RuntimeException e) {
+			throw log.cannotConvertDslParameter(
+					e.getMessage(), e, EventContexts.fromIndexFieldAbsolutePath( absoluteFieldPath )
+			);
+		}
 	}
 
 	@Override