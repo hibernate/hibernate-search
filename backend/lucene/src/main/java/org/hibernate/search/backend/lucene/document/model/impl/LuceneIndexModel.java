hibernate/hibernate-search-6-poc

HSEARCH-2546 Extend the use of failure contexts to most backend exceptions
We won't add them to an error report, but at least the way we handle
failure contexts will be consistent, both in our code and in messages
displayed to the user.

backend-lucene/src/main/java/org/hibernate/search/v6poc/backend/lucene/document/model/impl/LuceneIndexModel.java
@@ -9,9 +9,12 @@ package org.hibernate.search.v6poc.backend.lucene.document.model.impl;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.hibernate.search.v6poc.logging.spi.FailureContext;
+import org.hibernate.search.v6poc.logging.spi.FailureContexts;
+import org.hibernate.search.v6poc.util.impl.common.CollectionHelper;
+
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.core.KeywordAnalyzer;
-import org.hibernate.search.v6poc.util.impl.common.CollectionHelper;
 
 /**
  * @author Guillaume Smet
@@ -62,6 +65,10 @@ public class LuceneIndexModel implements AutoCloseable {
 
 	public String getIndexName() {
 		return indexName;
+	}
+
+	public FailureContext getFailureContext() {
+		return FailureContexts.fromIndexName( indexName );
 	}
 
 	public LuceneIndexSchemaFieldNode<?> getFieldNode(String absoluteFieldPath) {

backend-lucene/src/main/java/org/hibernate/search/v6poc/backend/lucene/impl/LuceneLocalDirectoryBackend.java
@@ -25,6 +25,8 @@ import org.hibernate.search.v6poc.backend.lucene.work.impl.LuceneWorkFactory;
 import org.hibernate.search.v6poc.backend.spi.BackendImplementor;
 import org.hibernate.search.v6poc.cfg.ConfigurationPropertySource;
 import org.hibernate.search.v6poc.backend.spi.BackendBuildContext;
+import org.hibernate.search.v6poc.logging.spi.FailureContext;
+import org.hibernate.search.v6poc.logging.spi.FailureContexts;
 import org.hibernate.search.v6poc.util.impl.common.Closer;
 import org.hibernate.search.v6poc.util.impl.common.LoggerFactory;
 
@@ -42,6 +44,7 @@ public class LuceneLocalDirectoryBackend implements BackendImplementor<LuceneRoo
 	private final LuceneQueryWorkOrchestrator queryOrchestrator;
 	private final MultiTenancyStrategy multiTenancyStrategy;
 
+	private final FailureContext failureContext;
 	private final IndexingBackendContext indexingContext;
 	private final SearchBackendContext searchContext;
 
@@ -53,15 +56,16 @@ public class LuceneLocalDirectoryBackend implements BackendImplementor<LuceneRoo
 		this.queryOrchestrator = new StubLuceneQueryWorkOrchestrator();
 		this.multiTenancyStrategy = multiTenancyStrategy;
 
+		this.failureContext = FailureContexts.fromBackendName( name );
 		this.indexingContext = new IndexingBackendContext(
-				this, new MMapDirectoryProvider( this, rootDirectory ),
+				failureContext, new MMapDirectoryProvider( failureContext, rootDirectory ),
 				workFactory, multiTenancyStrategy
 		);
 		this.searchContext = new SearchBackendContext(
-				this, workFactory, multiTenancyStrategy, queryOrchestrator
+				failureContext, workFactory, multiTenancyStrategy, queryOrchestrator
 		);
 
-		initializeRootDirectory( name, rootDirectory );
+		initializeRootDirectory( rootDirectory );
 	}
 
 	@Override
@@ -69,7 +73,9 @@ public class LuceneLocalDirectoryBackend implements BackendImplementor<LuceneRoo
 		if ( LuceneBackend.class.isAssignableFrom( clazz ) ) {
 			return (T) this;
 		}
-		throw log.backendUnwrappingWithUnknownType( clazz, LuceneBackend.class );
+		throw log.backendUnwrappingWithUnknownType(
+				clazz, LuceneBackend.class, failureContext
+		);
 	}
 
 	@Override
@@ -81,7 +87,7 @@ public class LuceneLocalDirectoryBackend implements BackendImplementor<LuceneRoo
 	public IndexManagerBuilder<LuceneRootDocumentBuilder> createIndexManagerBuilder(
 			String indexName, boolean multiTenancyEnabled, BackendBuildContext context, ConfigurationPropertySource propertySource) {
 		if ( multiTenancyEnabled && !multiTenancyStrategy.isMultiTenancySupported() ) {
-			throw log.multiTenancyRequiredButNotSupportedByBackend( this, indexName );
+			throw log.multiTenancyRequiredButNotSupportedByBackend( indexName, failureContext );
 		}
 
 		/*
@@ -112,10 +118,10 @@ public class LuceneLocalDirectoryBackend implements BackendImplementor<LuceneRoo
 				.toString();
 	}
 
-	private static void initializeRootDirectory(String name, Path rootDirectory) {
+	private void initializeRootDirectory(Path rootDirectory) {
 		if ( Files.exists( rootDirectory ) ) {
 			if ( !Files.isDirectory( rootDirectory ) || !Files.isWritable( rootDirectory ) ) {
-				throw log.localDirectoryBackendRootDirectoryNotWritableDirectory( name, rootDirectory );
+				throw log.localDirectoryBackendRootDirectoryNotWritableDirectory( rootDirectory, failureContext );
 			}
 		}
 		else {
@@ -123,7 +129,7 @@ public class LuceneLocalDirectoryBackend implements BackendImplementor<LuceneRoo
 				Files.createDirectories( rootDirectory );
 			}
 			catch (Exception e) {
-				throw log.unableToCreateRootDirectoryForLocalDirectoryBackend( name, rootDirectory, e );
+				throw log.unableToCreateRootDirectoryForLocalDirectoryBackend( rootDirectory, failureContext, e );
 			}
 		}
 	}

backend-lucene/src/main/java/org/hibernate/search/v6poc/backend/lucene/search/impl/LuceneSearchTargetModel.java
@@ -16,6 +16,8 @@ import org.hibernate.search.v6poc.backend.lucene.document.model.impl.LuceneIndex
 import org.hibernate.search.v6poc.backend.lucene.document.model.impl.LuceneIndexSchemaObjectNode;
 import org.hibernate.search.v6poc.backend.lucene.index.spi.ReaderProvider;
 import org.hibernate.search.v6poc.backend.lucene.logging.impl.Log;
+import org.hibernate.search.v6poc.logging.spi.FailureContext;
+import org.hibernate.search.v6poc.logging.spi.FailureContexts;
 import org.hibernate.search.v6poc.util.impl.common.LoggerFactory;
 
 public class LuceneSearchTargetModel {
@@ -36,6 +38,10 @@ public class LuceneSearchTargetModel {
 
 	public Set<String> getIndexNames() {
 		return indexNames;
+	}
+
+	public FailureContext getIndexesFailureContext() {
+		return FailureContexts.fromIndexNames( indexNames );
 	}
 
 	public Set<LuceneIndexModel> getIndexModels() {
@@ -60,13 +66,17 @@ public class LuceneSearchTargetModel {
 				else if ( !selectedSchemaNode.isCompatibleWith( schemaNode ) ) {
 					throw log.conflictingFieldTypesForSearch(
 							absoluteFieldPath,
-							selectedSchemaNode, indexModelForSelectedSchemaNode.getIndexName(),
-							schemaNode, indexModel.getIndexName() );
+							selectedSchemaNode, schemaNode,
+							FailureContexts.fromIndexNames(
+									indexModelForSelectedSchemaNode.getIndexName(),
+									indexModel.getIndexName()
+							)
+					);
 				}
 			}
 		}
 		if ( selectedSchemaNode == null ) {
-			throw log.unknownFieldForSearch( absoluteFieldPath, getIndexNames() );
+			throw log.unknownFieldForSearch( absoluteFieldPath, getIndexesFailureContext() );
 		}
 		return selectedSchemaNode;
 	}
@@ -79,7 +89,9 @@ public class LuceneSearchTargetModel {
 			if ( schemaNode != null ) {
 				found = true;
 				if ( !ObjectFieldStorage.NESTED.equals( schemaNode.getStorage() ) ) {
-					throw log.nonNestedFieldForNestedQuery( indexModel.getIndexName(), absoluteFieldPath );
+					throw log.nonNestedFieldForNestedQuery(
+							absoluteFieldPath, indexModel.getFailureContext()
+					);
 				}
 			}
 		}
@@ -87,10 +99,12 @@ public class LuceneSearchTargetModel {
 			for ( LuceneIndexModel indexModel : indexModels ) {
 				LuceneIndexSchemaFieldNode<?> schemaNode = indexModel.getFieldNode( absoluteFieldPath );
 				if ( schemaNode != null ) {
-					throw log.nonObjectFieldForNestedQuery( indexModel.getIndexName(), absoluteFieldPath );
+					throw log.nonObjectFieldForNestedQuery(
+							absoluteFieldPath, indexModel.getFailureContext()
+					);
 				}
 			}
-			throw log.unknownFieldForSearch( absoluteFieldPath, getIndexNames() );
+			throw log.unknownFieldForSearch( absoluteFieldPath, getIndexesFailureContext() );
 		}
 	}
 }

backend-lucene/src/main/java/org/hibernate/search/v6poc/backend/lucene/search/query/impl/LuceneSearcher.java
@@ -15,6 +15,8 @@ import org.apache.lucene.search.Sort;
 import org.apache.lucene.search.TopDocs;
 import org.hibernate.search.v6poc.backend.lucene.index.spi.ReaderProvider;
 import org.hibernate.search.v6poc.backend.lucene.search.reader.impl.MultiReaderFactory;
+import org.hibernate.search.v6poc.logging.spi.FailureContext;
+import org.hibernate.search.v6poc.logging.spi.FailureContexts;
 import org.hibernate.search.v6poc.search.SearchResult;
 
 /**
@@ -70,6 +72,10 @@ public class LuceneSearcher<T> implements AutoCloseable {
 
 	public Set<String> getIndexNames() {
 		return indexNames;
+	}
+
+	public FailureContext getFailureContext() {
+		return FailureContexts.fromIndexNames( indexNames );
 	}
 
 	@Override

backend-lucene/src/main/java/org/hibernate/search/v6poc/backend/lucene/work/impl/AbstractLuceneWork.java
@@ -6,6 +6,9 @@
  */
 package org.hibernate.search.v6poc.backend.lucene.work.impl;
 
+import org.hibernate.search.v6poc.logging.spi.FailureContext;
+import org.hibernate.search.v6poc.logging.spi.FailureContexts;
+
 /**
  * @author Guillaume Smet
  */
@@ -18,5 +21,9 @@ public abstract class AbstractLuceneWork<T> implements LuceneIndexWork<T> {
 	public AbstractLuceneWork(String workType, String indexName) {
 		this.workType = workType;
 		this.indexName = indexName;
+	}
+
+	protected final FailureContext getFailureContext() {
+		return FailureContexts.fromIndexName( indexName );
 	}
 }

integrationtest/backend-lucene/src/test/java/org/hibernate/search/v6poc/integrationtest/backend/lucene/ExtensionIT.java
@@ -33,6 +33,7 @@ import org.hibernate.search.v6poc.backend.index.spi.IndexSearchTarget;
 import org.hibernate.search.v6poc.backend.lucene.LuceneExtension;
 import org.hibernate.search.v6poc.engine.spi.SessionContext;
 import org.hibernate.search.v6poc.integrationtest.backend.tck.util.rule.SearchSetupHelper;
+import org.hibernate.search.v6poc.logging.spi.FailureContexts;
 import org.hibernate.search.v6poc.search.DocumentReference;
 import org.hibernate.search.v6poc.search.SearchPredicate;
 import org.hibernate.search.v6poc.search.SearchQuery;
@@ -40,6 +41,7 @@ import org.hibernate.search.v6poc.search.SearchSort;
 import org.hibernate.search.v6poc.spatial.GeoPoint;
 import org.hibernate.search.v6poc.spatial.ImmutableGeoPoint;
 import org.hibernate.search.v6poc.util.SearchException;
+import org.hibernate.search.v6poc.util.impl.integrationtest.common.FailureReportUtils;
 import org.hibernate.search.v6poc.util.impl.integrationtest.common.assertion.ProjectionsSearchResultAssert;
 import org.hibernate.search.v6poc.util.impl.integrationtest.common.stub.StubSessionContext;
 import org.hibernate.search.v6poc.util.impl.test.SubTest;
@@ -248,7 +250,10 @@ public class ExtensionIT {
 				)
 				.assertThrown()
 				.isInstanceOf( SearchException.class )
-				.hasMessageContaining( "Field 'nativeField' does not support defining predicates with the DSL: use the Lucene extension and a native query." );
+				.hasMessageContaining( "Native fields do not support defining predicates with the DSL: use the Lucene extension and a native query." )
+				.satisfies( FailureReportUtils.hasContext(
+						FailureContexts.fromIndexFieldAbsolutePath( "nativeField" )
+				) );
 	}
 
 	@Test
@@ -265,7 +270,10 @@ public class ExtensionIT {
 				)
 				.assertThrown()
 				.isInstanceOf( SearchException.class )
-				.hasMessageContaining( "Field 'nativeField' does not support defining sorts with the DSL: use the Lucene extension and a native sort." );
+				.hasMessageContaining( "Native fields do not support defining sorts with the DSL: use the Lucene extension and a native sort." )
+				.satisfies( FailureReportUtils.hasContext(
+						FailureContexts.fromIndexFieldAbsolutePath( "nativeField" )
+				) );
 	}
 
 	@Test
@@ -322,7 +330,10 @@ public class ExtensionIT {
 				} )
 				.assertThrown()
 				.hasCauseInstanceOf( SearchException.class )
-				.hasMessageContaining( "Field 'nativeField_unsupportedProjection' does not support projection." );
+				.hasMessageContaining( "This native field does not support projection." )
+				.satisfies( FailureReportUtils.hasCauseWithContext(
+						FailureContexts.fromIndexFieldAbsolutePath( "nativeField_unsupportedProjection" )
+				) );
 	}
 
 	@Test

integrationtest/backend-tck/src/main/java/org/hibernate/search/v6poc/integrationtest/backend/tck/search/predicate/spatial/SpatialWithinPolygonSearchPredicateIT.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.search.v6poc.backend.index.spi.IndexSearchTarget;
+import org.hibernate.search.v6poc.logging.spi.FailureContexts;
 import org.hibernate.search.v6poc.search.DocumentReference;
 import org.hibernate.search.v6poc.search.SearchQuery;
 import org.hibernate.search.v6poc.spatial.GeoPoint;
@@ -17,6 +18,7 @@ import org.hibernate.search.v6poc.spatial.GeoPolygon;
 import org.hibernate.search.v6poc.spatial.ImmutableGeoPoint;
 import org.hibernate.search.v6poc.spatial.ImmutableGeoPolygon;
 import org.hibernate.search.v6poc.util.SearchException;
+import org.hibernate.search.v6poc.util.impl.integrationtest.common.FailureReportUtils;
 import org.hibernate.search.v6poc.util.impl.integrationtest.common.assertion.DocumentReferencesSearchResultAssert;
 import org.hibernate.search.v6poc.util.impl.test.SubTest;
 
@@ -85,7 +87,9 @@ public class SpatialWithinPolygonSearchPredicateIT extends AbstractSpatialWithin
 				.assertThrown()
 				.isInstanceOf( SearchException.class )
 				.hasMessageContaining( "Spatial predicates are not supported by" )
-				.hasMessageContaining( " of field 'string'" );
+				.satisfies( FailureReportUtils.hasContext(
+						FailureContexts.fromIndexFieldAbsolutePath( "string" )
+				) );
 	}
 
 	@Test

hibernate/hibernate-search

backend/lucene/src/main/java/org/hibernate/search/backend/lucene/document/model/impl/LuceneIndexModel.java
@@ -9,9 +9,12 @@ package org.hibernate.search.backend.lucene.document.model.impl;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.hibernate.search.engine.logging.spi.FailureContext;
+import org.hibernate.search.engine.logging.spi.FailureContexts;
+import org.hibernate.search.util.impl.common.CollectionHelper;
+
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.core.KeywordAnalyzer;
-import org.hibernate.search.util.impl.common.CollectionHelper;
 
 /**
  * @author Guillaume Smet
@@ -62,6 +65,10 @@ public class LuceneIndexModel implements AutoCloseable {
 
 	public String getIndexName() {
 		return indexName;
+	}
+
+	public FailureContext getFailureContext() {
+		return FailureContexts.fromIndexName( indexName );
 	}
 
 	public LuceneIndexSchemaFieldNode<?> getFieldNode(String absoluteFieldPath) {

backend/lucene/src/main/java/org/hibernate/search/backend/lucene/impl/LuceneLocalDirectoryBackend.java
@@ -25,6 +25,8 @@ import org.hibernate.search.backend.lucene.work.impl.LuceneWorkFactory;
 import org.hibernate.search.engine.backend.spi.BackendImplementor;
 import org.hibernate.search.engine.cfg.ConfigurationPropertySource;
 import org.hibernate.search.engine.backend.spi.BackendBuildContext;
+import org.hibernate.search.engine.logging.spi.FailureContext;
+import org.hibernate.search.engine.logging.spi.FailureContexts;
 import org.hibernate.search.util.impl.common.Closer;
 import org.hibernate.search.util.impl.common.LoggerFactory;
 
@@ -42,6 +44,7 @@ public class LuceneLocalDirectoryBackend implements BackendImplementor<LuceneRoo
 	private final LuceneQueryWorkOrchestrator queryOrchestrator;
 	private final MultiTenancyStrategy multiTenancyStrategy;
 
+	private final FailureContext failureContext;
 	private final IndexingBackendContext indexingContext;
 	private final SearchBackendContext searchContext;
 
@@ -53,15 +56,16 @@ public class LuceneLocalDirectoryBackend implements BackendImplementor<LuceneRoo
 		this.queryOrchestrator = new StubLuceneQueryWorkOrchestrator();
 		this.multiTenancyStrategy = multiTenancyStrategy;
 
+		this.failureContext = FailureContexts.fromBackendName( name );
 		this.indexingContext = new IndexingBackendContext(
-				this, new MMapDirectoryProvider( this, rootDirectory ),
+				failureContext, new MMapDirectoryProvider( failureContext, rootDirectory ),
 				workFactory, multiTenancyStrategy
 		);
 		this.searchContext = new SearchBackendContext(
-				this, workFactory, multiTenancyStrategy, queryOrchestrator
+				failureContext, workFactory, multiTenancyStrategy, queryOrchestrator
 		);
 
-		initializeRootDirectory( name, rootDirectory );
+		initializeRootDirectory( rootDirectory );
 	}
 
 	@Override
@@ -69,7 +73,9 @@ public class LuceneLocalDirectoryBackend implements BackendImplementor<LuceneRoo
 		if ( LuceneBackend.class.isAssignableFrom( clazz ) ) {
 			return (T) this;
 		}
-		throw log.backendUnwrappingWithUnknownType( clazz, LuceneBackend.class );
+		throw log.backendUnwrappingWithUnknownType(
+				clazz, LuceneBackend.class, failureContext
+		);
 	}
 
 	@Override
@@ -81,7 +87,7 @@ public class LuceneLocalDirectoryBackend implements BackendImplementor<LuceneRoo
 	public IndexManagerBuilder<LuceneRootDocumentBuilder> createIndexManagerBuilder(
 			String indexName, boolean multiTenancyEnabled, BackendBuildContext context, ConfigurationPropertySource propertySource) {
 		if ( multiTenancyEnabled && !multiTenancyStrategy.isMultiTenancySupported() ) {
-			throw log.multiTenancyRequiredButNotSupportedByBackend( this, indexName );
+			throw log.multiTenancyRequiredButNotSupportedByBackend( indexName, failureContext );
 		}
 
 		/*
@@ -112,10 +118,10 @@ public class LuceneLocalDirectoryBackend implements BackendImplementor<LuceneRoo
 				.toString();
 	}
 
-	private static void initializeRootDirectory(String name, Path rootDirectory) {
+	private void initializeRootDirectory(Path rootDirectory) {
 		if ( Files.exists( rootDirectory ) ) {
 			if ( !Files.isDirectory( rootDirectory ) || !Files.isWritable( rootDirectory ) ) {
-				throw log.localDirectoryBackendRootDirectoryNotWritableDirectory( name, rootDirectory );
+				throw log.localDirectoryBackendRootDirectoryNotWritableDirectory( rootDirectory, failureContext );
 			}
 		}
 		else {
@@ -123,7 +129,7 @@ public class LuceneLocalDirectoryBackend implements BackendImplementor<LuceneRoo
 				Files.createDirectories( rootDirectory );
 			}
 			catch (Exception e) {
-				throw log.unableToCreateRootDirectoryForLocalDirectoryBackend( name, rootDirectory, e );
+				throw log.unableToCreateRootDirectoryForLocalDirectoryBackend( rootDirectory, failureContext, e );
 			}
 		}
 	}

backend/lucene/src/main/java/org/hibernate/search/backend/lucene/search/impl/LuceneSearchTargetModel.java
@@ -16,6 +16,8 @@ import org.hibernate.search.backend.lucene.document.model.impl.LuceneIndexSchema
 import org.hibernate.search.backend.lucene.document.model.impl.LuceneIndexSchemaObjectNode;
 import org.hibernate.search.backend.lucene.index.spi.ReaderProvider;
 import org.hibernate.search.backend.lucene.logging.impl.Log;
+import org.hibernate.search.engine.logging.spi.FailureContext;
+import org.hibernate.search.engine.logging.spi.FailureContexts;
 import org.hibernate.search.util.impl.common.LoggerFactory;
 
 public class LuceneSearchTargetModel {
@@ -36,6 +38,10 @@ public class LuceneSearchTargetModel {
 
 	public Set<String> getIndexNames() {
 		return indexNames;
+	}
+
+	public FailureContext getIndexesFailureContext() {
+		return FailureContexts.fromIndexNames( indexNames );
 	}
 
 	public Set<LuceneIndexModel> getIndexModels() {
@@ -60,13 +66,17 @@ public class LuceneSearchTargetModel {
 				else if ( !selectedSchemaNode.isCompatibleWith( schemaNode ) ) {
 					throw log.conflictingFieldTypesForSearch(
 							absoluteFieldPath,
-							selectedSchemaNode, indexModelForSelectedSchemaNode.getIndexName(),
-							schemaNode, indexModel.getIndexName() );
+							selectedSchemaNode, schemaNode,
+							FailureContexts.fromIndexNames(
+									indexModelForSelectedSchemaNode.getIndexName(),
+									indexModel.getIndexName()
+							)
+					);
 				}
 			}
 		}
 		if ( selectedSchemaNode == null ) {
-			throw log.unknownFieldForSearch( absoluteFieldPath, getIndexNames() );
+			throw log.unknownFieldForSearch( absoluteFieldPath, getIndexesFailureContext() );
 		}
 		return selectedSchemaNode;
 	}
@@ -79,7 +89,9 @@ public class LuceneSearchTargetModel {
 			if ( schemaNode != null ) {
 				found = true;
 				if ( !ObjectFieldStorage.NESTED.equals( schemaNode.getStorage() ) ) {
-					throw log.nonNestedFieldForNestedQuery( indexModel.getIndexName(), absoluteFieldPath );
+					throw log.nonNestedFieldForNestedQuery(
+							absoluteFieldPath, indexModel.getFailureContext()
+					);
 				}
 			}
 		}
@@ -87,10 +99,12 @@ public class LuceneSearchTargetModel {
 			for ( LuceneIndexModel indexModel : indexModels ) {
 				LuceneIndexSchemaFieldNode<?> schemaNode = indexModel.getFieldNode( absoluteFieldPath );
 				if ( schemaNode != null ) {
-					throw log.nonObjectFieldForNestedQuery( indexModel.getIndexName(), absoluteFieldPath );
+					throw log.nonObjectFieldForNestedQuery(
+							absoluteFieldPath, indexModel.getFailureContext()
+					);
 				}
 			}
-			throw log.unknownFieldForSearch( absoluteFieldPath, getIndexNames() );
+			throw log.unknownFieldForSearch( absoluteFieldPath, getIndexesFailureContext() );
 		}
 	}
 }

backend/lucene/src/main/java/org/hibernate/search/backend/lucene/search/query/impl/LuceneSearcher.java
@@ -15,6 +15,8 @@ import org.apache.lucene.search.Sort;
 import org.apache.lucene.search.TopDocs;
 import org.hibernate.search.backend.lucene.index.spi.ReaderProvider;
 import org.hibernate.search.backend.lucene.search.reader.impl.MultiReaderFactory;
+import org.hibernate.search.engine.logging.spi.FailureContext;
+import org.hibernate.search.engine.logging.spi.FailureContexts;
 import org.hibernate.search.engine.search.SearchResult;
 
 /**
@@ -70,6 +72,10 @@ public class LuceneSearcher<T> implements AutoCloseable {
 
 	public Set<String> getIndexNames() {
 		return indexNames;
+	}
+
+	public FailureContext getFailureContext() {
+		return FailureContexts.fromIndexNames( indexNames );
 	}
 
 	@Override

backend/lucene/src/main/java/org/hibernate/search/backend/lucene/work/impl/AbstractLuceneWork.java
@@ -6,6 +6,9 @@
  */
 package org.hibernate.search.backend.lucene.work.impl;
 
+import org.hibernate.search.engine.logging.spi.FailureContext;
+import org.hibernate.search.engine.logging.spi.FailureContexts;
+
 /**
  * @author Guillaume Smet
  */
@@ -18,5 +21,9 @@ public abstract class AbstractLuceneWork<T> implements LuceneIndexWork<T> {
 	public AbstractLuceneWork(String workType, String indexName) {
 		this.workType = workType;
 		this.indexName = indexName;
+	}
+
+	protected final FailureContext getFailureContext() {
+		return FailureContexts.fromIndexName( indexName );
 	}
 }

integrationtest/backend/lucene/src/test/java/org/hibernate/search/integrationtest/backend/lucene/ExtensionIT.java
@@ -33,6 +33,7 @@ import org.hibernate.search.engine.backend.index.spi.IndexSearchTarget;
 import org.hibernate.search.backend.lucene.LuceneExtension;
 import org.hibernate.search.engine.common.spi.SessionContext;
 import org.hibernate.search.integrationtest.backend.tck.util.rule.SearchSetupHelper;
+import org.hibernate.search.engine.logging.spi.FailureContexts;
 import org.hibernate.search.engine.search.DocumentReference;
 import org.hibernate.search.engine.search.SearchPredicate;
 import org.hibernate.search.engine.search.SearchQuery;
@@ -40,6 +41,7 @@ import org.hibernate.search.engine.search.SearchSort;
 import org.hibernate.search.engine.spatial.GeoPoint;
 import org.hibernate.search.engine.spatial.ImmutableGeoPoint;
 import org.hibernate.search.util.SearchException;
+import org.hibernate.search.util.impl.integrationtest.common.FailureReportUtils;
 import org.hibernate.search.util.impl.integrationtest.common.assertion.ProjectionsSearchResultAssert;
 import org.hibernate.search.util.impl.integrationtest.common.stub.StubSessionContext;
 import org.hibernate.search.util.impl.test.SubTest;
@@ -248,7 +250,10 @@ public class ExtensionIT {
 				)
 				.assertThrown()
 				.isInstanceOf( SearchException.class )
-				.hasMessageContaining( "Field 'nativeField' does not support defining predicates with the DSL: use the Lucene extension and a native query." );
+				.hasMessageContaining( "Native fields do not support defining predicates with the DSL: use the Lucene extension and a native query." )
+				.satisfies( FailureReportUtils.hasContext(
+						FailureContexts.fromIndexFieldAbsolutePath( "nativeField" )
+				) );
 	}
 
 	@Test
@@ -265,7 +270,10 @@ public class ExtensionIT {
 				)
 				.assertThrown()
 				.isInstanceOf( SearchException.class )
-				.hasMessageContaining( "Field 'nativeField' does not support defining sorts with the DSL: use the Lucene extension and a native sort." );
+				.hasMessageContaining( "Native fields do not support defining sorts with the DSL: use the Lucene extension and a native sort." )
+				.satisfies( FailureReportUtils.hasContext(
+						FailureContexts.fromIndexFieldAbsolutePath( "nativeField" )
+				) );
 	}
 
 	@Test
@@ -322,7 +330,10 @@ public class ExtensionIT {
 				} )
 				.assertThrown()
 				.hasCauseInstanceOf( SearchException.class )
-				.hasMessageContaining( "Field 'nativeField_unsupportedProjection' does not support projection." );
+				.hasMessageContaining( "This native field does not support projection." )
+				.satisfies( FailureReportUtils.hasCauseWithContext(
+						FailureContexts.fromIndexFieldAbsolutePath( "nativeField_unsupportedProjection" )
+				) );
 	}
 
 	@Test

integrationtest/backend/tck/src/main/java/org/hibernate/search/integrationtest/backend/tck/search/predicate/spatial/SpatialWithinPolygonSearchPredicateIT.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.search.engine.backend.index.spi.IndexSearchTarget;
+import org.hibernate.search.engine.logging.spi.FailureContexts;
 import org.hibernate.search.engine.search.DocumentReference;
 import org.hibernate.search.engine.search.SearchQuery;
 import org.hibernate.search.engine.spatial.GeoPoint;
@@ -17,6 +18,7 @@ import org.hibernate.search.engine.spatial.GeoPolygon;
 import org.hibernate.search.engine.spatial.ImmutableGeoPoint;
 import org.hibernate.search.engine.spatial.ImmutableGeoPolygon;
 import org.hibernate.search.util.SearchException;
+import org.hibernate.search.util.impl.integrationtest.common.FailureReportUtils;
 import org.hibernate.search.util.impl.integrationtest.common.assertion.DocumentReferencesSearchResultAssert;
 import org.hibernate.search.util.impl.test.SubTest;
 
@@ -85,7 +87,9 @@ public class SpatialWithinPolygonSearchPredicateIT extends AbstractSpatialWithin
 				.assertThrown()
 				.isInstanceOf( SearchException.class )
 				.hasMessageContaining( "Spatial predicates are not supported by" )
-				.hasMessageContaining( " of field 'string'" );
+				.satisfies( FailureReportUtils.hasContext(
+						FailureContexts.fromIndexFieldAbsolutePath( "string" )
+				) );
 	}
 
 	@Test