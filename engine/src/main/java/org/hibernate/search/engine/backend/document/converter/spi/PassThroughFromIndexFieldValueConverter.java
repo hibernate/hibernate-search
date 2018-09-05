yrodiere/hibernate-search-6-poc

HSEARCH-3223 Restore support for applying bridges automatically to projections

engine/src/main/java/org/hibernate/search/v6poc/backend/document/converter/spi/PassThroughFromIndexFieldValueConverter.java
@@ -1,0 +1,29 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.v6poc.backend.document.converter.spi;
+
+import org.hibernate.search.v6poc.backend.document.converter.FromIndexFieldValueConverter;
+
+public final class PassThroughFromIndexFieldValueConverter<F> implements FromIndexFieldValueConverter<F, F> {
+
+	private static final PassThroughFromIndexFieldValueConverter<Object> INSTANCE = new PassThroughFromIndexFieldValueConverter<>();
+
+	@SuppressWarnings("unchecked") // INSTANCE works for any F
+	public static <F> PassThroughFromIndexFieldValueConverter<F> get() {
+		return (PassThroughFromIndexFieldValueConverter<F>) INSTANCE;
+	}
+
+	private PassThroughFromIndexFieldValueConverter() {
+		// private, use get() instead
+	}
+
+	@Override
+	public F convert(F value) {
+		return value;
+	}
+
+}

engine/src/main/java/org/hibernate/search/v6poc/backend/document/spi/UserIndexFieldConverter.java
@@ -6,6 +6,7 @@
  */
 package org.hibernate.search.v6poc.backend.document.spi;
 
+import org.hibernate.search.v6poc.backend.document.converter.FromIndexFieldValueConverter;
 import org.hibernate.search.v6poc.backend.document.converter.ToIndexFieldValueConverter;
 
 /**
@@ -22,19 +23,33 @@ import org.hibernate.search.v6poc.backend.document.converter.ToIndexFieldValueCo
 public final class UserIndexFieldConverter<F> {
 
 	private final ToIndexFieldValueConverter<?, ? extends F> toIndexConverter;
+	private final FromIndexFieldValueConverter<? super F, ?> fromIndexConverter;
 
-	UserIndexFieldConverter(ToIndexFieldValueConverter<?, ? extends F> toIndexConverter) {
+	UserIndexFieldConverter(ToIndexFieldValueConverter<?,? extends F> toIndexConverter,
+			FromIndexFieldValueConverter<? super F,?> fromIndexConverter) {
 		this.toIndexConverter = toIndexConverter;
+		this.fromIndexConverter = fromIndexConverter;
 	}
 
 	public F convertFromDsl(Object value) {
 		return toIndexConverter.convertUnknown( value );
 	}
 
+	public Object convertFromProjection(F projection) {
+		if ( fromIndexConverter == null ) {
+			// FIXME detect this when the projection is configured and throw an exception with an explicit message instead. A converter set to null means we don't want to enable projections.
+			return projection;
+		}
+		return fromIndexConverter.convert( projection );
+	}
+
 	/**
 	 * Determine whether another converter is DSL-compatible with this one,
 	 * i.e. its {@link #convertFromDsl(Object)} method is guaranteed
 	 * to always return the same value as this converter's when given the same input.
+	 * <p>
+	 * Note: this method is separate from {@link #equals(Object)} because it might return true for two different objects,
+	 * e.g. two objects that implement {@link #convertFromProjection(Object)} differently.
 	 *
 	 * @param other Another {@link UserIndexFieldConverter}.
 	 * @return {@code true} if the given converter is DSL-compatible.

hibernate/hibernate-search

engine/src/main/java/org/hibernate/search/engine/backend/document/converter/spi/PassThroughFromIndexFieldValueConverter.java
@@ -1,0 +1,29 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.engine.backend.document.converter.spi;
+
+import org.hibernate.search.engine.backend.document.converter.FromIndexFieldValueConverter;
+
+public final class PassThroughFromIndexFieldValueConverter<F> implements FromIndexFieldValueConverter<F, F> {
+
+	private static final PassThroughFromIndexFieldValueConverter<Object> INSTANCE = new PassThroughFromIndexFieldValueConverter<>();
+
+	@SuppressWarnings("unchecked") // INSTANCE works for any F
+	public static <F> PassThroughFromIndexFieldValueConverter<F> get() {
+		return (PassThroughFromIndexFieldValueConverter<F>) INSTANCE;
+	}
+
+	private PassThroughFromIndexFieldValueConverter() {
+		// private, use get() instead
+	}
+
+	@Override
+	public F convert(F value) {
+		return value;
+	}
+
+}

engine/src/main/java/org/hibernate/search/engine/backend/document/spi/UserIndexFieldConverter.java
@@ -6,6 +6,7 @@
  */
 package org.hibernate.search.engine.backend.document.spi;
 
+import org.hibernate.search.engine.backend.document.converter.FromIndexFieldValueConverter;
 import org.hibernate.search.engine.backend.document.converter.ToIndexFieldValueConverter;
 
 /**
@@ -22,15 +23,19 @@ import org.hibernate.search.engine.backend.document.converter.ToIndexFieldValueC
 public final class UserIndexFieldConverter<F> {
 
 	private final ToIndexFieldValueConverter<?, ? extends F> dslToIndexConverter;
+	private final FromIndexFieldValueConverter<? super F, ?> projectionFromIndexConverter;
 
-	UserIndexFieldConverter(ToIndexFieldValueConverter<?, ? extends F> dslToIndexConverter) {
+	UserIndexFieldConverter(ToIndexFieldValueConverter<?,? extends F> dslToIndexConverter,
+			FromIndexFieldValueConverter<? super F,?> projectionFromIndexConverter) {
 		this.dslToIndexConverter = dslToIndexConverter;
+		this.projectionFromIndexConverter = projectionFromIndexConverter;
 	}
 
 	@Override
 	public String toString() {
 		return getClass().getName() + "["
 				+ "dslToIndexConverter=" + dslToIndexConverter
+				+ ", projectionFromIndexConverter=" + projectionFromIndexConverter
 				+ "]";
 	}
 
@@ -38,10 +43,21 @@ public final class UserIndexFieldConverter<F> {
 		return dslToIndexConverter.convertUnknown( value );
 	}
 
+	public Object convertFromProjection(F projection) {
+		if ( projectionFromIndexConverter == null ) {
+			// FIXME detect this when the projection is configured and throw an exception with an explicit message instead. A converter set to null means we don't want to enable projections.
+			return projection;
+		}
+		return projectionFromIndexConverter.convert( projection );
+	}
+
 	/**
 	 * Determine whether another converter is DSL-compatible with this one,
 	 * i.e. its {@link #convertFromDsl(Object)} method is guaranteed
 	 * to always return the same value as this converter's when given the same input.
+	 * <p>
+	 * Note: this method is separate from {@link #equals(Object)} because it might return true for two different objects,
+	 * e.g. two objects that implement {@link #convertFromProjection(Object)} differently.
 	 *
 	 * @param other Another {@link UserIndexFieldConverter}.
 	 * @return {@code true} if the given converter is DSL-compatible.