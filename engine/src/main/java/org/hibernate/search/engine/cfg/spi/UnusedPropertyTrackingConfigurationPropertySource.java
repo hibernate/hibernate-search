hibernate/hibernate-search-6-poc

HSEARCH-1869 Validate for each hibernate.search prefixed configuration property to be consumed

engine/src/main/java/org/hibernate/search/v6poc/cfg/spi/UnusedPropertyTrackingConfigurationPropertySource.java
@@ -1,0 +1,38 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.v6poc.cfg.spi;
+
+import java.util.Collections;
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.hibernate.search.v6poc.cfg.ConfigurationPropertySource;
+
+public final class UnusedPropertyTrackingConfigurationPropertySource implements ConfigurationPropertySource {
+
+	private final ConfigurationPropertySource delegate;
+
+	private final Set<String> unusedPropertyKeys;
+
+	public UnusedPropertyTrackingConfigurationPropertySource(
+			ConfigurationPropertySource delegate, Set<String> availablePropertyKeys) {
+		this.delegate = delegate;
+		this.unusedPropertyKeys = ConcurrentHashMap.newKeySet();
+		unusedPropertyKeys.addAll( availablePropertyKeys );
+	}
+
+	@Override
+	public Optional<?> get(String key) {
+		unusedPropertyKeys.remove( key );
+		return delegate.get( key );
+	}
+
+	public Set<String> getUnusedPropertyKeys() {
+		return Collections.unmodifiableSet( unusedPropertyKeys );
+	}
+}

orm/src/main/java/org/hibernate/search/v6poc/entity/orm/bootstrap/impl/HibernateSearchIntegrator.java
@@ -18,8 +18,9 @@ import org.hibernate.event.service.spi.DuplicationStrategy;
 import org.hibernate.event.service.spi.EventListenerRegistry;
 import org.hibernate.event.spi.EventType;
 import org.hibernate.integrator.spi.Integrator;
-import org.hibernate.search.v6poc.cfg.spi.ConfigurationProperty;
 import org.hibernate.search.v6poc.cfg.ConfigurationPropertySource;
+import org.hibernate.search.v6poc.cfg.spi.ConfigurationProperty;
+import org.hibernate.search.v6poc.cfg.spi.UnusedPropertyTrackingConfigurationPropertySource;
 import org.hibernate.search.v6poc.entity.orm.cfg.IndexingStrategyConfiguration;
 import org.hibernate.search.v6poc.entity.orm.cfg.SearchOrmSettings;
 import org.hibernate.search.v6poc.entity.orm.cfg.impl.HibernateOrmConfigurationPropertySource;
@@ -60,11 +61,38 @@ public class HibernateSearchIntegrator implements Integrator {
 					.withDefault( SearchOrmSettings.Defaults.ENABLE_DIRTY_CHECK )
 					.build();
 
+	private static final ConfigurationProperty<Boolean> ENABLE_CONFIGURATION_PROPERTY_TRACKING =
+			// We don't use the radical here, but the full property key: the property is retrieved before we apply the mask
+			ConfigurationProperty.forKey( SearchOrmSettings.ENABLE_CONFIGURATION_PROPERTY_TRACKING )
+					.asBoolean()
+					.withDefault( SearchOrmSettings.Defaults.ENABLE_CONFIGURATION_PROPERTY_TRACKING )
+					.build();
+
 	@Override
 	public void integrate(Metadata metadata, SessionFactoryImplementor sessionFactory, SessionFactoryServiceRegistry serviceRegistry) {
 		ConfigurationService configurationService = serviceRegistry.getService( ConfigurationService.class );
-		ConfigurationPropertySource propertySource = new HibernateOrmConfigurationPropertySource( configurationService )
-				.withMask( "hibernate.search" );
+		HibernateOrmConfigurationPropertySource hibernateOrmPropertySource =
+				new HibernateOrmConfigurationPropertySource( configurationService );
+		UnusedPropertyTrackingConfigurationPropertySource unusedPropertyTrackingPropertySource;
+		ConfigurationPropertySource unmaskedPropertySource;
+		if ( ENABLE_CONFIGURATION_PROPERTY_TRACKING.get( hibernateOrmPropertySource ) ) {
+			unusedPropertyTrackingPropertySource = hibernateOrmPropertySource.withUnusedPropertyTracking();
+			// Make sure to mark the "enable configuration property tracking" property as used
+			ENABLE_CONFIGURATION_PROPERTY_TRACKING.get( unusedPropertyTrackingPropertySource );
+			unmaskedPropertySource = unusedPropertyTrackingPropertySource;
+		}
+		else {
+			log.configurationPropertyTrackingDisabled();
+			unusedPropertyTrackingPropertySource = null;
+			unmaskedPropertySource = hibernateOrmPropertySource;
+		}
+
+		/*
+		 * Only apply the mask after we added support for unused property tracking,
+		 * because that tracking must work on the user's keys without a mask,
+		 * so as to report unused keys exactly as they were provided by the user.
+		 */
+		ConfigurationPropertySource propertySource = unmaskedPropertySource.withMask( "hibernate.search" );
 		JndiService namingService = serviceRegistry.getService( JndiService.class );
 
 		if ( ! AUTOREGISTER_LISTENERS.get( propertySource ) ) {
@@ -83,7 +111,7 @@ public class HibernateSearchIntegrator implements Integrator {
 		ServiceBinding<BeanResolver> hibernateOrmBeanResolverBinding = serviceRegistry.locateServiceBinding( BeanResolver.class );
 		HibernateSearchSessionFactoryObserver observer = new HibernateSearchSessionFactoryObserver(
 				metadata,
-				propertySource,
+				propertySource, unusedPropertyTrackingPropertySource,
 				fullTextIndexEventListener,
 				hibernateOrmClassLoaderService,
 				environmentSynchronizerBinding == null ? null : serviceRegistry.getService( EnvironmentSynchronizer.class ),

orm/src/main/java/org/hibernate/search/v6poc/entity/orm/bootstrap/impl/HibernateSearchSessionFactoryObserver.java
@@ -6,8 +6,10 @@
  */
 package org.hibernate.search.v6poc.entity.orm.bootstrap.impl;
 
+import java.lang.invoke.MethodHandles;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 import org.hibernate.SessionFactory;
@@ -19,18 +21,21 @@ import org.hibernate.engine.spi.SessionFactoryImplementor;
 import org.hibernate.mapping.PersistentClass;
 import org.hibernate.search.v6poc.cfg.ConfigurationPropertySource;
 import org.hibernate.search.v6poc.cfg.spi.ConfigurationProperty;
+import org.hibernate.search.v6poc.cfg.spi.UnusedPropertyTrackingConfigurationPropertySource;
 import org.hibernate.search.v6poc.engine.SearchMappingRepository;
 import org.hibernate.search.v6poc.engine.SearchMappingRepositoryBuilder;
 import org.hibernate.search.v6poc.engine.spi.ReflectionBeanResolver;
 import org.hibernate.search.v6poc.entity.orm.cfg.SearchOrmSettings;
 import org.hibernate.search.v6poc.entity.orm.event.impl.FullTextIndexEventListener;
 import org.hibernate.search.v6poc.entity.orm.impl.HibernateSearchContextService;
+import org.hibernate.search.v6poc.entity.orm.logging.impl.Log;
 import org.hibernate.search.v6poc.entity.orm.mapping.HibernateOrmMapping;
 import org.hibernate.search.v6poc.entity.orm.mapping.HibernateOrmMappingContributor;
 import org.hibernate.search.v6poc.entity.orm.mapping.HibernateOrmSearchMappingContributor;
 import org.hibernate.search.v6poc.entity.orm.spi.BeanResolver;
 import org.hibernate.search.v6poc.entity.orm.spi.EnvironmentSynchronizer;
 import org.hibernate.search.v6poc.entity.pojo.mapping.definition.annotation.AnnotationMappingDefinition;
+import org.hibernate.search.v6poc.util.impl.common.LoggerFactory;
 
 /**
  * A {@code SessionFactoryObserver} registered with Hibernate ORM during the integration phase.
@@ -41,6 +46,8 @@ import org.hibernate.search.v6poc.entity.pojo.mapping.definition.annotation.Anno
  */
 public class HibernateSearchSessionFactoryObserver implements SessionFactoryObserver {
 
+	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
+
 	private static final ConfigurationProperty<Boolean> ENABLE_ANNOTATION_MAPPING =
 			ConfigurationProperty.forKey( SearchOrmSettings.Radicals.ENABLE_ANNOTATION_MAPPING )
 					.asBoolean()
@@ -48,6 +55,7 @@ public class HibernateSearchSessionFactoryObserver implements SessionFactoryObse
 					.build();
 
 	private final ConfigurationPropertySource propertySource;
+	private final UnusedPropertyTrackingConfigurationPropertySource unusedPropertyTrackingPropertySource;
 	private final JndiService namingService;
 	private final ClassLoaderService classLoaderService;
 	private final EnvironmentSynchronizer environmentSynchronizer;
@@ -64,6 +72,7 @@ public class HibernateSearchSessionFactoryObserver implements SessionFactoryObse
 	HibernateSearchSessionFactoryObserver(
 			Metadata metadata,
 			ConfigurationPropertySource propertySource,
+			UnusedPropertyTrackingConfigurationPropertySource unusedPropertyTrackingPropertySource,
 			FullTextIndexEventListener listener,
 			ClassLoaderService classLoaderService,
 			EnvironmentSynchronizer environmentSynchronizer,
@@ -71,6 +80,7 @@ public class HibernateSearchSessionFactoryObserver implements SessionFactoryObse
 			JndiService namingService) {
 		this.metadata = metadata;
 		this.propertySource = propertySource;
+		this.unusedPropertyTrackingPropertySource = unusedPropertyTrackingPropertySource;
 		this.listener = listener;
 		this.classLoaderService = classLoaderService;
 		this.environmentSynchronizer = environmentSynchronizer;
@@ -169,6 +179,13 @@ public class HibernateSearchSessionFactoryObserver implements SessionFactoryObse
 					sessionFactoryImplementor.getServiceRegistry().getService( HibernateSearchContextService.class );
 			contextService.initialize( mappingRepository, mapping );
 			contextFuture.complete( contextService );
+
+			if ( unusedPropertyTrackingPropertySource != null ) {
+				Set<String> unusedPropertyKeys = unusedPropertyTrackingPropertySource.getUnusedPropertyKeys();
+				if ( !unusedPropertyKeys.isEmpty() ) {
+					log.configurationPropertyTrackingUnusedProperties( unusedPropertyKeys );
+				}
+			}
 
 			failedBoot = false;
 		}

orm/src/main/java/org/hibernate/search/v6poc/entity/orm/cfg/impl/HibernateOrmConfigurationPropertySource.java
@@ -6,10 +6,14 @@
  */
 package org.hibernate.search.v6poc.entity.orm.cfg.impl;
 
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 import org.hibernate.engine.config.spi.ConfigurationService;
 import org.hibernate.search.v6poc.cfg.ConfigurationPropertySource;
+import org.hibernate.search.v6poc.cfg.spi.UnusedPropertyTrackingConfigurationPropertySource;
+import org.hibernate.search.v6poc.entity.orm.cfg.SearchOrmSettings;
 
 public class HibernateOrmConfigurationPropertySource implements ConfigurationPropertySource {
 
@@ -24,5 +28,23 @@ public class HibernateOrmConfigurationPropertySource implements ConfigurationPro
 	@Override
 	public Optional<?> get(String key) {
 		return Optional.ofNullable( configurationService.getSetting( key, OBJECT_CONVERTER ) );
+	}
+
+	public UnusedPropertyTrackingConfigurationPropertySource withUnusedPropertyTracking() {
+		Set<String> availablePropertyKeys = extractHibernateSearchPropertyKeys( configurationService );
+		return new UnusedPropertyTrackingConfigurationPropertySource( this, availablePropertyKeys );
+	}
+
+	private Set<String> extractHibernateSearchPropertyKeys(ConfigurationService configurationService) {
+		Set<String> hibernateSearchPropertyKeys = new HashSet<>();
+		for ( Object key : configurationService.getSettings().keySet() ) {
+			if ( key instanceof String ) {
+				String stringKey = (String) key;
+				if ( stringKey.startsWith( SearchOrmSettings.PREFIX ) ) {
+					hibernateSearchPropertyKeys.add( stringKey );
+				}
+			}
+		}
+		return hibernateSearchPropertyKeys;
 	}
 }

hibernate/hibernate-search

engine/src/main/java/org/hibernate/search/engine/cfg/spi/UnusedPropertyTrackingConfigurationPropertySource.java
@@ -1,0 +1,38 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.engine.cfg.spi;
+
+import java.util.Collections;
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.hibernate.search.engine.cfg.ConfigurationPropertySource;
+
+public final class UnusedPropertyTrackingConfigurationPropertySource implements ConfigurationPropertySource {
+
+	private final ConfigurationPropertySource delegate;
+
+	private final Set<String> unusedPropertyKeys;
+
+	public UnusedPropertyTrackingConfigurationPropertySource(
+			ConfigurationPropertySource delegate, Set<String> availablePropertyKeys) {
+		this.delegate = delegate;
+		this.unusedPropertyKeys = ConcurrentHashMap.newKeySet();
+		unusedPropertyKeys.addAll( availablePropertyKeys );
+	}
+
+	@Override
+	public Optional<?> get(String key) {
+		unusedPropertyKeys.remove( key );
+		return delegate.get( key );
+	}
+
+	public Set<String> getUnusedPropertyKeys() {
+		return Collections.unmodifiableSet( unusedPropertyKeys );
+	}
+}

mapper/orm/src/main/java/org/hibernate/search/mapper/orm/bootstrap/impl/HibernateSearchIntegrator.java
@@ -18,8 +18,9 @@ import org.hibernate.event.service.spi.DuplicationStrategy;
 import org.hibernate.event.service.spi.EventListenerRegistry;
 import org.hibernate.event.spi.EventType;
 import org.hibernate.integrator.spi.Integrator;
-import org.hibernate.search.engine.cfg.spi.ConfigurationProperty;
 import org.hibernate.search.engine.cfg.ConfigurationPropertySource;
+import org.hibernate.search.engine.cfg.spi.ConfigurationProperty;
+import org.hibernate.search.engine.cfg.spi.UnusedPropertyTrackingConfigurationPropertySource;
 import org.hibernate.search.mapper.orm.cfg.IndexingStrategyConfiguration;
 import org.hibernate.search.mapper.orm.cfg.SearchOrmSettings;
 import org.hibernate.search.mapper.orm.cfg.impl.HibernateOrmConfigurationPropertySource;
@@ -60,11 +61,38 @@ public class HibernateSearchIntegrator implements Integrator {
 					.withDefault( SearchOrmSettings.Defaults.ENABLE_DIRTY_CHECK )
 					.build();
 
+	private static final ConfigurationProperty<Boolean> ENABLE_CONFIGURATION_PROPERTY_TRACKING =
+			// We don't use the radical here, but the full property key: the property is retrieved before we apply the mask
+			ConfigurationProperty.forKey( SearchOrmSettings.ENABLE_CONFIGURATION_PROPERTY_TRACKING )
+					.asBoolean()
+					.withDefault( SearchOrmSettings.Defaults.ENABLE_CONFIGURATION_PROPERTY_TRACKING )
+					.build();
+
 	@Override
 	public void integrate(Metadata metadata, SessionFactoryImplementor sessionFactory, SessionFactoryServiceRegistry serviceRegistry) {
 		ConfigurationService configurationService = serviceRegistry.getService( ConfigurationService.class );
-		ConfigurationPropertySource propertySource = new HibernateOrmConfigurationPropertySource( configurationService )
-				.withMask( "hibernate.search" );
+		HibernateOrmConfigurationPropertySource hibernateOrmPropertySource =
+				new HibernateOrmConfigurationPropertySource( configurationService );
+		UnusedPropertyTrackingConfigurationPropertySource unusedPropertyTrackingPropertySource;
+		ConfigurationPropertySource unmaskedPropertySource;
+		if ( ENABLE_CONFIGURATION_PROPERTY_TRACKING.get( hibernateOrmPropertySource ) ) {
+			unusedPropertyTrackingPropertySource = hibernateOrmPropertySource.withUnusedPropertyTracking();
+			// Make sure to mark the "enable configuration property tracking" property as used
+			ENABLE_CONFIGURATION_PROPERTY_TRACKING.get( unusedPropertyTrackingPropertySource );
+			unmaskedPropertySource = unusedPropertyTrackingPropertySource;
+		}
+		else {
+			log.configurationPropertyTrackingDisabled();
+			unusedPropertyTrackingPropertySource = null;
+			unmaskedPropertySource = hibernateOrmPropertySource;
+		}
+
+		/*
+		 * Only apply the mask after we added support for unused property tracking,
+		 * because that tracking must work on the user's keys without a mask,
+		 * so as to report unused keys exactly as they were provided by the user.
+		 */
+		ConfigurationPropertySource propertySource = unmaskedPropertySource.withMask( "hibernate.search" );
 		JndiService namingService = serviceRegistry.getService( JndiService.class );
 
 		if ( ! AUTOREGISTER_LISTENERS.get( propertySource ) ) {
@@ -83,7 +111,7 @@ public class HibernateSearchIntegrator implements Integrator {
 		ServiceBinding<BeanResolver> hibernateOrmBeanResolverBinding = serviceRegistry.locateServiceBinding( BeanResolver.class );
 		HibernateSearchSessionFactoryObserver observer = new HibernateSearchSessionFactoryObserver(
 				metadata,
-				propertySource,
+				propertySource, unusedPropertyTrackingPropertySource,
 				fullTextIndexEventListener,
 				hibernateOrmClassLoaderService,
 				environmentSynchronizerBinding == null ? null : serviceRegistry.getService( EnvironmentSynchronizer.class ),

mapper/orm/src/main/java/org/hibernate/search/mapper/orm/bootstrap/impl/HibernateSearchSessionFactoryObserver.java
@@ -6,8 +6,10 @@
  */
 package org.hibernate.search.mapper.orm.bootstrap.impl;
 
+import java.lang.invoke.MethodHandles;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 import org.hibernate.SessionFactory;
@@ -19,18 +21,21 @@ import org.hibernate.engine.spi.SessionFactoryImplementor;
 import org.hibernate.mapping.PersistentClass;
 import org.hibernate.search.engine.cfg.ConfigurationPropertySource;
 import org.hibernate.search.engine.cfg.spi.ConfigurationProperty;
+import org.hibernate.search.engine.cfg.spi.UnusedPropertyTrackingConfigurationPropertySource;
 import org.hibernate.search.engine.common.SearchMappingRepository;
 import org.hibernate.search.engine.common.SearchMappingRepositoryBuilder;
 import org.hibernate.search.engine.common.spi.ReflectionBeanResolver;
 import org.hibernate.search.mapper.orm.cfg.SearchOrmSettings;
 import org.hibernate.search.mapper.orm.event.impl.FullTextIndexEventListener;
 import org.hibernate.search.mapper.orm.impl.HibernateSearchContextService;
+import org.hibernate.search.mapper.orm.logging.impl.Log;
 import org.hibernate.search.mapper.orm.mapping.HibernateOrmMapping;
 import org.hibernate.search.mapper.orm.mapping.HibernateOrmMappingContributor;
 import org.hibernate.search.mapper.orm.mapping.HibernateOrmSearchMappingContributor;
 import org.hibernate.search.mapper.orm.spi.BeanResolver;
 import org.hibernate.search.mapper.orm.spi.EnvironmentSynchronizer;
 import org.hibernate.search.mapper.pojo.mapping.definition.annotation.AnnotationMappingDefinition;
+import org.hibernate.search.util.impl.common.LoggerFactory;
 
 /**
  * A {@code SessionFactoryObserver} registered with Hibernate ORM during the integration phase.
@@ -41,6 +46,8 @@ import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Annotation
  */
 public class HibernateSearchSessionFactoryObserver implements SessionFactoryObserver {
 
+	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
+
 	private static final ConfigurationProperty<Boolean> ENABLE_ANNOTATION_MAPPING =
 			ConfigurationProperty.forKey( SearchOrmSettings.Radicals.ENABLE_ANNOTATION_MAPPING )
 					.asBoolean()
@@ -48,6 +55,7 @@ public class HibernateSearchSessionFactoryObserver implements SessionFactoryObse
 					.build();
 
 	private final ConfigurationPropertySource propertySource;
+	private final UnusedPropertyTrackingConfigurationPropertySource unusedPropertyTrackingPropertySource;
 	private final JndiService namingService;
 	private final ClassLoaderService classLoaderService;
 	private final EnvironmentSynchronizer environmentSynchronizer;
@@ -64,6 +72,7 @@ public class HibernateSearchSessionFactoryObserver implements SessionFactoryObse
 	HibernateSearchSessionFactoryObserver(
 			Metadata metadata,
 			ConfigurationPropertySource propertySource,
+			UnusedPropertyTrackingConfigurationPropertySource unusedPropertyTrackingPropertySource,
 			FullTextIndexEventListener listener,
 			ClassLoaderService classLoaderService,
 			EnvironmentSynchronizer environmentSynchronizer,
@@ -71,6 +80,7 @@ public class HibernateSearchSessionFactoryObserver implements SessionFactoryObse
 			JndiService namingService) {
 		this.metadata = metadata;
 		this.propertySource = propertySource;
+		this.unusedPropertyTrackingPropertySource = unusedPropertyTrackingPropertySource;
 		this.listener = listener;
 		this.classLoaderService = classLoaderService;
 		this.environmentSynchronizer = environmentSynchronizer;
@@ -169,6 +179,13 @@ public class HibernateSearchSessionFactoryObserver implements SessionFactoryObse
 					sessionFactoryImplementor.getServiceRegistry().getService( HibernateSearchContextService.class );
 			contextService.initialize( mappingRepository, mapping );
 			contextFuture.complete( contextService );
+
+			if ( unusedPropertyTrackingPropertySource != null ) {
+				Set<String> unusedPropertyKeys = unusedPropertyTrackingPropertySource.getUnusedPropertyKeys();
+				if ( !unusedPropertyKeys.isEmpty() ) {
+					log.configurationPropertyTrackingUnusedProperties( unusedPropertyKeys );
+				}
+			}
 
 			failedBoot = false;
 		}

mapper/orm/src/main/java/org/hibernate/search/mapper/orm/cfg/impl/HibernateOrmConfigurationPropertySource.java
@@ -6,10 +6,14 @@
  */
 package org.hibernate.search.mapper.orm.cfg.impl;
 
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 import org.hibernate.engine.config.spi.ConfigurationService;
 import org.hibernate.search.engine.cfg.ConfigurationPropertySource;
+import org.hibernate.search.engine.cfg.spi.UnusedPropertyTrackingConfigurationPropertySource;
+import org.hibernate.search.mapper.orm.cfg.SearchOrmSettings;
 
 public class HibernateOrmConfigurationPropertySource implements ConfigurationPropertySource {
 
@@ -24,5 +28,23 @@ public class HibernateOrmConfigurationPropertySource implements ConfigurationPro
 	@Override
 	public Optional<?> get(String key) {
 		return Optional.ofNullable( configurationService.getSetting( key, OBJECT_CONVERTER ) );
+	}
+
+	public UnusedPropertyTrackingConfigurationPropertySource withUnusedPropertyTracking() {
+		Set<String> availablePropertyKeys = extractHibernateSearchPropertyKeys( configurationService );
+		return new UnusedPropertyTrackingConfigurationPropertySource( this, availablePropertyKeys );
+	}
+
+	private Set<String> extractHibernateSearchPropertyKeys(ConfigurationService configurationService) {
+		Set<String> hibernateSearchPropertyKeys = new HashSet<>();
+		for ( Object key : configurationService.getSettings().keySet() ) {
+			if ( key instanceof String ) {
+				String stringKey = (String) key;
+				if ( stringKey.startsWith( SearchOrmSettings.PREFIX ) ) {
+					hibernateSearchPropertyKeys.add( stringKey );
+				}
+			}
+		}
+		return hibernateSearchPropertyKeys;
 	}
 }