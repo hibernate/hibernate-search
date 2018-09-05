hibernate/hibernate-search-6-poc

HSEARCH-3087 Push the distance units to the backends

backend-elasticsearch/src/main/java/org/hibernate/search/v6poc/backend/elasticsearch/types/predicate/impl/GeoPointSpatialWithinCirclePredicateBuilder.java
@@ -12,6 +12,7 @@ import org.hibernate.search.v6poc.backend.elasticsearch.search.predicate.impl.Ab
 import org.hibernate.search.v6poc.backend.elasticsearch.search.predicate.impl.ElasticsearchSearchPredicateCollector;
 import org.hibernate.search.v6poc.backend.elasticsearch.types.codec.impl.ElasticsearchFieldCodec;
 import org.hibernate.search.v6poc.search.predicate.spi.SpatialWithinCirclePredicateBuilder;
+import org.hibernate.search.v6poc.spatial.DistanceUnit;
 import org.hibernate.search.v6poc.spatial.GeoPoint;
 
 import com.google.gson.JsonObject;
@@ -33,8 +34,8 @@ class GeoPointSpatialWithinCirclePredicateBuilder extends AbstractSearchPredicat
 	}
 
 	@Override
-	public void circle(GeoPoint center, double radiusInMeters) {
-		DISTANCE.set( getInnerObject(), radiusInMeters );
+	public void circle(GeoPoint center, double radius, DistanceUnit unit) {
+		DISTANCE.set( getInnerObject(), unit.toMeters( radius ) );
 		getInnerObject().add( absoluteFieldPath, codec.encode( center ) );
 	}
 

backend-lucene/src/main/java/org/hibernate/search/v6poc/backend/lucene/search/predicate/impl/AbstractSpatialWithinCirclePredicateBuilder.java
@@ -7,6 +7,7 @@
 package org.hibernate.search.v6poc.backend.lucene.search.predicate.impl;
 
 import org.hibernate.search.v6poc.search.predicate.spi.SpatialWithinCirclePredicateBuilder;
+import org.hibernate.search.v6poc.spatial.DistanceUnit;
 import org.hibernate.search.v6poc.spatial.GeoPoint;
 
 
@@ -24,8 +25,8 @@ public abstract class AbstractSpatialWithinCirclePredicateBuilder<T> extends Abs
 	}
 
 	@Override
-	public void circle(GeoPoint center, double radiusInMeters) {
+	public void circle(GeoPoint center, double radius, DistanceUnit unit) {
 		this.center = center;
-		this.radiusInMeters = radiusInMeters;
+		this.radiusInMeters = unit.toMeters( radius );
 	}
 }

engine/src/main/java/org/hibernate/search/v6poc/search/dsl/predicate/impl/SpatialWithinPredicateFieldSetContextImpl.java
@@ -21,6 +21,7 @@ import org.hibernate.search.v6poc.search.predicate.spi.SearchPredicateFactory;
 import org.hibernate.search.v6poc.search.predicate.spi.SpatialWithinBoundingBoxPredicateBuilder;
 import org.hibernate.search.v6poc.search.predicate.spi.SpatialWithinCirclePredicateBuilder;
 import org.hibernate.search.v6poc.search.predicate.spi.SpatialWithinPolygonPredicateBuilder;
+import org.hibernate.search.v6poc.spatial.DistanceUnit;
 import org.hibernate.search.v6poc.spatial.GeoBoundingBox;
 import org.hibernate.search.v6poc.spatial.GeoPoint;
 import org.hibernate.search.v6poc.spatial.GeoPolygon;
@@ -58,11 +59,12 @@ class SpatialWithinPredicateFieldSetContextImpl<N, C>
 	}
 
 	@Override
-	public SpatialWithinCirclePredicateContext<N> circle(GeoPoint center, double radiusInMeters) {
+	public SpatialWithinCirclePredicateContext<N> circle(GeoPoint center, double radius, DistanceUnit unit) {
 		Contracts.assertNotNull( center, "center" );
-		Contracts.assertNotNull( radiusInMeters, "radius" );
+		Contracts.assertNotNull( radius, "radius" );
+		Contracts.assertNotNull( unit, "unit" );
 
-		return commonState.circle( center, radiusInMeters );
+		return commonState.circle( center, radius, unit );
 	}
 
 	@Override
@@ -84,10 +86,10 @@ class SpatialWithinPredicateFieldSetContextImpl<N, C>
 		queryBuilders.forEach( collector );
 	}
 
-	private void generateWithinCircleQueryBuilders(GeoPoint center, double radiusInMeters) {
+	private void generateWithinCircleQueryBuilders(GeoPoint center, double radius, DistanceUnit unit) {
 		for ( String absoluteFieldPath : absoluteFieldPaths ) {
 			SpatialWithinCirclePredicateBuilder<C> predicateBuilder = commonState.getFactory().spatialWithinCircle( absoluteFieldPath );
-			predicateBuilder.circle( center, radiusInMeters );
+			predicateBuilder.circle( center, radius, unit );
 			if ( boost != null ) {
 				predicateBuilder.boost( boost );
 			}
@@ -123,9 +125,9 @@ class SpatialWithinPredicateFieldSetContextImpl<N, C>
 			super( factory, nextContextProvider );
 		}
 
-		public SpatialWithinCirclePredicateContext<N> circle(GeoPoint center, double radiusInMeters) {
+		public SpatialWithinCirclePredicateContext<N> circle(GeoPoint center, double radius, DistanceUnit unit) {
 			for ( SpatialWithinPredicateFieldSetContextImpl<N, C> fieldSetContext : getFieldSetContexts() ) {
-				fieldSetContext.generateWithinCircleQueryBuilders( center, radiusInMeters );
+				fieldSetContext.generateWithinCircleQueryBuilders( center, radius, unit );
 			}
 
 			return new SpatialWithinCirclePredicateContextImpl<N>( getNextContextProvider() );

engine/src/main/java/org/hibernate/search/v6poc/search/predicate/spi/SpatialWithinCirclePredicateBuilder.java
@@ -6,10 +6,11 @@
  */
 package org.hibernate.search.v6poc.search.predicate.spi;
 
+import org.hibernate.search.v6poc.spatial.DistanceUnit;
 import org.hibernate.search.v6poc.spatial.GeoPoint;
 
 public interface SpatialWithinCirclePredicateBuilder<C> extends SearchPredicateBuilder<C> {
 
-	void circle(GeoPoint center, double radiusInMeters);
+	void circle(GeoPoint center, double radius, DistanceUnit unit);
 
 }

util/internal/integrationtest/common/src/main/java/org/hibernate/search/v6poc/util/impl/integrationtest/common/stub/backend/search/predicate/impl/StubPredicateBuilder.java
@@ -14,6 +14,7 @@ import org.hibernate.search.v6poc.search.predicate.spi.RangePredicateBuilder;
 import org.hibernate.search.v6poc.search.predicate.spi.SpatialWithinBoundingBoxPredicateBuilder;
 import org.hibernate.search.v6poc.search.predicate.spi.SpatialWithinCirclePredicateBuilder;
 import org.hibernate.search.v6poc.search.predicate.spi.SpatialWithinPolygonPredicateBuilder;
+import org.hibernate.search.v6poc.spatial.DistanceUnit;
 import org.hibernate.search.v6poc.spatial.GeoBoundingBox;
 import org.hibernate.search.v6poc.spatial.GeoPoint;
 import org.hibernate.search.v6poc.spatial.GeoPolygon;
@@ -79,7 +80,7 @@ public class StubPredicateBuilder implements MatchAllPredicateBuilder<StubQueryE
 	}
 
 	@Override
-	public void circle(GeoPoint center, double radiusInMeters) {
+	public void circle(GeoPoint center, double radius, DistanceUnit unit) {
 		// No-op
 	}
 

hibernate/hibernate-search

backend/elasticsearch/src/main/java/org/hibernate/search/backend/elasticsearch/types/predicate/impl/GeoPointSpatialWithinCirclePredicateBuilder.java
@@ -12,6 +12,7 @@ import org.hibernate.search.backend.elasticsearch.search.predicate.impl.Abstract
 import org.hibernate.search.backend.elasticsearch.search.predicate.impl.ElasticsearchSearchPredicateCollector;
 import org.hibernate.search.backend.elasticsearch.types.codec.impl.ElasticsearchFieldCodec;
 import org.hibernate.search.engine.search.predicate.spi.SpatialWithinCirclePredicateBuilder;
+import org.hibernate.search.engine.spatial.DistanceUnit;
 import org.hibernate.search.engine.spatial.GeoPoint;
 
 import com.google.gson.JsonObject;
@@ -33,8 +34,8 @@ class GeoPointSpatialWithinCirclePredicateBuilder extends AbstractSearchPredicat
 	}
 
 	@Override
-	public void circle(GeoPoint center, double radiusInMeters) {
-		DISTANCE.set( getInnerObject(), radiusInMeters );
+	public void circle(GeoPoint center, double radius, DistanceUnit unit) {
+		DISTANCE.set( getInnerObject(), unit.toMeters( radius ) );
 		getInnerObject().add( absoluteFieldPath, codec.encode( center ) );
 	}
 

backend/lucene/src/main/java/org/hibernate/search/backend/lucene/search/predicate/impl/AbstractSpatialWithinCirclePredicateBuilder.java
@@ -7,6 +7,7 @@
 package org.hibernate.search.backend.lucene.search.predicate.impl;
 
 import org.hibernate.search.engine.search.predicate.spi.SpatialWithinCirclePredicateBuilder;
+import org.hibernate.search.engine.spatial.DistanceUnit;
 import org.hibernate.search.engine.spatial.GeoPoint;
 
 
@@ -24,8 +25,8 @@ public abstract class AbstractSpatialWithinCirclePredicateBuilder<T> extends Abs
 	}
 
 	@Override
-	public void circle(GeoPoint center, double radiusInMeters) {
+	public void circle(GeoPoint center, double radius, DistanceUnit unit) {
 		this.center = center;
-		this.radiusInMeters = radiusInMeters;
+		this.radiusInMeters = unit.toMeters( radius );
 	}
 }

engine/src/main/java/org/hibernate/search/engine/search/dsl/predicate/impl/SpatialWithinPredicateFieldSetContextImpl.java
@@ -21,6 +21,7 @@ import org.hibernate.search.engine.search.predicate.spi.SearchPredicateFactory;
 import org.hibernate.search.engine.search.predicate.spi.SpatialWithinBoundingBoxPredicateBuilder;
 import org.hibernate.search.engine.search.predicate.spi.SpatialWithinCirclePredicateBuilder;
 import org.hibernate.search.engine.search.predicate.spi.SpatialWithinPolygonPredicateBuilder;
+import org.hibernate.search.engine.spatial.DistanceUnit;
 import org.hibernate.search.engine.spatial.GeoBoundingBox;
 import org.hibernate.search.engine.spatial.GeoPoint;
 import org.hibernate.search.engine.spatial.GeoPolygon;
@@ -58,11 +59,12 @@ class SpatialWithinPredicateFieldSetContextImpl<N, C>
 	}
 
 	@Override
-	public SpatialWithinCirclePredicateContext<N> circle(GeoPoint center, double radiusInMeters) {
+	public SpatialWithinCirclePredicateContext<N> circle(GeoPoint center, double radius, DistanceUnit unit) {
 		Contracts.assertNotNull( center, "center" );
-		Contracts.assertNotNull( radiusInMeters, "radius" );
+		Contracts.assertNotNull( radius, "radius" );
+		Contracts.assertNotNull( unit, "unit" );
 
-		return commonState.circle( center, radiusInMeters );
+		return commonState.circle( center, radius, unit );
 	}
 
 	@Override
@@ -84,10 +86,10 @@ class SpatialWithinPredicateFieldSetContextImpl<N, C>
 		queryBuilders.forEach( collector );
 	}
 
-	private void generateWithinCircleQueryBuilders(GeoPoint center, double radiusInMeters) {
+	private void generateWithinCircleQueryBuilders(GeoPoint center, double radius, DistanceUnit unit) {
 		for ( String absoluteFieldPath : absoluteFieldPaths ) {
 			SpatialWithinCirclePredicateBuilder<C> predicateBuilder = commonState.getFactory().spatialWithinCircle( absoluteFieldPath );
-			predicateBuilder.circle( center, radiusInMeters );
+			predicateBuilder.circle( center, radius, unit );
 			if ( boost != null ) {
 				predicateBuilder.boost( boost );
 			}
@@ -123,9 +125,9 @@ class SpatialWithinPredicateFieldSetContextImpl<N, C>
 			super( factory, nextContextProvider );
 		}
 
-		public SpatialWithinCirclePredicateContext<N> circle(GeoPoint center, double radiusInMeters) {
+		public SpatialWithinCirclePredicateContext<N> circle(GeoPoint center, double radius, DistanceUnit unit) {
 			for ( SpatialWithinPredicateFieldSetContextImpl<N, C> fieldSetContext : getFieldSetContexts() ) {
-				fieldSetContext.generateWithinCircleQueryBuilders( center, radiusInMeters );
+				fieldSetContext.generateWithinCircleQueryBuilders( center, radius, unit );
 			}
 
 			return new SpatialWithinCirclePredicateContextImpl<N>( getNextContextProvider() );

engine/src/main/java/org/hibernate/search/engine/search/predicate/spi/SpatialWithinCirclePredicateBuilder.java
@@ -6,10 +6,11 @@
  */
 package org.hibernate.search.engine.search.predicate.spi;
 
+import org.hibernate.search.engine.spatial.DistanceUnit;
 import org.hibernate.search.engine.spatial.GeoPoint;
 
 public interface SpatialWithinCirclePredicateBuilder<C> extends SearchPredicateBuilder<C> {
 
-	void circle(GeoPoint center, double radiusInMeters);
+	void circle(GeoPoint center, double radius, DistanceUnit unit);
 
 }

util/internal/integrationtest/common/src/main/java/org/hibernate/search/util/impl/integrationtest/common/stub/backend/search/predicate/impl/StubPredicateBuilder.java
@@ -14,6 +14,7 @@ import org.hibernate.search.engine.search.predicate.spi.RangePredicateBuilder;
 import org.hibernate.search.engine.search.predicate.spi.SpatialWithinBoundingBoxPredicateBuilder;
 import org.hibernate.search.engine.search.predicate.spi.SpatialWithinCirclePredicateBuilder;
 import org.hibernate.search.engine.search.predicate.spi.SpatialWithinPolygonPredicateBuilder;
+import org.hibernate.search.engine.spatial.DistanceUnit;
 import org.hibernate.search.engine.spatial.GeoBoundingBox;
 import org.hibernate.search.engine.spatial.GeoPoint;
 import org.hibernate.search.engine.spatial.GeoPolygon;
@@ -79,7 +80,7 @@ public class StubPredicateBuilder implements MatchAllPredicateBuilder<StubQueryE
 	}
 
 	@Override
-	public void circle(GeoPoint center, double radiusInMeters) {
+	public void circle(GeoPoint center, double radius, DistanceUnit unit) {
 		// No-op
 	}
 