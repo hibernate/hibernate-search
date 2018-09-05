hibernate/hibernate-search-6-poc

HSEARCH-3127 Allow declaring native Lucene fields in the schema

backend-lucene/src/main/java/org/hibernate/search/v6poc/backend/lucene/document/model/dsl/LuceneIndexSchemaFieldContext.java
@@ -6,12 +6,28 @@
  */
 package org.hibernate.search.v6poc.backend.lucene.document.model.dsl;
 
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+import org.apache.lucene.index.IndexableField;
 import org.hibernate.search.v6poc.backend.document.model.dsl.IndexSchemaFieldContext;
+import org.hibernate.search.v6poc.backend.document.model.dsl.IndexSchemaFieldTerminalContext;
 
 
 /**
  * @author Guillaume Smet
  */
 public interface LuceneIndexSchemaFieldContext extends IndexSchemaFieldContext {
+
+	/**
+	 * Declares a native field.
+	 *
+	 * @param fieldProducer A bifunction producing a field. The first parameter is the absolute path of the field, the second one is the value.
+	 * @param fieldValueExtractor A function responsible for extracting the value from the field. The parameter is the Lucene field itself.
+	 * @param <V> The type of the value.
+	 * @return The DSL context.
+	 */
+	<V> IndexSchemaFieldTerminalContext<V> asLuceneField(BiFunction<String, V, IndexableField> fieldProducer,
+			Function<IndexableField, V> fieldValueExtractor);
 
 }

backend-lucene/src/main/java/org/hibernate/search/v6poc/backend/lucene/document/model/dsl/impl/LuceneIndexSchemaFieldContextImpl.java
@@ -7,15 +7,20 @@
 package org.hibernate.search.v6poc.backend.lucene.document.model.dsl.impl;
 
 import java.time.LocalDate;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
-import org.hibernate.search.v6poc.backend.document.model.dsl.IndexSchemaFieldContext;
+import org.apache.lucene.index.IndexableField;
+import org.hibernate.search.v6poc.backend.document.model.dsl.IndexSchemaFieldTerminalContext;
 import org.hibernate.search.v6poc.backend.document.model.dsl.IndexSchemaFieldTypedContext;
+import org.hibernate.search.v6poc.backend.lucene.document.model.dsl.LuceneIndexSchemaFieldContext;
 import org.hibernate.search.v6poc.backend.lucene.document.model.impl.LuceneIndexSchemaNodeCollector;
 import org.hibernate.search.v6poc.backend.lucene.document.model.impl.LuceneIndexSchemaNodeContributor;
 import org.hibernate.search.v6poc.backend.lucene.document.model.impl.LuceneIndexSchemaObjectNode;
 import org.hibernate.search.v6poc.backend.lucene.types.dsl.impl.GeoPointIndexSchemaFieldContext;
 import org.hibernate.search.v6poc.backend.lucene.types.dsl.impl.IntegerIndexSchemaFieldContext;
 import org.hibernate.search.v6poc.backend.lucene.types.dsl.impl.LocalDateIndexSchemaFieldContext;
+import org.hibernate.search.v6poc.backend.lucene.types.dsl.impl.LuceneFieldIndexSchemaFieldContext;
 import org.hibernate.search.v6poc.backend.lucene.types.dsl.impl.StringIndexSchemaFieldContext;
 import org.hibernate.search.v6poc.spatial.GeoPoint;
 import org.hibernate.search.v6poc.util.SearchException;
@@ -25,7 +30,7 @@ import org.hibernate.search.v6poc.util.impl.common.Contracts;
 /**
  * @author Guillaume Smet
  */
-public class LuceneIndexSchemaFieldContextImpl implements IndexSchemaFieldContext, LuceneIndexSchemaNodeContributor {
+public class LuceneIndexSchemaFieldContextImpl implements LuceneIndexSchemaFieldContext, LuceneIndexSchemaNodeContributor {
 
 	private final String relativeFieldName;
 
@@ -81,6 +86,12 @@ public class LuceneIndexSchemaFieldContextImpl implements IndexSchemaFieldContex
 		Contracts.assertNotNull( delegate, "delegate" );
 
 		delegate.contribute( collector, parentNode );
+	}
+
+	@Override
+	public <V> IndexSchemaFieldTerminalContext<V> asLuceneField(BiFunction<String, V, IndexableField> fieldProducer,
+			Function<IndexableField, V> fieldValueExtractor) {
+		return setDelegate( new LuceneFieldIndexSchemaFieldContext<>( relativeFieldName, fieldProducer, fieldValueExtractor ) );
 	}
 
 	private <T extends LuceneIndexSchemaNodeContributor> T setDelegate(T context) {

backend-lucene/src/main/java/org/hibernate/search/v6poc/backend/lucene/types/dsl/impl/LuceneFieldIndexSchemaFieldContext.java
@@ -1,0 +1,54 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.v6poc.backend.lucene.types.dsl.impl;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+import org.apache.lucene.index.IndexableField;
+import org.hibernate.search.v6poc.backend.document.spi.DeferredInitializationIndexFieldAccessor;
+import org.hibernate.search.v6poc.backend.lucene.document.impl.LuceneIndexFieldAccessor;
+import org.hibernate.search.v6poc.backend.lucene.document.model.impl.LuceneIndexSchemaFieldNode;
+import org.hibernate.search.v6poc.backend.lucene.document.model.impl.LuceneIndexSchemaNodeCollector;
+import org.hibernate.search.v6poc.backend.lucene.document.model.impl.LuceneIndexSchemaObjectNode;
+import org.hibernate.search.v6poc.backend.lucene.types.codec.impl.LuceneFieldFieldCodec;
+import org.hibernate.search.v6poc.backend.lucene.types.formatter.impl.SimpleCastingFieldFormatter;
+
+/**
+ * @author Guillaume Smet
+ */
+public class LuceneFieldIndexSchemaFieldContext<V, F extends IndexableField> extends AbstractLuceneIndexSchemaFieldTypedContext<V> {
+
+	private BiFunction<String, V, IndexableField> fieldProducer;
+
+	private Function<IndexableField, V> fieldValueExtractor;
+
+	public LuceneFieldIndexSchemaFieldContext(String relativeFieldName, BiFunction<String, V, IndexableField> fieldProducer,
+			Function<IndexableField, V> fieldValueExtractor) {
+		super( relativeFieldName );
+
+		this.fieldProducer = fieldProducer;
+		this.fieldValueExtractor = fieldValueExtractor;
+	}
+
+	@Override
+	protected void contribute(DeferredInitializationIndexFieldAccessor<V> accessor, LuceneIndexSchemaNodeCollector collector,
+			LuceneIndexSchemaObjectNode parentNode) {
+		LuceneIndexSchemaFieldNode<V> schemaNode = new LuceneIndexSchemaFieldNode<V>(
+				parentNode,
+				getRelativeFieldName(),
+				new SimpleCastingFieldFormatter<>(),
+				new LuceneFieldFieldCodec<>( fieldProducer, fieldValueExtractor ),
+				null,
+				null
+		);
+
+		accessor.initialize( new LuceneIndexFieldAccessor<>( schemaNode ) );
+
+		collector.collectFieldNode( schemaNode.getAbsoluteFieldPath(), schemaNode );
+	}
+}

hibernate/hibernate-search

backend/lucene/src/main/java/org/hibernate/search/backend/lucene/document/model/dsl/LuceneIndexSchemaFieldContext.java
@@ -6,12 +6,28 @@
  */
 package org.hibernate.search.backend.lucene.document.model.dsl;
 
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+import org.apache.lucene.index.IndexableField;
 import org.hibernate.search.engine.backend.document.model.dsl.IndexSchemaFieldContext;
+import org.hibernate.search.engine.backend.document.model.dsl.IndexSchemaFieldTerminalContext;
 
 
 /**
  * @author Guillaume Smet
  */
 public interface LuceneIndexSchemaFieldContext extends IndexSchemaFieldContext {
+
+	/**
+	 * Declares a native field.
+	 *
+	 * @param fieldProducer A bifunction producing a field. The first parameter is the absolute path of the field, the second one is the value.
+	 * @param fieldValueExtractor A function responsible for extracting the value from the field. The parameter is the Lucene field itself.
+	 * @param <V> The type of the value.
+	 * @return The DSL context.
+	 */
+	<V> IndexSchemaFieldTerminalContext<V> asLuceneField(BiFunction<String, V, IndexableField> fieldProducer,
+			Function<IndexableField, V> fieldValueExtractor);
 
 }

backend/lucene/src/main/java/org/hibernate/search/backend/lucene/document/model/dsl/impl/LuceneIndexSchemaFieldContextImpl.java
@@ -7,15 +7,20 @@
 package org.hibernate.search.backend.lucene.document.model.dsl.impl;
 
 import java.time.LocalDate;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
-import org.hibernate.search.engine.backend.document.model.dsl.IndexSchemaFieldContext;
+import org.apache.lucene.index.IndexableField;
+import org.hibernate.search.engine.backend.document.model.dsl.IndexSchemaFieldTerminalContext;
 import org.hibernate.search.engine.backend.document.model.dsl.IndexSchemaFieldTypedContext;
+import org.hibernate.search.backend.lucene.document.model.dsl.LuceneIndexSchemaFieldContext;
 import org.hibernate.search.backend.lucene.document.model.impl.LuceneIndexSchemaNodeCollector;
 import org.hibernate.search.backend.lucene.document.model.impl.LuceneIndexSchemaNodeContributor;
 import org.hibernate.search.backend.lucene.document.model.impl.LuceneIndexSchemaObjectNode;
 import org.hibernate.search.backend.lucene.types.dsl.impl.GeoPointIndexSchemaFieldContext;
 import org.hibernate.search.backend.lucene.types.dsl.impl.IntegerIndexSchemaFieldContext;
 import org.hibernate.search.backend.lucene.types.dsl.impl.LocalDateIndexSchemaFieldContext;
+import org.hibernate.search.backend.lucene.types.dsl.impl.LuceneFieldIndexSchemaFieldContext;
 import org.hibernate.search.backend.lucene.types.dsl.impl.StringIndexSchemaFieldContext;
 import org.hibernate.search.engine.spatial.GeoPoint;
 import org.hibernate.search.util.SearchException;
@@ -25,7 +30,7 @@ import org.hibernate.search.util.impl.common.Contracts;
 /**
  * @author Guillaume Smet
  */
-public class LuceneIndexSchemaFieldContextImpl implements IndexSchemaFieldContext, LuceneIndexSchemaNodeContributor {
+public class LuceneIndexSchemaFieldContextImpl implements LuceneIndexSchemaFieldContext, LuceneIndexSchemaNodeContributor {
 
 	private final String relativeFieldName;
 
@@ -81,6 +86,12 @@ public class LuceneIndexSchemaFieldContextImpl implements IndexSchemaFieldContex
 		Contracts.assertNotNull( delegate, "delegate" );
 
 		delegate.contribute( collector, parentNode );
+	}
+
+	@Override
+	public <V> IndexSchemaFieldTerminalContext<V> asLuceneField(BiFunction<String, V, IndexableField> fieldProducer,
+			Function<IndexableField, V> fieldValueExtractor) {
+		return setDelegate( new LuceneFieldIndexSchemaFieldContext<>( relativeFieldName, fieldProducer, fieldValueExtractor ) );
 	}
 
 	private <T extends LuceneIndexSchemaNodeContributor> T setDelegate(T context) {

backend/lucene/src/main/java/org/hibernate/search/backend/lucene/types/dsl/impl/LuceneFieldIndexSchemaFieldContext.java
@@ -1,0 +1,54 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.backend.lucene.types.dsl.impl;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+import org.apache.lucene.index.IndexableField;
+import org.hibernate.search.engine.backend.document.spi.DeferredInitializationIndexFieldAccessor;
+import org.hibernate.search.backend.lucene.document.impl.LuceneIndexFieldAccessor;
+import org.hibernate.search.backend.lucene.document.model.impl.LuceneIndexSchemaFieldNode;
+import org.hibernate.search.backend.lucene.document.model.impl.LuceneIndexSchemaNodeCollector;
+import org.hibernate.search.backend.lucene.document.model.impl.LuceneIndexSchemaObjectNode;
+import org.hibernate.search.backend.lucene.types.codec.impl.LuceneFieldFieldCodec;
+import org.hibernate.search.backend.lucene.types.formatter.impl.SimpleCastingFieldFormatter;
+
+/**
+ * @author Guillaume Smet
+ */
+public class LuceneFieldIndexSchemaFieldContext<V, F extends IndexableField> extends AbstractLuceneIndexSchemaFieldTypedContext<V> {
+
+	private BiFunction<String, V, IndexableField> fieldProducer;
+
+	private Function<IndexableField, V> fieldValueExtractor;
+
+	public LuceneFieldIndexSchemaFieldContext(String relativeFieldName, BiFunction<String, V, IndexableField> fieldProducer,
+			Function<IndexableField, V> fieldValueExtractor) {
+		super( relativeFieldName );
+
+		this.fieldProducer = fieldProducer;
+		this.fieldValueExtractor = fieldValueExtractor;
+	}
+
+	@Override
+	protected void contribute(DeferredInitializationIndexFieldAccessor<V> accessor, LuceneIndexSchemaNodeCollector collector,
+			LuceneIndexSchemaObjectNode parentNode) {
+		LuceneIndexSchemaFieldNode<V> schemaNode = new LuceneIndexSchemaFieldNode<V>(
+				parentNode,
+				getRelativeFieldName(),
+				new SimpleCastingFieldFormatter<>(),
+				new LuceneFieldFieldCodec<>( fieldProducer, fieldValueExtractor ),
+				null,
+				null
+		);
+
+		accessor.initialize( new LuceneIndexFieldAccessor<>( schemaNode ) );
+
+		collector.collectFieldNode( schemaNode.getAbsoluteFieldPath(), schemaNode );
+	}
+}