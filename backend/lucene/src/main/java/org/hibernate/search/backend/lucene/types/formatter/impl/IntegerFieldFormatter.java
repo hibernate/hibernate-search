hibernate/hibernate-search-6-poc

HSEARCH-3105 Split the inner classes in their own files

backend-lucene/src/main/java/org/hibernate/search/v6poc/backend/lucene/types/formatter/impl/IntegerFieldFormatter.java
@@ -1,0 +1,87 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.v6poc.backend.lucene.types.formatter.impl;
+
+import java.util.Objects;
+
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.IntPoint;
+import org.apache.lucene.document.NumericDocValuesField;
+import org.apache.lucene.document.StoredField;
+import org.apache.lucene.index.IndexableField;
+import org.hibernate.search.v6poc.backend.document.model.Sortable;
+import org.hibernate.search.v6poc.backend.document.model.Store;
+import org.hibernate.search.v6poc.backend.lucene.document.impl.LuceneDocumentBuilder;
+import org.hibernate.search.v6poc.backend.lucene.document.model.impl.LuceneFieldFormatter;
+import org.hibernate.search.v6poc.backend.lucene.document.model.impl.LuceneIndexSchemaObjectNode;
+
+public final class IntegerFieldFormatter implements LuceneFieldFormatter<Integer> {
+
+	private final Store store;
+
+	private final Sortable sortable;
+
+	public IntegerFieldFormatter(Store store, Sortable sortable) {
+		this.store = store;
+		this.sortable = sortable;
+	}
+
+	@Override
+	public void addFields(LuceneDocumentBuilder documentBuilder, LuceneIndexSchemaObjectNode parentNode, String fieldName, Integer value) {
+		if ( value == null ) {
+			return;
+		}
+
+		if ( Store.YES.equals( store ) ) {
+			documentBuilder.addField( parentNode, new StoredField( fieldName, value ) );
+		}
+
+		if ( Sortable.YES.equals( sortable ) ) {
+			documentBuilder.addField( parentNode, new NumericDocValuesField( fieldName, value.longValue() ) );
+		}
+
+		documentBuilder.addField( parentNode, new IntPoint( fieldName, value ) );
+	}
+
+	@Override
+	public Integer parse(Document document, String fieldName) {
+		IndexableField field = document.getField( fieldName );
+
+		if ( field == null ) {
+			return null;
+		}
+
+		return (Integer) field.numericValue();
+	}
+
+	@Override
+	public Object format(Object value) {
+		return value;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if ( this == obj ) {
+			return true;
+		}
+		if ( obj == null ) {
+			return false;
+		}
+		if ( IntegerFieldFormatter.class != obj.getClass() ) {
+			return false;
+		}
+
+		IntegerFieldFormatter other = (IntegerFieldFormatter) obj;
+
+		return Objects.equals( store, other.store ) && Objects.equals( sortable, other.sortable );
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash( store, sortable );
+	}
+}

backend-lucene/src/main/java/org/hibernate/search/v6poc/backend/lucene/types/formatter/impl/LocalDateFieldFormatter.java
@@ -1,0 +1,114 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.v6poc.backend.lucene.types.formatter.impl;
+
+import static java.time.temporal.ChronoField.DAY_OF_MONTH;
+import static java.time.temporal.ChronoField.MONTH_OF_YEAR;
+import static java.time.temporal.ChronoField.YEAR;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.ResolverStyle;
+import java.time.format.SignStyle;
+import java.util.Locale;
+import java.util.Objects;
+
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.LongPoint;
+import org.apache.lucene.document.NumericDocValuesField;
+import org.apache.lucene.document.StoredField;
+import org.apache.lucene.index.IndexableField;
+import org.hibernate.search.v6poc.backend.document.model.Sortable;
+import org.hibernate.search.v6poc.backend.document.model.Store;
+import org.hibernate.search.v6poc.backend.lucene.document.impl.LuceneDocumentBuilder;
+import org.hibernate.search.v6poc.backend.lucene.document.model.impl.LuceneFieldFormatter;
+import org.hibernate.search.v6poc.backend.lucene.document.model.impl.LuceneIndexSchemaObjectNode;
+
+public final class LocalDateFieldFormatter implements LuceneFieldFormatter<LocalDate> {
+
+	private static final DateTimeFormatter FORMATTER = new DateTimeFormatterBuilder()
+			.appendValue( YEAR, 4, 9, SignStyle.EXCEEDS_PAD )
+			.appendLiteral( '-' )
+			.appendValue( MONTH_OF_YEAR, 2 )
+			.appendLiteral( '-' )
+			.appendValue( DAY_OF_MONTH, 2 )
+			.toFormatter( Locale.ROOT )
+			.withResolverStyle( ResolverStyle.STRICT );
+
+	private final Store store;
+
+	private final Sortable sortable;
+
+	public LocalDateFieldFormatter(Store store, Sortable sortable) {
+		this.store = store;
+		this.sortable = sortable;
+	}
+
+	@Override
+	public void addFields(LuceneDocumentBuilder documentBuilder, LuceneIndexSchemaObjectNode parentNode, String fieldName, LocalDate value) {
+		if ( value == null ) {
+			return;
+		}
+
+		if ( Store.YES.equals( store ) ) {
+			documentBuilder.addField( parentNode, new StoredField( fieldName, FORMATTER.format( value ) ) );
+		}
+
+		long valueToEpochDay = value.toEpochDay();
+
+		if ( Sortable.YES.equals( sortable ) ) {
+			documentBuilder.addField( parentNode, new NumericDocValuesField( fieldName, valueToEpochDay ) );
+		}
+
+		documentBuilder.addField( parentNode, new LongPoint( fieldName, valueToEpochDay ) );
+	}
+
+	@Override
+	public Object format(Object value) {
+		return ((LocalDate) value).toEpochDay();
+	}
+
+	@Override
+	public LocalDate parse(Document document, String fieldName) {
+		IndexableField field = document.getField( fieldName );
+
+		if ( field == null ) {
+			return null;
+		}
+
+		String value = field.stringValue();
+
+		if ( value == null ) {
+			return null;
+		}
+
+		return LocalDate.parse( value, FORMATTER );
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if ( this == obj ) {
+			return true;
+		}
+		if ( obj == null ) {
+			return false;
+		}
+		if ( LocalDateFieldFormatter.class != obj.getClass() ) {
+			return false;
+		}
+
+		LocalDateFieldFormatter other = (LocalDateFieldFormatter) obj;
+
+		return Objects.equals( store, other.store ) && Objects.equals( sortable, other.sortable );
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash( store, sortable );
+	}
+}

backend-lucene/src/main/java/org/hibernate/search/v6poc/backend/lucene/types/formatter/impl/StringFieldFormatter.java
@@ -1,0 +1,86 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.v6poc.backend.lucene.types.formatter.impl;
+
+import java.util.Objects;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.FieldType;
+import org.apache.lucene.document.SortedDocValuesField;
+import org.apache.lucene.util.BytesRef;
+import org.hibernate.search.v6poc.backend.document.model.Sortable;
+import org.hibernate.search.v6poc.backend.lucene.document.impl.LuceneDocumentBuilder;
+import org.hibernate.search.v6poc.backend.lucene.document.model.impl.LuceneFieldFormatter;
+import org.hibernate.search.v6poc.backend.lucene.document.model.impl.LuceneIndexSchemaObjectNode;
+import org.hibernate.search.v6poc.backend.lucene.util.impl.AnalyzerUtils;
+
+public final class StringFieldFormatter implements LuceneFieldFormatter<String> {
+
+	private final Sortable sortable;
+
+	private final FieldType fieldType;
+
+	private final Analyzer normalizer;
+
+	public StringFieldFormatter(Sortable sortable, FieldType fieldType, Analyzer normalizer) {
+		this.sortable = sortable;
+		this.fieldType = fieldType;
+		this.normalizer = normalizer;
+	}
+
+	@Override
+	public void addFields(LuceneDocumentBuilder documentBuilder, LuceneIndexSchemaObjectNode parentNode, String fieldName, String value) {
+		if ( value == null ) {
+			return;
+		}
+
+		documentBuilder.addField( parentNode, new Field( fieldName, value, fieldType ) );
+
+		if ( Sortable.YES.equals( sortable ) ) {
+			documentBuilder.addField( parentNode, new SortedDocValuesField(
+					fieldName,
+					new BytesRef( normalizer != null ? AnalyzerUtils.analyzeSortableValue( normalizer, fieldName, value ) : value )
+			) );
+		}
+	}
+
+	@Override
+	public Object format(Object value) {
+		return value;
+	}
+
+	@Override
+	public String parse(Document document, String fieldName) {
+		return document.get( fieldName );
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if ( this == obj ) {
+			return true;
+		}
+		if ( obj == null ) {
+			return false;
+		}
+		if ( StringFieldFormatter.class != obj.getClass() ) {
+			return false;
+		}
+
+		StringFieldFormatter other = (StringFieldFormatter) obj;
+
+		return Objects.equals( sortable, other.sortable ) &&
+				Objects.equals( fieldType, other.fieldType ) &&
+				Objects.equals( normalizer, other.normalizer );
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash( sortable, fieldType, normalizer );
+	}
+}

backend-lucene/src/main/java/org/hibernate/search/v6poc/backend/lucene/types/predicate/impl/StringFieldQueryFactory.java
@@ -1,0 +1,99 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.v6poc.backend.lucene.types.predicate.impl;
+
+import java.util.Objects;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.search.TermRangeQuery;
+import org.apache.lucene.util.QueryBuilder;
+import org.hibernate.search.v6poc.backend.lucene.document.model.impl.LuceneFieldQueryFactory;
+import org.hibernate.search.v6poc.backend.lucene.document.model.impl.MatchQueryOptions;
+import org.hibernate.search.v6poc.backend.lucene.document.model.impl.RangeQueryOptions;
+import org.hibernate.search.v6poc.backend.lucene.types.formatter.impl.StringFieldFormatter;
+import org.hibernate.search.v6poc.backend.lucene.util.impl.AnalyzerUtils;
+
+public class StringFieldQueryFactory implements LuceneFieldQueryFactory {
+
+	private final Analyzer analyzerOrNormalizer;
+
+	private final boolean tokenized;
+
+	private final QueryBuilder queryBuilder;
+
+	public StringFieldQueryFactory(Analyzer analyzerOrNormalizer, boolean tokenized, QueryBuilder queryBuilder) {
+		this.analyzerOrNormalizer = analyzerOrNormalizer;
+		this.tokenized = tokenized;
+		this.queryBuilder = queryBuilder;
+	}
+
+	@Override
+	public Query createMatchQuery(String fieldName, Object value, MatchQueryOptions matchQueryOptions) {
+		String stringValue = (String) value;
+
+		if ( queryBuilder != null ) {
+			return queryBuilder.createBooleanQuery( fieldName, stringValue, matchQueryOptions.getOperator() );
+		}
+		else {
+			// we are in the case where we a have a normalizer here as the analyzer case has already been treated by
+			// the queryBuilder case above
+
+			return new TermQuery( new Term( fieldName, getAnalyzedValue( analyzerOrNormalizer, fieldName, stringValue ) ) );
+		}
+	}
+
+	@Override
+	public Query createRangeQuery(String fieldName, Object lowerLimit, Object upperLimit, RangeQueryOptions rangeQueryOptions) {
+		// Note that a range query only makes sense if only one token is returned by the analyzer
+		// and we should even consider forcing having a normalizer here, instead of supporting
+		// range queries on analyzed fields.
+
+		return TermRangeQuery.newStringRange(
+				fieldName,
+				getAnalyzedValue( analyzerOrNormalizer, fieldName, (String) lowerLimit ),
+				getAnalyzedValue( analyzerOrNormalizer, fieldName, (String) upperLimit ),
+				// we force the true value if the limit is null because of some Lucene checks down the hill
+				lowerLimit == null ? true : !rangeQueryOptions.isExcludeLowerLimit(),
+				upperLimit == null ? true : !rangeQueryOptions.isExcludeUpperLimit()
+		);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if ( this == obj ) {
+			return true;
+		}
+		if ( obj == null ) {
+			return false;
+		}
+		if ( StringFieldFormatter.class != obj.getClass() ) {
+			return false;
+		}
+
+		StringFieldQueryFactory other = (StringFieldQueryFactory) obj;
+
+		return Objects.equals( analyzerOrNormalizer, other.analyzerOrNormalizer ) &&
+				tokenized == other.tokenized &&
+				Objects.equals( queryBuilder, other.queryBuilder );
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash( analyzerOrNormalizer, tokenized, queryBuilder );
+	}
+
+	private static String getAnalyzedValue(Analyzer analyzerOrNormalizer, String fieldName, String stringValue) {
+		if ( analyzerOrNormalizer == null ) {
+			return stringValue;
+		}
+
+		return AnalyzerUtils.analyzeSortableValue( analyzerOrNormalizer, fieldName, stringValue );
+	}
+}

hibernate/hibernate-search

backend/lucene/src/main/java/org/hibernate/search/backend/lucene/types/formatter/impl/IntegerFieldFormatter.java
@@ -1,0 +1,87 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.backend.lucene.types.formatter.impl;
+
+import java.util.Objects;
+
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.IntPoint;
+import org.apache.lucene.document.NumericDocValuesField;
+import org.apache.lucene.document.StoredField;
+import org.apache.lucene.index.IndexableField;
+import org.hibernate.search.engine.backend.document.model.Sortable;
+import org.hibernate.search.engine.backend.document.model.Store;
+import org.hibernate.search.backend.lucene.document.impl.LuceneDocumentBuilder;
+import org.hibernate.search.backend.lucene.document.model.impl.LuceneFieldFormatter;
+import org.hibernate.search.backend.lucene.document.model.impl.LuceneIndexSchemaObjectNode;
+
+public final class IntegerFieldFormatter implements LuceneFieldFormatter<Integer> {
+
+	private final Store store;
+
+	private final Sortable sortable;
+
+	public IntegerFieldFormatter(Store store, Sortable sortable) {
+		this.store = store;
+		this.sortable = sortable;
+	}
+
+	@Override
+	public void addFields(LuceneDocumentBuilder documentBuilder, LuceneIndexSchemaObjectNode parentNode, String fieldName, Integer value) {
+		if ( value == null ) {
+			return;
+		}
+
+		if ( Store.YES.equals( store ) ) {
+			documentBuilder.addField( parentNode, new StoredField( fieldName, value ) );
+		}
+
+		if ( Sortable.YES.equals( sortable ) ) {
+			documentBuilder.addField( parentNode, new NumericDocValuesField( fieldName, value.longValue() ) );
+		}
+
+		documentBuilder.addField( parentNode, new IntPoint( fieldName, value ) );
+	}
+
+	@Override
+	public Integer parse(Document document, String fieldName) {
+		IndexableField field = document.getField( fieldName );
+
+		if ( field == null ) {
+			return null;
+		}
+
+		return (Integer) field.numericValue();
+	}
+
+	@Override
+	public Object format(Object value) {
+		return value;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if ( this == obj ) {
+			return true;
+		}
+		if ( obj == null ) {
+			return false;
+		}
+		if ( IntegerFieldFormatter.class != obj.getClass() ) {
+			return false;
+		}
+
+		IntegerFieldFormatter other = (IntegerFieldFormatter) obj;
+
+		return Objects.equals( store, other.store ) && Objects.equals( sortable, other.sortable );
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash( store, sortable );
+	}
+}

backend/lucene/src/main/java/org/hibernate/search/backend/lucene/types/formatter/impl/LocalDateFieldFormatter.java
@@ -1,0 +1,114 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.backend.lucene.types.formatter.impl;
+
+import static java.time.temporal.ChronoField.DAY_OF_MONTH;
+import static java.time.temporal.ChronoField.MONTH_OF_YEAR;
+import static java.time.temporal.ChronoField.YEAR;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.ResolverStyle;
+import java.time.format.SignStyle;
+import java.util.Locale;
+import java.util.Objects;
+
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.LongPoint;
+import org.apache.lucene.document.NumericDocValuesField;
+import org.apache.lucene.document.StoredField;
+import org.apache.lucene.index.IndexableField;
+import org.hibernate.search.engine.backend.document.model.Sortable;
+import org.hibernate.search.engine.backend.document.model.Store;
+import org.hibernate.search.backend.lucene.document.impl.LuceneDocumentBuilder;
+import org.hibernate.search.backend.lucene.document.model.impl.LuceneFieldFormatter;
+import org.hibernate.search.backend.lucene.document.model.impl.LuceneIndexSchemaObjectNode;
+
+public final class LocalDateFieldFormatter implements LuceneFieldFormatter<LocalDate> {
+
+	private static final DateTimeFormatter FORMATTER = new DateTimeFormatterBuilder()
+			.appendValue( YEAR, 4, 9, SignStyle.EXCEEDS_PAD )
+			.appendLiteral( '-' )
+			.appendValue( MONTH_OF_YEAR, 2 )
+			.appendLiteral( '-' )
+			.appendValue( DAY_OF_MONTH, 2 )
+			.toFormatter( Locale.ROOT )
+			.withResolverStyle( ResolverStyle.STRICT );
+
+	private final Store store;
+
+	private final Sortable sortable;
+
+	public LocalDateFieldFormatter(Store store, Sortable sortable) {
+		this.store = store;
+		this.sortable = sortable;
+	}
+
+	@Override
+	public void addFields(LuceneDocumentBuilder documentBuilder, LuceneIndexSchemaObjectNode parentNode, String fieldName, LocalDate value) {
+		if ( value == null ) {
+			return;
+		}
+
+		if ( Store.YES.equals( store ) ) {
+			documentBuilder.addField( parentNode, new StoredField( fieldName, FORMATTER.format( value ) ) );
+		}
+
+		long valueToEpochDay = value.toEpochDay();
+
+		if ( Sortable.YES.equals( sortable ) ) {
+			documentBuilder.addField( parentNode, new NumericDocValuesField( fieldName, valueToEpochDay ) );
+		}
+
+		documentBuilder.addField( parentNode, new LongPoint( fieldName, valueToEpochDay ) );
+	}
+
+	@Override
+	public Object format(Object value) {
+		return ((LocalDate) value).toEpochDay();
+	}
+
+	@Override
+	public LocalDate parse(Document document, String fieldName) {
+		IndexableField field = document.getField( fieldName );
+
+		if ( field == null ) {
+			return null;
+		}
+
+		String value = field.stringValue();
+
+		if ( value == null ) {
+			return null;
+		}
+
+		return LocalDate.parse( value, FORMATTER );
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if ( this == obj ) {
+			return true;
+		}
+		if ( obj == null ) {
+			return false;
+		}
+		if ( LocalDateFieldFormatter.class != obj.getClass() ) {
+			return false;
+		}
+
+		LocalDateFieldFormatter other = (LocalDateFieldFormatter) obj;
+
+		return Objects.equals( store, other.store ) && Objects.equals( sortable, other.sortable );
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash( store, sortable );
+	}
+}

backend/lucene/src/main/java/org/hibernate/search/backend/lucene/types/formatter/impl/StringFieldFormatter.java
@@ -1,0 +1,86 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.backend.lucene.types.formatter.impl;
+
+import java.util.Objects;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.FieldType;
+import org.apache.lucene.document.SortedDocValuesField;
+import org.apache.lucene.util.BytesRef;
+import org.hibernate.search.engine.backend.document.model.Sortable;
+import org.hibernate.search.backend.lucene.document.impl.LuceneDocumentBuilder;
+import org.hibernate.search.backend.lucene.document.model.impl.LuceneFieldFormatter;
+import org.hibernate.search.backend.lucene.document.model.impl.LuceneIndexSchemaObjectNode;
+import org.hibernate.search.backend.lucene.util.impl.AnalyzerUtils;
+
+public final class StringFieldFormatter implements LuceneFieldFormatter<String> {
+
+	private final Sortable sortable;
+
+	private final FieldType fieldType;
+
+	private final Analyzer normalizer;
+
+	public StringFieldFormatter(Sortable sortable, FieldType fieldType, Analyzer normalizer) {
+		this.sortable = sortable;
+		this.fieldType = fieldType;
+		this.normalizer = normalizer;
+	}
+
+	@Override
+	public void addFields(LuceneDocumentBuilder documentBuilder, LuceneIndexSchemaObjectNode parentNode, String fieldName, String value) {
+		if ( value == null ) {
+			return;
+		}
+
+		documentBuilder.addField( parentNode, new Field( fieldName, value, fieldType ) );
+
+		if ( Sortable.YES.equals( sortable ) ) {
+			documentBuilder.addField( parentNode, new SortedDocValuesField(
+					fieldName,
+					new BytesRef( normalizer != null ? AnalyzerUtils.analyzeSortableValue( normalizer, fieldName, value ) : value )
+			) );
+		}
+	}
+
+	@Override
+	public Object format(Object value) {
+		return value;
+	}
+
+	@Override
+	public String parse(Document document, String fieldName) {
+		return document.get( fieldName );
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if ( this == obj ) {
+			return true;
+		}
+		if ( obj == null ) {
+			return false;
+		}
+		if ( StringFieldFormatter.class != obj.getClass() ) {
+			return false;
+		}
+
+		StringFieldFormatter other = (StringFieldFormatter) obj;
+
+		return Objects.equals( sortable, other.sortable ) &&
+				Objects.equals( fieldType, other.fieldType ) &&
+				Objects.equals( normalizer, other.normalizer );
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash( sortable, fieldType, normalizer );
+	}
+}

backend/lucene/src/main/java/org/hibernate/search/backend/lucene/types/predicate/impl/StringFieldQueryFactory.java
@@ -1,0 +1,99 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.backend.lucene.types.predicate.impl;
+
+import java.util.Objects;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.search.TermRangeQuery;
+import org.apache.lucene.util.QueryBuilder;
+import org.hibernate.search.backend.lucene.document.model.impl.LuceneFieldQueryFactory;
+import org.hibernate.search.backend.lucene.document.model.impl.MatchQueryOptions;
+import org.hibernate.search.backend.lucene.document.model.impl.RangeQueryOptions;
+import org.hibernate.search.backend.lucene.types.formatter.impl.StringFieldFormatter;
+import org.hibernate.search.backend.lucene.util.impl.AnalyzerUtils;
+
+public class StringFieldQueryFactory implements LuceneFieldQueryFactory {
+
+	private final Analyzer analyzerOrNormalizer;
+
+	private final boolean tokenized;
+
+	private final QueryBuilder queryBuilder;
+
+	public StringFieldQueryFactory(Analyzer analyzerOrNormalizer, boolean tokenized, QueryBuilder queryBuilder) {
+		this.analyzerOrNormalizer = analyzerOrNormalizer;
+		this.tokenized = tokenized;
+		this.queryBuilder = queryBuilder;
+	}
+
+	@Override
+	public Query createMatchQuery(String fieldName, Object value, MatchQueryOptions matchQueryOptions) {
+		String stringValue = (String) value;
+
+		if ( queryBuilder != null ) {
+			return queryBuilder.createBooleanQuery( fieldName, stringValue, matchQueryOptions.getOperator() );
+		}
+		else {
+			// we are in the case where we a have a normalizer here as the analyzer case has already been treated by
+			// the queryBuilder case above
+
+			return new TermQuery( new Term( fieldName, getAnalyzedValue( analyzerOrNormalizer, fieldName, stringValue ) ) );
+		}
+	}
+
+	@Override
+	public Query createRangeQuery(String fieldName, Object lowerLimit, Object upperLimit, RangeQueryOptions rangeQueryOptions) {
+		// Note that a range query only makes sense if only one token is returned by the analyzer
+		// and we should even consider forcing having a normalizer here, instead of supporting
+		// range queries on analyzed fields.
+
+		return TermRangeQuery.newStringRange(
+				fieldName,
+				getAnalyzedValue( analyzerOrNormalizer, fieldName, (String) lowerLimit ),
+				getAnalyzedValue( analyzerOrNormalizer, fieldName, (String) upperLimit ),
+				// we force the true value if the limit is null because of some Lucene checks down the hill
+				lowerLimit == null ? true : !rangeQueryOptions.isExcludeLowerLimit(),
+				upperLimit == null ? true : !rangeQueryOptions.isExcludeUpperLimit()
+		);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if ( this == obj ) {
+			return true;
+		}
+		if ( obj == null ) {
+			return false;
+		}
+		if ( StringFieldFormatter.class != obj.getClass() ) {
+			return false;
+		}
+
+		StringFieldQueryFactory other = (StringFieldQueryFactory) obj;
+
+		return Objects.equals( analyzerOrNormalizer, other.analyzerOrNormalizer ) &&
+				tokenized == other.tokenized &&
+				Objects.equals( queryBuilder, other.queryBuilder );
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash( analyzerOrNormalizer, tokenized, queryBuilder );
+	}
+
+	private static String getAnalyzedValue(Analyzer analyzerOrNormalizer, String fieldName, String stringValue) {
+		if ( analyzerOrNormalizer == null ) {
+			return stringValue;
+		}
+
+		return AnalyzerUtils.analyzeSortableValue( analyzerOrNormalizer, fieldName, stringValue );
+	}
+}