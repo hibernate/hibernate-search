hibernate/hibernate-search-6-poc

HSEARCH-3059 Test multi-index searches

integrationtest/backend-lucene/src/test/java/org/hibernate/search/v6poc/integrationtest/backend/lucene/search/LuceneSearchMultiIndexIT.java
@@ -1,0 +1,182 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.v6poc.integrationtest.backend.lucene.search;
+
+import static org.hibernate.search.v6poc.util.impl.integrationtest.common.assertion.DocumentReferencesSearchResultAssert.assertThat;
+import static org.hibernate.search.v6poc.util.impl.integrationtest.common.stub.mapper.StubMapperUtils.referenceProvider;
+
+import org.hibernate.search.v6poc.backend.document.DocumentElement;
+import org.hibernate.search.v6poc.backend.document.IndexFieldAccessor;
+import org.hibernate.search.v6poc.backend.document.model.dsl.IndexSchemaElement;
+import org.hibernate.search.v6poc.backend.document.model.dsl.Sortable;
+import org.hibernate.search.v6poc.backend.document.model.dsl.Store;
+import org.hibernate.search.v6poc.backend.index.spi.ChangesetIndexWorker;
+import org.hibernate.search.v6poc.backend.index.spi.IndexManager;
+import org.hibernate.search.v6poc.backend.index.spi.IndexSearchTarget;
+import org.hibernate.search.v6poc.backend.index.spi.IndexSearchTargetBuilder;
+import org.hibernate.search.v6poc.engine.spi.SessionContext;
+import org.hibernate.search.v6poc.integrationtest.backend.tck.search.SearchMultiIndexIT;
+import org.hibernate.search.v6poc.integrationtest.backend.tck.util.rule.SearchSetupHelper;
+import org.hibernate.search.v6poc.search.DocumentReference;
+import org.hibernate.search.v6poc.search.SearchQuery;
+import org.hibernate.search.v6poc.util.impl.integrationtest.common.assertion.DocumentReferencesSearchResultAssert;
+import org.hibernate.search.v6poc.util.impl.integrationtest.common.stub.StubSessionContext;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * This is an extension of the backend TCK test {@link SearchMultiIndexIT}.
+ */
+public class LuceneSearchMultiIndexIT {
+
+	private static final String BACKEND_1 = "backend_1";
+
+	private static final String STRING_1 = "string_1";
+	private static final String STRING_2 = "string_2";
+
+	// Backend 1 / Index 1
+
+	private static final String DOCUMENT_1_1_1 = "1_1_1";
+	private static final String ADDITIONAL_FIELD_1_1_1 = "additional_field_1_1_1";
+
+	private static final String DOCUMENT_1_1_2 = "1_1_2";
+	private static final String ADDITIONAL_FIELD_1_1_2 = "additional_field_1_1_2";
+
+	// Backend 1 / Index 2
+
+	private static final String DOCUMENT_1_2_1 = "1_2_1";
+
+	@Rule
+	public SearchSetupHelper setupHelper = new SearchSetupHelper();
+
+	// Backend 1 / Index 1
+
+	private IndexAccessors_1_1 indexAccessors_1_1;
+	private IndexManager<?> indexManager_1_1;
+	private String indexName_1_1;
+
+	// Backend 1 / Index 2
+
+	private IndexAccessors_1_2 indexAccessors_1_2;
+	private IndexManager<?> indexManager_1_2;
+	private String indexName_1_2;
+
+	private SessionContext sessionContext = new StubSessionContext();
+
+	@Before
+	public void setup() {
+		setupHelper.withDefaultConfiguration( BACKEND_1 )
+				.withIndex(
+						"MappedType_1_1", "IndexName_1_1",
+						ctx -> this.indexAccessors_1_1 = new IndexAccessors_1_1( ctx.getSchemaElement() ),
+						(indexManager, indexName) -> {
+							this.indexManager_1_1 = indexManager;
+							this.indexName_1_1 = indexName;
+						}
+				)
+				.withIndex(
+						"MappedType_1_2", "IndexName_1_2",
+						ctx -> this.indexAccessors_1_2 = new IndexAccessors_1_2( ctx.getSchemaElement() ),
+						(indexManager, indexName) -> {
+							this.indexManager_1_2 = indexManager;
+							this.indexName_1_2 = indexName;
+						}
+				)
+				.setup();
+
+		initData();
+	}
+
+	@Test
+	public void field_in_one_index_only_is_supported_for_sorting() {
+		IndexSearchTargetBuilder searchTargetBuilder = indexManager_1_1.createSearchTarget();
+		indexManager_1_2.addToSearchTarget( searchTargetBuilder );
+		IndexSearchTarget searchTarget = searchTargetBuilder.build();
+
+		SearchQuery<DocumentReference> query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().all().end()
+				.sort().byField( "additionalField" ).asc().onMissingValue().sortLast().end()
+				.build();
+
+		DocumentReferencesSearchResultAssert.assertThat( query ).hasReferencesHitsExactOrder( c -> {
+			c.doc( indexName_1_1, DOCUMENT_1_1_1, DOCUMENT_1_1_2 );
+			c.doc( indexName_1_2, DOCUMENT_1_2_1 );
+		});
+
+		query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().all().end()
+				.sort().byField( "additionalField" ).desc().onMissingValue().sortLast().end()
+				.build();
+
+		DocumentReferencesSearchResultAssert.assertThat( query ).hasReferencesHitsExactOrder( c -> {
+			c.doc( indexName_1_1, DOCUMENT_1_1_2, DOCUMENT_1_1_1 );
+			c.doc( indexName_1_2, DOCUMENT_1_2_1 );
+		});
+	}
+
+	private void initData() {
+		// Backend 1 / Index 1
+
+		ChangesetIndexWorker<? extends DocumentElement> worker = indexManager_1_1.createWorker( sessionContext );
+
+		worker.add( referenceProvider( DOCUMENT_1_1_1 ), document -> {
+			indexAccessors_1_1.string.write( document, STRING_1 );
+			indexAccessors_1_1.additionalField.write( document, ADDITIONAL_FIELD_1_1_1 );
+		} );
+		worker.add( referenceProvider( DOCUMENT_1_1_2 ), document -> {
+			indexAccessors_1_1.string.write( document, STRING_2 );
+			indexAccessors_1_1.additionalField.write( document, ADDITIONAL_FIELD_1_1_2 );
+		} );
+
+		worker.execute().join();
+
+		IndexSearchTarget searchTarget = indexManager_1_1.createSearchTarget().build();
+		SearchQuery<DocumentReference> query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().all().end()
+				.build();
+		assertThat( query ).hasReferencesHitsAnyOrder( indexName_1_1, DOCUMENT_1_1_1, DOCUMENT_1_1_2 );
+
+		// Backend 1 / Index 2
+
+		worker = indexManager_1_2.createWorker( sessionContext );
+
+		worker.add( referenceProvider( DOCUMENT_1_2_1 ), document -> {
+			indexAccessors_1_2.string.write( document, STRING_1 );
+		} );
+
+		worker.execute().join();
+
+		searchTarget = indexManager_1_2.createSearchTarget().build();
+		query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().all().end()
+				.build();
+		assertThat( query ).hasReferencesHitsAnyOrder( indexName_1_2, DOCUMENT_1_2_1 );
+	}
+
+	private static class IndexAccessors_1_1 {
+		final IndexFieldAccessor<String> string;
+		final IndexFieldAccessor<String> additionalField;
+
+		IndexAccessors_1_1(IndexSchemaElement root) {
+			string = root.field( "string" ).asString().createAccessor();
+			additionalField = root.field( "additionalField" ).asString().sortable( Sortable.YES ).store( Store.YES ).createAccessor();
+		}
+	}
+
+	private static class IndexAccessors_1_2 {
+		final IndexFieldAccessor<String> string;
+
+		IndexAccessors_1_2(IndexSchemaElement root) {
+			string = root.field( "string" ).asString().createAccessor();
+		}
+	}
+}

hibernate/hibernate-search

integrationtest/backend/lucene/src/test/java/org/hibernate/search/integrationtest/backend/lucene/search/LuceneSearchMultiIndexIT.java
@@ -1,0 +1,182 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.integrationtest.backend.lucene.search;
+
+import static org.hibernate.search.util.impl.integrationtest.common.assertion.DocumentReferencesSearchResultAssert.assertThat;
+import static org.hibernate.search.util.impl.integrationtest.common.stub.mapper.StubMapperUtils.referenceProvider;
+
+import org.hibernate.search.engine.backend.document.DocumentElement;
+import org.hibernate.search.engine.backend.document.IndexFieldAccessor;
+import org.hibernate.search.engine.backend.document.model.dsl.IndexSchemaElement;
+import org.hibernate.search.engine.backend.document.model.dsl.Sortable;
+import org.hibernate.search.engine.backend.document.model.dsl.Store;
+import org.hibernate.search.engine.backend.index.spi.ChangesetIndexWorker;
+import org.hibernate.search.engine.backend.index.spi.IndexManager;
+import org.hibernate.search.engine.backend.index.spi.IndexSearchTarget;
+import org.hibernate.search.engine.backend.index.spi.IndexSearchTargetBuilder;
+import org.hibernate.search.engine.common.spi.SessionContext;
+import org.hibernate.search.integrationtest.backend.tck.search.SearchMultiIndexIT;
+import org.hibernate.search.integrationtest.backend.tck.util.rule.SearchSetupHelper;
+import org.hibernate.search.engine.search.DocumentReference;
+import org.hibernate.search.engine.search.SearchQuery;
+import org.hibernate.search.util.impl.integrationtest.common.assertion.DocumentReferencesSearchResultAssert;
+import org.hibernate.search.util.impl.integrationtest.common.stub.StubSessionContext;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * This is an extension of the backend TCK test {@link SearchMultiIndexIT}.
+ */
+public class LuceneSearchMultiIndexIT {
+
+	private static final String BACKEND_1 = "backend_1";
+
+	private static final String STRING_1 = "string_1";
+	private static final String STRING_2 = "string_2";
+
+	// Backend 1 / Index 1
+
+	private static final String DOCUMENT_1_1_1 = "1_1_1";
+	private static final String ADDITIONAL_FIELD_1_1_1 = "additional_field_1_1_1";
+
+	private static final String DOCUMENT_1_1_2 = "1_1_2";
+	private static final String ADDITIONAL_FIELD_1_1_2 = "additional_field_1_1_2";
+
+	// Backend 1 / Index 2
+
+	private static final String DOCUMENT_1_2_1 = "1_2_1";
+
+	@Rule
+	public SearchSetupHelper setupHelper = new SearchSetupHelper();
+
+	// Backend 1 / Index 1
+
+	private IndexAccessors_1_1 indexAccessors_1_1;
+	private IndexManager<?> indexManager_1_1;
+	private String indexName_1_1;
+
+	// Backend 1 / Index 2
+
+	private IndexAccessors_1_2 indexAccessors_1_2;
+	private IndexManager<?> indexManager_1_2;
+	private String indexName_1_2;
+
+	private SessionContext sessionContext = new StubSessionContext();
+
+	@Before
+	public void setup() {
+		setupHelper.withDefaultConfiguration( BACKEND_1 )
+				.withIndex(
+						"MappedType_1_1", "IndexName_1_1",
+						ctx -> this.indexAccessors_1_1 = new IndexAccessors_1_1( ctx.getSchemaElement() ),
+						(indexManager, indexName) -> {
+							this.indexManager_1_1 = indexManager;
+							this.indexName_1_1 = indexName;
+						}
+				)
+				.withIndex(
+						"MappedType_1_2", "IndexName_1_2",
+						ctx -> this.indexAccessors_1_2 = new IndexAccessors_1_2( ctx.getSchemaElement() ),
+						(indexManager, indexName) -> {
+							this.indexManager_1_2 = indexManager;
+							this.indexName_1_2 = indexName;
+						}
+				)
+				.setup();
+
+		initData();
+	}
+
+	@Test
+	public void field_in_one_index_only_is_supported_for_sorting() {
+		IndexSearchTargetBuilder searchTargetBuilder = indexManager_1_1.createSearchTarget();
+		indexManager_1_2.addToSearchTarget( searchTargetBuilder );
+		IndexSearchTarget searchTarget = searchTargetBuilder.build();
+
+		SearchQuery<DocumentReference> query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().all().end()
+				.sort().byField( "additionalField" ).asc().onMissingValue().sortLast().end()
+				.build();
+
+		DocumentReferencesSearchResultAssert.assertThat( query ).hasReferencesHitsExactOrder( c -> {
+			c.doc( indexName_1_1, DOCUMENT_1_1_1, DOCUMENT_1_1_2 );
+			c.doc( indexName_1_2, DOCUMENT_1_2_1 );
+		});
+
+		query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().all().end()
+				.sort().byField( "additionalField" ).desc().onMissingValue().sortLast().end()
+				.build();
+
+		DocumentReferencesSearchResultAssert.assertThat( query ).hasReferencesHitsExactOrder( c -> {
+			c.doc( indexName_1_1, DOCUMENT_1_1_2, DOCUMENT_1_1_1 );
+			c.doc( indexName_1_2, DOCUMENT_1_2_1 );
+		});
+	}
+
+	private void initData() {
+		// Backend 1 / Index 1
+
+		ChangesetIndexWorker<? extends DocumentElement> worker = indexManager_1_1.createWorker( sessionContext );
+
+		worker.add( referenceProvider( DOCUMENT_1_1_1 ), document -> {
+			indexAccessors_1_1.string.write( document, STRING_1 );
+			indexAccessors_1_1.additionalField.write( document, ADDITIONAL_FIELD_1_1_1 );
+		} );
+		worker.add( referenceProvider( DOCUMENT_1_1_2 ), document -> {
+			indexAccessors_1_1.string.write( document, STRING_2 );
+			indexAccessors_1_1.additionalField.write( document, ADDITIONAL_FIELD_1_1_2 );
+		} );
+
+		worker.execute().join();
+
+		IndexSearchTarget searchTarget = indexManager_1_1.createSearchTarget().build();
+		SearchQuery<DocumentReference> query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().all().end()
+				.build();
+		assertThat( query ).hasReferencesHitsAnyOrder( indexName_1_1, DOCUMENT_1_1_1, DOCUMENT_1_1_2 );
+
+		// Backend 1 / Index 2
+
+		worker = indexManager_1_2.createWorker( sessionContext );
+
+		worker.add( referenceProvider( DOCUMENT_1_2_1 ), document -> {
+			indexAccessors_1_2.string.write( document, STRING_1 );
+		} );
+
+		worker.execute().join();
+
+		searchTarget = indexManager_1_2.createSearchTarget().build();
+		query = searchTarget.query( sessionContext )
+				.asReferences()
+				.predicate().all().end()
+				.build();
+		assertThat( query ).hasReferencesHitsAnyOrder( indexName_1_2, DOCUMENT_1_2_1 );
+	}
+
+	private static class IndexAccessors_1_1 {
+		final IndexFieldAccessor<String> string;
+		final IndexFieldAccessor<String> additionalField;
+
+		IndexAccessors_1_1(IndexSchemaElement root) {
+			string = root.field( "string" ).asString().createAccessor();
+			additionalField = root.field( "additionalField" ).asString().sortable( Sortable.YES ).store( Store.YES ).createAccessor();
+		}
+	}
+
+	private static class IndexAccessors_1_2 {
+		final IndexFieldAccessor<String> string;
+
+		IndexAccessors_1_2(IndexSchemaElement root) {
+			string = root.field( "string" ).asString().createAccessor();
+		}
+	}
+}