hibernate/hibernate-search-6-poc

HSEARCH-3065 Use the context passed for nested queries
In the case of Elasticsearch, we don't have a context but we have to
adapt a few classes to the new way of doing things.

engine/src/main/java/org/hibernate/search/v6poc/search/dsl/predicate/impl/MatchAllPredicateContextImpl.java
@@ -6,8 +6,6 @@
  */
 package org.hibernate.search.v6poc.search.dsl.predicate.impl;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
@@ -15,8 +13,8 @@ import org.hibernate.search.v6poc.search.SearchPredicate;
 import org.hibernate.search.v6poc.search.dsl.predicate.MatchAllPredicateContext;
 import org.hibernate.search.v6poc.search.dsl.predicate.SearchPredicateContainerContext;
 import org.hibernate.search.v6poc.search.dsl.predicate.spi.SearchPredicateDslContext;
-import org.hibernate.search.v6poc.search.predicate.spi.MatchAllPredicateBuilder;
 import org.hibernate.search.v6poc.search.predicate.spi.BooleanJunctionPredicateBuilder;
+import org.hibernate.search.v6poc.search.predicate.spi.MatchAllPredicateBuilder;
 import org.hibernate.search.v6poc.search.predicate.spi.SearchPredicateContributor;
 import org.hibernate.search.v6poc.search.predicate.spi.SearchPredicateFactory;
 
@@ -64,8 +62,8 @@ class MatchAllPredicateContextImpl<N, CTX, C>
 	public void contribute(CTX context, C collector) {
 		if ( exceptContext != null ) {
 			BooleanJunctionPredicateBuilder<CTX, C> booleanBuilder = factory.bool();
-			builder.contribute( context, booleanBuilder.getMustCollector() );
-			exceptContext.contribute( context, booleanBuilder.getMustNotCollector() );
+			booleanBuilder.getMustCollector().accept( builder );
+			booleanBuilder.getMustNotCollector().accept( exceptContext.child );
 			booleanBuilder.contribute( context, collector );
 		}
 		else {
@@ -85,12 +83,11 @@ class MatchAllPredicateContextImpl<N, CTX, C>
 		return exceptContext;
 	}
 
-	private class MatchAllExceptContext implements SearchPredicateDslContext<MatchAllPredicateContext<N>, CTX, C>,
-			SearchPredicateContributor<CTX, C> {
-
-		private final List<SearchPredicateContributor<CTX, ? super C>> children = new ArrayList<>();
+	private class MatchAllExceptContext implements SearchPredicateDslContext<MatchAllPredicateContext<N>, CTX, C> {
 
 		private final SearchPredicateContainerContextImpl<MatchAllPredicateContext<N>, CTX, C> containerContext;
+
+		private SearchPredicateContributor<CTX, ? super C> child;
 
 		MatchAllExceptContext() {
 			this.containerContext = new SearchPredicateContainerContextImpl<>(
@@ -99,18 +96,12 @@ class MatchAllPredicateContextImpl<N, CTX, C>
 
 		@Override
 		public void addContributor(SearchPredicateContributor<CTX, ? super C> child) {
-			children.add( child );
+			this.child = child;
 		}
 
 		@Override
 		public MatchAllPredicateContext<N> getNextContext() {
 			return MatchAllPredicateContextImpl.this;
 		}
-
-		@Override
-		public void contribute(CTX context, C collector) {
-			children.forEach( c -> c.contribute( context, collector ) );
-		}
-
 	}
 }

util/internal/integrationtest/common/src/main/java/org/hibernate/search/v6poc/util/impl/integrationtest/common/stub/backend/search/predicate/impl/StubPredicateBuilder.java
@@ -6,11 +6,14 @@
  */
 package org.hibernate.search.v6poc.util.impl.integrationtest.common.stub.backend.search.predicate.impl;
 
+import java.util.function.Consumer;
+
 import org.hibernate.search.v6poc.search.predicate.spi.BooleanJunctionPredicateBuilder;
 import org.hibernate.search.v6poc.search.predicate.spi.MatchAllPredicateBuilder;
 import org.hibernate.search.v6poc.search.predicate.spi.MatchPredicateBuilder;
 import org.hibernate.search.v6poc.search.predicate.spi.NestedPredicateBuilder;
 import org.hibernate.search.v6poc.search.predicate.spi.RangePredicateBuilder;
+import org.hibernate.search.v6poc.search.predicate.spi.SearchPredicateContributor;
 import org.hibernate.search.v6poc.search.predicate.spi.SpatialWithinBoundingBoxPredicateBuilder;
 import org.hibernate.search.v6poc.search.predicate.spi.SpatialWithinCirclePredicateBuilder;
 import org.hibernate.search.v6poc.search.predicate.spi.SpatialWithinPolygonPredicateBuilder;
@@ -30,23 +33,23 @@ public class StubPredicateBuilder implements MatchAllPredicateBuilder<Void, Stub
 		SpatialWithinBoundingBoxPredicateBuilder<Void, StubQueryElementCollector> {
 
 	@Override
-	public StubQueryElementCollector getMustCollector() {
-		return StubQueryElementCollector.get();
+	public Consumer<SearchPredicateContributor<Void, ? super StubQueryElementCollector>> getMustCollector() {
+		return this::consumeBooleanContributor;
 	}
 
 	@Override
-	public StubQueryElementCollector getMustNotCollector() {
-		return StubQueryElementCollector.get();
+	public Consumer<SearchPredicateContributor<Void, ? super StubQueryElementCollector>> getMustNotCollector() {
+		return this::consumeBooleanContributor;
 	}
 
 	@Override
-	public StubQueryElementCollector getShouldCollector() {
-		return StubQueryElementCollector.get();
+	public Consumer<SearchPredicateContributor<Void, ? super StubQueryElementCollector>> getShouldCollector() {
+		return this::consumeBooleanContributor;
 	}
 
 	@Override
-	public StubQueryElementCollector getFilterCollector() {
-		return StubQueryElementCollector.get();
+	public Consumer<SearchPredicateContributor<Void, ? super StubQueryElementCollector>> getFilterCollector() {
+		return this::consumeBooleanContributor;
 	}
 
 	@Override
@@ -105,12 +108,18 @@ public class StubPredicateBuilder implements MatchAllPredicateBuilder<Void, Stub
 	}
 
 	@Override
-	public StubQueryElementCollector getNestedCollector() {
-		return StubQueryElementCollector.get();
+	public Consumer<SearchPredicateContributor<Void, ? super StubQueryElementCollector>> getNestedCollector() {
+		return this::consumeNestedContributor;
 	}
 
 	@Override
 	public void contribute(Void context, StubQueryElementCollector collector) {
 		collector.simulateCollectCall();
+	}
+
+	private void consumeNestedContributor(SearchPredicateContributor<Void, ? super StubQueryElementCollector> nestedContributor) {
+	}
+
+	private void consumeBooleanContributor(SearchPredicateContributor<Void, ? super StubQueryElementCollector> booleanContributor) {
 	}
 }

hibernate/hibernate-search

engine/src/main/java/org/hibernate/search/engine/search/dsl/predicate/impl/MatchAllPredicateContextImpl.java
@@ -6,8 +6,6 @@
  */
 package org.hibernate.search.engine.search.dsl.predicate.impl;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
@@ -15,8 +13,8 @@ import org.hibernate.search.engine.search.SearchPredicate;
 import org.hibernate.search.engine.search.dsl.predicate.MatchAllPredicateContext;
 import org.hibernate.search.engine.search.dsl.predicate.SearchPredicateContainerContext;
 import org.hibernate.search.engine.search.dsl.predicate.spi.SearchPredicateDslContext;
-import org.hibernate.search.engine.search.predicate.spi.MatchAllPredicateBuilder;
 import org.hibernate.search.engine.search.predicate.spi.BooleanJunctionPredicateBuilder;
+import org.hibernate.search.engine.search.predicate.spi.MatchAllPredicateBuilder;
 import org.hibernate.search.engine.search.predicate.spi.SearchPredicateContributor;
 import org.hibernate.search.engine.search.predicate.spi.SearchPredicateFactory;
 
@@ -64,8 +62,8 @@ class MatchAllPredicateContextImpl<N, CTX, C>
 	public void contribute(CTX context, C collector) {
 		if ( exceptContext != null ) {
 			BooleanJunctionPredicateBuilder<CTX, C> booleanBuilder = factory.bool();
-			builder.contribute( context, booleanBuilder.getMustCollector() );
-			exceptContext.contribute( context, booleanBuilder.getMustNotCollector() );
+			booleanBuilder.getMustCollector().accept( builder );
+			booleanBuilder.getMustNotCollector().accept( exceptContext.child );
 			booleanBuilder.contribute( context, collector );
 		}
 		else {
@@ -85,12 +83,11 @@ class MatchAllPredicateContextImpl<N, CTX, C>
 		return exceptContext;
 	}
 
-	private class MatchAllExceptContext implements SearchPredicateDslContext<MatchAllPredicateContext<N>, CTX, C>,
-			SearchPredicateContributor<CTX, C> {
-
-		private final List<SearchPredicateContributor<CTX, ? super C>> children = new ArrayList<>();
+	private class MatchAllExceptContext implements SearchPredicateDslContext<MatchAllPredicateContext<N>, CTX, C> {
 
 		private final SearchPredicateContainerContextImpl<MatchAllPredicateContext<N>, CTX, C> containerContext;
+
+		private SearchPredicateContributor<CTX, ? super C> child;
 
 		MatchAllExceptContext() {
 			this.containerContext = new SearchPredicateContainerContextImpl<>(
@@ -99,18 +96,12 @@ class MatchAllPredicateContextImpl<N, CTX, C>
 
 		@Override
 		public void addContributor(SearchPredicateContributor<CTX, ? super C> child) {
-			children.add( child );
+			this.child = child;
 		}
 
 		@Override
 		public MatchAllPredicateContext<N> getNextContext() {
 			return MatchAllPredicateContextImpl.this;
 		}
-
-		@Override
-		public void contribute(CTX context, C collector) {
-			children.forEach( c -> c.contribute( context, collector ) );
-		}
-
 	}
 }

util/internal/integrationtest/common/src/main/java/org/hibernate/search/util/impl/integrationtest/common/stub/backend/search/predicate/impl/StubPredicateBuilder.java
@@ -6,11 +6,14 @@
  */
 package org.hibernate.search.util.impl.integrationtest.common.stub.backend.search.predicate.impl;
 
+import java.util.function.Consumer;
+
 import org.hibernate.search.engine.search.predicate.spi.BooleanJunctionPredicateBuilder;
 import org.hibernate.search.engine.search.predicate.spi.MatchAllPredicateBuilder;
 import org.hibernate.search.engine.search.predicate.spi.MatchPredicateBuilder;
 import org.hibernate.search.engine.search.predicate.spi.NestedPredicateBuilder;
 import org.hibernate.search.engine.search.predicate.spi.RangePredicateBuilder;
+import org.hibernate.search.engine.search.predicate.spi.SearchPredicateContributor;
 import org.hibernate.search.engine.search.predicate.spi.SpatialWithinBoundingBoxPredicateBuilder;
 import org.hibernate.search.engine.search.predicate.spi.SpatialWithinCirclePredicateBuilder;
 import org.hibernate.search.engine.search.predicate.spi.SpatialWithinPolygonPredicateBuilder;
@@ -30,23 +33,23 @@ public class StubPredicateBuilder implements MatchAllPredicateBuilder<Void, Stub
 		SpatialWithinBoundingBoxPredicateBuilder<Void, StubQueryElementCollector> {
 
 	@Override
-	public StubQueryElementCollector getMustCollector() {
-		return StubQueryElementCollector.get();
+	public Consumer<SearchPredicateContributor<Void, ? super StubQueryElementCollector>> getMustCollector() {
+		return this::consumeBooleanContributor;
 	}
 
 	@Override
-	public StubQueryElementCollector getMustNotCollector() {
-		return StubQueryElementCollector.get();
+	public Consumer<SearchPredicateContributor<Void, ? super StubQueryElementCollector>> getMustNotCollector() {
+		return this::consumeBooleanContributor;
 	}
 
 	@Override
-	public StubQueryElementCollector getShouldCollector() {
-		return StubQueryElementCollector.get();
+	public Consumer<SearchPredicateContributor<Void, ? super StubQueryElementCollector>> getShouldCollector() {
+		return this::consumeBooleanContributor;
 	}
 
 	@Override
-	public StubQueryElementCollector getFilterCollector() {
-		return StubQueryElementCollector.get();
+	public Consumer<SearchPredicateContributor<Void, ? super StubQueryElementCollector>> getFilterCollector() {
+		return this::consumeBooleanContributor;
 	}
 
 	@Override
@@ -105,12 +108,18 @@ public class StubPredicateBuilder implements MatchAllPredicateBuilder<Void, Stub
 	}
 
 	@Override
-	public StubQueryElementCollector getNestedCollector() {
-		return StubQueryElementCollector.get();
+	public Consumer<SearchPredicateContributor<Void, ? super StubQueryElementCollector>> getNestedCollector() {
+		return this::consumeNestedContributor;
 	}
 
 	@Override
 	public void contribute(Void context, StubQueryElementCollector collector) {
 		collector.simulateCollectCall();
+	}
+
+	private void consumeNestedContributor(SearchPredicateContributor<Void, ? super StubQueryElementCollector> nestedContributor) {
+	}
+
+	private void consumeBooleanContributor(SearchPredicateContributor<Void, ? super StubQueryElementCollector> booleanContributor) {
 	}
 }