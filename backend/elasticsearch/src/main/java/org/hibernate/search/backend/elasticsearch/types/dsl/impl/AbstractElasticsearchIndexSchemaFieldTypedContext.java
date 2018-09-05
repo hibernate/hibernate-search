yrodiere/hibernate-search-6-poc

HSEARCH-3221 Check calls to createAccessor() during bootstrap
... because we're about to add multiple createAccessor() methods, and
misuse will become more likely.

And move index field definition code to helpers, to avoid code
duplication.

backend-elasticsearch/src/main/java/org/hibernate/search/v6poc/backend/elasticsearch/types/dsl/impl/AbstractElasticsearchIndexSchemaFieldTypedContext.java
@@ -7,8 +7,8 @@
 package org.hibernate.search.v6poc.backend.elasticsearch.types.dsl.impl;
 
 import org.hibernate.search.v6poc.backend.document.model.dsl.spi.IndexSchemaContext;
-import org.hibernate.search.v6poc.backend.document.spi.DeferredInitializationIndexFieldAccessor;
 import org.hibernate.search.v6poc.backend.document.IndexFieldAccessor;
+import org.hibernate.search.v6poc.backend.document.spi.IndexSchemaFieldDefinitionHelper;
 import org.hibernate.search.v6poc.backend.elasticsearch.document.model.dsl.ElasticsearchIndexSchemaFieldTypedContext;
 import org.hibernate.search.v6poc.backend.elasticsearch.document.model.impl.ElasticsearchIndexSchemaNodeCollector;
 import org.hibernate.search.v6poc.backend.elasticsearch.document.model.impl.ElasticsearchIndexSchemaNodeContributor;
@@ -22,27 +22,29 @@ public abstract class AbstractElasticsearchIndexSchemaFieldTypedContext<F>
 		implements ElasticsearchIndexSchemaFieldTypedContext<F>,
 		ElasticsearchIndexSchemaNodeContributor<PropertyMapping> {
 
-	protected final IndexSchemaContext schemaContext;
-
-	private final DeferredInitializationIndexFieldAccessor<F> accessor = new DeferredInitializationIndexFieldAccessor<>();
+	private final IndexSchemaFieldDefinitionHelper<F> helper;
 
 	AbstractElasticsearchIndexSchemaFieldTypedContext(IndexSchemaContext schemaContext) {
-		this.schemaContext = schemaContext;
+		this.helper = new IndexSchemaFieldDefinitionHelper<>( schemaContext );
 	}
 
 	@Override
 	public IndexFieldAccessor<F> createAccessor() {
-		return accessor;
+		return helper.createAccessor();
 	}
 
 	@Override
 	public PropertyMapping contribute(ElasticsearchIndexSchemaNodeCollector collector,
 			ElasticsearchIndexSchemaObjectNode parentNode) {
-		return contribute( accessor, collector, parentNode );
+		return contribute( helper, collector, parentNode );
 	}
 
-	protected abstract PropertyMapping contribute(DeferredInitializationIndexFieldAccessor<F> reference,
+	protected abstract PropertyMapping contribute(IndexSchemaFieldDefinitionHelper<F> helper,
 			ElasticsearchIndexSchemaNodeCollector collector,
 			ElasticsearchIndexSchemaObjectNode parentNode);
+
+	protected final IndexSchemaContext getSchemaContext() {
+		return helper.getSchemaContext();
+	}
 
 }

backend-elasticsearch/src/main/java/org/hibernate/search/v6poc/backend/elasticsearch/types/dsl/impl/AbstractScalarFieldTypedContext.java
@@ -9,10 +9,10 @@ package org.hibernate.search.v6poc.backend.elasticsearch.types.dsl.impl;
 import java.lang.invoke.MethodHandles;
 
 import org.hibernate.search.v6poc.backend.document.model.dsl.spi.IndexSchemaContext;
-import org.hibernate.search.v6poc.backend.document.spi.DeferredInitializationIndexFieldAccessor;
 import org.hibernate.search.v6poc.backend.document.model.dsl.Sortable;
 import org.hibernate.search.v6poc.backend.document.model.dsl.Store;
 import org.hibernate.search.v6poc.backend.document.model.dsl.IndexSchemaFieldTypedContext;
+import org.hibernate.search.v6poc.backend.document.spi.IndexSchemaFieldDefinitionHelper;
 import org.hibernate.search.v6poc.backend.elasticsearch.document.model.impl.ElasticsearchIndexSchemaNodeCollector;
 import org.hibernate.search.v6poc.backend.elasticsearch.document.model.impl.ElasticsearchIndexSchemaObjectNode;
 import org.hibernate.search.v6poc.backend.elasticsearch.document.model.impl.esnative.DataType;
@@ -29,8 +29,6 @@ abstract class AbstractScalarFieldTypedContext<F> extends AbstractElasticsearchI
 
 	private final String relativeFieldName;
 	private final DataType dataType;
-	private String analyzerName;
-	private String normalizerName;
 	private Store store = Store.DEFAULT;
 	private Sortable sortable = Sortable.DEFAULT;
 
@@ -43,14 +41,16 @@ abstract class AbstractScalarFieldTypedContext<F> extends AbstractElasticsearchI
 
 	@Override
 	public IndexSchemaFieldTypedContext<F> analyzer(String analyzerName) {
-		this.analyzerName = analyzerName;
-		return this;
+		throw log.cannotUseAnalyzerOnFieldType(
+				relativeFieldName, dataType, getSchemaContext().getEventContext()
+		);
 	}
 
 	@Override
 	public IndexSchemaFieldTypedContext<F> normalizer(String normalizerName) {
-		this.normalizerName = normalizerName;
-		return this;
+		throw log.cannotUseNormalizerOnFieldType(
+				relativeFieldName, dataType, getSchemaContext().getEventContext()
+		);
 	}
 
 	@Override
@@ -67,19 +67,12 @@ abstract class AbstractScalarFieldTypedContext<F> extends AbstractElasticsearchI
 
 	@Override
 	protected PropertyMapping contribute(
-			DeferredInitializationIndexFieldAccessor<F> reference,
+			IndexSchemaFieldDefinitionHelper<F> helper,
 			ElasticsearchIndexSchemaNodeCollector collector,
 			ElasticsearchIndexSchemaObjectNode parentNode) {
 		PropertyMapping mapping = new PropertyMapping();
 
 		mapping.setType( dataType );
-
-		if ( analyzerName != null ) {
-			throw log.cannotUseAnalyzerOnFieldType( relativeFieldName, dataType, schemaContext.getEventContext() );
-		}
-		if ( normalizerName != null ) {
-			throw log.cannotUseNormalizerOnFieldType( relativeFieldName, dataType, schemaContext.getEventContext() );
-		}
 
 		switch ( store ) {
 			case DEFAULT:

backend-lucene/src/main/java/org/hibernate/search/v6poc/backend/lucene/types/dsl/impl/AbstractLuceneIndexSchemaFieldTypedContext.java
@@ -11,9 +11,9 @@ import java.lang.invoke.MethodHandles;
 import org.apache.lucene.analysis.Analyzer;
 import org.hibernate.search.v6poc.backend.document.IndexFieldAccessor;
 import org.hibernate.search.v6poc.backend.document.model.dsl.spi.IndexSchemaContext;
-import org.hibernate.search.v6poc.backend.document.spi.DeferredInitializationIndexFieldAccessor;
 import org.hibernate.search.v6poc.backend.document.model.dsl.IndexSchemaFieldTypedContext;
 import org.hibernate.search.v6poc.backend.document.model.dsl.Store;
+import org.hibernate.search.v6poc.backend.document.spi.IndexSchemaFieldDefinitionHelper;
 import org.hibernate.search.v6poc.backend.lucene.document.model.dsl.LuceneIndexSchemaFieldTypedContext;
 import org.hibernate.search.v6poc.backend.lucene.document.model.impl.LuceneIndexSchemaNodeCollector;
 import org.hibernate.search.v6poc.backend.lucene.document.model.impl.LuceneIndexSchemaNodeContributor;
@@ -29,29 +29,28 @@ public abstract class AbstractLuceneIndexSchemaFieldTypedContext<F>
 
 	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
 
-	private final IndexSchemaContext schemaContext;
-	private final String relativeFieldName;
+	private final IndexSchemaFieldDefinitionHelper<F> helper;
 
-	private final DeferredInitializationIndexFieldAccessor<F> accessor = new DeferredInitializationIndexFieldAccessor<>();
+	private final String relativeFieldName;
 
 	private Store store;
 
 	protected AbstractLuceneIndexSchemaFieldTypedContext(IndexSchemaContext schemaContext, String relativeFieldName) {
-		this.schemaContext = schemaContext;
+		this.helper = new IndexSchemaFieldDefinitionHelper<>( schemaContext );
 		this.relativeFieldName = relativeFieldName;
 	}
 
 	@Override
 	public IndexFieldAccessor<F> createAccessor() {
-		return accessor;
+		return helper.createAccessor();
 	}
 
 	@Override
 	public void contribute(LuceneIndexSchemaNodeCollector collector, LuceneIndexSchemaObjectNode parentNode) {
-		contribute( accessor, collector, parentNode );
+		contribute( helper, collector, parentNode );
 	}
 
-	protected abstract void contribute(DeferredInitializationIndexFieldAccessor<F> reference, LuceneIndexSchemaNodeCollector collector,
+	protected abstract void contribute(IndexSchemaFieldDefinitionHelper<F> helper, LuceneIndexSchemaNodeCollector collector,
 			LuceneIndexSchemaObjectNode parentNode);
 
 	@Override
@@ -62,12 +61,12 @@ public abstract class AbstractLuceneIndexSchemaFieldTypedContext<F>
 
 	@Override
 	public IndexSchemaFieldTypedContext<F> analyzer(String analyzerName) {
-		throw log.cannotUseAnalyzerOnFieldType( relativeFieldName, schemaContext.getEventContext() );
+		throw log.cannotUseAnalyzerOnFieldType( relativeFieldName, getSchemaContext().getEventContext() );
 	}
 
 	@Override
 	public IndexSchemaFieldTypedContext<F> normalizer(String normalizerName) {
-		throw log.cannotUseNormalizerOnFieldType( relativeFieldName, schemaContext.getEventContext() );
+		throw log.cannotUseNormalizerOnFieldType( relativeFieldName, getSchemaContext().getEventContext() );
 	}
 
 	protected String getRelativeFieldName() {
@@ -84,5 +83,9 @@ public abstract class AbstractLuceneIndexSchemaFieldTypedContext<F>
 
 	protected Analyzer getNormalizer() {
 		return null;
+	}
+
+	protected final IndexSchemaContext getSchemaContext() {
+		return helper.getSchemaContext();
 	}
 }

engine/src/main/java/org/hibernate/search/v6poc/backend/document/spi/IndexSchemaFieldDefinitionHelper.java
@@ -1,0 +1,63 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.v6poc.backend.document.spi;
+
+import java.lang.invoke.MethodHandles;
+
+import org.hibernate.search.v6poc.backend.document.IndexFieldAccessor;
+import org.hibernate.search.v6poc.backend.document.model.dsl.spi.IndexSchemaContext;
+import org.hibernate.search.v6poc.logging.impl.Log;
+import org.hibernate.search.v6poc.util.impl.common.LoggerFactory;
+
+/**
+ * A helper for backends, making it easier to return accessors before they are completely defined.
+ */
+public final class IndexSchemaFieldDefinitionHelper<F> {
+
+	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
+
+	private final IndexSchemaContext schemaContext;
+
+	private final DeferredInitializationIndexFieldAccessor<F> rawAccessor =
+			new DeferredInitializationIndexFieldAccessor<>();
+
+	private boolean accessorCreated = false;
+
+	public IndexSchemaFieldDefinitionHelper(IndexSchemaContext schemaContext) {
+		this.schemaContext = schemaContext;
+	}
+
+	public IndexSchemaContext getSchemaContext() {
+		return schemaContext;
+	}
+
+	/**
+	 * @return A (potentially un-{@link #initialize(IndexFieldAccessor) initialized}) accessor
+	 */
+	public IndexFieldAccessor<F> createAccessor() {
+		if ( accessorCreated ) {
+			throw log.cannotCreateAccessorMultipleTimes( schemaContext.getEventContext() );
+		}
+		accessorCreated = true;
+		return rawAccessor;
+	}
+
+	/**
+	 * Initialize the field definition, enabling writes to an underlying field.
+	 * <p>
+	 * This method may or may not be called during bootstrap; if it isn't called,
+	 * writes triggered by the mapper through the accessor won't have any effect.
+	 *
+	 * @param delegate The delegate to use when writing to the accessor returned by {@link #createAccessor()}.
+	 */
+	public void initialize(IndexFieldAccessor<F> delegate) {
+		if ( !accessorCreated ) {
+			throw log.incompleteFieldDefinition( schemaContext.getEventContext() );
+		}
+		rawAccessor.initialize( delegate );
+	}
+}

engine/src/main/java/org/hibernate/search/v6poc/backend/document/spi/IndexSchemaObjectFieldDefinitionHelper.java
@@ -1,0 +1,63 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.v6poc.backend.document.spi;
+
+import java.lang.invoke.MethodHandles;
+
+import org.hibernate.search.v6poc.backend.document.IndexObjectFieldAccessor;
+import org.hibernate.search.v6poc.backend.document.model.dsl.spi.IndexSchemaContext;
+import org.hibernate.search.v6poc.logging.impl.Log;
+import org.hibernate.search.v6poc.util.impl.common.LoggerFactory;
+
+/**
+ * A helper for backends, making it easier to return accessors before they are completely defined.
+ */
+public final class IndexSchemaObjectFieldDefinitionHelper {
+
+	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
+
+	private final IndexSchemaContext schemaContext;
+
+	private final DeferredInitializationIndexObjectFieldAccessor rawAccessor =
+			new DeferredInitializationIndexObjectFieldAccessor();
+
+	private boolean accessorCreated = false;
+
+	public IndexSchemaObjectFieldDefinitionHelper(IndexSchemaContext schemaContext) {
+		this.schemaContext = schemaContext;
+	}
+
+	public IndexSchemaContext getSchemaContext() {
+		return schemaContext;
+	}
+
+	/**
+	 * @return A (potentially un-{@link #initialize(IndexObjectFieldAccessor) initialized}) accessor
+	 */
+	public IndexObjectFieldAccessor createAccessor() {
+		if ( accessorCreated ) {
+			throw log.cannotCreateAccessorMultipleTimes( schemaContext.getEventContext() );
+		}
+		accessorCreated = true;
+		return rawAccessor;
+	}
+
+	/**
+	 * Initialize the field definition, enabling writes to an underlying field.
+	 * <p>
+	 * This method may or may not be called during bootstrap; if it isn't called,
+	 * writes triggered by the mapper through the accessor won't have any effect.
+	 *
+	 * @param delegate The delegate to use when writing to the accessor returned by {@link #createAccessor()}.
+	 */
+	public void initialize(IndexObjectFieldAccessor delegate) {
+		if ( !accessorCreated ) {
+			throw log.incompleteFieldDefinition( schemaContext.getEventContext() );
+		}
+		rawAccessor.initialize( delegate );
+	}
+}

hibernate/hibernate-search

backend/elasticsearch/src/main/java/org/hibernate/search/backend/elasticsearch/types/dsl/impl/AbstractElasticsearchIndexSchemaFieldTypedContext.java
@@ -7,8 +7,8 @@
 package org.hibernate.search.backend.elasticsearch.types.dsl.impl;
 
 import org.hibernate.search.engine.backend.document.model.dsl.spi.IndexSchemaContext;
-import org.hibernate.search.engine.backend.document.spi.DeferredInitializationIndexFieldAccessor;
 import org.hibernate.search.engine.backend.document.IndexFieldAccessor;
+import org.hibernate.search.engine.backend.document.spi.IndexSchemaFieldDefinitionHelper;
 import org.hibernate.search.backend.elasticsearch.document.model.dsl.ElasticsearchIndexSchemaFieldTypedContext;
 import org.hibernate.search.backend.elasticsearch.document.model.impl.ElasticsearchIndexSchemaNodeCollector;
 import org.hibernate.search.backend.elasticsearch.document.model.impl.ElasticsearchIndexSchemaNodeContributor;
@@ -22,27 +22,29 @@ public abstract class AbstractElasticsearchIndexSchemaFieldTypedContext<F>
 		implements ElasticsearchIndexSchemaFieldTypedContext<F>,
 		ElasticsearchIndexSchemaNodeContributor<PropertyMapping> {
 
-	protected final IndexSchemaContext schemaContext;
-
-	private final DeferredInitializationIndexFieldAccessor<F> accessor = new DeferredInitializationIndexFieldAccessor<>();
+	private final IndexSchemaFieldDefinitionHelper<F> helper;
 
 	AbstractElasticsearchIndexSchemaFieldTypedContext(IndexSchemaContext schemaContext) {
-		this.schemaContext = schemaContext;
+		this.helper = new IndexSchemaFieldDefinitionHelper<>( schemaContext );
 	}
 
 	@Override
 	public IndexFieldAccessor<F> createAccessor() {
-		return accessor;
+		return helper.createAccessor();
 	}
 
 	@Override
 	public PropertyMapping contribute(ElasticsearchIndexSchemaNodeCollector collector,
 			ElasticsearchIndexSchemaObjectNode parentNode) {
-		return contribute( accessor, collector, parentNode );
+		return contribute( helper, collector, parentNode );
 	}
 
-	protected abstract PropertyMapping contribute(DeferredInitializationIndexFieldAccessor<F> reference,
+	protected abstract PropertyMapping contribute(IndexSchemaFieldDefinitionHelper<F> helper,
 			ElasticsearchIndexSchemaNodeCollector collector,
 			ElasticsearchIndexSchemaObjectNode parentNode);
+
+	protected final IndexSchemaContext getSchemaContext() {
+		return helper.getSchemaContext();
+	}
 
 }

backend/elasticsearch/src/main/java/org/hibernate/search/backend/elasticsearch/types/dsl/impl/AbstractScalarFieldTypedContext.java
@@ -9,10 +9,10 @@ package org.hibernate.search.backend.elasticsearch.types.dsl.impl;
 import java.lang.invoke.MethodHandles;
 
 import org.hibernate.search.engine.backend.document.model.dsl.spi.IndexSchemaContext;
-import org.hibernate.search.engine.backend.document.spi.DeferredInitializationIndexFieldAccessor;
 import org.hibernate.search.engine.backend.document.model.dsl.Sortable;
 import org.hibernate.search.engine.backend.document.model.dsl.Store;
 import org.hibernate.search.engine.backend.document.model.dsl.IndexSchemaFieldTypedContext;
+import org.hibernate.search.engine.backend.document.spi.IndexSchemaFieldDefinitionHelper;
 import org.hibernate.search.backend.elasticsearch.document.model.impl.ElasticsearchIndexSchemaNodeCollector;
 import org.hibernate.search.backend.elasticsearch.document.model.impl.ElasticsearchIndexSchemaObjectNode;
 import org.hibernate.search.backend.elasticsearch.document.model.impl.esnative.DataType;
@@ -29,8 +29,6 @@ abstract class AbstractScalarFieldTypedContext<F> extends AbstractElasticsearchI
 
 	private final String relativeFieldName;
 	private final DataType dataType;
-	private String analyzerName;
-	private String normalizerName;
 	private Store store = Store.DEFAULT;
 	private Sortable sortable = Sortable.DEFAULT;
 
@@ -43,14 +41,16 @@ abstract class AbstractScalarFieldTypedContext<F> extends AbstractElasticsearchI
 
 	@Override
 	public IndexSchemaFieldTypedContext<F> analyzer(String analyzerName) {
-		this.analyzerName = analyzerName;
-		return this;
+		throw log.cannotUseAnalyzerOnFieldType(
+				relativeFieldName, dataType, getSchemaContext().getEventContext()
+		);
 	}
 
 	@Override
 	public IndexSchemaFieldTypedContext<F> normalizer(String normalizerName) {
-		this.normalizerName = normalizerName;
-		return this;
+		throw log.cannotUseNormalizerOnFieldType(
+				relativeFieldName, dataType, getSchemaContext().getEventContext()
+		);
 	}
 
 	@Override
@@ -67,19 +67,12 @@ abstract class AbstractScalarFieldTypedContext<F> extends AbstractElasticsearchI
 
 	@Override
 	protected PropertyMapping contribute(
-			DeferredInitializationIndexFieldAccessor<F> reference,
+			IndexSchemaFieldDefinitionHelper<F> helper,
 			ElasticsearchIndexSchemaNodeCollector collector,
 			ElasticsearchIndexSchemaObjectNode parentNode) {
 		PropertyMapping mapping = new PropertyMapping();
 
 		mapping.setType( dataType );
-
-		if ( analyzerName != null ) {
-			throw log.cannotUseAnalyzerOnFieldType( relativeFieldName, dataType, schemaContext.getEventContext() );
-		}
-		if ( normalizerName != null ) {
-			throw log.cannotUseNormalizerOnFieldType( relativeFieldName, dataType, schemaContext.getEventContext() );
-		}
 
 		switch ( store ) {
 			case DEFAULT:

backend/lucene/src/main/java/org/hibernate/search/backend/lucene/types/dsl/impl/AbstractLuceneIndexSchemaFieldTypedContext.java
@@ -11,9 +11,9 @@ import java.lang.invoke.MethodHandles;
 import org.apache.lucene.analysis.Analyzer;
 import org.hibernate.search.engine.backend.document.IndexFieldAccessor;
 import org.hibernate.search.engine.backend.document.model.dsl.spi.IndexSchemaContext;
-import org.hibernate.search.engine.backend.document.spi.DeferredInitializationIndexFieldAccessor;
 import org.hibernate.search.engine.backend.document.model.dsl.IndexSchemaFieldTypedContext;
 import org.hibernate.search.engine.backend.document.model.dsl.Store;
+import org.hibernate.search.engine.backend.document.spi.IndexSchemaFieldDefinitionHelper;
 import org.hibernate.search.backend.lucene.document.model.dsl.LuceneIndexSchemaFieldTypedContext;
 import org.hibernate.search.backend.lucene.document.model.impl.LuceneIndexSchemaNodeCollector;
 import org.hibernate.search.backend.lucene.document.model.impl.LuceneIndexSchemaNodeContributor;
@@ -29,29 +29,28 @@ public abstract class AbstractLuceneIndexSchemaFieldTypedContext<F>
 
 	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
 
-	private final IndexSchemaContext schemaContext;
-	private final String relativeFieldName;
+	private final IndexSchemaFieldDefinitionHelper<F> helper;
 
-	private final DeferredInitializationIndexFieldAccessor<F> accessor = new DeferredInitializationIndexFieldAccessor<>();
+	private final String relativeFieldName;
 
 	private Store store;
 
 	protected AbstractLuceneIndexSchemaFieldTypedContext(IndexSchemaContext schemaContext, String relativeFieldName) {
-		this.schemaContext = schemaContext;
+		this.helper = new IndexSchemaFieldDefinitionHelper<>( schemaContext );
 		this.relativeFieldName = relativeFieldName;
 	}
 
 	@Override
 	public IndexFieldAccessor<F> createAccessor() {
-		return accessor;
+		return helper.createAccessor();
 	}
 
 	@Override
 	public void contribute(LuceneIndexSchemaNodeCollector collector, LuceneIndexSchemaObjectNode parentNode) {
-		contribute( accessor, collector, parentNode );
+		contribute( helper, collector, parentNode );
 	}
 
-	protected abstract void contribute(DeferredInitializationIndexFieldAccessor<F> reference, LuceneIndexSchemaNodeCollector collector,
+	protected abstract void contribute(IndexSchemaFieldDefinitionHelper<F> helper, LuceneIndexSchemaNodeCollector collector,
 			LuceneIndexSchemaObjectNode parentNode);
 
 	@Override
@@ -62,12 +61,12 @@ public abstract class AbstractLuceneIndexSchemaFieldTypedContext<F>
 
 	@Override
 	public IndexSchemaFieldTypedContext<F> analyzer(String analyzerName) {
-		throw log.cannotUseAnalyzerOnFieldType( relativeFieldName, schemaContext.getEventContext() );
+		throw log.cannotUseAnalyzerOnFieldType( relativeFieldName, getSchemaContext().getEventContext() );
 	}
 
 	@Override
 	public IndexSchemaFieldTypedContext<F> normalizer(String normalizerName) {
-		throw log.cannotUseNormalizerOnFieldType( relativeFieldName, schemaContext.getEventContext() );
+		throw log.cannotUseNormalizerOnFieldType( relativeFieldName, getSchemaContext().getEventContext() );
 	}
 
 	protected String getRelativeFieldName() {
@@ -84,5 +83,9 @@ public abstract class AbstractLuceneIndexSchemaFieldTypedContext<F>
 
 	protected Analyzer getNormalizer() {
 		return null;
+	}
+
+	protected final IndexSchemaContext getSchemaContext() {
+		return helper.getSchemaContext();
 	}
 }

engine/src/main/java/org/hibernate/search/engine/backend/document/spi/IndexSchemaFieldDefinitionHelper.java
@@ -1,0 +1,63 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.engine.backend.document.spi;
+
+import java.lang.invoke.MethodHandles;
+
+import org.hibernate.search.engine.backend.document.IndexFieldAccessor;
+import org.hibernate.search.engine.backend.document.model.dsl.spi.IndexSchemaContext;
+import org.hibernate.search.engine.logging.impl.Log;
+import org.hibernate.search.util.impl.common.LoggerFactory;
+
+/**
+ * A helper for backends, making it easier to return accessors before they are completely defined.
+ */
+public final class IndexSchemaFieldDefinitionHelper<F> {
+
+	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
+
+	private final IndexSchemaContext schemaContext;
+
+	private final DeferredInitializationIndexFieldAccessor<F> rawAccessor =
+			new DeferredInitializationIndexFieldAccessor<>();
+
+	private boolean accessorCreated = false;
+
+	public IndexSchemaFieldDefinitionHelper(IndexSchemaContext schemaContext) {
+		this.schemaContext = schemaContext;
+	}
+
+	public IndexSchemaContext getSchemaContext() {
+		return schemaContext;
+	}
+
+	/**
+	 * @return A (potentially un-{@link #initialize(IndexFieldAccessor) initialized}) accessor
+	 */
+	public IndexFieldAccessor<F> createAccessor() {
+		if ( accessorCreated ) {
+			throw log.cannotCreateAccessorMultipleTimes( schemaContext.getEventContext() );
+		}
+		accessorCreated = true;
+		return rawAccessor;
+	}
+
+	/**
+	 * Initialize the field definition, enabling writes to an underlying field.
+	 * <p>
+	 * This method may or may not be called during bootstrap; if it isn't called,
+	 * writes triggered by the mapper through the accessor won't have any effect.
+	 *
+	 * @param delegate The delegate to use when writing to the accessor returned by {@link #createAccessor()}.
+	 */
+	public void initialize(IndexFieldAccessor<F> delegate) {
+		if ( !accessorCreated ) {
+			throw log.incompleteFieldDefinition( schemaContext.getEventContext() );
+		}
+		rawAccessor.initialize( delegate );
+	}
+}

engine/src/main/java/org/hibernate/search/engine/backend/document/spi/IndexSchemaObjectFieldDefinitionHelper.java
@@ -1,0 +1,63 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.engine.backend.document.spi;
+
+import java.lang.invoke.MethodHandles;
+
+import org.hibernate.search.engine.backend.document.IndexObjectFieldAccessor;
+import org.hibernate.search.engine.backend.document.model.dsl.spi.IndexSchemaContext;
+import org.hibernate.search.engine.logging.impl.Log;
+import org.hibernate.search.util.impl.common.LoggerFactory;
+
+/**
+ * A helper for backends, making it easier to return accessors before they are completely defined.
+ */
+public final class IndexSchemaObjectFieldDefinitionHelper {
+
+	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
+
+	private final IndexSchemaContext schemaContext;
+
+	private final DeferredInitializationIndexObjectFieldAccessor rawAccessor =
+			new DeferredInitializationIndexObjectFieldAccessor();
+
+	private boolean accessorCreated = false;
+
+	public IndexSchemaObjectFieldDefinitionHelper(IndexSchemaContext schemaContext) {
+		this.schemaContext = schemaContext;
+	}
+
+	public IndexSchemaContext getSchemaContext() {
+		return schemaContext;
+	}
+
+	/**
+	 * @return A (potentially un-{@link #initialize(IndexObjectFieldAccessor) initialized}) accessor
+	 */
+	public IndexObjectFieldAccessor createAccessor() {
+		if ( accessorCreated ) {
+			throw log.cannotCreateAccessorMultipleTimes( schemaContext.getEventContext() );
+		}
+		accessorCreated = true;
+		return rawAccessor;
+	}
+
+	/**
+	 * Initialize the field definition, enabling writes to an underlying field.
+	 * <p>
+	 * This method may or may not be called during bootstrap; if it isn't called,
+	 * writes triggered by the mapper through the accessor won't have any effect.
+	 *
+	 * @param delegate The delegate to use when writing to the accessor returned by {@link #createAccessor()}.
+	 */
+	public void initialize(IndexObjectFieldAccessor delegate) {
+		if ( !accessorCreated ) {
+			throw log.incompleteFieldDefinition( schemaContext.getEventContext() );
+		}
+		rawAccessor.initialize( delegate );
+	}
+}