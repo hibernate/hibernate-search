hibernate/hibernate-search-6-poc

HSEARCH-2546 Make sure to only manipulate FailureContext in the application and not FailureContextElements
This will avoid patterns such as
FailureContext.create( FailureContexts.fromXXX( param ) )
which get cumbersome very fast when working with contexts composed
of a single element.
With this patch, the result of methods in FailureContexts can be used
directly as a full-blown FailureContext.

engine/src/main/java/org/hibernate/search/v6poc/logging/impl/RootFailureCollector.java
@@ -18,6 +18,7 @@ import org.hibernate.search.v6poc.logging.spi.ContextualFailureCollector;
 import org.hibernate.search.v6poc.logging.spi.FailureCollector;
 import org.hibernate.search.v6poc.logging.spi.FailureContext;
 import org.hibernate.search.v6poc.logging.spi.FailureContextElement;
+import org.hibernate.search.v6poc.logging.spi.FailureContexts;
 import org.hibernate.search.v6poc.logging.spi.SearchExceptionWithContext;
 import org.hibernate.search.v6poc.util.SearchException;
 import org.hibernate.search.v6poc.util.impl.common.LoggerFactory;
@@ -92,13 +93,6 @@ public class RootFailureCollector implements FailureCollector {
 	}
 
 	private static class FailureCollectorImpl implements FailureCollector {
-		private static final FailureContextElement DEFAULT_CONTEXT_ELEMENT = new FailureContextElement() {
-			@Override
-			public String render() {
-				return MESSAGES.defaultOnMissingContextElement();
-			}
-		};
-
 		protected final RootFailureCollector root;
 		private Map<FailureContextElement, ContextualFailureCollectorImpl> children;
 
@@ -113,17 +107,15 @@ public class RootFailureCollector implements FailureCollector {
 		@Override
 		public ContextualFailureCollectorImpl withContext(FailureContext context) {
 			List<FailureContextElement> elements = context.getElements();
+			// This should not happen, but we want to be extra-cautious to avoid failures while handling failures
 			if ( elements.isEmpty() ) {
-				/*
-				 * Do not fail, since we are already probably processing a failure.
-				 * Just log the problem and degrade gracefully.
-				 */
+				// Just log the problem and degrade gracefully.
 				log.unexpectedEmptyFailureContext( new SearchException( "Exception for stack trace" ) );
 				return withDefaultContext();
 			}
 			else {
 				FailureCollectorImpl failureCollector = this;
-				for ( FailureContextElement contextElement : context.getElements() ) {
+				for ( FailureContextElement contextElement : elements ) {
 					failureCollector = failureCollector.withContext( contextElement );
 				}
 				return (ContextualFailureCollectorImpl) failureCollector;
@@ -148,7 +140,7 @@ public class RootFailureCollector implements FailureCollector {
 		}
 
 		ContextualFailureCollectorImpl withDefaultContext() {
-			return withContext( DEFAULT_CONTEXT_ELEMENT );
+			return withContext( FailureContexts.getDefault() );
 		}
 
 		void appendContextTo(StringJoiner joiner) {

mapper-pojo/src/main/java/org/hibernate/search/v6poc/entity/pojo/logging/spi/PojoFailureContexts.java
@@ -12,7 +12,7 @@ import org.hibernate.search.v6poc.entity.pojo.logging.impl.PojoFailureContextMes
 import org.hibernate.search.v6poc.entity.pojo.model.path.PojoModelPath;
 import org.hibernate.search.v6poc.entity.pojo.model.spi.PojoRawTypeModel;
 import org.hibernate.search.v6poc.logging.spi.AbstractSimpleFailureContextElement;
-import org.hibernate.search.v6poc.logging.spi.FailureContextElement;
+import org.hibernate.search.v6poc.logging.spi.FailureContext;
 import org.hibernate.search.v6poc.logging.spi.FailureContexts;
 
 import org.jboss.logging.Messages;
@@ -24,27 +24,27 @@ public final class PojoFailureContexts {
 	private PojoFailureContexts() {
 	}
 
-	public static FailureContextElement fromType(PojoRawTypeModel<?> typeModel) {
+	public static FailureContext fromType(PojoRawTypeModel<?> typeModel) {
 		return FailureContexts.fromType( typeModel );
 	}
 
-	public static FailureContextElement fromPath(PojoModelPath unboundPath) {
-		return new AbstractSimpleFailureContextElement<PojoModelPath>( unboundPath ) {
+	public static FailureContext fromPath(PojoModelPath unboundPath) {
+		return FailureContext.create( new AbstractSimpleFailureContextElement<PojoModelPath>( unboundPath ) {
 			@Override
 			public String render(PojoModelPath param) {
 				String pathString = param == null ? "" : param.toPathString();
 				return MESSAGES.path( pathString );
 			}
-		};
+		} );
 	}
 
-	public static FailureContextElement fromAnnotation(Annotation annotation) {
-		return new AbstractSimpleFailureContextElement<Annotation>( annotation ) {
+	public static FailureContext fromAnnotation(Annotation annotation) {
+		return FailureContext.create( new AbstractSimpleFailureContextElement<Annotation>( annotation ) {
 			@Override
 			public String render(Annotation annotation) {
 				String annotationString = annotation.toString();
 				return MESSAGES.annotation( annotationString );
 			}
-		};
+		} );
 	}
 }

util/internal/integrationtest/common/src/main/java/org/hibernate/search/v6poc/util/impl/integrationtest/common/FailureReportUtils.java
@@ -11,6 +11,7 @@ import java.util.Arrays;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
+import org.hibernate.search.v6poc.logging.spi.FailureContext;
 import org.hibernate.search.v6poc.logging.spi.FailureContextElement;
 import org.hibernate.search.v6poc.logging.spi.SearchExceptionWithContext;
 
@@ -19,6 +20,18 @@ import org.assertj.core.api.Assertions;
 public final class FailureReportUtils {
 
 	private FailureReportUtils() {
+	}
+
+	/**
+	 * @param first The first part of the expected context.
+	 * @param others The other parts of the expected context, if any. To be concatenated to the first part.
+	 * @return A consumer representing an assertion to be passed as a parameter to
+	 * {@link org.assertj.core.api.AbstractThrowableAssert#satisfies(Consumer)}.
+	 */
+	public static Consumer<Throwable> hasContext(FailureContext first, FailureContext ... others) {
+		return hasContext(
+				FailureContext.concat( first, others ).getElements().toArray( new FailureContextElement[] { } )
+		);
 	}
 
 	/**

hibernate/hibernate-search

engine/src/main/java/org/hibernate/search/engine/logging/impl/RootFailureCollector.java
@@ -18,6 +18,7 @@ import org.hibernate.search.engine.logging.spi.ContextualFailureCollector;
 import org.hibernate.search.engine.logging.spi.FailureCollector;
 import org.hibernate.search.engine.logging.spi.FailureContext;
 import org.hibernate.search.engine.logging.spi.FailureContextElement;
+import org.hibernate.search.engine.logging.spi.FailureContexts;
 import org.hibernate.search.engine.logging.spi.SearchExceptionWithContext;
 import org.hibernate.search.util.SearchException;
 import org.hibernate.search.util.impl.common.LoggerFactory;
@@ -92,13 +93,6 @@ public class RootFailureCollector implements FailureCollector {
 	}
 
 	private static class FailureCollectorImpl implements FailureCollector {
-		private static final FailureContextElement DEFAULT_CONTEXT_ELEMENT = new FailureContextElement() {
-			@Override
-			public String render() {
-				return MESSAGES.defaultOnMissingContextElement();
-			}
-		};
-
 		protected final RootFailureCollector root;
 		private Map<FailureContextElement, ContextualFailureCollectorImpl> children;
 
@@ -113,17 +107,15 @@ public class RootFailureCollector implements FailureCollector {
 		@Override
 		public ContextualFailureCollectorImpl withContext(FailureContext context) {
 			List<FailureContextElement> elements = context.getElements();
+			// This should not happen, but we want to be extra-cautious to avoid failures while handling failures
 			if ( elements.isEmpty() ) {
-				/*
-				 * Do not fail, since we are already probably processing a failure.
-				 * Just log the problem and degrade gracefully.
-				 */
+				// Just log the problem and degrade gracefully.
 				log.unexpectedEmptyFailureContext( new SearchException( "Exception for stack trace" ) );
 				return withDefaultContext();
 			}
 			else {
 				FailureCollectorImpl failureCollector = this;
-				for ( FailureContextElement contextElement : context.getElements() ) {
+				for ( FailureContextElement contextElement : elements ) {
 					failureCollector = failureCollector.withContext( contextElement );
 				}
 				return (ContextualFailureCollectorImpl) failureCollector;
@@ -148,7 +140,7 @@ public class RootFailureCollector implements FailureCollector {
 		}
 
 		ContextualFailureCollectorImpl withDefaultContext() {
-			return withContext( DEFAULT_CONTEXT_ELEMENT );
+			return withContext( FailureContexts.getDefault() );
 		}
 
 		void appendContextTo(StringJoiner joiner) {

mapper/pojo/src/main/java/org/hibernate/search/mapper/pojo/logging/spi/PojoFailureContexts.java
@@ -12,7 +12,7 @@ import org.hibernate.search.mapper.pojo.logging.impl.PojoFailureContextMessages;
 import org.hibernate.search.mapper.pojo.model.path.PojoModelPath;
 import org.hibernate.search.mapper.pojo.model.spi.PojoRawTypeModel;
 import org.hibernate.search.engine.logging.spi.AbstractSimpleFailureContextElement;
-import org.hibernate.search.engine.logging.spi.FailureContextElement;
+import org.hibernate.search.engine.logging.spi.FailureContext;
 import org.hibernate.search.engine.logging.spi.FailureContexts;
 
 import org.jboss.logging.Messages;
@@ -24,27 +24,27 @@ public final class PojoFailureContexts {
 	private PojoFailureContexts() {
 	}
 
-	public static FailureContextElement fromType(PojoRawTypeModel<?> typeModel) {
+	public static FailureContext fromType(PojoRawTypeModel<?> typeModel) {
 		return FailureContexts.fromType( typeModel );
 	}
 
-	public static FailureContextElement fromPath(PojoModelPath unboundPath) {
-		return new AbstractSimpleFailureContextElement<PojoModelPath>( unboundPath ) {
+	public static FailureContext fromPath(PojoModelPath unboundPath) {
+		return FailureContext.create( new AbstractSimpleFailureContextElement<PojoModelPath>( unboundPath ) {
 			@Override
 			public String render(PojoModelPath param) {
 				String pathString = param == null ? "" : param.toPathString();
 				return MESSAGES.path( pathString );
 			}
-		};
+		} );
 	}
 
-	public static FailureContextElement fromAnnotation(Annotation annotation) {
-		return new AbstractSimpleFailureContextElement<Annotation>( annotation ) {
+	public static FailureContext fromAnnotation(Annotation annotation) {
+		return FailureContext.create( new AbstractSimpleFailureContextElement<Annotation>( annotation ) {
 			@Override
 			public String render(Annotation annotation) {
 				String annotationString = annotation.toString();
 				return MESSAGES.annotation( annotationString );
 			}
-		};
+		} );
 	}
 }

util/internal/integrationtest/common/src/main/java/org/hibernate/search/util/impl/integrationtest/common/FailureReportUtils.java
@@ -11,6 +11,7 @@ import java.util.Arrays;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
+import org.hibernate.search.engine.logging.spi.FailureContext;
 import org.hibernate.search.engine.logging.spi.FailureContextElement;
 import org.hibernate.search.engine.logging.spi.SearchExceptionWithContext;
 
@@ -19,6 +20,18 @@ import org.assertj.core.api.Assertions;
 public final class FailureReportUtils {
 
 	private FailureReportUtils() {
+	}
+
+	/**
+	 * @param first The first part of the expected context.
+	 * @param others The other parts of the expected context, if any. To be concatenated to the first part.
+	 * @return A consumer representing an assertion to be passed as a parameter to
+	 * {@link org.assertj.core.api.AbstractThrowableAssert#satisfies(Consumer)}.
+	 */
+	public static Consumer<Throwable> hasContext(FailureContext first, FailureContext ... others) {
+		return hasContext(
+				FailureContext.concat( first, others ).getElements().toArray( new FailureContextElement[] { } )
+		);
 	}
 
 	/**