yrodiere/hibernate-search-6-poc

HSEARCH-3221 Restore support for applying bridges automatically to predicate/sort DSL parameters

backend-lucene/src/main/java/org/hibernate/search/v6poc/backend/lucene/types/converter/impl/LuceneFieldConverter.java
@@ -6,15 +6,18 @@
  */
 package org.hibernate.search.v6poc.backend.lucene.types.converter.impl;
 
+import org.hibernate.search.v6poc.backend.document.spi.UserIndexFieldConverter;
+
 /**
  * Defines how a given value will be converted when performing search queries.
  * <p>
  * Used by predicate and sort builders in particular.
  *
+ * @param <F> The type of the index field.
  * @param <T> The type used internally when querying. May be different from the field type exposed to users;
  * see for example {@link LocalDateFieldConverter}.
  */
-public interface LuceneFieldConverter<T> {
+public interface LuceneFieldConverter<F, T> {
 
 	/**
 	 * @param value A value passed through the predicate or sort DSL.
@@ -22,11 +25,15 @@ public interface LuceneFieldConverter<T> {
 	 */
 	T convertFromDsl(Object value);
 
-	// equals()/hashCode() needs to be implemented if the converter is not a singleton
+	/**
+	 * Determine whether another converter is DSL-compatible with this one.
+	 *
+	 * @see org.hibernate.search.v6poc.backend.document.spi.UserIndexFieldConverter#isDslCompatibleWith(UserIndexFieldConverter)
+	 *
+	 * @param other Another {@link LuceneFieldConverter}, never {@code null}.
+	 * @return {@code true} if the given converter is DSL-compatible.
+	 * {@code false} otherwise, or when in doubt.
+	 */
+	boolean isDslCompatibleWith(LuceneFieldConverter<?, ?> other);
 
-	@Override
-	boolean equals(Object obj);
-
-	@Override
-	int hashCode();
 }

engine/src/main/java/org/hibernate/search/v6poc/backend/document/model/dsl/IndexSchemaFieldTerminalContext.java
@@ -7,6 +7,7 @@
 package org.hibernate.search.v6poc.backend.document.model.dsl;
 
 import org.hibernate.search.v6poc.backend.document.IndexFieldAccessor;
+import org.hibernate.search.v6poc.backend.document.converter.ToIndexFieldValueConverter;
 
 /**
  * @param <F> The type of field values.
@@ -14,5 +15,7 @@ import org.hibernate.search.v6poc.backend.document.IndexFieldAccessor;
 public interface IndexSchemaFieldTerminalContext<F> {
 
 	IndexFieldAccessor<F> createAccessor();
+
+	<V> IndexFieldAccessor<V> createAccessor(ToIndexFieldValueConverter<V, ? extends F> toIndexConverter);
 
 }

hibernate/hibernate-search

backend/lucene/src/main/java/org/hibernate/search/backend/lucene/types/converter/impl/LuceneFieldConverter.java
@@ -6,27 +6,35 @@
  */
 package org.hibernate.search.backend.lucene.types.converter.impl;
 
+import org.hibernate.search.engine.backend.document.spi.UserIndexFieldConverter;
+
 /**
  * Defines how a given value will be converted when performing search queries.
  * <p>
  * Used by predicate and sort builders in particular.
  *
+ * @param <F> The type of the index field.
  * @param <T> The type used internally when querying. May be different from the field type exposed to users;
  * see for example {@link LocalDateFieldConverter}.
  */
-public interface LuceneFieldConverter<T> {
+public interface LuceneFieldConverter<F, T> {
 
 	/**
 	 * @param value A value passed through the predicate or sort DSL.
 	 * @return A value of the type used internally when querying this field.
+	 * @throws RuntimeException If the value does not match the expected type.
 	 */
 	T convertFromDsl(Object value);
 
-	// equals()/hashCode() needs to be implemented if the converter is not a singleton
+	/**
+	 * Determine whether another converter is DSL-compatible with this one.
+	 *
+	 * @see org.hibernate.search.engine.backend.document.spi.UserIndexFieldConverter#isDslCompatibleWith(UserIndexFieldConverter)
+	 *
+	 * @param other Another {@link LuceneFieldConverter}, never {@code null}.
+	 * @return {@code true} if the given converter is DSL-compatible.
+	 * {@code false} otherwise, or when in doubt.
+	 */
+	boolean isDslCompatibleWith(LuceneFieldConverter<?, ?> other);
 
-	@Override
-	boolean equals(Object obj);
-
-	@Override
-	int hashCode();
 }

engine/src/main/java/org/hibernate/search/engine/backend/document/model/dsl/IndexSchemaFieldTerminalContext.java
@@ -7,6 +7,7 @@
 package org.hibernate.search.engine.backend.document.model.dsl;
 
 import org.hibernate.search.engine.backend.document.IndexFieldAccessor;
+import org.hibernate.search.engine.backend.document.converter.ToIndexFieldValueConverter;
 
 /**
  * @param <F> The type of field values.