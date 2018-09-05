hibernate/hibernate-search-6-poc

HSEARCH-2546 Rename FailureContext to EventContext
Since this class is now API, we'd better take care to name it properly.
It is possible that, in the future, we will also use it to log
non-failure events, such as INFO or DEBUG level events.

backend-elasticsearch/src/main/java/org/hibernate/search/v6poc/backend/elasticsearch/document/model/dsl/impl/ElasticsearchIndexSchemaRootNodeBuilder.java
@@ -14,8 +14,8 @@ import org.hibernate.search.v6poc.backend.elasticsearch.document.model.impl.esna
 import org.hibernate.search.v6poc.backend.elasticsearch.document.model.impl.esnative.RootTypeMapping;
 import org.hibernate.search.v6poc.backend.elasticsearch.document.model.impl.esnative.RoutingType;
 import org.hibernate.search.v6poc.backend.elasticsearch.multitenancy.impl.MultiTenancyStrategy;
-import org.hibernate.search.v6poc.util.FailureContext;
-import org.hibernate.search.v6poc.logging.spi.FailureContexts;
+import org.hibernate.search.v6poc.util.EventContext;
+import org.hibernate.search.v6poc.logging.spi.EventContexts;
 
 public class ElasticsearchIndexSchemaRootNodeBuilder extends AbstractElasticsearchIndexSchemaObjectNodeBuilder
 		implements IndexSchemaRootNodeBuilder, ElasticsearchRootIndexSchemaContributor {
@@ -32,9 +32,9 @@ public class ElasticsearchIndexSchemaRootNodeBuilder extends AbstractElasticsear
 	}
 
 	@Override
-	public FailureContext getFailureContext() {
-		return getIndexFailureContext()
-				.append( FailureContexts.indexSchemaRoot() );
+	public EventContext getEventContext() {
+		return getIndexEventContext()
+				.append( EventContexts.indexSchemaRoot() );
 	}
 
 	@Override
@@ -71,7 +71,7 @@ public class ElasticsearchIndexSchemaRootNodeBuilder extends AbstractElasticsear
 		return null;
 	}
 
-	FailureContext getIndexFailureContext() {
-		return FailureContexts.fromIndexName( hibernateSearchIndexName );
+	EventContext getIndexEventContext() {
+		return EventContexts.fromIndexName( hibernateSearchIndexName );
 	}
 }

backend-lucene/src/main/java/org/hibernate/search/v6poc/backend/lucene/types/codec/impl/LuceneFieldFieldCodec.java
@@ -15,7 +15,7 @@ import org.hibernate.search.v6poc.backend.lucene.document.impl.LuceneDocumentBui
 import org.hibernate.search.v6poc.backend.lucene.document.model.LuceneFieldContributor;
 import org.hibernate.search.v6poc.backend.lucene.document.model.LuceneFieldValueExtractor;
 import org.hibernate.search.v6poc.backend.lucene.logging.impl.Log;
-import org.hibernate.search.v6poc.logging.spi.FailureContexts;
+import org.hibernate.search.v6poc.logging.spi.EventContexts;
 import org.hibernate.search.v6poc.util.impl.common.LoggerFactory;
 
 public final class LuceneFieldFieldCodec<V> implements LuceneFieldCodec<V> {
@@ -44,7 +44,7 @@ public final class LuceneFieldFieldCodec<V> implements LuceneFieldCodec<V> {
 	public V decode(Document document, String absoluteFieldPath) {
 		if ( fieldValueExtractor == null ) {
 			throw log.unsupportedProjection(
-					FailureContexts.fromIndexFieldAbsolutePath( absoluteFieldPath )
+					EventContexts.fromIndexFieldAbsolutePath( absoluteFieldPath )
 			);
 		}
 

backend-lucene/src/main/java/org/hibernate/search/v6poc/backend/lucene/types/sort/impl/GeoPointFieldSortContributor.java
@@ -11,7 +11,7 @@ import java.lang.invoke.MethodHandles;
 import org.apache.lucene.document.LatLonDocValuesField;
 import org.hibernate.search.v6poc.backend.lucene.logging.impl.Log;
 import org.hibernate.search.v6poc.backend.lucene.search.sort.impl.LuceneSearchSortCollector;
-import org.hibernate.search.v6poc.logging.spi.FailureContexts;
+import org.hibernate.search.v6poc.logging.spi.EventContexts;
 import org.hibernate.search.v6poc.search.dsl.sort.SortOrder;
 import org.hibernate.search.v6poc.spatial.GeoPoint;
 import org.hibernate.search.v6poc.util.impl.common.LoggerFactory;
@@ -28,7 +28,7 @@ public class GeoPointFieldSortContributor implements LuceneFieldSortContributor 
 	@Override
 	public void contribute(LuceneSearchSortCollector collector, String absoluteFieldPath, SortOrder order, Object missingValue) {
 		throw log.traditionalSortNotSupportedByGeoPoint(
-				FailureContexts.fromIndexFieldAbsolutePath( absoluteFieldPath )
+				EventContexts.fromIndexFieldAbsolutePath( absoluteFieldPath )
 		);
 	}
 

integrationtest/backend-tck/src/main/java/org/hibernate/search/v6poc/integrationtest/backend/tck/search/predicate/MatchSearchPredicateIT.java
@@ -21,7 +21,7 @@ import org.hibernate.search.v6poc.backend.index.spi.IndexManager;
 import org.hibernate.search.v6poc.backend.index.spi.IndexSearchTarget;
 import org.hibernate.search.v6poc.engine.spi.SessionContext;
 import org.hibernate.search.v6poc.integrationtest.backend.tck.util.rule.SearchSetupHelper;
-import org.hibernate.search.v6poc.logging.spi.FailureContexts;
+import org.hibernate.search.v6poc.logging.spi.EventContexts;
 import org.hibernate.search.v6poc.search.DocumentReference;
 import org.hibernate.search.v6poc.search.SearchQuery;
 import org.hibernate.search.v6poc.spatial.GeoPoint;
@@ -124,7 +124,7 @@ public class MatchSearchPredicateIT {
 					.isInstanceOf( SearchException.class )
 					.hasMessageContaining( "Match predicates are not supported by" )
 					.satisfies( FailureReportUtils.hasContext(
-							FailureContexts.fromIndexFieldAbsolutePath( absoluteFieldPath )
+							EventContexts.fromIndexFieldAbsolutePath( absoluteFieldPath )
 					) );
 		}
 	}

integrationtest/backend-tck/src/main/java/org/hibernate/search/v6poc/integrationtest/backend/tck/search/predicate/spatial/SpatialWithinCircleSearchPredicateIT.java
@@ -7,7 +7,7 @@
 package org.hibernate.search.v6poc.integrationtest.backend.tck.search.predicate.spatial;
 
 import org.hibernate.search.v6poc.backend.index.spi.IndexSearchTarget;
-import org.hibernate.search.v6poc.logging.spi.FailureContexts;
+import org.hibernate.search.v6poc.logging.spi.EventContexts;
 import org.hibernate.search.v6poc.search.DocumentReference;
 import org.hibernate.search.v6poc.search.SearchQuery;
 import org.hibernate.search.v6poc.spatial.DistanceUnit;
@@ -90,7 +90,7 @@ public class SpatialWithinCircleSearchPredicateIT extends AbstractSpatialWithinS
 				.isInstanceOf( SearchException.class )
 				.hasMessageContaining( "Spatial predicates are not supported by" )
 				.satisfies( FailureReportUtils.hasContext(
-						FailureContexts.fromIndexFieldAbsolutePath( "string" )
+						EventContexts.fromIndexFieldAbsolutePath( "string" )
 				) );
 	}
 

hibernate/hibernate-search

backend/elasticsearch/src/main/java/org/hibernate/search/backend/elasticsearch/document/model/dsl/impl/ElasticsearchIndexSchemaRootNodeBuilder.java
@@ -14,8 +14,8 @@ import org.hibernate.search.backend.elasticsearch.document.model.impl.esnative.D
 import org.hibernate.search.backend.elasticsearch.document.model.impl.esnative.RootTypeMapping;
 import org.hibernate.search.backend.elasticsearch.document.model.impl.esnative.RoutingType;
 import org.hibernate.search.backend.elasticsearch.multitenancy.impl.MultiTenancyStrategy;
-import org.hibernate.search.util.FailureContext;
-import org.hibernate.search.engine.logging.spi.FailureContexts;
+import org.hibernate.search.util.EventContext;
+import org.hibernate.search.engine.logging.spi.EventContexts;
 
 public class ElasticsearchIndexSchemaRootNodeBuilder extends AbstractElasticsearchIndexSchemaObjectNodeBuilder
 		implements IndexSchemaRootNodeBuilder, ElasticsearchRootIndexSchemaContributor {
@@ -32,9 +32,9 @@ public class ElasticsearchIndexSchemaRootNodeBuilder extends AbstractElasticsear
 	}
 
 	@Override
-	public FailureContext getFailureContext() {
-		return getIndexFailureContext()
-				.append( FailureContexts.indexSchemaRoot() );
+	public EventContext getEventContext() {
+		return getIndexEventContext()
+				.append( EventContexts.indexSchemaRoot() );
 	}
 
 	@Override
@@ -71,7 +71,7 @@ public class ElasticsearchIndexSchemaRootNodeBuilder extends AbstractElasticsear
 		return null;
 	}
 
-	FailureContext getIndexFailureContext() {
-		return FailureContexts.fromIndexName( hibernateSearchIndexName );
+	EventContext getIndexEventContext() {
+		return EventContexts.fromIndexName( hibernateSearchIndexName );
 	}
 }

backend/lucene/src/main/java/org/hibernate/search/backend/lucene/types/codec/impl/LuceneFieldFieldCodec.java
@@ -15,7 +15,7 @@ import org.hibernate.search.backend.lucene.document.impl.LuceneDocumentBuilder;
 import org.hibernate.search.backend.lucene.document.model.LuceneFieldContributor;
 import org.hibernate.search.backend.lucene.document.model.LuceneFieldValueExtractor;
 import org.hibernate.search.backend.lucene.logging.impl.Log;
-import org.hibernate.search.engine.logging.spi.FailureContexts;
+import org.hibernate.search.engine.logging.spi.EventContexts;
 import org.hibernate.search.util.impl.common.LoggerFactory;
 
 public final class LuceneFieldFieldCodec<V> implements LuceneFieldCodec<V> {
@@ -44,7 +44,7 @@ public final class LuceneFieldFieldCodec<V> implements LuceneFieldCodec<V> {
 	public V decode(Document document, String absoluteFieldPath) {
 		if ( fieldValueExtractor == null ) {
 			throw log.unsupportedProjection(
-					FailureContexts.fromIndexFieldAbsolutePath( absoluteFieldPath )
+					EventContexts.fromIndexFieldAbsolutePath( absoluteFieldPath )
 			);
 		}
 

backend/lucene/src/main/java/org/hibernate/search/backend/lucene/types/sort/impl/GeoPointFieldSortContributor.java
@@ -11,7 +11,7 @@ import java.lang.invoke.MethodHandles;
 import org.apache.lucene.document.LatLonDocValuesField;
 import org.hibernate.search.backend.lucene.logging.impl.Log;
 import org.hibernate.search.backend.lucene.search.sort.impl.LuceneSearchSortCollector;
-import org.hibernate.search.engine.logging.spi.FailureContexts;
+import org.hibernate.search.engine.logging.spi.EventContexts;
 import org.hibernate.search.engine.search.dsl.sort.SortOrder;
 import org.hibernate.search.engine.spatial.GeoPoint;
 import org.hibernate.search.util.impl.common.LoggerFactory;
@@ -28,7 +28,7 @@ public class GeoPointFieldSortContributor implements LuceneFieldSortContributor 
 	@Override
 	public void contribute(LuceneSearchSortCollector collector, String absoluteFieldPath, SortOrder order, Object missingValue) {
 		throw log.traditionalSortNotSupportedByGeoPoint(
-				FailureContexts.fromIndexFieldAbsolutePath( absoluteFieldPath )
+				EventContexts.fromIndexFieldAbsolutePath( absoluteFieldPath )
 		);
 	}
 

integrationtest/backend/tck/src/main/java/org/hibernate/search/integrationtest/backend/tck/search/predicate/MatchSearchPredicateIT.java
@@ -21,7 +21,7 @@ import org.hibernate.search.engine.backend.index.spi.IndexManager;
 import org.hibernate.search.engine.backend.index.spi.IndexSearchTarget;
 import org.hibernate.search.engine.common.spi.SessionContext;
 import org.hibernate.search.integrationtest.backend.tck.util.rule.SearchSetupHelper;
-import org.hibernate.search.engine.logging.spi.FailureContexts;
+import org.hibernate.search.engine.logging.spi.EventContexts;
 import org.hibernate.search.engine.search.DocumentReference;
 import org.hibernate.search.engine.search.SearchQuery;
 import org.hibernate.search.engine.spatial.GeoPoint;
@@ -124,7 +124,7 @@ public class MatchSearchPredicateIT {
 					.isInstanceOf( SearchException.class )
 					.hasMessageContaining( "Match predicates are not supported by" )
 					.satisfies( FailureReportUtils.hasContext(
-							FailureContexts.fromIndexFieldAbsolutePath( absoluteFieldPath )
+							EventContexts.fromIndexFieldAbsolutePath( absoluteFieldPath )
 					) );
 		}
 	}

integrationtest/backend/tck/src/main/java/org/hibernate/search/integrationtest/backend/tck/search/predicate/spatial/SpatialWithinCircleSearchPredicateIT.java
@@ -7,7 +7,7 @@
 package org.hibernate.search.integrationtest.backend.tck.search.predicate.spatial;
 
 import org.hibernate.search.engine.backend.index.spi.IndexSearchTarget;
-import org.hibernate.search.engine.logging.spi.FailureContexts;
+import org.hibernate.search.engine.logging.spi.EventContexts;
 import org.hibernate.search.engine.search.DocumentReference;
 import org.hibernate.search.engine.search.SearchQuery;
 import org.hibernate.search.engine.spatial.DistanceUnit;
@@ -90,7 +90,7 @@ public class SpatialWithinCircleSearchPredicateIT extends AbstractSpatialWithinS
 				.isInstanceOf( SearchException.class )
 				.hasMessageContaining( "Spatial predicates are not supported by" )
 				.satisfies( FailureReportUtils.hasContext(
-						FailureContexts.fromIndexFieldAbsolutePath( "string" )
+						EventContexts.fromIndexFieldAbsolutePath( "string" )
 				) );
 	}
 