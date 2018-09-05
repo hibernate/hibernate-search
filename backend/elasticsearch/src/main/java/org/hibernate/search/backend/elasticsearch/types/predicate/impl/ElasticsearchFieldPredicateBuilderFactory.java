hibernate/hibernate-search-6-poc

HSEARCH-3087 Add support for within bounding box spatial predicates

backend-elasticsearch/src/main/java/org/hibernate/search/v6poc/backend/elasticsearch/types/predicate/impl/ElasticsearchFieldPredicateBuilderFactory.java
@@ -9,6 +9,7 @@ package org.hibernate.search.v6poc.backend.elasticsearch.types.predicate.impl;
 import org.hibernate.search.v6poc.backend.elasticsearch.search.predicate.impl.ElasticsearchSearchPredicateCollector;
 import org.hibernate.search.v6poc.search.predicate.spi.MatchPredicateBuilder;
 import org.hibernate.search.v6poc.search.predicate.spi.RangePredicateBuilder;
+import org.hibernate.search.v6poc.search.predicate.spi.SpatialWithinBoundingBoxPredicateBuilder;
 import org.hibernate.search.v6poc.search.predicate.spi.SpatialWithinCirclePredicateBuilder;
 import org.hibernate.search.v6poc.search.predicate.spi.SpatialWithinPolygonPredicateBuilder;
 
@@ -21,4 +22,6 @@ public interface ElasticsearchFieldPredicateBuilderFactory {
 	SpatialWithinCirclePredicateBuilder<ElasticsearchSearchPredicateCollector> createSpatialWithinCirclePredicateBuilder(String absoluteFieldPath);
 
 	SpatialWithinPolygonPredicateBuilder<ElasticsearchSearchPredicateCollector> createSpatialWithinPolygonPredicateBuilder(String absoluteFieldPath);
+
+	SpatialWithinBoundingBoxPredicateBuilder<ElasticsearchSearchPredicateCollector> createSpatialWithinBoundingBoxPredicateBuilder(String absoluteFieldPath);
 }

backend-lucene/src/main/java/org/hibernate/search/v6poc/backend/lucene/types/predicate/impl/LuceneFieldPredicateBuilderFactory.java
@@ -9,6 +9,7 @@ package org.hibernate.search.v6poc.backend.lucene.types.predicate.impl;
 import org.hibernate.search.v6poc.backend.lucene.search.predicate.impl.LuceneSearchPredicateCollector;
 import org.hibernate.search.v6poc.search.predicate.spi.MatchPredicateBuilder;
 import org.hibernate.search.v6poc.search.predicate.spi.RangePredicateBuilder;
+import org.hibernate.search.v6poc.search.predicate.spi.SpatialWithinBoundingBoxPredicateBuilder;
 import org.hibernate.search.v6poc.search.predicate.spi.SpatialWithinCirclePredicateBuilder;
 import org.hibernate.search.v6poc.search.predicate.spi.SpatialWithinPolygonPredicateBuilder;
 
@@ -24,6 +25,8 @@ public interface LuceneFieldPredicateBuilderFactory {
 	SpatialWithinCirclePredicateBuilder<LuceneSearchPredicateCollector> createSpatialWithinCirclePredicateBuilder(String absoluteFieldPath);
 
 	SpatialWithinPolygonPredicateBuilder<LuceneSearchPredicateCollector> createSpatialWithinPolygonPredicateBuilder(String absoluteFieldPath);
+
+	SpatialWithinBoundingBoxPredicateBuilder<LuceneSearchPredicateCollector> createSpatialWithinBoundingBoxPredicateBuilder(String absoluteFieldPath);
 
 	// equals()/hashCode() needs to be implemented if the predicate factory is not a singleton
 

engine/src/main/java/org/hibernate/search/v6poc/backend/spatial/GeoBoundingBox.java
@@ -1,0 +1,14 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.v6poc.backend.spatial;
+
+public interface GeoBoundingBox {
+
+	GeoPoint getTopLeft();
+
+	GeoPoint getBottomRight();
+}

engine/src/main/java/org/hibernate/search/v6poc/search/dsl/predicate/SpatialWithinBoundingBoxPredicateContext.java
@@ -1,0 +1,18 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.v6poc.search.dsl.predicate;
+
+
+/**
+ * The context used when for closing a within bounding box spatial predicate.
+ *
+ * @param <N> The type of the next context.
+ */
+public interface SpatialWithinBoundingBoxPredicateContext<N> {
+
+	N end();
+}

engine/src/main/java/org/hibernate/search/v6poc/search/dsl/predicate/SpatialWithinPredicateFieldSetContext.java
@@ -7,8 +7,10 @@
 package org.hibernate.search.v6poc.search.dsl.predicate;
 
 import org.hibernate.search.v6poc.backend.spatial.DistanceUnit;
+import org.hibernate.search.v6poc.backend.spatial.GeoBoundingBox;
 import org.hibernate.search.v6poc.backend.spatial.GeoPoint;
 import org.hibernate.search.v6poc.backend.spatial.GeoPolygon;
+import org.hibernate.search.v6poc.backend.spatial.ImmutableGeoBoundingBox;
 import org.hibernate.search.v6poc.backend.spatial.ImmutableGeoPoint;
 import org.hibernate.search.v6poc.util.impl.common.Contracts;
 
@@ -40,4 +42,11 @@ public interface SpatialWithinPredicateFieldSetContext<N> extends MultiFieldPred
 	}
 
 	SpatialWithinPolygonPredicateContext<N> polygon(GeoPolygon polygon);
+
+	SpatialWithinBoundingBoxPredicateContext<N> boundingBox(GeoBoundingBox boundingBox);
+
+	default SpatialWithinBoundingBoxPredicateContext<N> boundingBox(double topLeftLatitude, double topLeftLongitude, double bottomRightLatitude,
+			double bottomRightLongitude) {
+		return boundingBox( new ImmutableGeoBoundingBox( topLeftLatitude, topLeftLongitude, bottomRightLatitude, bottomRightLongitude ) );
+	}
 }

engine/src/main/java/org/hibernate/search/v6poc/search/dsl/predicate/impl/SpatialWithinPredicateFieldSetContextImpl.java
@@ -12,13 +12,16 @@ import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
+import org.hibernate.search.v6poc.backend.spatial.GeoBoundingBox;
 import org.hibernate.search.v6poc.backend.spatial.GeoPoint;
 import org.hibernate.search.v6poc.backend.spatial.GeoPolygon;
+import org.hibernate.search.v6poc.search.dsl.predicate.SpatialWithinBoundingBoxPredicateContext;
 import org.hibernate.search.v6poc.search.dsl.predicate.SpatialWithinCirclePredicateContext;
 import org.hibernate.search.v6poc.search.dsl.predicate.SpatialWithinPolygonPredicateContext;
 import org.hibernate.search.v6poc.search.dsl.predicate.SpatialWithinPredicateFieldSetContext;
 import org.hibernate.search.v6poc.search.predicate.spi.SearchPredicateBuilder;
 import org.hibernate.search.v6poc.search.predicate.spi.SearchPredicateFactory;
+import org.hibernate.search.v6poc.search.predicate.spi.SpatialWithinBoundingBoxPredicateBuilder;
 import org.hibernate.search.v6poc.search.predicate.spi.SpatialWithinCirclePredicateBuilder;
 import org.hibernate.search.v6poc.search.predicate.spi.SpatialWithinPolygonPredicateBuilder;
 import org.hibernate.search.v6poc.util.impl.common.CollectionHelper;
@@ -70,6 +73,13 @@ class SpatialWithinPredicateFieldSetContextImpl<N, C>
 	}
 
 	@Override
+	public SpatialWithinBoundingBoxPredicateContext<N> boundingBox(GeoBoundingBox boundingBox) {
+		Contracts.assertNotNull( boundingBox, "boundingBox" );
+
+		return commonState.boundingBox( boundingBox );
+	}
+
+	@Override
 	public void contributePredicateBuilders(Consumer<SearchPredicateBuilder<? super C>> collector) {
 		queryBuilders.forEach( collector );
 	}
@@ -89,6 +99,17 @@ class SpatialWithinPredicateFieldSetContextImpl<N, C>
 		for ( String absoluteFieldPath : absoluteFieldPaths ) {
 			SpatialWithinPolygonPredicateBuilder<C> predicateBuilder = commonState.getFactory().spatialWithinPolygon( absoluteFieldPath );
 			predicateBuilder.polygon( polygon );
+			if ( boost != null ) {
+				predicateBuilder.boost( boost );
+			}
+			queryBuilders.add( predicateBuilder );
+		}
+	}
+
+	private void generateWithinBoundingBoxQueryBuilders(GeoBoundingBox boundingBox) {
+		for ( String absoluteFieldPath : absoluteFieldPaths ) {
+			SpatialWithinBoundingBoxPredicateBuilder<C> predicateBuilder = commonState.getFactory().spatialWithinBoundingBox( absoluteFieldPath );
+			predicateBuilder.boundingBox( boundingBox );
 			if ( boost != null ) {
 				predicateBuilder.boost( boost );
 			}
@@ -117,5 +138,14 @@ class SpatialWithinPredicateFieldSetContextImpl<N, C>
 
 			return new SpatialWithinPolygonPredicateContextImpl<N>( getNextContextProvider() );
 		}
+
+		public SpatialWithinBoundingBoxPredicateContext<N> boundingBox(GeoBoundingBox boundingBox) {
+			for ( SpatialWithinPredicateFieldSetContextImpl<N, C> fieldSetContext : getFieldSetContexts() ) {
+				fieldSetContext.generateWithinBoundingBoxQueryBuilders( boundingBox );
+			}
+
+			return new SpatialWithinBoundingBoxPredicateContextImpl<N>( getNextContextProvider() );
+		}
 	}
+
 }

util/internal/integrationtest/common/src/main/java/org/hibernate/search/v6poc/util/impl/integrationtest/common/stub/backend/search/predicate/impl/StubSearchPredicateFactory.java
@@ -15,6 +15,7 @@ import org.hibernate.search.v6poc.search.predicate.spi.NestedPredicateBuilder;
 import org.hibernate.search.v6poc.search.predicate.spi.RangePredicateBuilder;
 import org.hibernate.search.v6poc.search.predicate.spi.SearchPredicateContributor;
 import org.hibernate.search.v6poc.search.predicate.spi.SearchPredicateFactory;
+import org.hibernate.search.v6poc.search.predicate.spi.SpatialWithinBoundingBoxPredicateBuilder;
 import org.hibernate.search.v6poc.search.predicate.spi.SpatialWithinCirclePredicateBuilder;
 import org.hibernate.search.v6poc.search.predicate.spi.SpatialWithinPolygonPredicateBuilder;
 
@@ -58,6 +59,11 @@ public class StubSearchPredicateFactory implements SearchPredicateFactory<StubQu
 
 	@Override
 	public SpatialWithinPolygonPredicateBuilder<StubQueryElementCollector> spatialWithinPolygon(String absoluteFieldPath) {
+		return new StubPredicateBuilder();
+	}
+
+	@Override
+	public SpatialWithinBoundingBoxPredicateBuilder<StubQueryElementCollector> spatialWithinBoundingBox(String absoluteFieldPath) {
 		return new StubPredicateBuilder();
 	}
 

hibernate/hibernate-search

backend/elasticsearch/src/main/java/org/hibernate/search/backend/elasticsearch/types/predicate/impl/ElasticsearchFieldPredicateBuilderFactory.java
@@ -9,6 +9,7 @@ package org.hibernate.search.backend.elasticsearch.types.predicate.impl;
 import org.hibernate.search.backend.elasticsearch.search.predicate.impl.ElasticsearchSearchPredicateCollector;
 import org.hibernate.search.engine.search.predicate.spi.MatchPredicateBuilder;
 import org.hibernate.search.engine.search.predicate.spi.RangePredicateBuilder;
+import org.hibernate.search.engine.search.predicate.spi.SpatialWithinBoundingBoxPredicateBuilder;
 import org.hibernate.search.engine.search.predicate.spi.SpatialWithinCirclePredicateBuilder;
 import org.hibernate.search.engine.search.predicate.spi.SpatialWithinPolygonPredicateBuilder;
 
@@ -21,4 +22,6 @@ public interface ElasticsearchFieldPredicateBuilderFactory {
 	SpatialWithinCirclePredicateBuilder<ElasticsearchSearchPredicateCollector> createSpatialWithinCirclePredicateBuilder(String absoluteFieldPath);
 
 	SpatialWithinPolygonPredicateBuilder<ElasticsearchSearchPredicateCollector> createSpatialWithinPolygonPredicateBuilder(String absoluteFieldPath);
+
+	SpatialWithinBoundingBoxPredicateBuilder<ElasticsearchSearchPredicateCollector> createSpatialWithinBoundingBoxPredicateBuilder(String absoluteFieldPath);
 }

backend/lucene/src/main/java/org/hibernate/search/backend/lucene/types/predicate/impl/LuceneFieldPredicateBuilderFactory.java
@@ -9,6 +9,7 @@ package org.hibernate.search.backend.lucene.types.predicate.impl;
 import org.hibernate.search.backend.lucene.search.predicate.impl.LuceneSearchPredicateCollector;
 import org.hibernate.search.engine.search.predicate.spi.MatchPredicateBuilder;
 import org.hibernate.search.engine.search.predicate.spi.RangePredicateBuilder;
+import org.hibernate.search.engine.search.predicate.spi.SpatialWithinBoundingBoxPredicateBuilder;
 import org.hibernate.search.engine.search.predicate.spi.SpatialWithinCirclePredicateBuilder;
 import org.hibernate.search.engine.search.predicate.spi.SpatialWithinPolygonPredicateBuilder;
 
@@ -24,6 +25,8 @@ public interface LuceneFieldPredicateBuilderFactory {
 	SpatialWithinCirclePredicateBuilder<LuceneSearchPredicateCollector> createSpatialWithinCirclePredicateBuilder(String absoluteFieldPath);
 
 	SpatialWithinPolygonPredicateBuilder<LuceneSearchPredicateCollector> createSpatialWithinPolygonPredicateBuilder(String absoluteFieldPath);
+
+	SpatialWithinBoundingBoxPredicateBuilder<LuceneSearchPredicateCollector> createSpatialWithinBoundingBoxPredicateBuilder(String absoluteFieldPath);
 
 	// equals()/hashCode() needs to be implemented if the predicate factory is not a singleton
 

engine/src/main/java/org/hibernate/search/engine/backend/spatial/GeoBoundingBox.java
@@ -1,0 +1,14 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.engine.backend.spatial;
+
+public interface GeoBoundingBox {
+
+	GeoPoint getTopLeft();
+
+	GeoPoint getBottomRight();
+}

engine/src/main/java/org/hibernate/search/engine/search/dsl/predicate/SpatialWithinBoundingBoxPredicateContext.java
@@ -1,0 +1,18 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.engine.search.dsl.predicate;
+
+
+/**
+ * The context used when for closing a within bounding box spatial predicate.
+ *
+ * @param <N> The type of the next context.
+ */
+public interface SpatialWithinBoundingBoxPredicateContext<N> {
+
+	N end();
+}

engine/src/main/java/org/hibernate/search/engine/search/dsl/predicate/SpatialWithinPredicateFieldSetContext.java
@@ -7,8 +7,10 @@
 package org.hibernate.search.engine.search.dsl.predicate;
 
 import org.hibernate.search.engine.backend.spatial.DistanceUnit;
+import org.hibernate.search.engine.backend.spatial.GeoBoundingBox;
 import org.hibernate.search.engine.backend.spatial.GeoPoint;
 import org.hibernate.search.engine.backend.spatial.GeoPolygon;
+import org.hibernate.search.engine.backend.spatial.ImmutableGeoBoundingBox;
 import org.hibernate.search.engine.backend.spatial.ImmutableGeoPoint;
 import org.hibernate.search.util.impl.common.Contracts;
 
@@ -40,4 +42,11 @@ public interface SpatialWithinPredicateFieldSetContext<N> extends MultiFieldPred
 	}
 
 	SpatialWithinPolygonPredicateContext<N> polygon(GeoPolygon polygon);
+
+	SpatialWithinBoundingBoxPredicateContext<N> boundingBox(GeoBoundingBox boundingBox);
+
+	default SpatialWithinBoundingBoxPredicateContext<N> boundingBox(double topLeftLatitude, double topLeftLongitude, double bottomRightLatitude,
+			double bottomRightLongitude) {
+		return boundingBox( new ImmutableGeoBoundingBox( topLeftLatitude, topLeftLongitude, bottomRightLatitude, bottomRightLongitude ) );
+	}
 }

engine/src/main/java/org/hibernate/search/engine/search/dsl/predicate/impl/SpatialWithinPredicateFieldSetContextImpl.java
@@ -12,13 +12,16 @@ import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
+import org.hibernate.search.engine.backend.spatial.GeoBoundingBox;
 import org.hibernate.search.engine.backend.spatial.GeoPoint;
 import org.hibernate.search.engine.backend.spatial.GeoPolygon;
+import org.hibernate.search.engine.search.dsl.predicate.SpatialWithinBoundingBoxPredicateContext;
 import org.hibernate.search.engine.search.dsl.predicate.SpatialWithinCirclePredicateContext;
 import org.hibernate.search.engine.search.dsl.predicate.SpatialWithinPolygonPredicateContext;
 import org.hibernate.search.engine.search.dsl.predicate.SpatialWithinPredicateFieldSetContext;
 import org.hibernate.search.engine.search.predicate.spi.SearchPredicateBuilder;
 import org.hibernate.search.engine.search.predicate.spi.SearchPredicateFactory;
+import org.hibernate.search.engine.search.predicate.spi.SpatialWithinBoundingBoxPredicateBuilder;
 import org.hibernate.search.engine.search.predicate.spi.SpatialWithinCirclePredicateBuilder;
 import org.hibernate.search.engine.search.predicate.spi.SpatialWithinPolygonPredicateBuilder;
 import org.hibernate.search.util.impl.common.CollectionHelper;
@@ -70,6 +73,13 @@ class SpatialWithinPredicateFieldSetContextImpl<N, C>
 	}
 
 	@Override
+	public SpatialWithinBoundingBoxPredicateContext<N> boundingBox(GeoBoundingBox boundingBox) {
+		Contracts.assertNotNull( boundingBox, "boundingBox" );
+
+		return commonState.boundingBox( boundingBox );
+	}
+
+	@Override
 	public void contributePredicateBuilders(Consumer<SearchPredicateBuilder<? super C>> collector) {
 		queryBuilders.forEach( collector );
 	}
@@ -89,6 +99,17 @@ class SpatialWithinPredicateFieldSetContextImpl<N, C>
 		for ( String absoluteFieldPath : absoluteFieldPaths ) {
 			SpatialWithinPolygonPredicateBuilder<C> predicateBuilder = commonState.getFactory().spatialWithinPolygon( absoluteFieldPath );
 			predicateBuilder.polygon( polygon );
+			if ( boost != null ) {
+				predicateBuilder.boost( boost );
+			}
+			queryBuilders.add( predicateBuilder );
+		}
+	}
+
+	private void generateWithinBoundingBoxQueryBuilders(GeoBoundingBox boundingBox) {
+		for ( String absoluteFieldPath : absoluteFieldPaths ) {
+			SpatialWithinBoundingBoxPredicateBuilder<C> predicateBuilder = commonState.getFactory().spatialWithinBoundingBox( absoluteFieldPath );
+			predicateBuilder.boundingBox( boundingBox );
 			if ( boost != null ) {
 				predicateBuilder.boost( boost );
 			}
@@ -117,5 +138,14 @@ class SpatialWithinPredicateFieldSetContextImpl<N, C>
 
 			return new SpatialWithinPolygonPredicateContextImpl<N>( getNextContextProvider() );
 		}
+
+		public SpatialWithinBoundingBoxPredicateContext<N> boundingBox(GeoBoundingBox boundingBox) {
+			for ( SpatialWithinPredicateFieldSetContextImpl<N, C> fieldSetContext : getFieldSetContexts() ) {
+				fieldSetContext.generateWithinBoundingBoxQueryBuilders( boundingBox );
+			}
+
+			return new SpatialWithinBoundingBoxPredicateContextImpl<N>( getNextContextProvider() );
+		}
 	}
+
 }

util/internal/integrationtest/common/src/main/java/org/hibernate/search/util/impl/integrationtest/common/stub/backend/search/predicate/impl/StubSearchPredicateFactory.java
@@ -15,6 +15,7 @@ import org.hibernate.search.engine.search.predicate.spi.NestedPredicateBuilder;
 import org.hibernate.search.engine.search.predicate.spi.RangePredicateBuilder;
 import org.hibernate.search.engine.search.predicate.spi.SearchPredicateContributor;
 import org.hibernate.search.engine.search.predicate.spi.SearchPredicateFactory;
+import org.hibernate.search.engine.search.predicate.spi.SpatialWithinBoundingBoxPredicateBuilder;
 import org.hibernate.search.engine.search.predicate.spi.SpatialWithinCirclePredicateBuilder;
 import org.hibernate.search.engine.search.predicate.spi.SpatialWithinPolygonPredicateBuilder;
 
@@ -58,6 +59,11 @@ public class StubSearchPredicateFactory implements SearchPredicateFactory<StubQu
 
 	@Override
 	public SpatialWithinPolygonPredicateBuilder<StubQueryElementCollector> spatialWithinPolygon(String absoluteFieldPath) {
+		return new StubPredicateBuilder();
+	}
+
+	@Override
+	public SpatialWithinBoundingBoxPredicateBuilder<StubQueryElementCollector> spatialWithinBoundingBox(String absoluteFieldPath) {
 		return new StubPredicateBuilder();
 	}
 