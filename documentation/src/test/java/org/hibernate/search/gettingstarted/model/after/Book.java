b933b89c32b63c65f14174ee2e89d9ae1b59e5c5
Davide D'Alto <davide@hibernate.org>
2018-10-19 15:39:29 +0100

    HSEARCH-2328 Include some entities in the Getting Started section
      Mainly to test that including java source code works.

---
+++ documentation/src/test/java/org/hibernate/search/gettingstarted/model/after/Book.java
@@ -0,0 +1,46 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.gettingstarted.model.after;
+
+// tag::book-entity-after-hsearch-gettingstarted[]
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import org.hibernate.search.engine.backend.document.model.dsl.Store;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.GenericField;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.IndexedEmbedded;
+
+@Entity
+@Indexed
+public class Book {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	@GenericField(store = Store.NO)
+	private String title;
+
+	@GenericField(store = Store.NO)
+	private String subtitle;
+
+	@IndexedEmbedded
+	@ManyToMany
+	private Set<Author> authors = new HashSet<Author>();
+
+	public Book() {
+	}
+
+	// ...
+}
+// end::book-entity-after-hsearch-gettingstarted[]
