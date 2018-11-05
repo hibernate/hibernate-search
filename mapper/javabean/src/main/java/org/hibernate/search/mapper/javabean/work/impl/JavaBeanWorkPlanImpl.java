ab07d7d5fd6652b7e92e3adfd9b0820980feb631
Yoann Rodière <yoann@hibernate.org>
2018-11-05 15:28:26 +0100

    HSEARCH-3398 Move PojoWorkPlan to SPI
    That's one more interface from the POJO module that will not leak into
    mapper APIs.

---
+++ mapper/javabean/src/main/java/org/hibernate/search/mapper/javabean/work/impl/JavaBeanWorkPlanImpl.java
@@ -0,0 +1,69 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.mapper.javabean.work.impl;
+
+import java.util.concurrent.CompletableFuture;
+
+import org.hibernate.search.mapper.javabean.work.JavaBeanWorkPlan;
+import org.hibernate.search.mapper.pojo.mapping.PojoWorkPlan;
+
+public class JavaBeanWorkPlanImpl implements JavaBeanWorkPlan {
+
+	private final PojoWorkPlan delegate;
+
+	public JavaBeanWorkPlanImpl(PojoWorkPlan delegate) {
+		this.delegate = delegate;
+	}
+
+	@Override
+	public void add(Object entity) {
+		delegate.add( entity );
+	}
+
+	@Override
+	public void add(Object id, Object entity) {
+		delegate.add( id, entity );
+	}
+
+	@Override
+	public void update(Object entity) {
+		delegate.update( entity );
+	}
+
+	@Override
+	public void update(Object id, Object entity) {
+		delegate.update( id, entity );
+	}
+
+	@Override
+	public void update(Object entity, String... dirtyPaths) {
+		delegate.update( entity, dirtyPaths );
+	}
+
+	@Override
+	public void update(Object id, Object entity, String... dirtyPaths) {
+		delegate.update( id, entity, dirtyPaths );
+	}
+
+	@Override
+	public void delete(Object entity) {
+		delegate.delete( entity );
+	}
+
+	@Override
+	public void delete(Object id, Object entity) {
+		delegate.delete( id, entity );
+	}
+
+	public void prepare() {
+		delegate.prepare();
+	}
+
+	public CompletableFuture<?> execute() {
+		return delegate.execute();
+	}
+}
