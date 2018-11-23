bda21a841ea7fbe93c4550a4bc783063c30bda74
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2018-11-23 10:46:06 +0100

    HSEARCH-3268 Test mass indexer API (base case)

---
+++ integrationtest/mapper/orm/src/test/java/org/hibernate/search/integrationtest/mapper/orm/batchindexing/BasicBatchIndexingIT.java
@@ -0,0 +1,143 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.integrationtest.mapper.orm.batchindexing;
+
+import static org.assertj.core.api.Fail.fail;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.search.integrationtest.mapper.orm.query.projection.LoadingProjectionIT;
+import org.hibernate.search.mapper.orm.Search;
+import org.hibernate.search.mapper.orm.cfg.IndexingStrategyConfiguration;
+import org.hibernate.search.mapper.orm.cfg.SearchOrmSettings;
+import org.hibernate.search.mapper.orm.hibernate.FullTextSession;
+import org.hibernate.search.mapper.orm.massindexing.MassIndexer;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.GenericField;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
+import org.hibernate.search.util.impl.integrationtest.common.rule.BackendMock;
+import org.hibernate.search.util.impl.integrationtest.orm.OrmSetupHelper;
+import org.hibernate.search.util.impl.integrationtest.orm.OrmUtils;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * Very basic test to probe an use of {@link MassIndexer} api.
+ */
+public class BasicBatchIndexingIT {
+
+	public static final String TITLE_1 = "Oliver Twist";
+	public static final String AUTHOR_1 = "Charles Dickens";
+	public static final String TITLE_2 = "Ulysses";
+	public static final String AUTHOR_2 = "James Joyce";
+	public static final String TITLE_3 = "Frankenstein";
+	public static final String AUTHOR_3 = "Mary Shelley";
+
+	@Rule
+	public BackendMock backendMock = new BackendMock( "stubBackend" );
+
+	@Rule
+	public OrmSetupHelper ormSetupHelper = new OrmSetupHelper();
+
+	private SessionFactory sessionFactory;
+
+	@Before
+	public void setup() {
+		backendMock.expectAnySchema( LoadingProjectionIT.Book.INDEX );
+
+		sessionFactory = ormSetupHelper.withBackendMock( backendMock )
+				.withPropertyRadical( SearchOrmSettings.Radicals.INDEXING_STRATEGY, IndexingStrategyConfiguration.MANUAL )
+				.setup( Book.class );
+
+		backendMock.verifyExpectationsMet();
+
+		initData();
+	}
+
+	@Test
+	public void defaultMassIndexerStartAndWait() throws Exception {
+		OrmUtils.withinSession( sessionFactory, session -> {
+			FullTextSession ftSession = Search.getFullTextSession( session );
+			MassIndexer indexer = ftSession.createIndexer();
+
+			backendMock.expectWorksAnyOrder( LoadingProjectionIT.Book.INDEX )
+					.add( "1", b -> b
+							.field( "title", TITLE_1 )
+							.field( "author", AUTHOR_1 )
+					)
+					.add( "2", b -> b
+							.field( "title", TITLE_2 )
+							.field( "author", AUTHOR_2 )
+					)
+					.add( "3", b -> b
+							.field( "title", TITLE_3 )
+							.field( "author", AUTHOR_3 )
+					)
+					.preparedThenExecuted();
+
+			try {
+				indexer.startAndWait();
+			}
+			catch (InterruptedException e) {
+				fail( "Unexpected InterruptedException: " + e.getMessage() );
+			}
+
+		} );
+
+		backendMock.verifyExpectationsMet();
+	}
+
+	private void initData() {
+		OrmUtils.withinTransaction( sessionFactory, session -> {
+			session.persist( new Book( 1, TITLE_1, AUTHOR_1 ) );
+			session.persist( new Book( 2, TITLE_2, AUTHOR_2 ) );
+			session.persist( new Book( 3, TITLE_3, AUTHOR_3 ) );
+		} );
+	}
+
+	@Entity
+	@Table(name = "book")
+	@Indexed(index = LoadingProjectionIT.Book.INDEX)
+	public static class Book {
+
+		public static final String INDEX = "Book";
+
+		@Id
+		private Integer id;
+
+		@GenericField
+		private String title;
+
+		@GenericField
+		private String author;
+
+		public Book() {
+		}
+
+		public Book(Integer id, String title, String author) {
+			this.id = id;
+			this.title = title;
+			this.author = author;
+		}
+
+		public Integer getId() {
+			return id;
+		}
+
+		public String getTitle() {
+			return title;
+		}
+
+		public String getAuthor() {
+			return author;
+		}
+	}
+}
