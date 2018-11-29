1256ff8a5071549142e7ee9ebef5b501c8e929ad
Yoann Rodière <yoann@hibernate.org>
2018-11-29 11:11:30 +0100

    HSEARCH-3101 Use aliases for backend types and make them point to the right backend factory

---
+++ backend/elasticsearch/src/main/java/org/hibernate/search/backend/elasticsearch/impl/ElasticsearchBeanConfigurer.java
@@ -0,0 +1,21 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.backend.elasticsearch.impl;
+
+import org.hibernate.search.engine.backend.spi.BackendFactory;
+import org.hibernate.search.engine.environment.bean.spi.BeanConfigurationContext;
+import org.hibernate.search.engine.environment.bean.spi.BeanConfigurer;
+
+public class ElasticsearchBeanConfigurer implements BeanConfigurer {
+	@Override
+	public void configure(BeanConfigurationContext context) {
+		context.define(
+				BackendFactory.class, "elasticsearch",
+				factoryCreationContext -> new ElasticsearchBackendFactory()
+		);
+	}
+}
