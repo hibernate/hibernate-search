56f99de5a5f0480d75e9bee88eceb25d11f299a1
Yoann Rodière <yoann@hibernate.org>
2018-11-30 10:43:15 +0100

    HSEARCH-3386 Add some javadoc to ConfigurationProperty

--- engine/src/main/java/org/hibernate/search/engine/cfg/spi/ConfigurationProperty.java
+++ engine/src/main/java/org/hibernate/search/engine/cfg/spi/ConfigurationProperty.java
@@ -13,12 +13,45 @@ import org.hibernate.search.engine.cfg.impl.KeyContextImpl;
 
 public interface ConfigurationProperty<T> {
 
+	/**
+	 * Get the value of this configuration property.
+	 *
+	 * @param source A configuration source.
+	 * @return The value of this property according to the given source.
+	 */
 	T get(ConfigurationPropertySource source);
 
+	/**
+	 * Resolve the key for this configuration property
+	 * as registered in the underlying configuration source,
+	 * if possible.
+	 * <p>
+	 * Useful for error messages addressed to the user.
+	 *
+	 * @param source A configuration source.
+	 * @return The value of this property according to the given source.
+	 * @see ConfigurationPropertySource#resolve(String)
+	 */
 	Optional<String> resolve(ConfigurationPropertySource source);
 
+	/**
+	 * Resolve the key for this configuration property
+	 * as registered in the underlying configuration source,
+	 * or, if not possible, just return the "raw" key passed to {@link #forKey(String)}.
+	 * <p>
+	 * Useful for debugging.
+	 *
+	 * @param source A configuration source.
+	 * @return The value of this property according to the given source.
+	 * @see #resolve(ConfigurationPropertySource)
+	 */
 	String resolveOrRaw(ConfigurationPropertySource source);
 
+	/**
+	 * Start the creation of a configuration property.
+	 * @param key The key for that configuration property.
+	 * @return A context allowing to further define the configuration property.
+	 */
 	static KeyContext forKey(String key) {
 		return new KeyContextImpl( key );
 	}
