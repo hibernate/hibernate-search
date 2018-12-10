3e962d4e468ca66383763e79cc4b97c8670a6cbb
Yoann Rodière <yoann@hibernate.org>
2018-12-10 10:51:26 +0100

    HSEARCH-3268 Correctly close Session/SearchManager resources in the mass indexer
    Problems reported by Sonar.

--- mapper/orm/src/main/java/org/hibernate/search/mapper/orm/massindexing/impl/OptionallyWrapInJTATransaction.java
+++ mapper/orm/src/main/java/org/hibernate/search/mapper/orm/massindexing/impl/OptionallyWrapInJTATransaction.java
@@ -50,20 +50,16 @@ public class OptionallyWrapInJTATransaction extends ErrorHandledRunnable {
 	@Override
 	public void runWithErrorHandler() throws Exception {
 		if ( wrapInTransaction ) {
-			final StatelessSession statelessSession = batchContext
-					.factory
-					.withStatelessOptions()
+			try ( StatelessSession statelessSession = batchContext.factory.withStatelessOptions()
 					.tenantIdentifier( tenantId )
-					.openStatelessSession();
-
-			if ( transactionTimeout != null ) {
-				batchContext.transactionManager.setTransactionTimeout( transactionTimeout );
+					.openStatelessSession() ) {
+				if ( transactionTimeout != null ) {
+					batchContext.transactionManager.setTransactionTimeout( transactionTimeout );
+				}
+				batchContext.transactionManager.begin();
+				statelessSessionAwareRunnable.run( statelessSession );
+				batchContext.transactionManager.commit();
 			}
-			batchContext.transactionManager.begin();
-			statelessSessionAwareRunnable.run( statelessSession );
-			batchContext.transactionManager.commit();
-
-			statelessSession.close();
 		}
 		else {
 			statelessSessionAwareRunnable.run( null );
