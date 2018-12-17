cab9844074d2378d03e831bf5e736ba5fb77cbef
Yoann Rodière <yoann@hibernate.org>
2018-12-17 10:52:13 +0100

    HSEARCH-3455 Test that source filtering is effective

---
+++ integrationtest/backend/elasticsearch/src/test/java/org/hibernate/search/integrationtest/backend/elasticsearch/search/query/ElasticsearchSearchQueryIT.java
@@ -0,0 +1,103 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.integrationtest.backend.elasticsearch.search.query;
+
+import org.hibernate.search.backend.elasticsearch.cfg.spi.SearchBackendElasticsearchSpiSettings;
+import org.hibernate.search.backend.elasticsearch.client.impl.Paths;
+import org.hibernate.search.backend.elasticsearch.client.spi.ElasticsearchRequest;
+import org.hibernate.search.backend.elasticsearch.util.spi.URLEncodedString;
+import org.hibernate.search.engine.backend.document.IndexFieldAccessor;
+import org.hibernate.search.engine.backend.document.model.dsl.IndexSchemaElement;
+import org.hibernate.search.engine.backend.document.model.dsl.Projectable;
+import org.hibernate.search.engine.search.SearchQuery;
+import org.hibernate.search.integrationtest.backend.elasticsearch.testsupport.util.ElasticsearchClientMock;
+import org.hibernate.search.integrationtest.backend.elasticsearch.testsupport.util.ElasticsearchRequestAssertionMode;
+import org.hibernate.search.integrationtest.backend.tck.util.rule.SearchSetupHelper;
+import org.hibernate.search.util.impl.integrationtest.common.stub.mapper.StubMappingIndexManager;
+import org.hibernate.search.util.impl.integrationtest.common.stub.mapper.StubMappingSearchTarget;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+/**
+ * Test the content of generated Elasticsearch search queries.
+ */
+public class ElasticsearchSearchQueryIT {
+
+	private static final String BACKEND_NAME = "myElasticsearchBackend";
+	private static final String INDEX_NAME = "indexname";
+
+	@Rule
+	public SearchSetupHelper setupHelper = new SearchSetupHelper();
+
+	@Rule
+	public ElasticsearchClientMock clientMock = new ElasticsearchClientMock();
+
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+
+	private IndexAccessors indexAccessors;
+	private StubMappingIndexManager indexManager;
+
+	@Before
+	public void setup() {
+		setupHelper.withDefaultConfiguration( BACKEND_NAME )
+				.withBackendProperty(
+						BACKEND_NAME, SearchBackendElasticsearchSpiSettings.CLIENT_FACTORY, clientMock.getFactory()
+				)
+				.withIndex(
+						"MappedType", INDEX_NAME,
+						ctx -> this.indexAccessors = new IndexAccessors( ctx.getSchemaElement() ),
+						indexManager -> this.indexManager = indexManager
+				)
+				.setup();
+	}
+
+	@Test
+	public void projection_sourceFiltering() {
+		StubMappingSearchTarget searchTarget = indexManager.createSearchTarget();
+
+		SearchQuery<Object> query = searchTarget.query()
+				.asProjection( f -> f.field( "string" ).toProjection() )
+				.predicate( f -> f.matchAll().toPredicate() )
+				.build();
+
+		clientMock.expectNext(
+				ElasticsearchRequest.post()
+						.pathComponent( URLEncodedString.fromString( INDEX_NAME ) )
+						.pathComponent( Paths._SEARCH )
+						.body( new Gson().fromJson( "{'_source':['string']}", JsonObject.class ) )
+						.build(),
+				ElasticsearchRequestAssertionMode.EXTENSIBLE
+		);
+
+		query.execute();
+	}
+
+	private static class IndexAccessors {
+		final IndexFieldAccessor<Integer> integer;
+		final IndexFieldAccessor<String> string;
+
+		IndexAccessors(IndexSchemaElement root) {
+			integer = root.field( "integer" )
+					.asInteger()
+					.projectable( Projectable.YES )
+					.createAccessor();
+			string = root.field( "string" )
+					.asString()
+					.projectable( Projectable.YES )
+					.createAccessor();
+		}
+	}
+
+
+}
