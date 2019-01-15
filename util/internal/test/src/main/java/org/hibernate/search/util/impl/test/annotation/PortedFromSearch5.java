0bcb227376af4fd7796052ca6fac9933ecedb0c3
Yoann Rodière <yoann@hibernate.org>
2019-01-15 18:32:27 +0100

    HSEARCH-3052 Add annotations and test categories to mark tests ported over from Search 5

---
+++ util/internal/test/src/main/java/org/hibernate/search/util/impl/test/annotation/PortedFromSearch5.java
@@ -0,0 +1,26 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.util.impl.test.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Indicates that a test was ported from another test from Search 5.
+ */
+@Retention(RetentionPolicy.SOURCE)
+@Target({ ElementType.METHOD, ElementType.TYPE })
+public @interface PortedFromSearch5 {
+
+	/**
+	 * @return A reference (FQCN + method if applicable) to the original test in Search 5.
+	 */
+	String original();
+
+}
