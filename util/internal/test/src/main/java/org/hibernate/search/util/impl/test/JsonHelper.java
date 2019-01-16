9e6a8a1b2a2fd673f8892da0ad67f8a776c15ff7
Yoann Rodière <yoann@hibernate.org>
2019-01-16 18:07:14 +0100

    HSEARCH-3052 Copy JsonHelper from Search 5

---
+++ util/internal/test/src/main/java/org/hibernate/search/util/impl/test/JsonHelper.java
@@ -0,0 +1,47 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.util.impl.test;
+
+import static org.junit.Assert.fail;
+
+import org.json.JSONException;
+import org.skyscreamer.jsonassert.JSONCompare;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.skyscreamer.jsonassert.JSONCompareResult;
+
+/**
+ * Helper functionality around JSON documents.
+ *
+ * @author Gunnar Morling
+ */
+public class JsonHelper {
+
+	private JsonHelper() {
+	}
+
+	public static void assertJsonEquals(String expectedJson, String actualJson) {
+		assertJsonEquals( expectedJson, actualJson, JSONCompareMode.NON_EXTENSIBLE );
+	}
+
+	public static void assertJsonEqualsIgnoringUnknownFields(String expectedJson, String actualJson) {
+		assertJsonEquals( expectedJson, actualJson, JSONCompareMode.LENIENT );
+	}
+
+	private static void assertJsonEquals(String expectedJson, String actualJson, JSONCompareMode mode) {
+		try {
+			JSONCompareResult result = JSONCompare.compareJSON( expectedJson, actualJson, mode );
+
+			if ( result.failed() ) {
+				fail( result.getMessage() + "; Actual: " + actualJson );
+				throw new IllegalStateException( "This should never happen" );
+			}
+		}
+		catch (JSONException e) {
+			throw new RuntimeException( e );
+		}
+	}
+}
