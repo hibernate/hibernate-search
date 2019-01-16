41516ce30cfef1cfa1a1038bdab30da278e920c2
Yoann Rodière <yoann@hibernate.org>
2019-01-16 09:13:27 +0100

    HSEARCH-3052 Copy Elasticsearch index administration code from Search 5

---
+++ backend/elasticsearch/src/main/java/org/hibernate/search/backend/elasticsearch/index/admin/impl/ElasticsearchSchemaAccessor.java
@@ -0,0 +1,164 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.backend.elasticsearch.index.admin.impl;
+
+import java.lang.invoke.MethodHandles;
+import java.util.concurrent.CompletionException;
+import java.util.Collections;
+import java.util.Map;
+
+import org.hibernate.search.backend.elasticsearch.cfg.ElasticsearchIndexStatus;
+import org.hibernate.search.backend.elasticsearch.client.impl.Paths;
+import org.hibernate.search.backend.elasticsearch.document.model.impl.esnative.RootTypeMapping;
+import org.hibernate.search.backend.elasticsearch.index.settings.impl.esnative.IndexSettings;
+import org.hibernate.search.backend.elasticsearch.logging.impl.Log;
+import org.hibernate.search.backend.elasticsearch.orchestration.impl.ElasticsearchWorkOrchestrator;
+import org.hibernate.search.backend.elasticsearch.util.spi.URLEncodedString;
+import org.hibernate.search.backend.elasticsearch.work.builder.factory.impl.ElasticsearchWorkBuilderFactory;
+import org.hibernate.search.backend.elasticsearch.work.impl.ElasticsearchWork;
+import org.hibernate.search.backend.elasticsearch.work.result.impl.CreateIndexResult;
+import org.hibernate.search.util.impl.common.Futures;
+import org.hibernate.search.util.impl.common.LoggerFactory;
+import org.hibernate.search.util.impl.common.Throwables;
+
+/**
+ * A utility implementing primitives for the various {@code ElasticsearchSchema*Impl}.
+ * @author Gunnar Morling
+ * @author Yoann Rodiere
+ */
+public class ElasticsearchSchemaAccessor {
+
+	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
+
+	private final ElasticsearchWorkBuilderFactory workFactory;
+
+	private final ElasticsearchWorkOrchestrator orchestrator;
+
+	public ElasticsearchSchemaAccessor(ElasticsearchWorkBuilderFactory workFactory,
+			ElasticsearchWorkOrchestrator orchestrator) {
+		this.workFactory = workFactory;
+		this.orchestrator = orchestrator;
+	}
+
+	public void createIndex(URLEncodedString indexName, IndexSettings settings, ExecutionOptions executionOptions) {
+		ElasticsearchWork<?> work = workFactory.createIndex( indexName ).settings( settings ).build();
+		execute( work );
+	}
+
+	/**
+	 * @param indexName The name of the index
+	 * @param settings The settings for the newly created index
+	 * @param executionOptions The execution options
+	 * @return {@code true} if the index was actually created, {@code false} if it already existed.
+	 */
+	public boolean createIndexIfAbsent(URLEncodedString indexName, IndexSettings settings, ExecutionOptions executionOptions) {
+		ElasticsearchWork<CreateIndexResult> work = workFactory.createIndex( indexName )
+				.settings( settings )
+				.ignoreExisting()
+				.build();
+		CreateIndexResult result = execute( work );
+		return CreateIndexResult.CREATED.equals( result );
+	}
+
+	public boolean indexExists(URLEncodedString indexName) {
+		ElasticsearchWork<Boolean> work = workFactory.indexExists( indexName ).build();
+		return execute( work );
+	}
+
+	public IndexMetadata getCurrentIndexMetadata(URLEncodedString indexName) {
+		IndexMetadata indexMetadata = new IndexMetadata();
+		indexMetadata.setName( indexName );
+
+		ElasticsearchWork<RootTypeMapping> getMappingWork = workFactory.getIndexTypeMapping( indexName ).build();
+		try {
+			RootTypeMapping mapping = execute( getMappingWork );
+			// FIXME the mapping name does not make sense, it's always "_doc" and there's only just one. Change something here?
+			Map<String, RootTypeMapping> mappings = Collections.singletonMap( Paths.TYPE_NAME.original, mapping );
+			indexMetadata.setMappings( mappings );
+		}
+		catch (RuntimeException e) {
+			throw log.elasticsearchMappingRetrievalForValidationFailed( e );
+		}
+
+		ElasticsearchWork<IndexSettings> getSettingsWork = workFactory.getIndexSettings( indexName ).build();
+		try {
+			IndexSettings indexSettings = execute( getSettingsWork );
+			indexMetadata.setSettings( indexSettings );
+		}
+		catch (RuntimeException e) {
+			throw log.elasticsearchIndexSettingsRetrievalForValidationFailed( e );
+		}
+
+		return indexMetadata;
+	}
+
+	public void updateSettings(URLEncodedString indexName, IndexSettings settings) {
+		ElasticsearchWork<?> work = workFactory.putIndexSettings( indexName, settings ).build();
+
+		try {
+			execute( work );
+		}
+		catch (RuntimeException e) {
+			throw log.elasticsearchSettingsUpdateFailed( indexName, e );
+		}
+	}
+
+	public void putMapping(URLEncodedString indexName, URLEncodedString mappingName, RootTypeMapping mapping) {
+		// FIXME the mapping name does not make sense, we can't chose it. Change something here?
+		ElasticsearchWork<?> work = workFactory.putIndexTypeMapping( indexName, mapping ).build();
+
+		try {
+			execute( work );
+		}
+		catch (RuntimeException e) {
+			throw log.elasticsearchMappingCreationFailed( indexName.original, mappingName.original, e );
+		}
+	}
+
+	public void waitForIndexStatus(final URLEncodedString indexName, ExecutionOptions executionOptions) {
+		ElasticsearchIndexStatus requiredIndexStatus = executionOptions.getRequiredIndexStatus();
+		String timeoutAndUnit = executionOptions.getIndexManagementTimeoutInMs() + "ms";
+
+		ElasticsearchWork<?> work =
+				workFactory.waitForIndexStatusWork( indexName, requiredIndexStatus, timeoutAndUnit )
+				.build();
+
+		execute( work );
+	}
+
+	public void dropIndex(URLEncodedString indexName, ExecutionOptions executionOptions) {
+		ElasticsearchWork<?> work = workFactory.dropIndex( indexName ).build();
+		execute( work );
+	}
+
+	public void closeIndex(URLEncodedString indexName) {
+		ElasticsearchWork<?> work = workFactory.closeIndex( indexName ).build();
+		execute( work );
+		log.closedIndex( indexName );
+	}
+
+	public void openIndex(URLEncodedString indexName) {
+		try {
+			ElasticsearchWork<?> work = workFactory.openIndex( indexName ).build();
+			execute( work );
+		}
+		catch (RuntimeException e) {
+			log.openedIndex( indexName );
+			throw e;
+		}
+		log.openedIndex( indexName );
+	}
+
+	private <T> T execute(ElasticsearchWork<T> work) {
+		try {
+			return Futures.unwrappedExceptionJoin( orchestrator.submit( work ) );
+		}
+		catch (CompletionException e) {
+			throw Throwables.expectRuntimeException( e );
+		}
+	}
+}
---
+++ backend/elasticsearch/src/main/java/org/hibernate/search/backend/elasticsearch/index/admin/impl/ElasticsearchSchemaDropper.java
@@ -0,0 +1,39 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.backend.elasticsearch.index.admin.impl;
+
+import org.hibernate.search.backend.elasticsearch.util.spi.URLEncodedString;
+import org.hibernate.search.util.SearchException;
+
+/**
+ * An object responsible for dropping an indexes.
+ *
+ * @author Yoann Rodiere
+ */
+public interface ElasticsearchSchemaDropper {
+
+	/**
+	 * Drops an index, throwing an exception if dropping fails.
+	 *
+	 * @param indexName The name of the index to drop.
+	 * @param executionOptions The execution options, giving more context information.
+	 * @throws SearchException If an error occurs.
+	 */
+	void drop(URLEncodedString indexName, ExecutionOptions executionOptions);
+
+	/**
+	 * Drops an index, throwing an exception if dropping fails.
+	 *
+	 * <p>This method will skip operations silently if the index does not exist.
+	 *
+	 * @param indexName The name of the index to drop.
+	 * @param executionOptions The execution options, giving more context information.
+	 * @throws SearchException If an error occurs.
+	 */
+	void dropIfExisting(URLEncodedString indexName, ExecutionOptions executionOptions);
+
+}
