2c4d8c60e906d92ea278f5304d60456d918d4b17
Yoann Rodière <yoann@hibernate.org>
2019-01-24 12:03:33 +0100

    Allow integrators to pre-build Hibernate Search from Metadata + service registry, without a SessionFactory

---
+++ mapper/orm/src/main/java/org/hibernate/search/mapper/orm/cfg/spi/HibernateOrmMapperSpiSettings.java
@@ -0,0 +1,37 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.mapper.orm.cfg.spi;
+
+import org.hibernate.search.mapper.orm.cfg.HibernateOrmMapperSettings;
+
+public final class HibernateOrmMapperSpiSettings {
+
+	public static final String PREFIX = HibernateOrmMapperSettings.PREFIX;
+
+	public static final String INTEGRATION_PARTIAL_BUILD_STATE =
+			PREFIX + Radicals.INTEGRATION_PARTIAL_BUILD_STATE;
+
+	public static class Radicals {
+		public static final String INTEGRATION_PARTIAL_BUILD_STATE = "integration_partial_build_state";
+
+		private Radicals() {
+		}
+	}
+
+	/**
+	 * Default values for the different settings if no values are given.
+	 */
+	public static final class Defaults {
+
+		private Defaults() {
+		}
+	}
+
+	private HibernateOrmMapperSpiSettings() {
+	}
+
+}
