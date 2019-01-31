5bdb77a73229357059173d61d279de740b1eaaa0
Yoann Rodière <yoann@hibernate.org>
2019-01-31 14:45:31 +0100

    HSEARCH-3458 Use our own entity types in CombiningLuceneAndElasticsearchIT

--- integrationtest/elasticsearch/src/test/java/org/hibernate/search/elasticsearch/test/CombiningLuceneAndElasticsearchIT.java
+++ integrationtest/elasticsearch/src/test/java/org/hibernate/search/elasticsearch/test/CombiningLuceneAndElasticsearchIT.java
@@ -6,27 +6,35 @@
  */
 package org.hibernate.search.elasticsearch.test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Fail.fail;
+
 import java.util.List;
 import java.util.Map;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
-import org.apache.lucene.queryparser.classic.QueryParser;
-import org.apache.lucene.search.MatchAllDocsQuery;
-import org.apache.lucene.search.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.search.FullTextSession;
 import org.hibernate.search.Search;
+import org.hibernate.search.annotations.DocumentId;
+import org.hibernate.search.annotations.Field;
+import org.hibernate.search.annotations.Indexed;
 import org.hibernate.search.elasticsearch.ElasticsearchQueries;
 import org.hibernate.search.exception.SearchException;
 import org.hibernate.search.query.engine.spi.QueryDescriptor;
 import org.hibernate.search.test.SearchTestBase;
 import org.hibernate.search.testsupport.TestConstants;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Fail.fail;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.MatchAllDocsQuery;
+import org.apache.lucene.search.Query;
 
 
 /**
@@ -41,11 +49,11 @@ public class CombiningLuceneAndElasticsearchIT extends SearchTestBase {
 		Session s = openSession();
 		Transaction tx = s.beginTransaction();
 
-		ResearchPaper paper = new ResearchPaper( "important research", "latest findings", "Once upon a time...", 100 );
+		ElasticsearchIndexedEntity paper = new ElasticsearchIndexedEntity( "important research" );
 		s.persist( paper );
 
-		ComicBook comicBook = new ComicBook( "The tales of Bob", "Once upon a time..." );
-		s.persist( comicBook );
+		LuceneIndexedEntity luceneIndexedEntity = new LuceneIndexedEntity( "The tales of Bob" );
+		s.persist( luceneIndexedEntity );
 
 		tx.commit();
 		s.close();
@@ -59,14 +67,14 @@ public class CombiningLuceneAndElasticsearchIT extends SearchTestBase {
 
 		List<?> result = session.createFullTextQuery(
 				ElasticsearchQueries.fromQueryString( "title:important" ),
-				ResearchPaper.class
+				ElasticsearchIndexedEntity.class
 		).list();
 
 		assertThat( result ).extracting( "title" ).containsExactly( "important research" );
 
 		result = session.createFullTextQuery(
 				queryParser.parse( "title:tales" ),
-				ComicBook.class
+				LuceneIndexedEntity.class
 		).list();
 
 		assertThat( result ).extracting( "title" ).containsExactly( "The tales of Bob" );
@@ -84,7 +92,7 @@ public class CombiningLuceneAndElasticsearchIT extends SearchTestBase {
 		try {
 			session.createFullTextQuery(
 					ElasticsearchQueries.fromQueryString( "title:tales" ),
-					ComicBook.class
+					LuceneIndexedEntity.class
 			).list();
 
 			fail( "Expected exception wasn't raised" );
@@ -105,7 +113,7 @@ public class CombiningLuceneAndElasticsearchIT extends SearchTestBase {
 		Transaction tx = s.beginTransaction();
 
 		try {
-			session.createFullTextQuery( new NotTranslatableQuery(), ResearchPaper.class ).list();
+			session.createFullTextQuery( new NotTranslatableQuery(), ElasticsearchIndexedEntity.class ).list();
 			fail( "Expected exception wasn't raised" );
 		}
 		catch (SearchException se) {
@@ -124,13 +132,13 @@ public class CombiningLuceneAndElasticsearchIT extends SearchTestBase {
 		Transaction tx = s.beginTransaction();
 
 		QueryDescriptor query = ElasticsearchQueries.fromJson( "{ 'query': { 'match_all' : {} } }" );
-		List<?> result = session.createFullTextQuery( query, ResearchPaper.class ).list();
+		List<?> result = session.createFullTextQuery( query, ElasticsearchIndexedEntity.class ).list();
 
 		for ( Object entity : result ) {
 			session.delete( entity );
 		}
 
-		result = session.createFullTextQuery( new MatchAllDocsQuery(), ComicBook.class ).list();
+		result = session.createFullTextQuery( new MatchAllDocsQuery(), LuceneIndexedEntity.class ).list();
 
 		for ( Object entity : result ) {
 			session.delete( entity );
@@ -143,14 +151,14 @@ public class CombiningLuceneAndElasticsearchIT extends SearchTestBase {
 
 	@Override
 	public Class<?>[] getAnnotatedClasses() {
-		return new Class[] { ResearchPaper.class, ComicBook.class };
+		return new Class[] { ElasticsearchIndexedEntity.class, LuceneIndexedEntity.class };
 	}
 
 	@Override
 	public void configure(Map<String, Object> settings) {
 		// default is ES
-		settings.put( "hibernate.search.comic_book.indexmanager", "directory-based" );
-		settings.put( "hibernate.search.comic_book.directory_provider", "local-heap" );
+		settings.put( "hibernate.search.luceneIndex.indexmanager", "directory-based" );
+		settings.put( "hibernate.search.luceneIndex.directory_provider", "local-heap" );
 	}
 
 	private static class NotTranslatableQuery extends Query {
@@ -161,4 +169,78 @@ public class CombiningLuceneAndElasticsearchIT extends SearchTestBase {
 		}
 
 	}
+
+	@Entity
+	@Indexed(index = "elasticsearchIndex")
+	public static class ElasticsearchIndexedEntity {
+
+		@Id
+		@GeneratedValue
+		@DocumentId
+		private Long id;
+
+		@Field
+		private String title;
+
+		ElasticsearchIndexedEntity() {
+		}
+
+		public ElasticsearchIndexedEntity(String title) {
+			this.title = title;
+		}
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getTitle() {
+			return title;
+		}
+
+		public void setTitle(String title) {
+			this.title = title;
+		}
+	}
+
+	@Entity
+	@Indexed(index = "luceneIndex")
+	public static class LuceneIndexedEntity {
+
+		@Id
+		@GeneratedValue
+		@DocumentId
+		private Long id;
+
+		@Field
+		private String title;
+
+		LuceneIndexedEntity() {
+		}
+
+		public LuceneIndexedEntity(String title) {
+			this.title = title;
+		}
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getTitle() {
+			return title;
+		}
+
+		public void setTitle(String title) {
+			this.title = title;
+		}
+	}
+
+
 }
