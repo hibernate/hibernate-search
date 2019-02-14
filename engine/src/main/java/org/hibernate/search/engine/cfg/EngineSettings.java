49e123660fe8ca9faec0edd1f0deb25038534104
Yoann Rodière <yoann@hibernate.org>
2019-02-14 09:18:20 +0100

    HSEARCH-3473 Move engine SPI settings to their own Settings class

--- engine/src/main/java/org/hibernate/search/engine/cfg/EngineSettings.java
+++ engine/src/main/java/org/hibernate/search/engine/cfg/EngineSettings.java
@@ -6,13 +6,6 @@
  */
 package org.hibernate.search.engine.cfg;
 
-import java.util.Collections;
-import java.util.List;
-
-import org.hibernate.search.engine.environment.bean.BeanReference;
-import org.hibernate.search.engine.environment.bean.spi.BeanConfigurer;
-import org.hibernate.search.engine.environment.bean.spi.BeanResolver;
-
 /**
  * Configuration properties for the Hibernate Search engine.
  */
@@ -37,23 +30,6 @@ public final class EngineSettings {
 	public static final String INDEXES = "indexes";
 
 	/**
-	 * The {@link BeanConfigurer} instances
-	 * used to pre-define beans.
-	 * <p>
-	 * Accepts either:
-	 * <ul>
-	 *     <li>A list, containing {@link BeanConfigurer} instances,
-	 *     or {@link BeanConfigurer} implementation classes to be resolved against the {@link BeanResolver},
-	 *     or Strings to be considered as bean name and resolved against the {@link BeanResolver}
-	 *     (fully-qualified class names will be instantiated using reflection).
-	 *     </li>
-	 *     <li>A String containing whitespace-separated bean names to be resolved against the {@link BeanResolver}
-	 *     (fully-qualified class names will be instantiated using reflection).</li>
-	 * </ul>
-	 */
-	public static final String BEAN_CONFIGURERS = "bean_configurers";
-
-	/**
 	 * Default values for the different settings if no values are given.
 	 */
 	public static final class Defaults {
@@ -61,6 +37,5 @@ public final class EngineSettings {
 		private Defaults() {
 		}
 
-		public static final List<BeanReference<? extends BeanConfigurer>> BEAN_CONFIGURERS = Collections.emptyList();
 	}
 }
