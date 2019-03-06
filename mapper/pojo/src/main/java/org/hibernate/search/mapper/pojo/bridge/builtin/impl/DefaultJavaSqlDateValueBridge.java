f8e38aa48cb891595ca4ca580e08c73985147807
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2019-03-06 17:57:42 +0100

    HSEARCH-3503 Support java.sql.Date mapped as instant

---
+++ mapper/pojo/src/main/java/org/hibernate/search/mapper/pojo/bridge/builtin/impl/DefaultJavaSqlDateValueBridge.java
@@ -0,0 +1,70 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.mapper.pojo.bridge.builtin.impl;
+
+import java.sql.Date;
+import java.time.Instant;
+
+import org.hibernate.search.engine.backend.types.converter.FromDocumentFieldValueConverter;
+import org.hibernate.search.engine.backend.types.converter.runtime.FromDocumentFieldValueConvertContext;
+import org.hibernate.search.engine.backend.types.dsl.StandardIndexFieldTypeContext;
+import org.hibernate.search.mapper.pojo.bridge.ValueBridge;
+import org.hibernate.search.mapper.pojo.bridge.binding.ValueBridgeBindingContext;
+import org.hibernate.search.mapper.pojo.bridge.runtime.ValueBridgeToIndexedValueContext;
+
+public final class DefaultJavaSqlDateValueBridge implements ValueBridge<Date, Instant> {
+
+	@Override
+	public String toString() {
+		return getClass().getSimpleName();
+	}
+
+	@Override
+	public StandardIndexFieldTypeContext<?, Instant> bind(ValueBridgeBindingContext<Date> context) {
+		return context.getTypeFactory().asInstant()
+				.projectionConverter( PojoDefaultSqlDateFromDocumentFieldValueConverter.INSTANCE );
+	}
+
+	@Override
+	public Instant toIndexedValue(Date value, ValueBridgeToIndexedValueContext context) {
+		return value == null ? null : Instant.ofEpochMilli( value.getTime() );
+	}
+
+	@Override
+	public Date cast(Object value) {
+		return (Date) value;
+	}
+
+	@Override
+	public boolean isCompatibleWith(ValueBridge<?, ?> other) {
+		if ( !getClass().equals( other.getClass() ) ) {
+			return false;
+		}
+		return true;
+	}
+
+	private static class PojoDefaultSqlDateFromDocumentFieldValueConverter
+			implements FromDocumentFieldValueConverter<Instant, Date> {
+		private static final PojoDefaultSqlDateFromDocumentFieldValueConverter INSTANCE = new PojoDefaultSqlDateFromDocumentFieldValueConverter();
+
+		@Override
+		public boolean isConvertedTypeAssignableTo(Class<?> superTypeCandidate) {
+			return superTypeCandidate.isAssignableFrom( Date.class );
+		}
+
+		@Override
+		public Date convert(Instant value, FromDocumentFieldValueConvertContext context) {
+			return value == null ? null : new Date( value.toEpochMilli() );
+		}
+
+		@Override
+		public boolean isCompatibleWith(FromDocumentFieldValueConverter<?, ?> other) {
+			return INSTANCE.equals( other );
+		}
+	}
+
+}
