864221189300c579511d11783a0917588b6fde38
Yoann Rodière <yoann@hibernate.org>
2019-03-27 15:52:07 +0100

    HSEARCH-3051 Restore the Elasticsearch AWS integration

---
+++ backend/elasticsearch-aws/src/main/java/org/hibernate/search/backend/elasticsearch/aws/cfg/ElasticsearchAwsBackendSettings.java
@@ -0,0 +1,67 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.backend.elasticsearch.aws.cfg;
+
+/**
+ * AWS-specific configuration properties for Elasticsearch backends.
+ * <p>
+ * Constants in this class are to be appended to a prefix to form a property key;
+ * see {@link org.hibernate.search.engine.cfg.BackendSettings} for details.
+ */
+public final class ElasticsearchAwsBackendSettings {
+
+	private ElasticsearchAwsBackendSettings() {
+	}
+
+	/**
+	 * Whether requests should be signed using the AWS credentials.
+	 * <p>
+	 * Expects a Boolean value such as {@code true} or {@code false},
+	 * or a string that can be parsed to such Boolean value.
+	 * <p>
+	 * Defaults to {@link Defaults#SIGNING_ENABLED}.
+	 */
+	public static final String SIGNING_ENABLED = "aws.signing.enabled";
+
+	/**
+	 * The AWS access key.
+	 * <p>
+	 * Expects a String value such as {@code AKIDEXAMPLE}.
+	 * <p>
+	 * No default: must be provided when signing is enabled.
+	 */
+	public static final String ACCESS_KEY = "aws.access_key";
+
+	/**
+	 * The AWS secret key.
+	 * <p>
+	 * Expects a String value such as {@code wJalrXUtnFEMI/K7MDENG+bPxRfiCYEXAMPLEKEY}
+	 * <p>
+	 * No default: must be provided when signing is enabled.
+	 */
+	public static final String SECRET_KEY = "aws.secret_key";
+
+	/**
+	 * The AWS region.
+	 * <p>
+	 * Expects a String value such as {@code us-east-1}.
+	 * <p>
+	 * No default: must be provided when signing is enabled.
+	 */
+	public static final String REGION = "aws.region";
+
+	/**
+	 * Default values for the different settings if no values are given.
+	 */
+	public static final class Defaults {
+
+		private Defaults() {
+		}
+
+		public static final boolean SIGNING_ENABLED = false;
+	}
+}
---
+++ backend/elasticsearch-aws/src/main/java/org/hibernate/search/backend/elasticsearch/aws/impl/ElasticsearchAwsBeanConfigurer.java
@@ -0,0 +1,22 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.backend.elasticsearch.aws.impl;
+
+import org.hibernate.search.backend.elasticsearch.client.spi.ElasticsearchHttpClientConfigurer;
+import org.hibernate.search.engine.environment.bean.BeanReference;
+import org.hibernate.search.engine.environment.bean.spi.BeanConfigurationContext;
+import org.hibernate.search.engine.environment.bean.spi.BeanConfigurer;
+
+public class ElasticsearchAwsBeanConfigurer implements BeanConfigurer {
+	@Override
+	public void configure(BeanConfigurationContext context) {
+		context.assignRole(
+				ElasticsearchHttpClientConfigurer.class,
+				BeanReference.of( ElasticsearchAwsHttpClientConfigurer.class )
+		);
+	}
+}
