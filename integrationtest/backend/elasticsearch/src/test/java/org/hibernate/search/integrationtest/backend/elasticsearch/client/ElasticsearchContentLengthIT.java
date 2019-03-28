881cadc81835fd12b33057d789b83ce6ca03a467
Yoann Rodière <yoann@hibernate.org>
2019-03-28 09:09:49 +0100

    HSEARCH-3051 Port ElasticsearchContentLengthIT to Search 6

--- integrationtest/backend/elasticsearch/src/test/java/org/hibernate/search/integrationtest/backend/elasticsearch/client/ElasticsearchContentLengthIT.java
+++ integrationtest/backend/elasticsearch/src/test/java/org/hibernate/search/integrationtest/backend/elasticsearch/client/ElasticsearchContentLengthIT.java
@@ -4,7 +4,7 @@
  * License: GNU Lesser General Public License (LGPL), version 2.1 or later
  * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
  */
-package org.hibernate.search.elasticsearch.test.client;
+package org.hibernate.search.integrationtest.backend.elasticsearch.client;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
@@ -19,24 +19,28 @@ import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Properties;
+import java.util.HashMap;
+import java.util.Map;
 
-import org.hibernate.search.cfg.spi.SearchConfiguration;
-import org.hibernate.search.elasticsearch.cfg.ElasticsearchEnvironment;
-import org.hibernate.search.elasticsearch.client.impl.DefaultElasticsearchClientFactory;
-import org.hibernate.search.elasticsearch.client.impl.ElasticsearchClient;
-import org.hibernate.search.elasticsearch.client.impl.ElasticsearchRequest;
-import org.hibernate.search.elasticsearch.client.impl.ElasticsearchRequest.Builder;
-import org.hibernate.search.elasticsearch.client.impl.ElasticsearchResponse;
-import org.hibernate.search.elasticsearch.client.impl.URLEncodedString;
-import org.hibernate.search.elasticsearch.testutil.junit.SkipOnAWS;
-import org.hibernate.search.elasticsearch.testutil.junit.SkipWithoutAWS;
-import org.hibernate.search.testsupport.TestForIssue;
-import org.hibernate.search.testsupport.junit.PortedToSearch6;
-import org.hibernate.search.testsupport.setup.BuildContextForTest;
-import org.hibernate.search.testsupport.setup.SearchConfigurationForTest;
-import org.hibernate.search.util.configuration.impl.MaskedProperty;
-import org.junit.After;
+import org.hibernate.search.backend.elasticsearch.cfg.ElasticsearchBackendSettings;
+import org.hibernate.search.backend.elasticsearch.client.impl.ElasticsearchClientFactoryImpl;
+import org.hibernate.search.backend.elasticsearch.client.spi.ElasticsearchClient;
+import org.hibernate.search.backend.elasticsearch.client.spi.ElasticsearchClientFactory;
+import org.hibernate.search.backend.elasticsearch.client.spi.ElasticsearchClientImplementor;
+import org.hibernate.search.backend.elasticsearch.client.spi.ElasticsearchRequest;
+import org.hibernate.search.backend.elasticsearch.client.spi.ElasticsearchResponse;
+import org.hibernate.search.backend.elasticsearch.gson.impl.DefaultGsonProvider;
+import org.hibernate.search.backend.elasticsearch.util.spi.URLEncodedString;
+import org.hibernate.search.engine.cfg.ConfigurationPropertySource;
+import org.hibernate.search.engine.environment.bean.BeanHolder;
+import org.hibernate.search.engine.environment.bean.BeanProvider;
+import org.hibernate.search.integrationtest.backend.elasticsearch.testsupport.categories.RequiresNoRequestPostProcessing;
+import org.hibernate.search.integrationtest.backend.elasticsearch.testsupport.categories.RequiresRequestPostProcessing;
+import org.hibernate.search.integrationtest.backend.tck.testsupport.util.TckConfiguration;
+import org.hibernate.search.util.impl.integrationtest.common.TestConfigurationProvider;
+import org.hibernate.search.util.impl.test.annotation.PortedFromSearch5;
+import org.hibernate.search.util.impl.test.annotation.TestForIssue;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -45,6 +49,7 @@ import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import com.github.tomakehurst.wiremock.matching.UrlPathPattern;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.stream.JsonWriter;
@@ -53,11 +58,9 @@ import com.google.gson.stream.JsonWriter;
  * Test that we manage to compute the content-length of Elasticsearch
  * requests appropriately (for small requests, or when computing digests, e.g. on AWS),
  * and use the "chunked" transfer-encoding otherwise.
- *
- * @author Yoann Rodiere
  */
 @TestForIssue(jiraKey = "HSEARCH-2849")
-@Category(PortedToSearch6.class)
+@PortedFromSearch5(original = "org.hibernate.search.elasticsearch.test.client.ElasticsearchContentLengthIT")
 public class ElasticsearchContentLengthIT {
 
 	private static final JsonObject BODY_PART = new JsonParser().parse( "{ \"foo\": \"bar\" }" ).getAsJsonObject();
@@ -81,23 +84,18 @@ public class ElasticsearchContentLengthIT {
 	private static final int BUFFER_LIMIT = 1024;
 
 	@Rule
-	public WireMockRule wireMockRule = new WireMockRule( wireMockConfig().port( 0 ).httpsPort( 0 ) /* Automatic port selection */ );
+	public WireMockRule wireMockRule =
+			new WireMockRule( wireMockConfig().port( 0 ).httpsPort( 0 ) /* Automatic port selection */ );
 
-	private DefaultElasticsearchClientFactory clientFactory = new DefaultElasticsearchClientFactory();
-
-	@After
-	public void stop() {
-		clientFactory.stop();
-	}
+	@Rule
+	public TestConfigurationProvider testConfigurationProvider = new TestConfigurationProvider();
 
 	@Test
 	public void tinyPayload() throws Exception {
-		SearchConfigurationForTest configuration = createConfiguration();
-
 		wireMockRule.stubFor( post( urlPathLike( "/myIndex/myType" ) )
 				.willReturn( elasticsearchResponse().withStatus( 200 ) ) );
 
-		try ( ElasticsearchClient client = createClient( configuration ) ) {
+		try ( ElasticsearchClientImplementor client = createClient() ) {
 			doPost( client, "/myIndex/myType", produceBody( 1 ) );
 			wireMockRule.verify(
 					postRequestedFor( urlPathLike( "/myIndex/myType" ) )
@@ -109,14 +107,12 @@ public class ElasticsearchContentLengthIT {
 
 	@Test
 	public void payloadJustBelowBufferSize() throws Exception {
-		SearchConfigurationForTest configuration = createConfiguration();
-
 		wireMockRule.stubFor( post( urlPathLike( "/myIndex/myType" ) )
 				.willReturn( elasticsearchResponse().withStatus( 200 ) ) );
 
 		int bodyPartCount = BUFFER_LIMIT / BODY_PART_BYTE_SIZE - 1;
 
-		try ( ElasticsearchClient client = createClient( configuration ) ) {
+		try ( ElasticsearchClientImplementor client = createClient() ) {
 			doPost( client, "/myIndex/myType", produceBody( bodyPartCount ) );
 			wireMockRule.verify(
 					postRequestedFor( urlPathLike( "/myIndex/myType" ) )
@@ -126,11 +122,13 @@ public class ElasticsearchContentLengthIT {
 		}
 	}
 
+	/**
+	 * When the request is big and is not post-processed (i.e. no AWS integration),
+	 * we can "stream" it to the remote cluster, and avoid storing it entirely in memory.
+	 */
 	@Test
-	@Category(SkipOnAWS.class)
-	public void payloadJustAboveBufferSize_nonAws() throws Exception {
-		SearchConfigurationForTest configuration = createConfiguration();
-
+	@Category(RequiresNoRequestPostProcessing.class)
+	public void payloadJustAboveBufferSize_noRequestPostProcessing() throws Exception {
 		wireMockRule.stubFor( post( urlPathLike( "/myIndex/myType" ) )
 				.willReturn( elasticsearchResponse().withStatus( 200 ) ) );
 
@@ -138,27 +136,29 @@ public class ElasticsearchContentLengthIT {
 				+ 1 // The last body part will require the use of a second buffer page
 				+ 1; // ... and there will still be body parts to write, so we'll give up on computing content-length
 
-		try ( ElasticsearchClient client = createClient( configuration ) ) {
+		try ( ElasticsearchClientImplementor client = createClient() ) {
 			doPost( client, "/myIndex/myType", produceBody( bodyPartCount ) );
 			wireMockRule.verify(
 					postRequestedFor( urlPathLike( "/myIndex/myType" ) )
 							.withHeader( "Transfer-Encoding", equalTo( "chunked" ) )
 							.withoutHeader( "Content-length" )
-					);
+			);
 		}
 	}
 
+	/**
+	 * When the request is post-processed (e.g. with the AWS integration),
+	 * we have to store it entirely in memory, so chunked transfer does not make sense.
+	 */
 	@Test
-	@Category(SkipWithoutAWS.class)
-	public void payloadJustAboveBufferSize_aws() throws Exception {
-		SearchConfigurationForTest configuration = createConfiguration();
-
+	@Category(RequiresRequestPostProcessing.class)
+	public void payloadJustAboveBufferSize_requestPostProcessing() throws Exception {
 		wireMockRule.stubFor( post( urlPathLike( "/myIndex/myType" ) )
 				.willReturn( elasticsearchResponse().withStatus( 200 ) ) );
 
 		int bodyPartCount = BUFFER_LIMIT / BODY_PART_BYTE_SIZE + 1;
 
-		try ( ElasticsearchClient client = createClient( configuration ) ) {
+		try ( ElasticsearchClientImplementor client = createClient() ) {
 			doPost( client, "/myIndex/myType", produceBody( bodyPartCount ) );
 			wireMockRule.verify(
 					postRequestedFor( urlPathLike( "/myIndex/myType" ) )
@@ -168,22 +168,30 @@ public class ElasticsearchContentLengthIT {
 		}
 	}
 
-	private SearchConfigurationForTest createConfiguration() {
-		return new SearchConfigurationForTest()
-				.addProperty( "hibernate.search.default." + ElasticsearchEnvironment.SERVER_URI, httpUrlFor( wireMockRule ) );
-	}
+	private ElasticsearchClientImplementor createClient() {
+		ConfigurationPropertySource defaultBackendProperties =
+				TckConfiguration.get().getBackendProperties( testConfigurationProvider, null );
 
-	private ElasticsearchClient createClient(SearchConfiguration configuration) {
-		clientFactory.start( configuration.getProperties(), new BuildContextForTest( configuration ) );
-		Properties maskedProperties = new MaskedProperty( configuration.getProperties(), "hibernate.search.default" );
-		return clientFactory.create( maskedProperties );
+		// Redirect requests to Wiremock
+		Map<String, Object> configurationOverride = new HashMap<>();
+		configurationOverride.put( ElasticsearchBackendSettings.HOSTS, httpUrlFor( wireMockRule ) );
+		ConfigurationPropertySource backendProperties =
+				defaultBackendProperties.withOverride( ConfigurationPropertySource.fromMap( configurationOverride ) );
+
+		BeanProvider beanProvider = testConfigurationProvider.createBeanProviderForTest();
+		try ( BeanHolder<ElasticsearchClientFactory> factoryHolder =
+				beanProvider.getBean( ElasticsearchClientFactoryImpl.REFERENCE ) ) {
+			return factoryHolder.get().create(
+					backendProperties, DefaultGsonProvider.create( GsonBuilder::new, true )
+			);
+		}
 	}
 
 	private ElasticsearchResponse doPost(ElasticsearchClient client, String path, Collection<JsonObject> bodyParts) {
 		return client.submit( buildRequest( ElasticsearchRequest.post(), path, bodyParts ) ).join();
 	}
 
-	private ElasticsearchRequest buildRequest(Builder builder, String path, Collection<JsonObject> bodyParts) {
+	private ElasticsearchRequest buildRequest(ElasticsearchRequest.Builder builder, String path, Collection<JsonObject> bodyParts) {
 		for ( String pathComponent : path.split( "/" ) ) {
 			if ( !pathComponent.isEmpty() ) {
 				URLEncodedString fromString = URLEncodedString.fromString( pathComponent );
