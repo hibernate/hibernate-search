e3deaa760c24a8d2d0a610b556b37ff8ca2f8030
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2019-04-11 12:09:17 +0200

    HSEARCH-1656 Test annotation on implemented interface

---
+++ integrationtest/mapper/pojo/src/test/java/org/hibernate/search/integrationtest/mapper/pojo/model/ImplementedInterfaceIT.java
@@ -0,0 +1,92 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.integrationtest.mapper.pojo.model;
+
+import org.hibernate.search.integrationtest.mapper.pojo.testsupport.util.rule.JavaBeanMappingSetupHelper;
+import org.hibernate.search.mapper.javabean.JavaBeanMapping;
+import org.hibernate.search.mapper.javabean.session.SearchSession;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.DocumentId;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.GenericField;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
+import org.hibernate.search.util.impl.integrationtest.common.rule.BackendMock;
+import org.hibernate.search.util.impl.test.annotation.TestForIssue;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+@TestForIssue(jiraKey = "HSEARCH-1656")
+public class ImplementedInterfaceIT {
+
+	@Rule
+	public BackendMock backendMock = new BackendMock( "stubBackend" );
+
+	@Rule
+	public JavaBeanMappingSetupHelper setupHelper = new JavaBeanMappingSetupHelper();
+
+	private JavaBeanMapping mapping;
+
+	@Before
+	public void setup() {
+		backendMock.expectSchema( IndexedPojo.class.getName(), b -> b
+				.field( "text", String.class )
+		);
+
+		mapping = setupHelper.withBackendMock( backendMock ).setup( IndexedPojo.class );
+		backendMock.verifyExpectationsMet();
+	}
+
+	@Test
+	public void index() {
+		try ( SearchSession session = mapping.createSession() ) {
+			IndexedPojo indexedPojo = new IndexedPojo( 1, "Using some other text here" );
+			session.getMainWorkPlan().add( indexedPojo );
+
+			backendMock.expectWorks( IndexedPojo.class.getName() ).add( "1", b -> b
+					.field( "text", "Using some other text here" )
+			).preparedThenExecuted();
+		}
+	}
+
+	public interface ImplementedInterface {
+
+		@GenericField
+		default String getText() {
+			return "Index your POJOs with Hibernate Search 6";
+		}
+	}
+
+	@Indexed
+	public static class IndexedPojo implements ImplementedInterface {
+
+		private Integer id;
+		private String text;
+
+		public IndexedPojo(Integer id, String text) {
+			this.id = id;
+			this.text = text;
+		}
+
+		@DocumentId
+		public Integer getId() {
+			return id;
+		}
+
+		public void setId(Integer id) {
+			this.id = id;
+		}
+
+		@Override
+		public String getText() {
+			return text;
+		}
+
+		public void setText(String text) {
+			this.text = text;
+		}
+	}
+}
