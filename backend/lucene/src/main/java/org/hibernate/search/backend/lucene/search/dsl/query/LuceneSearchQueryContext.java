33eb0b0ea3cddec703b98317dc6fe524ff2db7ba
Yoann Rodière <yoann@hibernate.org>
2019-05-03 17:14:49 +0200

    HSEARCH-3544 Add a Lucene extension to the query DSL

---
+++ backend/lucene/src/main/java/org/hibernate/search/backend/lucene/search/dsl/query/LuceneSearchQueryContext.java
@@ -0,0 +1,17 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.backend.lucene.search.dsl.query;
+
+import org.hibernate.search.engine.search.dsl.query.SearchQueryContext;
+
+public interface LuceneSearchQueryContext<Q> extends SearchQueryContext<LuceneSearchQueryContext<Q>, Q> {
+
+	// FIXME make toQuery return a Lucene-specific query type
+
+	// FIXME make sort(Consumer) accept a function that expects Lucene-specific input
+
+}
