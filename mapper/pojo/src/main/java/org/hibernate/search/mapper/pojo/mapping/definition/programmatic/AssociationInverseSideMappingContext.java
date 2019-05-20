691ea021b8c5f756522e5a573ec4389b8496ce25
Yoann Rodière <yoann@hibernate.org>
2019-05-20 11:40:10 +0200

    HSEARCH-3463 Add some javadoc to container-extractor-related APIs

--- mapper/pojo/src/main/java/org/hibernate/search/mapper/pojo/mapping/definition/programmatic/AssociationInverseSideMappingContext.java
+++ mapper/pojo/src/main/java/org/hibernate/search/mapper/pojo/mapping/definition/programmatic/AssociationInverseSideMappingContext.java
@@ -8,22 +8,44 @@ package org.hibernate.search.mapper.pojo.mapping.definition.programmatic;
 
 import org.hibernate.search.mapper.pojo.extractor.ContainerExtractor;
 import org.hibernate.search.mapper.pojo.extractor.ContainerExtractorPath;
+import org.hibernate.search.mapper.pojo.extractor.builtin.BuiltinContainerExtractor;
 
 /**
  * @author Yoann Rodiere
  */
 public interface AssociationInverseSideMappingContext extends PropertyMappingContext {
 
+	/**
+	 * @param extractorClass The type of container extractor to use.
+	 * @return {@code this}, for method chaining.
+	 */
 	@SuppressWarnings("rawtypes") // We need to allow raw container types, e.g. MapValueExtractor.class
 	default AssociationInverseSideMappingContext withExtractor(
 			Class<? extends ContainerExtractor> extractorClass) {
 		return withExtractors( ContainerExtractorPath.explicitExtractor( extractorClass ) );
 	}
 
+	/**
+	 * @param extractorType The type of container extractor to use.
+	 * @return {@code this}, for method chaining.
+	 */
+	default AssociationInverseSideMappingContext withExtractor(BuiltinContainerExtractor extractorType) {
+		return withExtractor( extractorType.getType() );
+	}
+
+	/**
+	 * Indicate that no container extractors should be applied,
+	 * not even the default ones.
+	 * @return {@code this}, for method chaining.
+	 */
 	default AssociationInverseSideMappingContext withoutExtractors() {
 		return withExtractors( ContainerExtractorPath.noExtractors() );
 	}
 
+	/**
+	 * @param extractorPath A {@link ContainerExtractorPath}.
+	 * @return {@code this}, for method chaining.
+	 */
 	AssociationInverseSideMappingContext withExtractors(ContainerExtractorPath extractorPath);
 
 }
--- mapper/pojo/src/main/java/org/hibernate/search/mapper/pojo/mapping/definition/programmatic/IndexingDependencyMappingContext.java
+++ mapper/pojo/src/main/java/org/hibernate/search/mapper/pojo/mapping/definition/programmatic/IndexingDependencyMappingContext.java
@@ -9,27 +9,46 @@ package org.hibernate.search.mapper.pojo.mapping.definition.programmatic;
 import org.hibernate.search.mapper.pojo.dirtiness.ReindexOnUpdate;
 import org.hibernate.search.mapper.pojo.extractor.ContainerExtractor;
 import org.hibernate.search.mapper.pojo.extractor.ContainerExtractorPath;
+import org.hibernate.search.mapper.pojo.extractor.builtin.BuiltinContainerExtractor;
 import org.hibernate.search.mapper.pojo.model.path.PojoModelPathValueNode;
 
-/**
- * @author Yoann Rodiere
- */
 public interface IndexingDependencyMappingContext extends PropertyMappingContext {
 
 	IndexingDependencyMappingContext reindexOnUpdate(ReindexOnUpdate reindexOnUpdate);
 
 	IndexingDependencyMappingContext derivedFrom(PojoModelPathValueNode pojoModelPath);
 
+	/**
+	 * @param extractorClass The type of container extractor to use.
+	 * @return {@code this}, for method chaining.
+	 */
 	@SuppressWarnings("rawtypes") // We need to allow raw container types, e.g. MapValueExtractor.class
 	default IndexingDependencyMappingContext withExtractor(
 			Class<? extends ContainerExtractor> extractorClass) {
 		return withExtractors( ContainerExtractorPath.explicitExtractor( extractorClass ) );
 	}
 
+	/**
+	 * @param extractorType The type of container extractor to use.
+	 * @return {@code this}, for method chaining.
+	 */
+	default IndexingDependencyMappingContext withExtractor(BuiltinContainerExtractor extractorType) {
+		return withExtractor( extractorType.getType() );
+	}
+
+	/**
+	 * Indicate that no container extractors should be applied,
+	 * not even the default ones.
+	 * @return {@code this}, for method chaining.
+	 */
 	default IndexingDependencyMappingContext withoutExtractors() {
 		return withExtractors( ContainerExtractorPath.noExtractors() );
 	}
 
+	/**
+	 * @param extractorPath A {@link ContainerExtractorPath}.
+	 * @return {@code this}, for method chaining.
+	 */
 	IndexingDependencyMappingContext withExtractors(ContainerExtractorPath extractorPath);
 
 }
