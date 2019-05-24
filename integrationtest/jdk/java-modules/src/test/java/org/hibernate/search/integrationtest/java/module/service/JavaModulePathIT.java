4fbe60cb801411c564f5ffc3a2b159de0eb8c501
Yoann Rodière <yoann@hibernate.org>
2019-05-24 11:33:25 +0200

    HSEARCH-3551 Fix and restore integration tests in the java module path

--- integrationtest/jdk/java-modules/src/test/java/org/hibernate/search/integrationtest/java/module/service/JavaModulePathIT.java
+++ integrationtest/jdk/java-modules/src/test/java/org/hibernate/search/integrationtest/java/module/service/JavaModulePathIT.java
@@ -9,18 +9,20 @@ package org.hibernate.search.integrationtest.java.module.service;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.hibernate.search.mapper.orm.Search;
 
-public class AuthorServiceIT {
+public class JavaModulePathIT {
 
 	/*
-	 * Test that the service successfully uses Hibernate Search in a JDK9 module.
+	 * Test that the service successfully uses Hibernate Search in the module path.
 	 * We don't really care about the features themselves,
-	 * but that's the easiest way to check that Hibernate Search is being used.
+	 * but the easiest way to check this is to just use Hibernate Search features and see if it works.
 	 */
 	@Test
 	public void test() {
 		checkIsInModulePath( Object.class );
 		checkIsInModulePath( AuthorService.class );
+		checkIsInModulePath( Search.class );
 
 		AuthorService service = new AuthorService();
 		service.add( "foo" );
