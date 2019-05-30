be1c35f6dba9c0ad5f4b65bc47950783ae1c7b32
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2019-05-30 18:26:29 +0200

    HSEARCH-3048 Test term vector at backend-lucene level

--- integrationtest/backend/lucene/src/test/java/org/hibernate/search/integrationtest/backend/lucene/mapping/LuceneFieldAttributesIT.java
+++ integrationtest/backend/lucene/src/test/java/org/hibernate/search/integrationtest/backend/lucene/mapping/LuceneFieldAttributesIT.java
@@ -15,7 +15,9 @@ import org.hibernate.search.engine.backend.document.DocumentElement;
 import org.hibernate.search.engine.backend.document.IndexFieldReference;
 import org.hibernate.search.engine.backend.document.model.dsl.IndexSchemaElement;
 import org.hibernate.search.engine.backend.index.spi.IndexWorkPlan;
+import org.hibernate.search.engine.backend.types.Norms;
 import org.hibernate.search.engine.backend.types.Projectable;
+import org.hibernate.search.engine.backend.types.TermVector;
 import org.hibernate.search.engine.search.query.SearchQuery;
 import org.hibernate.search.integrationtest.backend.tck.testsupport.configuration.DefaultAnalysisDefinitions;
 import org.hibernate.search.integrationtest.backend.tck.testsupport.util.rule.SearchSetupHelper;
@@ -26,6 +28,7 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import org.apache.lucene.document.Document;
+import org.apache.lucene.index.IndexableField;
 import org.assertj.core.api.Assertions;
 
 public class LuceneFieldAttributesIT {
@@ -53,7 +56,44 @@ public class LuceneFieldAttributesIT {
 	}
 
 	@Test
-	public void verifyIndexFieldTypes() {
+	public void verifyNorms() {
+		Document document = loadDocument();
+
+		// norms false => omit-norms true
+		Assertions.assertThat( document.getField( "keyword" ).fieldType().omitNorms() ).isTrue();
+		Assertions.assertThat( document.getField( "noNorms" ).fieldType().omitNorms() ).isTrue();
+
+		// norms true => omit-norms false
+		Assertions.assertThat( document.getField( "text" ).fieldType().omitNorms() ).isFalse();
+		Assertions.assertThat( document.getField( "norms" ).fieldType().omitNorms() ).isFalse();
+	}
+
+	@Test
+	public void verifyTermVector() {
+		Document document = loadDocument();
+
+		IndexableField field = document.getField( "text" );
+		// default no term vector stored
+		Assertions.assertThat( field.fieldType().storeTermVectors() ).isFalse();
+		Assertions.assertThat( field.fieldType().storeTermVectorPositions() ).isFalse();
+		Assertions.assertThat( field.fieldType().storeTermVectorOffsets() ).isFalse();
+		Assertions.assertThat( field.fieldType().storeTermVectorPayloads() ).isFalse();
+
+		field = document.getField( "termVector" );
+		Assertions.assertThat( field.fieldType().storeTermVectors() ).isTrue();
+		Assertions.assertThat( field.fieldType().storeTermVectorPositions() ).isFalse();
+		Assertions.assertThat( field.fieldType().storeTermVectorOffsets() ).isFalse();
+		Assertions.assertThat( field.fieldType().storeTermVectorPayloads() ).isFalse();
+
+		field = document.getField( "moreOptions" );
+		Assertions.assertThat( field.fieldType().storeTermVectors() ).isTrue();
+		// TODO these are not true:
+		// Assertions.assertThat( field.fieldType().storeTermVectorPositions() ).isTrue();
+		// Assertions.assertThat( field.fieldType().storeTermVectorOffsets() ).isTrue();
+		// Assertions.assertThat( field.fieldType().storeTermVectorPayloads() ).isTrue();
+	}
+
+	private Document loadDocument() {
 		SearchQuery<Document> query = indexManager.createSearchScope().query()
 				.asProjection(
 						f -> f.extension( LuceneExtension.get() ).document()
@@ -64,34 +104,50 @@ public class LuceneFieldAttributesIT {
 		List<Document> result = query.fetch().getHits();
 
 		Assertions.assertThat( result ).hasSize( 1 );
-		Document document = result.get( 0 );
-
-		// norms false => omit-norms true
-		Assertions.assertThat( document.getField( "keyword" ).fieldType().omitNorms() ).isTrue();
-
-		// norms true => omit-norms false
-		Assertions.assertThat( document.getField( "text" ).fieldType().omitNorms() ).isFalse();
+		return result.get( 0 );
 	}
 
 	private void initData() {
 		IndexWorkPlan<? extends DocumentElement> workPlan = indexManager.createWorkPlan();
-
 		workPlan.add( referenceProvider( "ID:1" ), document -> {
 			document.addValue( indexMapping.string, "keyword" );
 			document.addValue( indexMapping.text, TEXT );
+			document.addValue( indexMapping.norms, TEXT );
+			document.addValue( indexMapping.noNorms, TEXT );
+			document.addValue( indexMapping.termVector, TEXT );
+			document.addValue( indexMapping.moreOptions, "Search 6 groundwork - Add the missing common field type options compared to Search 5" );
 		} );
 
 		workPlan.execute().join();
 	}
 
 	private static class IndexMapping {
-
 		final IndexFieldReference<String> string;
 		final IndexFieldReference<String> text;
+		final IndexFieldReference<String> norms;
+		final IndexFieldReference<String> noNorms;
+		final IndexFieldReference<String> termVector;
+		final IndexFieldReference<String> moreOptions;
 
 		IndexMapping(IndexSchemaElement root) {
 			string = root.field( "keyword", f -> f.asString().projectable( Projectable.YES ) ).toReference();
 			text = root.field( "text", f -> f.asString().analyzer( DefaultAnalysisDefinitions.ANALYZER_STANDARD_ENGLISH.name ).projectable( Projectable.YES ) ).toReference();
+
+			norms = root.field( "norms", f -> f.asString().analyzer( DefaultAnalysisDefinitions.ANALYZER_STANDARD_ENGLISH.name ).projectable( Projectable.YES )
+					.norms( Norms.YES )
+			).toReference();
+
+			noNorms = root.field( "noNorms", f -> f.asString().analyzer( DefaultAnalysisDefinitions.ANALYZER_STANDARD_ENGLISH.name ).projectable( Projectable.YES )
+					.norms( Norms.NO )
+			).toReference();
+
+			termVector = root.field( "termVector", f -> f.asString().analyzer( DefaultAnalysisDefinitions.ANALYZER_STANDARD_ENGLISH.name ).projectable( Projectable.YES )
+					.termVector( TermVector.YES )
+			).toReference();
+
+			moreOptions = root.field( "moreOptions", f -> f.asString().analyzer( DefaultAnalysisDefinitions.ANALYZER_STANDARD_ENGLISH.name ).projectable( Projectable.YES )
+					.termVector( TermVector.WITH_POSITIONS_OFFSETS_PAYLOADS )
+			).toReference();
 		}
 	}
 }
