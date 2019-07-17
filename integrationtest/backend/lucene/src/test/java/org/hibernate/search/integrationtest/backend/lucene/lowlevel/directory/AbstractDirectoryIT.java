93eb684f2435aca64bb022c293c09b88b435678c
Yoann Rodière <yoann@hibernate.org>
2019-07-17 09:52:41 +0200

    HSEARCH-3440 Test error messages when setting an FS-only locking factory on non-FS directory providers

--- integrationtest/backend/lucene/src/test/java/org/hibernate/search/integrationtest/backend/lucene/lowlevel/directory/AbstractDirectoryIT.java
+++ integrationtest/backend/lucene/src/test/java/org/hibernate/search/integrationtest/backend/lucene/lowlevel/directory/AbstractDirectoryIT.java
@@ -28,6 +28,7 @@ import org.junit.rules.TemporaryFolder;
 
 public abstract class AbstractDirectoryIT {
 
+	protected static final String MAPPED_TYPE_NAME = "MappedTypeName";
 	protected static final String BACKEND_NAME = "BackendName";
 	protected static final String INDEX_NAME = "IndexName";
 
@@ -75,6 +76,7 @@ public abstract class AbstractDirectoryIT {
 				setupHelper.start( BACKEND_NAME )
 						.withIndex(
 								INDEX_NAME,
+								c -> c.mappedType( MAPPED_TYPE_NAME ),
 								ctx -> this.indexMapping = new IndexMapping( ctx.getSchemaElement() ),
 								indexManager -> this.indexManager = indexManager
 						)
