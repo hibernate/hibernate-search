f3bd525f7259ecff9ce4dc377a46067a84d2ae9b
Yoann Rodière <yoann@hibernate.org>
2019-08-09 10:56:20 +0200

    HSEARCH-3649 Fix missing diamond operators

--- integrationtest/backend/tck/src/main/java/org/hibernate/search/integrationtest/backend/tck/testsupport/types/DoubleFieldTypeDescriptor.java
+++ integrationtest/backend/tck/src/main/java/org/hibernate/search/integrationtest/backend/tck/testsupport/types/DoubleFieldTypeDescriptor.java
@@ -79,7 +79,7 @@ public class DoubleFieldTypeDescriptor extends FieldTypeDescriptor<Double> {
 
 	@Override
 	public Optional<IndexNullAsMatchPredicateExpectactions<Double>> getIndexNullAsMatchPredicateExpectations() {
-		return Optional.of( new IndexNullAsMatchPredicateExpectactions(
+		return Optional.of( new IndexNullAsMatchPredicateExpectactions<>(
 				0.0, 42.1
 		) );
 	}
