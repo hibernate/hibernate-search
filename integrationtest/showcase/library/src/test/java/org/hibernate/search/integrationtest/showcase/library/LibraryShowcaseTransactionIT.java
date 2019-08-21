6d458c317ee456a4171ec674f4be0bea3457502c
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2019-08-21 22:55:52 +0200

    HSEARCH-1270 Test inner transaction change flush rollback

---
+++ integrationtest/showcase/library/src/test/java/org/hibernate/search/integrationtest/showcase/library/LibraryShowcaseTransactionIT.java
@@ -0,0 +1,59 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.integrationtest.showcase.library;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Optional;
+
+import org.hibernate.search.integrationtest.showcase.library.model.Book;
+import org.hibernate.search.integrationtest.showcase.library.service.DocumentService;
+import org.hibernate.search.integrationtest.showcase.library.service.InnerTransactionRollbackService;
+import org.hibernate.search.util.impl.test.annotation.TestForIssue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@TestPropertySource(properties = { "automatic_indexing.strategy=session" })
+@ActiveProfiles(resolver = TestActiveProfilesResolver.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class LibraryShowcaseTransactionIT {
+
+	@Autowired
+	private InnerTransactionRollbackService innerTransactionRollbackService;
+
+	@Autowired
+	private DocumentService documentService;
+
+	@Test
+	@TestForIssue( jiraKey = "HSEARCH-1270" )
+	public void innerTransactionRollback() {
+		String outerIsbn = "0-4206-9749-7";
+		String innerIsbn = "0-1111-9749-7";
+
+		innerTransactionRollbackService.doOuter( outerIsbn, innerIsbn );
+
+		Optional<Book> outerBook = documentService.getByIsbn( outerIsbn );
+		Optional<Book> innerBook = documentService.getByIsbn( innerIsbn );
+
+		// Check that outer transaction data has been pushed to the index:
+		assertTrue( outerBook.isPresent() );
+
+		// Check that inner transaction data has NOT been pushed to the index:
+		assertFalse( innerBook.isPresent() );
+	}
+}
---
+++ integrationtest/showcase/library/src/test/java/org/hibernate/search/integrationtest/showcase/library/service/InnerTransactionRollbackService.java
@@ -0,0 +1,72 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.integrationtest.showcase.library.service;
+
+import javax.persistence.EntityManager;
+
+import org.hibernate.search.integrationtest.showcase.library.model.Book;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class InnerTransactionRollbackService {
+
+	@Autowired
+	private InnerTransactionRollbackService self;
+
+	@Autowired
+	private EntityManager entityManager;
+
+	/**
+	 * Persists a new book within an independent transaction.
+	 * Flushes and finally rollback throwing a {@link MakeRollbackException}.
+	 * <p>
+	 * When the nested transaction rollbacks, this is the case here,
+	 * {@code Propagation.REQUIRES_NEW} is equivalent to a {@code Propagation.NESTED},
+	 * what does really matter is that:
+	 * <b>inner transaction rollback does not affect outer transaction</b>.
+	 * <p>
+	 * {@code Propagation.REQUIRES_NEW} differs from {@code Propagation.NESTED}
+	 * when outer transaction rollbacks, this is NOT the case here,
+	 * with {@code Propagation.REQUIRES_NEW} the inner transaction can commit,
+	 * with {@code Propagation.NESTED} the inner transaction cannot commit.
+	 *
+	 * @param isbn the isbn code for the book that has to be created within the inner transaction
+	 * @throws MakeRollbackException
+	 */
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
+	public void doInner(String isbn) {
+		// This data is supposed to be NOT pushed to the index:
+		Book book = new Book( 731, isbn, "The Prince", "Niccolò Machiavelli", "Describes the human people behaviour...", "politics" );
+		entityManager.persist( book );
+		entityManager.flush();
+		throw new MakeRollbackException( "Rollback the inner transaction" );
+	}
+
+	public void doOuter(String outerIsbn, String nestedIsbn) {
+		// This data is supposed to be pushed to the index:
+		Book book = new Book( 739, outerIsbn, "The Late Mattia Pascal", "Luigi Pirandello", "Describes the human life conditions...", "misfortune" );
+		entityManager.persist( book );
+		try {
+			self.doInner( nestedIsbn );
+		}
+		catch (MakeRollbackException e) {
+			// Do not rollback the outer transaction
+		}
+		entityManager.flush();
+	}
+
+	private static class MakeRollbackException extends RuntimeException {
+		public MakeRollbackException(String message) {
+			super( message );
+		}
+	}
+}
