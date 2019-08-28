9b9de32f640259c64f8c58e3a0ce397977f2142d
Yoann Rodière <yoann@hibernate.org>
2019-08-28 17:03:55 +0200

    HSEARCH-3649 Add minimal documentation for aggregations

---
+++ documentation/src/test/java/org/hibernate/search/documentation/searchdsl/aggregation/Book.java
@@ -0,0 +1,81 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.documentation.searchdsl.aggregation;
+
+import java.sql.Date;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.hibernate.search.engine.backend.types.Aggregable;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.FullTextField;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.GenericField;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.KeywordField;
+
+@Entity
+@Indexed
+public class Book {
+
+	@Id
+	private Integer id;
+
+	@FullTextField(analyzer = "english")
+	private String title;
+
+	@GenericField(aggregable = Aggregable.YES)
+	private Double price;
+
+	@KeywordField(aggregable = Aggregable.YES)
+	private Genre genre;
+
+	// Using the legacy java.sql.Date type on purpose, to demonstrate how to ignore bridges
+	@GenericField(aggregable = Aggregable.YES)
+	private Date releaseDate;
+
+	public Book() {
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Genre getGenre() {
+		return genre;
+	}
+
+	public void setGenre(Genre genre) {
+		this.genre = genre;
+	}
+
+	public Date getReleaseDate() {
+		return releaseDate;
+	}
+
+	public void setReleaseDate(Date releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+}
