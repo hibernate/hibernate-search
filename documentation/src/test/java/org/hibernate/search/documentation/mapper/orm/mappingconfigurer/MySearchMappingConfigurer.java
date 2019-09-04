64c30b4f72765d565786c51d92e93a8eb8c3e82e
Yoann Rodière <yoann@hibernate.org>
2019-09-04 18:15:58 +0200

    HSEARCH-3676 Add an example to the documentation for the mapping configurer

---
+++ documentation/src/test/java/org/hibernate/search/documentation/mapper/orm/mappingconfigurer/MySearchMappingConfigurer.java
@@ -0,0 +1,23 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.documentation.mapper.orm.mappingconfigurer;
+
+import org.hibernate.search.mapper.orm.mapping.HibernateOrmMappingConfigurationContext;
+import org.hibernate.search.mapper.orm.mapping.HibernateOrmSearchMappingConfigurer;
+import org.hibernate.search.mapper.pojo.mapping.definition.programmatic.ProgrammaticMappingConfigurationContext;
+
+//tag::include[]
+public class MySearchMappingConfigurer implements HibernateOrmSearchMappingConfigurer {
+	@Override
+	public void configure(HibernateOrmMappingConfigurationContext context) {
+		ProgrammaticMappingConfigurationContext mapping = context.programmaticMapping();
+		mapping.type( Book.class )
+				.indexed()
+				.property( "title" ).fullTextField().analyzer( "english" );
+	}
+}
+//end::include[]
