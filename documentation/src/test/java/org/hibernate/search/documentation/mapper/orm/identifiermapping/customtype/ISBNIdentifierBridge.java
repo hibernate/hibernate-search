130dfbe86b239c76aeaf545e94ce85250c0dacb4
Yoann Rodière <yoann@hibernate.org>
2019-09-04 16:54:44 +0200

    HSEARCH-3676 Document identifier mapping

---
+++ documentation/src/test/java/org/hibernate/search/documentation/mapper/orm/identifiermapping/customtype/ISBNIdentifierBridge.java
@@ -0,0 +1,26 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.documentation.mapper.orm.identifiermapping.customtype;
+
+import org.hibernate.search.mapper.pojo.bridge.IdentifierBridge;
+import org.hibernate.search.mapper.pojo.bridge.runtime.IdentifierBridgeFromDocumentIdentifierContext;
+import org.hibernate.search.mapper.pojo.bridge.runtime.IdentifierBridgeToDocumentIdentifierContext;
+
+public class ISBNIdentifierBridge implements IdentifierBridge<ISBN> {
+
+	@Override
+	public String toDocumentIdentifier(ISBN value, IdentifierBridgeToDocumentIdentifierContext context) {
+		return value == null ? null : value.getStringValue();
+	}
+
+	@Override
+	public ISBN fromDocumentIdentifier(String documentIdentifier,
+			IdentifierBridgeFromDocumentIdentifierContext context) {
+		return documentIdentifier == null ? null : new ISBN( documentIdentifier );
+	}
+
+}
---
+++ documentation/src/test/java/org/hibernate/search/documentation/mapper/orm/identifiermapping/customtype/IdentifierMappingCustomTypeIT.java
@@ -0,0 +1,80 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.documentation.mapper.orm.identifiermapping.customtype;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import javax.persistence.EntityManagerFactory;
+
+import org.hibernate.search.documentation.testsupport.BackendConfigurations;
+import org.hibernate.search.engine.search.DocumentReference;
+import org.hibernate.search.mapper.orm.Search;
+import org.hibernate.search.mapper.orm.automaticindexing.AutomaticIndexingSynchronizationStrategyName;
+import org.hibernate.search.mapper.orm.cfg.HibernateOrmMapperSettings;
+import org.hibernate.search.mapper.orm.session.SearchSession;
+import org.hibernate.search.util.impl.integrationtest.common.rule.BackendConfiguration;
+import org.hibernate.search.util.impl.integrationtest.orm.OrmSetupHelper;
+import org.hibernate.search.util.impl.integrationtest.orm.OrmUtils;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class IdentifierMappingCustomTypeIT {
+	@Parameterized.Parameters(name = "{0}")
+	public static Object[] backendConfigurations() {
+		return BackendConfigurations.simple().toArray();
+	}
+
+	@Rule
+	public OrmSetupHelper setupHelper;
+
+	private EntityManagerFactory entityManagerFactory;
+
+	public IdentifierMappingCustomTypeIT(BackendConfiguration backendConfiguration) {
+		this.setupHelper = OrmSetupHelper.withSingleBackend( backendConfiguration );
+	}
+
+	@Before
+	public void setup() {
+		entityManagerFactory = setupHelper.start()
+				.withProperty(
+						HibernateOrmMapperSettings.AUTOMATIC_INDEXING_SYNCHRONIZATION_STRATEGY,
+						AutomaticIndexingSynchronizationStrategyName.SEARCHABLE
+				)
+				.setup( Book.class );
+	}
+
+	@Test
+	public void smoke() {
+		OrmUtils.withinJPATransaction( entityManagerFactory, entityManager -> {
+			Book book = new Book();
+			book.setIsbn( new ISBN( "9780586008355" ) );
+
+			entityManager.persist( book );
+		} );
+
+		OrmUtils.withinJPATransaction( entityManagerFactory, entityManager -> {
+			SearchSession searchSession = Search.session( entityManager );
+
+			List<DocumentReference> result = searchSession.search( Book.class )
+					.asProjection( f -> f.documentReference() )
+					.predicate( f -> f.matchAll() )
+					.fetchHits( 20 );
+
+			assertThat( result )
+					.extracting( DocumentReference::getId )
+					.containsExactly( "9780586008355" );
+		} );
+	}
+
+}
