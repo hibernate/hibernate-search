418176c0e8e55e8c44bcf6e5a77a9d1f90b2ffb8
Yoann Rodière <yoann@hibernate.org>
2019-09-04 15:32:19 +0200

    HSEARCH-3679 Document container extractors

---
+++ documentation/src/test/java/org/hibernate/search/documentation/mapper/orm/containerextractor/Book.java
@@ -0,0 +1,98 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.documentation.mapper.orm.containerextractor;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.MapKeyColumn;
+import javax.persistence.OrderBy;
+
+import org.hibernate.search.mapper.pojo.bridge.mapping.annotation.ValueBridgeRef;
+import org.hibernate.search.mapper.pojo.extractor.builtin.BuiltinContainerExtractors;
+import org.hibernate.search.mapper.pojo.extractor.mapping.annotation.ContainerExtract;
+import org.hibernate.search.mapper.pojo.extractor.mapping.annotation.ContainerExtraction;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.FullTextField;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.GenericField;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
+
+@Entity
+@Indexed
+public class Book {
+
+	@Id
+	private Integer id;
+
+	@FullTextField(analyzer = "name")
+	private String title;
+
+	// tag::no-extractor[]
+	@ManyToMany
+	@GenericField( // <1>
+			name = "authorCount",
+			valueBridge = @ValueBridgeRef(type = MyCollectionSizeBridge.class), // <2>
+			extraction = @ContainerExtraction(extract = ContainerExtract.NO) // <3>
+	)
+	private List<Author> authors = new ArrayList<>();
+	// end::no-extractor[]
+
+	// tag::explicit-extractor[]
+	@ElementCollection // <1>
+	@JoinTable(name = "book_pricebyformat")
+	@MapKeyColumn(name = "format")
+	@Column(name = "price")
+	@OrderBy("format asc")
+	@GenericField( // <2>
+			name = "availableFormats",
+			extraction = @ContainerExtraction(BuiltinContainerExtractors.MAP_KEY) // <3>
+	)
+	private Map<BookFormat, BigDecimal> priceByFormat = new LinkedHashMap<>();
+	// end::explicit-extractor[]
+
+	public Book() {
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public List<Author> getAuthors() {
+		return authors;
+	}
+
+	public void setAuthors(List<Author> authors) {
+		this.authors = authors;
+	}
+
+	public Map<BookFormat, BigDecimal> getPriceByFormat() {
+		return priceByFormat;
+	}
+
+	public void setPriceByFormat(Map<BookFormat, BigDecimal> priceByFormat) {
+		this.priceByFormat = priceByFormat;
+	}
+}
---
+++ documentation/src/test/java/org/hibernate/search/documentation/mapper/orm/containerextractor/ContainerExtractorIT.java
@@ -0,0 +1,91 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.documentation.mapper.orm.containerextractor;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigDecimal;
+import java.util.List;
+import javax.persistence.EntityManagerFactory;
+
+import org.hibernate.search.documentation.testsupport.BackendConfigurations;
+import org.hibernate.search.engine.search.common.ValueConvert;
+import org.hibernate.search.mapper.orm.Search;
+import org.hibernate.search.mapper.orm.automaticindexing.AutomaticIndexingSynchronizationStrategyName;
+import org.hibernate.search.mapper.orm.cfg.HibernateOrmMapperSettings;
+import org.hibernate.search.mapper.orm.session.SearchSession;
+import org.hibernate.search.util.impl.integrationtest.common.rule.BackendConfiguration;
+import org.hibernate.search.util.impl.integrationtest.orm.OrmSetupHelper;
+import org.hibernate.search.util.impl.integrationtest.orm.OrmUtils;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class ContainerExtractorIT {
+
+	@Parameterized.Parameters(name = "{0}")
+	public static Object[] backendConfigurations() {
+		return BackendConfigurations.simple().toArray();
+	}
+
+	@Rule
+	public OrmSetupHelper setupHelper;
+
+	private EntityManagerFactory entityManagerFactory;
+
+	public ContainerExtractorIT(BackendConfiguration backendConfiguration) {
+		this.setupHelper = OrmSetupHelper.withSingleBackend( backendConfiguration );
+	}
+
+	@Before
+	public void setup() {
+		entityManagerFactory = setupHelper.start()
+				.withProperty(
+						HibernateOrmMapperSettings.AUTOMATIC_INDEXING_SYNCHRONIZATION_STRATEGY,
+						AutomaticIndexingSynchronizationStrategyName.SEARCHABLE
+				)
+				.setup( Book.class, Author.class );
+	}
+
+	@Test
+	public void smoke() {
+		OrmUtils.withinJPATransaction( entityManagerFactory, entityManager -> {
+			Author author = new Author();
+			author.setId( 1 );
+			author.setName( "Isaac Asimov" );
+
+			Book book = new Book();
+			book.setId( 1 );
+			book.setTitle( "The Caves Of Steel" );
+			book.getAuthors().add( author );
+			author.getBooks().add( book );
+			book.getPriceByFormat().put( BookFormat.AUDIOBOOK, new BigDecimal( "15.99" ) );
+			book.getPriceByFormat().put( BookFormat.HARDCOVER, new BigDecimal( "25.99" ) );
+
+			entityManager.persist( author );
+			entityManager.persist( book );
+		} );
+
+		OrmUtils.withinJPATransaction( entityManagerFactory, entityManager -> {
+			SearchSession searchSession = Search.session( entityManager );
+
+			List<Book> result = searchSession.search( Book.class )
+					.predicate( f -> f.bool()
+							.must( f.match().onField( "availableFormats" ).matching( BookFormat.AUDIOBOOK ) )
+							.must( f.match().onField( "availableFormats" ).matching( BookFormat.HARDCOVER ) )
+							.must( f.match().onField( "authorCount" ).matching( 1, ValueConvert.NO ) )
+					)
+					.fetchHits( 20 );
+			assertThat( result ).hasSize( 1 );
+		} );
+	}
+
+}
