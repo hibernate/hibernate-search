da6a124b55ee0aa1a8a07485d29820f5bb4ea944
Yoann Rodière <yoann@hibernate.org>
2019-09-09 16:59:44 +0200

    HSEARCH-3671 Create utils to convert from Hibernate ORM API (EntityManager, SessionFactory, ...) to SPI (*Implementor)

---
+++ mapper/orm/src/main/java/org/hibernate/search/mapper/orm/common/impl/HibernateOrmUtils.java
@@ -0,0 +1,43 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.mapper.orm.common.impl;
+
+import java.lang.invoke.MethodHandles;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+import org.hibernate.engine.spi.SessionFactoryImplementor;
+import org.hibernate.engine.spi.SessionImplementor;
+import org.hibernate.search.mapper.orm.logging.impl.Log;
+import org.hibernate.search.util.common.logging.impl.LoggerFactory;
+
+public final class HibernateOrmUtils {
+
+	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
+
+	private HibernateOrmUtils() {
+	}
+
+	public static SessionFactoryImplementor toSessionFactoryImplementor(EntityManagerFactory entityManagerFactory) {
+		try {
+			return entityManagerFactory.unwrap( SessionFactoryImplementor.class );
+		}
+		catch (IllegalStateException e) {
+			throw log.hibernateSessionFactoryAccessError( e );
+		}
+	}
+
+	public static SessionImplementor toSessionImplementor(EntityManager entityManager) {
+		try {
+			return entityManager.unwrap( SessionImplementor.class );
+		}
+		catch (IllegalStateException e) {
+			throw log.hibernateSessionAccessError( e );
+		}
+	}
+
+}
