79b617945e416c3841c0711dd97cde6882eedcc2
Yoann Rodière <yoann@hibernate.org>
2019-09-17 09:29:02 +0200

    HSEARCH-3671 Implement stub aggregations
    It was forgotten in a previous PR.

---
+++ util/internal/integrationtest/common/src/main/java/org/hibernate/search/util/impl/integrationtest/common/stub/backend/search/aggregation/impl/StubSearchAggregation.java
@@ -0,0 +1,21 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.util.impl.integrationtest.common.stub.backend.search.aggregation.impl;
+
+import org.hibernate.search.engine.search.aggregation.SearchAggregation;
+
+class StubSearchAggregation<A> implements SearchAggregation<A> {
+	private final StubAggregationBuilder builder;
+
+	StubSearchAggregation(StubAggregationBuilder builder) {
+		this.builder = builder;
+	}
+
+	StubAggregationBuilder get() {
+		return builder;
+	}
+}
