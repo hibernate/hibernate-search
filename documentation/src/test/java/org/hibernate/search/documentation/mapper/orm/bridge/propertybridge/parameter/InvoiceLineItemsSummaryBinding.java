a4e96c2b7c2f6b9f5e3662fb6c8ebe63e7c9a2d7
Yoann Rodière <yoann@hibernate.org>
2019-09-19 17:54:53 +0200

    HSEARCH-3692 Document property bridges

---
+++ documentation/src/test/java/org/hibernate/search/documentation/mapper/orm/bridge/propertybridge/parameter/InvoiceLineItemsSummaryBinding.java
@@ -0,0 +1,28 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.documentation.mapper.orm.bridge.propertybridge.parameter;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.hibernate.search.mapper.pojo.bridge.mapping.annotation.PropertyBinderRef;
+import org.hibernate.search.mapper.pojo.bridge.mapping.annotation.declaration.PropertyBinding;
+
+//tag::include[]
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.METHOD, ElementType.FIELD })
+@PropertyBinding(binder = @PropertyBinderRef(type = InvoiceLineItemsSummaryBinder.class))
+@Documented
+public @interface InvoiceLineItemsSummaryBinding {
+
+	String fieldName() default ""; // <1>
+
+}
+//end::include[]
