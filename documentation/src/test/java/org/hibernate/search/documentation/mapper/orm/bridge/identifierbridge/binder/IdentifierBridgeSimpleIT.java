df36afee745e9c14fb827ba71c3bac6e95042e0c
Yoann Rodière <yoann@hibernate.org>
2019-09-20 11:58:36 +0200

    HSEARCH-3692 Document identifier bridges

---
+++ documentation/src/test/java/org/hibernate/search/documentation/mapper/orm/bridge/identifierbridge/binder/IdentifierBridgeSimpleIT.java
@@ -0,0 +1,77 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.documentation.mapper.orm.bridge.identifierbridge.binder;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import javax.persistence.EntityManagerFactory;
+
+import org.hibernate.search.documentation.testsupport.BackendConfigurations;
+import org.hibernate.search.mapper.orm.Search;
+import org.hibernate.search.mapper.orm.automaticindexing.AutomaticIndexingSynchronizationStrategyName;
+import org.hibernate.search.mapper.orm.cfg.HibernateOrmMapperSettings;
+import org.hibernate.search.mapper.orm.session.SearchSession;
+import org.hibernate.search.util.impl.integrationtest.common.rule.BackendConfiguration;
+import org.hibernate.search.util.impl.integrationtest.orm.OrmSetupHelper;
+import org.hibernate.search.util.impl.integrationtest.orm.OrmUtils;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class IdentifierBridgeSimpleIT {
+	@Parameterized.Parameters(name = "{0}")
+	public static Object[] backendConfigurations() {
+		return BackendConfigurations.simple().toArray();
+	}
+
+	@Rule
+	public OrmSetupHelper setupHelper;
+
+	private EntityManagerFactory entityManagerFactory;
+
+	public IdentifierBridgeSimpleIT(BackendConfiguration backendConfiguration) {
+		this.setupHelper = OrmSetupHelper.withSingleBackend( backendConfiguration );
+	}
+
+	@Before
+	public void setup() {
+		entityManagerFactory = setupHelper.start()
+				.withProperty(
+						HibernateOrmMapperSettings.AUTOMATIC_INDEXING_SYNCHRONIZATION_STRATEGY,
+						AutomaticIndexingSynchronizationStrategyName.SEARCHABLE
+				)
+				.setup( Book.class );
+	}
+
+	@Test
+	public void smoke() {
+		OrmUtils.withinJPATransaction( entityManagerFactory, entityManager -> {
+
+			Book book = new Book();
+			book.getId().setPublisherId( 1L );
+			book.getId().setPublisherSpecificBookId( 42L );
+			entityManager.persist( book );
+		} );
+
+		OrmUtils.withinJPATransaction( entityManagerFactory, entityManager -> {
+			SearchSession searchSession = Search.session( entityManager );
+
+			List<Book> result = searchSession.search( Book.class )
+					.predicate( f -> f.id().matching( new BookId( 1L, 42L ) ) )
+					.fetchHits( 20 );
+
+			assertThat( result ).hasSize( 1 );
+		} );
+	}
+
+}
---
+++ documentation/src/test/java/org/hibernate/search/documentation/mapper/orm/bridge/identifierbridge/ormcontext/IdentifierBridgeOrmContextIT.java
@@ -0,0 +1,82 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.documentation.mapper.orm.bridge.identifierbridge.ormcontext;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import javax.persistence.EntityManagerFactory;
+
+import org.hibernate.search.documentation.testsupport.BackendConfigurations;
+import org.hibernate.search.mapper.orm.Search;
+import org.hibernate.search.mapper.orm.automaticindexing.AutomaticIndexingSynchronizationStrategyName;
+import org.hibernate.search.mapper.orm.cfg.HibernateOrmMapperSettings;
+import org.hibernate.search.mapper.orm.common.EntityReference;
+import org.hibernate.search.mapper.orm.session.SearchSession;
+import org.hibernate.search.util.impl.integrationtest.common.rule.BackendConfiguration;
+import org.hibernate.search.util.impl.integrationtest.orm.OrmSetupHelper;
+import org.hibernate.search.util.impl.integrationtest.orm.OrmUtils;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class IdentifierBridgeOrmContextIT {
+	@Parameterized.Parameters(name = "{0}")
+	public static Object[] backendConfigurations() {
+		return BackendConfigurations.simple().toArray();
+	}
+
+	@Rule
+	public OrmSetupHelper setupHelper;
+
+	private EntityManagerFactory entityManagerFactory;
+
+	public IdentifierBridgeOrmContextIT(BackendConfiguration backendConfiguration) {
+		this.setupHelper = OrmSetupHelper.withSingleBackend( backendConfiguration );
+	}
+
+	@Before
+	public void setup() {
+		entityManagerFactory = setupHelper.start()
+				.withProperty(
+						HibernateOrmMapperSettings.AUTOMATIC_INDEXING_SYNCHRONIZATION_STRATEGY,
+						AutomaticIndexingSynchronizationStrategyName.SEARCHABLE
+				)
+				.setup( MyEntity.class );
+	}
+
+	@Test
+	public void smoke() {
+		// See MyDataValueBridge
+		entityManagerFactory.getProperties().put( "test.data.indexed", MyData.VALUE1 );
+		OrmUtils.withinJPATransaction( entityManagerFactory, entityManager -> {
+			MyEntity myEntity = new MyEntity();
+			myEntity.setId( MyData.VALUE3 );
+			entityManager.persist( myEntity );
+		} );
+
+		OrmUtils.withinJPATransaction( entityManagerFactory, entityManager -> {
+			// See MyDataValueBridge
+			entityManager.setProperty( "test.data.projected", MyData.VALUE2 );
+
+			SearchSession searchSession = Search.session( entityManager );
+
+			List<EntityReference> result = searchSession.search( MyEntity.class )
+					.asProjection( f -> f.entityReference() )
+					.predicate( f -> f.matchAll() )
+					.fetchHits( 20 );
+
+			assertThat( result ).extracting( EntityReference::getId ).containsExactly( MyData.VALUE2 );
+		} );
+	}
+
+}
