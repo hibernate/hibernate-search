29bc4bc1ca3800f719e694b37be7a7b899b8e94f
Yoann Rodière <yoann@hibernate.org>
2019-10-10 19:04:33 +0200

    HSEARCH-1375 Test failure reporting for IndexIndexer operations

--- integrationtest/backend/tck/src/main/java/org/hibernate/search/integrationtest/backend/tck/work/IndexIndexerIT.java
+++ integrationtest/backend/tck/src/main/java/org/hibernate/search/integrationtest/backend/tck/work/IndexIndexerIT.java
@@ -6,8 +6,11 @@
  */
 package org.hibernate.search.integrationtest.backend.tck.work;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hibernate.search.util.impl.integrationtest.common.stub.mapper.StubMapperUtils.referenceProvider;
 
+import java.io.IOException;
+import java.lang.invoke.MethodHandles;
 import java.util.concurrent.CompletableFuture;
 
 import org.hibernate.search.engine.backend.document.DocumentElement;
@@ -17,21 +20,30 @@ import org.hibernate.search.engine.backend.work.execution.DocumentCommitStrategy
 import org.hibernate.search.engine.backend.work.execution.spi.IndexIndexer;
 import org.hibernate.search.engine.backend.work.execution.spi.IndexWorkspace;
 import org.hibernate.search.engine.backend.common.DocumentReference;
+import org.hibernate.search.engine.cfg.EngineSettings;
 import org.hibernate.search.engine.search.query.SearchQuery;
 import org.hibernate.search.integrationtest.backend.tck.testsupport.util.rule.SearchSetupHelper;
+import org.hibernate.search.util.common.logging.impl.Log;
+import org.hibernate.search.util.common.logging.impl.LoggerFactory;
+import org.hibernate.search.util.impl.integrationtest.common.stub.StubFailureHandler;
 import org.hibernate.search.util.impl.integrationtest.common.stub.mapper.StubMappingIndexManager;
+import org.hibernate.search.util.impl.test.FutureAssert;
+import org.hibernate.search.util.impl.test.rule.ExpectedLog4jLog;
+import org.hibernate.search.util.impl.test.rule.StaticCounters;
 
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
+import org.apache.log4j.Level;
 import org.assertj.core.api.Assertions;
+import org.awaitility.Awaitility;
 
 /**
  * Verify that the {@link IndexIndexer}, provided by a backend, is working properly, storing correctly the indexes.
  */
 public class IndexIndexerIT {
+
+	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
 
 	private static final String INDEX_NAME = "lordOfTheRingsChapters";
 	private static final int NUMBER_OF_BOOKS = 200;
@@ -40,24 +52,18 @@ public class IndexIndexerIT {
 	public SearchSetupHelper setupHelper = new SearchSetupHelper();
 
 	@Rule
-	public ExpectedException thrown = ExpectedException.none();
+	public ExpectedLog4jLog logged = ExpectedLog4jLog.create();
+
+	@Rule
+	public StaticCounters staticCounters = new StaticCounters();
 
 	private IndexMapping indexMapping;
 	private StubMappingIndexManager indexManager;
 
-	@Before
-	public void before() {
-		setupHelper.start()
-				.withIndex(
-						INDEX_NAME,
-						ctx -> this.indexMapping = new IndexMapping( ctx.getSchemaElement() ),
-						indexManager -> this.indexManager = indexManager
-				)
-				.setup();
-	}
-
 	@Test
-	public void checkAllDocumentsAreSearchable() {
+	public void success() {
+		setup( null );
+
 		IndexIndexer<? extends DocumentElement> indexer =
 				indexManager.createIndexer( DocumentCommitStrategy.NONE );
 		CompletableFuture<?>[] tasks = new CompletableFuture<?>[NUMBER_OF_BOOKS];
@@ -69,7 +75,12 @@ public class IndexIndexerIT {
 				document.addValue( indexMapping.title, "The Lord of the Rings cap. " + id );
 			} );
 		}
-		CompletableFuture.allOf( tasks ).join();
+		CompletableFuture<?> future = CompletableFuture.allOf( tasks );
+		Awaitility.await().until( future::isDone );
+
+		// The operations should succeed.
+		FutureAssert.assertThat( future ).isSuccessful();
+
 		workspace.flush().join();
 
 		SearchQuery<DocumentReference> query = indexManager.createScope().query()
@@ -77,6 +88,84 @@ public class IndexIndexerIT {
 				.toQuery();
 
 		Assertions.assertThat( query.fetchTotalHitCount() ).isEqualTo( NUMBER_OF_BOOKS );
+	}
+
+	@Test
+	public void failure_defaultHandler() {
+		setup( null );
+
+		IndexIndexer<? extends DocumentElement> indexer =
+				indexManager.createIndexer( DocumentCommitStrategy.NONE );
+
+		// Trigger failures in the next operations
+		setupHelper.getBackendAccessor().ensureIndexOperationsFail( INDEX_NAME );
+
+		// The default failure handler should log at the ERROR level
+		logged.expectLevel( Level.ERROR ).once();
+
+		CompletableFuture<?> future = indexer.add( referenceProvider( "1" ), document -> {
+			document.addValue( indexMapping.title, "Document #1" );
+		} );
+		Awaitility.await().until( future::isDone );
+
+		// The operation should fail.
+		// Just check the failure is reported through the completable future.
+		FutureAssert.assertThat( future ).isFailed();
+
+		try {
+			setupHelper.cleanUp();
+		}
+		catch (RuntimeException | IOException e) {
+			log.debug( "Expected error while shutting down Hibernate Search, caused by the deletion of an index", e );
+		}
+	}
+
+	@Test
+	public void failure_customHandler() {
+		setup( StubFailureHandler.class.getName() );
+
+		IndexIndexer<? extends DocumentElement> indexer =
+				indexManager.createIndexer( DocumentCommitStrategy.NONE );
+
+		// Trigger failures in the next operations
+		setupHelper.getBackendAccessor().ensureIndexOperationsFail( INDEX_NAME );
+
+		// The default failure handler should not receive any failure
+		logged.expectLevel( Level.ERROR ).never();
+
+		assertThat( staticCounters.get( StubFailureHandler.HANDLE_INDEX_CONTEXT ) ).isEqualTo( 0 );
+		assertThat( staticCounters.get( StubFailureHandler.HANDLE_GENERIC_CONTEXT ) ).isEqualTo( 0 );
+
+		CompletableFuture<?> future = indexer.add( referenceProvider( "1" ), document -> {
+			document.addValue( indexMapping.title, "Document #1" );
+		} );
+		Awaitility.await().until( future::isDone );
+
+		// The operation should fail.
+		// Just check the failure is reported through the completable future.
+		FutureAssert.assertThat( future ).isFailed();
+
+		// The custom failure handler should have received a failure
+		assertThat( staticCounters.get( StubFailureHandler.HANDLE_INDEX_CONTEXT ) ).isEqualTo( 1 );
+		assertThat( staticCounters.get( StubFailureHandler.HANDLE_GENERIC_CONTEXT ) ).isEqualTo( 0 );
+
+		try {
+			setupHelper.cleanUp();
+		}
+		catch (RuntimeException | IOException e) {
+			log.debug( "Expected error while shutting down Hibernate Search, caused by the deletion of an index", e );
+		}
+	}
+
+	private void setup(String failureHandler) {
+		setupHelper.start()
+				.withPropertyRadical( EngineSettings.Radicals.BACKGROUND_FAILURE_HANDLER, failureHandler )
+				.withIndex(
+						INDEX_NAME,
+						ctx -> this.indexMapping = new IndexMapping( ctx.getSchemaElement() ),
+						indexManager -> this.indexManager = indexManager
+				)
+				.setup();
 	}
 
 	private static class IndexMapping {
