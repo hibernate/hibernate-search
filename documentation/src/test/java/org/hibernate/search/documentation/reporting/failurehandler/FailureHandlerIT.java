2b8ec46b36daddb4687999d86f23b9fb2ef49152
Yoann Rodière <yoann@hibernate.org>
2019-10-11 10:39:36 +0200

    HSEARCH-1353 Document background failure handling

---
+++ documentation/src/test/java/org/hibernate/search/documentation/reporting/failurehandler/FailureHandlerIT.java
@@ -0,0 +1,77 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.documentation.reporting.failurehandler;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.search.documentation.testsupport.BackendConfigurations;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.KeywordField;
+import org.hibernate.search.util.impl.integrationtest.common.rule.BackendConfiguration;
+import org.hibernate.search.util.impl.integrationtest.mapper.orm.OrmSetupHelper;
+import org.hibernate.search.util.impl.test.rule.StaticCounters;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class FailureHandlerIT {
+
+	@Parameterized.Parameters(name = "{0}")
+	public static Object[] backendConfigurations() {
+		return BackendConfigurations.simple().toArray();
+	}
+
+	@Rule
+	public OrmSetupHelper setupHelper;
+
+	@Rule
+	public StaticCounters staticCounters = new StaticCounters();
+
+	public FailureHandlerIT(BackendConfiguration backendConfiguration) {
+		this.setupHelper = OrmSetupHelper.withSingleBackend( backendConfiguration );
+	}
+
+	@Test
+	public void smoke() {
+		assertThat( staticCounters.get( MyFailureHandler.INSTANCES ) ).isEqualTo( 0 );
+
+		setupHelper.start()
+				.withProperties( "/reporting/failurehandler.properties" )
+				.setup( IndexedEntity.class );
+
+		assertThat( staticCounters.get( MyFailureHandler.INSTANCES ) ).isEqualTo( 1 );
+	}
+
+	@Entity(name = IndexedEntity.NAME)
+	@Indexed
+	static class IndexedEntity {
+
+		static final String NAME = "indexed";
+
+		@Id
+		@GeneratedValue
+		private Integer id;
+
+		@KeywordField
+		private String text;
+
+		public String getText() {
+			return text;
+		}
+
+		public void setText(String text) {
+			this.text = text;
+		}
+	}
+}
