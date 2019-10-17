133f0237b19a0a4aa468b08310e1e6bd67285b2b
Yoann Rodière <yoann@hibernate.org>
2019-10-17 16:17:15 +0200

    HSEARCH-1375 Automatically wrap failure handlers to catch and log unexpected exceptions
    It's rather critical in this case, since the failure handler is called
    in... failure handling code, where we usually don't expect another
    failure.

---
+++ engine/src/main/java/org/hibernate/search/engine/reporting/impl/FailSafeFailureHandlerWrapper.java
@@ -0,0 +1,47 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.engine.reporting.impl;
+
+import java.lang.invoke.MethodHandles;
+
+import org.hibernate.search.engine.logging.impl.Log;
+import org.hibernate.search.engine.reporting.FailureContext;
+import org.hibernate.search.engine.reporting.FailureHandler;
+import org.hibernate.search.engine.reporting.IndexFailureContext;
+import org.hibernate.search.util.common.logging.impl.LoggerFactory;
+
+public class FailSafeFailureHandlerWrapper implements FailureHandler {
+
+	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
+
+	private final FailureHandler delegate;
+
+	public FailSafeFailureHandlerWrapper(FailureHandler delegate) {
+		this.delegate = delegate;
+	}
+
+	@Override
+	public void handle(FailureContext context) {
+		try {
+			delegate.handle( context );
+		}
+		catch (Throwable t) {
+			log.failureInFailureHandler( t );
+		}
+	}
+
+	@Override
+	public void handle(IndexFailureContext context) {
+		try {
+			delegate.handle( context );
+		}
+		catch (Throwable t) {
+			log.failureInFailureHandler( t );
+		}
+	}
+
+}
