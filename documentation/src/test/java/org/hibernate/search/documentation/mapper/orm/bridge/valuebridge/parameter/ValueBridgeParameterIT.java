27483dfa4cc4844923689935252d1eb3ccc8ef6f
Yoann Rodière <yoann@hibernate.org>
2019-11-15 14:44:59 +0100

    HSEARCH-3135 Document how to implement a parameterized value bridge

---
+++ documentation/src/test/java/org/hibernate/search/documentation/mapper/orm/bridge/valuebridge/parameter/ValueBridgeParameterIT.java
@@ -0,0 +1,98 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.documentation.mapper.orm.bridge.valuebridge.parameter;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.Year;
+import java.util.List;
+import javax.persistence.EntityManagerFactory;
+
+import org.hibernate.search.documentation.testsupport.BackendConfigurations;
+import org.hibernate.search.mapper.orm.Search;
+import org.hibernate.search.mapper.orm.automaticindexing.AutomaticIndexingSynchronizationStrategyName;
+import org.hibernate.search.mapper.orm.cfg.HibernateOrmMapperSettings;
+import org.hibernate.search.mapper.orm.session.SearchSession;
+import org.hibernate.search.util.impl.integrationtest.common.rule.BackendConfiguration;
+import org.hibernate.search.util.impl.integrationtest.mapper.orm.OrmSetupHelper;
+import org.hibernate.search.util.impl.integrationtest.mapper.orm.OrmUtils;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class ValueBridgeParameterIT {
+	@Parameterized.Parameters(name = "{0}")
+	public static Object[] backendConfigurations() {
+		return BackendConfigurations.simple().toArray();
+	}
+
+	@Rule
+	public OrmSetupHelper setupHelper;
+
+	private EntityManagerFactory entityManagerFactory;
+
+	public ValueBridgeParameterIT(BackendConfiguration backendConfiguration) {
+		this.setupHelper = OrmSetupHelper.withSingleBackend( backendConfiguration );
+	}
+
+	@Before
+	public void setup() {
+		entityManagerFactory = setupHelper.start()
+				.withProperty(
+						HibernateOrmMapperSettings.AUTOMATIC_INDEXING_SYNCHRONIZATION_STRATEGY,
+						AutomaticIndexingSynchronizationStrategyName.SEARCHABLE
+				)
+				.setup( Book.class );
+	}
+
+	@Test
+	public void smoke() {
+		OrmUtils.withinJPATransaction( entityManagerFactory, entityManager -> {
+			Book book1 = new Book();
+			book1.setTitle( "The Government Is Evil" );
+			book1.setPublished( true );
+			book1.getCensorshipAssessments().put( Year.of( 2014 ), false );
+			book1.getCensorshipAssessments().put( Year.of( 2020 ), true );
+			entityManager.persist( book1 );
+
+			Book book2 = new Book();
+			book2.setTitle( "The Government Is Not The Problem, Politicians Are" );
+			book2.setPublished( false );
+			book2.getCensorshipAssessments().put( Year.of( 2014 ), false );
+			book2.getCensorshipAssessments().put( Year.of( 2020 ), false );
+			entityManager.persist( book2 );
+		} );
+
+		OrmUtils.withinJPATransaction( entityManagerFactory, entityManager -> {
+			SearchSession searchSession = Search.session( entityManager );
+
+			List<Book> result = searchSession.search( Book.class )
+					.predicate( f -> f.match().field( "published" )
+							.matching( false ) )
+					.fetchHits( 20 );
+
+			assertThat( result ).hasSize( 1 );
+		} );
+
+		OrmUtils.withinJPATransaction( entityManagerFactory, entityManager -> {
+			SearchSession searchSession = Search.session( entityManager );
+
+			List<Book> result = searchSession.search( Book.class )
+					.predicate( f -> f.match().field( "censorshipAssessments_allYears" )
+							.matching( true ) )
+					.fetchHits( 20 );
+
+			assertThat( result ).hasSize( 1 );
+		} );
+	}
+
+}
