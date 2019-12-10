c28bdb21edd898c1f54a62538a41f80360b1c9e9
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2019-12-10 14:33:05 +0100

    HSEARCH-3352 Introduce the query service program interface

---
+++ engine/src/main/java/org/hibernate/search/engine/search/query/spi/SearchQueryImplementor.java
@@ -0,0 +1,21 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.engine.search.query.spi;
+
+import org.hibernate.search.engine.search.query.SearchQuery;
+
+/**
+ * Defines the "service program contract" for {@link SearchQuery}.
+ * <p>
+ * Methods on {@link SearchQuery} are not supposed to change the internal state of the instance.
+ * Methods here can do that.
+ *
+ * @param <H> The type of query hits.
+ */
+public interface SearchQueryImplementor<H> extends SearchQuery<H> {
+
+}
