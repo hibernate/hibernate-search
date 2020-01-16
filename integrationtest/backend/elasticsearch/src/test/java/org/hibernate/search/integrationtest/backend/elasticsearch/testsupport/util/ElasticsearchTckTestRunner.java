183e6c25b4f09ed64168795c6453b435a436358a
Yoann Rodière <yoann@hibernate.org>
2020-01-16 10:02:08 +0100

    HSEARCH-3802 Revert an unnecessary change to ElasticsearchTckTestRunner

--- integrationtest/backend/elasticsearch/src/test/java/org/hibernate/search/integrationtest/backend/elasticsearch/testsupport/util/ElasticsearchTckTestRunner.java
+++ integrationtest/backend/elasticsearch/src/test/java/org/hibernate/search/integrationtest/backend/elasticsearch/testsupport/util/ElasticsearchTckTestRunner.java
@@ -21,6 +21,6 @@ import org.junit.runner.RunWith;
  * @author Gunnar Morling
  */
 @RunWith(ClasspathSuite.class)
-@ClasspathSuite.ClassnameFilters({ ".*\\.tck\\..*MultiTenancyBaseIT" })
+@ClasspathSuite.ClassnameFilters({ ".*\\.tck\\..*" })
 public class ElasticsearchTckTestRunner {
 }
