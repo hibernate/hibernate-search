986230824d1a226cf27b0763d7fffd4b0d3c1cca
Yoann Rodière <yoann@hibernate.org>
2020-01-21 16:52:23 +0100

    HSEARCH-3812 Adapt the timeout message to be relevant even for ES requests that are not queries
    For example a PUT /.../_mapping could time out too, and in that case
    the exception message should not mention a "search query".

--- integrationtest/backend/tck/src/main/java/org/hibernate/search/integrationtest/backend/tck/search/query/SearchQueryTimeoutIT.java
+++ integrationtest/backend/tck/src/main/java/org/hibernate/search/integrationtest/backend/tck/search/query/SearchQueryTimeoutIT.java
@@ -93,7 +93,7 @@ public class SearchQueryTimeoutIT {
 		SubTest.expectException( () -> query.fetchAll() )
 				.assertThrown()
 				.isInstanceOf( SearchTimeoutException.class )
-				.hasMessageContaining( "Search query exceeded the timeout of 0 seconds and 1 nanoseconds: " );
+				.hasMessageContaining( " exceeded the timeout of 0 seconds and 1 nanoseconds: " );
 	}
 
 	@Test
@@ -105,7 +105,7 @@ public class SearchQueryTimeoutIT {
 		SubTest.expectException( () -> query.fetchTotalHitCount() )
 				.assertThrown()
 				.isInstanceOf( SearchTimeoutException.class )
-				.hasMessageContaining( "Search query exceeded the timeout of 0 seconds and 1 nanoseconds: " );
+				.hasMessageContaining( " exceeded the timeout of 0 seconds and 1 nanoseconds: " );
 	}
 
 	@Test
