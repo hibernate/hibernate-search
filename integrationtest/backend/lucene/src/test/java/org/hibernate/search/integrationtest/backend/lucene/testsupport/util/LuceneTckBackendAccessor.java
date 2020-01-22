aadd55fce144dd898fe0124c6d5a1a12fdd0687e
Yoann Rodière <yoann@hibernate.org>
2020-01-22 13:32:18 +0100

    HSEARCH-3746 Create invalid index files when nuking a Lucene index
    So as to fail on Windows, too.

--- integrationtest/backend/lucene/src/test/java/org/hibernate/search/integrationtest/backend/lucene/testsupport/util/LuceneTckBackendAccessor.java
+++ integrationtest/backend/lucene/src/test/java/org/hibernate/search/integrationtest/backend/lucene/testsupport/util/LuceneTckBackendAccessor.java
@@ -7,10 +7,13 @@
 package org.hibernate.search.integrationtest.backend.lucene.testsupport.util;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
+import java.nio.file.StandardOpenOption;
 import java.nio.file.attribute.BasicFileAttributes;
 
 import org.hibernate.search.integrationtest.backend.tck.testsupport.util.TckBackendAccessor;
@@ -40,9 +43,8 @@ class LuceneTckBackendAccessor implements TckBackendAccessor {
 			// Try to remove the content of the index, or to prevent future access to the files
 			// if we can't delete them (can happen on windows).
 			Files.list( indexPath ).forEach( path -> nukeOrLogRecursively( path, indexName ) );
-			// We don't want to delete the index directory itself, because HSearch will just re-create it.
-			// Try to remove all permissions, which will prevent HSearch from creating new content.
-			nukeOrLog( indexPath, indexName, false, null );
+
+			nukeOrLogIndexTopLevelDirectory( indexPath, indexName );
 		}
 		catch (RuntimeException | IOException e) {
 			throw new IllegalStateException(
@@ -80,13 +82,51 @@ class LuceneTckBackendAccessor implements TckBackendAccessor {
 		}
 	}
 
-	private void nukeOrLog(Path path, String indexName, boolean allowDelete, IOException previousException) {
+	private void nukeOrLogIndexTopLevelDirectory(Path indexPath, String indexName) {
+		// We don't want to delete the index directory itself, because HSearch will just re-create it.
+		// Try to remove all permissions, which will prevent HSearch from creating new content.
+		boolean nukeSucceeded = nukeOrLog( indexPath, indexName, false, null );
+		if ( nukeSucceeded ) {
+			return; // Success, don't worry about the rest
+		}
+
+		// We didn't manage to make the index directory unreadable; we're probably on Windows.
+		// Let's create invalid files, so that attempts to write will fail.
+		for ( int i = 0; i < 20; i++ ) {
+			createInvalidFile( indexPath, indexName, "_" + i + ".cfe" );
+			createInvalidFile( indexPath, indexName, "_" + i + ".cfs" );
+			createInvalidFile( indexPath, indexName, "_" + i + ".si" );
+			createInvalidFile( indexPath, indexName, "segments_" + i );
+		}
+	}
+
+	private void createInvalidFile(Path indexPath, String indexName, String fileName) {
+		Path path = indexPath.resolve( fileName );
+		try {
+			try ( OutputStream os = Files.newOutputStream( path, StandardOpenOption.CREATE ) ) {
+				os.write( "qwfbqfbwqfqw".getBytes( StandardCharsets.UTF_8 ) );
+			}
+			makeInaccessible( path );
+		}
+		catch (IOException e) {
+			log.warnf(
+					e,
+					"Failed to create invalid file '%1$s' while nuking index '%2$s'"
+							+ " to trigger failures in tests."
+							+ " Index nuking is incomplete and may not trigger failures "
+							+ " depending on the current state of the Lucene backend.",
+					path, indexName
+			);
+		}
+	}
+
+	private boolean nukeOrLog(Path path, String indexName, boolean allowDelete, IOException previousException) {
 		IOException exception = previousException;
 		if ( allowDelete ) {
 			try {
 				// Try to delete it...
 				delete( path );
-				return; // Success, don't worry about the rest
+				return true; // Success, don't worry about the rest
 			}
 			catch (IOException e) {
 				exception = Throwables.combine( exception, e );
@@ -97,13 +137,14 @@ class LuceneTckBackendAccessor implements TckBackendAccessor {
 		// This may also prevent HSearch from re-creating the index automatically.
 		try {
 			makeInaccessible( path );
-			return; // Success, don't worry about the rest
+			return true; // Success, don't worry about the rest
 		}
 		catch (IOException e) {
 			exception = Throwables.combine( exception, e );
 		}
 
 		warnNukeFailed( exception, path, indexName );
+		return false;
 	}
 
 	private void delete(Path path) throws IOException {
@@ -124,9 +165,9 @@ class LuceneTckBackendAccessor implements TckBackendAccessor {
 	private void warnNukeFailed(IOException exception, Path path, String indexName) {
 		log.warnf(
 				exception,
-				"Failed to access file '%1$s' while deleting index '%2$s'"
+				"Failed to access file '%1$s' while nuking index '%2$s'"
 						+ " to trigger failures in tests."
-						+ " Index deletion is incomplete and may not trigger failures "
+						+ " Index nuking is incomplete and may not trigger failures "
 						+ " depending on the current state of the Lucene backend.",
 				path, indexName
 		);
