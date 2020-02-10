c31e62e3da473f1700cc7beb0a6b60fda51ee2de
Yoann Rodière <yoann@hibernate.org>
2020-02-10 17:00:40 +0100

    HSEARCH-3827 Add performance tests for the Lucene backend

---
+++ integrationtest/performance/backend/lucene/src/main/java/org/hibernate/search/integrationtest/performance/backend/lucene/testsupport/LucenePerformanceAnalysisConfigurer.java
@@ -0,0 +1,34 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.integrationtest.performance.backend.lucene.testsupport;
+
+import org.hibernate.search.backend.lucene.analysis.LuceneAnalysisConfigurationContext;
+import org.hibernate.search.backend.lucene.analysis.LuceneAnalysisConfigurer;
+import org.hibernate.search.integrationtest.performance.backend.base.testsupport.analysis.Analyzers;
+
+import org.apache.lucene.analysis.core.LowerCaseFilterFactory;
+import org.apache.lucene.analysis.miscellaneous.ASCIIFoldingFilterFactory;
+import org.apache.lucene.analysis.snowball.SnowballPorterFilterFactory;
+import org.apache.lucene.analysis.standard.StandardTokenizerFactory;
+
+public class LucenePerformanceAnalysisConfigurer implements LuceneAnalysisConfigurer {
+
+	@Override
+	public void configure(LuceneAnalysisConfigurationContext context) {
+		context.analyzer( Analyzers.ANALYZER_ENGLISH ).custom()
+				.tokenizer( StandardTokenizerFactory.class )
+				.tokenFilter( LowerCaseFilterFactory.class )
+				.tokenFilter( SnowballPorterFilterFactory.class )
+						.param( "language", "English" )
+				.tokenFilter( ASCIIFoldingFilterFactory.class );
+
+		context.normalizer( Analyzers.NORMALIZER_ENGLISH ).custom()
+				.tokenFilter( LowerCaseFilterFactory.class )
+				.tokenFilter( ASCIIFoldingFilterFactory.class );
+	}
+
+}
---
+++ integrationtest/performance/backend/lucene/src/test/java/org/hibernate/search/integrationtest/performance/backend/lucene/SmokeIT.java
@@ -0,0 +1,43 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.integrationtest.performance.backend.lucene;
+
+import org.junit.Test;
+
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
+
+/**
+ * Test that JMH benchmarks work correctly on a very short run.
+ * <p>
+ * This may not work correctly when run from the IDE.
+ * <p>
+ * See README to know how to run the benchmark from the command line to obtain more reliable results.
+ */
+public class SmokeIT {
+
+	@Test
+	public void test() throws RunnerException {
+		Options opts = new OptionsBuilder()
+				.include( ".*" )
+				.warmupIterations( 0 )
+				.measurementIterations( 1 )
+				.measurementTime( TimeValue.seconds( 1 ) )
+				.param( "initialIndexSize", "100" )
+				.param( "batchSize", "10" )
+				.param( "maxResults", "10" )
+				.shouldFailOnError( true )
+				.forks( 0 ) // To simplify debugging; Remember this implies JVM parameters via @Fork won't be applied.
+				.build();
+
+		new Runner( opts ).run();
+	}
+
+}
