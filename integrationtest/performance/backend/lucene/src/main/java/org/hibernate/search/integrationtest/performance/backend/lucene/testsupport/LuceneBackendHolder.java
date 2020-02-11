3de5a26fc6fd9cd10f94aa6ea20fc48e3a187a2b
Yoann Rodière <yoann@hibernate.org>
2020-02-11 13:00:23 +0100

    HSEARCH-3827 Enable comparison of multiple backend configurations in benchmarks

--- integrationtest/performance/backend/lucene/src/main/java/org/hibernate/search/integrationtest/performance/backend/lucene/testsupport/LuceneBackendHolder.java
+++ integrationtest/performance/backend/lucene/src/main/java/org/hibernate/search/integrationtest/performance/backend/lucene/testsupport/LuceneBackendHolder.java
@@ -16,11 +16,25 @@ import org.hibernate.search.engine.cfg.spi.ConfigurationPropertySource;
 import org.hibernate.search.integrationtest.performance.backend.base.testsupport.filesystem.TemporaryFileHolder;
 import org.hibernate.search.integrationtest.performance.backend.base.testsupport.index.AbstractBackendHolder;
 
+import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
 
 @State(Scope.Benchmark)
 public class LuceneBackendHolder extends AbstractBackendHolder {
+
+	/**
+	 * A list of configuration properties to apply to the backend and indexes.
+	 * <p>
+	 * Format: {@code <key>=<value>&<key2>=<value2>} (etc.).
+	 * Multiple configurations can be tested by providing multiple values for this parameter,
+	 * e.g. {@code foo=1&bar=2,foo=2&bar=1} for two configurations setting {@code foo} and {@code bar} to different values.
+	 * <p>
+	 * Note that configuration properties are applied both at the backend level and at the index level,
+	 * so using the "index_defaults." prefix is optional when setting index-level properties.
+	 */
+	@Param({ "", "io.commit_interval=1000", "io.commit_interval=1000&io.refresh_interval=1000", "io.strategy=debug" })
+	private String configuration;
 
 	@Override
 	protected ConfigurationPropertySource getDefaultBackendProperties(TemporaryFileHolder temporaryFileHolder)
@@ -32,5 +46,10 @@ public class LuceneBackendHolder extends AbstractBackendHolder {
 		map.put( LuceneBackendSettings.ANALYSIS_CONFIGURER, LucenePerformanceAnalysisConfigurer.class );
 
 		return ConfigurationPropertySource.fromMap( map );
+	}
+
+	@Override
+	protected String getConfigurationParameter() {
+		return configuration;
 	}
 }
