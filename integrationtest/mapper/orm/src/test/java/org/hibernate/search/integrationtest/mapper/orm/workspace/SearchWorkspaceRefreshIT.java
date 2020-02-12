9cde7c5e1832c9e45974c831c763358137a3412b
Yoann Rodière <yoann@hibernate.org>
2020-02-12 15:58:22 +0100

    HSEARCH-3460 Test APIs for explicit index refresh

---
+++ integrationtest/mapper/orm/src/test/java/org/hibernate/search/integrationtest/mapper/orm/workspace/SearchWorkspaceRefreshIT.java
@@ -0,0 +1,32 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.integrationtest.mapper.orm.workspace;
+
+import java.util.concurrent.CompletableFuture;
+
+import org.hibernate.search.mapper.orm.work.SearchWorkspace;
+import org.hibernate.search.util.impl.integrationtest.common.rule.BackendMock;
+import org.hibernate.search.util.impl.test.annotation.TestForIssue;
+
+@TestForIssue(jiraKey = "HSEARCH-3460")
+public class SearchWorkspaceRefreshIT extends AbstractSearchWorkspaceSimpleOperationIT {
+	@Override
+	protected void expectWork(BackendMock backendMock, String indexName, CompletableFuture<?> future) {
+		backendMock.expectIndexScopeWorks( indexName )
+				.refresh( future );
+	}
+
+	@Override
+	protected void executeSync(SearchWorkspace workspace) {
+		workspace.refresh();
+	}
+
+	@Override
+	protected CompletableFuture<?> executeAsync(SearchWorkspace workspace) {
+		return workspace.refreshAsync();
+	}
+}
