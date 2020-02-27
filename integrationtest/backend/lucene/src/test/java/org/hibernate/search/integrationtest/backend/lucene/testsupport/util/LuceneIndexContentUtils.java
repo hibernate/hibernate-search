4fa08be9e1251dfdcfe92c3e914df4e04e8f179b
Yoann Rodière <yoann@hibernate.org>
2020-02-27 14:29:36 +0100

    HSEARCH-3759 Remove unnecessary copying of indexes in Lucene integration tests

--- integrationtest/backend/lucene/src/test/java/org/hibernate/search/integrationtest/backend/lucene/testsupport/util/LuceneIndexContentUtils.java
+++ integrationtest/backend/lucene/src/test/java/org/hibernate/search/integrationtest/backend/lucene/testsupport/util/LuceneIndexContentUtils.java
@@ -7,19 +7,11 @@
 package org.hibernate.search.integrationtest.backend.lucene.testsupport.util;
 
 import java.io.IOException;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
 
 import org.hibernate.search.integrationtest.backend.tck.testsupport.util.rule.SearchSetupHelper;
 
-import org.junit.rules.TemporaryFolder;
-
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.FSDirectory;
 import org.assertj.core.api.iterable.ThrowingExtractor;
 
 public final class LuceneIndexContentUtils {
@@ -27,47 +19,14 @@ public final class LuceneIndexContentUtils {
 	private LuceneIndexContentUtils() {
 	}
 
-	public static <T> T doOnIndexCopy(SearchSetupHelper setupHelper, TemporaryFolder temporaryFolder,
+	// Reads directly from the index, without going through Hibernate Search
+	public static <T> T readIndex(SearchSetupHelper setupHelper,
 			String indexName, ThrowingExtractor<DirectoryReader, T, IOException> action) throws IOException {
-		Path indexCopyPath = temporaryFolder.getRoot().toPath().resolve( indexName + "_copy" );
-
-		T result;
 		LuceneTckBackendAccessor accessor = (LuceneTckBackendAccessor) setupHelper.getBackendAccessor();
-		try {
-			// Copy the index to be able to open a directory despite the lock
-			accessor.copyIndexContent( indexCopyPath, indexName );
-
-			try ( Directory directory = FSDirectory.open( indexCopyPath );
-					DirectoryReader reader = DirectoryReader.open( directory ) ) {
-				result = action.apply( reader );
-			}
+		try ( Directory directory = accessor.openDirectory( indexName );
+				DirectoryReader reader = DirectoryReader.open( directory ) ) {
+			return action.apply( reader );
 		}
-		finally {
-			try {
-				deleteRecursively( indexCopyPath );
-			}
-			catch (RuntimeException | IOException e) {
-				System.out.println( "Could not delete '" + indexCopyPath + "': " + e );
-			}
-		}
-
-		return result;
-	}
-
-	public static void deleteRecursively(Path path) throws IOException {
-		Files.walkFileTree( path, new SimpleFileVisitor<Path>() {
-			@Override
-			public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-				Files.delete( file );
-				return FileVisitResult.CONTINUE;
-			}
-
-			@Override
-			public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
-				Files.deleteIfExists( dir );
-				return FileVisitResult.CONTINUE;
-			}
-		} );
 	}
 
 }
