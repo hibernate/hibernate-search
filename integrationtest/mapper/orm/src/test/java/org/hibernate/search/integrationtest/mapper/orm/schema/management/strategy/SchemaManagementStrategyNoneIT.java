070168d85032554697120f25e08eb4f2708757f0
Yoann Rodière <yoann@hibernate.org>
2020-02-28 15:49:03 +0100

    HSEARCH-3759 Test the schema management strategy in the ORM mapper

---
+++ integrationtest/mapper/orm/src/test/java/org/hibernate/search/integrationtest/mapper/orm/schema/management/strategy/SchemaManagementStrategyNoneIT.java
@@ -0,0 +1,79 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.integrationtest.mapper.orm.schema.management.strategy;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.search.mapper.orm.cfg.HibernateOrmMapperSettings;
+import org.hibernate.search.mapper.orm.schema.management.SchemaManagementStrategyName;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
+import org.hibernate.search.util.impl.integrationtest.common.rule.BackendMock;
+import org.hibernate.search.util.impl.integrationtest.mapper.orm.OrmSetupHelper;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class SchemaManagementStrategyNoneIT {
+
+	@Rule
+	public BackendMock backendMock = new BackendMock( "stubBackend" );
+
+	@Rule
+	public OrmSetupHelper setupHelper = OrmSetupHelper.withBackendMock( backendMock );
+
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+
+	@Test
+	public void none() {
+		SessionFactory sessionFactory = setup();
+		// Nothing should have happened
+		backendMock.verifyExpectationsMet();
+
+		sessionFactory.close();
+		// Nothing should have happened
+		backendMock.verifyExpectationsMet();
+	}
+
+	private SessionFactory setup() {
+		backendMock.expectAnySchema( IndexedEntity1.NAME );
+		backendMock.expectAnySchema( IndexedEntity2.NAME );
+		return setupHelper.start()
+				.withProperty(
+						HibernateOrmMapperSettings.SCHEMA_MANAGEMENT_STRATEGY,
+						getStrategyName()
+				)
+				.setup( IndexedEntity1.class, IndexedEntity2.class );
+	}
+
+	protected String getStrategyName() {
+		return SchemaManagementStrategyName.NONE.getExternalRepresentation();
+	}
+
+	@Entity(name = IndexedEntity1.NAME)
+	@Indexed(index = IndexedEntity1.NAME)
+	private static class IndexedEntity1 {
+
+		private static final String NAME = "indexed1";
+
+		@Id
+		private Integer id;
+	}
+
+	@Entity(name = IndexedEntity2.NAME)
+	@Indexed(index = IndexedEntity2.NAME)
+	private static class IndexedEntity2 {
+
+		private static final String NAME = "indexed2";
+
+		@Id
+		private Integer id;
+	}
+}
