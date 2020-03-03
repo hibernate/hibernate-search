4745a9690ddf4eaf99256815395849d2575a1ffb
Yoann Rodière <yoann@hibernate.org>
2020-03-03 10:17:34 +0100

    HSEARCH-3849 Test the create-or-validate schema management strategy in the ORM mapper

---
+++ integrationtest/mapper/orm/src/test/java/org/hibernate/search/integrationtest/mapper/orm/schema/management/strategy/SchemaManagementStrategyCreateOrValidateIT.java
@@ -0,0 +1,30 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.integrationtest.mapper.orm.schema.management.strategy;
+
+import org.hibernate.search.mapper.orm.schema.management.SchemaManagementStrategyName;
+import org.hibernate.search.util.impl.integrationtest.common.rule.SchemaManagementWorkBehavior;
+import org.hibernate.search.util.impl.integrationtest.common.stub.backend.index.StubSchemaManagementWork;
+
+public class SchemaManagementStrategyCreateOrValidateIT extends AbstractSchemaManagementStrategyValidatingIT {
+
+	@Override
+	protected SchemaManagementStrategyName getStrategyName() {
+		return SchemaManagementStrategyName.CREATE_OR_VALIDATE;
+	}
+
+	@Override
+	protected void expectWork(String indexName, SchemaManagementWorkBehavior behavior) {
+		backendMock.expectSchemaManagementWorks( indexName )
+				.work( StubSchemaManagementWork.Type.CREATE_OR_VALIDATE, behavior );
+	}
+
+	@Override
+	protected void expectOnClose(String indexName) {
+		// No expectation
+	}
+}
