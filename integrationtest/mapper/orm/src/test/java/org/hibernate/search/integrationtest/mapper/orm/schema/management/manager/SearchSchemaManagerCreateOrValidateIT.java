dd599b0d121d913edf98602732d4041a558547e2
Yoann Rodière <yoann@hibernate.org>
2020-03-03 14:26:19 +0100

    HSEARCH-3849 Test the create-or-validate operation in the schema manager API of the ORM mapper

---
+++ integrationtest/mapper/orm/src/test/java/org/hibernate/search/integrationtest/mapper/orm/schema/management/manager/SearchSchemaManagerCreateOrValidateIT.java
@@ -0,0 +1,30 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.integrationtest.mapper.orm.schema.management.manager;
+
+import org.hibernate.search.mapper.orm.schema.management.SearchSchemaManager;
+import org.hibernate.search.util.impl.integrationtest.common.rule.SchemaManagementWorkBehavior;
+import org.hibernate.search.util.impl.integrationtest.common.stub.backend.index.StubSchemaManagementWork;
+
+public class SearchSchemaManagerCreateOrValidateIT extends AbstractSearchSchemaManagerValidatingSimpleOperationIT {
+
+	@Override
+	protected void execute(SearchSchemaManager manager) {
+		manager.createOrValidate();
+	}
+
+	@Override
+	protected void expectWork(String indexName, SchemaManagementWorkBehavior behavior) {
+		backendMock.expectSchemaManagementWorks( indexName )
+				.work( StubSchemaManagementWork.Type.CREATE_OR_VALIDATE, behavior );
+	}
+
+	@Override
+	protected void expectOnClose(String indexName) {
+		// No expectation
+	}
+}
