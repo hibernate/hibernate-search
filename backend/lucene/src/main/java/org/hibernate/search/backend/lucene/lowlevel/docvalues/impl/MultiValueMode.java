ca5cabd225b5295e7e774d171fe9fd51c14b81e4
Yoann Rodière <yoann@hibernate.org>
2020-03-10 17:19:00 +0100

    HSEARCH-3103 Remove dead code in MultiValueMode

--- backend/lucene/src/main/java/org/hibernate/search/backend/lucene/lowlevel/docvalues/impl/MultiValueMode.java
+++ backend/lucene/src/main/java/org/hibernate/search/backend/lucene/lowlevel/docvalues/impl/MultiValueMode.java
@@ -6,8 +6,6 @@
  */
 package org.hibernate.search.backend.lucene.lowlevel.docvalues.impl;
 
-import java.util.Locale;
-
 /**
  * Defines what values to pick in the case a document contains multiple values
  * for a particular field.
@@ -18,22 +16,5 @@ public enum MultiValueMode {
 	 * Pick the sum of all the values.
 	 */
 	SUM, AVG, MIN, MAX, MEDIAN;
-
-	/**
-	 * A case insensitive version of {@link #valueOf(String)}
-	 *
-	 * @param sortMode
-	 * @return
-	 * @throws IllegalArgumentException if the given string doesn't match a
-	 * sort mode or is <code>null</code>.
-	 */
-	public static MultiValueMode fromString(String sortMode) {
-		try {
-			return valueOf( sortMode.toUpperCase( Locale.ROOT ) );
-		}
-		catch (Exception e) {
-			throw new IllegalArgumentException( "Illegal sort mode: " + sortMode );
-		}
-	}
 
 }
