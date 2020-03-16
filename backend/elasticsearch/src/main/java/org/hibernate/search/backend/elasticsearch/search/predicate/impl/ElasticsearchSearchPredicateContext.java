be7424a021ca97a08ad4fe7d3aca3519ed1fc5e8
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2020-03-16 16:34:14 +0100

    HSEARCH-3752 Substitute recursion with loop for ES implicit nested

--- backend/elasticsearch/src/main/java/org/hibernate/search/backend/elasticsearch/search/predicate/impl/ElasticsearchSearchPredicateContext.java
+++ backend/elasticsearch/src/main/java/org/hibernate/search/backend/elasticsearch/search/predicate/impl/ElasticsearchSearchPredicateContext.java
@@ -6,25 +6,21 @@
  */
 package org.hibernate.search.backend.elasticsearch.search.predicate.impl;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
-
 import org.hibernate.search.engine.backend.session.spi.BackendSessionContext;
 
 public class ElasticsearchSearchPredicateContext {
 
 	private final BackendSessionContext sessionContext;
-	private final Set<String> explicitNestedPaths;
+	private final String nestedPath;
 
 	public ElasticsearchSearchPredicateContext(BackendSessionContext sessionContext) {
 		this.sessionContext = sessionContext;
-		this.explicitNestedPaths = Collections.emptySet();
+		this.nestedPath = null;
 	}
 
-	private ElasticsearchSearchPredicateContext(BackendSessionContext sessionContext, Set<String> explicitNestedPaths) {
+	private ElasticsearchSearchPredicateContext(BackendSessionContext sessionContext, String nestedPath) {
 		this.sessionContext = sessionContext;
-		this.explicitNestedPaths = Collections.unmodifiableSet( explicitNestedPaths );
+		this.nestedPath = nestedPath;
 	}
 
 	String getTenantId() {
@@ -32,20 +28,10 @@ public class ElasticsearchSearchPredicateContext {
 	}
 
 	public ElasticsearchSearchPredicateContext explicitNested(String path) {
-		HashSet<String> paths = new HashSet<>( explicitNestedPaths );
-		paths.add( path );
-		return new ElasticsearchSearchPredicateContext( sessionContext, paths );
+		return new ElasticsearchSearchPredicateContext( sessionContext, path );
 	}
 
-	public boolean isExplicitNested(String path) {
-		// Checking explicitNestedPaths#contins( path ) could be not enough
-		// for cases where explicit nest is used only for some nested level of the hierarchy:
-		// see NestedSearchPredicateIT#search_nestedOnTwoLevels_onlySecondLevel.
-		for ( String explicitNestedPath : explicitNestedPaths ) {
-			if ( explicitNestedPath.startsWith( path ) ) {
-				return true;
-			}
-		}
-		return false;
+	public String getNestedPath() {
+		return nestedPath;
 	}
 }
