7674f3a6f3cd2304e005e748c20071158168933d
Yoann Rodière <yoann@hibernate.org>
2020-03-16 10:17:36 +0100

    HSEARCH-3103 Revert the introduction of an unnecessary wrapper for ToParentBlockJoinQuery
    The wrapper doesn't do anything useful, at least in the current state of
    the code.

--- backend/lucene/src/main/java/org/hibernate/search/backend/lucene/search/predicate/impl/LuceneNestedPredicateBuilder.java
+++ backend/lucene/src/main/java/org/hibernate/search/backend/lucene/search/predicate/impl/LuceneNestedPredicateBuilder.java
@@ -14,7 +14,7 @@ import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.join.QueryBitSetProducer;
 import org.apache.lucene.search.join.ScoreMode;
-import org.hibernate.search.backend.lucene.lowlevel.query.impl.ESToParentBlockJoinQuery;
+import org.apache.lucene.search.join.ToParentBlockJoinQuery;
 
 class LuceneNestedPredicateBuilder extends AbstractLuceneSearchPredicateBuilder
 		implements NestedPredicateBuilder<LuceneSearchPredicateBuilder> {
@@ -52,6 +52,7 @@ class LuceneNestedPredicateBuilder extends AbstractLuceneSearchPredicateBuilder
 			parentQuery = Queries.nestedDocumentPathQuery( parentContext.getNestedPath() );
 		}
 
-		return new ESToParentBlockJoinQuery( childQueryBuilder.build(), new QueryBitSetProducer( parentQuery ), ScoreMode.Avg, absoluteFieldPath );
+		// TODO HSEARCH-3090 at some point we should have a parameter for the score mode
+		return new ToParentBlockJoinQuery( childQueryBuilder.build(), new QueryBitSetProducer( parentQuery ), ScoreMode.Avg );
 	}
 }
