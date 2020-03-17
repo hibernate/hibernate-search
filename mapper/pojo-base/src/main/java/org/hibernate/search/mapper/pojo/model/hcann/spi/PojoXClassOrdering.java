cd9e3e8e765175fd0ebfbdbf1b2644f1fdc58715
Yoann Rodière <yoann@hibernate.org>
2020-03-17 09:56:24 +0100

    HSEARCH-3848 Consider Object as a supertype of interfaces, too

--- mapper/pojo-base/src/main/java/org/hibernate/search/mapper/pojo/model/hcann/spi/PojoXClassOrdering.java
+++ mapper/pojo-base/src/main/java/org/hibernate/search/mapper/pojo/model/hcann/spi/PojoXClassOrdering.java
@@ -9,23 +9,26 @@ package org.hibernate.search.mapper.pojo.model.hcann.spi;
 import java.util.Arrays;
 import java.util.stream.Stream;
 
+import org.hibernate.annotations.common.reflection.ReflectionManager;
 import org.hibernate.annotations.common.reflection.XClass;
 import org.hibernate.search.util.common.reflect.spi.AbstractTypeOrdering;
 
 final class PojoXClassOrdering extends AbstractTypeOrdering<XClass> {
 
-	private static final PojoXClassOrdering INSTANCE = new PojoXClassOrdering();
+	private final ReflectionManager reflectionManager;
 
-	public static PojoXClassOrdering get() {
-		return INSTANCE;
-	}
-
-	private PojoXClassOrdering() {
+	PojoXClassOrdering(ReflectionManager reflectionManager) {
+		this.reflectionManager = reflectionManager;
 	}
 
 	@Override
 	protected XClass getSuperClass(XClass subType) {
-		return subType.getSuperclass();
+		XClass superClass = subType.getSuperclass();
+		if ( superClass == null && subType.isInterface() ) {
+			// Make sure Object is considered a superclass of *every* type, even interfaces.
+			superClass = reflectionManager.toXClass( Object.class );
+		}
+		return superClass;
 	}
 
 	@Override
