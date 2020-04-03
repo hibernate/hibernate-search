e24a879da59346693c95248bab4a5b551769e605
Waldemar Kłaczyński <wklaczynski@sabaservice.pl>
2020-04-03 21:01:36 +0200

    Fix Constraint Violation for impl in QueryCacheProvider and QueryCachingPolicyProvider

--- mapper/orm/src/main/java/org/hibernate/search/mapper/orm/cache/impl/HibernateOrmQueryCacheProvider.java
+++ mapper/orm/src/main/java/org/hibernate/search/mapper/orm/cache/impl/HibernateOrmQueryCacheProvider.java
@@ -9,11 +9,11 @@ package org.hibernate.search.mapper.orm.cache.impl;
 import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import org.apache.lucene.search.QueryCachingPolicy;
+import org.apache.lucene.search.QueryCache;
 import org.apache.lucene.util.Version;
 import org.hibernate.boot.registry.selector.spi.StrategyCreator;
 import org.hibernate.boot.registry.selector.spi.StrategySelector;
-import org.hibernate.search.backend.lucene.cache.spi.QueryCachingPolicyProvider;
+import org.hibernate.search.backend.lucene.cache.spi.QueryCacheProvider;
 import org.hibernate.search.engine.backend.spi.BackendBuildContext;
 import org.hibernate.search.engine.cfg.spi.ConfigurationPropertySource;
 import org.hibernate.search.mapper.orm.cfg.HibernateOrmMapperSettings;
@@ -21,11 +21,11 @@ import org.hibernate.search.mapper.orm.logging.impl.Log;
 import org.hibernate.search.util.common.logging.impl.LoggerFactory;
 import org.hibernate.service.spi.ServiceException;
 
-public class QueryCachingPolicyProviderImpl implements QueryCachingPolicyProvider {
+public class HibernateOrmQueryCacheProvider implements QueryCacheProvider {
 	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
 
 	@Override
-	public QueryCachingPolicy getQueryCachingPolicy(BackendBuildContext context, ConfigurationPropertySource properties, Version luceneVersion) {
+	public QueryCache getQueryCache(BackendBuildContext context, ConfigurationPropertySource properties, Version luceneVersion) {
 		StrategySelector selector = context.getServiceResolver().loadJmxService( StrategySelector.class );
 
 		Object setting = properties.get( HibernateOrmMapperSettings.QUERY_CACHING_POLICY_CLASS ).orElse( null );
@@ -33,17 +33,17 @@ public class QueryCachingPolicyProviderImpl implements QueryCachingPolicyProvide
 			return null;
 		}
 
-		QueryCachingPolicy queryCache = selector.resolveStrategy(
-			QueryCachingPolicy.class,
+		QueryCache queryCache = selector.resolveStrategy(
+			QueryCache.class,
 			setting,
-			(QueryCachingPolicy) null,
+			(QueryCache) null,
 			new StrategyCreatorQueryCacheImpl( properties, luceneVersion )
 		);
 
 		return queryCache;
 	}
 
-	private static class StrategyCreatorQueryCacheImpl implements StrategyCreator<QueryCachingPolicy> {
+	private static class StrategyCreatorQueryCacheImpl implements StrategyCreator<QueryCache> {
 		private final ConfigurationPropertySource properties;
 		private final Version luceneVersion;
 
@@ -53,29 +53,29 @@ public class QueryCachingPolicyProviderImpl implements QueryCachingPolicyProvide
 		}
 
 		@Override
-		public QueryCachingPolicy create(Class<? extends QueryCachingPolicy> strategyClass) {
+		public QueryCache create(Class<? extends QueryCache> strategyClass) {
 			// first look for a constructor accepting Properties
 			try {
-				final Constructor<? extends QueryCachingPolicy> ctor = strategyClass.getConstructor(
+				final Constructor<? extends QueryCache> ctor = strategyClass.getConstructor(
 					ConfigurationPropertySource.class,
 					Version.class
 				);
 				return ctor.newInstance( properties, luceneVersion );
 			}
 			catch (NoSuchMethodException e) {
-				log.debugf( "QueryCachingPolicy impl [%s] did not provide constructor accepting ConfigurationPropertySource, Version", strategyClass.getName() );
+				log.debugf( "QueryCache impl [%s] did not provide constructor accepting ConfigurationPropertySource, Version", strategyClass.getName() );
 			}
 			catch (IllegalAccessException | InstantiationException | InvocationTargetException e) {
-				throw new ServiceException( "Unable to call constructor of QueryCachingPolicy impl [" + strategyClass.getName() + "]", e );
+				throw new ServiceException( "Unable to call constructor of QueryCache impl [" + strategyClass.getName() + "]", e );
 			}
 
 			// finally try no-arg
 			try {
-				final Constructor<? extends QueryCachingPolicy> ctor = strategyClass.getConstructor();
+				final Constructor<? extends QueryCache> ctor = strategyClass.getConstructor();
 				return ctor.newInstance();
 			}
 			catch (NoSuchMethodException | IllegalArgumentException | IllegalAccessException | InstantiationException | InvocationTargetException e) {
-				throw new ServiceException( "Unable to call constructor of QueryCachingPolicy impl [" + strategyClass.getName() + "]", e );
+				throw new ServiceException( "Unable to call constructor of QueryCache impl [" + strategyClass.getName() + "]", e );
 			}
 		}
 	}
--- mapper/orm/src/main/java/org/hibernate/search/mapper/orm/cache/impl/HibernateOrmQueryCachingPolicyProvider.java
+++ mapper/orm/src/main/java/org/hibernate/search/mapper/orm/cache/impl/HibernateOrmQueryCachingPolicyProvider.java
@@ -9,11 +9,11 @@ package org.hibernate.search.mapper.orm.cache.impl;
 import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import org.apache.lucene.search.QueryCache;
+import org.apache.lucene.search.QueryCachingPolicy;
 import org.apache.lucene.util.Version;
 import org.hibernate.boot.registry.selector.spi.StrategyCreator;
 import org.hibernate.boot.registry.selector.spi.StrategySelector;
-import org.hibernate.search.backend.lucene.cache.spi.QueryCacheProvider;
+import org.hibernate.search.backend.lucene.cache.spi.QueryCachingPolicyProvider;
 import org.hibernate.search.engine.backend.spi.BackendBuildContext;
 import org.hibernate.search.engine.cfg.spi.ConfigurationPropertySource;
 import org.hibernate.search.mapper.orm.cfg.HibernateOrmMapperSettings;
@@ -21,11 +21,11 @@ import org.hibernate.search.mapper.orm.logging.impl.Log;
 import org.hibernate.search.util.common.logging.impl.LoggerFactory;
 import org.hibernate.service.spi.ServiceException;
 
-public class QueryCacheProviderImpl implements QueryCacheProvider {
+public class HibernateOrmQueryCachingPolicyProvider implements QueryCachingPolicyProvider {
 	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
 
 	@Override
-	public QueryCache getQueryCache(BackendBuildContext context, ConfigurationPropertySource properties, Version luceneVersion) {
+	public QueryCachingPolicy getQueryCachingPolicy(BackendBuildContext context, ConfigurationPropertySource properties, Version luceneVersion) {
 		StrategySelector selector = context.getServiceResolver().loadJmxService( StrategySelector.class );
 
 		Object setting = properties.get( HibernateOrmMapperSettings.QUERY_CACHING_POLICY_CLASS ).orElse( null );
@@ -33,17 +33,17 @@ public class QueryCacheProviderImpl implements QueryCacheProvider {
 			return null;
 		}
 
-		QueryCache queryCache = selector.resolveStrategy(
-			QueryCache.class,
+		QueryCachingPolicy queryCache = selector.resolveStrategy(
+			QueryCachingPolicy.class,
 			setting,
-			(QueryCache) null,
+			(QueryCachingPolicy) null,
 			new StrategyCreatorQueryCacheImpl( properties, luceneVersion )
 		);
 
 		return queryCache;
 	}
 
-	private static class StrategyCreatorQueryCacheImpl implements StrategyCreator<QueryCache> {
+	private static class StrategyCreatorQueryCacheImpl implements StrategyCreator<QueryCachingPolicy> {
 		private final ConfigurationPropertySource properties;
 		private final Version luceneVersion;
 
@@ -53,29 +53,29 @@ public class QueryCacheProviderImpl implements QueryCacheProvider {
 		}
 
 		@Override
-		public QueryCache create(Class<? extends QueryCache> strategyClass) {
+		public QueryCachingPolicy create(Class<? extends QueryCachingPolicy> strategyClass) {
 			// first look for a constructor accepting Properties
 			try {
-				final Constructor<? extends QueryCache> ctor = strategyClass.getConstructor(
+				final Constructor<? extends QueryCachingPolicy> ctor = strategyClass.getConstructor(
 					ConfigurationPropertySource.class,
 					Version.class
 				);
 				return ctor.newInstance( properties, luceneVersion );
 			}
 			catch (NoSuchMethodException e) {
-				log.debugf( "QueryCache impl [%s] did not provide constructor accepting ConfigurationPropertySource, Version", strategyClass.getName() );
+				log.debugf( "QueryCachingPolicy impl [%s] did not provide constructor accepting ConfigurationPropertySource, Version", strategyClass.getName() );
 			}
 			catch (IllegalAccessException | InstantiationException | InvocationTargetException e) {
-				throw new ServiceException( "Unable to call constructor of QueryCache impl [" + strategyClass.getName() + "]", e );
+				throw new ServiceException( "Unable to call constructor of QueryCachingPolicy impl [" + strategyClass.getName() + "]", e );
 			}
 
 			// finally try no-arg
 			try {
-				final Constructor<? extends QueryCache> ctor = strategyClass.getConstructor();
+				final Constructor<? extends QueryCachingPolicy> ctor = strategyClass.getConstructor();
 				return ctor.newInstance();
 			}
 			catch (NoSuchMethodException | IllegalArgumentException | IllegalAccessException | InstantiationException | InvocationTargetException e) {
-				throw new ServiceException( "Unable to call constructor of QueryCache impl [" + strategyClass.getName() + "]", e );
+				throw new ServiceException( "Unable to call constructor of QueryCachingPolicy impl [" + strategyClass.getName() + "]", e );
 			}
 		}
 	}
