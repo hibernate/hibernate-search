55ffe8ce5e3298d0a1fdc69096fdf97a12b9d58a
Yoann Rodière <yoann@hibernate.org>
2020-04-15 18:10:04 +0200

    HSEARCH-3881 Document aggregation filters

---
+++ documentation/src/test/java/org/hibernate/search/documentation/search/aggregation/BookEdition.java
@@ -0,0 +1,68 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.documentation.search.aggregation;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.search.engine.backend.types.Aggregable;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.FullTextField;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.GenericField;
+
+@Entity
+public class BookEdition {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	@ManyToOne
+	private Book book;
+
+	@FullTextField(analyzer = "english")
+	private String label;
+
+	@GenericField(aggregable = Aggregable.YES)
+	private Double price;
+
+	public BookEdition() {
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Book getBook() {
+		return book;
+	}
+
+	public void setBook(Book book) {
+		this.book = book;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+}
