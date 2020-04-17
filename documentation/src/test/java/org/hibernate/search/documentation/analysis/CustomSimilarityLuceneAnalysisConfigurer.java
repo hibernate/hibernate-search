c839620f1f71afd19b263c0cbb701f02aa39768e
Yoann Rodière <yoann@hibernate.org>
2020-04-17 17:21:11 +0200

    HSEARCH-3777 Document how to set a custom Similarity for Lucene

---
+++ documentation/src/test/java/org/hibernate/search/documentation/analysis/CustomSimilarityLuceneAnalysisConfigurer.java
@@ -0,0 +1,29 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.documentation.analysis;
+
+import org.hibernate.search.backend.lucene.analysis.LuceneAnalysisConfigurationContext;
+import org.hibernate.search.backend.lucene.analysis.LuceneAnalysisConfigurer;
+
+import org.apache.lucene.analysis.core.LowerCaseFilterFactory;
+import org.apache.lucene.analysis.miscellaneous.ASCIIFoldingFilterFactory;
+import org.apache.lucene.analysis.standard.StandardTokenizerFactory;
+import org.apache.lucene.search.similarities.ClassicSimilarity;
+
+// tag::include[]
+public class CustomSimilarityLuceneAnalysisConfigurer implements LuceneAnalysisConfigurer {
+	@Override
+	public void configure(LuceneAnalysisConfigurationContext context) {
+		context.similarity( new ClassicSimilarity() ); // <1>
+
+		context.analyzer( "english" ).custom() // <2>
+				.tokenizer( StandardTokenizerFactory.class )
+				.tokenFilter( LowerCaseFilterFactory.class )
+				.tokenFilter( ASCIIFoldingFilterFactory.class );
+	}
+}
+// end::include[]
