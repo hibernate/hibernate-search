de1a3c954b7d4cd83ebb2f3631b6333f4b02740c
Yoann Rodière <yoann@hibernate.org>
2020-04-23 15:16:20 +0200

    HSEARCH-3273 Add low-level representation of Elasticsearch dynamic mappings

---
+++ backend/elasticsearch/src/main/java/org/hibernate/search/backend/elasticsearch/lowlevel/index/mapping/impl/DynamicTemplateJsonAdapterFactory.java
@@ -0,0 +1,19 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.backend.elasticsearch.lowlevel.index.mapping.impl;
+
+import org.hibernate.search.backend.elasticsearch.gson.impl.AbstractConfiguredExtraPropertiesJsonAdapterFactory;
+
+class DynamicTemplateJsonAdapterFactory extends AbstractConfiguredExtraPropertiesJsonAdapterFactory {
+
+	@Override
+	protected <T> void addFields(Builder<T> builder) {
+		builder.add( "matchMappingType", String.class );
+		builder.add( "pathMatch", String.class );
+		builder.add( "mapping", PropertyMapping.class );
+	}
+}
--- backend/elasticsearch/src/main/java/org/hibernate/search/backend/elasticsearch/lowlevel/index/mapping/impl/RootTypeMappingJsonAdapterFactory.java
+++ backend/elasticsearch/src/main/java/org/hibernate/search/backend/elasticsearch/lowlevel/index/mapping/impl/RootTypeMappingJsonAdapterFactory.java
@@ -6,11 +6,19 @@
  */
 package org.hibernate.search.backend.elasticsearch.lowlevel.index.mapping.impl;
 
+import java.util.List;
+
+import com.google.gson.reflect.TypeToken;
+
 public class RootTypeMappingJsonAdapterFactory extends AbstractTypeMappingJsonAdapterFactory {
+
+	private static final TypeToken<List<NamedDynamicTemplate>> DYNAMIC_TEMPLATES_TYPE_TOKEN =
+			new TypeToken<List<NamedDynamicTemplate>>() { };
 
 	@Override
 	protected <T> void addFields(Builder<T> builder) {
 		super.addFields( builder );
 		builder.add( "routing", RoutingType.class );
+		builder.add( "dynamicTemplates", DYNAMIC_TEMPLATES_TYPE_TOKEN );
 	}
 }
