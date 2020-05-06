a1eb983c212dbec10cf63f4358fcd9f5f6d7a5a8
Yoann Rodière <yoann@hibernate.org>
2020-05-06 15:04:33 +0200

    HSEARCH-3589 Document how to retrieve information from the mapping/backends/indexManagers

---
+++ documentation/src/test/java/org/hibernate/search/documentation/backend/elasticsearch/client/ElasticsearchGetClientIT.java
@@ -0,0 +1,56 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.documentation.backend.elasticsearch.client;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import javax.persistence.EntityManagerFactory;
+
+import org.hibernate.search.backend.elasticsearch.ElasticsearchBackend;
+import org.hibernate.search.documentation.testsupport.ElasticsearchBackendConfiguration;
+import org.hibernate.search.engine.backend.Backend;
+import org.hibernate.search.mapper.orm.Search;
+import org.hibernate.search.mapper.orm.mapping.SearchMapping;
+import org.hibernate.search.util.impl.integrationtest.mapper.orm.OrmSetupHelper;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import org.elasticsearch.client.Request;
+import org.elasticsearch.client.Response;
+import org.elasticsearch.client.RestClient;
+
+public class ElasticsearchGetClientIT {
+
+	@Rule
+	public OrmSetupHelper setupHelper =
+			OrmSetupHelper.withSingleBackend( "myBackend", new ElasticsearchBackendConfiguration() );
+
+	private EntityManagerFactory entityManagerFactory;
+
+	@Before
+	public void setup() {
+		entityManagerFactory = setupHelper.start().setup( Book.class );
+	}
+
+	@Test
+	public void getClient() throws IOException {
+		//tag::getClient[]
+		SearchMapping mapping = Search.mapping( entityManagerFactory ); // <1>
+		Backend backend = mapping.getBackend( "myBackend" ); // <2>
+		ElasticsearchBackend elasticsearchBackend = backend.unwrap( ElasticsearchBackend.class ); // <3>
+		RestClient client = elasticsearchBackend.getClient( RestClient.class ); // <4>
+		//end::getClient[]
+
+		Response response = client.performRequest( new Request( "GET", "/" ) );
+		assertThat( response ).isNotNull();
+		assertThat( response.getStatusLine().getStatusCode() ).isEqualTo( 200 );
+	}
+
+}
---
+++ documentation/src/test/java/org/hibernate/search/documentation/backend/lucene/analyzer/Book.java
@@ -0,0 +1,69 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.documentation.backend.lucene.analyzer;
+
+import java.sql.Date;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.hibernate.search.engine.backend.types.Aggregable;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.FullTextField;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.GenericField;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
+
+@Entity
+@Indexed
+public class Book {
+
+	@Id
+	private Integer id;
+
+	@FullTextField(analyzer = "english")
+	private String title;
+
+	@GenericField(aggregable = Aggregable.YES)
+	private Double price;
+
+	@GenericField(aggregable = Aggregable.YES)
+	private Date releaseDate;
+
+	public Book() {
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Date getReleaseDate() {
+		return releaseDate;
+	}
+
+	public void setReleaseDate(Date releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+
+}
