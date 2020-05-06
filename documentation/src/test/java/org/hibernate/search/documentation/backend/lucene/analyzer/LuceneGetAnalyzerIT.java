8c9cb3adf6614de60c4095980e65ae952adc0af0
Yoann Rodière <yoann@hibernate.org>
2020-05-06 15:04:33 +0200

    HSEARCH-3589 Document how to retrieve information from the mapping/backends/indexManagers

---
+++ documentation/src/test/java/org/hibernate/search/documentation/backend/lucene/analyzer/LuceneGetAnalyzerIT.java
@@ -0,0 +1,68 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.documentation.backend.lucene.analyzer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+import javax.persistence.EntityManagerFactory;
+
+import org.hibernate.search.backend.lucene.LuceneBackend;
+import org.hibernate.search.backend.lucene.index.LuceneIndexManager;
+import org.hibernate.search.documentation.testsupport.LuceneBackendConfiguration;
+import org.hibernate.search.engine.backend.Backend;
+import org.hibernate.search.engine.backend.index.IndexManager;
+import org.hibernate.search.mapper.orm.Search;
+import org.hibernate.search.mapper.orm.mapping.SearchMapping;
+import org.hibernate.search.util.impl.integrationtest.mapper.orm.OrmSetupHelper;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import org.apache.lucene.analysis.Analyzer;
+
+public class LuceneGetAnalyzerIT {
+
+	@Rule
+	public OrmSetupHelper setupHelper =
+			OrmSetupHelper.withSingleBackend( "myBackend", new LuceneBackendConfiguration() );
+
+	private EntityManagerFactory entityManagerFactory;
+
+	@Before
+	public void setup() {
+		entityManagerFactory = setupHelper.start().setup( Book.class );
+	}
+
+	@Test
+	public void fromBackend() {
+		//tag::fromBackend[]
+		SearchMapping mapping = Search.mapping( entityManagerFactory ); // <1>
+		Backend backend = mapping.getBackend( "myBackend" ); // <2>
+		LuceneBackend luceneBackend = backend.unwrap( LuceneBackend.class ); // <3>
+		Optional<? extends Analyzer> analyzer = luceneBackend.analyzer( "english" ); // <4>
+		Optional<? extends Analyzer> normalizer = luceneBackend.normalizer( "isbn" ); // <5>
+		//end::fromBackend[]
+		assertThat( analyzer ).isNotNull();
+		assertThat( normalizer ).isNotNull().isNotSameAs( analyzer );
+	}
+
+	@Test
+	public void fromIndexManager() {
+		//tag::fromIndexManager[]
+		SearchMapping mapping = Search.mapping( entityManagerFactory ); // <1>
+		IndexManager indexManager = mapping.getIndexManager( "Book" ); // <2>
+		LuceneIndexManager luceneIndexManager = indexManager.unwrap( LuceneIndexManager.class ); // <3>
+		Analyzer indexingAnalyzer = luceneIndexManager.indexingAnalyzer(); // <4>
+		Analyzer searchAnalyzer = luceneIndexManager.searchAnalyzer(); // <5>
+		//end::fromIndexManager[]
+		assertThat( indexingAnalyzer ).isNotNull();
+		assertThat( searchAnalyzer ).isNotNull().isNotSameAs( indexingAnalyzer );
+	}
+
+}
---
+++ documentation/src/test/java/org/hibernate/search/documentation/mapper/orm/indexedentities/SearchMappingIndexedEntitiesIT.java
@@ -0,0 +1,139 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.documentation.mapper.orm.indexedentities;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.sql.Date;
+import java.util.Optional;
+import javax.persistence.EntityManagerFactory;
+
+import org.hibernate.search.documentation.testsupport.BackendConfigurations;
+import org.hibernate.search.engine.backend.Backend;
+import org.hibernate.search.engine.backend.index.IndexManager;
+import org.hibernate.search.engine.backend.metamodel.IndexDescriptor;
+import org.hibernate.search.engine.backend.metamodel.IndexObjectFieldDescriptor;
+import org.hibernate.search.engine.backend.metamodel.IndexObjectFieldTypeDescriptor;
+import org.hibernate.search.engine.backend.metamodel.IndexValueFieldDescriptor;
+import org.hibernate.search.engine.backend.metamodel.IndexValueFieldTypeDescriptor;
+import org.hibernate.search.mapper.orm.Search;
+import org.hibernate.search.mapper.orm.automaticindexing.session.AutomaticIndexingSynchronizationStrategyNames;
+import org.hibernate.search.mapper.orm.cfg.HibernateOrmMapperSettings;
+import org.hibernate.search.mapper.orm.mapping.SearchIndexedEntity;
+import org.hibernate.search.mapper.orm.mapping.SearchMapping;
+import org.hibernate.search.util.impl.integrationtest.common.rule.BackendConfiguration;
+import org.hibernate.search.util.impl.integrationtest.mapper.orm.OrmSetupHelper;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class SearchMappingIndexedEntitiesIT {
+
+	@Parameterized.Parameters(name = "{0}")
+	public static Object[] backendConfigurations() {
+		return BackendConfigurations.simple().toArray();
+	}
+
+	@Rule
+	public OrmSetupHelper setupHelper;
+
+	private EntityManagerFactory entityManagerFactory;
+
+	public SearchMappingIndexedEntitiesIT(BackendConfiguration backendConfiguration) {
+		this.setupHelper = OrmSetupHelper.withSingleBackend( backendConfiguration );
+	}
+
+	@Before
+	public void setup() {
+		entityManagerFactory = setupHelper.start()
+				.withProperty(
+						HibernateOrmMapperSettings.AUTOMATIC_INDEXING_SYNCHRONIZATION_STRATEGY,
+						AutomaticIndexingSynchronizationStrategyNames.SYNC
+				)
+				.setup( Book.class );
+	}
+
+	@Test
+	public void indexedEntities() {
+		//tag::indexedEntities[]
+		SearchMapping mapping = Search.mapping( entityManagerFactory ); // <1>
+		SearchIndexedEntity bookEntity = mapping.indexedEntity( Book.class ); // <2>
+		String jpaName = bookEntity.jpaName(); // <3>
+		IndexManager indexManager = bookEntity.indexManager(); // <4>
+		Backend backend = indexManager.backend(); // <5>
+		//end::indexedEntities[]
+		assertThat( jpaName ).isEqualTo( "Book" );
+		//tag::indexedEntities[]
+
+		SearchIndexedEntity bookEntity2 = mapping.indexedEntity( "Book" ); // <6>
+		Class<?> javaClass = bookEntity2.javaClass();
+		//end::indexedEntities[]
+		assertThat( javaClass ).isEqualTo( Book.class );
+		//tag::indexedEntities[]
+
+		for ( SearchIndexedEntity entity : mapping.allIndexedEntities() ) { // <7>
+			//end::indexedEntities[]
+			assertThat( entity.jpaName() ).isEqualTo( "Book" );
+			//tag::indexedEntities[]
+			// ...
+		}
+		//end::indexedEntities[]
+	}
+
+	@Test
+	public void indexMetamodel() {
+		SearchMapping mapping = Search.mapping( entityManagerFactory );
+		//tag::indexMetamodel[]
+		SearchIndexedEntity bookEntity = mapping.indexedEntity( Book.class ); // <1>
+		IndexManager indexManager = bookEntity.indexManager(); // <2>
+		IndexDescriptor indexDescriptor = indexManager.descriptor(); // <3>
+
+		indexDescriptor.field( "releaseDate" ).ifPresent( field -> { // <4>
+			String path = field.absolutePath(); // <5>
+			String relativeName = field.relativeName();
+			// Etc.
+			//end::indexMetamodel[]
+			assertThat( path ).isEqualTo( "releaseDate" );
+			assertThat( relativeName ).isEqualTo( "releaseDate" );
+			//tag::indexMetamodel[]
+
+			if ( field.isValueField() ) { // <6>
+				IndexValueFieldDescriptor valueField = field.toValueField(); // <7>
+
+				IndexValueFieldTypeDescriptor type = valueField.type(); // <8>
+				boolean projectable = type.isProjectable();
+				Class<?> dslArgumentClass = type.dslArgumentClass();
+				Class<?> projectedValueClass = type.projectedValueClass();
+				Optional<String> analyzerName = type.analyzerName();
+				Optional<String> searchAnalyzerName = type.searchAnalyzerName();
+				Optional<String> normalizerName = type.normalizerName();
+				// Etc.
+				//end::indexMetamodel[]
+				assertThat( projectable ).isFalse();
+				assertThat( dslArgumentClass ).isEqualTo( Date.class );
+				assertThat( projectedValueClass ).isEqualTo( Date.class );
+				assertThat( analyzerName ).isEmpty();
+				assertThat( searchAnalyzerName ).isEmpty();
+				assertThat( normalizerName ).isEmpty();
+				//tag::indexMetamodel[]
+			}
+			else if ( field.isObjectField() ) { // <9>
+				IndexObjectFieldDescriptor objectField = field.toObjectField();
+
+				IndexObjectFieldTypeDescriptor type = objectField.type();
+				boolean nested = type.isNested();
+				// Etc.
+			}
+		} );
+		//end::indexMetamodel[]
+	}
+
+}
