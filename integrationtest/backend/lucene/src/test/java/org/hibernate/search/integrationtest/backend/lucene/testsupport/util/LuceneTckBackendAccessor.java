hibernate/hibernate-search

commit 377705cd7e34238ac1b6a2e11cdcc4da0a09396b

HSEARCH-3902 Close indexes instead of deleting them to simulate failures on Elasticsearch indexes

integrationtest/backend/elasticsearch/src/test/java/org/hibernate/search/integrationtest/backend/elasticsearch/testsupport/util/ElasticsearchTckBackendAccessor.java
@@ -25,13 +25,7 @@ public class ElasticsearchTckBackendAccessor implements TckBackendAccessor {
 
 	@Override
 	public void ensureIndexOperationsFail(String indexName) {
-		client.index( indexName ).delete();
-		/*
-		 * Automatic index creation might get in the way and make operation succeed
-		 * even though the index doesn't exist, so we disable it.
-		 * We never rely on automatic index creation,
-		 * so leaving this setting in place afterwards is not a problem.
-		 */
-		client.clusterSettings( "action.auto_create_index" ).put( "false" );
+		// Close the index so that every operation will fail
+		client.index( indexName ).close();
 	}
 }

util/internal/integrationtest/backend/elasticsearch/src/main/java/org/hibernate/search/util/impl/integrationtest/backend/elasticsearch/rule/TestElasticsearchClient.java
@@ -125,6 +125,11 @@ public class TestElasticsearchClient implements TestRule, Closeable {
 			return this;
 		}
 
+		public IndexClient close() {
+			TestElasticsearchClient.this.closeIndex( primaryIndexName );
+			return this;
+		}
+
 		public IndexClient ensureDoesNotExist() {
 			TestElasticsearchClient.this.ensureIndexDoesNotExist( primaryIndexName );
 			return this;
@@ -392,6 +397,21 @@ public class TestElasticsearchClient implements TestRule, Closeable {
 	private void deleteIndex(URLEncodedString indexName) {
 		// We're okay with deletion failing if it's just because the index doesn't exist yet
 		tryDeleteESIndex( indexName );
+	}
+
+	private void closeIndex(URLEncodedString indexName) {
+		try {
+			performRequest( ElasticsearchRequest.post()
+					.pathComponent( indexName )
+					.pathComponent( Paths._CLOSE )
+					.build() );
+		}
+		catch (RuntimeException e) {
+			throw new AssertionFailure(
+					String.format( Locale.ROOT, "Error while trying to close index '%s' in the test client", indexName ),
+					e
+			);
+		}
 	}
 
 	private void createTemplate(String templateName, String templateString, int templateOrder, JsonObject settings) {

commit 0e7d751c57e671bf12b67944d99f204b84189feb

HSEARCH-3902 Use different methods to trigger failures for indexing and flush/merge/refresh operations
The changes introduced in 377705cd7e34238ac1b6a2e11cdcc4da0a09396b won't
work on AWS, because AWS doesn't allow closing indexes.

integrationtest/backend/elasticsearch/src/test/java/org/hibernate/search/integrationtest/backend/elasticsearch/testsupport/util/ElasticsearchTckBackendAccessor.java
@@ -9,6 +9,7 @@ package org.hibernate.search.integrationtest.backend.elasticsearch.testsupport.u
 import java.io.IOException;
 
 import org.hibernate.search.integrationtest.backend.tck.testsupport.util.TckBackendAccessor;
+import org.hibernate.search.util.impl.integrationtest.backend.elasticsearch.ElasticsearchIndexSettingsTestUtils;
 import org.hibernate.search.util.impl.integrationtest.backend.elasticsearch.rule.TestElasticsearchClient;
 
 public class ElasticsearchTckBackendAccessor implements TckBackendAccessor {
@@ -24,8 +25,19 @@ public class ElasticsearchTckBackendAccessor implements TckBackendAccessor {
 	}
 
 	@Override
-	public void ensureIndexOperationsFail(String indexName) {
-		// Close the index so that every operation will fail
-		client.index( indexName ).close();
+	public void ensureIndexingOperationsFail(String indexName) {
+		// There are many ways we could implement this method,
+		// but the one below is the only one that:
+		// 1. Will make even deletion operations fail (unlike deleting the index).
+		// 2. Will work on AWS Elasticsearch Service (unlike _close).
+
+		// Block read and write operations
+		client.index( indexName )
+				.settings().putDynamic( ElasticsearchIndexSettingsTestUtils.settingsEnableReadWrite( false ) );
+	}
+
+	@Override
+	public void ensureFlushMergeRefreshOperationsFail(String indexName) {
+		client.index( indexName ).delete();
 	}
 }

integrationtest/backend/lucene/src/test/java/org/hibernate/search/integrationtest/backend/lucene/testsupport/util/LuceneTckBackendAccessor.java
@@ -20,6 +20,8 @@ import org.hibernate.search.integrationtest.backend.tck.testsupport.util.TckBack
 import org.hibernate.search.util.common.impl.Throwables;
 import org.hibernate.search.util.impl.integrationtest.backend.lucene.LuceneTestIndexesPathConfiguration;
 
+import org.junit.AssumptionViolatedException;
+
 import org.jboss.logging.Logger;
 
 import org.apache.lucene.store.Directory;
@@ -41,7 +43,7 @@ public class LuceneTckBackendAccessor implements TckBackendAccessor {
 	}
 
 	@Override
-	public void ensureIndexOperationsFail(String indexName) {
+	public void ensureIndexingOperationsFail(String indexName) {
 		Path indexPath = indexesPath.resolve( indexName );
 		try {
 			// Try to corrupt the content of the index
@@ -58,6 +60,28 @@ public class LuceneTckBackendAccessor implements TckBackendAccessor {
 							+ " to trigger failures in tests.", e
 			);
 		}
+	}
+
+	@Override
+	public void ensureFlushMergeRefreshOperationsFail(String indexName) {
+		/*
+		 * Flush:
+		 * Lucene has optimizations in place to not apply flushes when there are no pending change in the writer.
+		 * Thus, even if we ruthlessly delete the index from the filesystem,
+		 * executing a flush will work most of the time,
+		 * because most of the time changes are already committed when the flush executes.
+		 *
+		 * Merge:
+		 * Lucene has optimizations in place to not apply mergeSegments() when there is only one segment.
+		 * Thus, even if we ruthlessly delete the index from the filesystem,
+		 * executing mergeSegments() will work most of the time,
+		 * because most of the time we will only have one segment.
+		 *
+		 * Refresh:
+		 * The refresh in the Lucene backend actually doesn't touch the index at all,
+		 * so we don't have any way to trigger failures.
+		 */
+		throw new AssumptionViolatedException( "Cannot simulate flush/merge/refresh failures for the Lucene backend" );
 	}
 
 	public Directory openDirectory(String indexName) throws IOException {

integrationtest/backend/lucene/src/test/java/org/hibernate/search/integrationtest/backend/lucene/testsupport/util/LuceneTckBackendFeatures.java
@@ -17,28 +17,6 @@ class LuceneTckBackendFeatures extends TckBackendFeatures {
 	}
 
 	@Override
-	public boolean flushWillFailIfAppliedToDeletedIndex() {
-		/*
-		 * Lucene has optimizations in place to not apply flushes when there are no pending change in the writer.
-		 * Thus, even if we ruthlessly delete the index from the filesystem,
-		 * executing a flush will work most of the time,
-		 * because most of the time changes are already committed when the flush executes.
-		 */
-		return false;
-	}
-
-	@Override
-	public boolean mergeSegmentsWillFailIfAppliedToDeletedIndex() {
-		/*
-		 * Lucene has optimizations in place to not apply mergeSegments() when there is only one segment.
-		 * Thus, even if we ruthlessly delete the index from the filesystem,
-		 * executing mergeSegments() will work most of the time,
-		 * because most of the time we will only have one segment.
-		 */
-		return false;
-	}
-
-	@Override
 	public boolean supportsDynamicChildFieldsInExistsPredicate() {
 		// See https://hibernate.atlassian.net/browse/HSEARCH-3905
 		return false;

integrationtest/backend/tck/src/main/java/org/hibernate/search/integrationtest/backend/tck/testsupport/util/TckBackendAccessor.java
@@ -10,6 +10,8 @@ import java.io.Closeable;
 
 public interface TckBackendAccessor extends Closeable {
 
-	void ensureIndexOperationsFail(String indexName);
+	void ensureIndexingOperationsFail(String indexName);
+
+	void ensureFlushMergeRefreshOperationsFail(String indexName);
 
 }

integrationtest/backend/tck/src/main/java/org/hibernate/search/integrationtest/backend/tck/testsupport/util/TckBackendFeatures.java
@@ -38,14 +38,6 @@ public class TckBackendFeatures {
 		return true;
 	}
 
-	public boolean flushWillFailIfAppliedToDeletedIndex() {
-		return true;
-	}
-
-	public boolean mergeSegmentsWillFailIfAppliedToDeletedIndex() {
-		return true;
-	}
-
 	public boolean lenientOnMultiIndexesCompatibilityChecks() {
 		// we decide to allow ALL the model incompatibilities Elasticsearch allows.
 		return false;

integrationtest/backend/tck/src/main/java/org/hibernate/search/integrationtest/backend/tck/work/AbstractIndexWorkspaceSimpleOperationIT.java
@@ -15,13 +15,13 @@ import java.util.concurrent.CompletableFuture;
 import org.hibernate.search.engine.backend.document.IndexFieldReference;
 import org.hibernate.search.engine.backend.document.model.dsl.IndexSchemaElement;
 import org.hibernate.search.engine.backend.work.execution.spi.IndexWorkspace;
+import org.hibernate.search.integrationtest.backend.tck.testsupport.util.TckBackendAccessor;
 import org.hibernate.search.integrationtest.backend.tck.testsupport.util.rule.SearchSetupHelper;
 import org.hibernate.search.util.common.logging.impl.Log;
 import org.hibernate.search.util.common.logging.impl.LoggerFactory;
 import org.hibernate.search.util.impl.integrationtest.mapper.stub.StubMappingIndexManager;
 import org.hibernate.search.util.impl.test.FutureAssert;
 
-import org.junit.Assume;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -67,14 +67,12 @@ public abstract class AbstractIndexWorkspaceSimpleOperationIT {
 
 	@Test
 	public void failure() {
-		Assume.assumeTrue( operationWillFailIfAppliedToDeletedIndex() );
-
 		setup();
 
 		IndexWorkspace workspace = indexManager.createWorkspace();
 
 		// Trigger failures in the next operations
-		setupHelper.getBackendAccessor().ensureIndexOperationsFail( INDEX_NAME );
+		ensureOperationsFail( setupHelper.getBackendAccessor(), INDEX_NAME );
 
 		CompletableFuture<?> future = executeAsync( workspace );
 		Awaitility.await().until( future::isDone );
@@ -91,7 +89,7 @@ public abstract class AbstractIndexWorkspaceSimpleOperationIT {
 		}
 	}
 
-	protected abstract boolean operationWillFailIfAppliedToDeletedIndex();
+	protected abstract void ensureOperationsFail(TckBackendAccessor accessor, String indexName);
 
 	protected void beforeInitData(StubMappingIndexManager indexManager) {
 		// Nothing to do by default.

integrationtest/backend/tck/src/main/java/org/hibernate/search/integrationtest/backend/tck/work/IndexIndexerIT.java
@@ -153,7 +153,7 @@ public class IndexIndexerIT {
 		IndexIndexer indexer = index.createIndexer();
 
 		// Trigger failures in the next operations
-		setupHelper.getBackendAccessor().ensureIndexOperationsFail( index.name() );
+		setupHelper.getBackendAccessor().ensureIndexingOperationsFail( index.name() );
 
 		CompletableFuture<?> future = indexer.add(
 				referenceProvider( "1" ),
@@ -179,7 +179,7 @@ public class IndexIndexerIT {
 		IndexIndexer indexer = index.createIndexer();
 
 		// Trigger failures in the next operations
-		setupHelper.getBackendAccessor().ensureIndexOperationsFail( index.name() );
+		setupHelper.getBackendAccessor().ensureIndexingOperationsFail( index.name() );
 
 		CompletableFuture<?> future = indexer.update(
 				referenceProvider( "1" ),
@@ -205,7 +205,7 @@ public class IndexIndexerIT {
 		IndexIndexer indexer = index.createIndexer();
 
 		// Trigger failures in the next operations
-		setupHelper.getBackendAccessor().ensureIndexOperationsFail( index.name() );
+		setupHelper.getBackendAccessor().ensureIndexingOperationsFail( index.name() );
 
 		CompletableFuture<?> future = indexer.delete(
 				referenceProvider( "1" ), commitStrategy, refreshStrategy

integrationtest/backend/tck/src/main/java/org/hibernate/search/integrationtest/backend/tck/work/IndexIndexingPlanIT.java
@@ -149,7 +149,7 @@ public class IndexIndexingPlanIT {
 		plan.add( referenceProvider( "2" ), document -> document.addValue( index.binding().title, "Title of Book 2" ) );
 
 		// Trigger failures in the next operations
-		setupHelper.getBackendAccessor().ensureIndexOperationsFail( index.name() );
+		setupHelper.getBackendAccessor().ensureIndexingOperationsFail( index.name() );
 
 		CompletableFuture<?> future = plan.execute();
 		Awaitility.await().until( future::isDone );
@@ -175,7 +175,7 @@ public class IndexIndexingPlanIT {
 		plan.update( referenceProvider( "2" ), document -> document.addValue( index.binding().title, "Title of Book 2" ) );
 
 		// Trigger failures in the next operations
-		setupHelper.getBackendAccessor().ensureIndexOperationsFail( index.name() );
+		setupHelper.getBackendAccessor().ensureIndexingOperationsFail( index.name() );
 
 		CompletableFuture<?> future = plan.execute();
 		Awaitility.await().until( future::isDone );
@@ -201,7 +201,7 @@ public class IndexIndexingPlanIT {
 		plan.delete( referenceProvider( "2" ) );
 
 		// Trigger failures in the next operations
-		setupHelper.getBackendAccessor().ensureIndexOperationsFail( index.name() );
+		setupHelper.getBackendAccessor().ensureIndexingOperationsFail( index.name() );
 
 		CompletableFuture<?> future = plan.execute();
 		Awaitility.await().until( future::isDone );
@@ -228,7 +228,7 @@ public class IndexIndexingPlanIT {
 		plan.add( referenceProvider( "2" ), document -> document.addValue( index.binding().title, "Title of Book 2" ) );
 
 		// Trigger failures in the next operations
-		setupHelper.getBackendAccessor().ensureIndexOperationsFail( index.name() );
+		setupHelper.getBackendAccessor().ensureIndexingOperationsFail( index.name() );
 
 		CompletableFuture<IndexIndexingPlanExecutionReport<StubEntityReference>> future = plan.executeAndReport();
 		Awaitility.await().until( future::isDone );

integrationtest/backend/tck/src/main/java/org/hibernate/search/integrationtest/backend/tck/work/IndexWorkspaceFlushIT.java
@@ -9,13 +9,14 @@ package org.hibernate.search.integrationtest.backend.tck.work;
 import java.util.concurrent.CompletableFuture;
 
 import org.hibernate.search.engine.backend.work.execution.spi.IndexWorkspace;
-import org.hibernate.search.integrationtest.backend.tck.testsupport.util.TckConfiguration;
+import org.hibernate.search.integrationtest.backend.tck.testsupport.util.TckBackendAccessor;
 import org.hibernate.search.util.impl.integrationtest.mapper.stub.StubMappingIndexManager;
 
 public class IndexWorkspaceFlushIT extends AbstractIndexWorkspaceSimpleOperationIT {
+
 	@Override
-	protected boolean operationWillFailIfAppliedToDeletedIndex() {
-		return TckConfiguration.get().getBackendFeatures().flushWillFailIfAppliedToDeletedIndex();
+	protected void ensureOperationsFail(TckBackendAccessor accessor, String indexName) {
+		accessor.ensureFlushMergeRefreshOperationsFail( indexName );
 	}
 
 	@Override

integrationtest/backend/tck/src/main/java/org/hibernate/search/integrationtest/backend/tck/work/IndexWorkspaceMergeSegmentsIT.java
@@ -9,13 +9,14 @@ package org.hibernate.search.integrationtest.backend.tck.work;
 import java.util.concurrent.CompletableFuture;
 
 import org.hibernate.search.engine.backend.work.execution.spi.IndexWorkspace;
-import org.hibernate.search.integrationtest.backend.tck.testsupport.util.TckConfiguration;
+import org.hibernate.search.integrationtest.backend.tck.testsupport.util.TckBackendAccessor;
 import org.hibernate.search.util.impl.integrationtest.mapper.stub.StubMappingIndexManager;
 
 public class IndexWorkspaceMergeSegmentsIT extends AbstractIndexWorkspaceSimpleOperationIT {
+
 	@Override
-	protected boolean operationWillFailIfAppliedToDeletedIndex() {
-		return TckConfiguration.get().getBackendFeatures().mergeSegmentsWillFailIfAppliedToDeletedIndex();
+	protected void ensureOperationsFail(TckBackendAccessor accessor, String indexName) {
+		accessor.ensureFlushMergeRefreshOperationsFail( indexName );
 	}
 
 	@Override

integrationtest/backend/tck/src/main/java/org/hibernate/search/integrationtest/backend/tck/work/IndexWorkspacePurgeIT.java
@@ -12,12 +12,14 @@ import java.util.Collections;
 import java.util.concurrent.CompletableFuture;
 
 import org.hibernate.search.engine.backend.work.execution.spi.IndexWorkspace;
+import org.hibernate.search.integrationtest.backend.tck.testsupport.util.TckBackendAccessor;
 import org.hibernate.search.util.impl.integrationtest.mapper.stub.StubMappingIndexManager;
 
 public class IndexWorkspacePurgeIT extends AbstractIndexWorkspaceSimpleOperationIT {
+
 	@Override
-	protected boolean operationWillFailIfAppliedToDeletedIndex() {
-		return true;
+	protected void ensureOperationsFail(TckBackendAccessor accessor, String indexName) {
+		accessor.ensureIndexingOperationsFail( indexName );
 	}
 
 	@Override

integrationtest/backend/tck/src/main/java/org/hibernate/search/integrationtest/backend/tck/work/IndexWorkspaceRefreshIT.java
@@ -11,6 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.concurrent.CompletableFuture;
 
 import org.hibernate.search.engine.backend.work.execution.spi.IndexWorkspace;
+import org.hibernate.search.integrationtest.backend.tck.testsupport.util.TckBackendAccessor;
 import org.hibernate.search.integrationtest.backend.tck.testsupport.util.rule.SearchSetupHelper;
 import org.hibernate.search.util.impl.integrationtest.mapper.stub.StubMappingIndexManager;
 
@@ -23,8 +24,8 @@ public class IndexWorkspaceRefreshIT extends AbstractIndexWorkspaceSimpleOperati
 	}
 
 	@Override
-	protected boolean operationWillFailIfAppliedToDeletedIndex() {
-		return false;
+	protected void ensureOperationsFail(TckBackendAccessor accessor, String indexName) {
+		accessor.ensureFlushMergeRefreshOperationsFail( indexName );
 	}
 
 	@Override

util/internal/integrationtest/backend/elasticsearch/src/main/java/org/hibernate/search/util/impl/integrationtest/backend/elasticsearch/ElasticsearchIndexSettingsTestUtils.java
@@ -1,0 +1,18 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.util.impl.integrationtest.backend.elasticsearch;
+
+public class ElasticsearchIndexSettingsTestUtils {
+
+	private ElasticsearchIndexSettingsTestUtils() {
+	}
+
+	public static String settingsEnableReadWrite(boolean enable) {
+		boolean block = !enable;
+		return "{'blocks.write': " + block + ", 'blocks.read': " + block + "}";
+	}
+}

util/internal/integrationtest/backend/elasticsearch/src/main/java/org/hibernate/search/util/impl/integrationtest/backend/elasticsearch/rule/TestElasticsearchClient.java
@@ -145,7 +145,7 @@ public class TestElasticsearchClient implements TestRule, Closeable {
 		}
 
 		public IndexSettingsClient settings() {
-			return settings( "" );
+			return settings( null );
 		}
 
 		public IndexSettingsClient settings(String settingsPath) {
@@ -625,12 +625,14 @@ public class TestElasticsearchClient implements TestRule, Closeable {
 	private JsonObject buildStructuredSettings(String settingsPath, String settings) {
 		JsonElement settingsJsonElement = toJsonElement( settings );
 
-		List<String> components = Arrays.asList( settingsPath.split( "\\." ) );
-		Collections.reverse( components );
-		for ( String property : components ) {
-			JsonObject parent = new JsonObject();
-			parent.add( property, settingsJsonElement );
-			settingsJsonElement = parent;
+		if ( settingsPath != null ) {
+			List<String> components = Arrays.asList( settingsPath.split( "\\." ) );
+			Collections.reverse( components );
+			for ( String property : components ) {
+				JsonObject parent = new JsonObject();
+				parent.add( property, settingsJsonElement );
+				settingsJsonElement = parent;
+			}
 		}
 
 		return settingsJsonElement.getAsJsonObject();
@@ -646,11 +648,13 @@ public class TestElasticsearchClient implements TestRule, Closeable {
 			return new JsonObject().toString();
 		}
 		JsonElement settings = index.getAsJsonObject().get( "settings" );
-		for ( String property : path.split( "\\." ) ) {
-			if ( settings == null ) {
-				break;
+		if ( path != null ) {
+			for ( String property : path.split( "\\." ) ) {
+				if ( settings == null ) {
+					break;
+				}
+				settings = settings.getAsJsonObject().get( property );
 			}
-			settings = settings.getAsJsonObject().get( property );
 		}
 		if ( settings == null ) {
 			return new JsonObject().toString();