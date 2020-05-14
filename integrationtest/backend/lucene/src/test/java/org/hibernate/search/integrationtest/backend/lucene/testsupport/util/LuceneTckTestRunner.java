ede39e4cdd8daf1205d25431f9019826e214b4b5
Yoann Rodière <yoann@hibernate.org>
2020-05-14 09:50:35 +0200

    HSEARCH-3902 Revert an accidental change to LuceneTckTestRunner
    This should never have been committed.

--- integrationtest/backend/lucene/src/test/java/org/hibernate/search/integrationtest/backend/lucene/testsupport/util/LuceneTckTestRunner.java
+++ integrationtest/backend/lucene/src/test/java/org/hibernate/search/integrationtest/backend/lucene/testsupport/util/LuceneTckTestRunner.java
@@ -17,6 +17,6 @@ import org.junit.runner.RunWith;
  * @author Gunnar Morling
  */
 @RunWith(ClasspathSuite.class)
-@ClasspathSuite.ClassnameFilters({ ".*\\.tck\\..*IndexIndexer.*IT" })
+@ClasspathSuite.ClassnameFilters({ ".*\\.tck\\..*" })
 public class LuceneTckTestRunner {
 }
