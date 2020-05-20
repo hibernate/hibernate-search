43ea154ac911d809b8f74e8c837aab3eabda1cd8
Yoann Rodière <yoann@hibernate.org>
2020-05-20 16:30:56 +0200

    HSEARCH-3925 Move a few metamodel-related methods out of HibernateOrmEntityIdEntityLoader
    Just to make the class simpler, as we're going to change it.

--- mapper/orm/src/main/java/org/hibernate/search/mapper/orm/common/impl/HibernateOrmUtils.java
+++ mapper/orm/src/main/java/org/hibernate/search/mapper/orm/common/impl/HibernateOrmUtils.java
@@ -10,8 +10,10 @@ import java.lang.invoke.MethodHandles;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
+import org.hibernate.AssertionFailure;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
 import org.hibernate.engine.spi.SessionImplementor;
+import org.hibernate.metamodel.spi.MetamodelImplementor;
 import org.hibernate.persister.entity.EntityPersister;
 import org.hibernate.search.mapper.orm.logging.impl.Log;
 import org.hibernate.search.util.common.logging.impl.LoggerFactory;
@@ -43,5 +45,43 @@ public final class HibernateOrmUtils {
 
 	public static boolean isSuperTypeOf(EntityPersister type1, EntityPersister type2) {
 		return type1.isSubclassEntityName( type2.getEntityName() );
+	}
+
+	public static EntityPersister toRootEntityType(SessionFactoryImplementor sessionFactory,
+			EntityPersister entityType) {
+		/*
+		 * We need to rely on Hibernate ORM's SPIs: this is complex stuff.
+		 * For example there may be class hierarchies such as A > B > C
+		 * where A and C are entity types and B is a mapped superclass.
+		 * So we need to exclude non-entity types, and for that we need the Hibernate ORM metamodel.
+		 */
+		MetamodelImplementor metamodel = sessionFactory.getMetamodel();
+		String rootEntityName = metamodel.entityPersister( entityType.getEntityName() ).getRootEntityName();
+		return metamodel.entityPersister( rootEntityName ).getEntityPersister();
+	}
+
+	public static EntityPersister toMostSpecificCommonEntitySuperType(MetamodelImplementor metamodel,
+			EntityPersister type1, EntityPersister type2) {
+		/*
+		 * We need to rely on Hibernate ORM's SPIs: this is complex stuff.
+		 * For example there may be class hierarchies such as A > B > C
+		 * where A and C are entity types and B is a mapped superclass.
+		 * So even if we know the two types have a common superclass,
+		 * we need to skip non-entity superclasses, and for that we need the Hibernate ORM metamodel.
+		 */
+		EntityPersister superTypeCandidate = type1;
+		while ( superTypeCandidate != null && !isSuperTypeOf( superTypeCandidate, type2 ) ) {
+			String superSuperTypeEntityName = superTypeCandidate.getEntityMetamodel().getSuperclass();
+			superTypeCandidate = superSuperTypeEntityName == null ? null
+					: metamodel.entityPersister( superSuperTypeEntityName ).getEntityPersister();
+		}
+		if ( superTypeCandidate == null ) {
+			throw new AssertionFailure(
+					"Cannot find a common entity supertype for " + type1.getEntityName()
+							+ " and " + type2.getEntityName() + "."
+							+ " There is a bug in Hibernate Search, please report it."
+			);
+		}
+		return superTypeCandidate;
 	}
 }
