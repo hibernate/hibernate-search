be1e80e5c32dacd52790ce31072de8f4587b4592
Yoann Rodière <yoann@hibernate.org>
2020-05-25 10:56:52 +0200

    HSEARCH-3925 Remove a few EntityKey and List instantiations in HibernateOrmEntityIdEntityLoader
    This is just an optimization.

--- mapper/orm/src/main/java/org/hibernate/search/mapper/orm/search/loading/impl/PersistenceContextLookupStrategy.java
+++ mapper/orm/src/main/java/org/hibernate/search/mapper/orm/search/loading/impl/PersistenceContextLookupStrategy.java
@@ -6,12 +6,9 @@
  */
 package org.hibernate.search.mapper.orm.search.loading.impl;
 
-import java.io.Serializable;
-
 import org.hibernate.engine.spi.EntityKey;
 import org.hibernate.engine.spi.PersistenceContext;
 import org.hibernate.engine.spi.SessionImplementor;
-import org.hibernate.persister.entity.EntityPersister;
 
 /**
  * A lookup strategy that checks the persistence context (first level cache).
@@ -20,36 +17,21 @@ import org.hibernate.persister.entity.EntityPersister;
  *
  * @author Emmanuel Bernard
  */
-class PersistenceContextLookupStrategy<E>
-		implements EntityLoadingCacheLookupStrategyImplementor<E> {
+class PersistenceContextLookupStrategy
+		implements EntityLoadingCacheLookupStrategyImplementor {
 
-	static PersistenceContextLookupStrategy<?> create(EntityPersister commonEntitySuperTypePersister,
-			SessionImplementor session) {
-		return new PersistenceContextLookupStrategy<>(
-				commonEntitySuperTypePersister, session
-		);
+	static PersistenceContextLookupStrategy create(SessionImplementor session) {
+		return new PersistenceContextLookupStrategy( session );
 	}
 
-	private final EntityPersister persister;
-	private final SessionImplementor session;
 	private final PersistenceContext persistenceContext;
 
-	private PersistenceContextLookupStrategy(EntityPersister persister,
-			SessionImplementor session) {
-		this.persister = persister;
-		this.session = session;
+	private PersistenceContextLookupStrategy(SessionImplementor session) {
 		this.persistenceContext = session.getPersistenceContext();
 	}
 
 	@Override
-	public E lookup(Object entityId) {
-		EntityKey entityKey = session.generateEntityKey( (Serializable) entityId, persister );
-		/*
-		 * The key was obtained from the persister for E,
-		 * so the key may only match an instance of E.
-		 */
-		@SuppressWarnings("unchecked")
-		E loadedEntityOrNull = (E) persistenceContext.getEntity( entityKey );
-		return loadedEntityOrNull;
+	public Object lookup(EntityKey entityKey) {
+		return persistenceContext.getEntity( entityKey );
 	}
 }
