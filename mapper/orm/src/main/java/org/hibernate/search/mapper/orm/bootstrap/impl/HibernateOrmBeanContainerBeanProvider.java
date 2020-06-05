9fc1cfab3266bc5dd992999d19e2fc0ab99ca873
Yoann Rodière <yoann@hibernate.org>
2020-06-05 14:03:14 +0200

    HSEARCH-3938 Eagerly initialize beans retrieved from the BeanContainer
    So that we can detect beans that cannot be found early.

--- mapper/orm/src/main/java/org/hibernate/search/mapper/orm/bootstrap/impl/HibernateOrmBeanContainerBeanProvider.java
+++ mapper/orm/src/main/java/org/hibernate/search/mapper/orm/bootstrap/impl/HibernateOrmBeanContainerBeanProvider.java
@@ -6,18 +6,25 @@
  */
 package org.hibernate.search.mapper.orm.bootstrap.impl;
 
+import java.lang.invoke.MethodHandles;
+
 import org.hibernate.resource.beans.container.spi.BeanContainer;
 import org.hibernate.resource.beans.container.spi.ContainedBean;
 import org.hibernate.resource.beans.spi.BeanInstanceProducer;
 import org.hibernate.search.engine.environment.bean.BeanHolder;
 import org.hibernate.search.engine.environment.bean.spi.BeanProvider;
 import org.hibernate.search.engine.environment.bean.spi.ReflectionBeanProvider;
+import org.hibernate.search.mapper.orm.logging.impl.Log;
 import org.hibernate.search.util.common.impl.Contracts;
+import org.hibernate.search.util.common.impl.SuppressingCloser;
+import org.hibernate.search.util.common.logging.impl.LoggerFactory;
 
 /**
  * A {@link BeanProvider} relying on a Hibernate ORM {@link BeanContainer} to resolve beans.
  */
 final class HibernateOrmBeanContainerBeanProvider implements BeanProvider {
+
+	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
 
 	private static final BeanContainer.LifecycleOptions LIFECYCLE_OPTIONS = new BeanContainer.LifecycleOptions() {
 		@Override
@@ -65,7 +72,27 @@ final class HibernateOrmBeanContainerBeanProvider implements BeanProvider {
 		ContainedBean<T> containedBean = beanContainer.getBean(
 				typeReference, LIFECYCLE_OPTIONS, fallbackInstanceProducer
 		);
-		return new HibernateOrmContainedBeanBeanHolderAdapter<>( containedBean );
+		BeanHolder<T> result = new HibernateOrmContainedBeanBeanHolderAdapter<>( containedBean );
+		// In some cases (ExtendedBeanManager in particular), the bean is retrieved lazily.
+		// This means the fallback instance producer is never called, which is a problem.
+		// Since we don't need lazy retrieval in our case (all beans are retrieved at bootstrap),
+		// we trigger initialization ourselves and use the fallback if necessary.
+		try {
+			result.get();
+		}
+		catch (Exception e) {
+			new SuppressingCloser( e ).push( result );
+			log.debugf( e, "Error resolving bean of type [%s] - using fallback", typeReference );
+			try {
+				result = BeanHolder.of( fallbackInstanceProducer.produceBeanInstance( typeReference ) );
+			}
+			catch (Exception e2) {
+				// Keep track of the original failure to retrieve the bean from the bean container.
+				e2.addSuppressed( e );
+				throw e2;
+			}
+		}
+		return result;
 	}
 
 	@Override
@@ -73,7 +100,27 @@ final class HibernateOrmBeanContainerBeanProvider implements BeanProvider {
 		ContainedBean<T> containedBean = beanContainer.getBean(
 				nameReference, typeReference, LIFECYCLE_OPTIONS, fallbackInstanceProducer
 		);
-		return new HibernateOrmContainedBeanBeanHolderAdapter<>( containedBean );
+		BeanHolder<T> result = new HibernateOrmContainedBeanBeanHolderAdapter<>( containedBean );
+		// In some cases (ExtendedBeanManager in particular), the bean is retrieved lazily.
+		// This means the fallback instance producer is never called, which is a problem.
+		// Since we don't need lazy retrieval in our case (all beans are retrieved at bootstrap),
+		// we trigger initialization ourselves and use the fallback if necessary.
+		try {
+			result.get();
+		}
+		catch (Exception e) {
+			new SuppressingCloser( e ).push( result );
+			log.debugf( e, "Error resolving bean [%s] of type [%s] - using fallback", nameReference, typeReference );
+			try {
+				result = BeanHolder.of( fallbackInstanceProducer.produceBeanInstance( nameReference, typeReference ) );
+			}
+			catch (Exception e2) {
+				// Keep track of the original failure to retrieve the bean from the bean container.
+				e2.addSuppressed( e );
+				throw e2;
+			}
+		}
+		return result;
 	}
 
 }
