cf95dfd784a7f8252da596cfc44ae0f08ee65633
Yoann Rodière <yoann@hibernate.org>
2020-06-08 19:21:05 +0200

    HSEARCH-3944 Add a Pair and Triplet classes for use in tests

---
+++ util/internal/test/src/main/java/org/hibernate/search/util/impl/test/data/Triplet.java
@@ -0,0 +1,62 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.util.impl.test.data;
+
+import java.util.Objects;
+
+public class Triplet<T0, T1, T2> {
+
+	private final T0 elem0;
+	private final T1 elem1;
+	private final T2 elem2;
+
+	public Triplet(T0 elem0, T1 elem1, T2 elem2) {
+		this.elem0 = elem0;
+		this.elem1 = elem1;
+		this.elem2 = elem2;
+	}
+
+	@Override
+	public String toString() {
+		return "Pair["
+				+ "elem0=" + elem0
+				+ ", elem1=" + elem1
+				+ ", elem2=" + elem2
+				+ "]";
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if ( this == o ) {
+			return true;
+		}
+		if ( o == null || getClass() != o.getClass() ) {
+			return false;
+		}
+		Triplet<?, ?, ?> other = (Triplet<?, ?, ?>) o;
+		return Objects.equals( elem0, other.elem0 )
+				&& Objects.equals( elem1, other.elem1 )
+				&& Objects.equals( elem2, other.elem2 );
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash( elem0, elem1, elem2 );
+	}
+
+	public T0 elem0() {
+		return elem0;
+	}
+
+	public T1 elem1() {
+		return elem1;
+	}
+
+	public T2 elem2() {
+		return elem2;
+	}
+}
