c7ce1371f5f5bede534e402adff0a11ab0d569fe
Yoann Rodière <yoann@hibernate.org>
2020-06-16 16:18:50 +0200

    HSEARCH-3929 Remove Java module tests in legacy code
    We already test this with a different test in the new codebase.

--- legacy/integrationtest/jdk9-modules/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module hibernate.search.integrationtest.jdk9.modules.client {
-	exports org.hibernate.search.test.integration.jdk9_modules.client.service;
-	requires org.hibernate.search.engine;
-	requires org.hibernate.search.orm;
-	requires java.persistence;
-	requires org.hibernate.orm.core;
-	requires lucene.analyzers.common;
-	requires lucene.core;
-}
