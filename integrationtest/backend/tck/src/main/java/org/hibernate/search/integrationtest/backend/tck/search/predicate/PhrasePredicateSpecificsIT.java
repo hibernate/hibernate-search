b0c61ea73d67b3ca192a7f8408208ec77de16e7f
Yoann Rodière <yoann@hibernate.org>
2020-06-23 17:39:01 +0200

    HSEARCH-3958 Use as much shared code as possible to test the phrase predicate

---
+++ integrationtest/backend/tck/src/main/java/org/hibernate/search/integrationtest/backend/tck/search/predicate/PhrasePredicateSpecificsIT.java
@@ -0,0 +1,158 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.integrationtest.backend.tck.search.predicate;
+
+import static org.hibernate.search.util.impl.integrationtest.common.assertion.SearchResultAssert.assertThatQuery;
+
+import java.util.function.Function;
+
+import org.hibernate.search.engine.backend.common.DocumentReference;
+import org.hibernate.search.engine.backend.document.model.dsl.IndexSchemaElement;
+import org.hibernate.search.engine.search.query.dsl.SearchQueryFinalStep;
+import org.hibernate.search.integrationtest.backend.tck.testsupport.configuration.DefaultAnalysisDefinitions;
+import org.hibernate.search.integrationtest.backend.tck.testsupport.types.AnalyzedStringFieldTypeDescriptor;
+import org.hibernate.search.integrationtest.backend.tck.testsupport.types.KeywordStringFieldTypeDescriptor;
+import org.hibernate.search.integrationtest.backend.tck.testsupport.util.SimpleFieldModel;
+import org.hibernate.search.integrationtest.backend.tck.testsupport.util.rule.SearchSetupHelper;
+import org.hibernate.search.util.impl.integrationtest.mapper.stub.BulkIndexer;
+import org.hibernate.search.util.impl.integrationtest.mapper.stub.SimpleMappedIndex;
+import org.hibernate.search.util.impl.test.annotation.PortedFromSearch5;
+
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+public class PhrasePredicateSpecificsIT {
+
+	private static final String DOCUMENT_1 = "document1";
+	private static final String DOCUMENT_2 = "document2";
+	private static final String DOCUMENT_3 = "document3";
+	private static final String DOCUMENT_4 = "document4";
+	private static final String DOCUMENT_5 = "document5";
+	private static final String EMPTY = "empty";
+
+	private static final String PHRASE_1 = "quick fox";
+	private static final String PHRASE_1_UNIQUE_TERM = "fox";
+	private static final String PHRASE_1_TEXT_EXACT_MATCH = "Once upon a time, there was a quick fox in a big house.";
+	private static final String PHRASE_1_TEXT_SLOP_1_MATCH = "Once upon a time, there was a quick brown fox in a big house.";
+	private static final String PHRASE_1_TEXT_SLOP_2_MATCH = "Once upon a time, there was a quick, sad brown fox in a big house.";
+	private static final String PHRASE_1_TEXT_SLOP_3_MATCH = "In the big house, the fox was quick.";
+	private static final String PHRASE_1_TEXT_SLOP_NO_MATCH = "Completely unrelated phrase.";
+
+	@ClassRule
+	public static final SearchSetupHelper setupHelper = new SearchSetupHelper();
+
+	private static final SimpleMappedIndex<IndexBinding> index = SimpleMappedIndex.of( IndexBinding::new );
+
+	private static final DataSet dataSet = new DataSet();
+
+	@BeforeClass
+	public static void setup() {
+		setupHelper.start().withIndexes( index ).setup();
+
+		BulkIndexer indexer = index.bulkIndexer();
+		dataSet.contribute( indexer );
+		indexer.join();
+	}
+
+	@Test
+	@PortedFromSearch5(original = "org.hibernate.search.test.dsl.DSLTest.testPhraseQuery")
+	public void phrase() {
+		String absoluteFieldPath = index.binding().analyzedStringField1.relativeFieldName;
+
+		assertThatQuery( index.query()
+				.where( f -> f.phrase().field( absoluteFieldPath ).matching( PHRASE_1 ) ) )
+				.hasDocRefHitsAnyOrder( index.typeName(), DOCUMENT_1 );
+	}
+
+	@Test
+	public void nonAnalyzedField() {
+		String absoluteFieldPath = index.binding().nonAnalyzedField.relativeFieldName;
+
+		assertThatQuery( index.query()
+				.where( f -> f.phrase().field( absoluteFieldPath ).matching( PHRASE_1 ) ) )
+				.hasNoHits();
+
+		assertThatQuery( index.query()
+				.where( f -> f.phrase().field( absoluteFieldPath ).matching( PHRASE_1_TEXT_EXACT_MATCH ) ) )
+				.hasDocRefHitsAnyOrder( index.typeName(), DOCUMENT_1 );
+	}
+
+	@Test
+	@PortedFromSearch5(original = "org.hibernate.search.test.dsl.DSLTest.testPhraseQuery")
+	public void singleTerm() {
+		String absoluteFieldPath = index.binding().analyzedStringField1.relativeFieldName;
+
+		assertThatQuery( index.query()
+				.where( f -> f.phrase().field( absoluteFieldPath ).matching( PHRASE_1_UNIQUE_TERM ) ) )
+				.hasDocRefHitsAnyOrder( index.typeName(), DOCUMENT_1, DOCUMENT_2, DOCUMENT_3, DOCUMENT_4 );
+	}
+
+	@Test
+	@PortedFromSearch5(original = "org.hibernate.search.test.dsl.DSLTest.testPhraseQuery")
+	public void slop() {
+		String absoluteFieldPath = index.binding().analyzedStringField1.relativeFieldName;
+		Function<Integer, SearchQueryFinalStep<DocumentReference>> createQuery = slop -> index.query()
+				.where( f -> f.phrase().field( absoluteFieldPath ).matching( PHRASE_1 ).slop( slop ) );
+
+		assertThatQuery( createQuery.apply( 0 ) )
+				.hasDocRefHitsAnyOrder( index.typeName(), DOCUMENT_1 );
+
+		assertThatQuery( createQuery.apply( 1 ) )
+				.hasDocRefHitsAnyOrder( index.typeName(), DOCUMENT_1, DOCUMENT_2 );
+
+		assertThatQuery( createQuery.apply( 2 ) )
+				.hasDocRefHitsAnyOrder( index.typeName(), DOCUMENT_1, DOCUMENT_2, DOCUMENT_3 );
+
+		assertThatQuery( createQuery.apply( 3 ) )
+				.hasDocRefHitsAnyOrder( index.typeName(), DOCUMENT_1, DOCUMENT_2, DOCUMENT_3, DOCUMENT_4 );
+
+		assertThatQuery( createQuery.apply( 50 ) )
+				.hasDocRefHitsAnyOrder( index.typeName(), DOCUMENT_1, DOCUMENT_2, DOCUMENT_3, DOCUMENT_4 );
+	}
+
+	private static class IndexBinding {
+		final SimpleFieldModel<String> analyzedStringField1;
+		final SimpleFieldModel<String> nonAnalyzedField;
+
+		IndexBinding(IndexSchemaElement root) {
+			analyzedStringField1 = SimpleFieldModel.mapperWithOverride( AnalyzedStringFieldTypeDescriptor.INSTANCE,
+					c -> c.asString().analyzer( DefaultAnalysisDefinitions.ANALYZER_STANDARD_ENGLISH.name ) )
+					.map( root, "analyzedString1" );
+			nonAnalyzedField = SimpleFieldModel.mapper( KeywordStringFieldTypeDescriptor.INSTANCE )
+					.map( root, "nonAnalyzedField" );
+		}
+	}
+
+	private static final class DataSet extends AbstractPredicateDataSet {
+		public DataSet() {
+			super( null );
+		}
+
+		public void contribute(BulkIndexer indexer) {
+			indexer.add( DOCUMENT_1, document -> {
+						document.addValue( index.binding().analyzedStringField1.reference, PHRASE_1_TEXT_EXACT_MATCH );
+						document.addValue( index.binding().nonAnalyzedField.reference, PHRASE_1_TEXT_EXACT_MATCH );
+					} )
+					.add( DOCUMENT_2, document -> {
+						document.addValue( index.binding().analyzedStringField1.reference, PHRASE_1_TEXT_SLOP_1_MATCH );
+						document.addValue( index.binding().nonAnalyzedField.reference, PHRASE_1_TEXT_SLOP_1_MATCH );
+					} )
+					.add( DOCUMENT_3, document -> {
+						document.addValue( index.binding().analyzedStringField1.reference, PHRASE_1_TEXT_SLOP_2_MATCH );
+						document.addValue( index.binding().nonAnalyzedField.reference, PHRASE_1_TEXT_SLOP_2_MATCH );
+					} )
+					.add( DOCUMENT_4, document -> {
+						document.addValue( index.binding().analyzedStringField1.reference, PHRASE_1_TEXT_SLOP_3_MATCH );
+					} )
+					.add( DOCUMENT_5, document -> {
+						document.addValue( index.binding().analyzedStringField1.reference, PHRASE_1_TEXT_SLOP_NO_MATCH );
+					} )
+					.add( EMPTY, document -> { } );
+		}
+	}
+}
---
+++ integrationtest/backend/tck/src/main/java/org/hibernate/search/integrationtest/backend/tck/search/predicate/PhrasePredicateTestValues.java
@@ -0,0 +1,34 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.integrationtest.backend.tck.search.predicate;
+
+import java.util.List;
+
+import org.hibernate.search.integrationtest.backend.tck.testsupport.types.FieldTypeDescriptor;
+
+public final class PhrasePredicateTestValues extends AbstractPredicateTestValues<String> {
+	private final List<String> values;
+
+	public PhrasePredicateTestValues(FieldTypeDescriptor<String> fieldType) {
+		super( fieldType );
+		this.values = fieldType.getUniquelyMatchableValues();
+	}
+
+	@Override
+	public String fieldValue(int docOrdinal) {
+		return values.get( docOrdinal );
+	}
+
+	public String matchingArg(int docOrdinal) {
+		return fieldValue( docOrdinal );
+	}
+
+	@Override
+	public int size() {
+		return values.size();
+	}
+}
