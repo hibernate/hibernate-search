360eb7828f46af9da9c53c36c0f473ea70af3bf0
Yoann Rodière <yoann@hibernate.org>
2020-07-21 17:08:39 +0200

    HSEARCH-3636 Add unit tests for *Settings static helper methods

---
+++ backend/lucene/src/test/java/org/hibernate/search/backend/lucene/cfg/LuceneIndexSettingsTest.java
@@ -0,0 +1,33 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.backend.lucene.cfg;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class LuceneIndexSettingsTest {
+
+	@Test
+	public void shardKey() {
+		assertThat( LuceneIndexSettings.shardKey( "shardId", "foo.bar" ) )
+				.isEqualTo( "hibernate.search.backend.shards.shardId.foo.bar" );
+
+		assertThat( LuceneIndexSettings.shardKey( "indexName", "shardId", "foo.bar" ) )
+				.isEqualTo( "hibernate.search.backend.indexes.indexName.shards.shardId.foo.bar" );
+		assertThat( LuceneIndexSettings.shardKey( null, "shardId", "foo.bar" ) )
+				.isEqualTo( "hibernate.search.backend.shards.shardId.foo.bar" );
+
+		assertThat( LuceneIndexSettings.shardKey( "backendName", "indexName", "shardId", "foo.bar" ) )
+				.isEqualTo( "hibernate.search.backends.backendName.indexes.indexName.shards.shardId.foo.bar" );
+		assertThat( LuceneIndexSettings.shardKey( null, "indexName", "shardId", "foo.bar" ) )
+				.isEqualTo( "hibernate.search.backend.indexes.indexName.shards.shardId.foo.bar" );
+		assertThat( LuceneIndexSettings.shardKey( "backendName", null, "shardId", "foo.bar" ) )
+				.isEqualTo( "hibernate.search.backends.backendName.shards.shardId.foo.bar" );
+	}
+
+}
