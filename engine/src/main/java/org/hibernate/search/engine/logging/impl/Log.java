0d3d827955a72a8350001e137b2903dbe0f12995
Yoann Rodière <yoann@hibernate.org>
2020-07-21 09:56:32 +0200

    HSEARCH-3967 Issue a warning when relying on "index_defaults" in configuration properties

--- engine/src/main/java/org/hibernate/search/engine/logging/impl/Log.java
+++ engine/src/main/java/org/hibernate/search/engine/logging/impl/Log.java
@@ -352,4 +352,12 @@ public interface Log extends BasicLogger {
 	SearchException invalidOutputTypeForField(@FormatWith(ClassFormatter.class) Class<?> type,
 			@FormatWith(ClassFormatter.class) Class<?> correctType,
 			@Param EventContext context);
+
+	@LogMessage(level = Logger.Level.WARN)
+	@Message(id = ID_OFFSET_2 + 85,
+			value = "Using configuration property '%1$s'. The prefix 'index_defaults' is deprecated and its support will ultimately be removed."
+					+ " Instead, you should just set defaults for index properties at the backend level."
+					+ " For example, set 'hibernate.search.backend.indexing.queue_size'"
+					+ " instead of 'hibernate.search.backend.index_defaults.indexing.queue_size'.")
+	void deprecatedIndexDefaultsPrefix(String key);
 }
