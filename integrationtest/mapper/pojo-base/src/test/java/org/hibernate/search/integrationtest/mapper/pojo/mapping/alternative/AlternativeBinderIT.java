03c899d24d014fd80c2a61daadea6601fa202d35
Yoann Rodière <yoann@hibernate.org>
2020-07-24 11:43:28 +0200

    HSEARCH-3311 Test AlternativeBinder

---
+++ integrationtest/mapper/pojo-base/src/test/java/org/hibernate/search/integrationtest/mapper/pojo/mapping/alternative/AlternativeBinderIT.java
@@ -0,0 +1,203 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.integrationtest.mapper.pojo.mapping.alternative;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.lang.invoke.MethodHandles;
+
+import org.hibernate.search.engine.backend.types.Projectable;
+import org.hibernate.search.mapper.pojo.bridge.builtin.annotation.AlternativeDiscriminator;
+import org.hibernate.search.integrationtest.mapper.pojo.testsupport.util.rule.JavaBeanMappingSetupHelper;
+import org.hibernate.search.mapper.javabean.mapping.SearchMapping;
+import org.hibernate.search.mapper.javabean.session.SearchSession;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.DocumentId;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
+import org.hibernate.search.util.common.SearchException;
+import org.hibernate.search.util.impl.integrationtest.common.FailureReportUtils;
+import org.hibernate.search.util.impl.integrationtest.common.rule.BackendMock;
+import org.hibernate.search.util.impl.test.annotation.TestForIssue;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * Test a particular use case for type bridges to provide a feature similar to
+ * {@code @AnalyzerDiscriminator} from Hibernate Search 5.
+ */
+@TestForIssue(jiraKey = "HSEARCH-3311")
+public class AlternativeBinderIT {
+
+	private static final String INDEX_NAME = "IndexName";
+
+	@Rule
+	public BackendMock backendMock = new BackendMock();
+
+	@Rule
+	public JavaBeanMappingSetupHelper setupHelper = JavaBeanMappingSetupHelper.withBackendMock( MethodHandles.lookup(), backendMock );
+
+	@Test
+	public void smoke() {
+		@Indexed(index = INDEX_NAME)
+		class IndexedEntity {
+			Integer id;
+			Language language;
+			String title;
+			String text;
+			@DocumentId
+			public Integer getId() {
+				return id;
+			}
+
+			@AlternativeDiscriminator
+			public Language getLanguage() {
+				return language;
+			}
+
+			@MultiLanguageField(projectable = Projectable.YES)
+			public String getTitle() {
+				return title;
+			}
+
+			@MultiLanguageField(name = "content")
+			public String getText() {
+				return text;
+			}
+		}
+
+		backendMock.expectSchema( INDEX_NAME, b -> b
+				.field( "title_en", String.class,
+						b2 -> b2.analyzerName( "text_en" ).projectable( Projectable.YES ) )
+				.field( "title_fr", String.class,
+						b2 -> b2.analyzerName( "text_fr" ).projectable( Projectable.YES ) )
+				.field( "title_de", String.class,
+						b2 -> b2.analyzerName( "text_de" ).projectable( Projectable.YES ) )
+				.field( "content_en", String.class,
+						b2 -> b2.analyzerName( "text_en" ).projectable( Projectable.DEFAULT ) )
+				.field( "content_fr", String.class,
+						b2 -> b2.analyzerName( "text_fr" ).projectable( Projectable.DEFAULT ) )
+				.field( "content_de", String.class,
+						b2 -> b2.analyzerName( "text_de" ).projectable( Projectable.DEFAULT ) )
+		);
+
+		SearchMapping mapping = setupHelper.start().setup( IndexedEntity.class );
+		backendMock.verifyExpectationsMet();
+
+		IndexedEntity entity1 = new IndexedEntity();
+		entity1.id = 1;
+		entity1.language = Language.ENGLISH;
+		entity1.title = "A title in English";
+		entity1.text = "Some content in English";
+		IndexedEntity entity2 = new IndexedEntity();
+		entity2.id = 2;
+		entity2.language = Language.FRENCH;
+		entity2.title = "Un titre en Français";
+		entity2.text = "Du contenu en Français";
+
+		try ( SearchSession session = mapping.createSession() ) {
+			session.indexingPlan().add( entity1 );
+			session.indexingPlan().add( entity2 );
+
+			backendMock.expectWorks( INDEX_NAME )
+					.add( "1", b -> b
+							.field( "title_en", entity1.title )
+							.field( "content_en", entity1.text ) )
+					.add( "2", b -> b
+							.field( "title_fr", entity2.title )
+							.field( "content_fr", entity2.text ) )
+					.processedThenExecuted();
+		}
+		backendMock.verifyExpectationsMet();
+	}
+
+	@Test
+	public void discriminator_missing() {
+		@Indexed(index = INDEX_NAME)
+		class IndexedEntity {
+			Integer id;
+			Language language;
+			String title;
+			String text;
+			@DocumentId
+			public Integer getId() {
+				return id;
+			}
+
+			public Language getLanguage() {
+				return language;
+			}
+
+			@MultiLanguageField(projectable = Projectable.YES)
+			public String getTitle() {
+				return title;
+			}
+
+			@MultiLanguageField(name = "content")
+			public String getText() {
+				return text;
+			}
+		}
+
+		assertThatThrownBy( () -> setupHelper.start().setup( IndexedEntity.class ) )
+				.isInstanceOf( SearchException.class )
+				.hasMessageMatching( FailureReportUtils.buildFailureReportPattern()
+						.typeContext( IndexedEntity.class.getName() )
+						.failure( "Could not find any property marked with @Alternative(id = null).",
+								"There must be exactly one such property in order to map property 'text' to multi-alternative fields." )
+						.failure( "Could not find any property marked with @Alternative(id = null).",
+								"There must be exactly one such property in order to map property 'title' to multi-alternative fields." )
+						.build()
+				);
+	}
+
+	@Test
+	public void discriminator_conflict() {
+		@Indexed(index = INDEX_NAME)
+		class IndexedEntity {
+			Integer id;
+			Language language;
+			Language otherLanguage;
+			String title;
+			String text;
+			@DocumentId
+			public Integer getId() {
+				return id;
+			}
+
+			@AlternativeDiscriminator
+			public Language getLanguage() {
+				return language;
+			}
+
+			@AlternativeDiscriminator
+			public Language getOtherLanguage() {
+				return otherLanguage;
+			}
+
+			@MultiLanguageField(projectable = Projectable.YES)
+			public String getTitle() {
+				return title;
+			}
+
+			@MultiLanguageField(name = "content")
+			public String getText() {
+				return text;
+			}
+		}
+
+		assertThatThrownBy( () -> setupHelper.start().setup( IndexedEntity.class ) )
+				.isInstanceOf( SearchException.class )
+				.hasMessageMatching( FailureReportUtils.buildFailureReportPattern()
+						.typeContext( IndexedEntity.class.getName() )
+						.failure( "Found multiple properties marked with @Alternative(id = null).",
+								"There must be exactly one such property in order to map property 'text' to multi-alternative fields." )
+						.failure( "Found multiple properties marked with @Alternative(id = null).",
+								"There must be exactly one such property in order to map property 'title' to multi-alternative fields." )
+						.build()
+				);
+	}
+}
---
+++ integrationtest/mapper/pojo-base/src/test/java/org/hibernate/search/integrationtest/mapper/pojo/mapping/alternative/MultiLanguageField.java
@@ -0,0 +1,55 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.integrationtest.mapper.pojo.mapping.alternative;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.hibernate.search.engine.backend.types.Projectable;
+import org.hibernate.search.engine.environment.bean.BeanReference;
+import org.hibernate.search.mapper.pojo.bridge.builtin.programmatic.AlternativeBinder;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.FullTextField;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.processing.PropertyMapping;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.processing.PropertyMappingAnnotationProcessor;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.processing.PropertyMappingAnnotationProcessorContext;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.processing.PropertyMappingAnnotationProcessorRef;
+import org.hibernate.search.mapper.pojo.mapping.definition.programmatic.PropertyMappingStep;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.METHOD, ElementType.FIELD })
+@Documented
+@PropertyMapping(processor = @PropertyMappingAnnotationProcessorRef(type = MultiLanguageField.Processor.class))
+public @interface MultiLanguageField {
+
+	/**
+	 * @return The field name, if any.
+	 *
+	 * @see FullTextField#name()
+	 */
+	String name() default "";
+
+	/**
+	 * @return The field name, if any.
+	 *
+	 * @see FullTextField#projectable()
+	 */
+	Projectable projectable() default Projectable.DEFAULT;
+
+	class Processor implements PropertyMappingAnnotationProcessor<MultiLanguageField> {
+		@Override
+		public void process(PropertyMappingStep mapping, MultiLanguageField annotation,
+				PropertyMappingAnnotationProcessorContext context) {
+			LanguageAlternativeBinderDelegate delegate = new LanguageAlternativeBinderDelegate(
+					annotation.name().isEmpty() ? null : annotation.name(), annotation.projectable() );
+			mapping.hostingType().binder( AlternativeBinder.create( Language.class,
+					context.annotatedElement().name(), String.class, BeanReference.ofInstance( delegate ) ) );
+		}
+	}
+}
