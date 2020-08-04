5fa1b753a6d4bd3f20f482dac3d397695d6ac472
Yoann Rodière <yoann@hibernate.org>
2020-08-04 12:24:39 +0200

    HSEARCH-3108 Fix obsolete references to getDependencies() (now dependencies())

--- mapper/pojo-base/src/main/java/org/hibernate/search/mapper/pojo/logging/impl/Log.java
+++ mapper/pojo-base/src/main/java/org/hibernate/search/mapper/pojo/logging/impl/Log.java
@@ -312,13 +312,13 @@ public interface Log extends BasicLogger {
 
 	@Message(id = ID_OFFSET_2 + 49,
 			value = "The binder did not declare any dependency to the entity model during binding."
-					+ " Declare dependencies using context.getDependencies().use(...) or,"
-					+ " if the bridge really does not depend on the entity model, context.getDependencies().useRootOnly()."
+					+ " Declare dependencies using context.dependencies().use(...) or,"
+					+ " if the bridge really does not depend on the entity model, context.dependencies().useRootOnly()."
 	)
 	SearchException missingBridgeDependencyDeclaration();
 
 	@Message(id = ID_OFFSET_2 + 50,
-			value = "The binder called context.getDependencies().useRootOnly() during binding,"
+			value = "The binder called context.dependencies().useRootOnly() during binding,"
 					+ " but also declared extra dependencies to the entity model."
 	)
 	SearchException inconsistentBridgeDependencyDeclaration();
