87df607f2e4a5c9ad67742d88ade63b5cdcfa6e9
Yoann Rodière <yoann@hibernate.org>
2020-08-20 16:28:27 +0200

    HSEARCH-3282 Remove the dependency from JSR-352 tests to hibernate-search-orm's test-jar
    It would be problematic if we move hibernate-search-orm tests to the main
    (non-legacy) codebase.

--- legacy/integrationtest/jsr352/src/test/java/org/hibernate/search/jsr352/massindexing/MassIndexingJobWithMultiTenancyIT.java
+++ legacy/integrationtest/jsr352/src/test/java/org/hibernate/search/jsr352/massindexing/MassIndexingJobWithMultiTenancyIT.java
@@ -11,10 +11,9 @@ import static org.hibernate.search.jsr352.test.util.JobTestUtil.findIndexedResul
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-
 import javax.batch.operations.JobOperator;
 import javax.batch.runtime.BatchRuntime;
 import javax.batch.runtime.BatchStatus;
@@ -22,35 +21,49 @@ import javax.batch.runtime.JobExecution;
 import javax.persistence.EntityManagerFactory;
 
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.SessionFactoryBuilder;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.dialect.Dialect;
+import org.hibernate.engine.spi.SessionFactoryImplementor;
 import org.hibernate.search.FullTextSession;
 import org.hibernate.search.Search;
+import org.hibernate.search.cfg.Environment;
 import org.hibernate.search.jsr352.massindexing.test.entity.Company;
 import org.hibernate.search.jsr352.test.util.JobTestUtil;
-import org.hibernate.search.test.SearchTestBase;
+import org.hibernate.search.jsr352.test.util.MultitenancyTestHelper;
 import org.hibernate.search.testsupport.junit.SkipOnElasticsearch;
 import org.hibernate.search.util.impl.CollectionHelper;
-import org.hibernate.testing.RequiresDialect;
+import org.hibernate.service.spi.ServiceRegistryImplementor;
+
+import org.junit.After;
+import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
+import org.apache.lucene.analysis.core.StopAnalyzer;
+import org.apache.lucene.util.Version;
+
 /**
  * @author Mincong Huang
  */
-@RequiresDialect(
-		comment = "The connection provider for this test ignores configuration and requires H2",
-		strictMatching = true,
-		value = org.hibernate.dialect.H2Dialect.class
-)
 // This test uses native APIs to load configuration, but ES-related configuration is in JPA's persistence.xml
 @Category(SkipOnElasticsearch.class)
-public class MassIndexingJobWithMultiTenancyIT extends SearchTestBase {
+public class MassIndexingJobWithMultiTenancyIT {
 
 	private static final String TARGET_TENANT_ID = "targetTenant";
 
 	private static final String UNUSED_TENANT_ID = "unusedTenant";
 
 	private static final int JOB_TIMEOUT_MS = 10_000;
+
+	private MultitenancyTestHelper multitenancyTestHelper = new MultitenancyTestHelper(
+			CollectionHelper.asSet( TARGET_TENANT_ID, UNUSED_TENANT_ID ) );
+
+	private SessionFactory sessionFactory;
 
 	private JobOperator jobOperator = BatchRuntime.getJobOperator();
 
@@ -60,13 +73,26 @@ public class MassIndexingJobWithMultiTenancyIT extends SearchTestBase {
 			new Company( "Microsoft" )
 	);
 
-	@Override
 	@Before
 	public void setUp() throws Exception {
-		super.setUp();
+		Assume.assumeTrue( "The connection provider for this test ignores configuration and requires H2",
+				Dialect.getDialect() instanceof org.hibernate.dialect.H2Dialect );
+
+		sessionFactory = buildSessionFactory();
 		persist( TARGET_TENANT_ID, companies );
 		purgeAll( TARGET_TENANT_ID, Company.class );
 	}
+
+	@After
+	public void cleanUp() {
+		try {
+			sessionFactory.close();
+		}
+		finally {
+			multitenancyTestHelper.close();
+		}
+	}
+
 
 	private <T> void persist(String tenantId, List<T> entities) {
 		try ( Session session = openSessionWithTenantId( tenantId ) ) {
@@ -97,7 +123,7 @@ public class MassIndexingJobWithMultiTenancyIT extends SearchTestBase {
 		JobTestUtil.waitForTermination( jobOperator, jobExecution, JOB_TIMEOUT_MS );
 		assertThat( jobExecution.getBatchStatus() ).isEqualTo( BatchStatus.COMPLETED );
 
-		EntityManagerFactory emf = getSessionFactory().unwrap( EntityManagerFactory.class );
+		EntityManagerFactory emf = sessionFactory.unwrap( EntityManagerFactory.class );
 
 		assertThat( findIndexedResultsInTenant( emf, Company.class, "name", "Google", TARGET_TENANT_ID ) ).hasSize( 1 );
 		assertThat( findIndexedResultsInTenant( emf, Company.class, "name", "Red Hat", TARGET_TENANT_ID ) ).hasSize( 1 );
@@ -109,22 +135,42 @@ public class MassIndexingJobWithMultiTenancyIT extends SearchTestBase {
 	}
 
 	private Session openSessionWithTenantId(String tenantId) {
-		return getSessionFactory().withOptions().tenantIdentifier( tenantId ).openSession();
+		return sessionFactory.withOptions().tenantIdentifier( tenantId ).openSession();
 	}
 
-	@Override
-	public Class<?>[] getAnnotatedClasses() {
-		return new Class<?>[] { Company.class };
+	private Map<String, Object> getConfigurationSettings() {
+		Map<String, Object> configurationSettings = new HashMap<>();
+		configurationSettings.put( "hibernate.search.lucene_version", Version.LATEST.toString() );
+		configurationSettings.put( "hibernate.search.default.directory_provider", "local-heap" );
+		configurationSettings.put( Environment.ANALYZER_CLASS, StopAnalyzer.class.getName() );
+		configurationSettings.put( "hibernate.search.default.indexwriter.merge_factor", "100" );
+		configurationSettings.put( "hibernate.search.default.indexwriter.max_buffered_docs", "1000" );
+		configurationSettings.put( org.hibernate.cfg.Environment.HBM2DDL_AUTO, "create-drop" );
+
+		configurationSettings.put( "hibernate.search.indexing_strategy", "manual" );
+
+		return configurationSettings;
 	}
 
-	@Override
-	public Set<String> multiTenantIds() {
-		return CollectionHelper.asSet( TARGET_TENANT_ID, UNUSED_TENANT_ID );
-	}
+	private SessionFactoryImplementor buildSessionFactory() {
+		Map<String, Object> settings = getConfigurationSettings();
+		multitenancyTestHelper.forceConfigurationSettings( settings );
 
-	@Override
-	public void configure(Map<String, Object> cfg) {
-		cfg.put( "hibernate.search.indexing_strategy", "manual" );
+		StandardServiceRegistryBuilder registryBuilder = new StandardServiceRegistryBuilder()
+				.applySettings( settings );
+
+		multitenancyTestHelper.enableIfNeeded( registryBuilder );
+
+		ServiceRegistryImplementor serviceRegistry = (ServiceRegistryImplementor) registryBuilder.build();
+
+		MetadataSources ms = new MetadataSources( serviceRegistry );
+		ms.addAnnotatedClass( Company.class );
+
+		Metadata metadata = ms.buildMetadata();
+		multitenancyTestHelper.exportSchema( serviceRegistry, metadata, settings );
+
+		final SessionFactoryBuilder sfb = metadata.getSessionFactoryBuilder();
+		return (SessionFactoryImplementor) sfb.build();
 	}
 
 }
