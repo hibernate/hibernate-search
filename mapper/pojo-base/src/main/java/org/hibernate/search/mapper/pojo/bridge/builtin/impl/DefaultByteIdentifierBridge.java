88726dcce91b61629ab179a6973a075a40682681
Yoann Rodière <yoann@hibernate.org>
2020-09-03 14:46:32 +0200

    HSEARCH-3993 Add default identifier bridges for Character, Byte, Boolean, Byte, Float, Double

---
+++ mapper/pojo-base/src/main/java/org/hibernate/search/mapper/pojo/bridge/builtin/impl/DefaultByteIdentifierBridge.java
@@ -0,0 +1,30 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.mapper.pojo.bridge.builtin.impl;
+
+import org.hibernate.search.mapper.pojo.bridge.IdentifierBridge;
+import org.hibernate.search.mapper.pojo.bridge.runtime.IdentifierBridgeFromDocumentIdentifierContext;
+import org.hibernate.search.mapper.pojo.bridge.runtime.IdentifierBridgeToDocumentIdentifierContext;
+
+public final class DefaultByteIdentifierBridge implements IdentifierBridge<Byte> {
+
+	@Override
+	public String toDocumentIdentifier(Byte propertyValue, IdentifierBridgeToDocumentIdentifierContext context) {
+		return propertyValue.toString();
+	}
+
+	@Override
+	public Byte fromDocumentIdentifier(String documentIdentifier, IdentifierBridgeFromDocumentIdentifierContext context) {
+		return Byte.parseByte( documentIdentifier );
+	}
+
+	@Override
+	public boolean isCompatibleWith(IdentifierBridge<?> other) {
+		return getClass().equals( other.getClass() );
+	}
+
+}
---
+++ mapper/pojo-base/src/main/java/org/hibernate/search/mapper/pojo/bridge/builtin/impl/DefaultDoubleIdentifierBridge.java
@@ -0,0 +1,30 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.mapper.pojo.bridge.builtin.impl;
+
+import org.hibernate.search.mapper.pojo.bridge.IdentifierBridge;
+import org.hibernate.search.mapper.pojo.bridge.runtime.IdentifierBridgeFromDocumentIdentifierContext;
+import org.hibernate.search.mapper.pojo.bridge.runtime.IdentifierBridgeToDocumentIdentifierContext;
+
+public final class DefaultDoubleIdentifierBridge implements IdentifierBridge<Double> {
+
+	@Override
+	public String toDocumentIdentifier(Double propertyValue, IdentifierBridgeToDocumentIdentifierContext context) {
+		return propertyValue.toString();
+	}
+
+	@Override
+	public Double fromDocumentIdentifier(String documentIdentifier, IdentifierBridgeFromDocumentIdentifierContext context) {
+		return Double.parseDouble( documentIdentifier );
+	}
+
+	@Override
+	public boolean isCompatibleWith(IdentifierBridge<?> other) {
+		return getClass().equals( other.getClass() );
+	}
+
+}
