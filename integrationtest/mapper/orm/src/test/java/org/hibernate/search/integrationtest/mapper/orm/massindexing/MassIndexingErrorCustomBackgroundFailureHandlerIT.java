cf1ec972f47c84532ed5dafc6ca11c5d4c424fcb
Yoann Rodière <yoann@hibernate.org>
2020-09-08 12:41:22 +0200

    HSEARCH-3864 Expect errors during mass indexing to be propagated as-is
    No wrapping, no calls to failure handlers, ... nothing. Just abort
    and propagate the error.

--- integrationtest/mapper/orm/src/test/java/org/hibernate/search/integrationtest/mapper/orm/massindexing/MassIndexingErrorCustomBackgroundFailureHandlerIT.java
+++ integrationtest/mapper/orm/src/test/java/org/hibernate/search/integrationtest/mapper/orm/massindexing/MassIndexingErrorCustomBackgroundFailureHandlerIT.java
@@ -44,63 +44,14 @@ public class MassIndexingErrorCustomBackgroundFailureHandlerIT extends AbstractM
 	}
 
 	@Override
-	protected void expectEntityIndexingFailureHandling(String entityName, String entityReferenceAsString,
-			String exceptionMessage, String failingOperationAsString) {
+	protected void expectNoFailureHandling() {
 		// We'll check in the assert*() method, see below.
 	}
 
 	@Override
-	protected void assertEntityIndexingFailureHandling(String entityName, String entityReferenceAsString,
-			String exceptionMessage, String failingOperationAsString) {
+	protected void assertNoFailureHandling() {
 		assertThat( staticCounters.get( StubFailureHandler.CREATE ) ).isEqualTo( 1 );
 		assertThat( staticCounters.get( StubFailureHandler.HANDLE_GENERIC_CONTEXT ) ).isEqualTo( 0 );
-		assertThat( staticCounters.get( StubFailureHandler.HANDLE_ENTITY_INDEXING_CONTEXT ) ).isEqualTo( 1 );
-	}
-
-	@Override
-	protected void expectEntityGetterFailureHandling(String exceptionMessage, String failingOperationAsString) {
-		// We'll check in the assert*() method, see below.
-	}
-
-	@Override
-	protected void assertEntityGetterFailureHandling(String exceptionMessage, String failingOperationAsString) {
-		assertThat( staticCounters.get( StubFailureHandler.CREATE ) ).isEqualTo( 1 );
-		// HANDLE_GENERIC_CONTEXT 2 instead of 0
-		assertThat( staticCounters.get( StubFailureHandler.HANDLE_GENERIC_CONTEXT ) ).isEqualTo( 2 );
-		// HANDLE_ENTITY_INDEXING_CONTEXT 0 instead of 1
 		assertThat( staticCounters.get( StubFailureHandler.HANDLE_ENTITY_INDEXING_CONTEXT ) ).isEqualTo( 0 );
-	}
-
-	@Override
-	protected void expectMassIndexerOperationFailureHandling(
-			Class<? extends Throwable> exceptionType, String exceptionMessage,
-			String failingOperationAsString) {
-		// We'll check in the assert*() method, see below.
-	}
-
-	@Override
-	protected void assertMassIndexerOperationFailureHandling(
-			Class<? extends Throwable> exceptionType, String exceptionMessage,
-			String failingOperationAsString) {
-		assertThat( staticCounters.get( StubFailureHandler.CREATE ) ).isEqualTo( 1 );
-		assertThat( staticCounters.get( StubFailureHandler.HANDLE_GENERIC_CONTEXT ) ).isEqualTo( 1 );
-	}
-
-	@Override
-	protected void expectEntityIndexingAndMassIndexerOperationFailureHandling(String entityName,
-			String entityReferenceAsString,
-			String failingEntityIndexingExceptionMessage, String failingEntityIndexingOperationAsString,
-			String failingMassIndexerOperationExceptionMessage, String failingMassIndexerOperationAsString) {
-		// We'll check in the assert*() method, see below.
-	}
-
-	@Override
-	protected void assertEntityIndexingAndMassIndexerOperationFailureHandling(String entityName,
-			String entityReferenceAsString,
-			String failingEntityIndexingExceptionMessage, String failingEntityIndexingOperationAsString,
-			String failingMassIndexerOperationExceptionMessage, String failingMassIndexerOperationAsString) {
-		assertThat( staticCounters.get( StubFailureHandler.CREATE ) ).isEqualTo( 1 );
-		assertThat( staticCounters.get( StubFailureHandler.HANDLE_GENERIC_CONTEXT ) ).isEqualTo( 1 );
-		assertThat( staticCounters.get( StubFailureHandler.HANDLE_ENTITY_INDEXING_CONTEXT ) ).isEqualTo( 1 );
 	}
 }
--- integrationtest/mapper/orm/src/test/java/org/hibernate/search/integrationtest/mapper/orm/massindexing/MassIndexingErrorDefaultBackgroundFailureHandlerIT.java
+++ integrationtest/mapper/orm/src/test/java/org/hibernate/search/integrationtest/mapper/orm/massindexing/MassIndexingErrorDefaultBackgroundFailureHandlerIT.java
@@ -7,7 +7,6 @@
 package org.hibernate.search.integrationtest.mapper.orm.massindexing;
 
 import org.hibernate.search.mapper.orm.massindexing.MassIndexingFailureHandler;
-import org.hibernate.search.util.impl.test.ExceptionMatcherBuilder;
 import org.hibernate.search.util.impl.test.rule.ExpectedLog4jLog;
 
 import org.junit.Rule;
@@ -30,95 +29,13 @@ public class MassIndexingErrorDefaultBackgroundFailureHandlerIT extends Abstract
 	}
 
 	@Override
-	protected void expectEntityIndexingFailureHandling(String entityName, String entityReferenceAsString,
-			String exceptionMessage, String failingOperationAsString) {
-		logged.expectEvent(
-				Level.ERROR,
-				ExceptionMatcherBuilder.isException( SimulatedError.class )
-						.withMessage( exceptionMessage )
-						.build(),
-				failingOperationAsString,
-				"Entities that could not be indexed correctly:",
-				entityReferenceAsString
-		)
-				.once();
+	protected void expectNoFailureHandling() {
+		logged.expectLevel( Level.ERROR ).never();
 	}
 
 	@Override
-	protected void assertEntityIndexingFailureHandling(String entityName, String entityReferenceAsString,
-			String exceptionMessage, String failingOperationAsString) {
+	protected void assertNoFailureHandling() {
 		// If we get there, everything works fine.
 	}
 
-	@Override
-	protected void expectEntityGetterFailureHandling(String exceptionMessage, String failingOperationAsString) {
-		logged.expectEvent(
-				Level.ERROR,
-				ExceptionMatcherBuilder.isException( SimulatedError.class )
-						.withMessage( exceptionMessage )
-						.build(),
-				failingOperationAsString
-		)
-				.times( 2 );
-	}
-
-	@Override
-	protected void assertEntityGetterFailureHandling(String exceptionMessage, String failingOperationAsString) {
-		// If we get there, everything works fine.
-	}
-
-	@Override
-	protected void expectMassIndexerOperationFailureHandling(
-			Class<? extends Throwable> exceptionType, String exceptionMessage,
-			String failingOperationAsString) {
-		logged.expectEvent(
-				Level.ERROR,
-				ExceptionMatcherBuilder.isException( exceptionType )
-						.withMessage( exceptionMessage )
-						.build(),
-				failingOperationAsString
-		)
-				.once();
-	}
-
-	@Override
-	protected void assertMassIndexerOperationFailureHandling(
-			Class<? extends Throwable> exceptionType, String exceptionMessage,
-			String failingOperationAsString) {
-		// If we get there, everything works fine.
-	}
-
-	@Override
-	protected void expectEntityIndexingAndMassIndexerOperationFailureHandling(String entityName,
-			String entityReferenceAsString,
-			String failingEntityIndexingExceptionMessage, String failingEntityIndexingOperationAsString,
-			String failingMassIndexerOperationExceptionMessage, String failingMassIndexerOperationAsString) {
-		logged.expectEvent(
-				Level.ERROR,
-				ExceptionMatcherBuilder.isException( SimulatedError.class )
-						.withMessage( failingEntityIndexingExceptionMessage )
-						.build(),
-				failingEntityIndexingOperationAsString,
-				"Entities that could not be indexed correctly:",
-				entityReferenceAsString
-		)
-				.once();
-
-		logged.expectEvent(
-				Level.ERROR,
-				ExceptionMatcherBuilder.isException( SimulatedError.class )
-						.withMessage( failingMassIndexerOperationExceptionMessage )
-						.build(),
-				failingMassIndexerOperationAsString
-		)
-				.once();
-	}
-
-	@Override
-	protected void assertEntityIndexingAndMassIndexerOperationFailureHandling(String entityName,
-			String entityReferenceAsString,
-			String failingEntityIndexingExceptionMessage, String failingEntityIndexingOperationAsString,
-			String failingMassIndexerOperationExceptionMessage, String failingMassIndexerOperationAsString) {
-		// If we get there, everything works fine.
-	}
 }
