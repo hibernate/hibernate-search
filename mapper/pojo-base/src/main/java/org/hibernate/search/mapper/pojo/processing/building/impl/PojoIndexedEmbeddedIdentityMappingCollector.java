2a5544741ce48cf9bd185ad1869a75a5bef00533
Yoann Rodière <yoann@hibernate.org>
2020-09-15 10:09:21 +0200

    HSEARCH-3071 Implement @IndexedEmbedded.includeEmbeddedObjectId with default value bridges

--- mapper/pojo-base/src/main/java/org/hibernate/search/mapper/pojo/processing/building/impl/PojoIndexedEmbeddedIdentityMappingCollector.java
+++ mapper/pojo-base/src/main/java/org/hibernate/search/mapper/pojo/processing/building/impl/PojoIndexedEmbeddedIdentityMappingCollector.java
@@ -6,17 +6,39 @@
  */
 package org.hibernate.search.mapper.pojo.processing.building.impl;
 
+import java.lang.invoke.MethodHandles;
+import java.util.Optional;
+
+import org.hibernate.search.engine.backend.types.Projectable;
+import org.hibernate.search.engine.backend.types.Searchable;
 import org.hibernate.search.mapper.pojo.bridge.mapping.programmatic.IdentifierBinder;
+import org.hibernate.search.mapper.pojo.extractor.mapping.programmatic.ContainerExtractorPath;
+import org.hibernate.search.mapper.pojo.logging.impl.Log;
+import org.hibernate.search.mapper.pojo.mapping.building.impl.PojoMappingHelper;
 import org.hibernate.search.mapper.pojo.model.path.impl.BoundPojoModelPathPropertyNode;
 import org.hibernate.search.mapper.pojo.model.path.impl.BoundPojoModelPathTypeNode;
+import org.hibernate.search.mapper.pojo.model.spi.PojoRawTypeModel;
+import org.hibernate.search.util.common.logging.impl.LoggerFactory;
 
-class PojoIndexedEmbeddedIdentityMappingCollector implements PojoIdentityMappingCollector {
+class PojoIndexedEmbeddedIdentityMappingCollector<E> implements PojoIdentityMappingCollector {
+	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
+
+	private final PojoRawTypeModel<E> typeModel;
+	private final PojoMappingHelper mappingHelper;
+
+	private BoundPojoModelPathPropertyNode<?, ?> identifierModelPath;
+	private IdentifierBinder identifierBinder;
+
+	PojoIndexedEmbeddedIdentityMappingCollector(PojoRawTypeModel<E> typeModel, PojoMappingHelper mappingHelper) {
+		this.typeModel = typeModel;
+		this.mappingHelper = mappingHelper;
+	}
 
 	@Override
 	public <T> void identifierBridge(BoundPojoModelPathPropertyNode<?, T> modelPath,
 			IdentifierBinder binder) {
-		// Ignored
-		// TODO bind a field if includeEmbeddedObjectId is true
+		this.identifierModelPath = modelPath;
+		this.identifierBinder = binder;
 	}
 
 	@Override
@@ -26,4 +48,26 @@ class PojoIndexedEmbeddedIdentityMappingCollector implements PojoIdentityMapping
 		// Ignored
 	}
 
+	public void contributeIdentifierField(AbstractPojoIndexingProcessorTypeNodeBuilder<?, ?> embeddedTypeNodeBuilder) {
+		if ( identifierModelPath == null ) {
+			// Fall back to the entity ID if possible
+			Optional<BoundPojoModelPathPropertyNode<E, ?>> entityIdPropertyPath = mappingHelper.indexModelBinder()
+					.createEntityIdPropertyPath( typeModel );
+			if ( entityIdPropertyPath.isPresent() ) {
+				identifierBridge( entityIdPropertyPath.get(), null );
+			}
+			else {
+				throw log.missingIdentifierMapping( typeModel );
+			}
+		}
+
+		if ( identifierBinder != null ) {
+			throw new UnsupportedOperationException( "Not implemented yet" );
+		}
+		embeddedTypeNodeBuilder.property( identifierModelPath.getPropertyModel().name() )
+				.value( ContainerExtractorPath.defaultExtractors() )
+				.valueBinder( null, null,
+						context -> context.standardTypeOptionsStep().searchable( Searchable.YES )
+								.projectable( Projectable.YES ) );
+	}
 }
