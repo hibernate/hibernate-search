a3edf8ce3816fafb3879b3e9fa056168dc13ca07
Yoann Rodière <yoann@hibernate.org>
2020-09-17 10:56:01 +0200

    HSEARCH-3986 Introduce utils to track open Lucene resources (directory, input, output)

---
+++ util/internal/integrationtest/backend/lucene/src/main/java/org/hibernate/search/util/impl/integrationtest/backend/lucene/directory/TrackingChecksumIndexInput.java
@@ -0,0 +1,145 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.util.impl.integrationtest.backend.lucene.directory;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.lucene.store.ChecksumIndexInput;
+import org.apache.lucene.store.IndexInput;
+import org.apache.lucene.store.RandomAccessInput;
+
+class TrackingChecksumIndexInput extends ChecksumIndexInput {
+
+	private final ChecksumIndexInput delegate;
+	private final OpenResourceTracker tracker;
+
+	public TrackingChecksumIndexInput(ChecksumIndexInput delegate, OpenResourceTracker tracker) {
+		super( "TrackingChecksumIndexInput(" + delegate.toString() + ")" );
+		this.delegate = delegate;
+		this.tracker = tracker;
+		tracker.onOpen( this );
+	}
+
+	@Override
+	public final void close() throws IOException {
+		delegate.close();
+		tracker.onClose( this );
+	}
+
+	@Override
+	public IndexInput clone() {
+		// Lucene never closes clones, so we don't track those.
+		return delegate.clone();
+	}
+
+	@Override
+	public long getChecksum() throws IOException {
+		return delegate.getChecksum();
+	}
+
+	@Override
+	public void seek(long pos) throws IOException {
+		delegate.seek( pos );
+	}
+
+	@Override
+	public long getFilePointer() {
+		return delegate.getFilePointer();
+	}
+
+	@Override
+	public long length() {
+		return delegate.length();
+	}
+
+	@Override
+	public IndexInput slice(String sliceDescription, long offset, long length) throws IOException {
+		return delegate.slice( sliceDescription, offset, length );
+	}
+
+	@Override
+	public RandomAccessInput randomAccessSlice(long offset, long length) throws IOException {
+		return delegate.randomAccessSlice( offset, length );
+	}
+
+	@Override
+	public byte readByte() throws IOException {
+		return delegate.readByte();
+	}
+
+	@Override
+	public void readBytes(byte[] b, int offset, int len) throws IOException {
+		delegate.readBytes( b, offset, len );
+	}
+
+	@Override
+	public void readBytes(byte[] b, int offset, int len, boolean useBuffer) throws IOException {
+		delegate.readBytes( b, offset, len, useBuffer );
+	}
+
+	@Override
+	public short readShort() throws IOException {
+		return delegate.readShort();
+	}
+
+	@Override
+	public int readInt() throws IOException {
+		return delegate.readInt();
+	}
+
+	@Override
+	public int readVInt() throws IOException {
+		return delegate.readVInt();
+	}
+
+	@Override
+	public int readZInt() throws IOException {
+		return delegate.readZInt();
+	}
+
+	@Override
+	public long readLong() throws IOException {
+		return delegate.readLong();
+	}
+
+	@Override
+	public void readLELongs(long[] dst, int offset, int length) throws IOException {
+		delegate.readLELongs( dst, offset, length );
+	}
+
+	@Override
+	public long readVLong() throws IOException {
+		return delegate.readVLong();
+	}
+
+	@Override
+	public long readZLong() throws IOException {
+		return delegate.readZLong();
+	}
+
+	@Override
+	public String readString() throws IOException {
+		return delegate.readString();
+	}
+
+	@Override
+	public Map<String, String> readMapOfStrings() throws IOException {
+		return delegate.readMapOfStrings();
+	}
+
+	@Override
+	public Set<String> readSetOfStrings() throws IOException {
+		return delegate.readSetOfStrings();
+	}
+
+	@Override
+	public void skipBytes(long numBytes) throws IOException {
+		delegate.skipBytes( numBytes );
+	}
+}
---
+++ util/internal/integrationtest/backend/lucene/src/main/java/org/hibernate/search/util/impl/integrationtest/backend/lucene/directory/TrackingDirectory.java
@@ -0,0 +1,104 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.util.impl.integrationtest.backend.lucene.directory;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Set;
+
+import org.apache.lucene.store.BaseDirectory;
+import org.apache.lucene.store.ChecksumIndexInput;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.IOContext;
+import org.apache.lucene.store.IndexInput;
+import org.apache.lucene.store.IndexOutput;
+import org.apache.lucene.store.LockFactory;
+
+class TrackingDirectory extends BaseDirectory {
+
+	private final Directory delegate;
+	private final OpenResourceTracker tracker;
+
+	public TrackingDirectory(Directory delegate, LockFactory lockFactory, OpenResourceTracker tracker) {
+		super( lockFactory );
+		this.delegate = delegate;
+		this.tracker = tracker;
+		tracker.onOpen( this );
+	}
+
+	@Override
+	public String[] listAll() throws IOException {
+		return delegate.listAll();
+	}
+
+	@Override
+	public void deleteFile(String name) throws IOException {
+		delegate.deleteFile( name );
+	}
+
+	@Override
+	public long fileLength(String name) throws IOException {
+		return delegate.fileLength( name );
+	}
+
+	@Override
+	public IndexOutput createOutput(String name, IOContext context) throws IOException {
+		return new TrackingIndexOutput( delegate.createOutput( name, context ), tracker );
+	}
+
+	@Override
+	public IndexOutput createTempOutput(String prefix, String suffix, IOContext context) throws IOException {
+		return new TrackingIndexOutput( delegate.createTempOutput( prefix, suffix, context ), tracker );
+	}
+
+	@Override
+	public void sync(Collection<String> names) throws IOException {
+		delegate.sync( names );
+	}
+
+	@Override
+	public void syncMetaData() throws IOException {
+		delegate.syncMetaData();
+	}
+
+	@Override
+	public void rename(String source, String dest) throws IOException {
+		delegate.rename( source, dest );
+	}
+
+	@Override
+	public IndexInput openInput(String name, IOContext context) throws IOException {
+		return new TrackingIndexInput( delegate.openInput( name, context ), tracker );
+	}
+
+	@Override
+	public ChecksumIndexInput openChecksumInput(String name, IOContext context) throws IOException {
+		return new TrackingChecksumIndexInput( delegate.openChecksumInput( name, context ), tracker );
+	}
+
+	@Override
+	public void close() throws IOException {
+		delegate.close();
+		tracker.onClose( this );
+	}
+
+	@Override
+	public void copyFrom(Directory from, String src, String dest, IOContext context) throws IOException {
+		delegate.copyFrom( from, src, dest, context );
+	}
+
+	@Override
+	public String toString() {
+		return delegate.toString();
+	}
+
+	@Override
+	public Set<String> getPendingDeletions() throws IOException {
+		return delegate.getPendingDeletions();
+	}
+
+}
