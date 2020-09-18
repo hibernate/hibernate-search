1c0695869d3a13de194b39db57dd63a1b17ddb26
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2020-09-18 09:20:51 +0200

    HSEARCH-3269 Rearrange packages and logs

---
+++ batch-jsr352/core/src/main/java/org/hibernate/search/batch/jsr352/core/massindexing/util/impl/CompositeIdOrder.java
@@ -0,0 +1,159 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.batch.jsr352.core.massindexing.util.impl;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.function.BiFunction;
+import javax.persistence.EmbeddedId;
+import javax.persistence.IdClass;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Order;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.hibernate.type.ComponentType;
+
+/**
+ * Order over multiple ID attributes.
+ * <p>
+ * This class should be used when target entity has multiple ID attributes,
+ * e.g. an entity annotated {@link IdClass}, or an entity having an
+ * {@link EmbeddedId}.
+ * <p>
+ * By default, {@code Restrictions.ge} and similar on
+ * a composite property will generate a non-lexicographical "greater or equal" condition.
+ * <p>
+ * For instance, a condition such as
+ * {@code Restrictions.ge("myEmbeddedDate", MyEmbeddedDate.yearMonthDay( 2017, 6, 20 )}
+ * will generate {@code year >= 2017 AND month >= 6 AND day >= 20}
+ * which is obviously not what any sane person would expect,
+ * since this will exclude July 1st to July 19th (among others).
+ * <p>
+ * This class fixes the issue by properly implementing lexicographical
+ * "greater or equal" and "lesser" conditions.
+ *
+ * @see <a href="https://hibernate.atlassian.net/browse/HSEARCH-2615">HSEARCH-2615</a>
+ *
+ * @author Mincong Huang
+ * @author Yoann Rodiere
+ */
+public class CompositeIdOrder implements IdOrder {
+
+	private final ComponentType componentType;
+
+	private final List<String> propertyPaths;
+
+	private final List<Integer> propertyIndices;
+
+	public CompositeIdOrder(String componentPath, ComponentType componentType) {
+		super();
+		this.componentType = componentType;
+
+		// Initialize with relative paths, but prepend a prefix below
+		this.propertyPaths = new ArrayList<>( Arrays.asList( componentType.getPropertyNames() ) );
+		this.propertyPaths.sort( Comparator.naturalOrder() );
+
+		String pathPrefix = componentPath == null ? "" : componentPath + ".";
+		this.propertyIndices = new ArrayList<>( propertyPaths.size() );
+		ListIterator<String> iterator = this.propertyPaths.listIterator();
+		while ( iterator.hasNext() ) {
+			String propertyName = iterator.next();
+
+			// We need the relative path of the property here
+			propertyIndices.add( componentType.getPropertyIndex( propertyName ) );
+
+			// Prepend the path prefix to each property; we will only use absolute path from now on
+			iterator.set( pathPrefix + propertyName );
+		}
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public Predicate idGreater(CriteriaBuilder builder, Root<?> root, Object idObj) {
+		BiFunction<String, Object, Predicate> strictOperator = (String path, Object obj) ->
+				builder.greaterThan( root.get( path ), (Comparable<? super Object>) idObj );
+		return restrictLexicographically( strictOperator, builder, root, idObj, false );
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public Predicate idGreaterOrEqual(CriteriaBuilder builder, Root<?> root, Object idObj) {
+		/*
+		 * Caution, using Restrictions::ge here won't cut it, we really need
+		 * to separate the strict operator from the equals.
+		 */
+		BiFunction<String, Object, Predicate> strictOperator = (String path, Object obj) ->
+				builder.greaterThan( root.get( path ), (Comparable<? super Object>) idObj );
+		return restrictLexicographically( strictOperator, builder, root, idObj, true );
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public Predicate idLesser(CriteriaBuilder builder, Root<?> root, Object idObj) {
+		BiFunction<String, Object, Predicate> strictOperator = (String path, Object obj) ->
+				builder.lessThan( root.get( path ), (Comparable<? super Object>) idObj );
+		return restrictLexicographically( strictOperator, builder, root, idObj, false );
+	}
+
+	@Override
+	public void addAscOrder(CriteriaBuilder builder, CriteriaQuery<?> criteria, Root<?> root) {
+		ArrayList<Order> orders = new ArrayList<>( propertyPaths.size() );
+		for ( String path : propertyPaths ) {
+			orders.add( builder.asc( root.get( path ) ) );
+		}
+		criteria.orderBy( orders );
+	}
+
+	private Predicate restrictLexicographically(BiFunction<String, Object, Predicate> strictOperator,
+			CriteriaBuilder builder, Root<?> root, Object idObj, boolean orEquals) {
+		int propertyPathsSize = propertyPaths.size();
+		int expressionsInOr = propertyPathsSize + ( orEquals ? 1 : 0 );
+
+		Predicate[] or = new Predicate[expressionsInOr];
+
+		for ( int i = 0; i < propertyPathsSize; i++ ) {
+			// Group expressions together in a single conjunction (A and B and C...).
+			Predicate[] and = new Predicate[i + 1];
+			int j = 0;
+			for ( ; j < and.length - 1; j++ ) {
+				// The first N-1 expressions have symbol `=`
+				String path = propertyPaths.get( j );
+				Object val = getPropertyValue( idObj, j );
+				and[j] = builder.equal( root.get( path ), val );
+			}
+			// The last expression has whatever symbol is defined by "strictOperator"
+			String path = propertyPaths.get( j );
+			Object val = getPropertyValue( idObj, j );
+			and[j] = strictOperator.apply( path, val );
+
+			or[i] = builder.and( and );
+		}
+
+		if ( orEquals ) {
+			Predicate[] and = new Predicate[propertyPathsSize];
+			for ( int i = 0; i < propertyPathsSize; i++ ) {
+				String path = propertyPaths.get( i );
+				Object val = getPropertyValue( idObj, i );
+				and[i] = builder.equal( root.get( path ), val );
+			}
+			or[or.length - 1] = builder.and( and );
+		}
+
+		// Group the disjunction of multiple expressions (X or Y or Z...).
+		return builder.or( or );
+	}
+
+	private Object getPropertyValue(Object obj, int ourIndex) {
+		int theirIndex = propertyIndices.get( ourIndex );
+		return componentType.getPropertyValue( obj, theirIndex );
+	}
+}
---
+++ batch-jsr352/jberet/src/main/java/org/hibernate/search/batch/jsr352/jberet/logging/impl/Log.java
@@ -0,0 +1,62 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.batch.jsr352.jberet.logging.impl;
+
+import org.hibernate.search.util.common.SearchException;
+import org.hibernate.search.util.common.logging.impl.MessageConstants;
+
+import org.jboss.logging.BasicLogger;
+import org.jboss.logging.annotations.Message;
+import org.jboss.logging.annotations.MessageLogger;
+import org.jboss.logging.annotations.ValidIdRange;
+import org.jboss.logging.annotations.ValidIdRanges;
+
+@MessageLogger(projectCode = MessageConstants.PROJECT_CODE)
+@ValidIdRanges({
+		// Search 5 :
+		@ValidIdRange(min = MessageConstants.BATCH_JSR352_CORE_ID_RANGE_MIN, max = MessageConstants.BATCH_JSR352_CORE_ID_RANGE_MIN + 5),
+		// Search 6 :
+		@ValidIdRange(min = MessageConstants.BATCH_JSR352_JBERET_ID_RANGE_MIN, max = MessageConstants.BATCH_JSR352_JBERET_ID_RANGE_MAX)
+})
+public interface Log extends BasicLogger {
+
+	// -----------------------------------
+	// Pre-existing messages from Search 5 (JSR-352 Core module)
+	// DO NOT ADD ANY NEW MESSAGES HERE
+	// -----------------------------------
+	int ID_OFFSET_1 = MessageConstants.BATCH_JSR352_CORE_ID_RANGE_MIN;
+
+	@Message(id = ID_OFFSET_1 + 2,
+			value = "No entity manager factory available in the CDI context with this bean name: '%1$s'."
+					+ " Make sure your entity manager factory is a named bean."
+	)
+	SearchException noAvailableEntityManagerFactoryInCDI(String reference);
+
+	@Message(id = ID_OFFSET_1 + 3,
+			value = "Unknown entity manager factory namespace: '%1$s'. Please use a supported namespace.")
+	SearchException unknownEntityManagerFactoryNamespace(String namespace);
+
+	@Message(id = ID_OFFSET_1 + 4,
+			value = "Exception while retrieving the EntityManagerFactory using @PersistenceUnit."
+					+ " This generally happens either because the persistence wasn't configured properly"
+					+ " or because there are multiple persistence units."
+	)
+	SearchException cannotRetrieveEntityManagerFactoryInJsr352();
+
+	@Message(id = ID_OFFSET_1 + 5,
+			value = "Multiple entity manager factories have been registered in the CDI context."
+					+ " Please provide the bean name for the selected entity manager factory to the batch indexing job through"
+					+ " the 'entityManagerFactoryReference' parameter."
+	)
+	SearchException ambiguousEntityManagerFactoryInJsr352();
+
+	// -----------------------------------
+	// New messages from Search 6 onwards
+	// -----------------------------------
+	int ID_OFFSET_3 = MessageConstants.BATCH_JSR352_JBERET_ID_RANGE_MIN;
+
+}
