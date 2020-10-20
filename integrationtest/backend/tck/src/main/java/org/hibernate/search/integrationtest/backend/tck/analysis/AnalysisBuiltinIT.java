04bebe4bb67e11601d568e9c84188e269cdce465
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2020-10-20 13:56:48 +0200

    HSEARCH-2584 Test built-in analyzers with backends

--- integrationtest/backend/tck/src/main/java/org/hibernate/search/integrationtest/backend/tck/analysis/AnalysisBuiltinIT.java
+++ integrationtest/backend/tck/src/main/java/org/hibernate/search/integrationtest/backend/tck/analysis/AnalysisBuiltinIT.java
@@ -10,7 +10,6 @@ import static org.hibernate.search.util.impl.integrationtest.common.assertion.Se
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Consumer;
 
 import org.hibernate.search.engine.backend.analysis.AnalyzerNames;
 import org.hibernate.search.engine.backend.common.DocumentReference;
@@ -65,17 +64,7 @@ public class AnalysisBuiltinIT {
 	@Test
 	public void analyzer_default() {
 		SimpleFieldModel<String> field = index.binding().defaultAnalyzer;
-
-		initData( field, b -> {
-			b.emptyDocument( "empty" );
-			b.document( "1", "twowords" );
-			b.document( "2", "two.words" );
-			b.document( "3", "two-words" );
-			b.document( "4", "two words" );
-			b.document( "5", "TWO WORDS" );
-			b.document( "6", "two wôrds" );
-			b.document( "7", "a stopword the stopword" );
-		} );
+		initData( field );
 
 		// Tokenize on space, hyphen
 		assertMatchQuery( field, "words" )
@@ -94,6 +83,110 @@ public class AnalysisBuiltinIT {
 				.hasDocRefHitsAnyOrder( index.typeName(), "7" );
 	}
 
+	@Test
+	public void analyzer_standard() {
+		SimpleFieldModel<String> field = index.binding().standardAnalyzer;
+		initData( field );
+
+		// Tokenize on space, hyphen
+		assertMatchQuery( field, "words" )
+				.hasDocRefHitsAnyOrder( index.typeName(), "3", "4", "5" );
+
+		// Case-insensitive
+		assertMatchQuery( field, "WORDS" )
+				.hasDocRefHitsAnyOrder( index.typeName(), "3", "4", "5" );
+		assertMatchQuery( field, "Words" )
+				.hasDocRefHitsAnyOrder( index.typeName(), "3", "4", "5" );
+
+		// No stopword removal
+		assertMatchQuery( field, "a" )
+				.hasDocRefHitsAnyOrder( index.typeName(), "7" );
+		assertMatchQuery( field, "the" )
+				.hasDocRefHitsAnyOrder( index.typeName(), "7" );
+	}
+
+	@Test
+	public void analyzer_simple() {
+		SimpleFieldModel<String> field = index.binding().simpleAnalyzer;
+		initData( field );
+
+		// Tokenize on space, hyphen, dot (basically, any character which is not a letter)
+		assertMatchQuery( field, "words" )
+				.hasDocRefHitsAnyOrder( index.typeName(), "2", "3", "4", "5" );
+
+		// Case-insensitive
+		assertMatchQuery( field, "WORDS" )
+				.hasDocRefHitsAnyOrder( index.typeName(), "2", "3", "4", "5" );
+		assertMatchQuery( field, "Words" )
+				.hasDocRefHitsAnyOrder( index.typeName(), "2", "3", "4", "5" );
+
+		// No stopword removal
+		assertMatchQuery( field, "a" )
+				.hasDocRefHitsAnyOrder( index.typeName(), "7" );
+		assertMatchQuery( field, "the" )
+				.hasDocRefHitsAnyOrder( index.typeName(), "7" );
+	}
+
+	@Test
+	public void analyzer_whitespace() {
+		SimpleFieldModel<String> field = index.binding().whitespaceAnalyzer;
+		initData( field );
+
+		// Tokenize on space only && case-sensitive
+		assertMatchQuery( field, "words" )
+				.hasDocRefHitsAnyOrder( index.typeName(), "4" );
+
+		// Case-sensitive
+		assertMatchQuery( field, "WORDS" )
+				.hasDocRefHitsAnyOrder( index.typeName(), "5" );
+		assertMatchQuery( field, "Words" )
+				.hasNoHits();
+
+		// No stopword removal
+		assertMatchQuery( field, "a" )
+				.hasDocRefHitsAnyOrder( index.typeName(), "7" );
+		assertMatchQuery( field, "the" )
+				.hasDocRefHitsAnyOrder( index.typeName(), "7" );
+	}
+
+	@Test
+	public void analyzer_stop() {
+		SimpleFieldModel<String> field = index.binding().stopAnalyzer;
+		initData( field );
+
+		// Tokenize on space, hyphen, dot (basically, any character which is not a letter)
+		assertMatchQuery( field, "words" )
+				.hasDocRefHitsAnyOrder( index.typeName(), "2", "3", "4", "5" );
+
+		// Case-insensitive
+		assertMatchQuery( field, "WORDS" )
+				.hasDocRefHitsAnyOrder( index.typeName(), "2", "3", "4", "5" );
+		assertMatchQuery( field, "Words" )
+				.hasDocRefHitsAnyOrder( index.typeName(), "2", "3", "4", "5" );
+
+		// Stopword removal
+		assertMatchQuery( field, "a" )
+				.hasNoHits();
+		assertMatchQuery( field, "the" )
+				.hasNoHits();
+	}
+
+	@Test
+	public void analyzer_keyword() {
+		SimpleFieldModel<String> field = index.binding().keywordAnalyzer;
+		initData( field );
+
+		// no match for any partial text
+		assertMatchQuery( field, "words" ).hasNoHits();
+		assertMatchQuery( field, "WORDS" ).hasNoHits();
+		assertMatchQuery( field, "Words" ).hasNoHits();
+		assertMatchQuery( field, "a" ).hasNoHits();
+		assertMatchQuery( field, "the" ).hasNoHits();
+
+		// as a keyword field, it will match only the whole text
+		assertMatchQuery( field, "two wôrds" ).hasDocRefHitsAnyOrder( index.typeName(), "6" );
+	}
+
 	private SearchResultAssert<DocumentReference> assertMatchQuery(SimpleFieldModel<String> fieldModel, String valueToMatch) {
 		StubMappingScope scope = index.createScope();
 
@@ -104,20 +197,50 @@ public class AnalysisBuiltinIT {
 		return assertThatQuery( query );
 	}
 
-	private void initData(SimpleFieldModel<String> fieldModel, Consumer<SingleFieldDocumentBuilder<String>> valueContributor) {
+	private void initData(SimpleFieldModel<String> fieldModel) {
 		index.bulkIndexer()
-				.add( fieldModel.reference, valueContributor )
+				.add( fieldModel.reference, AnalysisBuiltinIT::buildDocuments )
 				.join();
+	}
+
+	private static void buildDocuments(SingleFieldDocumentBuilder<String> builder) {
+		builder.emptyDocument( "empty" );
+		builder.document( "1", "twowords" );
+		builder.document( "2", "two.words" );
+		builder.document( "3", "two-words" );
+		builder.document( "4", "two words" );
+		builder.document( "5", "TWO WORDS" );
+		builder.document( "6", "two wôrds" );
+		builder.document( "7", "a stopword the stopword" );
 	}
 
 	private static class IndexBinding {
 		final SimpleFieldModel<String> defaultAnalyzer;
+		final SimpleFieldModel<String> standardAnalyzer;
+		final SimpleFieldModel<String> simpleAnalyzer;
+		final SimpleFieldModel<String> whitespaceAnalyzer;
+		final SimpleFieldModel<String> stopAnalyzer;
+		final SimpleFieldModel<String> keywordAnalyzer;
 
 		IndexBinding(IndexSchemaElement root) {
 			this.defaultAnalyzer = SimpleFieldModel.mapperWithOverride( KeywordStringFieldTypeDescriptor.INSTANCE,
 					f -> f.asString().analyzer( AnalyzerNames.DEFAULT ) )
 					.map( root, "default" );
+			this.standardAnalyzer = SimpleFieldModel.mapperWithOverride( KeywordStringFieldTypeDescriptor.INSTANCE,
+					f -> f.asString().analyzer( AnalyzerNames.STANDARD ) )
+					.map( root, "standard" );
+			this.simpleAnalyzer = SimpleFieldModel.mapperWithOverride( KeywordStringFieldTypeDescriptor.INSTANCE,
+					f -> f.asString().analyzer( AnalyzerNames.SIMPLE ) )
+					.map( root, "simple" );
+			this.whitespaceAnalyzer = SimpleFieldModel.mapperWithOverride( KeywordStringFieldTypeDescriptor.INSTANCE,
+					f -> f.asString().analyzer( AnalyzerNames.WHITESPACE ) )
+					.map( root, "whitespace" );
+			this.stopAnalyzer = SimpleFieldModel.mapperWithOverride( KeywordStringFieldTypeDescriptor.INSTANCE,
+					f -> f.asString().analyzer( AnalyzerNames.STOP ) )
+					.map( root, "stop" );
+			this.keywordAnalyzer = SimpleFieldModel.mapperWithOverride( KeywordStringFieldTypeDescriptor.INSTANCE,
+					f -> f.asString().analyzer( AnalyzerNames.KEYWORD ) )
+					.map( root, "keyword" );
 		}
 	}
-
 }
