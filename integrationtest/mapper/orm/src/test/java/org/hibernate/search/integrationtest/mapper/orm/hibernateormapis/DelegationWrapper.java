cf6635b302d57b8e8a38ef17ba15476698a6220c
Yoann Rodière <yoann@hibernate.org>
2020-11-14 09:06:34 +0100

    HSEARCH-4108 Test Search.session with session proxies
    Signed-off-by: Yoann Rodière <yoann@hibernate.org>

---
+++ integrationtest/mapper/orm/src/test/java/org/hibernate/search/integrationtest/mapper/orm/hibernateormapis/DelegationWrapper.java
@@ -0,0 +1,40 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.integrationtest.mapper.orm.hibernateormapis;
+
+import java.io.Serializable;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import org.hibernate.Session;
+
+/**
+ * @author Emmanuel Bernard
+ */
+public class DelegationWrapper implements InvocationHandler, Serializable {
+	Object realSession;
+
+	public DelegationWrapper(Session session) {
+		this.realSession = session;
+	}
+
+	@Override
+	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+		try {
+			return method.invoke( realSession, args );
+		}
+		catch (InvocationTargetException e) {
+			if ( e.getTargetException() instanceof RuntimeException ) {
+				throw (RuntimeException) e.getTargetException();
+			}
+			else {
+				throw e;
+			}
+		}
+	}
+}
---
+++ integrationtest/mapper/orm/src/test/java/org/hibernate/search/integrationtest/mapper/orm/hibernateormapis/ToSearchSessionFromSessionProxyIT.java
@@ -0,0 +1,202 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.integrationtest.mapper.orm.hibernateormapis;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hibernate.search.util.impl.integrationtest.mapper.orm.OrmUtils.with;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import java.lang.reflect.Proxy;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.SharedSessionContract;
+import org.hibernate.context.internal.ThreadLocalSessionContext;
+import org.hibernate.engine.jdbc.LobCreationContext;
+import org.hibernate.engine.spi.SessionImplementor;
+import org.hibernate.event.spi.EventSource;
+import org.hibernate.search.mapper.orm.Search;
+import org.hibernate.search.mapper.orm.session.SearchSession;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.FullTextField;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
+import org.hibernate.search.util.common.impl.Futures;
+import org.hibernate.search.util.impl.integrationtest.common.rule.BackendMock;
+import org.hibernate.search.util.impl.integrationtest.mapper.orm.OrmSetupHelper;
+import org.hibernate.search.util.impl.integrationtest.mapper.orm.OrmUtils;
+import org.hibernate.search.util.impl.test.annotation.PortedFromSearch5;
+import org.hibernate.search.util.impl.test.annotation.TestForIssue;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * @author Emmanuel Bernard
+ */
+@PortedFromSearch5(original = "org.hibernate.search.test.session.SessionTest")
+@TestForIssue(jiraKey = "HSEARCH-4108")
+public class ToSearchSessionFromSessionProxyIT {
+
+	//EventSource, org.hibernate.Session, LobCreationContext
+	private static final Class<?>[] SESS_PROXY_INTERFACES = new Class[] {
+			Session.class,
+			LobCreationContext.class,
+			EventSource.class,
+			SessionImplementor.class,
+			SharedSessionContract.class
+	};
+
+	@Rule
+	public BackendMock backendMock = new BackendMock();
+
+	@Rule
+	public OrmSetupHelper setupHelper = OrmSetupHelper.withBackendMock( backendMock );
+
+	private SessionFactory sessionFactory;
+
+	@Before
+	public void setup() {
+		backendMock.expectAnySchema( IndexedEntity.NAME );
+		sessionFactory = setupHelper.start()
+				// for this test we explicitly set the auto commit mode since we are not explicitly starting a transaction
+				// which could be a problem in some databases.
+				.withProperty( "hibernate.connection.autocommit", "true" )
+				//needed for testThreadBoundSessionWrappingOutOfTransaction
+				.withProperty( "hibernate.current_session_context_class", "thread" )
+				.setup( IndexedEntity.class );
+		backendMock.verifyExpectationsMet();
+	}
+
+	@Test
+	public void testSessionWrapper() {
+		with( sessionFactory ).runNoTransaction( s -> {
+			DelegationWrapper wrapper = new DelegationWrapper( s );
+			Session wrapped = (Session) Proxy.newProxyInstance(
+					Session.class.getClassLoader(),
+					SESS_PROXY_INTERFACES,
+					wrapper
+			);
+			try {
+				SearchSession searchSession = Search.session( wrapped );
+				assertNotNull( searchSession );
+				assertThat( searchSession.toEntityManager() ).isSameAs( wrapped );
+				assertThat( searchSession.toOrmSession() ).isSameAs( wrapped );
+			}
+			catch (ClassCastException e) {
+				e.printStackTrace();
+				fail( e.toString() );
+			}
+			wrapped.close();
+		} );
+	}
+
+	@Test
+	public void testThreadBoundSessionWrappingInTransaction() {
+		final Session sessionFromFirstThread = sessionFactory.getCurrentSession();
+		try {
+			OrmUtils.withinTransaction( sessionFromFirstThread, ignored -> {
+				SearchSession searchSessionFromFirstThread = Search.session( sessionFromFirstThread );
+				assertNotNull( searchSessionFromFirstThread );
+				assertThat( searchSessionFromFirstThread.toEntityManager() ).isSameAs( sessionFromFirstThread );
+				assertThat( searchSessionFromFirstThread.toOrmSession() ).isSameAs( sessionFromFirstThread );
+				ThreadPoolExecutor executorService = new ThreadPoolExecutor(
+						1, 1, 0L, TimeUnit.MILLISECONDS,
+						new LinkedBlockingQueue<>( 10 )
+				);
+				CompletableFuture<?> future = Futures.runAsync(
+						() -> {
+							Session sessionFromOtherThread = sessionFactory.getCurrentSession();
+							assertThat( sessionFromOtherThread ).isNotSameAs( sessionFromFirstThread );
+							SearchSession searchSessionFromOtherThread = Search.session( sessionFromOtherThread );
+							assertNotNull( searchSessionFromOtherThread );
+							assertThat( searchSessionFromOtherThread.toEntityManager() ).isSameAs( sessionFromOtherThread );
+							assertThat( searchSessionFromOtherThread.toOrmSession() ).isSameAs( sessionFromOtherThread );
+						},
+						executorService
+				);
+				Futures.unwrappedExceptionJoin( future );
+			} );
+		}
+		finally {
+			//clean up after the mess
+			ThreadLocalSessionContext.unbind( sessionFactory );
+		}
+	}
+
+	@Test
+	public void testThreadBoundSessionWrappingOutOfTransaction() {
+		final Session sessionFromFirstThread = sessionFactory.getCurrentSession();
+		try {
+			SearchSession searchSessionFromFirstThread = Search.session( sessionFromFirstThread );
+			assertNotNull( searchSessionFromFirstThread );
+			assertThat( searchSessionFromFirstThread.toEntityManager() ).isSameAs( sessionFromFirstThread );
+			assertThat( searchSessionFromFirstThread.toOrmSession() ).isSameAs( sessionFromFirstThread );
+			ThreadPoolExecutor executorService = new ThreadPoolExecutor(
+					1, 1, 0L, TimeUnit.MILLISECONDS,
+					new LinkedBlockingQueue<>( 10 )
+			);
+			CompletableFuture<?> future = Futures.runAsync(
+					() -> {
+						Session sessionFromOtherThread = sessionFactory.getCurrentSession();
+						assertThat( sessionFromOtherThread ).isNotSameAs( sessionFromFirstThread );
+						SearchSession searchSessionFromOtherThread = Search.session( sessionFromOtherThread );
+						assertNotNull( searchSessionFromOtherThread );
+						assertThat( searchSessionFromOtherThread.toEntityManager() ).isSameAs( sessionFromOtherThread );
+						assertThat( searchSessionFromOtherThread.toOrmSession() ).isSameAs( sessionFromOtherThread );
+					},
+					executorService
+			);
+			Futures.unwrappedExceptionJoin( future );
+		}
+		finally {
+			//clean up after the mess
+			ThreadLocalSessionContext.unbind( sessionFactory );
+		}
+	}
+
+	@Entity(name = IndexedEntity.NAME)
+	@Indexed
+	public static class IndexedEntity {
+
+		public static final String NAME = "indexed";
+
+		@Id
+		private Integer id;
+
+		@FullTextField
+		private String text;
+
+		@Override
+		public String toString() {
+			return "IndexedEntity[id=" + id + "]";
+		}
+
+		public Integer getId() {
+			return id;
+		}
+
+		public void setId(Integer id) {
+			this.id = id;
+		}
+
+		public String getText() {
+			return text;
+		}
+
+		public void setText(String text) {
+			this.text = text;
+		}
+
+	}
+}
