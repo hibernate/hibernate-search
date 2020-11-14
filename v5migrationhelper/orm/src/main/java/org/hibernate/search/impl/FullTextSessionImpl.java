89a290cac4e646b2adb0672ed5a6d18e08df43b0
Yoann Rodière <yoann@hibernate.org>
2020-11-14 08:13:29 +0100

    HSEARCH-4107 Replace unsafe casts to SessionImplementor with call to unwrap()
    Sometimes the session is proxified (e.g. in Spring 2.4), so casting it
    won't work, but unwrapping will.
    
    Signed-off-by: Yoann Rodière <yoann@hibernate.org>

--- v5migrationhelper/orm/src/main/java/org/hibernate/search/impl/FullTextSessionImpl.java
+++ v5migrationhelper/orm/src/main/java/org/hibernate/search/impl/FullTextSessionImpl.java
@@ -7,9 +7,12 @@
 package org.hibernate.search.impl;
 
 import java.io.Serializable;
+import java.lang.invoke.MethodHandles;
 
+import org.hibernate.Session;
 import org.hibernate.engine.spi.SessionDelegatorBaseImpl;
 import org.hibernate.engine.spi.SessionImplementor;
+import org.hibernate.event.spi.EventSource;
 import org.hibernate.search.FullTextQuery;
 import org.hibernate.search.FullTextSession;
 import org.hibernate.search.FullTextSharedSessionBuilder;
@@ -26,6 +29,8 @@ import org.hibernate.search.scope.spi.V5MigrationSearchScope;
 import org.hibernate.search.query.engine.spi.V5MigrationSearchSession;
 import org.hibernate.search.query.hibernate.impl.FullTextQueryImpl;
 import org.hibernate.search.scope.impl.V5MigrationOrmSearchScopeAdapter;
+import org.hibernate.search.util.logging.impl.Log;
+import org.hibernate.search.util.logging.impl.LoggerFactory;
 
 /**
  * Lucene full text search aware session.
@@ -37,11 +42,33 @@ import org.hibernate.search.scope.impl.V5MigrationOrmSearchScopeAdapter;
 final class FullTextSessionImpl extends SessionDelegatorBaseImpl
 		implements FullTextSession, SessionImplementor, V5MigrationSearchSession<SearchLoadingOptionsStep> {
 
+	private static final Log log = LoggerFactory.make( MethodHandles.lookup() );
+
+	private static SessionImplementor doUnwrap(Session session) {
+		if ( session == null ) {
+			throw log.getNullSessionPassedToFullTextSessionCreationException();
+		}
+		// Keeping both instanceofs in case the interface hierarchy changes.
+		else if ( session instanceof SessionImplementor && session instanceof EventSource ) {
+			// A session proxied with ThreadLocalSessionContext will implement all the interfaces we need,
+			// but won't allow a call to .unwrap() outside of a transaction.
+			// Thus we need to proceed with the cast.
+			return (EventSource) session;
+		}
+		else {
+			// Other proxies (such as Spring's after version 2.4),
+			// only implement Session, not SessionImplementor,
+			// but allow a call to .unwrap().
+			return session.unwrap( SessionImplementor.class );
+		}
+	}
+
+
 	private transient V5MigrationOrmSearchIntegratorAdapter searchIntegrator;
 	private transient SearchFactory searchFactoryAPI;
 
 	public FullTextSessionImpl(org.hibernate.Session session) {
-		super( (SessionImplementor) session );
+		super( doUnwrap( session ) );
 	}
 
 	@Override
