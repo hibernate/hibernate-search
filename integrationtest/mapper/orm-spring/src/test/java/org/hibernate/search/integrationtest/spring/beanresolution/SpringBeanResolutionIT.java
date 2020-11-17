490f8f85d9d9ba3b91f2a0e871fbe5b018a4e0b3
Yoann Rodière <yoann@hibernate.org>
2020-11-17 15:49:34 +0100

    HSEARCH-3479 Test Spring bean resolution
    In a similar fashion to CdiBeanResolutionIT.
    
    Signed-off-by: Yoann Rodière <yoann@hibernate.org>

---
+++ integrationtest/mapper/orm-spring/src/test/java/org/hibernate/search/integrationtest/spring/beanresolution/SpringBeanResolutionIT.java
@@ -0,0 +1,277 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.integrationtest.spring.beanresolution;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.persistence.Entity;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Id;
+
+import org.hibernate.search.engine.environment.bean.BeanHolder;
+import org.hibernate.search.engine.environment.bean.BeanReference;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
+import org.hibernate.search.util.impl.integrationtest.common.rule.BackendMock;
+import org.hibernate.search.util.impl.integrationtest.mapper.orm.OrmSetupHelper;
+import org.hibernate.search.util.impl.test.annotation.TestForIssue;
+import org.hibernate.search.util.impl.test.rule.StaticCounters;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import org.assertj.core.api.InstanceOfAssertFactories;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+/**
+ * Check that Hibernate Search will be able to get Spring beans from the BeanResolver thanks to the ORM integration.
+ * <p>
+ * We test this by retrieving custom beans from the backend mock,
+ * then checking the Spring-defined hooks (@PostConstruct and @PreDestroy) have been called
+ * exactly as many times as expected.
+ */
+@TestForIssue(jiraKey = { "HSEARCH-1316", "HSEARCH-3171" })
+public class SpringBeanResolutionIT {
+
+	@Configuration
+	@EnableAutoConfiguration
+	@EntityScan
+	@ComponentScan(basePackageClasses = SpringBeanResolutionIT.class)
+	public static class SpringConfig {
+		@Bean
+		public HibernatePropertiesCustomizer backendMockPropertiesCustomizer(ApplicationContext applicationContext) {
+			BackendMock backendMock = applicationContext.getEnvironment()
+					.getProperty( "test.backendMock", BackendMock.class );
+			return hibernateProperties ->
+					hibernateProperties.put( "hibernate.search.backend.type", backendMock.factory() );
+		}
+	}
+
+	@Rule
+	public BackendMock backendMock = new BackendMock();
+
+	@Rule
+	public OrmSetupHelper ormSetupHelper = OrmSetupHelper.withBackendMock( backendMock );
+
+	@Rule
+	public StaticCounters counters = new StaticCounters();
+
+	@Test
+	public void singleton_byType() {
+		doTest(
+				ExpectedScope.SINGLETON, UnnamedSingletonBean.KEYS,
+				BeanReference.of( UnnamedSingletonBean.class )
+		);
+	}
+
+	@Test
+	public void singleton_byName() {
+		doTest(
+				ExpectedScope.SINGLETON, NamedSingletonBean.KEYS,
+				BeanReference.of( InterfaceDefinedByMapper.class, NamedSingletonBean.NAME )
+		);
+	}
+
+	@Test
+	public void prototype_byType() {
+		doTest(
+				ExpectedScope.PROTOTYPE, UnnamedPrototypeBean.KEYS,
+				BeanReference.of( UnnamedPrototypeBean.class )
+		);
+	}
+
+	@Test
+	public void prototype_byName() {
+		doTest(
+				ExpectedScope.PROTOTYPE, NamedPrototypeBean.KEYS,
+				BeanReference.of( InterfaceDefinedByMapper.class, NamedPrototypeBean.NAME )
+		);
+	}
+
+	private <T> void doTest(ExpectedScope expectedScope, CounterKeys counterKeys, BeanReference<T> reference) {
+		List<BeanHolder<T>> retrievedBeans = new ArrayList<>();
+
+		backendMock.onCreate( context -> {
+			BeanHolder<T> retrievedBean1 = context.beanResolver().resolve( reference );
+			retrievedBeans.add( retrievedBean1 );
+			BeanHolder<T> retrievedBean2 = context.beanResolver().resolve( reference );
+			retrievedBeans.add( retrievedBean2 );
+		} );
+		backendMock.onStop( () -> {
+			for ( BeanHolder<T> retrievedBean : retrievedBeans ) {
+				retrievedBean.close();
+			}
+		} );
+		backendMock.expectAnySchema( IndexedEntity.NAME );
+
+		int expectedInstances = ExpectedScope.SINGLETON.equals( expectedScope ) ? 1 : 2;
+
+		try ( @SuppressWarnings("unused") ConfigurableApplicationContext applicationContext = startApplication() ) {
+			applicationContext.getBean( EntityManagerFactory.class ).getMetamodel();
+			backendMock.verifyExpectationsMet();
+
+			assertThat( retrievedBeans )
+					.hasSize( 2 )
+					.doesNotContainNull()
+					.extracting( BeanHolder::get )
+					.doesNotContainNull()
+					// All created beans should have been injected
+					.allSatisfy( bean -> assertThat( bean )
+							.asInstanceOf( InstanceOfAssertFactories.type( AbstractBeanBase.class ) )
+							.extracting( "injectedBean" )
+							.isNotNull()
+					);
+
+			assertThat( StaticCounters.get().get( counterKeys.instantiate ) ).isEqualTo( expectedInstances );
+			assertThat( StaticCounters.get().get( counterKeys.postConstruct ) ).isEqualTo( expectedInstances );
+			assertThat( StaticCounters.get().get( counterKeys.preDestroy ) ).isEqualTo( 0 );
+		}
+
+		switch ( expectedScope ) {
+			case PROTOTYPE:
+				// Spring does not call @PreDestroy on prototype beans
+				// See https://docs.spring.io/spring-framework/docs/5.3.1/reference/html/core.html#beans-factory-scopes-prototype
+				assertThat( StaticCounters.get().get( counterKeys.preDestroy ) ).isEqualTo( 0 );
+				break;
+			case SINGLETON:
+			default:
+				assertThat( StaticCounters.get().get( counterKeys.preDestroy ) ).isEqualTo( 1 );
+				break;
+		}
+	}
+
+	private ConfigurableApplicationContext startApplication() {
+		Map<String, Object> properties = new LinkedHashMap<>();
+		properties.put( "test.backendMock", backendMock );
+		return new SpringApplicationBuilder( SpringConfig.class )
+				.web( WebApplicationType.NONE )
+				.properties( properties )
+				.run();
+	}
+
+	@Entity(name = IndexedEntity.NAME)
+	@Indexed(index = IndexedEntity.NAME)
+	public static final class IndexedEntity {
+
+		static final String NAME = "indexed";
+
+		@Id
+		private Integer id;
+
+		public Integer getId() {
+			return id;
+		}
+
+		public void setId(Integer id) {
+			this.id = id;
+		}
+	}
+
+	public interface InterfaceDefinedByMapper {
+
+	}
+
+	@Component
+	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+	public static class InjectedBean {
+	}
+
+	public abstract static class AbstractBeanBase
+			implements InterfaceDefinedByMapper {
+		@Autowired
+		private InjectedBean injectedBean;
+
+		protected AbstractBeanBase() {
+			StaticCounters.get().increment( getCounterKeys().instantiate );
+		}
+
+		@PostConstruct
+		public void postConstruct() {
+			StaticCounters.get().increment( getCounterKeys().postConstruct );
+		}
+
+		@PreDestroy
+		public void preDestroy() {
+			StaticCounters.get().increment( getCounterKeys().preDestroy );
+		}
+
+		protected abstract CounterKeys getCounterKeys();
+	}
+
+	@Component
+	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+	public static class UnnamedPrototypeBean extends AbstractBeanBase {
+		public static final CounterKeys KEYS = new CounterKeys();
+
+		@Override
+		protected CounterKeys getCounterKeys() {
+			return KEYS;
+		}
+	}
+
+	@Component(NamedPrototypeBean.NAME)
+	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+	public static class NamedPrototypeBean extends AbstractBeanBase {
+		public static final String NAME = "NameOfDependentBean";
+		public static final CounterKeys KEYS = new CounterKeys();
+
+		@Override
+		protected CounterKeys getCounterKeys() {
+			return KEYS;
+		}
+	}
+
+	@Component
+	public static class UnnamedSingletonBean extends AbstractBeanBase {
+		public static final CounterKeys KEYS = new CounterKeys();
+
+		@Override
+		protected CounterKeys getCounterKeys() {
+			return KEYS;
+		}
+	}
+
+	@Component(NamedSingletonBean.NAME)
+	public static class NamedSingletonBean extends AbstractBeanBase {
+		public static final String NAME = "NameOfSingletonBean";
+		public static final CounterKeys KEYS = new CounterKeys();
+
+		@Override
+		protected CounterKeys getCounterKeys() {
+			return KEYS;
+		}
+	}
+
+	private static class CounterKeys {
+		final StaticCounters.Key instantiate = StaticCounters.createKey();
+		final StaticCounters.Key postConstruct = StaticCounters.createKey();
+		final StaticCounters.Key preDestroy = StaticCounters.createKey();
+	}
+
+	private enum ExpectedScope {
+		SINGLETON,
+		PROTOTYPE
+	}
+}
