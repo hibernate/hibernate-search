7d3e7c8adb2c565243ee4e091e7601aa6018bbb5
Yoann Rodière <yoann@hibernate.org>
2021-03-05 12:35:42 +0100

    HSEARCH-4184 Formatting fixes
    Signed-off-by: Yoann Rodière <yoann@hibernate.org>

--- mapper/pojo-base/src/main/java/org/hibernate/search/mapper/pojo/automaticindexing/impl/PojoImplicitReindexingResolverNode.java
+++ mapper/pojo-base/src/main/java/org/hibernate/search/mapper/pojo/automaticindexing/impl/PojoImplicitReindexingResolverNode.java
@@ -33,8 +33,8 @@ public abstract class PojoImplicitReindexingResolverNode<T> implements AutoClose
 	 *  @param collector A collector for entities that should be reindexed.
 	 * @param dirty A value that is dirty to some extent.
 	 * @param context The set of dirty paths in the object passed to the root reindexing resolver
- * (resolvers may delegate to other resolvers, but they will always pass the same dirtiness state to delegates).
- * {@code null} can be passed to mean "no information", in which case all paths are considered dirty.
+	 * (resolvers may delegate to other resolvers, but they will always pass the same dirtiness state to delegates).
+	 * {@code null} can be passed to mean "no information", in which case all paths are considered dirty.
 	 */
 	public abstract void resolveEntitiesToReindex(PojoReindexingCollector collector,
 			T dirty, PojoImplicitReindexingResolverRootContext context);
