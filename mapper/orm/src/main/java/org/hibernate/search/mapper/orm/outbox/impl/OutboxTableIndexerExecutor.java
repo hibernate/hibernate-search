5f9514835582c682c547a8640a0b5a09cf3811c0
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2021-03-10 16:30:45 +0100

    HSEARCH-4134 Store outbox event retrials

--- mapper/orm/src/main/java/org/hibernate/search/mapper/orm/outbox/impl/OutboxTableIndexerExecutor.java
+++ mapper/orm/src/main/java/org/hibernate/search/mapper/orm/outbox/impl/OutboxTableIndexerExecutor.java
@@ -9,7 +9,10 @@ package org.hibernate.search.mapper.orm.outbox.impl;
 import static org.hibernate.search.mapper.orm.common.impl.TransactionUtils.withinTransaction;
 
 import java.lang.invoke.MethodHandles;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -18,7 +21,10 @@ import org.hibernate.Session;
 import org.hibernate.query.Query;
 import org.hibernate.search.mapper.orm.automaticindexing.spi.AutomaticIndexingMappingContext;
 import org.hibernate.search.mapper.orm.logging.impl.Log;
+import org.hibernate.search.mapper.pojo.route.DocumentRouteDescriptor;
+import org.hibernate.search.mapper.pojo.route.DocumentRoutesDescriptor;
 import org.hibernate.search.util.common.logging.impl.LoggerFactory;
+import org.hibernate.search.util.common.serialization.spi.SerializationUtils;
 
 public class OutboxTableIndexerExecutor {
 
@@ -57,9 +63,14 @@ public class OutboxTableIndexerExecutor {
 			while ( !events.isEmpty() ) {
 				OutboxEventProcessing<OutboxEvent> eventProcessing = new OutboxEventProcessing<>(
 						mapping, session, events );
-
-				// TODO HSEARCH-4134 handle the failures
 				List<Integer> ids = eventProcessing.processEvents();
+
+				try {
+					createNewOutboxRetrials( session, eventProcessing.getFailedEvents() );
+				}
+				catch (Throwable throwable) {
+					log.errorf( throwable, "Failed to store outbox events for future retrials" );
+				}
 
 				try {
 					deleteOutboxEntities( session, ids );
@@ -89,6 +100,45 @@ public class OutboxTableIndexerExecutor {
 			session.clear();
 			return update;
 		} );
+	}
+
+	private static int createNewOutboxRetrials(Session session,
+			Map<OutboxEventReference, List<OutboxEvent>> failedEvents) {
+		return withinTransaction( session, () -> {
+			for ( Map.Entry<OutboxEventReference, List<OutboxEvent>> entry : failedEvents.entrySet() ) {
+				OutboxEventRetry eventRetry = new OutboxEventRetry(
+						entry.getKey(), mergeDocumentRoutes( entry.getValue() ) );
+				session.persist( eventRetry );
+			}
+
+			return 0;
+		} );
+	}
+
+	public static byte[] mergeDocumentRoutes(List<OutboxEvent> events) {
+		if ( events.isEmpty() ) {
+			return null;
+		}
+
+		if ( events.size() == 1 ) {
+			return events.get( 0 ).getDocumentRoutes();
+		}
+
+		DocumentRouteDescriptor currentRoute = null;
+		Collection<DocumentRouteDescriptor> previousRoutes = new HashSet<>();
+
+		for ( OutboxEvent event : events ) {
+			DocumentRoutesDescriptor routes = SerializationUtils.deserialize(
+					DocumentRoutesDescriptor.class, event.getDocumentRoutes() );
+
+			// always override the current route
+			// TODO HSEARCH-4186 See if the algorithm makes sense
+			currentRoute = routes.currentRoute();
+			previousRoutes.add( routes.currentRoute() );
+			previousRoutes.addAll( routes.previousRoutes() );
+		}
+
+		return SerializationUtils.serialize( DocumentRoutesDescriptor.of( currentRoute, previousRoutes ) );
 	}
 }
 
