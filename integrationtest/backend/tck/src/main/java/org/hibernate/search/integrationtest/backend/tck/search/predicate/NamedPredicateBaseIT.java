279d22380bc064d7d0ca397d613150bffd74f2f6
Yoann Rodière <yoann@hibernate.org>
2021-04-21 15:34:43 +0200

    HSEARCH-4166 Test automatic nested predicates around named predicates

--- integrationtest/backend/tck/src/main/java/org/hibernate/search/integrationtest/backend/tck/search/predicate/NamedPredicateBaseIT.java
+++ integrationtest/backend/tck/src/main/java/org/hibernate/search/integrationtest/backend/tck/search/predicate/NamedPredicateBaseIT.java
@@ -8,24 +8,20 @@ package org.hibernate.search.integrationtest.backend.tck.search.predicate;
 
 import static org.hibernate.search.util.impl.integrationtest.common.assertion.SearchResultAssert.assertThatQuery;
 
-import java.util.function.Function;
-
-import org.hibernate.search.engine.backend.common.DocumentReference;
 import org.hibernate.search.engine.backend.document.DocumentElement;
 import org.hibernate.search.engine.backend.document.IndexObjectFieldReference;
 import org.hibernate.search.engine.backend.document.model.dsl.IndexSchemaElement;
 import org.hibernate.search.engine.backend.document.model.dsl.IndexSchemaObjectField;
 import org.hibernate.search.engine.backend.types.ObjectStructure;
 import org.hibernate.search.engine.search.predicate.SearchPredicate;
+import org.hibernate.search.engine.search.predicate.dsl.SearchPredicateFactory;
 import org.hibernate.search.engine.search.predicate.factories.NamedPredicateProvider;
 import org.hibernate.search.engine.search.predicate.factories.NamedPredicateProviderContext;
-import org.hibernate.search.engine.search.query.dsl.SearchQueryFinalStep;
-import org.hibernate.search.integrationtest.backend.tck.testsupport.types.AnalyzedStringFieldTypeDescriptor;
+import org.hibernate.search.integrationtest.backend.tck.testsupport.types.KeywordStringFieldTypeDescriptor;
 import org.hibernate.search.integrationtest.backend.tck.testsupport.util.SimpleFieldModel;
 import org.hibernate.search.integrationtest.backend.tck.testsupport.util.rule.SearchSetupHelper;
 import org.hibernate.search.util.impl.integrationtest.mapper.stub.BulkIndexer;
 import org.hibernate.search.util.impl.integrationtest.mapper.stub.SimpleMappedIndex;
-import org.hibernate.search.util.impl.integrationtest.mapper.stub.StubMappingScope;
 
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
@@ -35,19 +31,13 @@ public class NamedPredicateBaseIT {
 
 	private static final String DOCUMENT_1 = "document1";
 	private static final String DOCUMENT_2 = "document2";
-	private static final String DOCUMENT_3 = "document3";
-	private static final String DOCUMENT_4 = "document4";
 	private static final String EMPTY = "empty";
 
-	private static final String TEXT_MATCHING_1 = "Localization in English is a must-have.";
-	private static final String TEXT_MATCHING_2 = "Internationalization allows to adapt the application to multiple locales.";
-	private static final String TEXT_MATCHING_3 = "A had to call the land lord.";
-	private static final String TEXT_MATCHING_4 = "I had some interaction with that lad.";
-
-	private static final String TERM_PATTERN_1 = "Localization";
-	private static final String TERM_PATTERN_2 = "Internationalization";
-	private static final String TERM_PATTERN_3 = "call";
-	private static final String TERM_PATTERN_4 = "interaction";
+	private static final String WORD_1 = "word1";
+	private static final String WORD_2 = "word2";
+	private static final String WORD_3 = "word3";
+	private static final String WORD_4 = "word4";
+	private static final String WORD_5 = "word5";
 
 	@ClassRule
 	public static final SearchSetupHelper setupHelper = new SearchSetupHelper();
@@ -66,62 +56,116 @@ public class NamedPredicateBaseIT {
 	}
 
 	@Test
-	public void searchNamedPredicateInRoot() {
-		StubMappingScope scope = index.createScope();
-		Function<String, SearchQueryFinalStep<DocumentReference>> createQuery = queryString -> scope.query()
-				.where( f -> f.named( "match_predicate" )
-				.param( "query", queryString ) );
-
-		assertThatQuery( createQuery.apply( TERM_PATTERN_1 ) )
+	public void root() {
+		assertThatQuery( index.query()
+				.where( f -> f.named( "match-both-fields" )
+						.param( "value1", WORD_1 )
+						.param( "value2", WORD_2 ) ) )
 				.hasDocRefHitsAnyOrder( index.typeName(), DOCUMENT_1 );
-
-		assertThatQuery( createQuery.apply( TERM_PATTERN_2 ) )
+		assertThatQuery( index.query()
+				.where( f -> f.named( "match-both-fields" )
+						.param( "value1", WORD_1 )
+						.param( "value2", WORD_5 ) ) )
 				.hasDocRefHitsAnyOrder( index.typeName(), DOCUMENT_2 );
-
-		assertThatQuery( createQuery.apply( TERM_PATTERN_3 ) )
-				.hasDocRefHitsAnyOrder( index.typeName(), DOCUMENT_3 );
-
-		assertThatQuery( createQuery.apply( TERM_PATTERN_4 ) )
-				.hasDocRefHitsAnyOrder( index.typeName(), DOCUMENT_4 );
 	}
 
 	@Test
-	public void searchNamedPredicateInNested() {
-		StubMappingScope scope = index.createScope();
-		Function<String, SearchQueryFinalStep<DocumentReference>> createQuery = queryString -> scope.query()
-				.where( f -> f.named( "nested.match_predicate" )
-				.param( "query", queryString ) );
-
-		assertThatQuery( createQuery.apply( TERM_PATTERN_1 ) )
+	public void nested() {
+		assertThatQuery( index.query()
+				.where( f -> f.named( "nested.match-both-fields" )
+						.param( "value1", WORD_1 )
+						.param( "value2", WORD_2 ) ) )
 				.hasDocRefHitsAnyOrder( index.typeName(), DOCUMENT_1 );
-
-		assertThatQuery( createQuery.apply( TERM_PATTERN_2 ) )
+		assertThatQuery( index.query()
+				.where( f -> f.named( "nested.match-both-fields" )
+						.param( "value1", WORD_1 )
+						.param( "value2", WORD_5 ) ) )
 				.hasDocRefHitsAnyOrder( index.typeName(), DOCUMENT_2 );
+		// This checks that we apply the nested predicate around the named predicate
+		assertThatQuery( index.query()
+				.where( f -> f.named( "nested.match-both-fields" )
+						.param( "value1", WORD_1 )
+						.param( "value2", WORD_4 ) ) )
+				.hasNoHits();
+	}
 
-		assertThatQuery( createQuery.apply( TERM_PATTERN_3 ) )
-				.hasDocRefHitsAnyOrder( index.typeName(), DOCUMENT_3 );
-
-		assertThatQuery( createQuery.apply( TERM_PATTERN_4 ) )
-				.hasDocRefHitsAnyOrder( index.typeName(), DOCUMENT_4 );
+	@Test
+	public void flattened() {
+		assertThatQuery( index.query()
+				.where( f -> f.named( "flattened.match-both-fields" )
+						.param( "value1", WORD_1 )
+						.param( "value2", WORD_2 ) ) )
+				.hasDocRefHitsAnyOrder( index.typeName(), DOCUMENT_1 );
+		assertThatQuery( index.query()
+				.where( f -> f.named( "flattened.match-both-fields" )
+						.param( "value1", WORD_1 )
+						.param( "value2", WORD_5 ) ) )
+				.hasDocRefHitsAnyOrder( index.typeName(), DOCUMENT_2 );
+		assertThatQuery( index.query()
+				.where( f -> f.named( "flattened.match-both-fields" )
+						.param( "value1", WORD_1 )
+						.param( "value2", WORD_4 ) ) )
+				.hasDocRefHitsAnyOrder( index.typeName(), DOCUMENT_1 );
 	}
 
 	private static class IndexBinding {
-		final SimpleFieldModel<String> fieldInRoot;
-		final IndexObjectFieldReference nested;
-		final SimpleFieldModel<String> fieldInNested;
+		final SimpleFieldModel<String> field1;
+		final SimpleFieldModel<String> field2;
+		final ObjectFieldBinding nested;
+		final ObjectFieldBinding flattened;
 
 		IndexBinding(IndexSchemaElement root) {
-			fieldInRoot = SimpleFieldModel.mapper( AnalyzedStringFieldTypeDescriptor.INSTANCE )
-					.map( root, "fieldInRoot" );
+			field1 = SimpleFieldModel.mapper( KeywordStringFieldTypeDescriptor.INSTANCE )
+					.map( root, "field1" );
+			field2 = SimpleFieldModel.mapper( KeywordStringFieldTypeDescriptor.INSTANCE )
+					.map( root, "field2" );
+			root.namedPredicate( "match-both-fields",
+					new TestNamedPredicateProvider( "field1", "field2" ) );
 
-			IndexSchemaObjectField nest = root.objectField( "nested", ObjectStructure.NESTED ).multiValued();
-			nested = nest.toReference();
+			nested = ObjectFieldBinding.create( root, "nested", ObjectStructure.NESTED );
+			flattened = ObjectFieldBinding.create( root, "flattened", ObjectStructure.FLATTENED );
+		}
 
-			fieldInNested = SimpleFieldModel.mapper( AnalyzedStringFieldTypeDescriptor.INSTANCE )
-					.map( nest, "fieldInNested" );
+		protected void initDocument(DocumentElement document, String value1, String value2,
+				String value3, String value4) {
+			document.addValue( field1.reference, value1 );
+			document.addValue( field2.reference, value2 );
 
-			root.namedPredicate( "match_predicate", new TestNamedPredicateProvider( "fieldInRoot" ) );
-			nest.namedPredicate( "match_predicate", new TestNamedPredicateProvider( "fieldInNested" ) );
+			DocumentElement nestedObject1 = document.addObject( nested.reference );
+			nestedObject1.addValue( nested.field1.reference, value1 );
+			nestedObject1.addValue( nested.field2.reference, value2 );
+			DocumentElement nestedObject2 = document.addObject( nested.reference );
+			nestedObject2.addValue( nested.field1.reference, value3 );
+			nestedObject2.addValue( nested.field2.reference, value4 );
+
+			DocumentElement flattenedObject1 = document.addObject( flattened.reference );
+			flattenedObject1.addValue( flattened.field1.reference, value1 );
+			flattenedObject1.addValue( flattened.field2.reference, value2 );
+			DocumentElement flattenedObject2 = document.addObject( flattened.reference );
+			flattenedObject2.addValue( flattened.field1.reference, value3 );
+			flattenedObject2.addValue( flattened.field2.reference, value4 );
+		}
+	}
+
+	static class ObjectFieldBinding {
+		final SimpleFieldModel<String> field1;
+		final SimpleFieldModel<String> field2;
+		final IndexObjectFieldReference reference;
+
+		static ObjectFieldBinding create(IndexSchemaElement parent, String relativeFieldName,
+				ObjectStructure structure) {
+			IndexSchemaObjectField objectField = parent.objectField( relativeFieldName, structure ).multiValued();
+			return new ObjectFieldBinding( objectField );
+		}
+
+		ObjectFieldBinding(IndexSchemaObjectField objectField) {
+			reference = objectField.toReference();
+			field1 = SimpleFieldModel.mapper( KeywordStringFieldTypeDescriptor.INSTANCE )
+					.map( objectField, "field1" );
+			field2 = SimpleFieldModel.mapper( KeywordStringFieldTypeDescriptor.INSTANCE )
+					.map( objectField, "field2" );
+			objectField.namedPredicate( "match-both-fields",
+					new TestNamedPredicateProvider( "field1", "field2" ) );
 		}
 	}
 
@@ -132,30 +176,8 @@ public class NamedPredicateBaseIT {
 
 		public void contribute(BulkIndexer indexer) {
 			indexer
-					.add( DOCUMENT_1, document -> {
-						document.addValue( index.binding().fieldInRoot.reference, TEXT_MATCHING_1 );
-						IndexObjectFieldReference level1 = index.binding().nested;
-						DocumentElement nested = document.addObject( level1 );
-						nested.addValue( index.binding().fieldInNested.reference, TEXT_MATCHING_1 );
-					} )
-					.add( DOCUMENT_2, document -> {
-						document.addValue( index.binding().fieldInRoot.reference, TEXT_MATCHING_2 );
-						IndexObjectFieldReference level1 = index.binding().nested;
-						DocumentElement nested = document.addObject( level1 );
-						nested.addValue( index.binding().fieldInNested.reference, TEXT_MATCHING_2 );
-					} )
-					.add( DOCUMENT_3, document -> {
-						document.addValue( index.binding().fieldInRoot.reference, TEXT_MATCHING_3 );
-						IndexObjectFieldReference level1 = index.binding().nested;
-						DocumentElement nested = document.addObject( level1 );
-						nested.addValue( index.binding().fieldInNested.reference, TEXT_MATCHING_3 );
-					} )
-					.add( DOCUMENT_4, document -> {
-						document.addValue( index.binding().fieldInRoot.reference, TEXT_MATCHING_4 );
-						IndexObjectFieldReference level1 = index.binding().nested;
-						DocumentElement nested = document.addObject( level1 );
-						nested.addValue( index.binding().fieldInNested.reference, TEXT_MATCHING_4 );
-					} )
+					.add( DOCUMENT_1, document -> index.binding().initDocument( document, WORD_1, WORD_2, WORD_3, WORD_4 ) )
+					.add( DOCUMENT_2, document -> index.binding().initDocument( document, WORD_1, WORD_5, WORD_3, WORD_5 ) )
 					.add( EMPTY, document -> {
 					} );
 		}
@@ -163,19 +185,24 @@ public class NamedPredicateBaseIT {
 
 	public static class TestNamedPredicateProvider implements NamedPredicateProvider {
 
-		private final String fieldName;
+		private final String field1Name;
+		private final String field2Name;
 
-		public TestNamedPredicateProvider(String fieldName) {
-			this.fieldName = fieldName;
+		public TestNamedPredicateProvider(String field1Name, String field2Name) {
+			this.field1Name = field1Name;
+			this.field2Name = field2Name;
 		}
 
 		@Override
 		public SearchPredicate create(NamedPredicateProviderContext context) {
-			String absoluteFieldPath = context.absolutePath( fieldName );
-			String queryString = (String) context.param( "query" );
-			return context.predicate()
-					.match().field( absoluteFieldPath )
-					.matching( queryString )
+			String field1Path = context.absolutePath( field1Name );
+			String field2Path = context.absolutePath( field2Name );
+			String word1 = (String) context.param( "value1" );
+			String word2 = (String) context.param( "value2" );
+			SearchPredicateFactory f = context.predicate();
+			return f.bool()
+					.must( f.match().field( field1Path ).matching( word1 ) )
+					.must( f.match().field( field2Path ).matching( word2 ) )
 					.toPredicate();
 		}
 	}
