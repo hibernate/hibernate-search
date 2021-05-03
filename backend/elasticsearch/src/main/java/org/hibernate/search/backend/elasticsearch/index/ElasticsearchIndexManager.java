57da29695deb3f73084d3a7fdf59d5d7410b62ac
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2021-05-03 18:17:02 +0200

    HSEARCH-4204 Introduce ES index descriptor

--- backend/elasticsearch/src/main/java/org/hibernate/search/backend/elasticsearch/index/ElasticsearchIndexManager.java
+++ backend/elasticsearch/src/main/java/org/hibernate/search/backend/elasticsearch/index/ElasticsearchIndexManager.java
@@ -7,11 +7,15 @@
 package org.hibernate.search.backend.elasticsearch.index;
 
 import org.hibernate.search.backend.elasticsearch.ElasticsearchBackend;
+import org.hibernate.search.backend.elasticsearch.metamodel.ElasticsearchIndexDescriptor;
 import org.hibernate.search.engine.backend.index.IndexManager;
 
 public interface ElasticsearchIndexManager extends IndexManager {
 
 	@Override
 	ElasticsearchBackend backend();
+
+	@Override
+	ElasticsearchIndexDescriptor descriptor();
 
 }
