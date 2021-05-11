72769f648f41f34561ad1f7c25bd10f757212881
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2021-05-11 21:24:53 +0200

    HSEARCH-4198 Test outbox with Spring & JTA

---
+++ integrationtest/mapper/orm-spring/src/test/java/org/hibernate/search/integrationtest/spring/jta/JtaAndSpringOutboxApplicationConfiguration.java
@@ -0,0 +1,21 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.integrationtest.spring.jta;
+
+import org.hibernate.search.util.impl.integrationtest.common.rule.BackendMock;
+import org.hibernate.search.util.impl.integrationtest.common.rule.BackendWorkThreadingExpectations;
+
+public class JtaAndSpringOutboxApplicationConfiguration extends JtaAndSpringApplicationConfiguration {
+
+	@Override
+	public BackendMock backendMock() {
+		BackendMock backendMock = super.backendMock();
+		backendMock.indexingWorkThreadingExpectations(
+				BackendWorkThreadingExpectations.async( ".*Outbox table.*" ) );
+		return backendMock;
+	}
+}
