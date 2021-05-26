a074944e06e246ae8cfac73871a819c175e8ec9f
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2021-05-26 21:38:36 +0200

    HSEARCH-4065 Test provided index reader with shards

--- integrationtest/backend/lucene/src/test/java/org/hibernate/search/integrationtest/backend/lucene/sharding/ShardingExplicitIT.java
+++ integrationtest/backend/lucene/src/test/java/org/hibernate/search/integrationtest/backend/lucene/sharding/ShardingExplicitIT.java
@@ -6,13 +6,25 @@
  */
 package org.hibernate.search.integrationtest.backend.lucene.sharding;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Collections;
 import java.util.Set;
 
+import org.hibernate.search.backend.lucene.LuceneExtension;
 import org.hibernate.search.backend.lucene.cfg.LuceneIndexSettings;
 import org.hibernate.search.integrationtest.backend.lucene.testsupport.util.LuceneTckBackendSetupStrategy;
 import org.hibernate.search.integrationtest.backend.tck.sharding.AbstractShardingRoutingKeyIT;
 import org.hibernate.search.integrationtest.backend.tck.testsupport.util.TckBackendSetupStrategy;
 import org.hibernate.search.util.common.impl.CollectionHelper;
+import org.hibernate.search.util.impl.integrationtest.mapper.stub.StubMappingScope;
+
+import org.junit.Test;
+
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.MatchAllDocsQuery;
+import org.apache.lucene.search.TopDocs;
 
 /**
  * A basic test for explicit sharding with explicit routing keys.
@@ -36,4 +48,22 @@ public class ShardingExplicitIT extends AbstractShardingRoutingKeyIT {
 		super( ignored -> explicitShardingBackendSetupStrategy( SHARD_IDS ), SHARD_IDS );
 	}
 
+	@Test
+	public void indexReaderAccessor() throws Exception {
+		StubMappingScope scope = index.createScope();
+
+		try ( IndexReader indexReader = scope.extension( LuceneExtension.get() ).openIndexReader() ) {
+			IndexSearcher searcher = new IndexSearcher( indexReader );
+			TopDocs topDocs = searcher.search( new MatchAllDocsQuery(), 1000 );
+			assertThat( topDocs.scoreDocs ).hasSize( 300 );
+		}
+
+		try ( IndexReader indexReader = scope.extension( LuceneExtension.get() ).openIndexReader(
+				Collections.singleton( SHARD_ID_1 )
+		) ) {
+			IndexSearcher searcher = new IndexSearcher( indexReader );
+			TopDocs topDocs = searcher.search( new MatchAllDocsQuery(), 1000 );
+			assertThat( topDocs.scoreDocs ).hasSize( 100 );
+		}
+	}
 }
