136d8aea575259f05bcf6c616d249945ae1817a9
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2021-06-07 18:02:10 +0200

    HSEARCH-4228 Use id as a second key to order events
    This will mitigate (but not completely solve) the issue of having a low moment time resolution

--- integrationtest/mapper/orm/src/test/java/org/hibernate/search/integrationtest/mapper/orm/automaticindexing/strategy/outbox/FilteringOutboxEventFinder.java
+++ integrationtest/mapper/orm/src/test/java/org/hibernate/search/integrationtest/mapper/orm/automaticindexing/strategy/outbox/FilteringOutboxEventFinder.java
@@ -37,7 +37,7 @@ class FilteringOutboxEventFinder implements OutboxEventFinder {
 		}
 
 		Query<OutboxEvent> query = session.createQuery(
-				"select e from OutboxEvent e where e.id in :ids order by e.moment", OutboxEvent.class );
+				"select e from OutboxEvent e where e.id in :ids order by e.moment, e.id", OutboxEvent.class );
 		query.setMaxResults( maxResults );
 		query.setParameter( "ids", allowedIds );
 		List<OutboxEvent> returned = query.list();
@@ -73,7 +73,7 @@ class FilteringOutboxEventFinder implements OutboxEventFinder {
 	public List<OutboxEvent> findOutboxEventsNoFilter(Session session) {
 		checkFiltering();
 		Query<OutboxEvent> query = session.createQuery(
-				"select e from OutboxEvent e order by e.moment", OutboxEvent.class );
+				"select e from OutboxEvent e order by e.moment, e.id", OutboxEvent.class );
 		return query.list();
 	}
 
@@ -87,7 +87,7 @@ class FilteringOutboxEventFinder implements OutboxEventFinder {
 	public List<Long> findOutboxEventIdsNoFilter(Session session) {
 		checkFiltering();
 		Query<Long> query = session.createQuery(
-				"select e.id from OutboxEvent e order by e.moment", Long.class );
+				"select e.id from OutboxEvent e order by e.moment, e.id", Long.class );
 		return query.list();
 	}
 
