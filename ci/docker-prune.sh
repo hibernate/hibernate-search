b0c8e91cdfe370cc3b50d6081bc0ba2c1ab379f7
Yoann Rodière <yoann@hibernate.org>
2021-08-06 10:15:16 +0200

    HSEARCH-4285 Backport HSEARCH-4281 Only prune container images unused for more than 24h
    So as not to trigger an image pull for every single build.

--- ci/docker-prune.sh
+++ ci/docker-prune.sh
@@ -2,6 +2,6 @@
 # Prune container-related resources. Mostly useful on CI to free up resources.
 
 docker container prune -f || true
-docker image prune -f || true
+docker image prune -f --filter "until=24h" || true
 docker network prune -f || true
 docker volume prune -f || true
