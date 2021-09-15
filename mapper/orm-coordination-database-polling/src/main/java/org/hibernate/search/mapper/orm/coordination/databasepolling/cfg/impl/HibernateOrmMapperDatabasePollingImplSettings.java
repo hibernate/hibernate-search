c3fe4848a53c22c89fa5b18bd52e8bcdb4905ec6
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2021-09-15 17:26:10 +0200

    HSEARCH-4209 Move mapper settings used by tests

--- mapper/orm-coordination-database-polling/src/main/java/org/hibernate/search/mapper/orm/coordination/databasepolling/cfg/impl/HibernateOrmMapperDatabasePollingImplSettings.java
+++ mapper/orm-coordination-database-polling/src/main/java/org/hibernate/search/mapper/orm/coordination/databasepolling/cfg/impl/HibernateOrmMapperDatabasePollingImplSettings.java
@@ -4,14 +4,14 @@
  * License: GNU Lesser General Public License (LGPL), version 2.1 or later
  * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
  */
-package org.hibernate.search.mapper.orm.cfg.impl;
+package org.hibernate.search.mapper.orm.coordination.databasepolling.cfg.impl;
 
 /**
  * Implementation-related settings, used for testing only.
  */
-public final class HibernateOrmMapperImplSettings {
+public final class HibernateOrmMapperDatabasePollingImplSettings {
 
-	private HibernateOrmMapperImplSettings() {
+	private HibernateOrmMapperDatabasePollingImplSettings() {
 	}
 
 	public static final class CoordinationRadicals {
