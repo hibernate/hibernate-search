a64ab022efb68a27e4ce5491e3c645b8e68db5f8
Yoann Rodière <yoann@hibernate.org>
2021-09-30 11:08:09 +0200

    HSEARCH-4331 Guard identifier bridge tests against implicit, unavoidable java.sql.Date => java.util.Date conversions

--- integrationtest/mapper/pojo-base/src/test/java/org/hibernate/search/integrationtest/mapper/pojo/mapping/definition/DocumentIdDefaultBridgeBaseIT.java
+++ integrationtest/mapper/pojo-base/src/test/java/org/hibernate/search/integrationtest/mapper/pojo/mapping/definition/DocumentIdDefaultBridgeBaseIT.java
@@ -14,7 +14,9 @@ import static org.junit.Assume.assumeTrue;
 import java.lang.invoke.MethodHandles;
 import java.util.Collections;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.hibernate.search.engine.backend.types.converter.FromDocumentValueConverter;
 import org.hibernate.search.engine.backend.types.converter.ToDocumentValueConverter;
@@ -119,7 +121,7 @@ public class DocumentIdDefaultBridgeBaseIT<I> {
 	@Test
 	public void projection_entityReference() {
 		try ( SearchSession session = mapping.createSession() ) {
-			Iterator<I> entityIdentifierIterator = typeDescriptor.values().entityModelValues.iterator();
+			Iterator<I> entityIdentifierIterator = getProjectionValues().iterator();
 			for ( String documentIdentifierValue : typeDescriptor.values().documentIdentifierValues ) {
 				I entityIdentifierValue = entityIdentifierIterator.next();
 				backendMock.expectSearchReferences(
@@ -153,7 +155,7 @@ public class DocumentIdDefaultBridgeBaseIT<I> {
 	@Test
 	public void projection_id() {
 		try ( SearchSession session = mapping.createSession() ) {
-			Iterator<I> entityIdentifierIterator = typeDescriptor.values().entityModelValues.iterator();
+			Iterator<I> entityIdentifierIterator = getProjectionValues().iterator();
 			for ( String documentIdentifierValue : typeDescriptor.values().documentIdentifierValues ) {
 				I entityIdentifierValue = entityIdentifierIterator.next();
 				backendMock.expectSearchIds(
@@ -271,6 +273,12 @@ public class DocumentIdDefaultBridgeBaseIT<I> {
 						.isEqualTo( projectionValue );
 			}
 		}
+	}
+
+	private List<I> getProjectionValues() {
+		return typeDescriptor.values().entityModelValues.stream()
+				.map( typeDescriptor::toProjectedValue )
+				.collect( Collectors.toList() );
 	}
 
 	/**
