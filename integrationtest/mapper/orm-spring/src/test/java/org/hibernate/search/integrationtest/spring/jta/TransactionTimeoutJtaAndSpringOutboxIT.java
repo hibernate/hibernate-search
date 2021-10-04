b1192b2bc4c63fa5ca78461969f9e66570864244
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2021-10-04 20:39:23 +0200

    HSEARCH-4282 Test outbox transaction timeout

---
+++ integrationtest/mapper/orm-spring/src/test/java/org/hibernate/search/integrationtest/spring/jta/TransactionTimeoutJtaAndSpringOutboxIT.java
@@ -0,0 +1,68 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.integrationtest.spring.jta;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+
+import javax.persistence.EntityManagerFactory;
+
+import org.hibernate.engine.spi.SessionFactoryImplementor;
+import org.hibernate.resource.transaction.spi.TransactionCoordinatorBuilder;
+import org.hibernate.search.integrationtest.spring.jta.dao.SnertDAO;
+import org.hibernate.search.integrationtest.spring.jta.entity.Snert;
+import org.hibernate.search.integrationtest.spring.jta.listener.TimeoutFailureCollector;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = JtaAndSpringOutboxApplicationConfiguration.class)
+@ActiveProfiles({ "jta", "outbox", "transaction-timeout" })
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public class TransactionTimeoutJtaAndSpringOutboxIT {
+
+	@Autowired
+	private SnertDAO snertDAO;
+
+	@Autowired
+	private EntityManagerFactory entityManagerFactory;
+
+	@Before
+	public void checkJta() {
+		assertThat( entityManagerFactory.unwrap( SessionFactoryImplementor.class )
+				.getServiceRegistry().getService( TransactionCoordinatorBuilder.class ) )
+				.returns( true, TransactionCoordinatorBuilder::isJta );
+
+		TimeoutFailureCollector.EXCEPTIONS.clear();
+	}
+
+	@Test
+	public void test() throws Exception {
+		Snert snert = new Snert();
+		snert.setId( 1L );
+		snert.setName( "dave" );
+		snert.setNickname( "dude" );
+		snert.setAge( 99 );
+		snert.setCool( Boolean.TRUE );
+		snertDAO.persist( snert );
+
+		await( "Waiting for indexing assertions" )
+				.untilAsserted( () -> assertThat( TimeoutFailureCollector.EXCEPTIONS ).isNotEmpty() );
+
+		Throwable exception = TimeoutFailureCollector.EXCEPTIONS.iterator().next();
+		Throwable cause = exception.getCause().getCause().getCause();
+		assertThat( cause ).hasMessageContaining( "transaction timed out" );
+	}
+}
---
+++ integrationtest/mapper/orm-spring/src/test/java/org/hibernate/search/integrationtest/spring/jta/listener/TimeoutSessionEventListener.java
@@ -0,0 +1,31 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.integrationtest.spring.jta.listener;
+
+import org.hibernate.BaseSessionEventListener;
+import org.hibernate.search.mapper.orm.coordination.databasepolling.impl.DatabasePollingCooordinationStrategy;
+
+public class TimeoutSessionEventListener extends BaseSessionEventListener {
+
+	@Override
+	public void jdbcPrepareStatementEnd() {
+		String name = Thread.currentThread().getName();
+		if ( !name.contains( DatabasePollingCooordinationStrategy.PROCESSOR_NAME_PREFIX ) ) {
+			return;
+		}
+
+		try {
+			// this should provoke a timeout in TransactionTimeoutJtaAndSpringOutboxIT test
+			Thread.sleep( 1001 );
+		}
+		catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			throw new RuntimeException( e );
+		}
+	}
+
+}
