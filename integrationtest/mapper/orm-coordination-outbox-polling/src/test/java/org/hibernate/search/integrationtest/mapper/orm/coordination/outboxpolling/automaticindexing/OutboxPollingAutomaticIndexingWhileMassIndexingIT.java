47296dec3e4675a42f5e4f4bf521315233c69334
Yoann Rodière <yoann@hibernate.org>
2021-11-26 11:57:12 +0100

    HSEARCH-4316 Test automatic indexing with multi-tenancy enabled

--- integrationtest/mapper/orm-coordination-outbox-polling/src/test/java/org/hibernate/search/integrationtest/mapper/orm/coordination/outboxpolling/automaticindexing/OutboxPollingAutomaticIndexingWhileMassIndexingIT.java
+++ integrationtest/mapper/orm-coordination-outbox-polling/src/test/java/org/hibernate/search/integrationtest/mapper/orm/coordination/outboxpolling/automaticindexing/OutboxPollingAutomaticIndexingWhileMassIndexingIT.java
@@ -38,7 +38,7 @@ public class OutboxPollingAutomaticIndexingWhileMassIndexingIT {
 			.coordinationStrategy( CoordinationStrategyExpectations.outboxPollingAndMassIndexing() );
 
 	@Test
-	public void test() throws InterruptedException {
+	public void singleTenant() throws InterruptedException {
 		backendMock.expectSchema( IndexedEntity.NAME, b -> b
 				.field( "text", String.class, f -> f.analyzerName( AnalyzerNames.DEFAULT ) ) );
 
@@ -99,6 +99,100 @@ public class OutboxPollingAutomaticIndexingWhileMassIndexingIT {
 
 		Search.mapping( sessionFactory )
 				.scope( Object.class ).massIndexer()
+				.startAndWait();
+
+		backendMock.verifyExpectationsMet();
+	}
+
+	@Test
+	public void multiTenant() throws InterruptedException {
+		String tenant1Id = "tenant1";
+		String tenant2Id = "tenant2";
+
+		backendMock.expectSchema( IndexedEntity.NAME, b -> b
+				.field( "text", String.class, f -> f.analyzerName( AnalyzerNames.DEFAULT ) ) );
+
+		SessionFactory sessionFactory = setupHelper.start()
+				.tenants( tenant1Id, tenant2Id )
+				.setup( IndexedEntity.class );
+
+		with( sessionFactory, tenant1Id ).runInTransaction( session -> {
+			IndexedEntity entity = new IndexedEntity( 1, "initial value for tenant 1" );
+			session.save( entity );
+
+			backendMock.expectWorks( IndexedEntity.NAME, tenant1Id )
+					.addOrUpdate( String.valueOf( 1 ), b -> b
+							.field( "text", "initial value for tenant 1" ) );
+		} );
+
+		with( sessionFactory, tenant2Id ).runInTransaction( session -> {
+			IndexedEntity entity = new IndexedEntity( 1, "initial value for tenant 2" );
+			session.save( entity );
+
+			backendMock.expectWorks( IndexedEntity.NAME, tenant2Id )
+					.addOrUpdate( String.valueOf( 1 ), b -> b
+							.field( "text", "initial value for tenant 2" ) );
+		} );
+
+		// Wait for the initial indexing to be over.
+		backendMock.verifyExpectationsMet();
+
+		// Before loading entities, we expect mass indexing to perform a few operations
+		backendMock.expectIndexScaleWorks( IndexedEntity.NAME, tenant1Id )
+				.purge()
+				.mergeSegments();
+
+		// Upon loading the entity for mass indexing:
+		IndexedEntity.getTextConcurrentOperation.set( () -> {
+			// 1. We make sure this operation doesn't get executed multiple times.
+			IndexedEntity.getTextConcurrentOperation.set( () -> { } );
+			// 2. We simulate a concurrent transaction that updates another entity in a different tenant.
+			with( sessionFactory, tenant2Id ).runInTransaction( session -> {
+				IndexedEntity entity = session.get( IndexedEntity.class, 1 );
+				entity.setText( "updated value for tenant 2" );
+				backendMock.expectWorks( IndexedEntity.NAME, tenant2Id )
+						.addOrUpdate( String.valueOf( 1 ), b -> b
+								.field( "text", "updated value for tenant 2" ) );
+			} );
+			// 3. We expect the resulting event to be processed concurrently,
+			// because mass indexing is NOT in progress for this tenant.
+			backendMock.verifyExpectationsMet();
+			// 4. We simulate a concurrent transaction that updates the entity being mass-indexed, in the same tenant.
+			with( sessionFactory, tenant1Id ).runInTransaction( session -> {
+				IndexedEntity entity = session.get( IndexedEntity.class, 1 );
+				entity.setText( "updated value for tenant 1" );
+			} );
+			// 5. We give the event processor some time to process the change
+			// (it shouldn't process it, since mass indexing is in progress).
+			try {
+				Thread.sleep( 1000 );
+			}
+			catch (InterruptedException e) {
+				throw new RuntimeException( e );
+			}
+			// 6. We expect nothing more to be indexed at this point,
+			// because the event processor for tenant 1 should be suspended.
+			backendMock.verifyExpectationsMet();
+
+			// Later, we expect the mass indexer to reindex the entity it is currently loading, with the initial value...
+			backendMock.expectWorks( IndexedEntity.NAME, tenant1Id, DocumentCommitStrategy.NONE, DocumentRefreshStrategy.NONE )
+					.add( String.valueOf( 1 ), b -> b
+							.field( "text", "initial value for tenant 1" ) );
+
+			// Then we expect the mass indexer to perform finishing operations...
+			backendMock.expectIndexScaleWorks( IndexedEntity.NAME, tenant1Id )
+					.flush()
+					.refresh();
+
+			// ... and ONLY AFTER THAT,
+			// we expect the event processor to reindex the same entity with the updated value.
+			backendMock.expectWorks( IndexedEntity.NAME, tenant1Id )
+					.addOrUpdate( String.valueOf( 1 ), b -> b
+							.field( "text", "updated value for tenant 1" ) );
+		} );
+
+		Search.mapping( sessionFactory )
+				.scope( Object.class ).massIndexer( tenant1Id )
 				.startAndWait();
 
 		backendMock.verifyExpectationsMet();
