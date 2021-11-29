49fb9e7bba6bc906bd7e5e102c090bb4a5c96647
Yoann Rodière <yoann@hibernate.org>
2021-11-29 16:08:05 +0100

    HSEARCH-4316 Take into account HBM2DDL settings in MultitenancyTestHelper
    So that we can create multiple session factories with multitenancy
    enabled, with HBM2DDL enabled in one session factory only in order to
    avoid conflicts.

--- util/internal/integrationtest/mapper/orm/src/main/java/org/hibernate/search/util/impl/integrationtest/mapper/orm/multitenancy/impl/MultitenancyTestHelper.java
+++ util/internal/integrationtest/mapper/orm/src/main/java/org/hibernate/search/util/impl/integrationtest/mapper/orm/multitenancy/impl/MultitenancyTestHelper.java
@@ -9,20 +9,10 @@ package org.hibernate.search.util.impl.integrationtest.mapper.orm.multitenancy.i
 import static org.junit.Assume.assumeTrue;
 
 import org.hibernate.MultiTenancyStrategy;
-import org.hibernate.boot.spi.MetadataImplementor;
 import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.dialect.Dialect;
 import org.hibernate.dialect.H2Dialect;
-import org.hibernate.engine.jdbc.connections.spi.ConnectionProvider;
-import org.hibernate.engine.jdbc.connections.spi.MultiTenantConnectionProvider;
 import org.hibernate.search.util.impl.integrationtest.mapper.orm.SimpleSessionFactoryBuilder;
-import org.hibernate.service.spi.ServiceRegistryImplementor;
-import org.hibernate.tool.schema.internal.HibernateSchemaManagementTool;
-import org.hibernate.tool.schema.internal.SchemaCreatorImpl;
-import org.hibernate.tool.schema.internal.SchemaDropperImpl;
-import org.hibernate.tool.schema.internal.exec.GenerationTargetToDatabase;
-
-import org.hibernate.testing.boot.JdbcConnectionAccessImpl;
 
 /**
  * Utility to help setting up a test SessionFactory which uses multi-tenancy based
@@ -45,7 +35,10 @@ public class MultitenancyTestHelper {
 	}
 
 	private void attachTo(SimpleSessionFactoryBuilder builder) {
-		builder.setProperty( org.hibernate.cfg.Environment.HBM2DDL_AUTO, "none" );
+		// Force our own schema management tool which creates the schema for all tenants.
+		builder.onServiceRegistryBuilder( srb -> srb.addInitiator(
+				new MultitenancyTestHelperSchemaManagementTool.Initiator( tenantIds ) ) );
+
 		builder.setProperty( AvailableSettings.MULTI_TENANT, MultiTenancyStrategy.DATABASE.name() );
 		builder.setProperty( AvailableSettings.MULTI_TENANT_CONNECTION_PROVIDER,
 				new H2LazyMultiTenantConnectionProvider( tenantIds ) );
@@ -57,41 +50,6 @@ public class MultitenancyTestHelper {
 			assumeTrue( "This test relies on multi-tenancy, which can currently only be set up with H2",
 					dialect instanceof H2Dialect );
 		} );
-
-		builder.onMetadata( this::exportSchema );
-	}
-
-	private void exportSchema(MetadataImplementor metadata) {
-		ServiceRegistryImplementor serviceRegistry = (ServiceRegistryImplementor) metadata.getMetadataBuildingOptions()
-				.getServiceRegistry();
-		HibernateSchemaManagementTool tool = new HibernateSchemaManagementTool();
-		tool.injectServices( serviceRegistry );
-		final GenerationTargetToDatabase[] databaseTargets = createSchemaTargets( serviceRegistry );
-		new SchemaDropperImpl( serviceRegistry ).doDrop(
-				metadata,
-				true,
-				databaseTargets
-		);
-		new SchemaCreatorImpl( serviceRegistry ).doCreation(
-				metadata,
-				true,
-				databaseTargets
-		);
-	}
-
-	private GenerationTargetToDatabase[] createSchemaTargets(ServiceRegistryImplementor serviceRegistry) {
-		H2LazyMultiTenantConnectionProvider multiTenantConnectionProvider = (H2LazyMultiTenantConnectionProvider)
-				serviceRegistry.getService( MultiTenantConnectionProvider.class );
-		GenerationTargetToDatabase[] targets = new GenerationTargetToDatabase[tenantIds.length];
-		int index = 0;
-		for ( String tenantId : tenantIds ) {
-			ConnectionProvider connectionProvider = multiTenantConnectionProvider.selectConnectionProvider( tenantId );
-			targets[index] = new GenerationTargetToDatabase(
-						new DdlTransactionIsolatorTestingImpl( serviceRegistry,
-								new JdbcConnectionAccessImpl( connectionProvider ) ) );
-			index++;
-		}
-		return targets;
 	}
 
 }
