4b762c2e899aec773d7867106058b1f939a422bf
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2022-01-03 15:42:35 +0100

    HSEARCH-4283 Apply suggestions from code review
    Co-authored-by: Yoann Rodière <yoann@hibernate.org>

--- mapper/orm-coordination-outbox-polling/src/main/java/org/hibernate/search/mapper/orm/coordination/outboxpolling/OutboxPollingExtension.java
+++ mapper/orm-coordination-outbox-polling/src/main/java/org/hibernate/search/mapper/orm/coordination/outboxpolling/OutboxPollingExtension.java
@@ -15,8 +15,19 @@ import org.hibernate.search.mapper.orm.coordination.outboxpolling.mapping.Outbox
 import org.hibernate.search.mapper.orm.mapping.SearchMapping;
 import org.hibernate.search.mapper.orm.mapping.SearchMappingExtension;
 import org.hibernate.search.mapper.orm.mapping.spi.CoordinationStrategyContext;
+import org.hibernate.search.util.common.annotation.Incubating;
 import org.hibernate.search.util.common.logging.impl.LoggerFactory;
 
+/**
+ * An extension to the search mapping, giving access to features specific to the outbox polling coordination strategy.
+ * <p>
+ * <strong>WARNING:</strong> while this type is API, because instances should be manipulated by users,
+ * all of its methods are considered SPIs and therefore should never be called directly by users.
+ * In short, users are only expected to get instances of this type from an API and pass it to another API.
+ *
+ * @see #get()
+ */
+@Incubating
 public class OutboxPollingExtension implements SearchMappingExtension<OutboxPollingSearchMapping> {
 
 	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
