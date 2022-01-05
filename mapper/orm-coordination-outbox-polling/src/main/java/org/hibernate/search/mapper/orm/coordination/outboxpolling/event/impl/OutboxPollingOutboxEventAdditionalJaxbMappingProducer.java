932a5165c5ee858af5371914a9af7f49aee4ee7e
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2022-01-05 21:05:56 +0100

    HSEARCH-4425 Mark non-nullable properties in OutboxEvent definition

--- mapper/orm-coordination-outbox-polling/src/main/java/org/hibernate/search/mapper/orm/coordination/outboxpolling/event/impl/OutboxPollingOutboxEventAdditionalJaxbMappingProducer.java
+++ mapper/orm-coordination-outbox-polling/src/main/java/org/hibernate/search/mapper/orm/coordination/outboxpolling/event/impl/OutboxPollingOutboxEventAdditionalJaxbMappingProducer.java
@@ -58,12 +58,12 @@ public final class OutboxPollingOutboxEventAdditionalJaxbMappingProducer
 			"                <param name=\"increment_size\">1</param>\n" +
 			"            </generator>\n" +
 			"        </id>\n" +
-			"        <property name=\"entityName\" type=\"string\" length=\"256\" />\n" +
-			"        <property name=\"entityId\" type=\"string\" length=\"256\" />\n" +
-			"        <property name=\"entityIdHash\" type=\"integer\" index=\"entityIdHash\" />\n" +
-			"        <property name=\"payload\" type=\"materialized_blob\" />\n" +
-			"        <property name=\"retries\" type=\"integer\" />\n" +
-			"        <property name=\"processAfter\" type=\"Instant\" index=\"processAfter\" />\n" +
+			"        <property name=\"entityName\" type=\"string\" length=\"256\" nullable=\"false\" />\n" +
+			"        <property name=\"entityId\" type=\"string\" length=\"256\" nullable=\"false\" />\n" +
+			"        <property name=\"entityIdHash\" type=\"integer\" index=\"entityIdHash\" nullable=\"false\" />\n" +
+			"        <property name=\"payload\" type=\"materialized_blob\" nullable=\"false\" />\n" +
+			"        <property name=\"retries\" type=\"integer\" nullable=\"false\" />\n" +
+			"        <property name=\"processAfter\" type=\"Instant\" index=\"processAfter\" nullable=\"true\" />\n" +
 			"        <property name=\"status\" index=\"status\" nullable=\"false\" />\n" +
 			"    </class>\n" +
 			"</hibernate-mapping>\n";
