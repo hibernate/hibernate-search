ff595400e2eb0e33b21503d60cefd674c53562d9
Yoann Rodière <yoann@hibernate.org>
2022-01-17 17:01:02 +0100

    HSEARCH-4450 Expose lists of outbox-polling classes requiring reflection through an SPI

--- util/internal/test/common/src/main/java/org/hibernate/search/util/impl/test/jar/JandexUtils.java
+++ util/internal/test/common/src/main/java/org/hibernate/search/util/impl/test/jar/JandexUtils.java
@@ -17,7 +17,10 @@ import org.jboss.jandex.AnnotationInstance;
 import org.jboss.jandex.AnnotationTarget;
 import org.jboss.jandex.ClassInfo;
 import org.jboss.jandex.DotName;
+import org.jboss.jandex.FieldInfo;
 import org.jboss.jandex.Index;
+import org.jboss.jandex.MethodInfo;
+import org.jboss.jandex.Type;
 
 public final class JandexUtils {
 	private static final DotName RETENTION = DotName.createSimple( Retention.class.getName() );
@@ -83,6 +86,80 @@ public final class JandexUtils {
 		}
 		for ( ClassInfo subclass : index.getAllKnownSubclasses( className ) ) {
 			collectClassHierarchiesRecursively( index, subclass.name(), classes );
+		}
+	}
+
+	private static Set<DotName> collectModelClassesRecursively(Index index, Set<DotName> initialClasses) {
+		Set<DotName> classes = new HashSet<>();
+		for ( DotName initialClass : initialClasses ) {
+			collectModelClassesRecursively( index, initialClass, classes );
+		}
+		return classes;
+	}
+
+	private static void collectModelClassesRecursively(Index index, DotName className, Set<DotName> classes) {
+		if ( className.toString().startsWith( "java." ) ) {
+			return;
+		}
+		if ( classes.contains( className ) ) {
+			return;
+		}
+		ClassInfo clazz = index.getClassByName( className );
+		if ( clazz == null ) {
+			return;
+		}
+		classes.add( className );
+		collectModelClassesRecursively( index, clazz.superName(), classes );
+		for ( DotName interfaceName : clazz.interfaceNames() ) {
+			collectModelClassesRecursively( index, interfaceName, classes );
+		}
+		for ( ClassInfo subclass : index.getAllKnownSubclasses( className ) ) {
+			collectModelClassesRecursively( index, subclass.name(), classes );
+		}
+
+		for ( FieldInfo field : clazz.fields() ) {
+			collectModelClassesRecursively( index, field.type(), classes );
+		}
+		for ( FieldInfo field : clazz.fields() ) {
+			collectModelClassesRecursively( index, field.type(), classes );
+		}
+		for ( MethodInfo methodInfo : clazz.methods() ) {
+			if ( !methodInfo.parameters().isEmpty() ) {
+				// Definitely not a getter, just skip.
+				continue;
+			}
+			collectModelClassesRecursively( index, methodInfo.returnType(), classes );
+		}
+	}
+
+	private static void collectModelClassesRecursively(Index index, Type type, Set<DotName> classes) {
+		switch ( type.kind() ) {
+			case CLASS:
+				collectModelClassesRecursively( index, type.name(), classes );
+				break;
+			case ARRAY:
+				collectModelClassesRecursively( index, type.asArrayType().component(), classes );
+				break;
+			case TYPE_VARIABLE:
+				for ( Type bound : type.asTypeVariable().bounds() ) {
+					collectModelClassesRecursively( index, bound, classes );
+				}
+				break;
+			case WILDCARD_TYPE:
+				collectModelClassesRecursively( index, type.asWildcardType().extendsBound(), classes );
+				collectModelClassesRecursively( index, type.asWildcardType().superBound(), classes );
+				break;
+			case PARAMETERIZED_TYPE:
+				collectModelClassesRecursively( index, type.name(), classes );
+				for ( Type argument : type.asParameterizedType().arguments() ) {
+					collectModelClassesRecursively( index, argument, classes );
+				}
+				break;
+			case PRIMITIVE:
+			case VOID:
+			case UNRESOLVED_TYPE_VARIABLE:
+				// Ignore
+				break;
 		}
 	}
 }
