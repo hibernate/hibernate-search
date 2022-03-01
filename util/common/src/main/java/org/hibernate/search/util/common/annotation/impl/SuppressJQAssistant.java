b366f44fddb8161ef33b2fb474c6940ec7dda7f2
Yoann Rodière <yoann@hibernate.org>
2022-03-01 08:54:03 +0100

    HSEARCH-4494 Allow suppression of JQAssistant via annotations for some rules

---
+++ util/common/src/main/java/org/hibernate/search/util/common/annotation/impl/SuppressJQAssistant.java
@@ -0,0 +1,29 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.util.common.annotation.impl;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.CLASS;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * Internal annotation to suppress some JQAssistant rules.
+ * <p>
+ * Note that rules must be specifically designed to take the annotation into account;
+ * not all rules are.
+ */
+@Documented
+@Target(TYPE)
+@Retention(CLASS)
+public @interface SuppressJQAssistant {
+
+	String reason();
+
+}
