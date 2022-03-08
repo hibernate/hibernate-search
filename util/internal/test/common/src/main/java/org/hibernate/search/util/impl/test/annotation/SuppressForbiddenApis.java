a2a291644e1bb1d96460073b72c6be45790a915c
Yoann Rodière <yoann@hibernate.org>
2022-03-08 11:11:47 +0100

    HSEARCH-4507 Allow suppression JQAssistant/ForbiddenApis in util/internal/test/common
    This module cannot rely on the suppression annotations from  util/internal/common,
    because that would introduce a cyclic dependency.

---
+++ util/internal/test/common/src/main/java/org/hibernate/search/util/impl/test/annotation/SuppressForbiddenApis.java
@@ -0,0 +1,28 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.util.impl.test.annotation;
+
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.CLASS;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * Internal annotation to suppress forbidden-apis warnings/errors.
+ */
+@Documented
+@Target({ TYPE, METHOD, CONSTRUCTOR })
+@Retention(CLASS)
+public @interface SuppressForbiddenApis {
+
+	String reason();
+
+}
