0a1b02df29deaa4b2318af09d14e11a78dfef7fd
Yoann Rodière <yoann@hibernate.org>
2022-06-23 18:36:41 +0200

    HSEARCH-4319 More clearly split documentation of the ORM mapper and of (relatively) mapper-agnostic themes

---
+++ documentation/src/test/java/org/hibernate/search/documentation/mapper/orm/gettingstarted/withhsearch/customanalysis/Book.java
@@ -0,0 +1,84 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.documentation.mapper.orm.gettingstarted.withhsearch.customanalysis;
+
+// tag::include[]
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.FullTextField;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.GenericField;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.IndexedEmbedded;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.KeywordField;
+
+@Entity
+@Indexed
+public class Book {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	@FullTextField(analyzer = "english") // <1>
+	private String title;
+
+	@KeywordField
+	private String isbn;
+
+	@GenericField
+	private int pageCount;
+
+	@ManyToMany
+	@IndexedEmbedded
+	private Set<Author> authors = new HashSet<>();
+
+	public Book() {
+	}
+
+	// Getters and setters
+	// ...
+
+	// tag::getters-setters[]
+	public Integer getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public int getPageCount() {
+		return pageCount;
+	}
+
+	public void setPageCount(int pageCount) {
+		this.pageCount = pageCount;
+	}
+
+	public Set<Author> getAuthors() {
+		return authors;
+	}
+	// end::getters-setters[]
+}
+// end::include[]
