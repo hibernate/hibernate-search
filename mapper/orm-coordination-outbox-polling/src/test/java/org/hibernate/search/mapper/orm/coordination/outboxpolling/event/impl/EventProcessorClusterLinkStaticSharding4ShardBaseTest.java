145f9f4f7f685d0a7dd165eef82c098ed6191687
Yoann Rodière <yoann@hibernate.org>
2022-07-22 14:04:49 +0200

    HSEARCH-4647 Slightly simplify the mechanics of retries in outbox polling coordination

--- mapper/orm-coordination-outbox-polling/src/test/java/org/hibernate/search/mapper/orm/coordination/outboxpolling/event/impl/EventProcessorClusterLinkStaticSharding4ShardBaseTest.java
+++ mapper/orm-coordination-outbox-polling/src/test/java/org/hibernate/search/mapper/orm/coordination/outboxpolling/event/impl/EventProcessorClusterLinkStaticSharding4ShardBaseTest.java
@@ -75,7 +75,7 @@ public class EventProcessorClusterLinkStaticSharding4ShardBaseTest {
 
 		@Override
 		protected EventProcessorClusterLinkPulseExpectations onNoOtherAgents() {
-			return expectSuspendedAndPulseASAP();
+			return expectSuspendedAndPulseAfterDelay( POLLING_INTERVAL );
 		}
 
 		@Override
@@ -93,7 +93,7 @@ public class EventProcessorClusterLinkStaticSharding4ShardBaseTest {
 
 		@Override
 		protected EventProcessorClusterLinkPulseExpectations onNoOtherAgents() {
-			return expectSuspendedAndPulseASAP();
+			return expectSuspendedAndPulseAfterDelay( POLLING_INTERVAL );
 		}
 
 		@Override
@@ -112,7 +112,7 @@ public class EventProcessorClusterLinkStaticSharding4ShardBaseTest {
 
 		@Override
 		protected EventProcessorClusterLinkPulseExpectations onNoOtherAgents() {
-			return expectSuspendedAndPulseASAP();
+			return expectSuspendedAndPulseAfterDelay( POLLING_INTERVAL );
 		}
 
 		@Override
@@ -131,7 +131,7 @@ public class EventProcessorClusterLinkStaticSharding4ShardBaseTest {
 
 		@Override
 		protected EventProcessorClusterLinkPulseExpectations onNoOtherAgents() {
-			return expectSuspendedAndPulseASAP();
+			return expectSuspendedAndPulseAfterDelay( POLLING_INTERVAL );
 		}
 
 		@Override
