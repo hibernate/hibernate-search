256051d2fc227c3b7b887709d6af5f8e0274aea1
Yoann Rodière <yoann@hibernate.org>
2022-07-26 15:23:53 +0200

    HSEARCH-4654 Test for deadlocks in RootFailureCollector

--- engine/src/test/java/org/hibernate/search/engine/reporting/spi/RootFailureCollectorTest.java
+++ engine/src/test/java/org/hibernate/search/engine/reporting/spi/RootFailureCollectorTest.java
@@ -8,6 +8,14 @@ package org.hibernate.search.engine.reporting.spi;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.awaitility.Awaitility.await;
+
+import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.junit.Test;
 
@@ -35,6 +43,51 @@ public class RootFailureCollectorTest {
 				// Check that we mention that some failures are not being reported
 				.hasMessageContainingAll( "Hibernate Search encountered " + ( RootFailureCollector.FAILURE_LIMIT + 10 )
 						+ " failures during RootName",
+						"Only the first " + RootFailureCollector.FAILURE_LIMIT + " failures are displayed here",
+						"See the logs for extra failures" )
+				// Check that we didn't report failures after the limit was reached
+				.message().satisfies( message -> {
+					assertThat( countOccurrences( message, "Error #" ) )
+							.as( "Number of errors reported" ).isEqualTo( RootFailureCollector.FAILURE_LIMIT );
+				} );
+	}
+
+	/**
+	 * Triggers many more failures than the failure limit
+	 * in concurrent tasks that create contextual failure collectors then add a failure.
+	 * <p>
+	 * This used to lead to a deadlock, because we would:
+	 * <ul>
+	 *     <li>lock on a child to add a failure</li>
+	 *     <li>check on the root whether there are more failures than the limit</li>
+	 *     <li>lock on the root to access the children</li>
+	 *     <li>try to lock on each child one after the other,
+	 *     to render their failures and include that in the "failure limit reached" exception message</li>
+	 * </ul>
+	 * Do that concurrently from two different children, and you're likely to end up with a deadlock.
+	 */
+	@Test
+	public void failureLimit_concurrency() {
+		RootFailureCollector rootFailureCollector = new RootFailureCollector( "RootName" );
+		List<Runnable> runnables = IntStream.range( 0, RootFailureCollector.FAILURE_LIMIT + 1000 )
+				.mapToObj( i -> (Runnable) () -> {
+					ContextualFailureCollector failureCollector = rootFailureCollector.withContext(
+							EventContexts.fromType( "Type #" + i ) );
+					failureCollector.add( "Error #" + i );
+				} )
+				.collect( Collectors.toList() );
+		ForkJoinPool pool = new ForkJoinPool( 10 );
+		List<ForkJoinTask<?>> tasks = runnables.stream().map( pool::submit )
+				.collect( Collectors.toList() );
+		await().atMost( 2, TimeUnit.SECONDS ).until( pool::isQuiescent );
+		pool.shutdownNow();
+		assertThat( tasks )
+				.hasSameSizeAs( runnables )
+				.allSatisfy( task -> assertThat( task ).isDone() );
+		assertThatThrownBy( rootFailureCollector::checkNoFailure )
+				// Check that we mention that some failures are not being reported
+				.hasMessageContainingAll( "Hibernate Search encountered " + ( RootFailureCollector.FAILURE_LIMIT + 1000 )
+								+ " failures during RootName",
 						"Only the first " + RootFailureCollector.FAILURE_LIMIT + " failures are displayed here",
 						"See the logs for extra failures" )
 				// Check that we didn't report failures after the limit was reached
