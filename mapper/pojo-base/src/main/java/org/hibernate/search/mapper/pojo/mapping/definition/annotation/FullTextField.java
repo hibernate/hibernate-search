7c4bb69df42b698a3b8e918e72b557a2041c82a6
Yoann Rodière <yoann@hibernate.org>
2022-08-16 12:45:48 +0200

    HSEARCH-4551 Document that full-text fields are not aggregable

--- mapper/pojo-base/src/main/java/org/hibernate/search/mapper/pojo/mapping/definition/annotation/FullTextField.java
+++ mapper/pojo-base/src/main/java/org/hibernate/search/mapper/pojo/mapping/definition/annotation/FullTextField.java
@@ -40,7 +40,7 @@ import org.hibernate.search.mapper.pojo.mapping.definition.annotation.processing
  * </ul>
  * <p>
  * If you want to index a non-String value, use the {@link GenericField} annotation instead.
- * If you want to index a String value, but don't want the field to be analyzed, or want it to be sortable,
+ * If you want to index a String value, but don't want the field to be analyzed, or want it to be sortable or aggregable,
  * use the {@link KeywordField} annotation instead.
  */
 @Documented
