ed5c393dfd476273f0067003ae6737972d8622e6
Yoann Rodière <yoann@hibernate.org>
2022-09-07 10:01:11 +0200

    HSEARCH-4695 Handle local branches and any remote name in ci/dependency-update/perform-update.sh

--- ci/dependency-update/perform-update.sh
+++ ci/dependency-update/perform-update.sh
@@ -1,9 +1,30 @@
 #!/bin/bash -e
 
+function find_single_rev() {
+	local list
+	list="$(git rev-parse --verify --symbolic "${@}" 2>/dev/null || true)"
+	case $(echo "$list" | wc -l) in
+		0)
+			return
+			;;
+	  1)
+			echo -n $list
+			;;
+		*)
+			echo 1>&2 "Found multiple revs matching '${*}':"
+			echo 1>&2 "$list"
+			echo 1>&2 "If you know which one you want to use, try to check out that branch locally, then switch back to branch '$BRANCH_NAME' and try again."
+			echo 1>&2 "Aborting."
+			exit 1
+			;;
+	esac
+}
+
 if (( $# < 1 ))
 then
 	echo 1>&2 "Usage: $0 <name of update> <comma-separated properties to update>"
 	echo 1>&2 "See the settings() method in ci/dependency-update/Jenkinsfile for valid values."
+	echo 1>&2 "Aborting."
 	exit 1
 fi
 
@@ -14,16 +35,32 @@ shift
 
 : ${BRANCH_NAME:="$(git branch --show-current)"}
 
+WIP_BRANCH_NAME="wip/${BRANCH_NAME}/dependency-update/${DEPENDENCY_UPDATE_NAME}"
+echo "Searching for WIP branch '$WIP_BRANCH_NAME'..."
+REV_TO_MERGE="$(find_single_rev "${WIP_BRANCH_NAME}")"
+if [ -n "$REV_TO_MERGE" ]
+then
+	echo "Found local WIP branch: '$REV_TO_MERGE'"
+else
+	REV_TO_MERGE="$(find_single_rev --remotes="*/${WIP_BRANCH_NAME}")"
+	if [ -n "$REV_TO_MERGE" ]
+	then
+		echo "Found remote-tracking WIP branch: '$REV_TO_MERGE'"
+	else
+		echo "No WIP branch found (neither local nor remote-tracking)."
+	fi
+fi
+
+echo "Detaching HEAD..."
 git switch --detach
 
-BRANCH_TO_MERGE="origin/wip/${BRANCH_NAME}/dependency-update/${DEPENDENCY_UPDATE_NAME}"
-if git rev-parse --quiet --verify "${BRANCH_TO_MERGE}"
+if [ -n "$REV_TO_MERGE" ]
 then
-	echo "WIP branch found; merging."
-	git merge --no-edit "${BRANCH_TO_MERGE}"
+	echo "Merging '$REV_TO_MERGE'..."
+	git merge --no-edit "$REV_TO_MERGE"
 	echo "Merge complete."
 else
-	echo "No WIP branch found; skipping merge."
+	echo "Skipping merge."
 fi
 
 if [ -n "$PROPERTIES" ]
