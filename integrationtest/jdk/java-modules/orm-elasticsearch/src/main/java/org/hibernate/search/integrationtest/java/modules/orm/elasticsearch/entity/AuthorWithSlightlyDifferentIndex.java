e2b1571ce14321dd5018153736ff9710fadc4752
marko-bekhta <marko.prykladna@gmail.com>
2022-10-12 09:26:33 +0200

    HSEARCH-4703 Add Lucene specific module path based tests

---
+++ integrationtest/jdk/java-modules/orm-elasticsearch/src/main/java/org/hibernate/search/integrationtest/java/modules/orm/elasticsearch/entity/AuthorWithSlightlyDifferentIndex.java
@@ -0,0 +1,43 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.integrationtest.java.modules.orm.elasticsearch.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.search.engine.backend.analysis.AnalyzerNames;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.FullTextField;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
+
+// See AuthorService#triggerValidationFailure
+@Entity(name = "Author")
+@Indexed
+public class AuthorWithSlightlyDifferentIndex {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	@FullTextField(analyzer = AnalyzerNames.DEFAULT)
+	private String name;
+
+	public AuthorWithSlightlyDifferentIndex() {
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ integrationtest/jdk/java-modules/orm-lucene/src/main/java/module-info.java
@@ -0,0 +1,32 @@
+module org.hibernate.search.integrationtest.java.module.orm.lucene {
+	exports org.hibernate.search.integrationtest.java.modules.orm.lucene.service;
+	opens org.hibernate.search.integrationtest.java.modules.orm.lucene.entity to
+			org.hibernate.orm.core,
+			/*
+			 * TODO HSEARCH-4302 This part of the "opens" directive ideally should not be necessary.
+			 *  Hopefully we should be able to ask for a MethodHandles.Lookup instance from Hibernate ORM
+			 *  and take advantage of the fact the package is already open to Hibernate ORM?
+			 */
+			org.hibernate.search.mapper.orm;
+	opens org.hibernate.search.integrationtest.java.modules.orm.lucene.config to
+			org.hibernate.search.engine; // For reflective instantiation of the analysis configurer
+
+	requires java.persistence;
+	requires org.hibernate.orm.core;
+	requires org.hibernate.search.mapper.orm;
+	requires org.hibernate.search.backend.lucene;
+
+	// This should be re-exported transitively by org.hibernate.search.mapper.orm
+	// but currently isn't, because org.hibernate.search.mapper.orm
+	// is still an automatic module
+	requires org.hibernate.search.engine;
+	requires org.hibernate.search.mapper.pojo;
+
+	/*
+	 * This is necessary in order to use SessionFactory,
+	 * which extends "javax.naming.Referenceable".
+	 * Without this, compilation as a Java module fails.
+	 */
+	requires java.naming;
+
+}
