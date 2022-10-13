5cbaf1477fb49b35a5fb9a11a8d88d12ad4dd79f
marko-bekhta <marko.prykladna@gmail.com>
2022-10-13 17:13:20 +0200

    HSEARCH-4703 Create required module-info records for outbox polling module
    - add moditect config
    - add integration test module for module path tests

---
+++ integrationtest/java/modules/orm-coordination-outbox-polling-elasticsearch/src/main/java/org/hibernate/search/integrationtest/java/modules/orm/elasticsearch/coordination/outboxpolling/entity/Author.java
@@ -0,0 +1,42 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.integrationtest.java.modules.orm.elasticsearch.coordination.outboxpolling.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.search.integrationtest.java.modules.orm.elasticsearch.coordination.outboxpolling.config.MyElasticsearchAnalysisConfigurer;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.FullTextField;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
+
+@Entity
+@Indexed
+public class Author {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	@FullTextField(analyzer = MyElasticsearchAnalysisConfigurer.MY_ANALYZER)
+	private String name;
+
+	public Author() {
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
