500fa03a91c29475e962c9a80c7cc10d0a2da30e
marko-bekhta <marko.prykladna@gmail.com>
2022-10-24 15:46:02 +0200

    HSEARCH-4700 Simplify operation submitters and use BlockingQueue interface

--- engine/src/main/java/org/hibernate/search/engine/backend/work/execution/spi/OperationSubmitter.java
+++ engine/src/main/java/org/hibernate/search/engine/backend/work/execution/spi/OperationSubmitter.java
@@ -6,12 +6,8 @@
  */
 package org.hibernate.search.engine.backend.work.execution.spi;
 
-import java.lang.invoke.MethodHandles;
-import java.util.Queue;
 import java.util.concurrent.BlockingQueue;
-
-import org.hibernate.search.engine.logging.impl.Log;
-import org.hibernate.search.util.common.logging.impl.LoggerFactory;
+import java.util.concurrent.RejectedExecutionException;
 
 
 /**
@@ -26,52 +22,35 @@ public enum OperationSubmitter {
 	 */
 	BLOCKING {
 		@Override
-		public <T> void submitToQueue(Queue<T> queue, T element) {
-			if ( queue instanceof BlockingQueue ) {
-				try {
-					( (BlockingQueue<T>) queue ).put( element );
-				}
-				catch (InterruptedException e) {
-					throw log.failedToSubmitToQueue( element, e.getMessage(), e );
-				}
-			}
-			else {
-				queue.add( element );
-			}
+		public <T> void submitToQueue(BlockingQueue<T> queue, T element) throws InterruptedException {
+			queue.put( element );
 		}
 	},
 	/**
-	 * When using this submitter adding a new element will cause a {@link WorkQueueFullException} when the underlying
+	 * When using this submitter adding a new element will cause a {@link RejectedExecutionException} when the underlying
 	 * queue is a {@link java.util.concurrent.BlockingQueue} and it is at its maximum capacity.
 	 */
-	REJECTING_EXECUTION_EXCEPTION {
+	REJECTED_EXECUTION_EXCEPTION {
 		@Override
-		public <T> void submitToQueue(Queue<T> queue, T element) {
-			try {
-				queue.add( element );
-			}
-			catch (IllegalStateException e) {
-				//means we had a blocking queue, and it's full:
-				throw log.cannotAcceptMoreWork( element, e.getMessage(), e );
+		public <T> void submitToQueue(BlockingQueue<T> queue, T element) {
+			if ( !queue.offer( element ) ) {
+				throw new RejectedExecutionException();
 			}
 		}
 	};
-
-	private static final Log log = LoggerFactory.make( Log.class, MethodHandles.lookup() );
-
 
 	/**
 	 * Defines how an element will be submitted to the queue. Currently supported implementations:
 	 * <ul>
 	 *     <li>{@link #BLOCKING}</li>
-	 *     <li>{@link #REJECTING_EXECUTION_EXCEPTION}</li>
+	 *     <li>{@link #REJECTED_EXECUTION_EXCEPTION}</li>
 	 * </ul>
 	 * <p>
-	 * Depending on the implementation might throw {@link WorkQueueFullException}.
+	 * Depending on the implementation might throw {@link RejectedExecutionException}.
 	 *
 	 * @param queue
 	 * @param element
 	 */
-	public abstract <T> void submitToQueue(Queue<T> queue, T element);
+	public abstract <T> void submitToQueue(BlockingQueue<T> queue, T element) throws InterruptedException;
 
 }
