6b07beac80cf9872a49709aac819b7268c5eb021
marko-bekhta <marko.prykladna@gmail.com>
2022-10-31 10:58:53 +0100

    HSEARCH-4615 Create configuration context and configurer for Standalone POJO mapper

---
+++ integrationtest/mapper/pojo-base/src/test/java/org/hibernate/search/integrationtest/mapper/pojo/mapping/definition/PojoStandaloneSearchMappingConfigurerIT.java
@@ -0,0 +1,140 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.integrationtest.mapper.pojo.mapping.definition;
+
+import java.lang.invoke.MethodHandles;
+
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.DocumentId;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.KeywordField;
+import org.hibernate.search.mapper.pojo.standalone.cfg.StandalonePojoMapperSettings;
+import org.hibernate.search.mapper.pojo.standalone.mapping.StandalonePojoMappingConfigurationContext;
+import org.hibernate.search.mapper.pojo.standalone.mapping.StandalonePojoMappingConfigurer;
+import org.hibernate.search.util.impl.integrationtest.common.rule.BackendMock;
+import org.hibernate.search.util.impl.integrationtest.mapper.pojo.standalone.StandalonePojoMappingSetupHelper;
+import org.hibernate.search.util.impl.test.annotation.TestForIssue;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class PojoStandaloneSearchMappingConfigurerIT {
+
+	@Rule
+	public BackendMock backendMock = new BackendMock();
+
+	@Rule
+	public StandalonePojoMappingSetupHelper setupHelper = StandalonePojoMappingSetupHelper.withBackendMock( MethodHandles.lookup(), backendMock );
+
+	@Test
+	public void none() {
+		backendMock.expectSchema( IndexedEntity.INDEX, b -> b
+				.field( "annotationMapped", String.class )
+		);
+
+		setupHelper.start()
+				.setup( IndexedEntity.class );
+		backendMock.verifyExpectationsMet();
+	}
+
+	@Test
+	public void single() {
+		backendMock.expectSchema( IndexedEntity.INDEX, b -> b
+				.field( "annotationMapped", String.class )
+				.field( "nonAnnotationMapped1", String.class )
+		);
+
+		setupHelper.start()
+				.expectCustomBeans()
+				.withProperty( StandalonePojoMapperSettings.MAPPING_CONFIGURER,
+						MappingConfigurer1.class.getName() )
+				.setup( IndexedEntity.class );
+		backendMock.verifyExpectationsMet();
+	}
+
+	public static class MappingConfigurer1 implements StandalonePojoMappingConfigurer {
+		@Override
+		public void configure(StandalonePojoMappingConfigurationContext context) {
+			context.programmaticMapping().type( IndexedEntity.class )
+					.property( "nonAnnotationMapped1" )
+					.keywordField();
+		}
+	}
+
+	@Test
+	@TestForIssue(jiraKey = "HSEARCH-4594")
+	public void multiple() {
+		backendMock.expectSchema( IndexedEntity.INDEX, b -> b
+				.field( "annotationMapped", String.class )
+				.field( "nonAnnotationMapped1", String.class )
+				.field( "nonAnnotationMapped2", String.class )
+		);
+
+		setupHelper.start()
+				.expectCustomBeans()
+				.withProperty( StandalonePojoMapperSettings.MAPPING_CONFIGURER,
+						MappingConfigurer1.class.getName() + "," + MappingConfigurer2.class.getName() )
+				.setup( IndexedEntity.class );
+		backendMock.verifyExpectationsMet();
+	}
+
+	public static class MappingConfigurer2 implements StandalonePojoMappingConfigurer {
+		@Override
+		public void configure(StandalonePojoMappingConfigurationContext context) {
+			context.programmaticMapping().type( IndexedEntity.class )
+					.property( "nonAnnotationMapped2" )
+					.keywordField();
+		}
+	}
+
+	@Indexed(index = IndexedEntity.INDEX)
+	public static final class IndexedEntity {
+		public static final String INDEX = "IndexedEntity";
+
+		@DocumentId
+		private Integer id;
+
+		@KeywordField
+		private String annotationMapped;
+
+		private String nonAnnotationMapped1;
+
+		private String nonAnnotationMapped2;
+
+		public Integer getId() {
+			return id;
+		}
+
+		public void setId(Integer id) {
+			this.id = id;
+		}
+
+		public String getAnnotationMapped() {
+			return annotationMapped;
+		}
+
+		public void setAnnotationMapped(String annotationMapped) {
+			this.annotationMapped = annotationMapped;
+		}
+
+		public String getNonAnnotationMapped1() {
+			return nonAnnotationMapped1;
+		}
+
+		public void setNonAnnotationMapped1(String nonAnnotationMapped1) {
+			this.nonAnnotationMapped1 = nonAnnotationMapped1;
+		}
+
+		public String getNonAnnotationMapped2() {
+			return nonAnnotationMapped2;
+		}
+
+		public void setNonAnnotationMapped2(String nonAnnotationMapped2) {
+			this.nonAnnotationMapped2 = nonAnnotationMapped2;
+		}
+	}
+
+}
--- mapper/pojo-standalone/src/main/java/org/hibernate/search/mapper/pojo/standalone/impl/StandalonePojoMappingInitiator.java
+++ mapper/pojo-standalone/src/main/java/org/hibernate/search/mapper/pojo/standalone/impl/StandalonePojoMappingInitiator.java
@@ -6,10 +6,18 @@
  */
 package org.hibernate.search.mapper.pojo.standalone.impl;
 
+import java.util.List;
+
 import org.hibernate.search.engine.cfg.spi.ConfigurationProperty;
+import org.hibernate.search.engine.cfg.spi.OptionalConfigurationProperty;
+import org.hibernate.search.engine.environment.bean.BeanHolder;
+import org.hibernate.search.engine.environment.bean.BeanReference;
 import org.hibernate.search.engine.mapper.mapping.building.spi.MappingBuildContext;
 import org.hibernate.search.engine.mapper.mapping.building.spi.MappingConfigurationCollector;
+import org.hibernate.search.engine.tenancy.spi.TenancyMode;
 import org.hibernate.search.mapper.pojo.standalone.cfg.StandalonePojoMapperSettings;
+import org.hibernate.search.mapper.pojo.standalone.mapping.StandalonePojoMappingConfigurationContext;
+import org.hibernate.search.mapper.pojo.standalone.mapping.StandalonePojoMappingConfigurer;
 import org.hibernate.search.mapper.pojo.standalone.mapping.impl.StandalonePojoMapperDelegate;
 import org.hibernate.search.mapper.pojo.standalone.mapping.impl.StandalonePojoMappingPartialBuildState;
 import org.hibernate.search.mapper.pojo.standalone.mapping.metadata.EntityConfigurer;
@@ -21,12 +29,19 @@ import org.hibernate.search.mapper.pojo.mapping.building.spi.PojoMapperDelegate;
 import org.hibernate.search.mapper.pojo.mapping.building.spi.PojoTypeMetadataContributor;
 import org.hibernate.search.mapper.pojo.mapping.spi.AbstractPojoMappingInitiator;
 
-public class StandalonePojoMappingInitiator extends AbstractPojoMappingInitiator<StandalonePojoMappingPartialBuildState> {
+public class StandalonePojoMappingInitiator extends AbstractPojoMappingInitiator<StandalonePojoMappingPartialBuildState>
+		implements StandalonePojoMappingConfigurationContext {
 
 	private static final ConfigurationProperty<SchemaManagementStrategyName> SCHEMA_MANAGEMENT_STRATEGY
 			= ConfigurationProperty.forKey( StandalonePojoMapperSettings.Radicals.SCHEMA_MANAGEMENT_STRATEGY )
-					.as( SchemaManagementStrategyName.class, SchemaManagementStrategyName::of )
-					.withDefault( StandalonePojoMapperSettings.Defaults.SCHEMA_MANAGEMENT_STRATEGY )
+			.as( SchemaManagementStrategyName.class, SchemaManagementStrategyName::of )
+			.withDefault( StandalonePojoMapperSettings.Defaults.SCHEMA_MANAGEMENT_STRATEGY )
+			.build();
+
+	private static final OptionalConfigurationProperty<List<BeanReference<? extends StandalonePojoMappingConfigurer>>> MAPPING_CONFIGURER =
+			ConfigurationProperty.forKey( StandalonePojoMapperSettings.Radicals.MAPPING_CONFIGURER )
+					.asBeanReference( StandalonePojoMappingConfigurer.class )
+					.multivalued()
 					.build();
 
 	private final StandalonePojoEntityTypeMetadataProvider.Builder entityTypeMetadataProviderBuilder;
@@ -39,13 +54,26 @@ public class StandalonePojoMappingInitiator extends AbstractPojoMappingInitiator
 		entityTypeMetadataProviderBuilder = new StandalonePojoEntityTypeMetadataProvider.Builder( introspector );
 	}
 
-	public <E> void addEntityType(Class<E> clazz, String entityName, EntityConfigurer<E> configurerOrNull) {
+	public <E> StandalonePojoMappingInitiator addEntityType(Class<E> clazz, String entityName, EntityConfigurer<E> configurerOrNull) {
 		entityTypeMetadataProviderBuilder.addEntityType( clazz, entityName, configurerOrNull );
+
+		return this;
 	}
 
 	@Override
 	public void configure(MappingBuildContext buildContext,
 			MappingConfigurationCollector<PojoTypeMetadataContributor> configurationCollector) {
+		// Apply the user-provided mapping configurer if necessary.
+		// Has to happen before building entityTypeMetadataProvider as configurers can add more entities.
+		MAPPING_CONFIGURER.getAndMap( buildContext.configurationPropertySource(), buildContext.beanResolver()::resolve )
+				.ifPresent( holder -> {
+					try ( BeanHolder<List<StandalonePojoMappingConfigurer>> configurerHolder = holder ) {
+						for ( StandalonePojoMappingConfigurer configurer : configurerHolder.get() ) {
+							configurer.configure( this );
+						}
+					}
+				} );
+
 		entityTypeMetadataProvider = entityTypeMetadataProviderBuilder.build();
 
 		SchemaManagementStrategyName schemaManagementStrategyName = SCHEMA_MANAGEMENT_STRATEGY.get(
@@ -53,11 +81,17 @@ public class StandalonePojoMappingInitiator extends AbstractPojoMappingInitiator
 		schemaManagementListener = new SchemaManagementListener( schemaManagementStrategyName );
 
 		addConfigurationContributor( new StandalonePojoTypeConfigurationContributor( entityTypeMetadataProvider ) );
+
 		super.configure( buildContext, configurationCollector );
 	}
 
 	@Override
 	protected PojoMapperDelegate<StandalonePojoMappingPartialBuildState> createMapperDelegate() {
 		return new StandalonePojoMapperDelegate( entityTypeMetadataProvider, schemaManagementListener );
+	}
+
+	@Override
+	public void multiTenancyEnabled(boolean multiTenancyEnabled) {
+		this.tenancyMode( multiTenancyEnabled ? TenancyMode.MULTI_TENANCY : TenancyMode.SINGLE_TENANCY );
 	}
 }
