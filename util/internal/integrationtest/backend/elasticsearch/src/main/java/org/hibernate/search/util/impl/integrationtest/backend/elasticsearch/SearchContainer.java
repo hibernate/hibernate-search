80e21b0785da71b14c3060cb7d661102e0cbf9fb
marko-bekhta <marko.prykladna@gmail.com>
2022-12-01 17:48:53 +0100

    HSEARCH-4674 Use test containers for Elasticsearch/OpenSearch

---
+++ util/internal/integrationtest/backend/elasticsearch/src/main/java/org/hibernate/search/util/impl/integrationtest/backend/elasticsearch/SearchContainer.java
@@ -0,0 +1,99 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.util.impl.integrationtest.backend.elasticsearch;
+
+import java.time.Duration;
+
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.HttpWaitStrategy;
+import org.testcontainers.utility.DockerImageName;
+
+public final class SearchContainer {
+
+	private SearchContainer() {
+	}
+
+	private static final GenericContainer<?> SEARCH_CONTAINER;
+
+	static {
+		String distribution = System.getProperty( "org.hibernate.search.integrationtest.backend.elasticsearch.distribution" );
+		String tag = System.getProperty( "org.hibernate.search.integrationtest.backend.elasticsearch.tag" );
+
+		SEARCH_CONTAINER = "elastic".equalsIgnoreCase( distribution ) ? elasticsearch( tag ) : opensearch( tag );
+	}
+
+	public static int mappedPort(int port) {
+		startIfNeeded();
+		return SEARCH_CONTAINER.getMappedPort( port );
+	}
+
+	public static String host() {
+		startIfNeeded();
+		return SEARCH_CONTAINER.getHost();
+	}
+
+	private static void startIfNeeded() {
+		if ( !SEARCH_CONTAINER.isRunning() ) {
+			SEARCH_CONTAINER.start();
+		}
+	}
+
+	private static GenericContainer<?> elasticsearch(String imageTag) {
+		return common( "elastic/elasticsearch", imageTag )
+				.withEnv( "logger.level", "WARN" )
+				.withEnv( "discovery.type", "single-node" )
+				// Older images require HTTP authentication for all requests;
+				// it's not practical for testing, so we disable that:
+				.withEnv( "xpack.security.enabled", "false" )
+				// Limit the RAM usage.
+				// Recent versions of ES limit themselves to 50% of the total available RAM,
+				// but on CI this is sometimes too much, as we also have the Maven JVM
+				// and the JVM that runs tests taking up a significant amount of RAM,
+				// leaving too little for filesystem caches and resulting in freezes:
+				.withEnv( "ES_JAVA_OPTS", "-Xms1g -Xmx1g" )
+				// Disable disk-based shard allocation thresholds: on large, relatively full disks (>90% used),
+				// it will lead to index creation to get stuck waiting for other nodes to join the cluster,
+				// which will never happen since we only have one node.
+				// See https://www.elastic.co/guide/en/elasticsearch/reference/7.17/modules-cluster.html#disk-based-shard-allocation
+				.withEnv( "cluster.routing.allocation.disk.threshold_enabled", "false" )
+				;
+	}
+
+	private static GenericContainer<?> opensearch(String imageTag) {
+		return common( "opensearchproject/opensearch", imageTag )
+				.withEnv( "logger.level", "WARN" )
+				.withEnv( "discovery.type", "single-node" )
+				// Prevent swapping
+				.withEnv( "bootstrap.memory_lock", "true" )
+				// OpenSearch expects SSL and uses a self-signed certificate by default;
+				// it's not practical for testing, so we disable that.
+				.withEnv( "plugins.security.disabled", "true" )
+				// ISM floods the logs with errors, and we don't need it.
+				// See https://docs-beta.opensearch.org/im-plugin/ism/settings/
+				.withEnv( "plugins.index_state_management.enabled", "false" )
+				// Disable disk-based shard allocation thresholds: on large, relatively full disks (>90% used),
+				// it will lead to index creation to get stuck waiting for other nodes to join the cluster,
+				// which will never happen since we only have one node.
+				// See https://www.elastic.co/guide/en/elasticsearch/reference/7.17/modules-cluster.html#disk-based-shard-allocation
+				// See https://opensearch.org/docs/latest/opensearch/popular-api/#change-disk-watermarks-or-other-cluster-settings
+				.withEnv( "cluster.routing.allocation.disk.threshold_enabled", "false" )
+				;
+	}
+
+	/*
+	 * Suppress "Resource leak: '<unassigned Closeable value>' is never closed". Testcontainers take care of closing
+	 * this resource in the end.
+	 */
+	@SuppressWarnings("resource")
+	private static GenericContainer<?> common(String image, String tag) {
+		return new GenericContainer<>( DockerImageName.parse( image ).withTag( tag ) )
+				.withExposedPorts( 9200, 9300 )
+				.waitingFor( new HttpWaitStrategy().forPort( 9200 ).forStatusCode( 200 ) )
+				.withStartupTimeout( Duration.ofMinutes( 2 ) )
+				.withReuse( true );
+	}
+}
