bf018a379ef36192d330027e064d0e1258c3c854
marko-bekhta <marko.prykladna@gmail.com>
2022-12-07 10:50:00 +0100

    HSEARCH-4674 Make documentation tests aware of testcontainers

--- documentation/src/test/java/org/hibernate/search/documentation/testsupport/DocumentationSetupHelper.java
+++ documentation/src/test/java/org/hibernate/search/documentation/testsupport/DocumentationSetupHelper.java
@@ -28,6 +28,7 @@ import org.hibernate.search.util.impl.integrationtest.common.extension.BackendCo
 import org.hibernate.search.util.impl.integrationtest.common.extension.BackendSetupStrategy;
 import org.hibernate.search.util.impl.integrationtest.common.extension.MappingSetupHelper;
 import org.hibernate.search.util.impl.integrationtest.common.stub.backend.BackendMappingHandle;
+import org.hibernate.search.util.impl.integrationtest.mapper.orm.DatabaseContainer;
 import org.hibernate.search.util.impl.integrationtest.mapper.orm.HibernateOrmMappingHandle;
 import org.hibernate.search.util.impl.integrationtest.mapper.orm.OrmAssertionHelper;
 import org.hibernate.search.util.impl.integrationtest.mapper.orm.SimpleSessionFactoryBuilder;
@@ -183,6 +184,10 @@ public final class DocumentationSetupHelper
 			// Ensure we don't build Jandex indexes needlessly:
 			// discovery based on Jandex ought to be tested in real projects that don't use this setup helper.
 			withProperty( HibernateOrmMapperSettings.MAPPING_BUILD_MISSING_DISCOVERED_JANDEX_INDEXES, false );
+
+			// DB properties:
+			DatabaseContainer.configuration().add( overriddenProperties );
+
 			// Ensure overridden properties will be applied
 			withConfiguration( builder -> overriddenProperties.forEach( builder::setProperty ) );
 		}
