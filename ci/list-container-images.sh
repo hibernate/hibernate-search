213bdbde9c7938d75463f415c19d79bcfdfa33f4
Yoann Rodière <yoann@hibernate.org>
2023-01-03 16:29:55 +0100

    HSEARCH-4770 Add missing words to comment in ci/list-container-images.sh

--- ci/list-container-images.sh
+++ ci/list-container-images.sh
@@ -4,7 +4,7 @@
 LIST_FILE=$(mktemp)
 trap "rm -f $LIST_FILE" EXIT
 
-# See the configuration of the m
+# See the configuration of the maven-scripting-plugin in the main POM
 ./mvnw scripting:eval@list-container-images -DcontainerImagesListFile="$LIST_FILE" "${@}" 1>&2
 
 sort -u "$LIST_FILE" | { grep -Ev "^\s*$" || true; }
