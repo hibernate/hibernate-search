ad6ca7e5c92d5eeabcccaf0d1ae6b19417595994
Yoann Rodière <yoann@hibernate.org>
2023-01-06 12:01:57 +0100

    HSEARCH-4775 Avoid direct references to setParameter methods in ORM query adapters
    ORM 6.2 makes some changes to the setParameter methods, so we'd better
    avoid overriding them altogether. Overriding locateBinding() methods is
    safe, though.

--- v5migrationhelper/orm/src/main/java/org/hibernate/search/query/hibernate/impl/FullTextQueryImpl.java
+++ v5migrationhelper/orm/src/main/java/org/hibernate/search/query/hibernate/impl/FullTextQueryImpl.java
@@ -7,7 +7,6 @@
 package org.hibernate.search.query.hibernate.impl;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,6 +16,7 @@ import java.util.function.Function;
 
 import jakarta.persistence.FlushModeType;
 import jakarta.persistence.LockModeType;
+import jakarta.persistence.Parameter;
 import jakarta.persistence.PersistenceException;
 import jakarta.persistence.QueryTimeoutException;
 
@@ -35,7 +35,9 @@ import org.hibernate.query.internal.QueryOptionsImpl;
 import org.hibernate.query.spi.AbstractQuery;
 import org.hibernate.query.spi.ParameterMetadataImplementor;
 import org.hibernate.query.spi.QueryImplementor;
+import org.hibernate.query.spi.QueryParameterBinding;
 import org.hibernate.query.spi.QueryParameterBindings;
+import org.hibernate.query.spi.QueryParameterImplementor;
 import org.hibernate.query.spi.ScrollableResultsImplementor;
 import org.hibernate.search.FullTextQuery;
 import org.hibernate.search.engine.search.query.SearchScroll;
@@ -288,17 +290,22 @@ public class FullTextQueryImpl extends AbstractQuery implements FullTextQuery {
 	}
 
 	@Override
-	public QueryImplementor<?> setParameterList(String name, Object[] values) {
+	protected QueryParameterBinding locateBinding(String name) {
 		throw parametersNoSupported();
 	}
 
 	@Override
-	public QueryImplementor<?> setParameterList(String s, Collection collection, Class aClass) {
+	protected QueryParameterBinding locateBinding(int position) {
 		throw parametersNoSupported();
 	}
 
 	@Override
-	public QueryImplementor<?> setParameterList(int i, Collection collection, Class aClass) {
+	protected QueryParameterBinding locateBinding(Parameter parameter) {
+		throw parametersNoSupported();
+	}
+
+	@Override
+	protected QueryParameterBinding locateBinding(QueryParameterImplementor parameter) {
 		throw parametersNoSupported();
 	}
 
