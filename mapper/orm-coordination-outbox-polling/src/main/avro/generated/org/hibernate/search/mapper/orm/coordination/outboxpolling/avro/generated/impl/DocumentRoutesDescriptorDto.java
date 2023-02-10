3754d27e52022ecb3f340ac1800e4dd13820751b
Yoann Rodière <yoann@hibernate.org>
2023-02-10 12:23:14 +0100

    HSEARCH-4774 Update Avro-generated source files
    No impact really, that's just to get rid of annoying changed files
    appearing in `git status` when rebuilding.

--- mapper/orm-coordination-outbox-polling/src/main/avro/generated/org/hibernate/search/mapper/orm/coordination/outboxpolling/avro/generated/impl/DocumentRoutesDescriptorDto.java
+++ mapper/orm-coordination-outbox-polling/src/main/avro/generated/org/hibernate/search/mapper/orm/coordination/outboxpolling/avro/generated/impl/DocumentRoutesDescriptorDto.java
@@ -23,10 +23,10 @@ public class DocumentRoutesDescriptorDto extends org.apache.avro.specific.Specif
   private static final SpecificData MODEL$ = new SpecificData();
 
   private static final BinaryMessageEncoder<DocumentRoutesDescriptorDto> ENCODER =
-      new BinaryMessageEncoder<DocumentRoutesDescriptorDto>(MODEL$, SCHEMA$);
+      new BinaryMessageEncoder<>(MODEL$, SCHEMA$);
 
   private static final BinaryMessageDecoder<DocumentRoutesDescriptorDto> DECODER =
-      new BinaryMessageDecoder<DocumentRoutesDescriptorDto>(MODEL$, SCHEMA$);
+      new BinaryMessageDecoder<>(MODEL$, SCHEMA$);
 
   /**
    * Return the BinaryMessageEncoder instance used by this class.
@@ -50,7 +50,7 @@ public class DocumentRoutesDescriptorDto extends org.apache.avro.specific.Specif
    * @return a BinaryMessageDecoder instance for this class backed by the given SchemaStore
    */
   public static BinaryMessageDecoder<DocumentRoutesDescriptorDto> createDecoder(SchemaStore resolver) {
-    return new BinaryMessageDecoder<DocumentRoutesDescriptorDto>(MODEL$, SCHEMA$, resolver);
+    return new BinaryMessageDecoder<>(MODEL$, SCHEMA$, resolver);
   }
 
   /**
@@ -93,9 +93,14 @@ public class DocumentRoutesDescriptorDto extends org.apache.avro.specific.Specif
     this.previousRoutes = previousRoutes;
   }
 
+  @Override
   public org.apache.avro.specific.SpecificData getSpecificData() { return MODEL$; }
+
+  @Override
   public org.apache.avro.Schema getSchema() { return SCHEMA$; }
+
   // Used by DatumWriter.  Applications should not call.
+  @Override
   public java.lang.Object get(int field$) {
     switch (field$) {
     case 0: return currentRoute;
@@ -105,6 +110,7 @@ public class DocumentRoutesDescriptorDto extends org.apache.avro.specific.Specif
   }
 
   // Used by DatumReader.  Applications should not call.
+  @Override
   @SuppressWarnings(value="unchecked")
   public void put(int field$, java.lang.Object value$) {
     switch (field$) {
