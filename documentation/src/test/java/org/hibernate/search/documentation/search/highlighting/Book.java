23b22c5960515672f56567acabbd16936af7eefd
marko-bekhta <marko.prykladna@gmail.com>
2023-06-06 13:05:36 +0200

    HSEARCH-4863 Update Highlight DSL docs
    - add a few highlight result examples
    - change the section title to match others
    - split the type examples into own blocks
    - use the same Book for all examples

--- documentation/src/test/java/org/hibernate/search/documentation/search/highlighting/Book.java
+++ documentation/src/test/java/org/hibernate/search/documentation/search/highlighting/Book.java
@@ -6,13 +6,17 @@
  */
 package org.hibernate.search.documentation.search.highlighting;
 
+import java.util.List;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 import org.hibernate.search.engine.backend.types.Highlightable;
 import org.hibernate.search.engine.backend.types.Projectable;
+import org.hibernate.search.engine.backend.types.TermVector;
 import org.hibernate.search.mapper.pojo.mapping.definition.annotation.FullTextField;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.GenericField;
 import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
 
 // tag::basics[]
@@ -25,16 +29,42 @@ public class Book {
 	@Id
 	private Integer id;
 
-	@FullTextField(analyzer = "english", projectable = Projectable.YES,
-			highlightable = { Highlightable.PLAIN, Highlightable.UNIFIED }) // <1>
+	@FullTextField(analyzer = "english") // <1>
+	private String author;
+
+	@FullTextField(analyzer = "english",
+			highlightable = { Highlightable.PLAIN, Highlightable.UNIFIED }) // <2>
 	private String title;
 
-	@FullTextField(analyzer = "english", projectable = Projectable.YES, highlightable = Highlightable.ANY) // <2>
+	@FullTextField(analyzer = "english",
+			highlightable = Highlightable.ANY) // <3>
 	@Column(length = 10000)
 	private String description;
 
+	@FullTextField(analyzer = "english",
+			projectable = Projectable.YES,
+			termVector = TermVector.WITH_POSITIONS_OFFSETS) // <4>
+	@Column(length = 10000)
+	@ElementCollection
+	private List<String> text;
+
+	@GenericField // <5>
+	@Column(length = 10000)
+	@ElementCollection
+	private List<String> keywords;
+
+
 	// end::basics[]
 	public Book() {
+	}
+
+	public Book(Integer id, String author, String title, String description, List<String> text, List<String> keywords) {
+		this.id = id;
+		this.author = author;
+		this.title = title;
+		this.description = description;
+		this.text = text;
+		this.keywords = keywords;
 	}
 
 	public Integer getId() {
