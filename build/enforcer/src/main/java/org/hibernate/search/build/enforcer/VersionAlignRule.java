b9998fd517f37cdce4ac2b2db6d9f8821e88be18
marko-bekhta <marko.prykladna@gmail.com>
2023-07-19 12:40:58 +0200

    HSEARCH-4900 Add version align enforcer rule
    - that validates that the version in a property matches the version of a managed dependency

---
+++ build/enforcer/src/main/java/org/hibernate/search/build/enforcer/VersionAlignRule.java
@@ -0,0 +1,58 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.build.enforcer;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.apache.maven.enforcer.rule.api.AbstractEnforcerRule;
+import org.apache.maven.enforcer.rule.api.EnforcerRuleException;
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.model.Dependency;
+
+@Named("versionAlignRule") // rule name - must start with lowercase character
+public class VersionAlignRule extends AbstractEnforcerRule {
+
+	/**
+	 * Rule parameter as list of items.
+	 */
+	private List<VersionAlignData> propertiesToCheck;
+
+	// Inject needed Maven components
+	@Inject
+	private MavenSession session;
+
+	public void execute() throws EnforcerRuleException {
+		for ( VersionAlignData data : propertiesToCheck ) {
+			boolean found = false;
+			for ( Dependency dependency : session.getCurrentProject().getDependencyManagement().getDependencies() ) {
+				if ( data.getArtifact().equals( dependency.getGroupId() + ":" + dependency.getArtifactId() ) ) {
+					if ( !dependency.getVersion().equals( data.getProperty() ) ) {
+						throw new EnforcerRuleException( "Property for version of " + dependency
+								+ " is incorrect. Version property is set to " + data.getProperty() );
+					}
+					else {
+						found = true;
+					}
+				}
+			}
+			if ( !found && data.isFailOnNotFound() ) {
+				throw new EnforcerRuleException(
+						"Wasn't able to find the `" + data.getArtifact() + "` among managed dependencies." );
+			}
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "VersionAlignRule{" +
+				"propertiesToCheck=" + propertiesToCheck +
+				'}';
+	}
+}
