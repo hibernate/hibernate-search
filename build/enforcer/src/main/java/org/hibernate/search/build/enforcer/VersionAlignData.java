72a12136a2b348229c0c06a58b435ab99a51a80a
marko-bekhta <marko.prykladna@gmail.com>
2023-07-19 12:40:58 +0200

    HSEARCH-4900 Add version align enforcer rule
    - that validates that the version in a property matches the version of a managed dependency

---
+++ build/enforcer/src/main/java/org/hibernate/search/build/enforcer/VersionAlignData.java
@@ -0,0 +1,48 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.build.enforcer;
+
+public class VersionAlignData {
+
+	private String property;
+	private String artifact;
+	private boolean failOnNotFound = false;
+
+	public String getProperty() {
+		return property;
+	}
+
+	public void setProperty(String property) {
+		this.property = property;
+	}
+
+	public String getArtifact() {
+		return artifact;
+	}
+
+	public void setArtifact(String artifact) {
+		this.artifact = artifact;
+	}
+
+	public boolean isFailOnNotFound() {
+		return failOnNotFound;
+	}
+
+	public void setFailOnNotFound(boolean failOnNotFound) {
+		this.failOnNotFound = failOnNotFound;
+	}
+
+	@Override
+	public String toString() {
+		return "{" +
+				"property='" + property + '\'' +
+				", artifact='" + artifact + '\'' +
+				", failOnNotFound=" + failOnNotFound +
+				'}';
+	}
+
+}
