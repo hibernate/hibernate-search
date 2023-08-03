98481f8471663259bb9bc7bcc2d9b8984777f3b6
marko-bekhta <marko.prykladna@gmail.com>
2023-08-03 16:54:00 +0200

    HSEARCH-4744 Add a test for repackaged jar

---
+++ integrationtest/mapper/orm-spring-uberjar/application/src/main/java/org/hibernate/search/integrationtest/spring/repackaged/application/RepackagedApplication.java
@@ -0,0 +1,25 @@
+/*
+ * Hibernate Search, full-text search for your domain model
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.search.integrationtest.spring.repackaged.application;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+// CHECKSTYLE:OFF: HideUtilityClassConstructor
+@SpringBootApplication
+public class RepackagedApplication {
+	public static void main(String[] args) {
+		ConfigurableApplicationContext ctx =
+				new SpringApplicationBuilder( RepackagedApplication.class ).web( WebApplicationType.NONE ).run();
+		System.out.println( "Spring Boot application started" );
+		ctx.getBean( Config.TestHibernateSearch.class );
+		ctx.close();
+	}
+}
+// CHECKSTYLE:ON
