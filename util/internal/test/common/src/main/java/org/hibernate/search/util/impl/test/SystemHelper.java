627dc891895ccdd4d8e95cb7391beefac3ffdb29
Yoann Rodière <yoann@hibernate.org>
2023-08-08 12:56:58 +0200

    HSEARCH-4744 Run all Spring repackaged application tests within failsafe
    So that test results are reported like any other test.

--- util/internal/test/common/src/main/java/org/hibernate/search/util/impl/test/SystemHelper.java
+++ util/internal/test/common/src/main/java/org/hibernate/search/util/impl/test/SystemHelper.java
@@ -6,7 +6,18 @@
  */
 package org.hibernate.search.util.impl.test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 import org.hibernate.search.util.impl.test.annotation.SuppressForbiddenApis;
+
+import com.google.common.base.Charsets;
+
+import org.awaitility.Awaitility;
 
 public final class SystemHelper {
 
@@ -20,6 +31,32 @@ public final class SystemHelper {
 		return oldValue == null
 				? () -> System.clearProperty( key )
 				: () -> System.setProperty( key, oldValue );
+	}
+
+	public static Process runCommandWithInheritedIO(String... command) throws IOException {
+		// We can't use `inheritIO()` in surefire as that would bypass surefire's
+		// output capturingand lead to warnings.
+		// So we use an old-fashioned solution...
+
+		Process process = new ProcessBuilder( command )
+				.redirectInput( ProcessBuilder.Redirect.INHERIT )
+				.start();
+
+		// Drain the output/errors streams
+		ExecutorService service = Executors.newFixedThreadPool( 2 );
+		service.submit( () -> new BufferedReader( new InputStreamReader( process.getInputStream(), Charsets.UTF_8 ) )
+				.lines()
+				.forEach( System.out::println ) );
+		service.submit( () -> new BufferedReader( new InputStreamReader( process.getErrorStream(), Charsets.UTF_8 ) )
+				.lines()
+				.forEach( System.err::println ) );
+		service.shutdown();
+
+		Awaitility.await( process + " termination" )
+				.atMost( 1, TimeUnit.MINUTES )
+				.until( () -> !process.isAlive() );
+
+		return process;
 	}
 
 	public interface SystemPropertyRestorer extends AutoCloseable {
