fa8be34b9c14f8e8354ce0e7dc6acc27c5e077a3
Yoann Rodière <yoann@hibernate.org>
2023-08-09 12:28:44 +0200

    HSEARCH-4897 Fix HibernateSearchPreIntegrationService exposing prefixed/non-defaulted configuration property source

--- engine/src/test/java/org/hibernate/search/engine/environment/bean/impl/BeanResolverImplRetrievalBuiltinTest.java
+++ engine/src/test/java/org/hibernate/search/engine/environment/bean/impl/BeanResolverImplRetrievalBuiltinTest.java
@@ -34,7 +34,6 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
-import org.mockito.Answers;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
@@ -54,7 +53,7 @@ public class BeanResolverImplRetrievalBuiltinTest {
 	@Mock
 	private BeanProvider beanManagerBeanProviderMock;
 
-	@Mock(answer = Answers.CALLS_REAL_METHODS)
+	@Mock
 	private ConfigurationPropertySource configurationSourceMock;
 
 	@Mock
@@ -83,9 +82,11 @@ public class BeanResolverImplRetrievalBuiltinTest {
 
 		when( serviceResolverMock.loadJavaServices( BeanConfigurer.class ) )
 				.thenReturn( Collections.singletonList( beanConfigurer1 ) );
+		when( configurationSourceMock.withMask( any() ) )
+				.thenCallRealMethod();
 		when( configurationSourceMock.withFallback( any() ) )
 				.thenCallRealMethod();
-		when( configurationSourceMock.get( EngineSpiSettings.Radicals.BEAN_CONFIGURERS ) )
+		when( configurationSourceMock.get( EngineSpiSettings.BEAN_CONFIGURERS ) )
 				.thenReturn( (Optional) Optional.of( Collections.singletonList( beanConfigurer2 ) ) );
 		beanResolver = BeanResolverImpl.create( classResolverMock, serviceResolverMock, beanManagerBeanProviderMock,
 				configurationSourceMock );
