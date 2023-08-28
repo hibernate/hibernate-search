hibernate/hibernate-search

commit c36c8f1c37340be7fcfdbb3a51d6d6974e8b3821

HSEARCH-4924 Jenkinsfile: Avoid cleanup between pre-build of sources and ITs
Without this patch, compiled files get deleted when we call
withMavenWorkspace a second time.

Jenkinsfile
@@ -375,40 +375,24 @@ Resulting execution plan:
 """
 }
 
-runBuildOnNode( NODE_PATTERN_BASE, [time: 2, unit: 'HOURS'] ) {
-	stage('Pre-build sources') {
-		// we want this stage to only be executed when we are planning to use incremental build
-		if (!incrementalBuild) {
-			echo 'Skipping pre-building sources for non-pull request builds.'
-			helper.markStageSkipped()
-			return
-		}
-		if (!enableDefaultBuild) {
-			echo 'Skipping default build and integration tests in the default environment'
-			helper.markStageSkipped()
-			return
-		}
-		withMavenWorkspace {
-			sh """ \
-					mvn clean install \
-					--fail-at-end \
-					-Pdist -Pjqassistant -Pci-sources-check \
-					-DskipITs -DskipTests \
-			"""
-
-			dir(helper.configuration.maven.localRepositoryPath) {
-				stash name:'default-build-result', includes:"org/hibernate/search/**"
-			}
-		}
+stage('Default build') {
+	if (!enableDefaultBuild) {
+		echo 'Skipping default build and integration tests in the default environment'
+		helper.markStageSkipped()
+		return
 	}
-	stage('Default build') {
-		if (!enableDefaultBuild) {
-			echo 'Skipping default build and integration tests in the default environment'
-			helper.markStageSkipped()
-			return
-		}
+	runBuildOnNode( NODE_PATTERN_BASE, [time: 2, unit: 'HOURS'] ) {
 		withMavenWorkspace(mavenSettingsConfig: deploySnapshot ? helper.configuration.file.deployment.maven.settingsId : null) {
-			if ( incrementalBuild ) {
+			if (incrementalBuild) {
+				// We won't be building all classes in the incremental build,
+				// which is a problem for Sonar since it needs access to compiled classes.
+				// So in incremental builds, we build all sources first, but skipping tests.
+				sh """ \
+						mvn clean install \
+						--fail-at-end \
+						-Pdist -Pjqassistant -Pci-sources-check \
+						-DskipITs -DskipTests \
+				"""
 				dir(helper.configuration.maven.localRepositoryPath) {
 					unstash name:'default-build-result'
 				}

commit 091501fd49cc75b333c45871386fd3f4b3ee801c

HSEARCH-4924 Jenkinsfile: Fix stashing in the default build
The error was introduced in c36c8f1c37340be7fcfdbb3a51d6d6974e8b3821

Jenkinsfile
@@ -404,7 +404,7 @@ stage('Default build') {
 					"} \
 			"""
 			dir(helper.configuration.maven.localRepositoryPath) {
-				unstash name:'default-build-result'
+				stash name:'default-build-result', includes:"org/hibernate/search/**"
 			}
 
 			if (!enableDefaultBuildIT) {