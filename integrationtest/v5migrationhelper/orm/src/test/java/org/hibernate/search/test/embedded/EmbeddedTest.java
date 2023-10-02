638aadfae8dd59dd56da606ab99168bd91c20b68
Yoann Rodière <yoann@hibernate.org>
2023-10-02 17:18:11 +0200

    HSEARCH-4971 Update file headers to Apache-2.0

--- integrationtest/v5migrationhelper/orm/src/test/java/org/hibernate/search/test/embedded/EmbeddedTest.java
+++ integrationtest/v5migrationhelper/orm/src/test/java/org/hibernate/search/test/embedded/EmbeddedTest.java
@@ -1,8 +1,6 @@
 /*
- * Hibernate Search, full-text search for your domain model
- *
- * License: GNU Lesser General Public License (LGPL), version 2.1 or later
- * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright Red Hat Inc. and Hibernate Authors
  */
 package org.hibernate.search.test.embedded;
 
@@ -23,7 +21,6 @@ import org.junit.jupiter.api.Test;
 import org.apache.lucene.document.LongPoint;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
-import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.TermQuery;
