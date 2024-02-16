3c5d6ae18c59a987ddf2d1a0bc9a06a5d46670ea
marko-bekhta <marko.prykladna@gmail.com>
2024-02-16 12:08:42 +0100

    HSEARCH-4963 Add an API to run analysis on a given String (Lucene)

--- engine/src/main/java/org/hibernate/search/engine/backend/index/IndexManager.java
+++ engine/src/main/java/org/hibernate/search/engine/backend/index/IndexManager.java
@@ -6,9 +6,15 @@
  */
 package org.hibernate.search.engine.backend.index;
 
+import java.util.List;
+import java.util.concurrent.CompletionStage;
+
 import org.hibernate.search.engine.backend.Backend;
+import org.hibernate.search.engine.backend.analysis.AnalysisToken;
 import org.hibernate.search.engine.backend.metamodel.IndexDescriptor;
+import org.hibernate.search.engine.backend.work.execution.OperationSubmitter;
 import org.hibernate.search.util.common.SearchException;
+import org.hibernate.search.util.common.annotation.Incubating;
 
 /**
  * An index manager as viewed by Hibernate Search users.
@@ -27,6 +33,49 @@ public interface IndexManager {
 	 * @return A descriptor of this index, exposing in particular a list of field and their characteristics.
 	 */
 	IndexDescriptor descriptor();
+
+	/**
+	 * Applies the analyzer to a given string to produce a list of tokens.
+	 *
+	 * @param analyzerName The name of the configured analyzer to apply.
+	 * @param terms The string to apply the analysis to.
+	 * @return The list of tokens produced by the analyzer for a given string.
+	 */
+	@Incubating
+	List<? extends AnalysisToken> analyze(String analyzerName, String terms);
+
+	/**
+	 * Applies the normalizer to a given string to produce a normalized token.
+	 *
+	 * @param normalizerName The name of the configured normalizer to apply.
+	 * @param terms The string to apply the normalizer to.
+	 * @return The token produced by the normalizer for a given string.
+	 */
+	@Incubating
+	AnalysisToken normalize(String normalizerName, String terms);
+
+	/**
+	 * Applies the analyzer to a given string to produce a list of tokens in an async manner.
+	 *
+	 * @param analyzerName The name of the configured analyzer to apply.
+	 * @param terms The string to apply the analysis to.
+	 * @param operationSubmitter How to handle request to submit operation when the queue is full.
+	 * @return A future that will ultimately provide the list of tokens produced by the analyzer for a given string.
+	 */
+	@Incubating
+	CompletionStage<List<? extends AnalysisToken>> analyzeAsync(String analyzerName, String terms,
+			OperationSubmitter operationSubmitter);
+
+	/**
+	 * Applies the normalizer to a given string to produce a normalized token in an async manner.
+	 *
+	 * @param normalizerName The name of the configured normalizer to apply.
+	 * @param terms The string to apply the normalizer to.
+	 * @param operationSubmitter How to handle request to submit operation when the queue is full.
+	 * @return A future that will ultimately provide the token produced by the normalizer for a given string.
+	 */
+	@Incubating
+	CompletionStage<AnalysisToken> normalizeAsync(String normalizerName, String terms, OperationSubmitter operationSubmitter);
 
 	// TODO HSEARCH-3129 add standard APIs related to statistics?
 
