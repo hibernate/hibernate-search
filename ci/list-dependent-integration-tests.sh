da9a36680c13af423c98910cad29f29cb08b5bfe
Yoann Rodière <yoann@hibernate.org>
2024-02-29 11:07:24 +0100

    HSEARCH-5094 Disable build scan and build cache for irrelevant Maven executions

--- ci/list-dependent-integration-tests.sh
+++ ci/list-dependent-integration-tests.sh
@@ -12,8 +12,11 @@ fi
 IFS="," COMMA_SEPARATED_TESTED_ARTIFACT_IDS="$*"
 TESTED_ARTIFACT_IDS_REGEXP="(\Q$(echo "$*" | perl -pe 's/,/\\E|\\Q/g')\E)"
 
+# We don't want to use the build cache or build scans for this execution
+MISC_MAVEN_ARGS="-Dscan=false -Dno-build-cache"
+
 {
-	./mvnw dependency:list -pl :hibernate-search-parent-integrationtest -am -amd -DincludeArtifactIds="$COMMA_SEPARATED_TESTED_ARTIFACT_IDS"
+	./mvnw dependency:list $MISC_MAVEN_ARGS -pl :hibernate-search-parent-integrationtest -am -amd -Dscan=false -DincludeArtifactIds="$COMMA_SEPARATED_TESTED_ARTIFACT_IDS"
 } \
 	| perl -0777 -pe "s/(hibernate-search-.*) ---(\n(?!.*Building).*)*\n.*:$TESTED_ARTIFACT_IDS_REGEXP:jar:/\nMATCH:\$1\n/g" \
 	| perl -ne 'print if s/MATCH:(.*)/:$1/g' \
