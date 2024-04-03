6e667baa2b6787bc53cfde565fd23bb5c1d972cd
marko-bekhta <marko.prykladna@gmail.com>
2024-04-03 19:09:24 +0200

    HSEARCH-5082 Use Hibernate Models instead of HCANN in the ORM mapper

---
+++ mapper/pojo-base/src/main/java/org/hibernate/search/mapper/pojo/model/models/spi/PojoModelsGenericContextHelper.java
@@ -0,0 +1,35 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.search.mapper.pojo.model.models.spi;
+
+import java.lang.reflect.Type;
+
+import org.hibernate.search.mapper.pojo.model.spi.GenericContextAwarePojoGenericTypeModel;
+import org.hibernate.search.mapper.pojo.model.spi.PojoPropertyModel;
+import org.hibernate.search.mapper.pojo.model.spi.PojoRawTypeModel;
+
+public class PojoModelsGenericContextHelper implements GenericContextAwarePojoGenericTypeModel.Helper {
+	private final AbstractPojoModelsBootstrapIntrospector introspector;
+
+	public PojoModelsGenericContextHelper(AbstractPojoModelsBootstrapIntrospector introspector) {
+		this.introspector = introspector;
+	}
+
+	@Override
+	public <T> PojoRawTypeModel<T> rawTypeModel(Class<T> clazz) {
+		return introspector.typeModel( clazz );
+	}
+
+	@Override
+	public Object propertyCacheKey(PojoPropertyModel<?> rawPropertyModel) {
+		return rawPropertyModel; // Properties are instantiated only once per type model
+	}
+
+	@Override
+	public Type propertyGenericType(PojoPropertyModel<?> rawPropertyModel) {
+		AbstractPojoModelsPropertyModel<?, ?> propertyModel = (AbstractPojoModelsPropertyModel<?, ?>) rawPropertyModel;
+		return propertyModel.getterGenericReturnType();
+	}
+}
