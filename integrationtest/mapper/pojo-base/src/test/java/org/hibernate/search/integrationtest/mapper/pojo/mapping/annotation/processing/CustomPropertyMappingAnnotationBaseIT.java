e6ada824cf1a2d3057018896a5c1cb3c417940cd
marko-bekhta <marko.prykladna@gmail.com>
2024-04-04 12:06:37 +0200

    HSEARCH-5082 Use Hibernate Models instead of HCANN in the Standalone POJO mapper

--- integrationtest/mapper/pojo-base/src/test/java/org/hibernate/search/integrationtest/mapper/pojo/mapping/annotation/processing/CustomPropertyMappingAnnotationBaseIT.java
+++ integrationtest/mapper/pojo-base/src/test/java/org/hibernate/search/integrationtest/mapper/pojo/mapping/annotation/processing/CustomPropertyMappingAnnotationBaseIT.java
@@ -273,7 +273,8 @@ class CustomPropertyMappingAnnotationBaseIT {
 
 	@Retention(RetentionPolicy.RUNTIME)
 	@Target({ ElementType.FIELD, ElementType.METHOD })
-	private @interface AnalyzerAnnotation {
+	// must be public for Hibernate Models to access #name() :
+	public @interface AnalyzerAnnotation {
 
 		String name();
 
