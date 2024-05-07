ceca29012ed44b40513f72284fd3795386a6d80d
marko-bekhta <marko.prykladna@gmail.com>
2024-05-07 18:58:18 +0200

    HSEARCH-3319 Create reference traits interfaces

---
+++ engine/src/main/java/org/hibernate/search/engine/search/reference/sort/FieldSortFieldReference.java
@@ -0,0 +1,8 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.search.engine.search.reference.sort;
+
+public interface FieldSortFieldReference<SR, T> extends TypedSortFieldReference<SR, T> {
+}
