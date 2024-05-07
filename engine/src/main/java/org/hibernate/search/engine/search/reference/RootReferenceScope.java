24bd34a3685bb22a4f3c3f641d58ebb845da4f53
marko-bekhta <marko.prykladna@gmail.com>
2024-05-07 18:58:18 +0200

    HSEARCH-3319 Create reference traits interfaces

---
+++ engine/src/main/java/org/hibernate/search/engine/search/reference/RootReferenceScope.java
@@ -0,0 +1,15 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.search.engine.search.reference;
+
+import org.hibernate.search.engine.common.EntityReference;
+import org.hibernate.search.engine.mapper.scope.SearchScope;
+import org.hibernate.search.engine.mapper.scope.SearchScopeProvider;
+
+public interface RootReferenceScope<SR, T> {
+	Class<SR> rootReferenceType();
+
+	<ER extends EntityReference, S extends SearchScope<T, ER>, P extends SearchScopeProvider<ER>> S create(P scopeProvider);
+}
