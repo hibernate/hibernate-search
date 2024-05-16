77c2a7bdc1c88006d2ae2c2894cd3f3d3d93772a
marko-bekhta <marko.prykladna@gmail.com>
2024-05-16 16:05:50 +0200

    HSEARCH-3319 Use mapper specific root-scope-creators

--- engine/src/main/java/org/hibernate/search/engine/search/reference/RootReferenceScope.java
+++ engine/src/main/java/org/hibernate/search/engine/search/reference/RootReferenceScope.java
@@ -4,12 +4,9 @@
  */
 package org.hibernate.search.engine.search.reference;
 
-import org.hibernate.search.engine.common.EntityReference;
-import org.hibernate.search.engine.mapper.scope.SearchScope;
-import org.hibernate.search.engine.mapper.scope.SearchScopeProvider;
+import org.hibernate.search.util.common.annotation.Incubating;
 
+@Incubating
 public interface RootReferenceScope<SR, T> {
 	Class<SR> rootReferenceType();
-
-	<ER extends EntityReference, S extends SearchScope<SR, T, ER>, P extends SearchScopeProvider<ER>> S create(P scopeProvider);
 }
