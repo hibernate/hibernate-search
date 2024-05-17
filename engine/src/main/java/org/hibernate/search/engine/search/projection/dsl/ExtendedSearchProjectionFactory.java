e4050165eadbe426994736bab327a5058a7611fd
marko-bekhta <marko.prykladna@gmail.com>
2024-05-17 18:47:24 +0200

    HSEARCH-3319 Remove now unnecessary withRoot(Reference)

--- engine/src/main/java/org/hibernate/search/engine/search/projection/dsl/ExtendedSearchProjectionFactory.java
+++ engine/src/main/java/org/hibernate/search/engine/search/projection/dsl/ExtendedSearchProjectionFactory.java
@@ -6,8 +6,6 @@
  */
 package org.hibernate.search.engine.search.projection.dsl;
 
-import org.hibernate.search.engine.search.reference.object.ObjectFieldReference;
-
 /**
  * A base interface for subtypes of {@link SearchProjectionFactory} allowing to
  * easily override the self type for all relevant methods.
@@ -24,9 +22,4 @@ public interface ExtendedSearchProjectionFactory<SR, S extends ExtendedSearchPro
 	@Override
 	S withRoot(String objectFieldPath);
 
-
-	@Override
-	default S withRoot(ObjectFieldReference<? super SR> objectFieldReference) {
-		return withRoot( objectFieldReference.absolutePath() );
-	}
 }
