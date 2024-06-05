0a69bbff1f4df3d96ac7d1051c99eab87ffd8c95
marko-bekhta <marko.prykladna@gmail.com>
2024-06-05 12:08:00 +0200

    HSEARCH-5156 Update code source resource resolution

--- util/common/src/test/java/org/hibernate/search/util/common/jar/impl/CodeSourceTest.java
+++ util/common/src/test/java/org/hibernate/search/util/common/jar/impl/CodeSourceTest.java
@@ -196,7 +196,7 @@ class CodeSourceTest {
 
 		try ( JarFile outerJar = new JarFile( jarPath.toFile() ) ) {
 			@SuppressWarnings("deprecation") // For JDK 20+
-			URL innerJarURL = JarUrl.create( jarPath.toFile(), classesDirRelativeString );
+			URL innerJarURL = JarUrl.create( jarPath.toFile(), null, classesDirRelativeString );
 			try ( URLClassLoader isolatedClassLoader = createIsolatedClassLoader( innerJarURL ) ) {
 				Class<?> classInIsolatedClassLoader = isolatedClassLoader.loadClass( SimpleClass.class.getName() );
 
