5a7aaf231ea6e8f54fe788b19cadbb8d3a65b25c
Jenkins <ci@hibernate.org>
2024-08-07 06:47:19 +0000

    [Jenkins release job] Preparing next development iteration

--- mapper/orm-outbox-polling/src/main/avro/generated/org/hibernate/search/mapper/orm/outboxpolling/avro/generated/impl/DocumentRouteDescriptorDto.java
+++ mapper/orm-outbox-polling/src/main/avro/generated/org/hibernate/search/mapper/orm/outboxpolling/avro/generated/impl/DocumentRouteDescriptorDto.java
@@ -5,7 +5,6 @@
  */
 package org.hibernate.search.mapper.orm.outboxpolling.avro.generated.impl;
 
-import org.apache.avro.generic.GenericArray;
 import org.apache.avro.specific.SpecificData;
 import org.apache.avro.util.Utf8;
 import org.apache.avro.message.BinaryMessageEncoder;
