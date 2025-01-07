8f1a2739765e2a4dd88bb32d33aed1ac991c3b78
marko-bekhta <marko.prykladna@gmail.com>
2025-01-07 12:44:25 +0100

    HSEARCH-5298 Use a UserType instead of an attribute converter for an ISBN ID

---
+++ documentation/src/test/java/org/hibernate/search/documentation/testsupport/data/ISBNUserType.java
@@ -0,0 +1,56 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.search.documentation.testsupport.data;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+
+import org.hibernate.type.descriptor.WrapperOptions;
+import org.hibernate.usertype.UserType;
+
+/**
+ * In some cases, e.g. with {@link jakarta.persistence.Id}, an {@link jakarta.persistence.AttributeConverter attribute converter}
+ * cannot be used, then we'll rely on a user type instead.
+ */
+public class ISBNUserType implements UserType<ISBN> {
+
+	@Override
+	public int getSqlType() {
+		return Types.VARCHAR;
+	}
+
+	@Override
+	public Class<ISBN> returnedClass() {
+		return ISBN.class;
+	}
+
+	@Override
+	public ISBN deepCopy(ISBN value) {
+		return value == null ? null : ISBN.parse( value.getStringValue() );
+	}
+
+	@Override
+	public boolean isMutable() {
+		return false;
+	}
+
+	@Override
+	public ISBN nullSafeGet(ResultSet rs, int position, WrapperOptions options) throws SQLException {
+		String result = rs.getString( position );
+		return rs.wasNull() ? null : ISBN.parse( result );
+	}
+
+	@Override
+	public void nullSafeSet(PreparedStatement st, ISBN value, int position, WrapperOptions options) throws SQLException {
+		if ( value == null ) {
+			st.setNull( position, getSqlType() );
+		}
+		else {
+			st.setString( position, value.getStringValue() );
+		}
+	}
+}
