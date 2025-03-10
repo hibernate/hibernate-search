266644f4035837c51f56605f7d0c8216111a8cc8
marko-bekhta <marko.prykladna@gmail.com>
2025-03-10 16:40:41 +0100

    HSEARCH-3319 More field references

--- engine/src/main/java/org/hibernate/search/engine/search/aggregation/dsl/SumAggregationFieldStep.java
+++ engine/src/main/java/org/hibernate/search/engine/search/aggregation/dsl/SumAggregationFieldStep.java
@@ -8,6 +8,8 @@ import java.util.function.Function;
 
 import org.hibernate.search.engine.search.common.ValueModel;
 import org.hibernate.search.engine.search.predicate.dsl.SearchPredicateFactory;
+import org.hibernate.search.engine.search.reference.aggregation.MaxAggregationFieldReference;
+import org.hibernate.search.engine.search.reference.aggregation.SumAggregationFieldReference;
 import org.hibernate.search.util.common.annotation.Incubating;
 
 /**
@@ -43,4 +45,15 @@ public interface SumAggregationFieldStep<SR, PDF extends SearchPredicateFactory<
 	 */
 	<F> SumAggregationOptionsStep<SR, ?, PDF, F> field(String fieldPath, Class<F> type, ValueModel valueModel);
 
+	/**
+	 * Target the given field in the avg aggregation.
+	 *
+	 * @param reference The field reference representing a <a href="SearchAggregationFactory.html#field-paths">path</a> to the index field to aggregate.
+	 * @param <F> The type of field values.
+	 * @return The next step.
+	 */
+	@Incubating
+	default <F> SumAggregationOptionsStep<SR, ?, PDF, F> field(SumAggregationFieldReference<SR, F> reference) {
+		return field( reference.absolutePath(), reference.aggregationType(), reference.valueModel() );
+	}
 }
---
+++ engine/src/main/java/org/hibernate/search/engine/search/reference/aggregation/MaxAggregationFieldReference.java
@@ -0,0 +1,8 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.search.engine.search.reference.aggregation;
+
+public interface MaxAggregationFieldReference<SR, T> extends TypedAggregationFieldReference<SR, T> {
+}
