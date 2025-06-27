889f1fd7c84dc5da6a93c464950c978cf64caf37
marko-bekhta <marko.prykladna@gmail.com>
2025-06-27 16:26:31 +0200

    HSEARCH-3661 Do not do rollup for range aggregation

--- backend/lucene/src/main/java/org/hibernate/search/backend/lucene/lowlevel/collector/impl/RangeCollector.java
+++ backend/lucene/src/main/java/org/hibernate/search/backend/lucene/lowlevel/collector/impl/RangeCollector.java
@@ -27,12 +27,9 @@ import org.apache.lucene.search.SimpleCollector;
 public class RangeCollector extends SimpleCollector {
 
 	private final LongMultiValuesSource valuesSource;
-	private final LongRangeNode root;
 	private final long[] boundaries;
-	private final long[] countsPerBoundaries;
+	private final IntArrayList[] countsPerBoundaries;
 
-	private int leafUpto;
-	private boolean filled = false;
 	private final long[] counts;
 
 	private LongMultiValues values;
@@ -73,12 +70,12 @@ public class RangeCollector extends SimpleCollector {
 		Arrays.sort( endsList.buffer, 0, endsList.elementsCount );
 
 		// Build elementaryIntervals (a 1D Venn diagram):
-		List<InclusiveRange> elementaryIntervals = new ArrayList<>();
+		List<EffectiveRange> elementaryIntervals = new ArrayList<>();
 		int upto0 = 1;
 		long v = endsList.get( 0 );
 		long prev;
 		if ( endsMap.get( v ) == 3 ) {
-			elementaryIntervals.add( new InclusiveRange( v, v ) );
+			elementaryIntervals.add( new EffectiveRange( v, v ) );
 			prev = v + 1;
 		}
 		else {
@@ -92,16 +89,16 @@ public class RangeCollector extends SimpleCollector {
 				// This point is both an end and a start; we need to
 				// separate it:
 				if ( v > prev ) {
-					elementaryIntervals.add( new InclusiveRange( prev, v - 1 ) );
+					elementaryIntervals.add( new EffectiveRange( prev, v - 1 ) );
 				}
-				elementaryIntervals.add( new InclusiveRange( v, v ) );
+				elementaryIntervals.add( new EffectiveRange( v, v ) );
 				prev = v + 1;
 			}
 			else if ( flags == 1 ) {
 				// This point is only the start of an interval;
 				// attach it to next interval:
 				if ( v > prev ) {
-					elementaryIntervals.add( new InclusiveRange( prev, v - 1 ) );
+					elementaryIntervals.add( new EffectiveRange( prev, v - 1 ) );
 				}
 				prev = v;
 			}
@@ -109,33 +106,34 @@ public class RangeCollector extends SimpleCollector {
 				assert flags == 2;
 				// This point is only the end of an interval; attach
 				// it to last interval:
-				elementaryIntervals.add( new InclusiveRange( prev, v ) );
+				elementaryIntervals.add( new EffectiveRange( prev, v ) );
 				prev = v + 1;
 			}
 			upto0++;
 		}
 
-		// Build binary tree on top of intervals:
-		root = split( 0, elementaryIntervals.size(), elementaryIntervals );
-
-		// Set outputs, so we know which range to output for
-		// each node in the tree:
-		for ( int i = 0; i < ranges.length; i++ ) {
-			root.addOutputs( i, ranges[i] );
-		}
-
 		// Set boundaries (ends of each elementary interval):
 		boundaries = new long[elementaryIntervals.size()];
+		countsPerBoundaries = new IntArrayList[boundaries.length];
 		for ( int i = 0; i < boundaries.length; i++ ) {
-			boundaries[i] = elementaryIntervals.get( i ).end;
+			EffectiveRange interval = elementaryIntervals.get( i );
+			boundaries[i] = interval.max();
+			IntArrayList list = new IntArrayList();
+			countsPerBoundaries[i] = list;
+			for ( int j = 0; j < ranges.length; j++ ) {
+				if ( interval.min() >= ranges[j].min() && interval.max() <= ranges[j].max() ) {
+					list.add( j );
+				}
+			}
 		}
 
-		countsPerBoundaries = new long[boundaries.length];
 		counts = new long[ranges.length];
 	}
 
 	private void incrementCountForLeafWithIndex(int index) {
-		countsPerBoundaries[index]++;
+		for ( IntCursor cursor : countsPerBoundaries[index] ) {
+			counts[cursor.value]++;
+		}
 	}
 
 	private int findLeafIndex(long v) {
@@ -163,133 +161,6 @@ public class RangeCollector extends SimpleCollector {
 		}
 	}
 
-	/** Fills counts corresponding to the original input
-	 * ranges, returning the missing count (how many hits
-	 * didn't match any ranges). */
-	private void fillCounts(long[] counts) {
-		leafUpto = 0;
-		rollup( root, counts, false );
-	}
-
-	private long rollup(LongRangeNode node, long[] counts, boolean sawOutputs) {
-		long count;
-		sawOutputs |= node.outputs != null;
-		if ( node.left != null ) {
-			count = rollup( node.left, counts, sawOutputs );
-			count += rollup( node.right, counts, sawOutputs );
-		}
-		else {
-			// Leaf:
-			count = countsPerBoundaries[leafUpto];
-			leafUpto++;
-		}
-		if ( node.outputs != null ) {
-			for ( IntCursor rangeIndexCursor : node.outputs ) {
-				counts[rangeIndexCursor.value] += count;
-			}
-		}
-		return count;
-	}
-
-	private static LongRangeNode split(int start, int end, List<InclusiveRange> elementaryIntervals) {
-		if ( start == end - 1 ) {
-			// leaf
-			InclusiveRange range = elementaryIntervals.get( start );
-			return new LongRangeNode( range.start, range.end, null, null );
-		}
-		else {
-			int mid = ( start + end ) >>> 1;
-			LongRangeNode left = split( start, mid, elementaryIntervals );
-			LongRangeNode right = split( mid, end, elementaryIntervals );
-			return new LongRangeNode( left.start, right.end, left, right );
-		}
-	}
-
-	private record InclusiveRange(long start, long end) {
-		private InclusiveRange {
-			assert end >= start;
-		}
-
-		@Override
-		public String toString() {
-			return start + " to " + end;
-		}
-	}
-
-	/** Holds one node of the segment tree. */
-	private static class LongRangeNode {
-		private final LongRangeNode left;
-		private final LongRangeNode right;
-
-		// Our range, inclusive:
-		private final long start;
-		private final long end;
-
-		// Which range indices to output when a query goes
-		// through this node:
-		IntArrayList outputs;
-
-		public LongRangeNode(long start, long end, LongRangeNode left, LongRangeNode right) {
-			this.start = start;
-			this.end = end;
-			this.left = left;
-			this.right = right;
-		}
-
-		@Override
-		public String toString() {
-			StringBuilder sb = new StringBuilder();
-			toString( sb, 0 );
-			return sb.toString();
-		}
-
-		static void indent(StringBuilder sb, int depth) {
-			for ( int i = 0; i < depth; i++ ) {
-				sb.append( "  " );
-			}
-		}
-
-		/** Recursively assigns range outputs to each node. */
-		void addOutputs(int index, EffectiveRange range) {
-			if ( start >= range.min() && end <= range.max() ) {
-				// Our range is fully included in the incoming
-				// range; add to our output list:
-				if ( outputs == null ) {
-					outputs = new IntArrayList();
-				}
-				outputs.add( index );
-			}
-			else if ( left != null ) {
-				assert right != null;
-				// Recurse:
-				left.addOutputs( index, range );
-				right.addOutputs( index, range );
-			}
-		}
-
-		void toString(StringBuilder sb, int depth) {
-			indent( sb, depth );
-			if ( left == null ) {
-				assert right == null;
-				sb.append( "leaf: " ).append( start ).append( " to " ).append( end );
-			}
-			else {
-				sb.append( "node: " ).append( start ).append( " to " ).append( end );
-			}
-			if ( outputs != null ) {
-				sb.append( " outputs=" );
-				sb.append( outputs );
-			}
-			sb.append( '\n' );
-
-			if ( left != null ) {
-				assert right != null;
-				left.toString( sb, depth + 1 );
-				right.toString( sb, depth + 1 );
-			}
-		}
-	}
-
 	@Override
 	public void collect(int doc) throws IOException {
 		if ( values.advanceExact( doc ) ) {
@@ -305,10 +176,6 @@ public class RangeCollector extends SimpleCollector {
 	}
 
 	public long[] counts() {
-		if ( !filled ) {
-			filled = true;
-			fillCounts( counts );
-		}
 		return counts;
 	}
 
