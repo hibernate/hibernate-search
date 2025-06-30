75be8ac4a72b4d2bd919d2a607224777fb77ec82
marko-bekhta <marko.prykladna@gmail.com>
2025-06-30 16:04:54 +0200

    HSEARCH-3661 WIP making aggregation value work

--- backend/lucene/src/main/java/org/hibernate/search/backend/lucene/search/aggregation/impl/AggregationExtractContext.java
+++ backend/lucene/src/main/java/org/hibernate/search/backend/lucene/search/aggregation/impl/AggregationExtractContext.java
@@ -4,63 +4,23 @@
  */
 package org.hibernate.search.backend.lucene.search.aggregation.impl;
 
-import java.util.Set;
-
 import org.hibernate.search.backend.lucene.lowlevel.collector.impl.CollectorKey;
 import org.hibernate.search.backend.lucene.lowlevel.join.impl.NestedDocsProvider;
-import org.hibernate.search.backend.lucene.search.extraction.impl.HibernateSearchMultiCollectorManager;
 import org.hibernate.search.backend.lucene.search.predicate.impl.PredicateRequestContext;
-import org.hibernate.search.backend.lucene.search.query.impl.LuceneSearchQueryIndexScope;
-import org.hibernate.search.engine.backend.session.spi.BackendSessionContext;
 import org.hibernate.search.engine.backend.types.converter.runtime.FromDocumentValueConvertContext;
-import org.hibernate.search.engine.search.query.spi.QueryParameters;
 
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.search.Collector;
 import org.apache.lucene.search.Query;
 
-public class AggregationExtractContext {
+public interface AggregationExtractContext {
+	PredicateRequestContext toPredicateRequestContext(String absolutePath);
 
-	private final LuceneSearchQueryIndexScope<?, ?> queryIndexScope;
-	private final BackendSessionContext sessionContext;
-	private final IndexReader indexReader;
-	private final FromDocumentValueConvertContext fromDocumentValueConvertContext;
-	private final HibernateSearchMultiCollectorManager.MultiCollectedResults multiCollectedResults;
-	private final Set<String> routingKeys;
-	private final QueryParameters parameters;
+	IndexReader getIndexReader();
 
-	public AggregationExtractContext(LuceneSearchQueryIndexScope<?, ?> queryIndexScope, BackendSessionContext sessionContext,
-			IndexReader indexReader,
-			FromDocumentValueConvertContext fromDocumentValueConvertContext,
-			HibernateSearchMultiCollectorManager.MultiCollectedResults multiCollectedResults, Set<String> routingKeys,
-			QueryParameters parameters) {
-		this.queryIndexScope = queryIndexScope;
-		this.sessionContext = sessionContext;
-		this.indexReader = indexReader;
-		this.fromDocumentValueConvertContext = fromDocumentValueConvertContext;
-		this.multiCollectedResults = multiCollectedResults;
-		this.routingKeys = routingKeys;
-		this.parameters = parameters;
-	}
+	FromDocumentValueConvertContext fromDocumentValueConvertContext();
 
-	public PredicateRequestContext toPredicateRequestContext(String absolutePath) {
-		return PredicateRequestContext.withSession( queryIndexScope, sessionContext, routingKeys, parameters )
-				.withNestedPath( absolutePath );
-	}
+	<C extends Collector, T> T getCollectorResults(CollectorKey<C, T> key);
 
-	public IndexReader getIndexReader() {
-		return indexReader;
-	}
-
-	public FromDocumentValueConvertContext fromDocumentValueConvertContext() {
-		return fromDocumentValueConvertContext;
-	}
-
-	public <C extends Collector, T> T getCollectorResults(CollectorKey<C, T> key) {
-		return multiCollectedResults.get( key );
-	}
-
-	public NestedDocsProvider createNestedDocsProvider(String nestedDocumentPath, Query nestedFilter) {
-		return new NestedDocsProvider( nestedDocumentPath, nestedFilter );
-	}
+	NestedDocsProvider createNestedDocsProvider(String nestedDocumentPath, Query nestedFilter);
 }
--- engine/src/main/java/org/hibernate/search/engine/search/aggregation/spi/RangeAggregationBuilder.java
+++ engine/src/main/java/org/hibernate/search/engine/search/aggregation/spi/RangeAggregationBuilder.java
@@ -6,17 +6,20 @@ package org.hibernate.search.engine.search.aggregation.spi;
 
 import java.util.Map;
 
+import org.hibernate.search.engine.search.aggregation.SearchAggregation;
 import org.hibernate.search.engine.search.common.ValueModel;
 import org.hibernate.search.engine.search.predicate.SearchPredicate;
 import org.hibernate.search.util.common.data.Range;
 
-public interface RangeAggregationBuilder<K> extends SearchAggregationBuilder<Map<Range<K>, Long>> {
+public interface RangeAggregationBuilder<K, A> extends SearchAggregationBuilder<Map<Range<K>, A>> {
 
 	interface TypeSelector {
-		<K> RangeAggregationBuilder<K> type(Class<K> expectedType, ValueModel valueModel);
+		<K> RangeAggregationBuilder<K, Long> type(Class<K> expectedType, ValueModel valueModel);
 	}
 
 	void filter(SearchPredicate filter);
 
 	void range(Range<? extends K> range);
+
+	<T> RangeAggregationBuilder<K, T> withValue(SearchAggregation<T> aggregation);
 }
