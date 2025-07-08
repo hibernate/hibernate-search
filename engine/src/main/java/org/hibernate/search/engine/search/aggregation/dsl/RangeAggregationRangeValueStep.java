3b120f920d96619335c837a50b79bed7b08fb6d0
marko-bekhta <marko.prykladna@gmail.com>
2025-07-08 18:12:23 +0200

    HSEARCH-3661 Document range/terms .values(..)

--- engine/src/main/java/org/hibernate/search/engine/search/aggregation/dsl/RangeAggregationRangeValueStep.java
+++ engine/src/main/java/org/hibernate/search/engine/search/aggregation/dsl/RangeAggregationRangeValueStep.java
@@ -9,6 +9,7 @@ import java.util.function.Function;
 
 import org.hibernate.search.engine.search.aggregation.SearchAggregation;
 import org.hibernate.search.engine.search.predicate.dsl.TypedSearchPredicateFactory;
+import org.hibernate.search.util.common.annotation.Incubating;
 import org.hibernate.search.util.common.data.Range;
 
 /**
@@ -18,13 +19,35 @@ import org.hibernate.search.util.common.data.Range;
  * @param <PDF> The type of factory used to create predicates in {@link RangeAggregationOptionsStep#filter(Function)}.
  * @param <F> The type of the targeted field.
  */
+@Incubating
 public interface RangeAggregationRangeValueStep<
 		SR,
 		PDF extends TypedSearchPredicateFactory<SR>,
 		F> {
-
+	/**
+	 * Specify which aggregation to apply to the documents within the range.
+	 * <p>
+	 * This allows to "group" the documents by "ranges" and then apply one of the aggregations from {@link SearchAggregationFactory}
+	 * to the documents in that group.
+	 *
+	 * @param aggregation The aggregation to apply to the documents within each range.
+	 * @return The next step in range aggregation definition.
+	 * @param <T> The type of the aggregated results within a range.
+	 */
+	@Incubating
 	<T> RangeAggregationOptionsStep<SR, ?, PDF, F, Map<Range<F>, T>> value(SearchAggregation<T> aggregation);
 
+	/**
+	 * Specify which aggregation to apply to the documents within the range.
+	 * <p>
+	 * This allows to "group" the documents by "ranges" and then apply one of the aggregations from {@link SearchAggregationFactory}
+	 * to the documents in that group.
+	 *
+	 * @param aggregation The aggregation to apply to the documents within each range.
+	 * @return The next step in range aggregation definition.
+	 * @param <T> The type of the aggregated results within a range.
+	 */
+	@Incubating
 	default <T> RangeAggregationOptionsStep<SR, ?, PDF, F, Map<Range<F>, T>> value(AggregationFinalStep<T> aggregation) {
 		return value( aggregation.toAggregation() );
 	}
