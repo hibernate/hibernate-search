ddfd51e36ba219b514bed6ab4bb337cc7928dc15
marko-bekhta <marko.prykladna@gmail.com>
2025-07-18 17:40:27 +0200

    HSEARCH-5423 Introduce search method to reference scopes

--- mapper/orm/src/main/java/org/hibernate/search/mapper/orm/scope/HibernateOrmRootReferenceScope.java
+++ mapper/orm/src/main/java/org/hibernate/search/mapper/orm/scope/HibernateOrmRootReferenceScope.java
@@ -4,11 +4,19 @@
  */
 package org.hibernate.search.mapper.orm.scope;
 
+import org.hibernate.search.engine.search.query.dsl.SearchQuerySelectStep;
 import org.hibernate.search.engine.search.reference.RootReferenceScope;
+import org.hibernate.search.mapper.orm.session.SearchSession;
 import org.hibernate.search.util.common.annotation.Incubating;
 
 @Incubating
 public interface HibernateOrmRootReferenceScope<SR, T> extends RootReferenceScope<SR, T> {
 
 	TypedSearchScope<SR, T> scope(SearchScopeProvider scopeProvider);
+
+	@SuppressWarnings("deprecation")
+	default SearchQuerySelectStep<SR, ?, org.hibernate.search.mapper.orm.common.EntityReference, T, ?, ?, ?> search(
+			SearchSession session) {
+		return session.search( scope( session ) );
+	}
 }
