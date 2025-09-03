98be8f213cde5b11434bf7196f81dfee38c72c19
marko-bekhta <marko.prykladna@gmail.com>
2025-09-03 19:10:32 +0200

    HSEARCH-5464 OpenSearch client

---
+++ backend/elasticsearch-client/opensearch-rest-client/src/main/java/org/hibernate/search/backend/elasticsearch/client/elasticsearch/lowlevel/impl/ElasticsearchHttpClientConfigurationContextImpl.java
@@ -0,0 +1,43 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.search.backend.elasticsearch.client.elasticsearch.lowlevel.impl;
+
+import org.hibernate.search.backend.elasticsearch.client.elasticsearch.lowlevel.ElasticsearchHttpClientConfigurationContext;
+import org.hibernate.search.engine.cfg.ConfigurationPropertySource;
+import org.hibernate.search.engine.environment.bean.BeanResolver;
+
+import org.apache.hc.client5.http.impl.async.HttpAsyncClientBuilder;
+
+final class ElasticsearchHttpClientConfigurationContextImpl
+		implements ElasticsearchHttpClientConfigurationContext {
+	private final BeanResolver beanResolver;
+	private final ConfigurationPropertySource configurationPropertySource;
+	private final HttpAsyncClientBuilder clientBuilder;
+
+	ElasticsearchHttpClientConfigurationContextImpl(
+			BeanResolver beanResolver,
+			ConfigurationPropertySource configurationPropertySource,
+			HttpAsyncClientBuilder clientBuilder) {
+		this.beanResolver = beanResolver;
+		this.configurationPropertySource = configurationPropertySource;
+		this.clientBuilder = clientBuilder;
+	}
+
+	@Override
+	public BeanResolver beanResolver() {
+		return beanResolver;
+	}
+
+	@Override
+	public ConfigurationPropertySource configurationPropertySource() {
+		return configurationPropertySource;
+	}
+
+	@Override
+	public HttpAsyncClientBuilder clientBuilder() {
+		return clientBuilder;
+	}
+
+}
