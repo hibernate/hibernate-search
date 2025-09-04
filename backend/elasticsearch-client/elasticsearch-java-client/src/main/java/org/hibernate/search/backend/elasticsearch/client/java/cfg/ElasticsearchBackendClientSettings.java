2f0ef999972cbf0aeff2a381cbd45e5bdf561ce5
marko-bekhta <marko.prykladna@gmail.com>
2025-09-04 18:25:54 +0200

    HSEARCH-5464 Rename the clients

---
+++ backend/elasticsearch-client/elasticsearch-java-client/src/main/java/org/hibernate/search/backend/elasticsearch/client/java/cfg/ElasticsearchBackendClientSettings.java
@@ -0,0 +1,43 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright Red Hat Inc. and Hibernate Authors
+ */
+package org.hibernate.search.backend.elasticsearch.client.java.cfg;
+
+import org.hibernate.search.backend.elasticsearch.client.common.cfg.ElasticsearchBackendClientCommonSettings;
+import org.hibernate.search.backend.elasticsearch.client.java.ElasticsearchHttpClientConfigurer;
+
+/**
+ * Specific configuration properties for the Elasticsearch backend's rest client based on the Elasticsearch's low-level rest client.
+ * <p>
+ * Constants in this class are to be appended to a prefix to form a property key;
+ * see {@link org.hibernate.search.engine.cfg.BackendSettings} for details.
+ *
+ * @author Gunnar Morling
+ */
+public final class ElasticsearchBackendClientSettings {
+
+	private ElasticsearchBackendClientSettings() {
+	}
+
+	/**
+	* A {@link ElasticsearchHttpClientConfigurer} that defines custom HTTP client configuration.
+	* <p>
+	* It can be used for example to tune the SSL context to accept self-signed certificates.
+	* It allows overriding other HTTP client settings, such as {@link ElasticsearchBackendClientCommonSettings#USERNAME} or {@link ElasticsearchBackendClientCommonSettings#MAX_CONNECTIONS_PER_ROUTE}.
+	* <p>
+	* Expects a reference to a bean of type {@link ElasticsearchHttpClientConfigurer}.
+	* <p>
+	* Defaults to no value.
+	*/
+	public static final String CLIENT_CONFIGURER = "client.configurer";
+
+	/**
+	 * Default values for the different settings if no values are given.
+	 */
+	public static final class Defaults {
+
+		private Defaults() {
+		}
+	}
+}
