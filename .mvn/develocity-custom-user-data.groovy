6674e6987fbd5a041b1a15092152f942300c518a
marko-bekhta <marko.prykladna@gmail.com>
2026-04-03 17:47:30 +0200

    HSEARCH-5595 Switch to the newer hibernate-develocity-maven-extension version that handles the userdata for checkstyle

--- .mvn/develocity-custom-user-data.groovy
+++
@@ -1,9 +0,0 @@
-buildCache.registerMojoMetadataProvider(context -> {
-    context.withPlugin("maven-checkstyle-plugin", () -> {
-        context.inputs(inputs -> {
-            inputs.fileSet("testSourceDirectories", fileSet ->
-                fileSet.excludes("**/target/**")
-            )
-        })
-    })
-})
