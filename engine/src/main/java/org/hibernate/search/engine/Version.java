b01221c9ea98875a51db4fd15ca0ca9ae25e71de
marko-bekhta <marko.prykladna@gmail.com>
2026-06-15 22:28:01 +0200

    HSEARCH-5648 Remove Version.getVersionString()

--- engine/src/main/java/org/hibernate/search/engine/Version.java
+++ engine/src/main/java/org/hibernate/search/engine/Version.java
@@ -4,8 +4,6 @@
  */
 package org.hibernate.search.engine;
 
-import org.hibernate.search.util.common.annotation.Search5DeprecatedAPI;
-
 /**
  * @author Emmanuel Bernard
  * @author Hardy Ferentschik
@@ -22,16 +20,6 @@ public final class Version {
 	public static String versionString() {
 		// This implementation is replaced during the build with another one that returns the correct value.
 		return "UNKNOWN";
-	}
-
-	/**
-	 * @return A string representation of the version of Hibernate Search.
-	 * @deprecated Use {@link #versionString()} instead.
-	 */
-	@Deprecated(since = "6.0")
-	@Search5DeprecatedAPI
-	public static String getVersionString() {
-		return versionString();
 	}
 
 }
